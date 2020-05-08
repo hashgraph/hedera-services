4af568aa749fe9fba384a5c0509c49ba53de25e8
ljianghedera <leo.jiang@hedera.com>
2020-05-08 14:34:35 -0500

    See what git clone command that runs.

--- .circleci/scripts/clone-infra-repo.sh
+++ .circleci/scripts/clone-infra-repo.sh
@@ -14,6 +14,7 @@ else
   ci_echo "Using '$BRANCH' of infrastructure repo..."
   cd /
   cat ~/.ssh/config
+  set -x
   GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     git clone git@github.com:swirlds${INFRASTRUCTURE_REPO} \
       --branch "$BRANCH"
