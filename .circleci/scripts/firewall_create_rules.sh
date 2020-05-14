18edad5790d0d5ec8efc7716b3b7020622022152
Jeffrey Tang <jeffrey@swirlds.com>
2020-05-14 16:08:30 -0500

    fix scp
    Signed-off-by: Jeffrey Tang <jeffrey@swirlds.com>
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/firewall_create_rules.sh
+++ .circleci/scripts/firewall_create_rules.sh
@@ -6,9 +6,14 @@
 function firewall_creat_rules {
   for HOST in ${TF_HOSTS[@]}; do
     echo "------- firewall_creat_rules --------- "
+
+    #copy script to remote instances
+    scp -p -o StrictHostKeyChecking=no ${REPO}/.circleci/scripts/block_ubuntu.sh ubuntu@$HOST:$HAPI_APP_DIR/
+    ssh -o StrictHostKeyChecking=no ubuntu@$HOST "cd $HAPI_APP_DIR; ls -ltr"
+
     # invoke backgroun script 
     ssh -o StrictHostKeyChecking=no ubuntu@$HOST "nohup ./block_ubuntu.sh &"
-
+    sleep 5
     ssh -o StrictHostKeyChecking=no ubuntu@$HOST "sudo ps -ef |grep block "
   done
 }
