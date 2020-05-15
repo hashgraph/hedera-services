764b3c25d199cf606e5ca49b519304123c85404d
Jeffrey Tang <jeffrey@swirlds.com>
2020-05-15 16:58:10 -0500

    block last node
    Signed-off-by: Jeffrey Tang <jeffrey@swirlds.com>

--- .circleci/scripts/firewall_create_rules.sh
+++ .circleci/scripts/firewall_create_rules.sh
@@ -4,8 +4,11 @@
 . /repo/.circleci/scripts/utils.sh
 
 function firewall_creat_rules {
-  for HOST in ${TF_HOSTS[@]}; do
-    echo "------- firewall_creat_rules --------- "
+
+    # running block script at the last node
+    HOST=${TF_HOSTS[${#TF_HOSTS[@]}-1]}
+
+    echo "------- firewall_creat_rules $HOST --------- "
 
     #copy script to remote instances
     scp -p -o StrictHostKeyChecking=no ${REPO}/.circleci/scripts/block_ubuntu.sh ubuntu@$HOST:$HAPI_APP_DIR/
@@ -19,7 +22,6 @@ function firewall_creat_rules {
     
     # only run block port on the first node
     break
-  done
 }
 
 firewall_creat_rules
