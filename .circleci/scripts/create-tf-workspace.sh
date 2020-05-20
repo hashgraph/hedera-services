b97862946abe42674b43825820361a319140e2ac
Quan Nguyen <quannguyen@swirlds.com>
2020-05-20 17:42:01 -0500

    Blank out the ami_id to test default value.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/create-tf-workspace.sh
+++ .circleci/scripts/create-tf-workspace.sh
@@ -4,7 +4,7 @@ NUM_HOSTS=${1:-4}
 LIVENESS_TIMEOUT_SECS=${2:-120}
 VAR_FILE=${3:-'ci.tfvars'}
 AWS_REGION=${4:-'us-east-1'}
-AWS_AMI_ID=${5:-'ami-0578b0bee3eb885c6'}
+AWS_AMI_ID=${5:-''}
 
 . ${REPO}/.circleci/scripts/terraform-functions.sh
 
