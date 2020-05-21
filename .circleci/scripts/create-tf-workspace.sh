50ae02014d33f43b89e74cb29189a46fca1707cd
Quan Nguyen <quannguyen@swirlds.com>
2020-05-20 20:44:33 -0500

    Escape the quotes. Print out more debug.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/create-tf-workspace.sh
+++ .circleci/scripts/create-tf-workspace.sh
@@ -5,8 +5,8 @@ LIVENESS_TIMEOUT_SECS=${2:-120}
 VAR_FILE=${3:-'ci.tfvars'}
 AWS_REGION=${4:-'us-east-1'}
 AWS_AMI_ID=${5:-''}
+echo $AWS_AMI_ID
 
 . ${REPO}/.circleci/scripts/terraform-functions.sh
-
 
 tf_provision $NUM_HOSTS $LIVENESS_TIMEOUT_SECS $VAR_FILE $AWS_REGION $AWS_AMI_ID
