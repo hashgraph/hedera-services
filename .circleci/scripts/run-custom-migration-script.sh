432bf25c532763d9e9392dbb4e40bdc0affb497e
Anirudh Ghanta <anirudh.ghanta@hedera.com>
2020-06-11 07:25:35 +0530

    added boto3 installation
    Signed-off-by: Anirudh Ghanta <anirudh.ghanta@hedera.com>

--- .circleci/scripts/run-custom-migration-script.sh
+++ .circleci/scripts/run-custom-migration-script.sh
@@ -5,4 +5,6 @@ cd $REPO
 
 CONFIG_FILE_PATH='${REPO}/.circleci/scripts/resources/customMigrationConfig.json'
 
+pip install boto
+pip install boto3
 python3 ${REPO}/.circleci/scripts/automateCustomMigration.py  ${CONFIG_FILE_PATH}
