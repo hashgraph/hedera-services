95cef2309aa7be02710c64a8c32446a13638c96e
Quan Nguyen <quannguyen@swirlds.com>
2020-06-11 11:16:20 -0500

    Show current CircleCI stage when workflow name is not available.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/prepare-slack-message.sh
+++ .circleci/scripts/prepare-slack-message.sh
@@ -4,13 +4,12 @@
 
 mkdir -p $DIAG_DIR
 
-WORKFLOW_DESC="<n/a>"
-if [[ -n "$1" ]]; then
-  WORKFLOW_DESC="$1"
-fi
-
 LINK_TXT="CircleCI Job #${CIRCLE_BUILD_NUM}"
 echo "<${CIRCLE_BUILD_URL}|${LINK_TXT}>" > $SLACK_MSG_FILE
 echo "Branch: ${CIRCLE_BRANCH}" >> $SLACK_MSG_FILE
 echo "Commit ID: ${CIRCLE_SHA1}" >> $SLACK_MSG_FILE
-echo "Workflow: ${WORKFLOW_DESC}" >> $SLACK_MSG_FILE
+if [[ -n "$1" ]]; then
+  echo "Workflow: $1" >> $SLACK_MSG_FILE
+else
+  echo "Stage: ${CIRCLE_STAGE}" >> $SLACK_MSG_FILE
+fi
