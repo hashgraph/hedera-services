beb905bac299c4f44779964297af1157d7ea6168
Anirudh Ghanta <anirudh.ghanta@hedera.com>
2020-06-16 00:04:36 +0530

    justmigration
    Signed-off-by: Anirudh Ghanta <anirudh.ghanta@hedera.com>

--- .circleci/scripts/automateCustomMigration.py
+++ .circleci/scripts/automateCustomMigration.py
@@ -318,7 +318,7 @@ def validateLogs():
 	for n in range(0, NO_OF_NODES):
 		loaded_log = "SwirldsPlatform - Platform {} has loaded a saved state for round".format(n)
 		with open( "/repo/output/{}/swirlds.log".format(n)) as swirldsLog_f:
-			print(swirldsLog_f)
+			print(swirldsLog_f.read())
 			if loaded_log in swirldsLog_f.read():
 				print ("Saved state is loaded on platform {}".format(n))
 			else:
