dc3107f4785b534cdcfc3f26158f342cbbee0a50
Quan Nguyen <quannguyen@swirlds.com>
2020-06-15 22:14:18 -0500

    Send AWS summary to hedera-regression channel.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/aws-summary.sh
+++ .circleci/scripts/aws-summary.sh
@@ -8,4 +8,4 @@ java \
 -Daws.secretKey=${AWS_SECRET_ACCESS_KEY} \
 -cp regression/regression.jar com.swirlds.regression.AWSServerCheck \
 xoxp-344480056389-344925970834-610132896599-fb69be9200db37ce0b0d55a852b2a5dc \
-UEVPV4HDY
+hedera-regression
