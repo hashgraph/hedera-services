46bd13bfe9117ecfb0c54f4255eba1a02c5cbf07
Quan Nguyen <quannguyen@swirlds.com>
2020-06-16 11:19:23 -0500

    Another fix that @ljianghedera requested to send report to hedera-regression channel for all workflows from master branch by default.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/call-svcs-app-slack.sh
+++ .circleci/scripts/call-svcs-app-slack.sh
@@ -5,5 +5,11 @@ set +e
 
 ensure_slackclient
 
+PARAMS="$@"
+if [[ "${CIRCLE_BRANCH}" == "master" ]]; then
+  PARAMS+=("-c" "hedera-regression")
+fi
+echo "Params for svcs-app-slack.py: ${PARAMS[*]}"
+
 SLACK_API_TOKEN=xoxb-344480056389-890228995125-MfvKfFwJtL0ba2Ms8HDf8656 \
-  python3 ${REPO}/.circleci/scripts/svcs-app-slack.py $*
+  python3 ${REPO}/.circleci/scripts/svcs-app-slack.py ${PARAMS[*]}
