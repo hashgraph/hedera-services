b341a5c7a7c927a86d427d1eb4f9182bbcdcff36
Anirudh Ghanta <anirudh.ghanta@hedera.com>
2020-06-22 09:25:43 -0500

    moving to aws cli

--- .circleci/scripts/run-custom-migration-script.sh
+++ .circleci/scripts/run-custom-migration-script.sh
@@ -5,6 +5,4 @@ cd $REPO
 
 CONFIG_FILE_PATH='/repo/.circleci/scripts/resources/customMigrationConfig.json'
 
-pip3 install boto
-pip3 install boto3
 python3 ${REPO}/.circleci/scripts/automateCustomMigration.py  ${CONFIG_FILE_PATH}
