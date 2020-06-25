f8ee0bac455a000c731ce1a1e2168955ab056eab
Quan Nguyen <quannguyen@swirlds.com>
2020-06-24 22:11:28 -0500

    Revert "Fix permission to access infra repo."
    This reverts commit fad406138ef59d3cc73416f1a6eb63140c69e8ce.
    
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

--- .circleci/scripts/clone-infra-repo.sh
+++ .circleci/scripts/clone-infra-repo.sh
@@ -15,16 +15,13 @@ else
   ci_echo "Using '$BRANCH' of infrastructure repo..." \
      | tee -a ${REPO}/test-clients/output/hapi-client.log
   cd /
-  SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
-  GIT_SSH_COMMAND="$SSH_COMMAND" \
+  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
     git clone git@github.com:swirlds${INFRASTRUCTURE_REPO}.git \
       --branch "$BRANCH" \
         | tee -a ${REPO}/test-clients/output/hapi-client.log
   SHA1="$2"
   cd infrastructure
-  GIT_SSH_COMMAND="$SSH_COMMAND" \
-    git reset --hard $SHA1
-  SHA1=$(GIT_SSH_COMMAND="$SSH_COMMAND" \
-    git show -s --format=%h)
+  git reset --hard $SHA1
+  SHA1=$(git show -s --format=%h)
   ci_echo "Using commit sha1 '$SHA1' of infrastructure repo..."
 fi
