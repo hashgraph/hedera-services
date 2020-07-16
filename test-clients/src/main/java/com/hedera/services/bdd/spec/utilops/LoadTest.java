943262a212eefe39fb6c9e0e53f697e888c47bba
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-07-16 17:03:53 -0500

    enable numberOfThreads using ciPropertiesMap to run from SuiteRunner

--- test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/LoadTest.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/LoadTest.java
@@ -87,7 +87,7 @@ public class LoadTest extends HapiApiSuite {
 				.tps(targetTPS.isPresent() ? LoadTest::getTargetTPS : settings::getTps)
 				.tolerance(settings::getTolerancePercentage)
 				.allowedSecsBelow(settings::getAllowedSecsBelow)
-				.setNumberOfThreads(threadNumber.isPresent() ? threadNumber.getAsInt() : 1)
+				.setNumberOfThreads(threadNumber.isPresent() ? threadNumber.getAsInt() : settings.getThreadNumber())
 				.lasting(
 						(testDurationMinutes.isPresent() ?
 								LoadTest::getTestDurationMinutes :
--- test-clients/src/main/java/com/hedera/services/bdd/suites/perf/PerfTestLoadSettings.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/perf/PerfTestLoadSettings.java
@@ -29,17 +29,18 @@ public class PerfTestLoadSettings {
 	public static final int DEFAULT_MINS = 5;
 	public static final int DEFAULT_ALLOWED_SECS_BELOW = 60;
 	public static final int DEFAULT_BURST_SIZE = 5;
+	public static final int DEFAULT_THREAD_NUMBER = 10;
 
 	private int tps = DEFAULT_TPS;
 	private int tolerancePercentage = DEFAULT_TOLERANCE_PERCENTAGE;
 	private int mins = DEFAULT_MINS;
 	private int allowedSecsBelow = DEFAULT_ALLOWED_SECS_BELOW;
 	private int burstSize = DEFAULT_BURST_SIZE;
+	private int threadNumber = DEFAULT_THREAD_NUMBER;
 
 	public int getTps() {
 		return tps;
 	}
-
 	public int getTolerancePercentage() {
 		return tolerancePercentage;
 	}
@@ -54,6 +55,10 @@ public class PerfTestLoadSettings {
 
 	public int getBurstSize() {
 		return burstSize;
+	}
+
+	public int getThreadNumber() {
+		return threadNumber;
 	}
 
 	public void setFrom(HapiPropertySource ciProps) {
@@ -71,6 +76,9 @@ public class PerfTestLoadSettings {
 		}
 		if (ciProps.has("allowedSecsBelow")) {
 			allowedSecsBelow = ciProps.getInteger("allowedSecsBelow");
+		}
+		if (ciProps.has("threadNumber")) {
+			threadNumber = ciProps.getInteger("threadNumber");
 		}
 	}
 
