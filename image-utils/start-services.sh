2e4c6cd338b80820ee0b25bc8e89cc8e154da699
tinker-michaelj <michael.tinker@hedera.com>
2020-08-03 18:56:10 -0500

    Use default Docker Compose network
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- image-utils/start-services.sh
+++ image-utils/start-services.sh
@@ -3,6 +3,6 @@ cp config-mount/*.properties data/config
 cp config-mount/* . 
 sed -i "s/NODE_ID/${NODE_ID}/" settings.txt
 cat settings.txt
-./wait-for-it 172.20.4.2:5432 -t 60
+./wait-for-it hedera-postgres:5432 -t 60
 java -cp 'data/lib/*' -Dflag=1 -Dfile.encoding='utf-8' \
   com.swirlds.platform.Browser
