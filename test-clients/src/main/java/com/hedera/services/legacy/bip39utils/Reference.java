423aeba96fe3ad92eed674f45833a18d6576edcb
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-08-04 11:24:54 -0500

    remove swirlds in comments #379

--- test-clients/src/main/java/com/hedera/services/legacy/bip39utils/Reference.java
+++ test-clients/src/main/java/com/hedera/services/legacy/bip39utils/Reference.java
@@ -1,21 +1,3 @@
-/*
- * @author Leemon Baird
- * <p>
- * (c) 2016-2020 Hedera, Inc.
- * <p>
- * This software is the confidential and proprietary information of  
- * Hedera, Inc. ("Confidential Information"). You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into 
- * with Hedera.
- * <p>
- * HEDERA MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT THE SUITABILITY OF
- * THE SOFTWARE, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
- * TO THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE, OR NON-INFRINGEMENT. HEDERA SHALL NOT BE LIABLE FOR
- * ANY DAMAGES SUFFERED BY LICENSEE AS A RESULT OF USING, MODIFYING OR 
- * DISTRIBUTING THIS SOFTWARE OR ITS DERIVATIVES.
- */
 package com.hedera.services.legacy.bip39utils;
 
 /*-
