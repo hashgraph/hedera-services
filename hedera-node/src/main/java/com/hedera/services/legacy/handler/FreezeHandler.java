90d0104bad590181a8f28e19e423e934c46eefbe
Jeffrey Tang <jeffrey@swirlds.com>
2020-08-07 14:55:35 -0500

    add debug

--- hedera-node/src/main/java/com/hedera/services/legacy/handler/FreezeHandler.java
+++ hedera-node/src/main/java/com/hedera/services/legacy/handler/FreezeHandler.java
@@ -84,6 +84,7 @@ public class FreezeHandler {
 		if (transactionBody.getFreeze().hasUpdateFile()) {
 			//save the file ID and will be used after platform goes into maintenance mode
 			updateFeatureFile = transactionBody.getFreeze().getUpdateFile();
+			log.info("{} saved updateFeatureFile {}", LOG_PREFIX, updateFeatureFile);
 		}
 		try {
 			platform.setFreezeTime(
