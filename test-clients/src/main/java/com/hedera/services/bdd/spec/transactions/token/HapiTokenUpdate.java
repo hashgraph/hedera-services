9add28b28b708e5457a2924afede4bf5984b199c
failfmi <oscurocalma@gmail.com>
2020-09-09 17:47:50 +0300

    bdd(token-update): add admin setter

--- test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/token/HapiTokenUpdate.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/token/HapiTokenUpdate.java
@@ -150,6 +150,7 @@ public class HapiTokenUpdate extends HapiTxnOp<HapiTokenUpdate> {
 						TokenManagement.class, b -> {
 							b.setToken(TxnUtils.asRef(id));
 							newSymbol.ifPresent(b::setSymbol);
+							newAdminKey.ifPresent(a -> b.setAdminKey(spec.registry().getKey(a)));
 							newTreasury.ifPresent(a -> b.setTreasury(spec.registry().getAccountID(a)));
 							newSupplyKey.ifPresent(k -> b.setSupplyKey(spec.registry().getKey(k)));
 							newWipeKey.ifPresent(k -> b.setWipeKey(spec.registry().getKey(k)));
