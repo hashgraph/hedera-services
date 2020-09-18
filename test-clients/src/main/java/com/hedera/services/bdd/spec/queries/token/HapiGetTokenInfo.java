632ef739418ab8806ab8fc4c61206bdb7771a239
tinker-michaelj <michael.tinker@hedera.com>
2020-09-18 12:25:56 -0500

    Add license headers
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/bdd/spec/queries/token/HapiGetTokenInfo.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/queries/token/HapiGetTokenInfo.java
@@ -6,12 +6,12 @@ package com.hedera.services.bdd.spec.queries.token;
  * ​
  * Copyright (C) 2018 - 2020 Hedera Hashgraph, LLC
  * ​
- * Licensed under the Apache License, Token 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
