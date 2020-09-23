17f789859428672fd16cdbc5f2940edd2ed47dcb
Quan Nguyen <quannguyen@swirlds.com>
2020-09-22 22:09:11 -0500

    Add eet for transactions with only sigMap.
    Signed-off-by: Quan Nguyen <quannguyen@swirlds.com>

---
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/records/SignedTransactionBytesRecordsSuite.java
@@ -0,0 +1,87 @@
+package com.hedera.services.bdd.suites.records;
+
+/*-
+ * ‌
+ * Hedera Services Test Clients
+ * ​
+ * Copyright (C) 2018 - 2020 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import com.hedera.services.bdd.spec.HapiApiSpec;
+import com.hedera.services.bdd.suites.HapiApiSuite;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileUpdate;
+import static com.hedera.services.bdd.spec.transactions.crypto.HapiCryptoTransfer.tinyBarsFromTo;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.NOT_SUPPORTED;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_ACCOUNT_ID;
+
+public class SignedTransactionBytesRecordsSuite extends HapiApiSuite {
+	private static final Logger log = LogManager.getLogger(SignedTransactionBytesRecordsSuite.class);
+
+	public static void main(String... args) {
+		new SignedTransactionBytesRecordsSuite().runSuiteSync();
+	}
+
+	@Override
+	protected List<HapiApiSpec> getSpecsInSuite() {
+		return List.of(
+				new HapiApiSpec[] {
+						transactionsWithOnlySigMap()
+				}
+		);
+	}
+
+	private HapiApiSpec transactionsWithOnlySigMap() {
+		return defaultHapiSpec("TransactionsWithOnlySigMap")
+				.given(
+						cryptoTransfer(tinyBarsFromTo(GENESIS, MASTER, 1L))
+								.via("failedCryptoTransaction")
+								.asTxnWithOnlySigMap()
+								.hasPrecheck(INVALID_TRANSACTION_BODY),
+						fileCreate("bytecode").path(PATH_TO_LOOKUP_BYTECODE),
+						fileUpdate("bytecode").path(PATH_TO_LOOKUP_BYTECODE)
+								.via("failedFileTransaction")
+								.asTxnWithOnlySigMap()
+								.hasPrecheck(INVALID_TRANSACTION_BODY)
+				).when(
+						contractCreate("test")
+								.bytecode("bytecode")
+								.balance(1_000L)
+								.via("failedContractTransaction")
+								.asTxnWithOnlySigMap()
+								.hasPrecheck(NOT_SUPPORTED)
+				).then(
+						getTxnRecord("failedCryptoTransaction").hasCostAnswerPrecheck(INVALID_ACCOUNT_ID),
+						getTxnRecord("failedFileTransaction").hasCostAnswerPrecheck(INVALID_ACCOUNT_ID),
+						getTxnRecord("failedContractTransaction").hasCostAnswerPrecheck(INVALID_ACCOUNT_ID)
+				);
+	}
+
+	@Override
+	protected Logger getResultsLogger() {
+		return log;
+	}
+}
