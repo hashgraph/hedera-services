bb8ce8aa8d2390795c827e2a76e6eb0ec39506ca
Jeffrey Tang <jeffrey@swirlds.com>
2020-09-28 09:34:43 -0500

    change to local file system

--- hedera-node/src/main/java/com/hedera/services/files/SpecialFileSystem.java
+++ hedera-node/src/main/java/com/hedera/services/files/SpecialFileSystem.java
@@ -2,40 +2,62 @@ package com.hedera.services.files;
 
 import com.hedera.services.legacy.logic.ApplicationConstants;
 import com.hederahashgraph.api.proto.java.FileID;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
-public class SpecialFileSystem
-{
-	Map<FileID, byte[]> fileMap = new HashMap<>();
+public class SpecialFileSystem {
+	private static final Logger log = LogManager.getLogger(SpecialFileSystem.class);
+	private static String fileSystemLocation = "specialFiles/";
+	Set<FileID> fileSet = new HashSet<>();
 
-	public SpecialFileSystem()
-	{
+	public SpecialFileSystem() {
 		// Create empty file 0.0.150
-		FileID fid = FileID.newBuilder().setFileNum(ApplicationConstants.UPDATE_FEATURE_FILE_ACCOUNT_NUM)
+		FileID fid150 = FileID.newBuilder()
+				.setFileNum(ApplicationConstants.UPDATE_FEATURE_FILE_ACCOUNT_NUM)
 				.setRealmNum(ApplicationConstants.DEFAULT_FILE_REALM)
 				.setShardNum(ApplicationConstants.DEFAULT_FILE_SHARD).build();
-		fileMap.put(fid, new byte[0]);
+
+		createEmptyIfNotExist(fid150);
 	}
 
-	public void fileAppend(FileID fileID) {
-
+	private void createEmptyIfNotExist(FileID fileID) {
+		File testFile = new File(fileSystemLocation + fileIDtoDotString(fileID));
+		if (testFile.exists()) {
+			log.info("File {} already exists", fileID);
+		} else {
+			log.info("Creating empty File {}", fileID);
+			put(fileID, new byte[0]);
+		}
 	}
 
+	private static String fileIDtoDotString(FileID fileID) {
+		return "File" + fileID.getShardNum() + "." + fileID.getRealmNum() + "." + fileID.getFileNum();
+	}
 	public byte[] get(FileID fileID) {
-		return fileMap.get(fileID);
+		try {
+			return FileUtils.readFileToByteArray(new File(fileSystemLocation + fileIDtoDotString(fileID)));
+		} catch (IOException e) {
+			log.error("{} Error when reading fileID {} from local filesystem", fileID, e);
+			return new byte[0];
+		}
 	}
 
 	public void put(FileID fileID, byte[] content) {
-		fileMap.put(fileID, content);
+		fileSet.add(fileID);
+		try {
+			FileUtils.writeByteArrayToFile(new File(fileSystemLocation + fileIDtoDotString(fileID)), content);
+		} catch (IOException e) {
+			log.error("{} Error when writing fileID {} to local filesystem", fileID, e);
+		}
 	}
 
-	public void remove(FileID fileID) {
-		//Don't delete only clear the content
-		fileMap.put(fileID, new byte[0]);
-	}
 	public boolean isSpeicalFileID(FileID fileID) {
-		return fileMap.containsKey(fileID);
+		return fileSet.contains(fileID);
 	}
 }
