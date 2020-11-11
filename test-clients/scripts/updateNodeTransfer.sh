7b4f034df3a18368edcffaa78ab7f4779631f004
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2020-11-11 11:35:00 -0600

    add comment
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>

--- test-clients/scripts/updateNodeTransfer.sh
+++ test-clients/scripts/updateNodeTransfer.sh
@@ -8,6 +8,7 @@ UPDATE_NODE_IP_ADDRESS=""
 NODE0_IP_ADDRESS=""
 CONFIG_FILE_PATH=""
 copyFilesToUpdateNode() {
+  # copy config.txt , File0.0.150 and saved state to update nodes to start from the state
   set -x
   sudo scp -o StrictHostKeyChecking=no -i services-regression.pem -p $CONFIG_FILE_PATH ubuntu@$UPDATE_NODE_IP_ADDRESS:/home/ubuntu/remoteExperiment
   echo "Copying config.txt to the update Node"
@@ -21,6 +22,7 @@ copyFilesToUpdateNode() {
 }
 
 parseNewConfig() {
+  # parse config.txt to get the node 0 IP address and update node Ip address
   CONFIG_FILE_PATH="../HapiApp2.0/config.txt"
   mapfile -t addressLinesArray < <(grep -Po '^address.*$' $CONFIG_FILE_PATH)
   NODE0_IP_ADDRESS=$(cut -d',' -f7 <<<${addressLinesArray[0]})
