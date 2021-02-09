8e9d01b07ec60ad5365c9754eb2fa12004ff7fcb
tinker-michaelj <michael.tinker@hedera.com>
2021-02-09 10:54:39 -0600

    Update headers with 2020 -> 2021
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/legacy/core/AccountKeyListObj.java
+++ test-clients/src/main/java/com/hedera/services/legacy/core/AccountKeyListObj.java
@@ -4,7 +4,7 @@ package com.hedera.services.legacy.core;
  * ‌
  * Hedera Services Node
  * ​
- * Copyright (C) 2018 - 2020 Hedera Hashgraph, LLC
+ * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
  * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
