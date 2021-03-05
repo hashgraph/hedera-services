9b8af4f90ddaede3cae03199c0ca1eb7b5e2b02f
anighanta <anirudh.ghanta@hedera.com>
2021-03-05 16:17:07 -0600

    add a command to calulcate fee for basic service operations
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- test-clients/src/main/java/com/hedera/services/yahcli/Yahcli.java
+++ test-clients/src/main/java/com/hedera/services/yahcli/Yahcli.java
@@ -2,6 +2,7 @@ package com.hedera.services.yahcli;
 
 import com.hedera.services.yahcli.commands.accounts.AccountsCommand;
 import com.hedera.services.yahcli.commands.accounts.BalanceCommand;
+import com.hedera.services.yahcli.commands.fees.FeesCommand;
 import com.hedera.services.yahcli.commands.files.SysFilesCommand;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -18,7 +19,8 @@ import java.util.concurrent.Callable;
 		subcommands = {
 				HelpCommand.class,
 				AccountsCommand.class,
-				SysFilesCommand.class
+				SysFilesCommand.class,
+				FeesCommand.class
 		},
 		description = "Perform operations against well-known entities on a Hedera Services network")
 public class Yahcli implements Callable<Integer> {
