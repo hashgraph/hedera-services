4bec3db1bcdc0a55bd8577f2c02a1266409b1c36
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2021-03-10 18:49:36 -0600

    fix INVALID_SCHEDULE_ID error thrown

--- test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/schedule/HapiScheduleSign.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/schedule/HapiScheduleSign.java
@@ -30,6 +30,7 @@ import com.hedera.services.bdd.spec.queries.schedule.HapiGetScheduleInfo;
 import com.hedera.services.bdd.spec.transactions.HapiTxnOp;
 import com.hedera.services.usage.schedule.ScheduleSignUsage;
 import com.hederahashgraph.api.proto.java.HederaFunctionality;
+import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 import com.hederahashgraph.api.proto.java.ScheduleInfo;
 import com.hederahashgraph.api.proto.java.ScheduleSignTransactionBody;
 import com.hederahashgraph.api.proto.java.Transaction;
@@ -51,6 +52,7 @@ import static com.hedera.services.bdd.spec.transactions.TxnUtils.suFrom;
 import static com.hedera.services.bdd.spec.transactions.schedule.HapiScheduleCreate.correspondingScheduledTxnId;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.ScheduleSign;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OK;
 import static java.util.stream.Collectors.toList;
 
 public class HapiScheduleSign extends HapiTxnOp<HapiScheduleSign> {
@@ -105,14 +107,19 @@ public class HapiScheduleSign extends HapiTxnOp<HapiScheduleSign> {
 			bytesToSign = explicitBytes.get();
 		} else {
 			if (lookupBytesToSign) {
-				var subOp = getScheduleInfo(schedule);
+				var subOp = getScheduleInfo(schedule)
+						.hasCostAnswerPrecheckFrom(OK, ResponseCodeEnum.INVALID_SCHEDULE_ID);
 				allRunFor(spec, subOp);
-				var info = subOp.getResponse().getScheduleGetInfo().getScheduleInfo();
-				bytesToSign = info.getTransactionBody().toByteArray();
-				if (verboseLoggingOn) {
-					log.info("Found transaction to sign: {}", TransactionBody.parseFrom(bytesToSign));
+				if (subOp.getResponse() == null) {
+					bytesToSign = new byte[] { };
 				} else {
-					log.info("Found {} bytes to sign", bytesToSign.length);
+					var info = subOp.getResponse().getScheduleGetInfo().getScheduleInfo();
+					bytesToSign = info.getTransactionBody().toByteArray();
+					if (verboseLoggingOn) {
+						log.info("Found transaction to sign: {}", TransactionBody.parseFrom(bytesToSign));
+					} else {
+						log.info("Found {} bytes to sign", bytesToSign.length);
+					}
 				}
 			} else {
 				try {
