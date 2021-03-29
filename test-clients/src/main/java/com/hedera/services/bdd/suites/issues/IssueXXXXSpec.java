fdfab4b0e331cd737512351ddfbc59a757826679
tinker-michaelj <michael.tinker@hedera.com>
2021-03-29 13:33:55 -0500

    Split steady-state throttling to spec meant for regression
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/issues/IssueXXXXSpec.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/issues/IssueXXXXSpec.java
@@ -20,69 +20,38 @@ package com.hedera.services.bdd.suites.issues;
  * ‍
  */
 
-import com.google.common.base.Stopwatch;
 import com.hedera.services.bdd.spec.HapiApiSpec;
-
-import static com.hedera.services.bdd.spec.HapiApiSpec.*;
-
-import com.hedera.services.bdd.spec.HapiSpecOperation;
-import com.hedera.services.bdd.spec.infrastructure.OpProvider;
+import com.hedera.services.bdd.spec.HapiSpecSetup;
 import com.hedera.services.bdd.spec.infrastructure.meta.ContractResources;
-import com.hedera.services.bdd.spec.queries.crypto.HapiGetAccountBalance;
 import com.hedera.services.bdd.suites.HapiApiSuite;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 
-import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.hedera.services.bdd.spec.HapiApiSpec.defaultFailingHapiSpec;
+import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getFileContents;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
-import static com.hedera.services.bdd.spec.transactions.TxnVerbs.*;
-import static com.hedera.services.bdd.spec.transactions.crypto.HapiCryptoTransfer.tinyBarsFromTo;
-import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.inParallel;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.runWithProvider;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileUpdate;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hedera.services.bdd.suites.utils.sysfiles.serdes.ThrottleDefsLoader.protoDefsFromResource;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.BUSY;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OK;
-import static java.util.concurrent.TimeUnit.SECONDS;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Function;
-import java.util.stream.IntStream;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.AUTHORIZATION_FAILED;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OPERATION_REPEATED_IN_BUCKET_GROUPS;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.THROTTLE_GROUP_HAS_ZERO_OPS_PER_SEC;
 
 public class IssueXXXXSpec extends HapiApiSuite {
 	private static final Logger log = LogManager.getLogger(IssueXXXXSpec.class);
 
-	private static final int CI_NETWORK_SIZE = 4;
-	private static final int LOCAL_NETWORK_SIZE = 3;
-
-
-	private static final double THROUGHPUT_LIMITS_XFER_NETWORK_TPS = 100.0;
-	private static final double TEST_THROUGHPUT_LIMITS_MINT_NETWORK_TPS = 30.0;
-	private static final double PRIORITY_RESERVATIONS_CONTRACT_CALL_NETWORK_TPS = 2.0;
-	private static final double CREATION_LIMITS_CRYPTO_CREATE_NETWORK_TPS = 1.0;
-	private static final double FREE_QUERY_LIMITS_GET_BALANCE_NETWORK_QPS = 100.0;
-
-	private static final int NETWORK_SIZE = LOCAL_NETWORK_SIZE;
-
-	private static final double expectedXferTps = THROUGHPUT_LIMITS_XFER_NETWORK_TPS / NETWORK_SIZE;
-	private static final double expectedMintTps = TEST_THROUGHPUT_LIMITS_MINT_NETWORK_TPS / NETWORK_SIZE;
-	private static final double expectedContractCallTps =
-			PRIORITY_RESERVATIONS_CONTRACT_CALL_NETWORK_TPS / NETWORK_SIZE;
-	private static final double expectedCryptoCreateTps = CREATION_LIMITS_CRYPTO_CREATE_NETWORK_TPS / NETWORK_SIZE;
-	private static final double expectedGetBalanceQps = FREE_QUERY_LIMITS_GET_BALANCE_NETWORK_QPS / NETWORK_SIZE;
-	private static final double toleratedPercentDeviation = 5.0;
-
-	private AtomicLong duration = new AtomicLong(240);
-	private AtomicReference<TimeUnit> unit = new AtomicReference<>(SECONDS);
-	private AtomicInteger maxOpsPerSec = new AtomicInteger(500);
+	private static final String defaultCongestionMultipliers =
+			HapiSpecSetup.getDefaultNodeProps().get("fees.percentCongestionMultipliers");
 
 	public static void main(String... args) {
 		new IssueXXXXSpec().runSuiteSync();
@@ -92,229 +61,122 @@ public class IssueXXXXSpec extends HapiApiSuite {
 	protected List<HapiApiSpec> getSpecsInSuite() {
 		return List.of(
 				new HapiApiSpec[] {
-						multipleSelfDestructsAreSafe(),
-						
-//						setArtificialLimits(),
-//						checkTps("Xfers", expectedXferTps, xferOps()),
-//						checkTps("Mints", expectedMintTps, mintOps()),
-//						checkTps("ContractCalls", expectedContractCallTps, scCallOps()),
-//						checkTps("CryptoCreates", expectedCryptoCreateTps, cryptoCreateOps()),
-//						restoreDevLimits(),
-//						checkBalanceQps(1000, expectedGetBalanceQps)
+						throttleDefsHaveExpectedDefaults(),
+						throttleDefsRejectUnauthorizedPayers(),
+						throttleUpdateRejectsMultiGroupAssignment(),
+//						throttleUpdateWithZeroGroupOpsPerSecFails(),
+						canUpdateMultipliersDynamically(),
 				}
 		);
 	}
 
-	private HapiApiSpec setArtificialLimits() {
+	private HapiApiSpec canUpdateMultipliersDynamically() {
 		var artificialLimits = protoDefsFromResource("testSystemFiles/artificial-limits.json");
-		return defaultHapiSpec("SetArtificialLimits")
-				.given().when().then(
-						fileUpdate(THROTTLE_DEFS)
-								.payingWith(EXCHANGE_RATE_CONTROL)
-								.contents(artificialLimits.toByteArray())
-				);
-	}
-
-	private HapiApiSpec restoreDevLimits() {
 		var defaultThrottles = protoDefsFromResource("testSystemFiles/throttles-dev.json");
-		return defaultHapiSpec("RestoreDevLimits")
-				.given().when().then(
-						fileUpdate(THROTTLE_DEFS)
-								.payingWith(EXCHANGE_RATE_CONTROL)
-								.contents(defaultThrottles.toByteArray())
-				);
-	}
 
-	private HapiApiSpec checkTps(String txn, double expectedTps, Function<HapiApiSpec, OpProvider> provider) {
-		return defaultHapiSpec("Throttles" + txn + "AsExpected")
-				.given().when(
-						runWithProvider(provider)
-								.lasting(duration::get, unit::get)
-								.maxOpsPerSec(maxOpsPerSec::get)
-				).then(
-						withOpContext((spec, opLog) -> {
-							var actualTps = 1.0 * spec.finalAdhoc() / duration.get();
-							var percentDeviation = Math.abs(actualTps / expectedTps - 1.0) * 100.0;
-							opLog.info("Total ops accepted in {} {} = {} ==> {}tps vs {}tps expected ({}% deviation)",
-									duration.get(),
-									unit.get(),
-									spec.finalAdhoc(),
-									String.format("%.3f", actualTps),
-									String.format("%.3f", expectedTps),
-									String.format("%.3f", percentDeviation));
-							Assert.assertEquals(0.0, percentDeviation, toleratedPercentDeviation);
-						})
-				);
-	}
+		AtomicLong normalPrice = new AtomicLong();
+		AtomicLong sevenXPrice = new AtomicLong();
 
-	private HapiApiSpec checkBalanceQps(int burstSize, double expectedQps) {
-		return defaultHapiSpec("CheckBalanceQps").given(
-				cryptoCreate("curious").payingWith(GENESIS)
-		).when().then(
-				withOpContext((spec, opLog) -> {
-					int numBusy = 0;
-					int askedSoFar = 0;
-					int secsToRun = (int)duration.get();
-					var watch = Stopwatch.createStarted();
-					while (watch.elapsed(SECONDS) < secsToRun) {
-						var subOps = IntStream.range(0, burstSize)
-								.mapToObj(ignore -> getAccountBalance("0.0.2")
-										.noLogging()
-										.payingWith("curious")
-										.hasAnswerOnlyPrecheckFrom(BUSY, OK))
-								.toArray(HapiSpecOperation[]::new);
-						var burst = inParallel(subOps);
-						allRunFor(spec, burst);
-						askedSoFar += burstSize;
-						for (int i = 0; i < burstSize; i++) {
-							var op = (HapiGetAccountBalance) subOps[i];
-							if (op.getResponse().getCryptogetAccountBalance().getBalance() == 0) {
-								numBusy++;
-							}
-						}
-						opLog.info("{}/{} queries BUSY so far in {}ms",
-								numBusy,
-								askedSoFar,
-								watch.elapsed(TimeUnit.MILLISECONDS));
-					}
-					var elapsedMs = watch.elapsed(TimeUnit.MILLISECONDS);
-					var numAnswered = askedSoFar - numBusy;
-					var actualQps = (1.0 * numAnswered) / elapsedMs * 1000.0;
-					var percentDeviation = Math.abs(actualQps / expectedQps - 1.0) * 100.0;
-					opLog.info("Total ops accepted in {} {} = {} ==> {}qps vs {}qps expected ({}% deviation)",
-							elapsedMs,
-							"ms",
-							numAnswered,
-							String.format("%.3f", actualQps),
-							String.format("%.3f", expectedQps),
-							String.format("%.3f", percentDeviation));
-					Assert.assertEquals(0.0, percentDeviation, toleratedPercentDeviation);
-				})
-		);
-	}
-
-	private HapiApiSpec multipleSelfDestructsAreSafe() {
-		return defaultHapiSpec("MultipleSelfDestructsAreSafe")
+		return defaultHapiSpec("CanUpdateMultipliersDynamically")
 				.given(
-						fileCreate("bytecode").path(ContractResources.FUSE_BYTECODE_PATH),
-						contractCreate("fuse").bytecode("bytecode")
-				).when(
-						contractCall("fuse", ContractResources.LIGHT_ABI).via("lightTxn")
-				).then(
-						getTxnRecord("lightTxn").logged()
-				);
-	}
-
-	private Function<HapiApiSpec, OpProvider> xferOps() {
-		return spec -> new OpProvider() {
-			@Override
-			public List<HapiSpecOperation> suggestedInitializers() {
-				return List.of(
 						cryptoCreate("civilian")
 								.payingWith(GENESIS)
-								.balance(ONE_MILLION_HBARS)
-								.withRecharging(),
-						cryptoCreate("nobody")
-								.payingWith(GENESIS)
-								.balance(0L)
-				);
-			}
-
-			@Override
-			public Optional<HapiSpecOperation> get() {
-				var op = cryptoTransfer(tinyBarsFromTo("civilian", "nobody", 1))
-						.noLogging()
-						.deferStatusResolution()
-						.payingWith("civilian")
-						.hasPrecheckFrom(OK, BUSY);
-				return Optional.of(op);
-			}
-		};
-	}
-
-	private Function<HapiApiSpec, OpProvider> cryptoCreateOps() {
-		var i = new AtomicInteger(0);
-
-		return spec -> new OpProvider() {
-			@Override
-			public List<HapiSpecOperation> suggestedInitializers() {
-				return List.of(
-						cryptoCreate("civilian")
-								.payingWith(GENESIS)
-								.balance(ONE_MILLION_HBARS)
-								.withRecharging()
-				);
-			}
-
-			@Override
-			public Optional<HapiSpecOperation> get() {
-				var op = cryptoCreate("w/e" + i.getAndIncrement())
-						.noLogging()
-						.deferStatusResolution()
-						.payingWith("civilian")
-						.hasPrecheckFrom(OK, BUSY);
-				return Optional.of(op);
-			}
-		};
-	}
-
-
-	private Function<HapiApiSpec, OpProvider> scCallOps() {
-		return spec -> new OpProvider() {
-			@Override
-			public List<HapiSpecOperation> suggestedInitializers() {
-				return List.of(
+								.balance(ONE_MILLION_HBARS),
 						fileCreate("bytecode")
 								.path(ContractResources.MULTIPURPOSE_BYTECODE_PATH)
 								.payingWith(GENESIS),
 						contractCreate("scMulti")
 								.bytecode("bytecode")
 								.payingWith(GENESIS),
-						cryptoCreate("civilian")
-								.balance(ONE_MILLION_HBARS)
-								.payingWith(GENESIS)
+						contractCall("scMulti")
+								.payingWith("civilian")
+								.fee(ONE_HUNDRED_HBARS)
+								.sending(ONE_HBAR)
+								.via("cheapCall"),
+						getTxnRecord("cheapCall")
+								.providingFeeTo(normalPrice::set)
+				).when(
+						fileUpdate(APP_PROPERTIES)
+								.payingWith(EXCHANGE_RATE_CONTROL)
+								.overridingProps(Map.of(
+										"fees.percentCongestionMultipliers", "1,7x"
+								)),
+						fileUpdate(THROTTLE_DEFS)
+								.payingWith(EXCHANGE_RATE_CONTROL)
+								.contents(artificialLimits.toByteArray()),
+						contractCall("scMulti")
+								.payingWith("civilian")
+								.fee(ONE_HUNDRED_HBARS)
+								.sending(ONE_HBAR)
+								.via("pricyCall")
+				).then(
+						getTxnRecord("pricyCall")
+								.providingFeeTo(sevenXPrice::set),
+						withOpContext((spec, opLog) -> {
+							Assert.assertEquals(
+									"~7x multiplier should be in affect!",
+									7.0,
+									(1.0 * sevenXPrice.get()) / normalPrice.get(),
+									0.1);
+						}),
+						fileUpdate(THROTTLE_DEFS)
+								.payingWith(EXCHANGE_RATE_CONTROL)
+								.contents(defaultThrottles.toByteArray()),
+						fileUpdate(APP_PROPERTIES)
+								.payingWith(EXCHANGE_RATE_CONTROL)
+								.overridingProps(Map.of(
+										"fees.percentCongestionMultipliers", defaultCongestionMultipliers
+								))
 				);
-			}
-
-			@Override
-			public Optional<HapiSpecOperation> get() {
-				var op = contractCall("scMulti")
-						.noLogging()
-						.deferStatusResolution()
-						.payingWith("civilian")
-						.sending(ONE_HBAR)
-						.hasPrecheckFrom(OK, BUSY);
-				return Optional.of(op);
-			}
-		};
 	}
 
-	private Function<HapiApiSpec, OpProvider> mintOps() {
-		return spec -> new OpProvider() {
-			@Override
-			public List<HapiSpecOperation> suggestedInitializers() {
-				return List.of(
-						newKeyNamed("supply"),
-						cryptoCreate(TOKEN_TREASURY)
-								.payingWith(GENESIS)
-								.balance(ONE_MILLION_HBARS),
-						tokenCreate("token")
-								.treasury(TOKEN_TREASURY)
-								.supplyKey("supply")
-				);
-			}
+	private HapiApiSpec throttleUpdateWithZeroGroupOpsPerSecFails() {
+		var zeroOpsPerSecThrottles = protoDefsFromResource("testSystemFiles/zero-ops-group.json");
 
-			@Override
-			public Optional<HapiSpecOperation> get() {
-				var op = mintToken("token", 1L)
-						.noLogging()
-						.deferStatusResolution()
-						.signedBy(TOKEN_TREASURY, "supply")
-						.payingWith(TOKEN_TREASURY)
-						.hasPrecheckFrom(OK, BUSY);
-				return Optional.of(op);
-			}
-		};
+		return defaultFailingHapiSpec("ThrottleUpdateWithZeroGroupOpsPerSecFails")
+				.given( ).when( ).then(
+						fileUpdate(THROTTLE_DEFS)
+								.contents(zeroOpsPerSecThrottles.toByteArray())
+								.hasKnownStatus(THROTTLE_GROUP_HAS_ZERO_OPS_PER_SEC)
+				);
+	}
+
+	private HapiApiSpec throttleUpdateRejectsMultiGroupAssignment() {
+		var multiGroupThrottles = protoDefsFromResource("testSystemFiles/duplicated-operation.json");
+
+		return defaultHapiSpec("ThrottleUpdateRejectsMultiGroupAssignment")
+				.given( ).when( ).then(
+						fileUpdate(THROTTLE_DEFS)
+								.contents(multiGroupThrottles.toByteArray())
+								.hasKnownStatus(OPERATION_REPEATED_IN_BUCKET_GROUPS)
+				);
+	}
+
+	private HapiApiSpec throttleDefsRejectUnauthorizedPayers() {
+		return defaultHapiSpec("ThrottleDefsRejectUnauthorizedPayers")
+				.given(
+						cryptoCreate("civilian")
+				).when( ).then(
+						fileUpdate(THROTTLE_DEFS)
+								.contents("BOOM")
+								.payingWith("civilian")
+								.hasPrecheck(AUTHORIZATION_FAILED),
+						fileUpdate(THROTTLE_DEFS)
+								.contents("BOOM")
+								.payingWith(FEE_SCHEDULE_CONTROL)
+								.hasPrecheck(AUTHORIZATION_FAILED)
+				);
+	}
+
+	private HapiApiSpec throttleDefsHaveExpectedDefaults() {
+		var defaultThrottles = protoDefsFromResource("testSystemFiles/throttles-dev.json");
+
+		return defaultHapiSpec("ThrottleDefsExistOnStartup")
+				.given( ).when( ).then(
+						getFileContents(THROTTLE_DEFS)
+								.payingWith(GENESIS)
+								.hasContents(ignore -> defaultThrottles.toByteArray())
+				);
 	}
 
 	@Override
