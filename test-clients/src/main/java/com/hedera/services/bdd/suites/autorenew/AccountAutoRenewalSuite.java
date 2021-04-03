d6f3a4540bfd65f75f06acb76c4ef22237b1577b
Quan Nguyen <quan.nguyen@hedera.com>
2021-04-03 10:54:34 -0500

    Add a test for account to expire.
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>

---
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/autorenew/AccountAutoRenewalSuite.java
@@ -0,0 +1,71 @@
+package com.hedera.services.bdd.suites.autorenew;
+
+/*-
+ * ‌
+ * Hedera Services Test Clients
+ * ​
+ * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import com.hedera.services.bdd.spec.HapiApiSpec;
+import com.hedera.services.bdd.suites.HapiApiSuite;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+
+import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountInfo;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoTransfer;
+import static com.hedera.services.bdd.spec.transactions.crypto.HapiCryptoTransfer.tinyBarsFromTo;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.overriding;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.sleepFor;
+
+public class AccountAutoRenewalSuite extends HapiApiSuite {
+	private static final Logger log = LogManager.getLogger(AccountAutoRenewalSuite.class);
+
+	public static void main(String... args) {
+		new AccountAutoRenewalSuite().runSuiteSync();
+	}
+
+	@Override
+	protected List<HapiApiSpec> getSpecsInSuite() {
+		return List.of(
+				accountExpires()
+		);
+	}
+
+	private HapiApiSpec accountExpires() {
+		return defaultHapiSpec("AccountExpires")
+				.given(
+						overriding("ledger.autoRenewPeriod.minDuration", "10"),
+						cryptoCreate("willExpire").autoRenewSecs(10).balance(ONE_HBAR)
+				)
+				.when(
+						sleepFor(120 * 1000)
+				)
+				.then(
+						getAccountInfo("willExpire").logged(),
+						cryptoTransfer(tinyBarsFromTo("willExpire", NODE, 1L))
+				);
+	}
+
+	@Override
+	protected Logger getResultsLogger() {
+		return log;
+	}
+}
