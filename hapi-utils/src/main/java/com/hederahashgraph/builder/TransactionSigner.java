33a79d0aaba714bcdfc81cd26ecedf6e0aa8d2d0
anighanta <anirudh.ghanta@hedera.com>
2021-05-19 13:53:46 -0500

    throw the created exception when privateKey list size doesnt match with public keyList size
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hapi-utils/src/main/java/com/hederahashgraph/builder/TransactionSigner.java
+++ hapi-utils/src/main/java/com/hederahashgraph/builder/TransactionSigner.java
@@ -242,7 +242,7 @@ public class TransactionSigner {
       byte[] bodyBytes = CommonUtils.extractTransactionBodyBytes(transaction);
 
       if(pubKeysList.size() != privKeysList.size()) {
-        new Exception("public and private keys size mismtach! pubKeysList size = " + pubKeysList.size() + ", privKeysList size = " + privKeysList.size());
+        throw new Exception("public and private keys size mismtach! pubKeysList size = " + pubKeysList.size() + ", privKeysList size = " + privKeysList.size());
       }
 
       List<SignaturePair> pairs = new ArrayList<>();
