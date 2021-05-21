581b8a90cadbff23ffb04740022dd2670de46062
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2021-05-21 12:29:51 -0500

    remove main method in SignatureVerifier and unused methods as they are not used anywhere
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>

--- hapi-utils/src/main/java/com/hedera/services/legacy/proto/utils/SignatureVerifier.java
+++ hapi-utils/src/main/java/com/hedera/services/legacy/proto/utils/SignatureVerifier.java
@@ -28,12 +28,7 @@ import net.i2p.crypto.eddsa.spec.EdDSAParameterSpec;
 import net.i2p.crypto.eddsa.spec.EdDSAPublicKeySpec;
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -49,7 +44,6 @@ import java.security.spec.InvalidParameterSpecException;
 import java.security.spec.X509EncodedKeySpec;
 
 public class SignatureVerifier {
-  private static final Logger log = LogManager.getLogger(SignatureVerifier.class);
 
   /**
    * Crypto algorithm for key pair
@@ -127,40 +121,6 @@ public class SignatureVerifier {
   }
 
   /**
-   * Checks the format of a public address.
-   *
-   * @param pubKeyStr the public address in hex
-   * @return true if the format is correct, false otherwise
-   */
-  public static boolean checkECDSAAddressFormat(String pubKeyStr) throws Exception {
-    boolean rv = false;
-
-    try {
-      if (pubKeyStr.startsWith("04")) {
-        if (pubKeyStr.length() != 194) {
-          throw new Exception("Public address is not 194 bytes. Address = " + pubKeyStr);
-        }
-
-        SignatureGenerator.getEC384PublicKeyFromBinary(pubKeyStr);
-        rv = true;
-      } else {
-        byte[] pubKeyBytes;
-        pubKeyBytes = getPubKeyBytes(pubKeyStr);
-
-        X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(pubKeyBytes);
-        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-        keyFactory.generatePublic(pubKeySpec);
-        rv = true;
-      }
-
-    } catch (Exception e) {
-      rv = false;
-      throw new Exception("Invalid address detected: " + pubKeyStr, e);
-    }
-    return rv;
-  }
-
-  /**
    * Verifies a signature for a message with an EC public key in binary format.
    *
    * @param signature the digital signature to be verified
@@ -224,105 +184,6 @@ public class SignatureVerifier {
     byte[] rv = null;
     rv = Hex.decodeHex(signature);
     return rv;
-  }
-
-  public static void main(String[] args)  {
-
-    /* Verify a DSA signature */
-
-    if (args.length != 3) {
-      System.out.println("Usage: VerSig publickeyfile signaturefile datafile");
-      System.exit(1);
-    }
-
-    /* import encoded public key */
-    byte[] encKey = null;
-    try (FileInputStream  keyfis = new FileInputStream(args[0])) {
-        encKey = new byte[keyfis.available()];
-        int bytesRead = keyfis.read(encKey);
-        logWarnIfMismatch(bytesRead, encKey.length);
-    } catch (IOException e) {
-      System.err.println("Caught IOException when reading public key file " + e);
-      log.error("Caught IOException when reading public key file ", e );
-    }
-
-    X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
-
-    PublicKey pubKey = null;
-    try {
-      KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
-      pubKey = keyFactory.generatePublic(pubKeySpec);
-    } catch ( NoSuchAlgorithmException | NoSuchProviderException |InvalidKeySpecException e) {
-      System.err.println("Caught exception when generating public key " + e);
-      log.error("Caught Exception when generating public key", e);
-    }
-
-      /* input the signature bytes */
-    byte[] sigToVerify = null;
-    try(FileInputStream sigfis = new FileInputStream(args[1])) {
-      sigToVerify = new byte[sigfis.available()];
-      int bytesRead = sigfis.read(sigToVerify);
-      logWarnIfMismatch(bytesRead, sigToVerify.length);
-    } catch (IOException e) {
-      System.err.println("Caught IOException when reading public key file " + e);
-      log.error("Caught IOException when reading public key file", e );
-    }
-
-      /* create a Signature object and initialize it with the public key */
-    Signature sig = null;
-    try {
-      sig = Signature.getInstance("SHA1withDSA", "SUN");
-      sig.initVerify(pubKey);
-    } catch ( InvalidKeyException | NoSuchAlgorithmException | NoSuchProviderException  e) {
-      System.err.println("Caught exception when initializing verifying signature " + e);
-      log.error("Caught Exception when initializing verifying signature", e);
-    }
-      /* Update and verify the data */
-
-    try (FileInputStream datafis = new FileInputStream(args[2]);
-         BufferedInputStream bufin = new BufferedInputStream(datafis)) {
-      byte[] buffer = new byte[1024];
-      int len;
-      while (bufin.available() != 0) {
-        len = bufin.read(buffer);
-        sig.update(buffer, 0, len);
-      }
-    } catch (IOException | SignatureException e) {
-      System.err.println("Caught exception when reading data file " + e);
-      log.error("Caught Exception when reading data file", e);
-    }
-
-    boolean verifies = false;
-    try {
-      verifies = sig.verify(sigToVerify);
-    } catch (SignatureException e) {
-      System.err.println("Caught exception when verifying signature " + e);
-      log.error("Caught Exception when verifying signature", e);
-    }
-    System.out.println("signature verifies: " + verifies);
-  }
-
-  private static void logWarnIfMismatch(int bytesRead, int expectedBytesLength) {
-    if (bytesRead != expectedBytesLength) {
-      log.warn("Total bytes read from file doesn't match expected length");
-    }
-  }
-
-  public static boolean verifyED25519Signature(String pubKeyStr, String message, String signature)
-      throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, UnsupportedEncodingException, DecoderException {
-
-    byte[] sigBytes = getSigBytes(signature);
-    byte[] msgBytes = getMsgBytes(message);
-    byte[] pubKeyBytes = getPubKeyBytes(pubKeyStr);
-
-    EdDSAParameterSpec spec = EdDSANamedCurveTable.getByName(EdDSANamedCurveTable.ED_25519);
-    EdDSAPublicKeySpec pubKey = new EdDSAPublicKeySpec(pubKeyBytes, spec);
-    PublicKey vKey = new EdDSAPublicKey(pubKey);
-    Signature verifier = new EdDSAEngine(MessageDigest.getInstance(spec.getHashAlgorithm()));
-
-    verifier.initVerify(vKey);
-    verifier.update(msgBytes);
-    return verifier.verify(sigBytes);
   }
 
   public static boolean verifyED25519(byte[] pubKeyBytes, byte[] msgBytes, byte[] sigBytes)
