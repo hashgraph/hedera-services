08db8c871d2d50d09e0c975d984e78cf01e3d928
tinker-michaelj <michael.tinker@hedera.com>
2021-05-28 15:40:21 -0500

    Need more collaborators
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/fees/charging/TxnChargingPolicyAgent.java
+++ hedera-node/src/main/java/com/hedera/services/fees/charging/TxnChargingPolicyAgent.java
@@ -1,6 +1,7 @@
 package com.hedera.services.fees.charging;
 
 import com.hedera.services.context.TransactionContext;
+import com.hedera.services.context.primitives.StateView;
 import com.hedera.services.fees.FeeCalculator;
 import com.hedera.services.records.TxnIdRecentHistory;
 import com.hedera.services.state.logic.AwareNodeDiligenceScreen;
@@ -8,29 +9,34 @@ import com.hedera.services.utils.TxnAccessor;
 import com.hederahashgraph.api.proto.java.TransactionID;
 
 import java.util.Map;
+import java.util.function.Supplier;
 
 /**
  * Uses a (non-triggered) transaction's duplicate classification and
- * node due diligence screen to pick one of the three charging policies.
+ * node due diligence screen to pick one of three charging policies
+ * to use for the fees due for the active transaction.
  *
  * Please see {@link FeeChargingPolicy} for details.
  */
 public class TxnChargingPolicyAgent {
 	private final FeeCalculator fees;
 	private final TransactionContext txnCtx;
+	private final Supplier<StateView> currentView;
 	private final AwareNodeDiligenceScreen nodeDiligenceScreen;
 	private final Map<TransactionID, TxnIdRecentHistory> txnHistories;
 
 	public TxnChargingPolicyAgent(
 			FeeCalculator fees,
 			TransactionContext txnCtx,
+			Supplier<StateView> currentView,
 			AwareNodeDiligenceScreen nodeDiligenceScreen,
 			Map<TransactionID, TxnIdRecentHistory> txnHistories
 	) {
 		this.fees = fees;
 		this.txnCtx = txnCtx;
-		this.nodeDiligenceScreen = nodeDiligenceScreen;
+		this.currentView = currentView;
 		this.txnHistories = txnHistories;
+		this.nodeDiligenceScreen = nodeDiligenceScreen;
 	}
 
 	/**
