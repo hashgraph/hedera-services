8fede807d688b6f37eb42f6e71b602099140eff0
Richard Bair <richard.bair@hedera.com>
2021-05-28 13:03:19 -0700

    Add JMH project for benchmarking. Include contract call test.

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -11,18 +11,12 @@ dependencies {
     jmhImplementation(files("../hedera-node/target/classes"));
     jmhImplementation(files("../hapi-fees/target/classes"));
     jmhImplementation(files("../hapi-utils/target/classes"));
+//    jmhImplementation("com.swirlds:swirlds-common:0.9.2-rbair");
+//    jmhImplementation("com.swirlds:swirlds-fcmap:0.9.2-rbair");
+    jmhImplementation("com.hedera.hashgraph:hedera-protobuf-java-api:0.14.0-SNAPSHOT")
+    jmhImplementation("com.hedera.hashgraph:ethereumj-core:1.12.0-v0.5.0")
 
     jmhImplementation("com.hedera.hashgraph:sdk:2.0.5")
-    jmhImplementation("com.swirlds:swirlds-common:0.9.4")
     jmhRuntimeOnly("io.grpc:grpc-okhttp:1.35.0")
     jmhRuntimeOnly("org.slf4j:slf4j-simple:1.7.29")
-    jmhRuntimeOnly("org.eclipse.collections:eclipse-collections-api:10.4.0")
-    jmhRuntimeOnly("org.eclipse.collections:eclipse-collections:10.4.0")
-}
-
-jmh {
-    jvmArgs.set(listOf(
-        "-Djna.library.path=/opt/homebrew/Cellar/libsodium/1.0.18_1/lib",
-        "-XX:MaxInlineSize=128",
-        "-XX:InlineSmallCode=1024").asIterable());
 }
