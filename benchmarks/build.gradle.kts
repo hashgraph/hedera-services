deaf20193d0416568d312deeee00179dc257e414
Richard Bair <richard.bair@hedera.com>
2021-06-01 13:07:18 -0700

    Remove unused dependencies

--- benchmarks/build.gradle.kts
+++ benchmarks/build.gradle.kts
@@ -11,10 +11,6 @@ dependencies {
     jmhImplementation(files("../hedera-node/target/classes"));
     jmhImplementation(files("../hapi-fees/target/classes"));
     jmhImplementation(files("../hapi-utils/target/classes"));
-//    jmhImplementation("com.swirlds:swirlds-common:0.9.2-rbair");
-//    jmhImplementation("com.swirlds:swirlds-fcmap:0.9.2-rbair");
-    jmhImplementation("com.hedera.hashgraph:hedera-protobuf-java-api:0.14.0-SNAPSHOT")
-    jmhImplementation("com.hedera.hashgraph:ethereumj-core:1.12.0-v0.5.0")
 
     jmhImplementation("com.hedera.hashgraph:sdk:2.0.5")
     jmhRuntimeOnly("io.grpc:grpc-okhttp:1.35.0")
