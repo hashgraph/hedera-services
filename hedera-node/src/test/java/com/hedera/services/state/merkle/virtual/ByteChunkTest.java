cc85483dd8ffd857c93b1aa65b4ece54c48a2ec2
Richard Bair <richard.bair@hedera.com>
2021-06-02 08:27:39 -0700

    Add some ByteChunk tests

--- hedera-node/src/test/java/com/hedera/services/state/merkle/virtual/ByteChunkTest.java
+++ hedera-node/src/test/java/com/hedera/services/state/merkle/virtual/ByteChunkTest.java
@@ -12,55 +12,55 @@ import static org.junit.jupiter.api.Assertions.assertNotSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ByteChunkTest {
-//    @Test
-//    public void createByteChunk() {
-//        final var sourceArray = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk = new EthValue(sourceArray);
-//        assertArrayEquals(sourceArray, chunk.asByteArray());
-//        assertNotSame(sourceArray, chunk.asByteArray());
-//    }
-//
-//    @Test
-//    public void sourceArrayIsCopied() {
-//        final var sourceArray = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk = new EthValue(sourceArray);
-//
-//        // Modify the source array. This should have no impact on the internal array of the ByteChunk.
-//        sourceArray[0] = 10;
-//        assertFalse(Arrays.equals(sourceArray, chunk.asByteArray()));
-//    }
-//
-//    @Test
-//    public void hashesSame() {
-//        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk1 = new EthValue(sourceArray1);
-//
-//        final var sourceArray2 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk2 = new EthValue(sourceArray2);
-//
-//        assertEquals(chunk1.hashCode(), chunk2.hashCode());
-//    }
-//
-//    @Test
-//    public void chunksAreEqual() {
-//        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk1 = new EthValue(sourceArray1);
-//
-//        final var sourceArray2 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk2 = new EthValue(sourceArray2);
-//
-//        assertEquals(chunk1, chunk2);
-//    }
-//
-//    @Test
-//    public void chunksAreNotEqual() {
-//        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
-//        final var chunk1 = new EthValue(sourceArray1);
-//
-//        final var sourceArray2 = Arrays.copyOf(new byte[] { 10, 2, 3, 4}, 32);
-//        final var chunk2 = new EthValue(sourceArray2);
-//
-//        assertNotEquals(chunk1, chunk2);
-//    }
+    @Test
+    public void createByteChunk() {
+        final var sourceArray = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk = new ByteChunk(sourceArray);
+        assertArrayEquals(sourceArray, chunk.asByteArray());
+        assertNotSame(sourceArray, chunk.asByteArray());
+    }
+
+    @Test
+    public void sourceArrayIsCopied() {
+        final var sourceArray = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk = new ByteChunk(sourceArray);
+
+        // Modify the source array. This should have no impact on the internal array of the ByteChunk.
+        sourceArray[0] = 10;
+        assertFalse(Arrays.equals(sourceArray, chunk.asByteArray()));
+    }
+
+    @Test
+    public void hashesSame() {
+        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk1 = new ByteChunk(sourceArray1);
+
+        final var sourceArray2 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk2 = new ByteChunk(sourceArray2);
+
+        assertEquals(chunk1.hashCode(), chunk2.hashCode());
+    }
+
+    @Test
+    public void chunksAreEqual() {
+        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk1 = new ByteChunk(sourceArray1);
+
+        final var sourceArray2 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk2 = new ByteChunk(sourceArray2);
+
+        assertEquals(chunk1, chunk2);
+    }
+
+    @Test
+    public void chunksAreNotEqual() {
+        final var sourceArray1 = Arrays.copyOf(new byte[] { 1, 2, 3, 4}, 32);
+        final var chunk1 = new ByteChunk(sourceArray1);
+
+        final var sourceArray2 = Arrays.copyOf(new byte[] { 10, 2, 3, 4}, 32);
+        final var chunk2 = new ByteChunk(sourceArray2);
+
+        assertNotEquals(chunk1, chunk2);
+    }
 
 }
