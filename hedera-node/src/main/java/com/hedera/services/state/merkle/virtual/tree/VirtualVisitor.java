5701f14676a6ac1a382b6fc0a38708c164934f0f
Richard Bair <richard.bair@hedera.com>
2021-06-03 14:46:42 -0700

    Remove generics

--- hedera-node/src/main/java/com/hedera/services/state/merkle/virtual/tree/VirtualVisitor.java
+++ hedera-node/src/main/java/com/hedera/services/state/merkle/virtual/tree/VirtualVisitor.java
@@ -1,13 +1,11 @@
 package com.hedera.services.state.merkle.virtual.tree;
 
-import com.swirlds.common.crypto.Hashable;
-
-public class VirtualVisitor<K, V extends Hashable> {
-    public void visitParent(VirtualTreeInternal<K, V> parent) {
+public class VirtualVisitor {
+    public void visitParent(VirtualTreeInternal parent) {
 
     }
 
-    public void visitLeaf(VirtualTreeLeaf<K, V> leaf) {
+    public void visitLeaf(VirtualTreeLeaf leaf) {
 
     }
 
