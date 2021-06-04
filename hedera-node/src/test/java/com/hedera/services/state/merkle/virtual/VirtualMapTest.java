c9b8e4233ff9e241032698ad861ff687ff37de4a
Richard Bair <richard.bair@hedera.com>
2021-06-03 18:22:11 -0700

    Refactor to reduce garbage

--- hedera-node/src/test/java/com/hedera/services/state/merkle/virtual/VirtualMapTest.java
+++ hedera-node/src/test/java/com/hedera/services/state/merkle/virtual/VirtualMapTest.java
@@ -1,53 +1,64 @@
 package com.hedera.services.state.merkle.virtual;
 
 import com.hedera.services.state.merkle.virtual.persistence.VirtualDataSource;
-import com.hedera.services.state.merkle.virtual.persistence.VirtualRecord;
-import com.hedera.services.state.merkle.EthValue;
+import com.hedera.services.state.merkle.virtual.tree.VirtualTreeInternal;
+import com.hedera.services.state.merkle.virtual.tree.VirtualTreeLeaf;
 import com.hedera.services.state.merkle.virtual.tree.VirtualTreePath;
+import com.swirlds.common.crypto.Hash;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class VirtualMapTest {
 
     @Test
-    public void lookupNonExistent() {
-        final var ds = new InMemoryDataSource();
-        final var v = new VirtualMap<EthValue, EthValue>();
-        v.setDataSource(ds);
-
-        final var key = asBlock("DoesNotExist");
-        Assertions.assertNull(v.getValue(key));
+    public void nullDataSourceThrows() {
+        assertThrows(NullPointerException.class, () -> new VirtualMap(null));
     }
 
     @Test
-    public void putAndGetItem() {
+    public void lookupNonExistent() {
         final var ds = new InMemoryDataSource();
-        final var v = new VirtualMap<EthValue, EthValue>();
-        v.setDataSource(ds);
+        final var v = new VirtualMap(ds);
 
-        final var key = asBlock("fruit");
-        final var value = asBlock("apple");
+        final var key = asKey("DoesNotExist");
+        Assertions.assertNull(v.getValue(key));
+    }
 
-        v.putValue(key, value);
-        Assertions.assertEquals(value, v.getValue(key));
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17})
+    public void putAndGetItems(int numItems) {
+        final var ds = new InMemoryDataSource();
+        final var v = new VirtualMap(ds);
+
+        for (int i=0; i<numItems; i++) {
+            final var key = asKey(i);
+            final var value = asValue(i);
+            v.putValue(key, value);
+        }
+
+        for (int i=0; i<numItems; i++) {
+            Assertions.assertEquals(asValue(i), v.getValue(asKey(i)));
+        }
     }
 
     @Test
     public void replaceItem() {
         final var ds = new InMemoryDataSource();
-        final var v = new VirtualMap<EthValue, EthValue>();
-        v.setDataSource(ds);
+        final var v = new VirtualMap(ds);
 
-        final var key = asBlock("fruit");
-        final var value = asBlock("apple");
-        final var value2 = asBlock("banana");
+        final var key = asKey("fruit");
+        final var value = asValue("apple");
+        final var value2 = asValue("banana");
 
         v.putValue(key, value);
         v.putValue(key, value2);
@@ -57,13 +68,12 @@ public class VirtualMapTest {
     @Test
     public void addManyItemsAndFindThemAll() {
         final var ds = new InMemoryDataSource();
-        final var v = new VirtualMap<EthValue, EthValue>();
-        v.setDataSource(ds);
+        final var v = new VirtualMap(ds);
 
-        final var expected = new HashMap<EthValue, EthValue>();
-        for (int i=0; i<10_000_000; i++) {
-            final var key = asBlock(i + "");
-            final var value = asBlock((i + 100_000_000) + "");
+        final var expected = new HashMap<VirtualKey, VirtualValue>();
+        for (int i=0; i<1_000_000; i++) {
+            final var key = asKey(i + "");
+            final var value = asValue((i + 100_000_000) + "");
             expected.put(key, value);
             v.putValue(key, value);
         }
@@ -74,17 +84,16 @@ public class VirtualMapTest {
     @Test
     public void addManyItemsUsingManyMapsAndReadThemAllFromOne() {
         final var ds = new InMemoryDataSource();
-        var v = new VirtualMap<EthValue, EthValue>();
-        v.setDataSource(ds);
+        var v = new VirtualMap(ds);
 
-        final var expected = new HashMap<EthValue, EthValue>();
+        final var expected = new HashMap<VirtualKey, VirtualValue>();
         for (int i=0; i<1_000_000; i++) {
             if (i > 0 && i % 15 == 0) {
-                v = new VirtualMap<EthValue, EthValue>();
-                v.setDataSource(ds);
+                v.commit();
+                v = new VirtualMap(ds);
             }
-            final var key = asBlock(i + "");
-            final var value = asBlock((i + 100_000_000) + "");
+            final var key = asKey(i + "");
+            final var value = asValue((i + 100_000_000) + "");
             expected.put(key, value);
             v.putValue(key, value);
 //            System.out.println(v.getAsciiArt());
@@ -96,76 +105,101 @@ public class VirtualMapTest {
 
     @Test
     public void quickAndDirty() {
-        final var ds = new InMemoryDataSource();
-        VirtualMap<EthValue, EthValue> map = new VirtualMap<>();
-        map.setDataSource(ds);
-        for (int i=0; i<1_000_000; i++) {
-            final var key = asKey(i);
-            final var value = asValue(i);
-            map.putValue(key, value);
-//            System.out.println(map.getAsciiArt());
-        }
-
-        Random rand = new Random();
-        for (int idx=0; idx<10_000; idx++) {
-            map = new VirtualMap<EthValue, EthValue>();
-            map.setDataSource(ds);
-            for (int j = 0; j < 100; j++) {
-                final var i = rand.nextInt(1_000_000);
-                map.putValue(asKey(i), asValue(i + 1_000_000));
-            }
-        }
+//        final var ds = new InMemoryDataSource();
+//        VirtualMap map = new VirtualMap<>();
+//        map.setDataSource(ds);
+//        for (int i=0; i<1_000_000; i++) {
+//            final var key = asKey(i);
+//            final var value = asValue(i);
+//            map.putValue(key, value);
+////            System.out.println(map.getAsciiArt());
+//        }
+//
+//        Random rand = new Random();
+//        for (int idx=0; idx<10_000; idx++) {
+//            map = new VirtualMap();
+//            map.setDataSource(ds);
+//            for (int j = 0; j < 100; j++) {
+//                final var i = rand.nextInt(1_000_000);
+//                map.putValue(asKey(i), asValue(i + 1_000_000));
+//            }
+//        }
     }
 
-    private EthValue asKey(int index) {
-        return new EthValue(Arrays.copyOf(("key" + index).getBytes(), 32));
+    private VirtualKey asKey(String txt) {
+        return new VirtualKey(Arrays.copyOf(txt.getBytes(), 32));
     }
 
-    private EthValue asValue(int index) {
-        return new EthValue(Arrays.copyOf(("val" + index).getBytes(), 32));
+    private VirtualKey asKey(int index) {
+        return new VirtualKey(Arrays.copyOf(("key" + index).getBytes(), 32));
+    }
+
+    private VirtualValue asValue(String txt) {
+        return new VirtualValue(Arrays.copyOf(txt.getBytes(), 32));
+    }
+
+    private VirtualValue asValue(int index) {
+        return new VirtualValue(Arrays.copyOf(("val" + index).getBytes(), 32));
     }
 
     // TODO Delete items... how to do that? Set them to null I guess...
     // TODO Test hashing the tree
 
-    private EthValue asBlock(String s) {
-        return new EthValue(Arrays.copyOf(s.getBytes(StandardCharsets.UTF_8), 32));
-    }
+//    private EthValue asBlock(String s) {
+//        return new EthValue(Arrays.copyOf(s.getBytes(StandardCharsets.UTF_8), 32));
+//    }
 
     // Simple implementation for testing purposes
-    private static final class InMemoryDataSource implements VirtualDataSource<EthValue, EthValue> {
-        private Map<EthValue, VirtualRecord<EthValue, EthValue>> recordsByKey = new HashMap<>();
-        private Map<VirtualTreePath, VirtualRecord<EthValue, EthValue>> recordsByPath = new HashMap<>();
+    private static final class LeafRecord {
+        private VirtualKey key;
+        private Hash hash;
+        private VirtualTreePath path;
+        private VirtualValue value;
+    }
+
+    private static final class InMemoryDataSource implements VirtualDataSource {
+        private Map<VirtualKey, LeafRecord> leaves = new HashMap<>();
+        private Map<VirtualTreePath, Hash> parents = new HashMap<>();
         private VirtualTreePath firstLeafPath;
         private VirtualTreePath lastLeafPath;
         private boolean closed = false;
 
         @Override
-        public VirtualRecord<EthValue, EthValue> getRecord(EthValue key) {
-            return recordsByKey.get(key);
+        public VirtualTreeInternal load(VirtualTreePath parentPath) {
+            final var hash = parents.get(parentPath);
+            return hash == null ? null : new VirtualTreeInternal(hash, parentPath);
         }
 
         @Override
-        public VirtualRecord<EthValue, EthValue> getRecord(VirtualTreePath path) {
-            return recordsByPath.get(path);
+        public VirtualTreeLeaf load(VirtualKey leafKey) {
+            final var rec = leaves.get(leafKey);
+            return rec == null ? null : new VirtualTreeLeaf(rec.hash, rec.path, rec.key, rec.value);
         }
 
         @Override
-        public void deleteRecord(VirtualRecord<EthValue, EthValue> record) {
-            if (record != null) {
-                recordsByPath.remove(record.getPath());
-                recordsByKey.remove(record.getKey());
-            }
+        public void save(VirtualTreeInternal parent) {
+            parents.put(parent.getPath(), parent.hash());
         }
 
         @Override
-        public void setRecord(VirtualRecord<EthValue, EthValue> record) {
-            if (record != null) {
-                if (record.isLeaf()) {
-                    recordsByKey.put(record.getKey(), record);
-                }
-                recordsByPath.put(record.getPath(), record);
-            }
+        public void save(VirtualTreeLeaf leaf) {
+            final var rec = new LeafRecord();
+            rec.hash = leaf.hash();
+            rec.key = leaf.getKey();
+            rec.path = leaf.getPath();
+            rec.value = leaf.getData();
+            leaves.put(leaf.getKey(), rec);
+        }
+
+        @Override
+        public void delete(VirtualTreeInternal parent) {
+            // TODO potentially dangerous, what if a new parent has been put here??
+            parents.remove(parent.getPath());
+        }
+
+        @Override
+        public void delete(VirtualTreeLeaf leaf) {
+            leaves.remove(leaf.getKey()); // Always safe.
         }
 
         @Override
