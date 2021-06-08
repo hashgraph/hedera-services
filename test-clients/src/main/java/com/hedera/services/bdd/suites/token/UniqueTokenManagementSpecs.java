761ad6a30805a183c60255c6e32dab00827a1d12
Yoan Sredkov <yoansredkov@gmail.com>
2021-06-08 11:02:24 +0300

    E2E assertion
    Signed-off-by: Yoan Sredkov <yoansredkov@gmail.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/token/UniqueTokenManagementSpecs.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/token/UniqueTokenManagementSpecs.java
@@ -34,15 +34,23 @@ import java.util.List;
 
 import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountInfo;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getReceipt;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTokenInfo;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTokenNftInfo;
+import static com.hedera.services.bdd.spec.queries.crypto.ExpectedTokenRel.relationshipWith;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.mintToken;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenFreeze;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.ACCOUNT_FROZEN_FOR_TOKEN;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.FAIL_INVALID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_NFT_ID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_TOKEN_NFT_SERIAL_NUMBER;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TOKEN_HAS_NO_SUPPLY_KEY;
 
 public class UniqueTokenManagementSpecs extends HapiApiSuite {
 	private static final org.apache.logging.log4j.Logger log = LogManager.getLogger(UniqueTokenManagementSpecs.class);
@@ -55,15 +63,17 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 	@Override
 	protected List<HapiApiSpec> getSpecsInSuite() {
 		return List.of(new HapiApiSpec[]{
-						sadPathWithRepeatedMetadata(),
 						getTokenNftInfoWork(),
-						sadPathWithFrozenToken(),
 						uniqueTokenHappyPath(),
-						happyPathOneMintFiveMetadata(),
-						happyPathFiveMintOneMetadata(),
+//						sadPathWithNoSupplyKey(),
+//						sadPathWithFrozenToken(),
+						sadPathWithRepeatedMetadata(),
+						happyPathCallingMintOnceWithFiveMetadata(),
+						happyPathCallMintFiveTimesWithOneMetadata(),
 				}
 		);
 	}
+
 
 	private HapiApiSpec uniqueTokenHappyPath() {
 		return defaultHapiSpec("UniqueTokenHappyPath")
@@ -79,30 +89,40 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 				).when(
 						mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo"), ByteString.copyFromUtf8("memo1"))).via("mintTxn")
 				).then(
-
-						getAccountBalance(TOKEN_TREASURY)
-								.hasTokenBalance(TOKEN_NAME, 2),
-
 						getReceipt("mintTxn")
-								.hasPriorityStatus(ResponseCodeEnum.SUCCESS),
+								.hasPriorityStatus(SUCCESS)
+								.hasCostAnswerPrecheck(SUCCESS),
 
 						getTokenNftInfo(TOKEN_NAME, 1)
 								.hasSerialNum(1)
 								.hasMetadata(ByteString.copyFromUtf8("memo"))
-								.hasTokenID(TOKEN_NAME),
+								.hasTokenID(TOKEN_NAME)
+								.hasAccountID(TOKEN_TREASURY),
 
 						getTokenNftInfo(TOKEN_NAME, 2)
 								.hasSerialNum(2)
 								.hasMetadata(ByteString.copyFromUtf8("memo1"))
 								.hasTokenID(TOKEN_NAME)
+								.hasAccountID(TOKEN_TREASURY),
+
+						getTokenNftInfo(TOKEN_NAME, 3)
+								.hasCostAnswerPrecheck(INVALID_NFT_ID),
+
+						getAccountBalance(TOKEN_TREASURY)
+								.hasTokenBalance(TOKEN_NAME, 2),
+						getTokenInfo(TOKEN_NAME)
+								.hasTreasury(TOKEN_TREASURY)
+								.hasTotalSupply(0),
+						getAccountInfo(TOKEN_TREASURY)
+								.hasToken(relationshipWith(TOKEN_NAME))
 				);
 	}
 
-	private HapiApiSpec happyPathOneMintFiveMetadata() {
-		return defaultHapiSpec("happyPathOneMintFiveMetadata")
+	private HapiApiSpec happyPathCallingMintOnceWithFiveMetadata() {
+		return defaultHapiSpec("happyPathCallingMintOnceWithFiveMetadata")
 				.given(
 						newKeyNamed("supplyKey"),
-						cryptoCreate(TOKEN_TREASURY).balance(10000L),
+						cryptoCreate(TOKEN_TREASURY),
 						tokenCreate(TOKEN_NAME)
 								.tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
 								.supplyType(TokenSupplyType.FINITE)
@@ -111,7 +131,6 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 								.maxSupply(100000)
 								.memo("memo")
 								.treasury(TOKEN_TREASURY)
-								.via("mintTxn")
 				).when(
 						mintToken(TOKEN_NAME, List.of(
 								ByteString.copyFromUtf8("memo"),
@@ -121,28 +140,41 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 								ByteString.copyFromUtf8("memo4")
 						))
 				).then(
-						getTokenNftInfo("token", 1).hasSerialNum(1).hasMetadata(ByteString.copyFromUtf8("memo")),
-						getTokenNftInfo("token", 2).hasSerialNum(2).hasMetadata(ByteString.copyFromUtf8("memo1")),
-						getTokenNftInfo("token", 3).hasSerialNum(3).hasMetadata(ByteString.copyFromUtf8("memo2")),
-						getTokenNftInfo("token", 4).hasSerialNum(4).hasMetadata(ByteString.copyFromUtf8("memo3")),
+						getTokenNftInfo(TOKEN_NAME, 1)
+								.hasSerialNum(1)
+								.hasMetadata(ByteString.copyFromUtf8("memo")),
+						getTokenNftInfo(TOKEN_NAME, 2)
+								.hasSerialNum(2)
+								.hasMetadata(ByteString.copyFromUtf8("memo1")),
+						getTokenNftInfo(TOKEN_NAME, 3)
+								.hasSerialNum(3)
+								.hasMetadata(ByteString.copyFromUtf8("memo2")),
+						getTokenNftInfo(TOKEN_NAME, 4)
+								.hasSerialNum(4)
+								.hasMetadata(ByteString.copyFromUtf8("memo3")),
 
-						getTokenNftInfo("token", 5).hasSerialNum(5)
+						getTokenNftInfo(TOKEN_NAME, 5).hasSerialNum(5)
 								.hasMetadata(ByteString.copyFromUtf8("memo4"))
 								.hasTokenID(TOKEN_NAME)
-								.hasAccountID(TOKEN_TREASURY)
+								.hasAccountID(TOKEN_TREASURY),
+
+						getAccountInfo(TOKEN_TREASURY)
+								.hasToken(relationshipWith(TOKEN_NAME))
 				);
 	}
 
-	private HapiApiSpec happyPathFiveMintOneMetadata() {
-		return defaultHapiSpec("happyPathFiveMintOneMetadata")
+	private HapiApiSpec happyPathCallMintFiveTimesWithOneMetadata() {
+		return defaultHapiSpec("happyPathCallMintFiveTimesWithOneMetadata")
 				.given(
 						newKeyNamed("supplyKey"),
 						cryptoCreate(TOKEN_TREASURY),
 						tokenCreate(TOKEN_NAME)
 								.tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
-								.supplyType(TokenSupplyType.INFINITE)
-								.initialSupply(0)
+								.supplyType(TokenSupplyType.FINITE)
 								.supplyKey("supplyKey")
+								.initialSupply(0)
+								.maxSupply(100000)
+								.memo("memo")
 								.treasury(TOKEN_TREASURY)
 				).when(
 						mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo"))),
@@ -152,11 +184,38 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 						mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo")))
 
 				).then(
-						getTokenNftInfo(TOKEN_NAME, 1).hasSerialNum(1).hasMetadata(ByteString.copyFromUtf8("memo")),
-						getTokenNftInfo(TOKEN_NAME, 2).hasSerialNum(2).hasMetadata(ByteString.copyFromUtf8("memo")),
-						getTokenNftInfo(TOKEN_NAME, 3).hasSerialNum(3).hasMetadata(ByteString.copyFromUtf8("memo")),
-						getTokenNftInfo(TOKEN_NAME, 4).hasSerialNum(4).hasMetadata(ByteString.copyFromUtf8("memo")),
-						getTokenNftInfo(TOKEN_NAME, 5).hasSerialNum(5).hasMetadata(ByteString.copyFromUtf8("memo"))
+						getTokenNftInfo(TOKEN_NAME, 1)
+								.hasSerialNum(1)
+								.hasMetadata(ByteString.copyFromUtf8("memo"))
+								.hasAccountID(TOKEN_TREASURY)
+								.hasTokenID(TOKEN_NAME),
+
+						getTokenNftInfo(TOKEN_NAME, 2)
+								.hasSerialNum(2)
+								.hasMetadata(ByteString.copyFromUtf8("memo"))
+								.hasAccountID(TOKEN_TREASURY)
+								.hasTokenID(TOKEN_NAME),
+
+						getTokenNftInfo(TOKEN_NAME, 3)
+								.hasSerialNum(3)
+								.hasMetadata(ByteString.copyFromUtf8("memo"))
+								.hasAccountID(TOKEN_TREASURY)
+								.hasTokenID(TOKEN_NAME),
+
+						getTokenNftInfo(TOKEN_NAME, 4)
+								.hasSerialNum(4)
+								.hasMetadata(ByteString.copyFromUtf8("memo"))
+								.hasAccountID(TOKEN_TREASURY)
+								.hasTokenID(TOKEN_NAME),
+
+						getTokenNftInfo(TOKEN_NAME, 5)
+								.hasSerialNum(5)
+								.hasMetadata(ByteString.copyFromUtf8("memo"))
+								.hasAccountID(TOKEN_TREASURY)
+								.hasTokenID(TOKEN_NAME),
+
+						getAccountInfo(TOKEN_TREASURY)
+								.hasToken(relationshipWith(TOKEN_NAME))
 				);
 	}
 
@@ -176,8 +235,28 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 				).when(
 						tokenFreeze(TOKEN_NAME, TOKEN_TREASURY)
 				).then(
-						mintToken("token", List.of(ByteString.copyFromUtf8("memo"))).hasKnownStatus(ResponseCodeEnum.ACCOUNT_FROZEN_FOR_TOKEN)
+						mintToken("token", List.of(ByteString.copyFromUtf8("memo"))).via("mintTxn")
+								.hasKnownStatus(ResponseCodeEnum.ACCOUNT_FROZEN_FOR_TOKEN),
+						getReceipt("mintTxn").hasAnswerOnlyPrecheck(ACCOUNT_FROZEN_FOR_TOKEN),
+						getAccountInfo(TOKEN_TREASURY).hasNoTokenRelationship(TOKEN_NAME)
 				);
+	}
+
+	private HapiApiSpec sadPathWithNoSupplyKey() {
+		return defaultHapiSpec("sadPathWithNoSupplyKey").given(
+				cryptoCreate(TOKEN_TREASURY),
+				tokenCreate(TOKEN_NAME)
+						.tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
+						.supplyType(TokenSupplyType.INFINITE)
+						.initialSupply(0)
+						.treasury(TOKEN_TREASURY)
+		).when(
+
+		).then(
+				mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo")))
+						.via("mintTxt")
+						.hasKnownStatus(TOKEN_HAS_NO_SUPPLY_KEY)
+		);
 	}
 
 	private HapiApiSpec getTokenNftInfoWork() {
@@ -185,25 +264,25 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 		return defaultHapiSpec("getTokenNftInfoWorks")
 				.given(
 						newKeyNamed("supplyKey"),
-						cryptoCreate(TOKEN_TREASURY).balance(0L)
+						cryptoCreate(TOKEN_TREASURY)
 				).when(
-						tokenCreate("non-fungible-unique-finite")
+						tokenCreate(TOKEN_NAME)
 								.tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
 								.supplyType(TokenSupplyType.FINITE)
 								.supplyKey("supplyKey")
 								.initialSupply(0)
 								.maxSupply(100)
 								.treasury(TOKEN_TREASURY),
-						mintToken("non-fungible-unique-finite", List.of(ByteString.copyFromUtf8("memo")))
+						mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo")))
 				).then(
-						getTokenNftInfo("non-fungible-unique-finite", 0)
+						getTokenNftInfo(TOKEN_NAME, 0)
 								.hasCostAnswerPrecheck(INVALID_TOKEN_NFT_SERIAL_NUMBER),
-						getTokenNftInfo("non-fungible-unique-finite", -1)
+						getTokenNftInfo(TOKEN_NAME, -1)
 								.hasCostAnswerPrecheck(INVALID_TOKEN_NFT_SERIAL_NUMBER),
-						getTokenNftInfo("non-fungible-unique-finite", 2)
+						getTokenNftInfo(TOKEN_NAME, 2)
 								.hasCostAnswerPrecheck(INVALID_NFT_ID),
-						getTokenNftInfo("non-fungible-unique-finite", 1)
-								.hasTokenID("non-fungible-unique-finite")
+						getTokenNftInfo(TOKEN_NAME, 1)
+								.hasTokenID(TOKEN_NAME)
 								.hasAccountID(TOKEN_TREASURY)
 								.hasMetadata(ByteString.copyFromUtf8("memo"))
 								.hasSerialNum(1)
@@ -224,7 +303,12 @@ public class UniqueTokenManagementSpecs extends HapiApiSuite {
 				).when(
 				).then(
 						mintToken(TOKEN_NAME, List.of(ByteString.copyFromUtf8("memo"), ByteString.copyFromUtf8("memo")))
-								.hasKnownStatus(ResponseCodeEnum.INVALID_TRANSACTION_BODY)
+								.via("mintTxn")
+								.hasKnownStatus(INVALID_TRANSACTION_BODY),
+
+						getReceipt("mintTxn")
+								.hasCostAnswerPrecheck(INVALID_TRANSACTION_BODY)
+								.hasPriorityStatus(FAIL_INVALID)
 				);
 	}
 
