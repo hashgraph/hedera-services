dd65555c0923aae9177249070d99f30033b2cf5e
tinker-michaelj <michael.tinker@hedera.com>
2021-06-10 09:57:12 -0500

    Add Javadoc/rename constant as OVERFLOW_ERROR in OverflowCheckingCalc
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hapi-fees/src/main/java/com/hedera/services/calc/OverflowCheckingCalc.java
+++ hapi-fees/src/main/java/com/hedera/services/calc/OverflowCheckingCalc.java
@@ -30,9 +30,37 @@ import com.hederahashgraph.fee.FeeObject;
 import static com.hedera.services.usage.SingletonEstimatorUtils.ESTIMATOR_UTILS;
 import static com.hederahashgraph.fee.FeeBuilder.FEE_DIVISOR_FACTOR;
 
+/**
+ * A specialized fee calculator that throws an exception if any step of the
+ * fee calculation overflows.
+ *
+ * (Because all prices and usage estimates are known to be non-negative,
+ * checking for an overflow means just checking for a number less than zero.)
+ */
 public class OverflowCheckingCalc {
-	private static final String POSITIVITY_ERROR = "All fee contributions and intermediate terms must be positive";
+	private static final String OVERFLOW_ERROR = "A fee calculation step overflowed; " +
+			"the operation cannot be priced, and therefore cannot be performed";
 
+	/**
+	 * Returns the network, node, and services fees for an operation given four inputs.
+	 *
+	 * The first input to the calculation is a resource usage estimate in the form of
+	 * an instance of {@link UsageAccumulator}. (See the Javadoc on hat class for a
+	 * detailed description of resource types.)
+	 *
+	 * The second input is a {@code FeeData} instance that has the price of each
+	 * resource in units of 1/1000th of a tinycent.
+	 *
+	 * The third input is the active exchange rate between ℏ and ¢ (equivalently,
+	 * between tinybar and tinycent); and the final input is a multiplier that is
+	 * almost always one, except in cases of extreme congestion pricing.
+	 *
+	 * @param usage the resources used by an operation
+	 * @param prices the prices of those resources, in units of 1/1000th of a tinycent
+	 * @param rate the exchange rate between ℏ and ¢
+	 * @param multiplier a scale factor determined by congestion pricing
+	 * @throws IllegalArgumentException if any step of the calculation overflows
+	 */
 	public FeeObject fees(UsageAccumulator usage, FeeData prices, ExchangeRate rate, long multiplier) {
 		final long networkFeeTinycents = networkFeeInTinycents(usage, prices.getNetworkdata());
 		final long nodeFeeTinycents = nodeFeeInTinycents(usage, prices.getNodedata());
@@ -43,7 +71,7 @@ public class OverflowCheckingCalc {
 		final long serviceFee = tinycentsToTinybars(serviceFeeTinycents, rate) * multiplier;
 
 		if (networkFee < 0 || nodeFee < 0 || serviceFee < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 
 		return new FeeObject(nodeFee, networkFee, serviceFee);
@@ -84,39 +112,39 @@ public class OverflowCheckingCalc {
 	/* These verbose accumulators signatures are to avoid any performance hit from varargs */
 	long safeAccumulateFour(long base, long a, long b, long c, long d) {
 		if (base < 0 || a < 0 || b < 0 || c < 0 || d < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		var sum = safeAccumulateThree(base, a, b, c);
 		sum += d;
 		if (sum < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		return sum;
 	}
 
 	long safeAccumulateThree(long base, long a, long b, long c) {
 		if (base < 0 || a < 0 || b < 0 || c < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		var sum = safeAccumulateTwo(base, a, b);
 		sum += c;
 		if (sum < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		return sum;
 	}
 
 	long safeAccumulateTwo(long base, long a, long b) {
 		if (base < 0 || a < 0 || b < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		base += a;
 		if (base < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		base += b;
 		if (base < 0) {
-			throw new IllegalArgumentException(POSITIVITY_ERROR);
+			throw new IllegalArgumentException(OVERFLOW_ERROR);
 		}
 		return base;
 	}
