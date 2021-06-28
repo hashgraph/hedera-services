568d1f276f6d2e2fb21e00fc405b2d52c1f3df85
Richard Bair <richard.bair@hedera.com>
2021-06-28 12:21:28 -0700

    Implement equals and hashcode

--- hedera-node/src/main/java/com/hedera/services/state/merkle/virtual/ContractUint256.java
+++ hedera-node/src/main/java/com/hedera/services/state/merkle/virtual/ContractUint256.java
@@ -44,4 +44,17 @@ public class ContractUint256 implements SelfSerializable {
         }
         serializableDataOutputStream.writeByteArray(arr);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ContractUint256 that = (ContractUint256) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
