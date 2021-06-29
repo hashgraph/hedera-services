b313ed8207caf9d919b48cf1e6fd0f2f3e5b1a1c
jasperpotts <jasper.potts@hedera.com>
2021-06-29 12:27:45 -0700

    Moved old tests out of way

--- hedera-node/src/test/java/com/hedera/services/state/merkle/vitrualh/MemMapDataStoreTest.java
+++ hedera-node/src/test/java/com/hedera/services/state/merkle/vitrualh/MemMapDataStoreTest.java
@@ -1,4 +1,4 @@
-package com.hedera.services.state.merkle.virtual;
+package com.hedera.services.state.merkle.vitrualh;
 
 import com.hedera.services.state.merkle.virtualh.persistence.mmap.MemMapDataStore;
 import org.junit.jupiter.api.*;
