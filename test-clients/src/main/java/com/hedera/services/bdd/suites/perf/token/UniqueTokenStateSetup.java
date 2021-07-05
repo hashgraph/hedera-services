1303f52085ddc25721851ad928b68c8f7ade16e0
tinker-michaelj <michael.tinker@hedera.com>
2021-07-05 00:37:13 -0500

    Use 1/10th scale setup 10x
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/perf/token/UniqueTokenStateSetup.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/perf/token/UniqueTokenStateSetup.java
@@ -57,9 +57,9 @@ public class UniqueTokenStateSetup extends HapiApiSuite {
 	private static final Logger log = LogManager.getLogger(UniqueTokenStateSetup.class);
 
 	private static final int MINT_TPS = 250;
-	private static final long SECS_TO_RUN = 40500;
+	private static final long SECS_TO_RUN = 4050;
 
-	private static final int NUM_UNIQ_TOKENS = 100_000;
+	private static final int NUM_UNIQ_TOKENS = 10_000;
 	private static final int UNIQ_TOKENS_BURST_SIZE = 1000;
 	private static final int UNIQ_TOKENS_POST_BURST_PAUSE_MS = 2500;
 	private static final int NFTS_PER_UNIQ_TOKEN = 1000;
