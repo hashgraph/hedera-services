6aa4d30e51c6ce3714e4a4bc76484c65bbb815fc
tinker-michaelj <michael.tinker@hedera.com>
2021-07-05 23:13:38 -0500

    Fix Sonar-flagged bug
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hapi-fees/src/main/java/com/hedera/services/usage/token/TokenGetNftInfosUsage.java
+++ hapi-fees/src/main/java/com/hedera/services/usage/token/TokenGetNftInfosUsage.java
@@ -31,9 +31,11 @@ import static com.hederahashgraph.fee.FeeBuilder.BASIC_ENTITY_ID_SIZE;
 import static com.hederahashgraph.fee.FeeBuilder.INT_SIZE;
 
 public class TokenGetNftInfosUsage extends QueryUsage {
+	final static long INT_SIZE_AS_LONG = INT_SIZE;
+
 	public TokenGetNftInfosUsage(Query query) {
 		super(query.getTokenGetNftInfos().getHeader().getResponseType());
-		updateTb(BASIC_ENTITY_ID_SIZE + 2 * INT_SIZE);
+		updateTb(BASIC_ENTITY_ID_SIZE + 2 * INT_SIZE_AS_LONG);
 	}
 
 	public static TokenGetNftInfosUsage newEstimate(Query query) {
