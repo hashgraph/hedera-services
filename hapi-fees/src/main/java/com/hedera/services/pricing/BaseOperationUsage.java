9d1d122e2662242623078fded5df9b1753543ac6
tinker-michaelj <michael.tinker@hedera.com>
2021-07-08 12:29:59 -0500

    Fix unit test?
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hapi-fees/src/main/java/com/hedera/services/pricing/BaseOperationUsage.java
+++ hapi-fees/src/main/java/com/hedera/services/pricing/BaseOperationUsage.java
@@ -23,6 +23,7 @@ package com.hedera.services.pricing;
 import com.google.protobuf.ByteString;
 import com.hedera.services.usage.BaseTransactionMeta;
 import com.hedera.services.usage.SigUsage;
+import com.hedera.services.usage.TxnUsageEstimator;
 import com.hedera.services.usage.consensus.ConsensusOpsUsage;
 import com.hedera.services.usage.consensus.SubmitMessageMeta;
 import com.hedera.services.usage.state.UsageAccumulator;
@@ -43,6 +44,7 @@ import com.hederahashgraph.api.proto.java.TransactionBody;
 
 import java.util.List;
 
+import static com.hedera.services.usage.SingletonEstimatorUtils.ESTIMATOR_UTILS;
 import static com.hederahashgraph.api.proto.java.SubType.TOKEN_NON_FUNGIBLE_UNIQUE;
 
 /**
@@ -122,11 +124,12 @@ class BaseOperationUsage {
 						.addMetadata(CANONICAL_NFT_METADATA))
 				.build();
 
-		final var baseUsage = TokenMintUsage.newEstimate(canonicalTxn, SINGLE_SIG_USAGE)
+		final var helper = new TxnUsageEstimator(SINGLE_SIG_USAGE, canonicalTxn, ESTIMATOR_UTILS);
+		final var estimator = new TokenMintUsage(canonicalTxn, helper);
+		final var baseUsage = estimator
 				.givenSubType(TOKEN_NON_FUNGIBLE_UNIQUE)
 				.givenExpectedLifetime(THREE_MONTHS_IN_SECONDS)
 				.get();
-		System.out.println(baseUsage);
 		return UsageAccumulator.fromGrpc(baseUsage);
 	}
 
