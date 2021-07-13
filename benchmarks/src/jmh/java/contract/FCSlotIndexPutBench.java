2703ff9ea5a5fea47f22b1f40f8967b69c1ddeea
Richard Bair <richard.bair@hedera.com>
2021-07-13 10:00:18 -0700

    Warning: not fully working

--- benchmarks/src/jmh/java/contract/FCSlotIndexPutBench.java
+++ benchmarks/src/jmh/java/contract/FCSlotIndexPutBench.java
@@ -2,9 +2,6 @@ package contract;
 
 import com.hedera.services.state.merkle.virtual.ContractKey;
 import com.hedera.services.state.merkle.virtual.ContractUint256;
-import com.hedera.services.state.merkle.virtual.persistence.FCSlotIndex;
-import com.hedera.services.state.merkle.virtual.persistence.fcmmap.FCSlotIndexUsingFCHashMap;
-import com.hedera.services.state.merkle.virtual.persistence.fcmmap.FCSlotIndexUsingMemMapFile;
 import com.hedera.services.state.submerkle.EntityId;
 import com.hedera.services.store.models.Id;
 import com.swirlds.common.constructable.ClassConstructorPair;
@@ -25,62 +22,62 @@ import java.util.concurrent.TimeUnit;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class FCSlotIndexPutBench {
-    public static final Path STORE_PATH = Path.of("store");
-    public static final Id ID = new Id(1,2,3);
-
-
-    @Param({
-//                "FCSlotIndexUsingFCHashMap",
-            "FCSlotIndexUsingMemMapFile"
-    })
-    private String fcSlotIndexImpl;
-
-    // state
-    public FCSlotIndex<ContractKey> slotIndex;
-    public Random random = new Random(1234);
-    public int iteration = 0;
-
-    @Setup(Level.Trial)
-    public void setup() {
-        System.out.println("EmptyState.setup");
-        try {
-            ConstructableRegistry.registerConstructable(new ClassConstructorPair(EntityId.class,EntityId::new));
-        } catch (ConstructableRegistryException e) {
-            e.printStackTrace();
-        }
-        try {
-            // delete any old store
-            FCVirtualMapTestUtils.deleteDirectoryAndContents(STORE_PATH);
-            // get slot index suppliers
-            switch (fcSlotIndexImpl) {
-                case "FCSlotIndexUsingMemMapFile" -> slotIndex = new FCSlotIndexUsingMemMapFile<>(STORE_PATH, "FCSlotIndexBench",
-                        1024*1024, 32, ContractKey.SERIALIZED_SIZE, 16, 16,256);
-                case "FCSlotIndexUsingFCHashMap" -> slotIndex = new FCSlotIndexUsingFCHashMap<>();
-            }
-            // reset iteration counter
-            iteration = 0;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @TearDown(Level.Trial)
-    public void tearDown() {
-        slotIndex.release();
-        FCVirtualMapTestUtils.printDirectorySize(STORE_PATH);
-    }
-
-
-    @TearDown(Level.Iteration)
-    public void count() {
-        System.out.println("slotIndex.keyCount() = " + slotIndex.keyCount());
-    }
-
-
-    @Benchmark
-    public void put() throws Exception {
-        slotIndex.putSlot(new ContractKey(ID,new ContractUint256(BigInteger.valueOf(iteration))), iteration);
-        iteration ++;
-    }
+//    public static final Path STORE_PATH = Path.of("store");
+//    public static final Id ID = new Id(1,2,3);
+//
+//
+//    @Param({
+////                "FCSlotIndexUsingFCHashMap",
+//            "FCSlotIndexUsingMemMapFile"
+//    })
+//    private String fcSlotIndexImpl;
+//
+//    // state
+//    public FCSlotIndex<ContractKey> slotIndex;
+//    public Random random = new Random(1234);
+//    public int iteration = 0;
+//
+//    @Setup(Level.Trial)
+//    public void setup() {
+//        System.out.println("EmptyState.setup");
+//        try {
+//            ConstructableRegistry.registerConstructable(new ClassConstructorPair(EntityId.class,EntityId::new));
+//        } catch (ConstructableRegistryException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            // delete any old store
+//            FCVirtualMapTestUtils.deleteDirectoryAndContents(STORE_PATH);
+//            // get slot index suppliers
+//            switch (fcSlotIndexImpl) {
+//                case "FCSlotIndexUsingMemMapFile" -> slotIndex = new FCSlotIndexUsingMemMapFile<>(STORE_PATH, "FCSlotIndexBench",
+//                        1024*1024, 32, ContractKey.SERIALIZED_SIZE, 16, 16,256);
+//                case "FCSlotIndexUsingFCHashMap" -> slotIndex = new FCSlotIndexUsingFCHashMap<>();
+//            }
+//            // reset iteration counter
+//            iteration = 0;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @TearDown(Level.Trial)
+//    public void tearDown() {
+//        slotIndex.release();
+//        FCVirtualMapTestUtils.printDirectorySize(STORE_PATH);
+//    }
+//
+//
+//    @TearDown(Level.Iteration)
+//    public void count() {
+//        System.out.println("slotIndex.keyCount() = " + slotIndex.keyCount());
+//    }
+//
+//
+//    @Benchmark
+//    public void put() throws Exception {
+//        slotIndex.putSlot(new ContractKey(ID,new ContractUint256(BigInteger.valueOf(iteration))), iteration);
+//        iteration ++;
+//    }
 
 }
--- benchmarks/src/jmh/java/fcmmap/FCVirtualMapDataStoreBench.java
+++ benchmarks/src/jmh/java/fcmmap/FCVirtualMapDataStoreBench.java
@@ -1,10 +1,6 @@
 package fcmmap;
 
 import com.hedera.services.state.merkle.MerkleAccountState;
-import com.hedera.services.state.merkle.virtual.persistence.FCSlotIndex;
-import com.hedera.services.state.merkle.virtual.persistence.FCVirtualMapLeafStore;
-import com.hedera.services.state.merkle.virtual.persistence.SlotStore;
-import com.hedera.services.state.merkle.virtual.persistence.fcmmap.FCVirtualMapLeafStoreImpl;
 import com.hedera.services.state.submerkle.EntityId;
 import com.swirlds.common.constructable.ClassConstructorPair;
 import com.swirlds.common.constructable.ConstructableRegistry;
@@ -34,99 +30,99 @@ import static fcmmap.FCVirtualMapTestUtils.*;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class FCVirtualMapDataStoreBench {
-    private static final int HASH_DATA_SIZE = DigestType.SHA_384.digestLength() + Integer.BYTES + Integer.BYTES; // int for digest type and int for byte array length
-    public static final Path STORE_PATH = Path.of("store");
-
-    @State(Scope.Benchmark)
-    public static class EmptyState {
-
-        @Param({
-                "com.hedera.services.state.merkle.virtual.persistence.fcmmap.FCSlotIndexUsingFCHashMap"
-        })
-        private String fcSlotIndexImpl;
-        @Param({
-                "com.hedera.services.state.merkle.virtual.persistence.fcmmap.MemMapSlotStore"
-        })
-        private String slotStoreImpl;
-
-        // state
-        public FCVirtualMapLeafStore<SerializableAccount, SerializableLong, MerkleAccountState> store;
-        public Random random = new Random(1234);
-        public int iteration = 0;
-
-        @Setup(Level.Iteration)
-        public void setup() {
-//            System.out.println("FCVirtualMapDataStoreBench.cleanAndCreate");
+//    private static final int HASH_DATA_SIZE = DigestType.SHA_384.digestLength() + Integer.BYTES + Integer.BYTES; // int for digest type and int for byte array length
+//    public static final Path STORE_PATH = Path.of("store");
+//
+//    @State(Scope.Benchmark)
+//    public static class EmptyState {
+//
+//        @Param({
+//                "com.hedera.services.state.merkle.virtual.persistence.fcmmap.FCSlotIndexUsingFCHashMap"
+//        })
+//        private String fcSlotIndexImpl;
+//        @Param({
+//                "com.hedera.services.state.merkle.virtual.persistence.fcmmap.MemMapSlotStore"
+//        })
+//        private String slotStoreImpl;
+//
+//        // state
+//        public FCVirtualMapLeafStore<SerializableAccount, SerializableLong, MerkleAccountState> store;
+//        public Random random = new Random(1234);
+//        public int iteration = 0;
+//
+//        @Setup(Level.Iteration)
+//        public void setup() {
+////            System.out.println("FCVirtualMapDataStoreBench.cleanAndCreate");
+////            try {
+////                ConstructableRegistry.registerConstructable(new ClassConstructorPair(EntityId.class,EntityId::new));
+////            } catch (ConstructableRegistryException e) {
+////                e.printStackTrace();
+////            }
+////            try {
+////                // delete any old store
+////                FCVirtualMapTestUtils.deleteDirectoryAndContents(STORE_PATH);
+////                // create new store dir
+////                Files.createDirectories(STORE_PATH);
+////                // get slot index suppliers
+////                Supplier<FCSlotIndex<SerializableLong>> longSlotIndexProvider = FCVirtualMapTestUtils.supplerFromClass(fcSlotIndexImpl);
+////                Supplier<FCSlotIndex<SerializableAccount>> accountIndexProvider = FCVirtualMapTestUtils.supplerFromClass(fcSlotIndexImpl);
+////                // get slot store suppler
+////                Supplier<SlotStore> slotStoreSupplier = FCVirtualMapTestUtils.supplerFromClass(slotStoreImpl);
+////                // measure the size of a MerkleAccountState
+////                int sizeOfMerkleAccountState = FCVirtualMapTestUtils.measureLengthOfSerializable(FCVirtualMapTestUtils.createRandomMerkleAccountState(1, random));
+////                // create and open store
+////                store = new FCVirtualMapLeafStoreImpl<SerializableAccount, SerializableLong, MerkleAccountState>(STORE_PATH, 250,
+////                        8 * 3, 8, sizeOfMerkleAccountState,
+////                        longSlotIndexProvider.get(), accountIndexProvider.get(),
+////                        SerializableAccount::new, SerializableLong::new, MerkleAccountState::new, slotStoreSupplier);
+////                store.open();
+////                // reset iteration counter
+////                iteration = 0;
+////            } catch (Exception e) {
+////                e.printStackTrace();
+////            }
+//        }
+//
+//        @TearDown(Level.Iteration)
+//        public void tearDown() {
+//            System.out.println("store.leafCount() = " + store.leafCount());
+//            store.release();
+//            FCVirtualMapTestUtils.printDirectorySize(STORE_PATH);
+//        }
+//    }
+//
+//    @State(Scope.Benchmark)
+//    public static class FullState extends EmptyState {
+//
+//        @Setup(Level.Iteration)
+//        public void setup() {
+//            super.setup();
 //            try {
-//                ConstructableRegistry.registerConstructable(new ClassConstructorPair(EntityId.class,EntityId::new));
-//            } catch (ConstructableRegistryException e) {
-//                e.printStackTrace();
-//            }
-//            try {
-//                // delete any old store
-//                FCVirtualMapTestUtils.deleteDirectoryAndContents(STORE_PATH);
-//                // create new store dir
-//                Files.createDirectories(STORE_PATH);
-//                // get slot index suppliers
-//                Supplier<FCSlotIndex<SerializableLong>> longSlotIndexProvider = FCVirtualMapTestUtils.supplerFromClass(fcSlotIndexImpl);
-//                Supplier<FCSlotIndex<SerializableAccount>> accountIndexProvider = FCVirtualMapTestUtils.supplerFromClass(fcSlotIndexImpl);
-//                // get slot store suppler
-//                Supplier<SlotStore> slotStoreSupplier = FCVirtualMapTestUtils.supplerFromClass(slotStoreImpl);
-//                // measure the size of a MerkleAccountState
-//                int sizeOfMerkleAccountState = FCVirtualMapTestUtils.measureLengthOfSerializable(FCVirtualMapTestUtils.createRandomMerkleAccountState(1, random));
-//                // create and open store
-//                store = new FCVirtualMapLeafStoreImpl<SerializableAccount, SerializableLong, MerkleAccountState>(STORE_PATH, 250,
-//                        8 * 3, 8, sizeOfMerkleAccountState,
-//                        longSlotIndexProvider.get(), accountIndexProvider.get(),
-//                        SerializableAccount::new, SerializableLong::new, MerkleAccountState::new, slotStoreSupplier);
-//                store.open();
-//                // reset iteration counter
-//                iteration = 0;
+//                for (int i = 0; i < 10_000_000; i++) {
+//                    store.saveLeaf(
+//                            new SerializableAccount(i,i,i),
+//                            new SerializableLong(i), FCVirtualMapTestUtils.createRandomMerkleAccountState(i, random));
+//                }
 //            } catch (Exception e) {
 //                e.printStackTrace();
 //            }
-        }
-
-        @TearDown(Level.Iteration)
-        public void tearDown() {
-            System.out.println("store.leafCount() = " + store.leafCount());
-            store.release();
-            FCVirtualMapTestUtils.printDirectorySize(STORE_PATH);
-        }
-    }
-
-    @State(Scope.Benchmark)
-    public static class FullState extends EmptyState {
-
-        @Setup(Level.Iteration)
-        public void setup() {
-            super.setup();
-            try {
-                for (int i = 0; i < 10_000_000; i++) {
-                    store.saveLeaf(
-                            new SerializableAccount(i,i,i),
-                            new SerializableLong(i), FCVirtualMapTestUtils.createRandomMerkleAccountState(i, random));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-
-    @Benchmark
-    public void _0_saveLeaf(EmptyState state) throws Exception {
-        state.store.saveLeaf(
-                new SerializableAccount(state.iteration,state.iteration,state.iteration),
-                new SerializableLong(state.iteration), FCVirtualMapTestUtils.createRandomMerkleAccountState(state.iteration, state.random));
-        state.iteration ++;
-    }
-
-    @Benchmark
-    public void _1_randomLoadLeafByPath(FullState state) throws Exception {
-        int index = state.random.nextInt(1000);
-        MerkleAccountState merkleAccountState = state.store.loadLeafValueByPath(new SerializableLong(index));
-        if (merkleAccountState == null || merkleAccountState.balance() != index) System.err.println("Got wrong value back");
-    }
+//        }
+//    }
+//
+//
+//    @Benchmark
+//    public void _0_saveLeaf(EmptyState state) throws Exception {
+//        state.store.saveLeaf(
+//                new SerializableAccount(state.iteration,state.iteration,state.iteration),
+//                new SerializableLong(state.iteration), FCVirtualMapTestUtils.createRandomMerkleAccountState(state.iteration, state.random));
+//        state.iteration ++;
+//    }
+//
+//    @Benchmark
+//    public void _1_randomLoadLeafByPath(FullState state) throws Exception {
+//        int index = state.random.nextInt(1000);
+//        MerkleAccountState merkleAccountState = state.store.loadLeafValueByPath(new SerializableLong(index));
+//        if (merkleAccountState == null || merkleAccountState.balance() != index) System.err.println("Got wrong value back");
+//    }
 
 }
