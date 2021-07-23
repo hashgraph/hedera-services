e52fa42d6c4b61882907eb8ec1d09d73fd8b9cb6
tinker-michaelj <michael.tinker@hedera.com>
2021-07-23 18:57:30 -0500

    Disable CI for now
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/store/tokens/UniqTokenViewsManager.java
+++ hedera-node/src/main/java/com/hedera/services/store/tokens/UniqTokenViewsManager.java
@@ -1,8 +1,12 @@
 package com.hedera.services.store.tokens;
 
+import com.hedera.services.state.merkle.MerkleEntityId;
+import com.hedera.services.state.merkle.MerkleToken;
+import com.hedera.services.state.merkle.MerkleUniqueToken;
 import com.hedera.services.state.merkle.MerkleUniqueTokenId;
 import com.hedera.services.state.submerkle.EntityId;
 import com.swirlds.fchashmap.FCOneToManyRelation;
+import com.swirlds.fcmap.FCMap;
 
 import java.util.Iterator;
 import java.util.function.Supplier;
@@ -10,33 +14,69 @@ import java.util.function.Supplier;
 public class UniqTokenViewsManager {
 	private final Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> nftsByType;
 	private final Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> nftsByOwner;
+	private final Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> treasuryNftsByType;
 
 	public UniqTokenViewsManager(
 			Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> nftsByType,
-			Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> nftsByOwner
+			Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> nftsByOwner,
+			Supplier<FCOneToManyRelation<EntityId, MerkleUniqueTokenId>> treasuryNftsByType
 	) {
 		this.nftsByType = nftsByType;
 		this.nftsByOwner = nftsByOwner;
+		this.treasuryNftsByType = treasuryNftsByType;
 	}
 
-	public void createAssociation(MerkleUniqueTokenId nft, EntityId firstOwner) {
-		nftsByType.get().associate(nft.tokenId(), nft);
-		nftsByOwner.get().associate(firstOwner, nft);
+	public void rebuildNotice(
+			FCMap<MerkleEntityId, MerkleToken> tokens,
+			FCMap<MerkleUniqueTokenId, MerkleUniqueToken> nfts
+	) {
+		final var curNftsByType = nftsByType.get();
+		final var curNftsByOwner = nftsByOwner.get();
+		final var curTreasuryNftsByType = treasuryNftsByType.get();
+
+		nfts.forEach((nftId, nft) -> {
+			final var tokenId = nftId.tokenId();
+			final var token = tokens.get(tokenId.asMerkle());
+			if (token == null) {
+				return;
+			}
+			curNftsByType.associate(tokenId, nftId);
+			final var ownerId = nft.getOwner();
+			final var isTreasuryOwned = token.treasury().equals(ownerId);
+			if (isTreasuryOwned) {
+				curTreasuryNftsByType.associate(tokenId, nftId);
+			} else {
+				curNftsByOwner.associate(ownerId, nftId);
+			}
+		});
 	}
 
-	public void forgetAssociation(MerkleUniqueTokenId nft, EntityId lastOwner) {
-		nftsByType.get().disassociate(nft.tokenId(), nft);
-		nftsByOwner.get().disassociate(lastOwner, nft);
+	public void mintNotice(MerkleUniqueTokenId nft, EntityId firstOwner) {
+		throw new AssertionError("Not implemented!");
 	}
 
-	public void changeAssociation(MerkleUniqueTokenId nft, EntityId prevOwner, EntityId newOwner) {
-		final var currentNftsByOwner = nftsByOwner.get();
-		currentNftsByOwner.disassociate(prevOwner, nft);
-		currentNftsByOwner.associate(newOwner, nft);
+	public void wipeNotice(MerkleUniqueTokenId nft, EntityId lastOwner) {
+		throw new AssertionError("Not implemented!");
+	}
+
+	public void burnNotice(MerkleUniqueTokenId nft) {
+		throw new AssertionError("Not implemented!");
+	}
+
+	public void nonTreasuryExchangeNotice(MerkleUniqueTokenId nft, EntityId prevOwner, EntityId newOwner) {
+		throw new AssertionError("Not implemented!");
+	}
+
+	public void treasuryExitNotice(MerkleUniqueTokenId nft, EntityId newOwner) {
+		throw new AssertionError("Not implemented!");
+	}
+
+	public void treasuryReturnNotice(MerkleUniqueTokenId nft, EntityId prevOwner) {
+		throw new AssertionError("Not implemented!");
 	}
 
 	public Iterator<MerkleUniqueTokenId> ownedAssociations(EntityId owner, int start, int end) {
-		return nftsByOwner.get().get(owner, start, end);
+		throw new AssertionError("Not implemented!");
 	}
 
 	public Iterator<MerkleUniqueTokenId> typedAssociations(EntityId type, int start, int end) {
--- hedera-node/src/test/java/com/hedera/services/store/tokens/UniqTokenViewsManagerTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/tokens/UniqTokenViewsManagerTest.java
@@ -1,18 +1,26 @@
 package com.hedera.services.store.tokens;
 
+import com.hedera.services.state.merkle.MerkleEntityId;
+import com.hedera.services.state.merkle.MerkleToken;
+import com.hedera.services.state.merkle.MerkleUniqueToken;
 import com.hedera.services.state.merkle.MerkleUniqueTokenId;
 import com.hedera.services.state.submerkle.EntityId;
 import com.swirlds.fchashmap.FCOneToManyRelation;
+import com.swirlds.fcmap.FCMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.function.BiConsumer;
 
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static com.hedera.services.state.submerkle.RichInstant.MISSING_INSTANT;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willAnswer;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -20,64 +28,67 @@ class UniqTokenViewsManagerTest {
 	@Mock
 	private Iterator<MerkleUniqueTokenId> expectedIterator;
 	@Mock
+	private FCMap<MerkleEntityId, MerkleToken> tokens;
+	@Mock
+	private FCMap<MerkleUniqueTokenId, MerkleUniqueToken> nfts;
+	@Mock
 	private FCOneToManyRelation<EntityId, MerkleUniqueTokenId> nftsByType;
 	@Mock
 	private FCOneToManyRelation<EntityId, MerkleUniqueTokenId> nftsByOwner;
+	@Mock
+	private FCOneToManyRelation<EntityId, MerkleUniqueTokenId> treasuryNftsByType;
 
 	private UniqTokenViewsManager subject;
 
 	@BeforeEach
 	void setUp() {
-		subject = new UniqTokenViewsManager(() -> nftsByType, () -> nftsByOwner);
+		subject = new UniqTokenViewsManager(() -> nftsByType, () -> nftsByOwner, () -> treasuryNftsByType);
 	}
 
 	@Test
-	void createsAssociationAsExpected() {
-		// when:
-		subject.createAssociation(oneNft, aOwner);
-
-		// then:
-		verify(nftsByType).associate(cToken, oneNft);
-		verify(nftsByOwner).associate(aOwner, oneNft);
-	}
-
-	@Test
-	void forgetsAssociationAsExpected() {
-		// when:
-		subject.forgetAssociation(oneNft, aOwner);
-
-		// then:
-		verify(nftsByType).disassociate(cToken, oneNft);
-		verify(nftsByOwner).disassociate(aOwner, oneNft);
-	}
-
-	@Test
-	void changesAssociationAsExpected() {
-		// when:
-		subject.changeAssociation(oneNft, aOwner, bOwner);
-
-		// then:
-		verify(nftsByOwner).disassociate(aOwner, oneNft);
-		verify(nftsByOwner).associate(bOwner, oneNft);
-	}
-
-	@Test
-	void ownedAssociationsAsExpected() {
-		given(nftsByOwner.get(aOwner, someStart, someEnd)).willReturn(expectedIterator);
+	void safelyRebuildDifferentiatesTreasuryOwned() {
+		// given:
+		willAnswer(invocationOnMock -> {
+			final BiConsumer<MerkleUniqueTokenId, MerkleUniqueToken> consumer = invocationOnMock.getArgument(0);
+			consumer.accept(aOneNftId, firstOwnedANft);
+			consumer.accept(bOneNftId, firstOwnedBNft);
+			consumer.accept(missingTokenNftId, tokenDeletedNft);
+			return null;
+		}).given(nfts).forEach(any());
+		// and:
+		given(tokens.get(aTokenId.asMerkle())).willReturn(aToken);
+		given(tokens.get(bTokenId.asMerkle())).willReturn(bToken);
 
 		// when:
-		final var actualIterator = subject.ownedAssociations(aOwner, someStart, someEnd);
+		subject.rebuildNotice(tokens, nfts);
 
 		// then:
-		assertSame(expectedIterator, actualIterator);
+		verify(nftsByType).associate(aTokenId, aOneNftId);
+		verify(treasuryNftsByType).associate(aTokenId, aOneNftId);
+		// and:
+		verify(nftsByType).associate(bTokenId, bOneNftId);
+		verify(nftsByOwner).associate(firstOwner, bOneNftId);
 	}
 
-	private final int someStart = 1;
-	private final int someEnd = 2;
-	private final EntityId aOwner = new EntityId(1, 2, 3);
-	private final EntityId bOwner = new EntityId(2, 3, 4);
-	private final EntityId cToken = new EntityId(3,4 , 5);
-	private final MerkleUniqueTokenId oneNft = new MerkleUniqueTokenId(cToken, 1L);
-	private final MerkleUniqueTokenId twoNft = new MerkleUniqueTokenId(cToken, 2L);
-	private final MerkleUniqueTokenId threeNft = new MerkleUniqueTokenId(cToken, 3L);
+	private final EntityId firstOwner = new EntityId(1, 2, 3);
+	private final EntityId secondOwner = new EntityId(2, 3, 4);
+	private final EntityId aTokenId = new EntityId(3, 4, 5);
+	private final EntityId bTokenId = new EntityId(4, 5, 6);
+	private final EntityId cTokenId = new EntityId(5, 6, 7);
+	private final MerkleUniqueTokenId aOneNftId = new MerkleUniqueTokenId(aTokenId, 1L);
+	private final MerkleUniqueTokenId bOneNftId = new MerkleUniqueTokenId(bTokenId, 1L);
+	private final MerkleUniqueTokenId missingTokenNftId = new MerkleUniqueTokenId(cTokenId, 666L);
+	private final MerkleToken aToken = new MerkleToken(
+			1_234_567L, 1_234L, 1,
+			"Hi", "EVERYBODY",
+			false, true, firstOwner);
+	private final MerkleToken bToken = new MerkleToken(
+			1_234_567L, 1_234L, 1,
+			"Bye", "YOU",
+			false, true, secondOwner);
+	private byte[] someMeta = "SOMETHING".getBytes(StandardCharsets.UTF_8);
+	private byte[] otherMeta = "ELSE".getBytes(StandardCharsets.UTF_8);
+	private final MerkleUniqueToken firstOwnedANft = new MerkleUniqueToken(firstOwner, someMeta, MISSING_INSTANT);
+	private final MerkleUniqueToken firstOwnedBNft = new MerkleUniqueToken(firstOwner, otherMeta, MISSING_INSTANT);
+	private final MerkleUniqueToken tokenDeletedNft = new MerkleUniqueToken(secondOwner, otherMeta, MISSING_INSTANT);
 }
