e7013eb54732bf1b4ae51e883c174a0c587a5c6b
CordonaCodeCraft <77110630+CordonaCodeCraft@users.noreply.github.com>
2021-07-23 17:27:37 +0300

    Refactoring of token grant/revoke KYC (#1841)
    * refactor HTS token grant and revoke Kyc transition logic
    
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>
    
    * Refactoring
    
    Signed-off-by: Ventsislav Stoevski <cordonahcodecraft@gmail.com>
    
    Co-authored-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/txns/token/TokenRevokeKycTransitionLogic.java
+++ hedera-node/src/main/java/com/hedera/services/txns/token/TokenRevokeKycTransitionLogic.java
@@ -21,8 +21,9 @@ package com.hedera.services.txns.token;
  */
 
 import com.hedera.services.context.TransactionContext;
-import com.hedera.services.ledger.HederaLedger;
-import com.hedera.services.store.tokens.TokenStore;
+import com.hedera.services.store.AccountStore;
+import com.hedera.services.store.TypedTokenStore;
+import com.hedera.services.store.models.Id;
 import com.hedera.services.txns.TransitionLogic;
 import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 import com.hederahashgraph.api.proto.java.TokenRevokeKycTransactionBody;
@@ -30,45 +31,62 @@ import com.hederahashgraph.api.proto.java.TransactionBody;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.FAIL_INVALID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_ACCOUNT_ID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_TOKEN_ID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OK;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
 
 public class TokenRevokeKycTransitionLogic implements TransitionLogic {
 	private static final Logger log = LogManager.getLogger(TokenRevokeKycTransitionLogic.class);
 
 	private final Function<TransactionBody, ResponseCodeEnum> SEMANTIC_CHECK = this::validate;
 
-	private final TokenStore tokenStore;
-	private final HederaLedger ledger;
 	private final TransactionContext txnCtx;
+	private final TypedTokenStore tokenStore;
+	private final AccountStore accountStore;
 
 	public TokenRevokeKycTransitionLogic(
-			TokenStore tokenStore,
-			HederaLedger ledger,
-			TransactionContext txnCtx
+			TransactionContext txnCtx,
+			TypedTokenStore tokenStore,
+			AccountStore accountStore
 	) {
 		this.txnCtx = txnCtx;
-		this.ledger = ledger;
 		this.tokenStore = tokenStore;
+		this.accountStore = accountStore;
 	}
 
 	@Override
 	public void doStateTransition() {
-		try {
-			var op = txnCtx.accessor().getTxn().getTokenRevokeKyc();
-			var token = tokenStore.resolve(op.getToken());
-			var outcome	= ledger.revokeKyc(op.getAccount(), token);
-			txnCtx.setStatus(outcome == OK ? SUCCESS : outcome);
-		} catch (Exception e) {
-			log.warn("Unhandled error while processing :: {}!", txnCtx.accessor().getSignedTxnWrapper(), e);
-			txnCtx.setStatus(FAIL_INVALID);
-		}
+
+		/* --- Translate from gRPC types --- */
+
+		final var op = txnCtx.accessor().getTxn().getTokenRevokeKyc();
+
+		final var grpcTokenId = op.getToken();
+		final var grpcAccountId = op.getAccount();
+
+		/* --- Convert to model ids --- */
+
+		final var targetTokenId = Id.fromGrpcToken(grpcTokenId);
+		final var targetAccountId = Id.fromGrpcAccount(grpcAccountId);
+
+		/* --- Load the model objects --- */
+
+		final var loadedToken = tokenStore.loadToken(targetTokenId);
+		final var loadedAccount = accountStore.loadAccount(targetAccountId);
+
+		final var tokenRelationship = tokenStore.loadTokenRelationship(loadedToken, loadedAccount);
+
+		/* --- Do the business logic --- */
+
+		tokenRelationship.changeKycState(false);
+
+		/* --- Persist the updated models --- */
+
+		tokenStore.persistTokenRelationships(List.of(tokenRelationship));
 	}
 
 	@Override
