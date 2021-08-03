b4607c2d6ced1139ffad64cda408cf597a712992
ljianghedera <leo.jiang@hedera.com>
2021-08-03 13:35:02 -0500

    Add license header.
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>

--- test-clients/src/main/java/com/hedera/services/yahcli/commands/system/SysFreezeCommand.java
+++ test-clients/src/main/java/com/hedera/services/yahcli/commands/system/SysFreezeCommand.java
@@ -1,14 +1,32 @@
 package com.hedera.services.yahcli.commands.system;
 
+/*-
+ * ‌
+ * Hedera Services Test Clients
+ * ​
+ * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
 import com.hedera.services.yahcli.Yahcli;
 import com.hedera.services.yahcli.suites.FreezeSuite;
 import picocli.CommandLine;
 
-import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.concurrent.Callable;
 
 import static com.hedera.services.yahcli.config.ConfigUtils.configFrom;
@@ -39,7 +57,6 @@ public class SysFreezeCommand implements Callable<Integer> {
 
 		return 0;
 	}
-
 
 	private Instant ensureFreezeStartTime(String timeStampInStr) {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withZone(ZoneId.systemDefault());
