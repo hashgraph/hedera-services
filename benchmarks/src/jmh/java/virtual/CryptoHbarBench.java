05e0ea18a1dd87af3fbc3ff3af6d2ce8a8c13e30
Richard Bair <richard.bair@hedera.com>
2021-08-09 09:24:36 -0700

    Latest benchmarking fixes

--- benchmarks/src/jmh/java/virtual/CryptoHbarBench.java
+++ benchmarks/src/jmh/java/virtual/CryptoHbarBench.java
@@ -10,8 +10,17 @@ import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  */
@@ -20,6 +29,7 @@ import java.util.concurrent.atomic.AtomicLong;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class CryptoHbarBench extends VFCMapBenchBase<VFCMapBenchBase.Id, VFCMapBenchBase.Account> {
+    private static final ThreadGroup PREPPER_GROUP = new ThreadGroup("PrepperGroup");
 
     @Param({"10000"})
     public int targetOpsPerSecond;
@@ -33,12 +43,20 @@ public class CryptoHbarBench extends VFCMapBenchBase<VFCMapBenchBase.Id, VFCMapB
     @Param({"lmdbMem", "lmdb", "rocksdb", "jasperdb"})
     public DataSourceType dsType;
 
+    @Param({"5"})
+    public int prepperThreadCount;
+
     // This is the map we will be testing!
     private VirtualMap<Id, Account> virtualMap;
+
+    private ExecutorService prepService;
+
+    private AtomicReference<CompletableFuture<List<Account[]>>> accountsFutureRef = new AtomicReference<>();
 
     @Setup
     public void prepare() throws Exception {
         virtualMap = createMap(dsType, Id.SERIALIZED_SIZE, Id::new, Account.SERIALIZED_SIZE, Account::new, numEntities);
+        prepService = Executors.newFixedThreadPool(prepperThreadCount, Pipeline.threadFactory("Preppers", PREPPER_GROUP));
 
         if (preFill) {
             for (int i = 0; i < numEntities; i++) {
@@ -68,6 +86,8 @@ public class CryptoHbarBench extends VFCMapBenchBase<VFCMapBenchBase.Id, VFCMapB
         }
 
         printDataStoreSize();
+
+        prepNextRound();
     }
 
     @TearDown
@@ -76,57 +96,67 @@ public class CryptoHbarBench extends VFCMapBenchBase<VFCMapBenchBase.Id, VFCMapB
         /*store.close();*/
     }
 
+    private void prepNextRound() {
+        final var cf = new CompletableFuture<List<Account[]>>();
+        accountsFutureRef.set(cf);
+        final var accounts = new ArrayList<Account[]>(targetOpsPerSecond);
+        final var futures = new ArrayList<Future<List<Account[]>>>(prepperThreadCount);
+        for (int i=0; i<prepperThreadCount; i++) {
+            final var numOps = targetOpsPerSecond / prepperThreadCount;
+            final var future = prepService.submit(() -> {
+                List<Account[]> threadAccounts = new ArrayList<>(numOps);
+                for (int j=0; j<numOps; j++) {
+                    final var senderId = new Id(rand.nextInt((int)numEntities));
+                    final var receiverId = new Id(rand.nextInt((int)numEntities));
+
+                    final var sender = virtualMap.getForModify(senderId);
+                    final var receiver = virtualMap.getForModify(receiverId);
+                    threadAccounts.add(new Account[] {sender, receiver});
+                }
+                return threadAccounts;
+            });
+            futures.add(future);
+        }
+
+        for (final var future : futures) {
+            try {
+                accounts.addAll(future.get()); // block on each
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        cf.complete(accounts);
+    }
+
     /**
      * Benchmarks update operations of an existing tree.
      */
     @Benchmark
-    public void update() {
-//        System.out.println(virtualMap.toDebugString());
-        // Start modifying the new fast copy
-        for (int j=0; j<targetOpsPerSecond; j++) {
-            // Read the two accounts involved in the token transfer
-            // Debit and Credit them for hbar balances
-            final var senderId = new Id(rand.nextInt((int)numEntities));
-            final var receiverId = new Id(rand.nextInt((int)numEntities));
+    public void handleTransactions() throws Exception {
+        final var f = accountsFutureRef.getAndSet(null);
+        final var accounts = f.get();
+        prepNextRound();
 
-            final var sender = virtualMap.getForModify(senderId);
-            final var receiver = virtualMap.getForModify(receiverId);
-
-            if (sender == null) {
-                try {
-                    System.err.println("Sender was null: id=" + senderId.getNum() +
-                            ", path=" + virtualMap.getDataSource().loadLeafPath(senderId));
-                    virtualMap.getForModify(senderId);
-                } catch (Exception e) {
-                    System.err.println("Sender was null. Could not load leaf path");
-                }
-            } else if (receiver == null) {
-                try {
-                    System.err.println("Receiver was null: id=" + receiverId.getNum() +
-                            ", path=" + virtualMap.getDataSource().loadLeafPath(receiverId));
-                    ;
-                } catch (Exception e) {
-                    System.err.println("Receiver was null. Could not load leaf path");
-                }
-            } else {
-                final var tinyBars = rand.nextInt(10);
-                sender.setHbarBalance(sender.getHbarBalance() - tinyBars);
-                receiver.setHbarBalance(receiver.getHbarBalance() + tinyBars);
-            }
+        for (final var arr : accounts) {
+            final var sender = arr[0];
+            final var receiver = arr[1];
+            final var tinyBars = rand.nextInt(10);
+            sender.setHbarBalance(sender.getHbarBalance() - tinyBars);
+            receiver.setHbarBalance(receiver.getHbarBalance() + tinyBars);
         }
 
-//        System.out.println(virtualMap.toDebugString());
         virtualMap = pipeline.endRound(virtualMap);
     }
 
     public static void main(String[] args) throws Exception {
         final var test = new CryptoHbarBench();
-        test.numEntities = 1_000_000;
+        test.numEntities = 100_000;
         test.dsType = DataSourceType.jasperdb;
         test.preFill = true;
         test.targetOpsPerSecond = 10_000;
+        test.prepperThreadCount = 5;
         test.prepare();
-        System.out.println(test.virtualMap.toDebugString());
 
         for (int i=0; i< test.numEntities; i++) {
             final var value = test.virtualMap.getForModify(new Id(i));
@@ -139,7 +169,7 @@ public class CryptoHbarBench extends VFCMapBenchBase<VFCMapBenchBase.Id, VFCMapB
         final var start = new AtomicLong();
         while (true) {
             start.set(System.nanoTime());
-            test.update();
+            test.handleTransactions();
             totalCount += test.targetOpsPerSecond;
             printTestUpdate(start, test.targetOpsPerSecond, totalCount, "Iteration");
         }
