a649ec8b6da6739a0bcc26ae29af024fb25a26f8
jasperpotts <jasper.potts@hedera.com>
2021-08-18 09:38:50 -0700

    Removed typo in MerkleAccount

--- hedera-node/src/main/java/com/hedera/services/state/merkle/MerkleAccount.java
+++ hedera-node/src/main/java/com/hedera/services/state/merkle/MerkleAccount.java
@@ -309,7 +309,7 @@ public class MerkleAccount extends AbstractNaryMerkleInternal implements MerkleI
 		write(buffer, state());
 
 		FCQueue<ExpirableTxnRecord> records = records();
-		buffer.putInt(records.size());Cryptography
+		buffer.putInt(records.size());
 		for (ExpirableTxnRecord record : records) {
 			write(buffer, record);
 		}
