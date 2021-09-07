35d546565b28227aec9a6f586a825ea233797be1
ljianghedera <55505519+ljianghedera@users.noreply.github.com>
2021-09-07 15:08:19 -0500

    Initial cut of move TokenCreate fee to non-gRpc based calculation. (#2090)
    * Initial cut of move TokenCreate fee to non-gRpc based calculation.
    
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>
    
    * Replace gRpc based token UsageEstimator with UsageAccumulator.
    
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>
    
    * Fix Sonarcloud code smell issues.
    
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>
    
    * Code coverage.
    
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>
    
    * Clean up.
    
    Signed-off-by: ljianghedera <leo.jiang@hedera.com>
    
    * More clean up and lines coverage.
    
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>
    
    Co-authored-by: Quan Nguyen <quan.nguyen@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/fees/calculation/utils/AccessorBasedUsages.java
+++ hedera-node/src/main/java/com/hedera/services/fees/calculation/utils/AccessorBasedUsages.java
@@ -1,11 +1,22 @@
 package com.hedera.services.fees.calculation.utils;
 
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.ConsensusSubmitMessage;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.CryptoTransfer;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.FileAppend;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenCreate;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenFeeScheduleUpdate;
+
+import java.util.EnumSet;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 /*-
  * ‌
  * Hedera Services Node
- * ​
+ *
  * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
- * ​
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,23 +43,10 @@ import com.hedera.services.usage.token.TokenOpsUsage;
 import com.hedera.services.utils.TxnAccessor;
 import com.hederahashgraph.api.proto.java.HederaFunctionality;
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import java.util.EnumSet;
-
-import static com.hederahashgraph.api.proto.java.HederaFunctionality.ConsensusSubmitMessage;
-import static com.hederahashgraph.api.proto.java.HederaFunctionality.CryptoTransfer;
-import static com.hederahashgraph.api.proto.java.HederaFunctionality.FileAppend;
-import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenFeeScheduleUpdate;
-
 @Singleton
 public class AccessorBasedUsages {
-	private static final EnumSet<HederaFunctionality> supportedOps = EnumSet.of(
-			FileAppend,
-			CryptoTransfer,
-			ConsensusSubmitMessage,
-			TokenFeeScheduleUpdate
-	);
+	private static final EnumSet<HederaFunctionality> supportedOps = EnumSet.of(FileAppend, CryptoTransfer,
+			ConsensusSubmitMessage, TokenFeeScheduleUpdate, TokenCreate);
 
 	private final ExpandHandleSpanMapAccessor spanMapAccessor = new ExpandHandleSpanMapAccessor();
 
@@ -61,14 +59,9 @@ public class AccessorBasedUsages {
 	private final GlobalDynamicProperties dynamicProperties;
 
 	@Inject
-	public AccessorBasedUsages(
-			FileOpsUsage fileOpsUsage,
-			TokenOpsUsage tokenOpsUsage,
-			CryptoOpsUsage cryptoOpsUsage,
-			OpUsageCtxHelper opUsageCtxHelper,
-			ConsensusOpsUsage consensusOpsUsage,
-			GlobalDynamicProperties dynamicProperties
-	) {
+	public AccessorBasedUsages(FileOpsUsage fileOpsUsage, TokenOpsUsage tokenOpsUsage, CryptoOpsUsage cryptoOpsUsage,
+			OpUsageCtxHelper opUsageCtxHelper, ConsensusOpsUsage consensusOpsUsage,
+			GlobalDynamicProperties dynamicProperties) {
 		this.fileOpsUsage = fileOpsUsage;
 		this.tokenOpsUsage = tokenOpsUsage;
 		this.cryptoOpsUsage = cryptoOpsUsage;
@@ -92,6 +85,8 @@ public class AccessorBasedUsages {
 			estimateFeeScheduleUpdate(sigUsage, accessor, baseMeta, into);
 		} else if (function == FileAppend) {
 			estimateFileAppend(sigUsage, accessor, baseMeta, into);
+		} else if (function == TokenCreate) {
+			estimateTokenCreate(sigUsage, accessor, baseMeta, into);
 		}
 	}
 
@@ -99,46 +94,35 @@ public class AccessorBasedUsages {
 		return supportedOps.contains(function);
 	}
 
-	private void estimateFeeScheduleUpdate(
-			SigUsage sigUsage,
-			TxnAccessor accessor,
-			BaseTransactionMeta baseMeta,
-			UsageAccumulator into
-	) {
+	private void estimateFeeScheduleUpdate(SigUsage sigUsage, TxnAccessor accessor, BaseTransactionMeta baseMeta,
+			UsageAccumulator into) {
 		final var op = accessor.getTxn().getTokenFeeScheduleUpdate();
 		final var opMeta = spanMapAccessor.getFeeScheduleUpdateMeta(accessor);
 		final var usageCtx = opUsageCtxHelper.ctxForFeeScheduleUpdate(op);
 		tokenOpsUsage.feeScheduleUpdateUsage(sigUsage, baseMeta, opMeta, usageCtx, into);
 	}
 
-	private void estimateFileAppend(
-			SigUsage sigUsage,
-			TxnAccessor accessor,
-			BaseTransactionMeta baseMeta,
-			UsageAccumulator into
-	) {
+	private void estimateFileAppend(SigUsage sigUsage, TxnAccessor accessor, BaseTransactionMeta baseMeta,
+			UsageAccumulator into) {
 		final var opMeta = opUsageCtxHelper.metaForFileAppend(accessor.getTxn());
 		fileOpsUsage.fileAppendUsage(sigUsage, opMeta, baseMeta, into);
 	}
 
-	private void estimateCryptoTransfer(
-			SigUsage sigUsage,
-			TxnAccessor accessor,
-			BaseTransactionMeta baseMeta,
-			UsageAccumulator into
-	) {
+	private void estimateCryptoTransfer(SigUsage sigUsage, TxnAccessor accessor, BaseTransactionMeta baseMeta,
+			UsageAccumulator into) {
 		final var xferMeta = accessor.availXferUsageMeta();
 		xferMeta.setTokenMultiplier(dynamicProperties.feesTokenTransferUsageMultiplier());
 		cryptoOpsUsage.cryptoTransferUsage(sigUsage, xferMeta, baseMeta, into);
 	}
 
-	private void estimateSubmitMessage(
-			SigUsage sigUsage,
-			TxnAccessor accessor,
-			BaseTransactionMeta baseMeta,
-			UsageAccumulator into
-	) {
+	private void estimateSubmitMessage(SigUsage sigUsage, TxnAccessor accessor, BaseTransactionMeta baseMeta,
+			UsageAccumulator into) {
 		final var submitMeta = accessor.availSubmitUsageMeta();
 		consensusOpsUsage.submitMessageUsage(sigUsage, submitMeta, baseMeta, into);
+	}
+	private void estimateTokenCreate(SigUsage sigUsage, TxnAccessor accessor, BaseTransactionMeta baseMeta,
+			UsageAccumulator into) {
+		final var tokenCreateMeta = accessor.getSpanMapAccessor().getTokenCreateMeta(accessor);
+		tokenOpsUsage.tokenCreateUsage(sigUsage, baseMeta, tokenCreateMeta, into);
 	}
 }
