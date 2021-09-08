4d56c557046e5f63a7df65d0c233c6cfc4ccca42
anighanta <anirudh.ghanta@hedera.com>
2021-09-08 10:31:41 -0500

    address issue #2100
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

---
+++ hedera-node/src/main/java/com/hedera/services/txns/token/TokenOpsValidator.java
@@ -0,0 +1,10 @@
+package com.hedera.services.txns.validation;
+
+import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
+import com.hederahashgraph.api.proto.java.TransactionBody;
+
+public class TokenOpsValidator {
+	public static ResponseCodeEnum validate(TransactionBody txnBody, OptionValidator validator) {
+
+	}
+}
