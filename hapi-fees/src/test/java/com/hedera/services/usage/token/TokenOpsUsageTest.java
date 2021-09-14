7b6abfbd8238f52d80921364c9071356f96d95d5
Quan Nguyen <45947300+qnswirlds@users.noreply.github.com>
2021-09-14 11:11:32 -0500

    02139: Add comments in empty methods - Round 1 (#2185)
    * Add comments in empty methods. #2139
    
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>
    
    * Consolidate tests in ScreenedSysFilePropsTest suite. #2172
    
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>
    
    * More clean-up for #2139
    
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>
    
    * Make all comments consistent.
    
    Signed-off-by: Quan Nguyen <quan.nguyen@hedera.com>

--- hapi-fees/src/test/java/com/hedera/services/usage/token/TokenOpsUsageTest.java
+++ hapi-fees/src/test/java/com/hedera/services/usage/token/TokenOpsUsageTest.java
@@ -9,9 +9,9 @@ package com.hedera.services.usage.token;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,18 +40,17 @@ import static com.hedera.services.test.AdapterUtils.feeDataFrom;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TokenOpsUsageTest {
-	private final TokenOpsUsage subject = new TokenOpsUsage();
+	private static final TokenOpsUsage subject = new TokenOpsUsage();
 
 	@Test
 	void knowsBytesNeededToReprCustomFeeSchedule() {
-		// setup:
 		final var expectedHbarFixed = FeeBuilder.LONG_SIZE + FeeBuilder.BASIC_ENTITY_ID_SIZE;
 		final var expectedHtsFixed = FeeBuilder.LONG_SIZE + 2 * FeeBuilder.BASIC_ENTITY_ID_SIZE;
 		final var expectedFractional = 4 * FeeBuilder.LONG_SIZE + FeeBuilder.BASIC_ENTITY_ID_SIZE;
 		final var expectedRoyaltyNoFallback = 2 * FeeBuilder.LONG_SIZE + FeeBuilder.BASIC_ENTITY_ID_SIZE;
 		final var expectedRoyaltyHtsFallback = 3 * FeeBuilder.LONG_SIZE + 2 * FeeBuilder.BASIC_ENTITY_ID_SIZE;
 		final var expectedRoyaltyHbarFallback = 3 * FeeBuilder.LONG_SIZE + FeeBuilder.BASIC_ENTITY_ID_SIZE;
-		// given:
+
 		final var perHbarFixedFee = subject.bytesNeededToRepr(1, 0, 0, 0, 0, 0);
 		final var perHtsFixedFee = subject.bytesNeededToRepr(0, 1, 0, 0, 0, 0);
 		final var perFracFee = subject.bytesNeededToRepr(0, 0, 1, 0, 0, 0);
@@ -60,7 +59,6 @@ class TokenOpsUsageTest {
 		final var perRoyaltyHbarFallbackFee = subject.bytesNeededToRepr(0, 0, 0, 0, 0, 1);
 		final var oneOfEach = subject.bytesNeededToRepr(1, 1, 1, 1, 1, 1);
 
-		// expect:
 		assertEquals(expectedHbarFixed, perHbarFixedFee);
 		assertEquals(expectedHtsFixed, perHtsFixedFee);
 		assertEquals(expectedFractional, perFracFee);
@@ -79,14 +77,13 @@ class TokenOpsUsageTest {
 
 	@Test
 	void canCountFeeTypes() {
-		// setup:
 		final List<CustomFee> aSchedule = new ArrayList<>();
 		aSchedule.add(CustomFee.newBuilder().setFixedFee(FixedFee.getDefaultInstance()).build());
 		aSchedule.add(CustomFee.newBuilder().setFixedFee(FixedFee.newBuilder()
-				.setDenominatingTokenId(IdUtils.asToken("1.2.3")))
+						.setDenominatingTokenId(IdUtils.asToken("1.2.3")))
 				.build());
 		aSchedule.add(CustomFee.newBuilder().setFixedFee(FixedFee.newBuilder()
-				.setDenominatingTokenId(IdUtils.asToken("1.2.3")))
+						.setDenominatingTokenId(IdUtils.asToken("1.2.3")))
 				.build());
 		aSchedule.add(CustomFee.newBuilder().setFractionalFee(FractionalFee.getDefaultInstance()).build());
 		aSchedule.add(CustomFee.newBuilder().setFractionalFee(FractionalFee.getDefaultInstance()).build());
@@ -98,43 +95,33 @@ class TokenOpsUsageTest {
 				.setFallbackFee(FixedFee.newBuilder().setDenominatingTokenId(IdUtils.asToken("1.2.3"))
 						.build())).build());
 
-		// given:
 		final var expected = subject.bytesNeededToRepr(1, 2, 3, 1, 1, 1);
 
-		// when:
 		final var actual = subject.bytesNeededToRepr(aSchedule);
 
-		// expect:
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	void accumulatesBptAndRbhAsExpected() {
-		// setup:
 		final var now = 1_234_567L;
 		final var lifetime = 7776000L;
 		final var expiry = now + lifetime;
-		final var curSize = subject.bytesNeededToRepr(1, 0 ,1, 1, 0, 1);
-		final var newSize = subject.bytesNeededToRepr(2, 1 ,0, 2, 1, 0);
+		final var curSize = subject.bytesNeededToRepr(1, 0, 1, 1, 0, 1);
+		final var newSize = subject.bytesNeededToRepr(2, 1, 0, 2, 1, 0);
 		final var ctx = new ExtantFeeScheduleContext(expiry, curSize);
 		final var opMeta = new FeeScheduleUpdateMeta(now, newSize);
-		// and:
 		final var sigUsage = new SigUsage(1, 2, 3);
 		final var baseMeta = new BaseTransactionMeta(50, 0);
-		// and:
 		final var exp = new UsageAccumulator();
 		exp.resetForTransaction(baseMeta, sigUsage);
 		exp.addBpt(newSize + FeeBuilder.BASIC_ENTITY_ID_SIZE);
 		exp.addRbs((newSize - curSize) * lifetime);
 
-		// given:
 		final var ans = new UsageAccumulator();
 
-		// when:
 		subject.feeScheduleUpdateUsage(sigUsage, baseMeta, opMeta, ctx, ans);
 
-		// then:
 		assertEquals(feeDataFrom(exp), feeDataFrom(ans));
 	}
-
 }
