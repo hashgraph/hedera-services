2097ca1784eb381e331370e1a20a9503b705fd2b
jasperpotts <jasper.potts@hedera.com>
2021-10-14 17:50:23 -0700

    Changed add and update benchmark to have much bigger batches, so that they are not overly effected by per-flush overheads which was causing bad results.

--- benchmarks/src/jmh/java/jasperdb/virtualdatasource/AddBench.java
+++ benchmarks/src/jmh/java/jasperdb/virtualdatasource/AddBench.java
@@ -20,18 +20,23 @@ import static utils.CommonTestUtils.hash;
 
 @SuppressWarnings("DefaultAnnotationParam")
 @Warmup(iterations = 2, time = 10, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 20, time = 10, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 60, time = 10, timeUnit = TimeUnit.SECONDS)
 @Fork(1)
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class AddBench {
+    // This is much larger than realistic but keeps files big enough to measure writing performance
+    public static final int NUMBER_OF_LEAVES_ADDED_PER_ROUND = 10_000;
+    public static final int NUMBER_OF_ROUNDS_PER_FLUSH = 20;
+    public static final int NUMBER_OF_LEAVES_ADDED_PER_FLUSH =
+            NUMBER_OF_ROUNDS_PER_FLUSH * NUMBER_OF_LEAVES_ADDED_PER_ROUND;
 
     @Benchmark
     public void add500Leaves(DatabaseMergingState databaseState) throws IOException {
         final long firstLeafPath = databaseState.dataSource.getFirstLeafPath();
         final long lastLeafPath = databaseState.dataSource.getLastLeafPath();
-        final long newFirstLeafPath = firstLeafPath + 500;
-        final long newLastLeafPath = lastLeafPath + 1000;
+        final long newFirstLeafPath = firstLeafPath + NUMBER_OF_LEAVES_ADDED_PER_FLUSH;
+        final long newLastLeafPath = lastLeafPath + NUMBER_OF_LEAVES_ADDED_PER_FLUSH + NUMBER_OF_LEAVES_ADDED_PER_FLUSH;
         var internalRecordStream = LongStream.range(firstLeafPath,newFirstLeafPath)
                 .mapToObj(path -> new VirtualInternalRecord(path, hash((int)path)));
         var leafRecordStream = LongStream.range(lastLeafPath,newLastLeafPath+1)
--- benchmarks/src/jmh/java/jasperdb/virtualdatasource/UpdateBench.java
+++ benchmarks/src/jmh/java/jasperdb/virtualdatasource/UpdateBench.java
@@ -31,9 +31,16 @@ import static utils.CommonTestUtils.hash;
 @BenchmarkMode(Mode.Throughput)
 @OutputTimeUnit(TimeUnit.SECONDS)
 public class UpdateBench {
+    public static final int NUMBER_OF_LEAVES_CHANGED_PER_ROUND = 1000; // 2x 500 required
+    public static final int NUMBER_OF_INTERNAL_NODES_CHANGED_PER_LEAF = 25;
+    public static final int NUMBER_OF_ROUNDS_PER_FLUSH = 20; 
+    public static final int NUMBER_OF_LEAVES_CHANGED_PER_FLUSH =
+            NUMBER_OF_ROUNDS_PER_FLUSH * NUMBER_OF_LEAVES_CHANGED_PER_ROUND;
+    public static final int NUMBER_OF_INTERNALS_CHANGED_PER_FLUSH = 
+            NUMBER_OF_LEAVES_CHANGED_PER_FLUSH * NUMBER_OF_INTERNAL_NODES_CHANGED_PER_LEAF;
     public Random random;
-    private final long[] randomInternalPaths = new long[5000];
-    private final long[] randomLeafPaths = new long[500];
+    private final long[] randomInternalPaths = new long[NUMBER_OF_INTERNALS_CHANGED_PER_FLUSH];
+    private final long[] randomLeafPaths = new long[NUMBER_OF_LEAVES_CHANGED_PER_FLUSH];
 
     @Setup(Level.Trial)
     public void setup() throws IOException {
@@ -42,19 +49,19 @@ public class UpdateBench {
 
     @Setup(Level.Invocation)
     public void setupInvocation(DatabaseState databaseState) {
-        for (int i = 0; i < 5000; i++) {
+        for (int i = 0; i < NUMBER_OF_INTERNALS_CHANGED_PER_FLUSH; i++) {
             randomInternalPaths[i] = random.nextInt(databaseState.initialDataSize);
         }
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < NUMBER_OF_LEAVES_CHANGED_PER_FLUSH; i++) {
             randomLeafPaths[i] = databaseState.initialDataSize + randomInternalPaths[i];
         }
     }
 
     @Benchmark
-    public void update500leaves(DatabaseMergingState databaseState) throws IOException {
-        var internalRecordStream = IntStream.range(0,5000)
+    public void updateLeavesAndInternals(DatabaseMergingState databaseState) throws IOException {
+        var internalRecordStream = IntStream.range(0,NUMBER_OF_INTERNALS_CHANGED_PER_FLUSH)
                 .mapToObj(i -> new VirtualInternalRecord(randomInternalPaths[i], hash((int)randomInternalPaths[i])));
-        var leafRecordStream = IntStream.range(0,500)
+        var leafRecordStream = IntStream.range(0,NUMBER_OF_LEAVES_CHANGED_PER_FLUSH)
                 .mapToObj(i -> {
                     final long path = randomLeafPaths[i];
                     return new VirtualLeafRecord<>(
