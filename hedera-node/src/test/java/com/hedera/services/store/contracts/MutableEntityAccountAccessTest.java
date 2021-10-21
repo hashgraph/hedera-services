31173bd9eb879e99d5999d456dc67387100def59
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2021-10-21 15:48:41 -0500

    fix all sonar issues
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/store/contracts/MutableEntityAccountAccessTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/contracts/MutableEntityAccountAccessTest.java
@@ -5,6 +5,8 @@ import com.hedera.services.state.virtual.ContractKey;
 import com.hedera.services.state.virtual.ContractValue;
 import com.hedera.services.state.virtual.VirtualBlobKey;
 import com.hedera.services.state.virtual.VirtualBlobValue;
+import com.hedera.test.utils.IdUtils;
+import com.hederahashgraph.api.proto.java.AccountID;
 import com.swirlds.virtualmap.VirtualMap;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,24 +14,34 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 @ExtendWith(MockitoExtension.class)
 class MutableEntityAccountAccessTest {
 	@Mock
 	private HederaLedger ledger;
 	@Mock
-	private VirtualMap<ContractKey, ContractValue> storage;
+	private Supplier<VirtualMap<ContractKey, ContractValue>> storage;
 	@Mock
-	private VirtualMap<VirtualBlobKey, VirtualBlobValue> bytecode;
+	private Supplier<VirtualMap<VirtualBlobKey, VirtualBlobValue>> bytecode;
 
 	private MutableEntityAccess subject;
 
+	private AccountID id = IdUtils.asAccount("0.0.1234");
+
 	@BeforeEach
 	void setUp() {
-
+		subject = new MutableEntityAccess(ledger, storage, bytecode);
+		when(ledger.getBalance(id)).thenReturn(1234L);
 	}
 
+	// need a lot of test coverage. Adding test to resolve sonar issues
 	@Test
-	void testsSpawn(){
-
+	void testBalance() {
+		final var balance = subject.getBalance(id);
+		assertEquals(ledger.getBalance(id), balance);
 	}
 }
--- hedera-node/src/test/java/com/hedera/services/store/contracts/StaticEntityAccessTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/contracts/StaticEntityAccessTest.java
@@ -1,12 +1,16 @@
 package com.hedera.services.store.contracts;
 
 import com.hedera.services.context.primitives.StateView;
+import com.hedera.services.ledger.accounts.HederaAccountCustomizer;
 import com.hedera.services.state.merkle.MerkleAccount;
+import com.hedera.services.state.submerkle.EntityId;
 import com.hedera.services.state.virtual.ContractKey;
 import com.hedera.services.state.virtual.ContractValue;
 import com.hedera.services.state.virtual.VirtualBlobKey;
 import com.hedera.services.state.virtual.VirtualBlobValue;
 import com.hedera.services.utils.EntityNum;
+import com.hedera.test.utils.IdUtils;
+import com.hederahashgraph.api.proto.java.AccountID;
 import com.swirlds.merkle.map.MerkleMap;
 import com.swirlds.virtualmap.VirtualMap;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +19,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StaticEntityAccessTest {
@@ -30,17 +36,31 @@ class StaticEntityAccessTest {
 
 	private StaticEntityAccess subject;
 
+	private AccountID id = IdUtils.asAccount("0.0.1234");
+	MerkleAccount someAccount = new HederaAccountCustomizer()
+			.isReceiverSigRequired(false)
+			.proxy(EntityId.MISSING_ENTITY_ID)
+			.isDeleted(false)
+			.expiry(1234L)
+			.memo("")
+			.isSmartContract(false)
+			.autoRenewPeriod(1234L)
+			.customizing(new MerkleAccount());
+
 	@BeforeEach
 	void setUp() {
 		given(stateView.storage()).willReturn(blobs);
 		given(stateView.accounts()).willReturn(accounts);
 		given(stateView.contractStorage()).willReturn(storage);
+		given(accounts.get(EntityNum.fromAccountId(id))).willReturn(someAccount);
 
 		subject = new StaticEntityAccess(stateView);
 	}
 
+	// need a lot of test coverage. Adding test to resolve sonar issues
 	@Test
-	void testsSpawn() {
-
+	void testBalance() {
+		final var balance = subject.getBalance(id);
+		assertEquals(someAccount.getBalance(), balance);
 	}
 }
