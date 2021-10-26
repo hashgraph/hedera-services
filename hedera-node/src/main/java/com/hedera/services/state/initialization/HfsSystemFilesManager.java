39a3854009f3f6edfc1fd091a8e43114ca7cc3ca
tinker-michaelj <michael.tinker@hedera.com>
2021-10-26 18:48:00 -0500

    Fix typo
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/state/initialization/HfsSystemFilesManager.java
+++ hedera-node/src/main/java/com/hedera/services/state/initialization/HfsSystemFilesManager.java
@@ -189,7 +189,7 @@ public final class HfsSystemFilesManager implements SystemFilesManager {
 			if (!hfs.exists(disFid)) {
 				materialize(disFid, systemFileInfo(), new byte[0]);
 			} else if (!specialFiles.contains(disFid)) {
-				/* This can be the case for file 0.0.150, whose metadata was
+				/* This can be the case for file 0.0.150, whose metadata had
 				* been created for the legacy MerkleDiskFs. But whatever its
 				* contents were doesn't matter now. Just make sure it exists
 				* in the MerkleSpecialFiles! */
