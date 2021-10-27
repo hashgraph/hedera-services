8bc2be3721ba055550c18dd3a984a2db632cd565
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2021-10-27 14:17:22 +0300

    Remove unnecessary change
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/src/main/java/com/hedera/services/state/submerkle/ExpirableTxnRecord.java
+++ hedera-node/src/main/java/com/hedera/services/state/submerkle/ExpirableTxnRecord.java
@@ -547,14 +547,6 @@ public class ExpirableTxnRecord implements FCQueueElement {
 			return new ExpirableTxnRecord(this);
 		}
 
-		public SolidityFnResult getContractCallResult() {
-			return contractCallResult;
-		}
-
-		public SolidityFnResult getContractCreateResult() {
-			return contractCreateResult;
-		}
-
 		public Builder clear() {
 			fee = 0;
 			txnId = null;
