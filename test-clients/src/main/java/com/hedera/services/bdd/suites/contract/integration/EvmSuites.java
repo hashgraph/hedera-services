ef8e67adf2793fdc5010b7c05b9fe6a0b772c628
Yoan Sredkov <yoansredkov@gmail.com>
2021-11-01 16:14:05 +0200

    hbar transfers between contracts
    Signed-off-by: Yoan Sredkov <yoansredkov@gmail.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/contract/integration/EvmSuites.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/contract/integration/EvmSuites.java
@@ -3,6 +3,7 @@ package com.hedera.services.bdd.suites.contract.integration;
 import com.google.common.base.Splitter;
 import com.hedera.services.bdd.spec.HapiApiSpec;
 import com.hedera.services.bdd.spec.infrastructure.meta.ContractResources;
+import com.hedera.services.bdd.spec.persistence.Contract;
 import com.hedera.services.bdd.suites.HapiApiSuite;
 import com.hedera.services.bdd.suites.contract.openzeppelin.ERC1155ContractInteractions;
 import org.apache.logging.log4j.LogManager;
@@ -12,21 +13,27 @@ import java.util.List;
 
 import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.FACTORY_SELF_DESTRUCT_CONSTRUCTOR_CONTRACT;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountInfo;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getContractInfo;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getContractRecords;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileAppend;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileCreate;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.updateLargeFile;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.ACCOUNT_DELETED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_DELETED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OK;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.TRANSACTION_OVERSIZE;
+
+/**
+ *  Major todo - find how to use HBars in smart contracts
+ */
 
 public class EvmSuites extends HapiApiSuite {
     private final Logger LOGGER = LogManager.getLogger();
@@ -39,7 +46,8 @@ public class EvmSuites extends HapiApiSuite {
     protected List<HapiApiSpec> getSpecsInSuite() {
         return List.of(
 //                HSCS_EVM_008_SelfDesctructUpdatesHederaAccount(),
-                HSCS_EVM_0014_ContractCreateFailsOnLargeContracts()
+//                HSCS_EVM_0014_ContractCreateFailsOnLargeContracts(),
+                HSCS_EVM_005_TransferOfHBarsWorksBetweenContracts()
         );
     }
 
@@ -64,12 +72,13 @@ public class EvmSuites extends HapiApiSuite {
                 );
     }
 
+    // Done - for now
     // Validates that ContractCreate fails to create a contract > 24KiB
     // By uploading a LARGE file with multiple file appends, it is possible
     // However, trying to do it at 1 tx will result in TRANSACTION_OVERSIZE
     private HapiApiSpec HSCS_EVM_0014_ContractCreateFailsOnLargeContracts() {
         final var PAYER = "payer";
-        final var data = ERC1155ContractInteractions.readFileContents(ContractResources.LARGE_CONTRACT);
+        final var data = ERC1155ContractInteractions.getFileContents(ContractResources.LARGE_CONTRACT);
         return defaultHapiSpec("HSCS_EVM_0014_ContractCreateFailsOnLargeContracts")
                 .given(
                         cryptoCreate(PAYER)
@@ -97,6 +106,7 @@ public class EvmSuites extends HapiApiSuite {
                 .then();
     }
 
+    // TODO:
     private HapiApiSpec HSCS_EVM_0010_MultiSignatureAccounts() {
         return defaultHapiSpec("HSCS_EVM_009_MinimumChargeOfTxnGasLimit")
                 .given()
@@ -104,6 +114,7 @@ public class EvmSuites extends HapiApiSuite {
                 .then();
     }
 
+    // TODO:
     private HapiApiSpec HSCS_EVM_006_ContractHBarTransferToAccount() {
         return defaultHapiSpec("HSCS_EVM_006_ContractHBarTransferToAccount")
                 .given()
@@ -112,12 +123,41 @@ public class EvmSuites extends HapiApiSuite {
     }
 
     private HapiApiSpec HSCS_EVM_005_TransferOfHBarsWorksBetweenContracts() {
+        final var ACCOUNT = "account";
+        final var CONTRACT_FROM = "contract1";
+        final var CONTRACT_TO = "contract2";
         return defaultHapiSpec("HSCS_EVM_005_TransferOfHBarsWorksBetweenContracts")
-                .given()
-                .when()
-                .then();
+                .given(
+                        cryptoCreate(ACCOUNT).balance(ONE_MILLION_HBARS),
+
+                        fileCreate("contract1Bytecode").path(ContractResources.TRANSFERRING_CONTRACT).payingWith(ACCOUNT),
+                        contractCreate(CONTRACT_FROM).bytecode("contract1Bytecode").balance(10_000L).payingWith(ACCOUNT),
+
+                        contractCreate(CONTRACT_TO).bytecode("contract1Bytecode").balance(10_000L).payingWith(ACCOUNT),
+
+                        getContractInfo(CONTRACT_FROM).saveToRegistry("contract_from"),
+                        getContractInfo(CONTRACT_TO).saveToRegistry("contract_to"),
+                        getAccountInfo(ACCOUNT).savingSnapshot("accountInfo")
+                )
+                .when(
+                        withOpContext((spec, log) -> {
+                            var cto = spec.registry().getContractInfo("contract_to").getContractAccountID();
+
+                            var transferCall = contractCall(
+                                    CONTRACT_FROM,
+                                    ContractResources.TRANSFERRING_CONTRACT_TRANSFERTOADDRESS,
+                                    cto, 10)
+                                    .payingWith(ACCOUNT).logged();
+                            allRunFor(spec, transferCall);
+                        })
+                )
+                .then(
+                        getAccountBalance(CONTRACT_FROM).hasTinyBars(10_000 - 10),
+                        getAccountBalance(CONTRACT_TO).hasTinyBars(10_000 + 10)
+                );
     }
 
+    // TODO:
     private HapiApiSpec HSCS_EVM_004_GasRefundingLogicWorksAsExpected() {
         return defaultHapiSpec("HSCS_EVM_004_GasRefundingLogic")
                 .given()
