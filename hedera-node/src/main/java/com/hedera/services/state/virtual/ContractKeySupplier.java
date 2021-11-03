2ce92a09c6852dde6aea8efa2a47b3a1bdb5cd3f
anighanta <anirudh.ghanta@hedera.com>
2021-11-03 10:43:49 -0500

    add more unittest coverage and add licence headers
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/state/virtual/ContractKeySupplier.java
+++ hedera-node/src/main/java/com/hedera/services/state/virtual/ContractKeySupplier.java
@@ -1,5 +1,25 @@
 package com.hedera.services.state.virtual;
 
+/*-
+ * ‌
+ * Hedera Services Node
+ * ​
+ * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
 import com.swirlds.common.io.SerializableDataInputStream;
 import com.swirlds.common.io.SerializableDataOutputStream;
 import com.swirlds.jasperdb.SelfSerializableSupplier;
@@ -7,8 +27,8 @@ import com.swirlds.jasperdb.SelfSerializableSupplier;
 import java.io.IOException;
 
 public class ContractKeySupplier implements SelfSerializableSupplier<ContractKey> {
-	private static final long CLASS_ID = 0x1f536a740add7115L;
-	private static final int CURRENT_VERSION = 1;
+	static final long CLASS_ID = 0x1f536a740add7115L;
+	static final int CURRENT_VERSION = 1;
 
 	@Override
 	public void deserialize(SerializableDataInputStream in, int version) throws IOException {
