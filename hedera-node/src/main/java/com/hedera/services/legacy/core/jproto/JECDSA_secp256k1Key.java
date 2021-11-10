346bb36df811d600c2b4384535b3b2464dfbee13
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2021-11-10 00:02:49 -0600

    add JECDSA_secp256k1Key unit test

--- hedera-node/src/main/java/com/hedera/services/legacy/core/jproto/JECDSA_secp256k1Key.java
+++ hedera-node/src/main/java/com/hedera/services/legacy/core/jproto/JECDSA_secp256k1Key.java
@@ -2,18 +2,15 @@ package com.hedera.services.legacy.core.jproto;
 
 import com.swirlds.common.CommonUtils;
 
-import java.util.Arrays;
-
 /**
- * Maps to proto Key of type secp256k1Key.
+ * Maps to proto Key of type ECDSA_secp256k1Key
  */
 
 public class JECDSA_secp256k1Key extends JKey {
 
 	private static final long serialVersionUID = 1L;
 	private byte[] ECDSA_secp256k1Key = null;
-	public static final int ECDSA_secp256_BYTE_LENGTH = 33;
-	public static final String startValue = "0x02";
+	public static final int ECDSAsecp256_COMPRESSED_BYTE_LENGTH = 33;
 
 	public JECDSA_secp256k1Key(final byte[] ECDSA_secp256k1Key) {
 		this.ECDSA_secp256k1Key = ECDSA_secp256k1Key;
@@ -26,11 +23,9 @@ public class JECDSA_secp256k1Key extends JKey {
 
 	@Override
 	public boolean isValid() {
-		if (isEmpty()) {
-			return false;
-		} else if (ECDSA_secp256k1Key.length != ECDSA_secp256_BYTE_LENGTH) {
-			return false;
-		} else if (ECDSA_secp256k1Key[0] != 0x02 && ECDSA_secp256k1Key[0] != 0x03) {
+		if (isEmpty()
+				|| (ECDSA_secp256k1Key.length != ECDSAsecp256_COMPRESSED_BYTE_LENGTH)
+				|| (ECDSA_secp256k1Key[0] != 0x02 && ECDSA_secp256k1Key[0] != 0x03)) {
 			return false;
 		}
 		return true;
---
+++ hedera-node/src/test/java/com/hedera/services/legacy/core/jproto/JECDSA_secp256k1KeyTest.java
@@ -0,0 +1,87 @@
+package com.hedera.services.legacy.core.jproto;
+
+/*-
+ * ‌
+ * Hedera Services Node
+ * ​
+ * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import com.hedera.services.files.HFileMeta;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class JECDSA_secp256k1KeyTest {
+	JECDSA_secp256k1Key subject;
+	byte[] bytes;
+
+	@BeforeEach
+	void setUp() {
+		bytes = new byte[33];
+		bytes[0] = 0x03;
+		subject = new JECDSA_secp256k1Key(bytes);
+	}
+
+	@Test
+	void emptyJECDSA_secp256k1KeyTest() {
+		JECDSA_secp256k1Key key1 = new JECDSA_secp256k1Key(null);
+		assertTrue(key1.isEmpty());
+		assertFalse(key1.isValid());
+
+		JECDSA_secp256k1Key key2 = new JECDSA_secp256k1Key(new byte[0]);
+		assertTrue(key2.isEmpty());
+		assertFalse(key2.isValid());
+	}
+
+	@Test
+	void nonEmptyInvalidLengthJECDSA_secp256k1KeyTest() {
+		JECDSA_secp256k1Key key = new JECDSA_secp256k1Key(new byte[1]);
+		assertFalse(key.isEmpty());
+		assertFalse(key.isValid());
+	}
+
+	@Test
+	void nonEmptyValid0x02JECDSA_secp256k1KeyTest() {
+		byte[] bytes = new byte[33];
+		bytes[0] = 0x02;
+		JECDSA_secp256k1Key key = new JECDSA_secp256k1Key(bytes);
+		assertFalse(key.isEmpty());
+		assertTrue(key.isValid());
+	}
+
+	@Test
+	void nonEmptyValid0x03JECDSA_secp256k1KeyTest() {
+		assertFalse(subject.isEmpty());
+		assertTrue(subject.isValid());
+	}
+
+	@Test
+	void getterWorks() {
+		assertEquals(bytes, subject.getECDSAsecp256k1Key());
+	}
+
+	@Test
+	void toStringWorks() {
+		assertEquals(
+				"<JECDSA_secp256k1Key: ECDSA_secp256k1Key " +
+						"hex=030000000000000000000000000000000000000000000000000000000000000000>",
+				subject.toString());
+	}
+}
