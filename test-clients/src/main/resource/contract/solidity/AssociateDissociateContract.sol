c10d62445bf69ccbd9f2b7c3795629171912f6f5
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2021-12-03 12:48:47 +0200

    Update AssociateDissociateContract to use new sol files
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- test-clients/src/main/resource/contract/solidity/AssociateDissociateContract.sol
+++ test-clients/src/main/resource/contract/solidity/AssociateDissociateContract.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.6.12;
 
 import "./HederaTokenService.sol";
-import "./HederaResponseCodes.sol";
 
-contract AssociateDissociateContract {
+contract AssociateDissociateContract is HederaTokenService {
 
     address tokenAddress;
 
