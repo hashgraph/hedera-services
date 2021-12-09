a06c4d9f607bae50b3aa6387aa9dbbf1cd631bd9
Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>
2021-12-09 18:56:15 +0200

    added test that asserts that dissociate precompile has expected semantics for deleted tokens
    Signed-off-by: Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/DissociatePrecompileSuite.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/DissociatePrecompileSuite.java
@@ -20,14 +20,17 @@ package com.hedera.services.bdd.suites.contract.precompile;
  * ‍
  */
 
+import com.google.protobuf.ByteString;
 import com.hedera.services.bdd.spec.HapiApiSpec;
 import com.hedera.services.bdd.spec.infrastructure.meta.ContractResources;
 import com.hedera.services.bdd.suites.HapiApiSuite;
 import com.hederahashgraph.api.proto.java.AccountID;
 import com.hederahashgraph.api.proto.java.TokenID;
+import com.hederahashgraph.api.proto.java.TokenType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -35,6 +38,7 @@ import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.HapiPropertySource.asDotDelimitedLongArray;
 import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.DISSOCIATE_TOKEN;
 import static com.hedera.services.bdd.spec.keys.KeyShape.DELEGATE_CONTRACT;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountInfo;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTokenInfo;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
@@ -47,6 +51,8 @@ import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.mintToken;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenAssociate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenDelete;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenFreeze;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenUnfreeze;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenUpdate;
 import static com.hedera.services.bdd.spec.transactions.token.TokenMovement.moving;
@@ -57,6 +63,8 @@ import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hedera.services.bdd.suites.contract.Utils.asAddress;
 import static com.hedera.services.bdd.suites.token.TokenAssociationSpecs.FREEZABLE_TOKEN_ON_BY_DEFAULT;
 import static com.hedera.services.bdd.suites.token.TokenAssociationSpecs.KNOWABLE_TOKEN;
+import static com.hedera.services.bdd.suites.token.TokenAssociationSpecs.MULTI_KEY;
+import static com.hedera.services.bdd.suites.token.TokenAssociationSpecs.TBD_TOKEN;
 import static com.hedera.services.bdd.suites.utils.MiscEETUtils.metadata;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_REVERT_EXECUTED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
@@ -94,7 +102,8 @@ public class DissociatePrecompileSuite extends HapiApiSuite {
 				dissociatePrecompileWithSigsForFungibleWorks(),
 				dissociatePrecompileWitContractIdSigForFungibleWorks(),
 				dissociatePrecompileWithSigsForNFTWorks(),
-				dissociatePrecompileWitContractIdSigForNFTWorks()
+				dissociatePrecompileWitContractIdSigForNFTWorks(),
+				dissociatePrecompileHasExpectedSemanticsForDeletedTokens()
 		);
 	}
 
@@ -215,7 +224,7 @@ public class DissociatePrecompileSuite extends HapiApiSuite {
 		AtomicReference<TokenID> freezeKeyOnTokenID = new AtomicReference<>();
 
 		AtomicReference<TokenID> tk1TokenID = new AtomicReference<>();
-		return defaultHapiSpec("dissociatePrecompileWitContractIdSigForFungibleWorks")
+		return defaultHapiSpec("DissociatePrecompileWitContractIdSigForFungibleWorks")
 				.given(
 						newKeyNamed("kycKey"),
 						newKeyNamed("freezeKey"),
@@ -321,7 +330,7 @@ public class DissociatePrecompileSuite extends HapiApiSuite {
 		AtomicReference<TokenID> freezeKeyOnTokenID = new AtomicReference<>();
 
 		AtomicReference<TokenID> tk1TokenID = new AtomicReference<>();
-		return defaultHapiSpec("dissociatePrecompileWithSigsForNFTWorks")
+		return defaultHapiSpec("DissociatePrecompileWithSigsForNFTWorks")
 				.given(
 						newKeyNamed(multiKey),
 						newKeyNamed("kycKey"),
@@ -429,7 +438,7 @@ public class DissociatePrecompileSuite extends HapiApiSuite {
 		AtomicReference<TokenID> freezeKeyOnTokenID = new AtomicReference<>();
 
 		AtomicReference<TokenID> tk1TokenID = new AtomicReference<>();
-		return defaultHapiSpec("dissociatePrecompileWitContractIdSigForNFTWorks")
+		return defaultHapiSpec("DissociatePrecompileWitContractIdSigForNFTWorks")
 				.given(
 						newKeyNamed("kycKey"),
 						newKeyNamed("freezeKey"),
@@ -523,6 +532,129 @@ public class DissociatePrecompileSuite extends HapiApiSuite {
 								.hasToken(relationshipWith(KNOWABLE_TOKEN))
 								.hasNoTokenRelationship(FREEZABLE_TOKEN_ON_BY_DEFAULT)
 								.logged()
+				);
+	}
+
+	public HapiApiSpec dissociatePrecompileHasExpectedSemanticsForDeletedTokens() {
+		final String tbdUniqToken = "UniqToBeDeleted";
+		final String zeroBalanceFrozen = "0bFrozen";
+		final String zeroBalanceUnfrozen = "0bUnfrozen";
+		final String nonZeroBalanceFrozen = "1bFrozen";
+		final String nonZeroBalanceUnfrozen = "1bUnfrozen";
+		final var theContract = "associateDissociateContract";
+		final long initialSupply = 100L;
+		final long nonZeroXfer = 10L;
+		final var firstMeta = ByteString.copyFrom("FIRST".getBytes(StandardCharsets.UTF_8));
+		final var secondMeta = ByteString.copyFrom("SECOND".getBytes(StandardCharsets.UTF_8));
+		final var thirdMeta = ByteString.copyFrom("THIRD".getBytes(StandardCharsets.UTF_8));
+
+		AtomicReference<AccountID> treasuryID = new AtomicReference<>();
+		AtomicReference<AccountID> zeroBalanceFrozenID = new AtomicReference<>();
+		AtomicReference<AccountID> zeroBalanceUnfrozenID = new AtomicReference<>();
+		AtomicReference<AccountID> nonZeroBalanceFrozenID = new AtomicReference<>();
+		AtomicReference<AccountID> nonZeroBalanceUnfrozenID = new AtomicReference<>();
+		AtomicReference<TokenID> tbdTokenID = new AtomicReference<>();
+		AtomicReference<TokenID> tbdUniqueTokenID = new AtomicReference<>();
+
+		return defaultHapiSpec("DissociatePrecompileHasExpectedSemanticsForDeletedTokens")
+				.given(
+						newKeyNamed(MULTI_KEY),
+						fileCreate(theContract)
+								.path(ContractResources.ASSOCIATE_DISSOCIATE_CONTRACT),
+						cryptoCreate(TOKEN_TREASURY)
+								.balance(10 * ONE_HUNDRED_HBARS)
+								.exposingCreatedIdTo(treasuryID::set),
+						tokenCreate(TBD_TOKEN)
+								.adminKey(MULTI_KEY)
+								.supplyKey(MULTI_KEY)
+								.initialSupply(initialSupply)
+								.treasury(TOKEN_TREASURY)
+								.freezeKey(MULTI_KEY)
+								.freezeDefault(true)
+								.exposingCreatedIdTo(id -> tbdTokenID.set(asToken(id))),
+						tokenCreate(tbdUniqToken)
+								.tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
+								.treasury(TOKEN_TREASURY)
+								.adminKey(MULTI_KEY)
+								.supplyKey(MULTI_KEY)
+								.initialSupply(0)
+								.exposingCreatedIdTo(id -> tbdUniqueTokenID.set(asToken(id))),
+						cryptoCreate(zeroBalanceFrozen)
+								.balance(10 * ONE_HUNDRED_HBARS)
+								.exposingCreatedIdTo(zeroBalanceFrozenID::set),
+						cryptoCreate(zeroBalanceUnfrozen)
+								.balance(10 * ONE_HUNDRED_HBARS)
+								.exposingCreatedIdTo(zeroBalanceUnfrozenID::set),
+						cryptoCreate(nonZeroBalanceFrozen)
+								.balance(10 * ONE_HUNDRED_HBARS)
+								.exposingCreatedIdTo(nonZeroBalanceFrozenID::set),
+						cryptoCreate(nonZeroBalanceUnfrozen)
+								.balance(10 * ONE_HUNDRED_HBARS)
+								.exposingCreatedIdTo(nonZeroBalanceUnfrozenID::set)
+				).when(
+
+						withOpContext(
+								(spec, opLog) ->
+										allRunFor(
+												spec,
+												contractCreate("AssociateDissociate")
+														.bytecode(theContract)
+														.gas(100_000),
+												tokenAssociate(zeroBalanceFrozen, TBD_TOKEN),
+												tokenAssociate(zeroBalanceUnfrozen, TBD_TOKEN),
+												tokenAssociate(nonZeroBalanceFrozen, TBD_TOKEN),
+												tokenAssociate(nonZeroBalanceUnfrozen, TBD_TOKEN),
+												mintToken(tbdUniqToken, List.of(firstMeta, secondMeta, thirdMeta)),
+												getAccountInfo(TOKEN_TREASURY).hasOwnedNfts(3),
+												tokenUnfreeze(TBD_TOKEN, zeroBalanceUnfrozen),
+												tokenUnfreeze(TBD_TOKEN, nonZeroBalanceUnfrozen),
+												tokenUnfreeze(TBD_TOKEN, nonZeroBalanceFrozen),
+
+												cryptoTransfer(moving(nonZeroXfer, TBD_TOKEN).between(TOKEN_TREASURY, nonZeroBalanceFrozen)),
+												cryptoTransfer(moving(nonZeroXfer, TBD_TOKEN).between(TOKEN_TREASURY, nonZeroBalanceUnfrozen)),
+
+												tokenFreeze(TBD_TOKEN, nonZeroBalanceFrozen),
+												getAccountBalance(TOKEN_TREASURY)
+														.hasTokenBalance(TBD_TOKEN, initialSupply - 2 * nonZeroXfer),
+												tokenDelete(TBD_TOKEN),
+												tokenDelete(tbdUniqToken),
+												contractCall("AssociateDissociate", DISSOCIATE_TOKEN,
+														asAddress(zeroBalanceFrozenID.get()), asAddress(tbdTokenID.get()))
+														.payingWith(zeroBalanceFrozen)
+														.alsoSigningWithFullPrefix(MULTI_KEY),
+												contractCall("AssociateDissociate", DISSOCIATE_TOKEN,
+														asAddress(zeroBalanceUnfrozenID.get()), asAddress(tbdTokenID.get()))
+														.alsoSigningWithFullPrefix(MULTI_KEY)
+														.payingWith(zeroBalanceUnfrozen),
+												contractCall("AssociateDissociate", DISSOCIATE_TOKEN,
+														asAddress(nonZeroBalanceFrozenID.get()), asAddress(tbdTokenID.get()))
+														.alsoSigningWithFullPrefix(MULTI_KEY)
+														.payingWith(nonZeroBalanceFrozen),
+												contractCall("AssociateDissociate", DISSOCIATE_TOKEN,
+														asAddress(nonZeroBalanceUnfrozenID.get()), asAddress(tbdTokenID.get()))
+														.alsoSigningWithFullPrefix(MULTI_KEY)
+														.payingWith(nonZeroBalanceUnfrozen),
+												contractCall("AssociateDissociate", DISSOCIATE_TOKEN,
+														asAddress(treasuryID.get()), asAddress(tbdUniqueTokenID.get()))
+														.alsoSigningWithFullPrefix(MULTI_KEY)
+														.payingWith(TOKEN_TREASURY)
+										)
+						)
+				).then(
+						getAccountInfo(zeroBalanceFrozen)
+								.hasNoTokenRelationship(TBD_TOKEN),
+						getAccountInfo(zeroBalanceUnfrozen)
+								.hasNoTokenRelationship(TBD_TOKEN),
+						getAccountInfo(nonZeroBalanceFrozen)
+								.hasNoTokenRelationship(TBD_TOKEN),
+						getAccountInfo(nonZeroBalanceUnfrozen)
+								.hasNoTokenRelationship(TBD_TOKEN),
+						getAccountInfo(TOKEN_TREASURY)
+								.hasToken(relationshipWith(TBD_TOKEN))
+								.hasNoTokenRelationship(tbdUniqToken)
+								.hasOwnedNfts(0),
+						getAccountBalance(TOKEN_TREASURY)
+								.hasTokenBalance(TBD_TOKEN, initialSupply - 2 * nonZeroXfer)
 				);
 	}
 
