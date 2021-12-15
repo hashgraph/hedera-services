ab38b9155961ece6bad5fce7faf13a1157ab7071
tinker-michaelj <michael.tinker@hedera.com>
2021-12-15 00:44:36 -0600

    Restore legacy file meta deserialization
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/legacy/unit/serialization/HFileMetaSerdeTest.java
+++ hedera-node/src/test/java/com/hedera/services/legacy/unit/serialization/HFileMetaSerdeTest.java
@@ -28,15 +28,18 @@ import com.hedera.services.state.serdes.DomainSerdes;
 import com.hedera.services.state.serdes.IoReadingFunction;
 import com.hedera.services.state.serdes.IoWritingConsumer;
 import com.hedera.test.factories.scenarios.TxnHandlingScenario;
+import com.hedera.test.utils.IdUtils;
 import com.hederahashgraph.api.proto.java.FileGetInfoResponse.FileInfo;
 import com.hederahashgraph.api.proto.java.FileID;
 import com.hederahashgraph.api.proto.java.Timestamp;
+import com.swirlds.common.CommonUtils;
 import com.swirlds.common.io.SerializableDataInputStream;
 import com.swirlds.common.io.SerializableDataOutputStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -46,12 +49,10 @@ import java.util.function.Function;
 
 import static com.hedera.services.files.HFileMetaSerde.MAX_CONCEIVABLE_MEMO_UTF8_BYTES;
 import static com.hedera.services.files.HFileMetaSerde.MEMO_VERSION;
-import static com.hedera.services.files.HFileMetaSerde.PRE_MEMO_VERSION;
 import static com.hedera.services.files.HFileMetaSerde.deserialize;
 import static com.hedera.services.files.HFileMetaSerde.streamContentDiscovery;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.argThat;
 import static org.mockito.BDDMockito.given;
@@ -64,6 +65,28 @@ class HFileMetaSerdeTest {
 	private static final String memo = "Remember me?";
 	private static final boolean deleted = true;
 
+	private static final String hexedLegacyKnown =
+			"00000000000000010000000000ee994300000000000002e101000000000012d6870000000000000" +
+					"0020000000000ecb1f000000000000002c00000000400000000000000020000000000ecf2ea0000" +
+					"0000000000209d4eb9cb63c543274d3a15c69335d50c31c948574ef9ae146075a197680502c4000" +
+					"00000000000020000000000ecd26d00000000000001380000000100000000000000020000000000" +
+					"ecb1f0000000000000011c0000000200000000000000020000000000ecb1f000000000000000c80" +
+					"000000200000000000000020000000000ecb1f00000000000000074000000020000000000000002" +
+					"0000000000ecf2ea00000000000000204020560eb6a77e8f690eb545dfbbbaee4b516ff8454cf47" +
+					"8e3935e2c6b2ff33200000000000000020000000000ecf2ea0000000000000020707d1263ca1070" +
+					"2286b02ffdf2b4f6c211ab8d8fe85cde0ac14b2af17efc4a7400000000000000020000000000ecf" +
+					"2ea0000000000000020e60fa77b38ed257b294f87f4a62416563530a5decea0f2054f8e86a7c463" +
+					"819000000000000000020000000000ecf2ea000000000000002001ddac1c439e1e20819c854b267" +
+					"ee3af3e790cd96c44ace40a89ff733501e2ec00000000000000020000000000ecf2ea0000000000" +
+					"0000201ebff7723e958d7bf444b42787f5aa9962dd001b368c17cca89a6aea0aa3dae0000000000" +
+					"00000020000000000ecb1f000000000000000e40000000100000000000000020000000000ecd26d" +
+					"00000000000000c80000000200000000000000020000000000ecb1f000000000000000ac0000000" +
+					"300000000000000020000000000ecf2ea0000000000000020ca5667c34cd53224770969525e5cfb" +
+					"19c0ad42adfb1e22a981e4ebc68df3947e00000000000000020000000000ecf2ea0000000000000" +
+					"020dd8858bde754f7f1f933b749e6fff61163e0a992d89936ad2718bc5b822c0e3e000000000000" +
+					"00020000000000ecf2ea000000000000002047c8c60779a621d370686f7b8ae2b670e65d67864da" +
+					"b067af7c4407ebeadc7b3";
+
 	private DomainSerdes serdes;
 	private HFileMetaSerde.StreamContentDiscovery discovery;
 	private Function<InputStream, SerializableDataInputStream> serInFactory;
@@ -74,13 +97,6 @@ class HFileMetaSerdeTest {
 	@BeforeEach
 	void setUp() {
 		known = new HFileMeta(deleted, wacl, expiry, memo);
-	}
-
-	@Test
-	void throwsOnNoLongerSupportedPreMemoVersion() throws IOException {
-		final var in = mock(DataInputStream.class);
-		given(in.readLong()).willReturn(PRE_MEMO_VERSION);
-		assertThrows(IllegalArgumentException.class, () -> deserialize(in));
 	}
 
 	@Test
@@ -131,6 +147,20 @@ class HFileMetaSerdeTest {
 				any(IoWritingConsumer.class));
 
 		undoStaticMocking();
+	}
+
+	@Test
+	void legacySerdeTest() throws Exception {
+		final var fid = IdUtils.asFile("0.0.1001");
+		final var expInfo = toGrpc(known, fid, 1024);
+		final var legacyRepr = CommonUtils.unhex(hexedLegacyKnown);
+
+		final var replica = deserialize(new DataInputStream(new ByteArrayInputStream(legacyRepr)));
+		final var replicaInfo = toGrpc(replica, fid, 1024);
+
+		assertEquals(expInfo.getExpirationTime(), replicaInfo.getExpirationTime());
+		assertEquals(expInfo.getDeleted(), replicaInfo.getDeleted());
+		assertEquals(expInfo.getKeys().getKeysCount(), replicaInfo.getKeys().getKeysCount());
 	}
 
 	@SuppressWarnings("unchecked")
