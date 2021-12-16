0f613c7eea8b1c11dd5adf2ebe8127cb832c5814
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2021-12-16 15:57:44 -0600

    sonar issues

--- hapi-fees/src/test/java/com/hedera/services/usage/SigUsageTest.java
+++ hapi-fees/src/test/java/com/hedera/services/usage/SigUsageTest.java
@@ -25,7 +25,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class SigUsageTest {
+class SigUsageTest {
 	@Test
 	void gettersWork() {
 		SigUsage one = new SigUsage(3, 256, 2);
