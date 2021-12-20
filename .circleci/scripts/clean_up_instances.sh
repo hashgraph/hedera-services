fb7157c6bc87354f64d5a67adbb98f1b88d5a81c
Neeha <52669918+Neeharika-Sompalli@users.noreply.github.com>
2021-12-20 10:21:14 -0600

    Fix gcp cleanup script (#2640)
    * fix clean up script
    
    * fix clean up script
    
    * revert branch

--- .circleci/scripts/clean_up_instances.sh
+++ .circleci/scripts/clean_up_instances.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 projectName="$1"
-timestamp=`TZ=UTC date -v-6H "+%Y-%m-%dT%H:%M:%S"`
+timestamp=`TZ=UTC date -d '6 hours ago' "+%Y-%m-%dT%H:%M:%S"`
 echo "Project Name : $projectName"
 echo "Instances before $timestamp will be deleted"
 list=`gcloud compute instance-groups managed list --filter="(name~.*gcp-daily.* OR name~.*gcp-ondemand.* OR name~.*gcp-weekly.* OR name~.*gcp-commit-.*) AND creationTimestamp<$timestamp" --format="value(name,zone.scope())" --project=$projectName`
