f26267b42f772808c03fd4c6075474c5dfe3e9bd
anighanta <anirudh.ghanta@hedera.com>
2021-12-30 10:52:43 +0530

    testing CI test runs
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/utils/LogUtilsTest.java
+++ hedera-node/src/test/java/com/hedera/services/utils/LogUtilsTest.java
@@ -9,12 +9,15 @@ import com.hederahashgraph.api.proto.java.TransactionBody;
 import com.hederahashgraph.api.proto.java.TransactionID;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static com.hedera.services.txns.crypto.AutoCreationLogic.THREE_MONTHS_IN_SECONDS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,23 +25,25 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LogUtilsTest {
-	private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-	private static final PrintStream originalOut = System.out;
+//	private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+//	private static final PrintStream originalOut = System.out;
+	private static final String HGCAA_LOG_PATH = "output/hgcaa.log";
 
 	@BeforeAll
-	public static void setUpStreams() {
-		System.setOut(new PrintStream(outContent));
+	public static void clearHgcaaLog() throws FileNotFoundException {
+		new PrintWriter(HGCAA_LOG_PATH).close();
+//		System.setOut(new PrintStream(outContent));
 	}
 
-	@AfterAll
-	public static void restoreStreams() {
-		System.setOut(originalOut);
-	}
+//	@AfterAll
+//	public static void restoreStreams() {
+//		System.setOut(originalOut);
+//	}
 
 	@Test
-	void ignoresJndiInGrpc() {
+	void ignoresJndiInGrpc() throws IOException {
 		final var malUri = "${jndi:https://previewnet.mirrornode.hedera.com/api/v1/accounts?account.id=0.0.90}";
-		final var escapedUri = "\\d{jndi\\ghttps\\g//previewnet.mirrornode.hedera.com/api/v1/accounts?account.id=0.0.90}";
+		final var escapedUri = LogUtils.escapeBytes(ByteString.copyFromUtf8(malUri));
 		final var message = "We are Doomed!! %s";
 		final var stackStraceSample = "at org.apache.logging.log4j.core.net.JndiManager.lookup";
 		final CryptoCreateTransactionBody createTxnBody = CryptoCreateTransactionBody.newBuilder()
@@ -55,11 +60,12 @@ class LogUtilsTest {
 		final var logger = LogManager.getLogger();
 
 		// log4j-test has no {nolookups} tag, so if the encoding is not done for grpc, we can exploit log4J
-		LogUtils.encodeGrpcAndLog(logger, Level.WARN, message, txnBody);
+		LogUtils.encodeGrpcAndLog(logger, Level.WARN, message, txn);
 
-		final var actualLogWithNoJndiLookUp = outContent.toString();
+		final var actualLogWithNoJndiLookUp = readHgcaaLog();
 		assertFalse(actualLogWithNoJndiLookUp.contains(stackStraceSample));
-		assertTrue(actualLogWithNoJndiLookUp.contains(escapedUri));
+		assertTrue(actualLogWithNoJndiLookUp.contains(escapedUri),
+				"actual : " + actualLogWithNoJndiLookUp + " expected : " + escapedUri);
 
 		// If using log4J version 2.14 or older the following test code is relevant.
 		/*
@@ -75,5 +81,9 @@ class LogUtilsTest {
 		final var malUri = "${jndi:https://previewnet.mirrornode.hedera.com/api/v1/accounts?account.id=0.0.90}";
 
 		assertEquals(malUri, LogUtils.unescapeBytes(LogUtils.escapeBytes(ByteString.copyFromUtf8(malUri))));
+	}
+
+	private String readHgcaaLog() throws IOException {
+		return Files.readString(Path.of(HGCAA_LOG_PATH), StandardCharsets.UTF_8);
 	}
 }
