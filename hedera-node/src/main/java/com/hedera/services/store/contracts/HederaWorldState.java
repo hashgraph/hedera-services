8407538af6fbc6155d887d9940c7d5ae667e39fe
tinker-michaelj <michael.tinker@hedera.com>
2022-01-09 18:17:37 -0600

    Restore null-safety to HederaWorldState.Account getCode()/hasCode()/getCodeHash()
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/store/contracts/HederaWorldState.java
+++ hedera-node/src/main/java/com/hedera/services/store/contracts/HederaWorldState.java
@@ -35,6 +35,7 @@ import org.apache.tuweni.units.bigints.UInt256;
 import org.hyperledger.besu.datatypes.Address;
 import org.hyperledger.besu.datatypes.Hash;
 import org.hyperledger.besu.datatypes.Wei;
+import org.hyperledger.besu.evm.Code;
 import org.hyperledger.besu.evm.Gas;
 import org.hyperledger.besu.evm.account.Account;
 import org.hyperledger.besu.evm.account.AccountStorageEntry;
@@ -59,6 +60,8 @@ import static com.hedera.services.utils.EntityIdUtils.asTypedSolidityAddress;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.FAIL_INVALID;
 
 public class HederaWorldState implements HederaMutableWorldState {
+	private static final Code EMPTY_CODE = new Code(Bytes.EMPTY, Hash.hash(Bytes.EMPTY));
+
 	private final EntityIdSource ids;
 	private final EntityAccess entityAccess;
 	private final Map<Address, Address> sponsorMap = new LinkedHashMap<>();
@@ -207,7 +210,7 @@ public class HederaWorldState implements HederaMutableWorldState {
 
 		@Override
 		public Bytes getCode() {
-			return codeCache.getIfPresent(address).getBytes();
+			return getCodeInternal().getBytes();
 		}
 
 		public EntityId getProxyAccount() {
@@ -225,7 +228,7 @@ public class HederaWorldState implements HederaMutableWorldState {
 
 		@Override
 		public Hash getCodeHash() {
-			return codeCache.getIfPresent(address).getCodeHash();
+			return getCodeInternal().getCodeHash();
 		}
 
 		@Override
@@ -290,6 +293,11 @@ public class HederaWorldState implements HederaMutableWorldState {
 
 		public AccountID getAccount() {
 			return account;
+		}
+
+		private Code getCodeInternal() {
+			final var code = codeCache.getIfPresent(address);
+			return (code == null) ? EMPTY_CODE : code;
 		}
 	}
 
