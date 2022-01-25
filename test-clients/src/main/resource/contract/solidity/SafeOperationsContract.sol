c522e02585caaf2ca54ca6e6c54047ae6cab98ff
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2022-01-25 08:13:13 +0200

    Move SafeHTS and dependencies to hip-206/safe folder Add more gas to EETs for safe HTS since changes from master require it
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- test-clients/src/main/resource/contract/solidity/SafeOperationsContract.sol
+++ test-clients/src/main/resource/contract/solidity/SafeOperationsContract.sol
@@ -5,14 +5,42 @@ pragma experimental ABIEncoderV2;
 import "./SafeHTS.sol";
 
 contract SafeOperationsContract {
-    using SafeHTS for IHederaTokenService;
+    using SafeHTS for IHTS;
 
     function safeTokenAssociate(address sender, address tokenAddress) external {
-        IHederaTokenService(tokenAddress).safeAssociateToken(sender);
+        IHTS(tokenAddress).safeAssociateToken(sender);
     }
 
     function safeTokenDissociate(address sender, address tokenAddress) external {
-        IHederaTokenService(tokenAddress).safeDissociateToken(sender);
+        IHTS(tokenAddress).safeDissociateToken(sender);
+    }
+
+    function safeTokensTransfer(address token, address[] memory accountIds, int64[] memory amounts) external {
+        IHTS(token).safeTransferTokens(accountIds, amounts);
+    }
+
+    function safeNFTsTransfer(address token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) external {
+        IHTS(token).safeTransferNFTs(sender, receiver, serialNumber);
+    }
+
+    function safeTokenTransfer(address token, address sender, address receiver, int64 amount) external {
+        IHTS(token).safeTransferToken(sender, receiver, amount);
+    }
+
+    function safeNFTTransfer(address token, address sender, address receiver, int64 serialNum) external {
+        IHTS(token).safeTransferNFT(sender, receiver, serialNum);
+    }
+
+    function safeTokenMint(address token, uint64 amount, bytes[] memory metadata) external
+    returns (uint64 newTotalSupply, int64[] memory serialNumbers)
+    {
+        (newTotalSupply, serialNumbers) = IHTS(token).safeMintToken(amount, metadata);
+    }
+
+    function safeTokenBurn(address token, uint64 amount, int64[] memory serialNumbers) external
+    returns (uint64 newTotalSupply)
+    {
+        (newTotalSupply) = IHTS(token).safeBurnToken(amount, serialNumbers);
     }
 
     function safeTokensAssociate(address account, address[] memory tokens) external {
@@ -23,35 +51,7 @@ contract SafeOperationsContract {
         SafeHTS.safeDissociateTokens(account, tokens);
     }
 
-    function safeTokensTransfer(address token, address[] memory accountIds, int64[] memory amounts) external {
-        IHederaTokenService(token).safeTransferTokens(accountIds, amounts);
-    }
-
-    function safeNFTsTransfer(address token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) external {
-        IHederaTokenService(token).safeTransferNFTs(sender, receiver, serialNumber);
-    }
-
-    function safeTokenTransfer(address token, address sender, address receiver, int64 amount) external {
-        IHederaTokenService(token).safeTransferToken(sender, receiver, amount);
-    }
-
-    function safeNFTTransfer(address token, address sender, address receiver, int64 serialNum) external {
-        IHederaTokenService(token).safeTransferNFT(sender, receiver, serialNum);
-    }
-
-    function safeTransferCrypto(IHederaTokenService.TokenTransferList[] memory tokenTransfers) external {
+    function safeTransferCrypto(IHTS.TokenTransferList[] memory tokenTransfers) external {
         SafeHTS.safeCryptoTransfer(tokenTransfers);
-    }
-
-    function safeTokenMint(address token, uint64 amount, bytes[] memory metadata) external
-    returns (uint64 newTotalSupply, int64[] memory serialNumbers)
-    {
-        (newTotalSupply, serialNumbers) = IHederaTokenService(token).safeMintToken(amount, metadata);
-    }
-
-    function safeTokenBurn(address token, uint64 amount, int64[] memory serialNumbers) external
-    returns (uint64 newTotalSupply)
-    {
-        (newTotalSupply) = IHederaTokenService(token).safeBurnToken(amount, serialNumbers);
     }
 }
--- test-clients/src/main/resource/contract/solidity/hip-206/safe/SafeHTS.sol
+++ test-clients/src/main/resource/contract/solidity/hip-206/safe/SafeHTS.sol
@@ -2,26 +2,18 @@
 pragma solidity >=0.5.0 <0.9.0;
 pragma experimental ABIEncoderV2;
 
-import "./IHederaTokenService.sol";
+import "./IHTS.sol";
 import "./HederaResponseCodes.sol";
 
 library SafeHTS {
 
     address constant precompileAddress = address(0x167);
 
-    function safeCryptoTransfer(IHederaTokenService.TokenTransferList[] memory tokenTransfers) internal {
-        int32 responseCode;
-        (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.cryptoTransfer.selector, tokenTransfers));
-        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
-        require(responseCode == HederaResponseCodes.SUCCESS, "Safe crypto transfer failed!");
-    }
-
-    function safeMintToken(IHederaTokenService token, uint64 amount, bytes[] memory metadata) internal
+    function safeMintToken(IHTS token, uint64 amount, bytes[] memory metadata) internal
     returns (uint64 newTotalSupply, int64[] memory serialNumbers) {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.delegatecall(
-            abi.encodeWithSelector(IHederaTokenService.mintToken.selector,
+            abi.encodeWithSelector(IHTS.mintToken.selector,
             token, amount, metadata));
         (responseCode, newTotalSupply, serialNumbers) =
         success
@@ -30,12 +22,12 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe mint failed!");
     }
 
-    function safeBurnToken(IHederaTokenService token, uint64 amount, int64[] memory serialNumbers) internal
+    function safeBurnToken(IHTS token, uint64 amount, int64[] memory serialNumbers) internal
     returns (uint64 newTotalSupply)
     {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.burnToken.selector,
+            abi.encodeWithSelector(IHTS.burnToken.selector,
             token, amount, serialNumbers));
         (responseCode, newTotalSupply) =
         success
@@ -44,75 +36,83 @@ library SafeHTS {
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe burn failed!");
     }
 
-    function safeAssociateTokens(address account, address[] memory tokens) internal {
+    function safeAssociateToken(IHTS token, address account) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.associateTokens.selector,
-            account, tokens));
-        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
-        require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple associations failed!");
-    }
-
-    function safeAssociateToken(IHederaTokenService token, address account) internal {
-        int32 responseCode;
-        (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.associateToken.selector,
+            abi.encodeWithSelector(IHTS.associateToken.selector,
             account, token));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe single association failed!");
     }
 
-    function safeDissociateTokens(address account, address[] memory tokens) internal {
+    function safeDissociateToken(IHTS token, address account) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.dissociateTokens.selector,
-            account, tokens));
-        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
-        require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple dissociations failed!");
-    }
-
-    function safeDissociateToken(IHederaTokenService token, address account) internal {
-        int32 responseCode;
-        (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.dissociateToken.selector,
+            abi.encodeWithSelector(IHTS.dissociateToken.selector,
             account, token));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe single dissociation failed!");
     }
 
-    function safeTransferTokens(IHederaTokenService token, address[] memory accountIds, int64[] memory amounts) internal {
+    function safeTransferTokens(IHTS token, address[] memory accountIds, int64[] memory amounts) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.transferTokens.selector,
+            abi.encodeWithSelector(IHTS.transferTokens.selector,
             token, accountIds, amounts));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe tokens transfer failed!");
     }
 
-    function safeTransferNFTs(IHederaTokenService token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) internal {
+    function safeTransferNFTs(IHTS token, address[] memory sender, address[] memory receiver, int64[] memory serialNumber) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.transferNFTs.selector,
+            abi.encodeWithSelector(IHTS.transferNFTs.selector,
             token, sender, receiver, serialNumber));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe NFTs transfer failed!");
     }
 
-    function safeTransferToken(IHederaTokenService token, address sender, address receiver, int64 amount) internal {
+    function safeTransferToken(IHTS token, address sender, address receiver, int64 amount) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.transferToken.selector,
+            abi.encodeWithSelector(IHTS.transferToken.selector,
             token, sender, receiver, amount));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe token transfer failed!");
     }
 
-    function safeTransferNFT(IHederaTokenService token, address sender, address receiver, int64 serialNumber) internal {
+    function safeTransferNFT(IHTS token, address sender, address receiver, int64 serialNumber) internal {
         int32 responseCode;
         (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.transferNFT.selector,
+            abi.encodeWithSelector(IHTS.transferNFT.selector,
             token, sender, receiver, serialNumber));
         responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
         require(responseCode == HederaResponseCodes.SUCCESS, "Safe NFT transfer failed!");
+    }
+
+    function safeAssociateTokens(address account, address[] memory tokens) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHTS.associateTokens.selector,
+            account, tokens));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple associations failed!");
+    }
+
+    function safeDissociateTokens(address account, address[] memory tokens) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHTS.dissociateTokens.selector,
+            account, tokens));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe multiple dissociations failed!");
+    }
+
+    function safeCryptoTransfer(IHTS.TokenTransferList[] memory tokenTransfers) internal {
+        int32 responseCode;
+        (bool success, bytes memory result) = precompileAddress.call(
+            abi.encodeWithSelector(IHTS.cryptoTransfer.selector, tokenTransfers));
+        responseCode = success ? abi.decode(result, (int32)) : HederaResponseCodes.UNKNOWN;
+        require(responseCode == HederaResponseCodes.SUCCESS, "Safe crypto transfer failed!");
     }
 }
