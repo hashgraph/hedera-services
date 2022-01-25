897c563a8e54d7258c3a7519f74e217af9b661eb
Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>
2022-01-25 12:53:52 +0200

    adding solidity methods and ABI structures for all supported ERC20/ERC721 operations
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@gmail.com>

--- test-clients/src/main/resource/contract/solidity/ERC20Contract.sol
+++ test-clients/src/main/resource/contract/solidity/ERC20Contract.sol
@@ -1,10 +1,35 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
 contract ERC20Contract {
 
+    function name(address token) public {
+        IERC20Metadata(token).name();
+    }
+
+    function symbol(address token) public {
+        IERC20Metadata(token).symbol();
+    }
+
+    function decimals(address token) public {
+        IERC20Metadata(token).decimals();
+    }
+
+    function totalSupply(address token) public {
+        IERC20(token).totalSupply();
+    }
+
     function balanceOf(address token, address account) public {
         IERC20(token).balanceOf(account);
+    }
+
+    function transfer(address token, address recipient, uint256 amount) public {
+        IERC20(token).transfer(recipient, amount);
+    }
+
+    function transferFrom(address token, address sender, address recipient, uint256 amount) public {
+        IERC20(token).transferFrom(sender, recipient, amount);
     }
 }
