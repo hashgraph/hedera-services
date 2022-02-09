9c0a85aa8272f75e7470c4289cb74877da3bd1d5
Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
2022-02-09 12:54:20 +0200

    Precompiles: Include Contract Function Result in Precompiles (#2800)
    * Contract function result internal (#831)
    
    * Precompiles: include contract function result (#794)
    
    * HSCS-PREC-008 Scenario Layout
    
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    
    * Child Record Contract Call Result extension
    
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    
    * Extend child records.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Add import.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Set proper gas usage in ContractCallResult.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Refactor ContractCallResult implementation.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Minor refactoring and fixing formatting.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Revert ExpiringCreations.java changes.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Adapt unit tests of HTSPrecompiledContract for gasRequirement.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Fix failing tests.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Fix failing tests.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    Co-authored-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    
    * Remove TODO
    
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    
    * Add ContractFunctionResult when precompile fails.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Minor refactoring of test classes
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Stop contractCallResult from nulling on revert.
    
    Signed-off-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    
    * Populate error field of ContractCallResult.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Fix missing licenses.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * Fix contractCallResult from nulling on revert.
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * When reverting a child record, keep all the values for the contractCallResult field instead of just keeping the error message
    Rename the isFullReset flag to isRevertOfChildRecord
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    * Externalize successful EVM create message calls as internal records (#2813)
    
    * Initial commit, introduce AbstractRecordingCreateOperation
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Implement AbstractRecordingCreateOperation, externalize skeleton child creation records
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Finish basic record customization; TODO: add sponsor-derived info to synthetic creates
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Fix unit test
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Complete customization of synthetic contract creations
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Add license headers and javadoc
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Fix argument for ContractFunctionResult contractID field
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    * Fixes in ContractHTSSuite
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    Co-authored-by: Dimitar Dinev <mitkojc@gmail.com>
    Co-authored-by: Georgi Yazovaliyski <georgi.yazovaliiski@gmail.com>
    Co-authored-by: Michael Tinker <michael.tinker@hedera.com>
    
    * Update bytecode and abi for HbarFeeCollector contract
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    * rename boolean isRevertOfChildRecord to removeCallResult and invert the check
    call contractIdFromEvmAddress to parse the EVM Address
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    Co-authored-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    Co-authored-by: Dimitar Dinev <mitkojc@gmail.com>
    Co-authored-by: Michael Tinker <michael.tinker@hedera.com>
    Signed-off-by: Ray Juang <radford.juang@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/state/submerkle/ExpirableTxnRecord.java
+++ hedera-node/src/main/java/com/hedera/services/state/submerkle/ExpirableTxnRecord.java
@@ -701,7 +701,7 @@ public class ExpirableTxnRecord implements FCQueueElement {
 			receipt = null;
 			consensusTime = null;
 
-			nullOutSideEffectFields();
+			nullOutSideEffectFields(true);
 
 			return this;
 		}
@@ -711,7 +711,7 @@ public class ExpirableTxnRecord implements FCQueueElement {
 				throw new IllegalStateException("Cannot revert a record with a built receipt");
 			}
 			receiptBuilder.revert();
-			nullOutSideEffectFields();
+			nullOutSideEffectFields(false);
 		}
 
 		public void excludeHbarChangesFrom(final ExpirableTxnRecord.Builder that) {
@@ -771,9 +771,8 @@ public class ExpirableTxnRecord implements FCQueueElement {
 				.thenComparingLong(EntityId::shard)
 				.thenComparingLong(EntityId::realm);
 
-		private void nullOutSideEffectFields() {
+		private void nullOutSideEffectFields(boolean removeCallResult) {
 			transferList = null;
-			contractCallResult = null;
 			contractCreateResult = null;
 			tokens = NO_TOKENS;
 			tokenAdjustments = NO_TOKEN_ADJUSTMENTS;
@@ -782,6 +781,10 @@ public class ExpirableTxnRecord implements FCQueueElement {
 			assessedCustomFees = NO_CUSTOM_FEES;
 			newTokenAssociations = NO_NEW_TOKEN_ASSOCIATIONS;
 			alias = MISSING_ALIAS;
+			/*- if this is a revert of a child record we want to have contractCallResult -*/
+			if (removeCallResult) {
+				contractCallResult = null;
+			}
 		}
 
 		public CurrencyAdjustments getTransferList() {
