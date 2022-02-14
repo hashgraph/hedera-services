180d46f5ad20215a97c0d87cdc55a64b3e339446
Neeha <52669918+Neeharika-Sompalli@users.noreply.github.com>
2022-02-14 15:23:26 -0600

    Uncomment commented specs (#2865)
    * uncomment tests
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>
    
    * Set allowance units to negative for appropriate NFT adjustments
    
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>
    
    * unit test to validate nft balanceChange for approved allowance
    
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>
    
    * add to SuiteRunner
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>
    
    * bump up state version to 0.23
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>
    
    * modify names
    
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>
    
    Co-authored-by: anighanta <anirudh.ghanta@hedera.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/SuiteRunner.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/SuiteRunner.java
@@ -66,6 +66,8 @@ import com.hedera.services.bdd.suites.contract.records.LogsSuite;
 import com.hedera.services.bdd.suites.contract.records.RecordsSuite;
 import com.hedera.services.bdd.suites.crypto.AutoAccountCreationSuite;
 import com.hedera.services.bdd.suites.crypto.AutoAccountUpdateSuite;
+import com.hedera.services.bdd.suites.crypto.CryptoAdjustAllowanceSuite;
+import com.hedera.services.bdd.suites.crypto.CryptoApproveAllowanceSuite;
 import com.hedera.services.bdd.suites.crypto.CryptoCornerCasesSuite;
 import com.hedera.services.bdd.suites.crypto.CryptoCreateForSuiteRunner;
 import com.hedera.services.bdd.suites.crypto.CryptoCreateSuite;
@@ -491,6 +493,9 @@ public class SuiteRunner {
 		put("FreezeAbort", aof(FreezeAbort::new));
 		/* Memo validation */
 		put("MemoValidation", aof(MemoValidation::new));
+		/* Approval and Allowance */
+		put("CryptoApproveAllowanceSuite", aof(CryptoApproveAllowanceSuite::new));
+		put("CryptoAdjustAllowanceSuite", aof(CryptoAdjustAllowanceSuite::new));
 	}};
 
 	static boolean runAsync;
