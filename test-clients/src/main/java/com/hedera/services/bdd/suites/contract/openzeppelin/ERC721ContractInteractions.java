ff38e84b6fb2e3bafd8b79433c84c71f50abfad6
Ventsislav Stoevski <cordonahcodecraft@gmail.com>
2022-03-02 12:31:58 +0200

    fix: refactors ERC721ContractInteractions
    Signed-off-by: Ventsislav Stoevski <cordonahcodecraft@gmail.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/contract/openzeppelin/ERC721ContractInteractions.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/contract/openzeppelin/ERC721ContractInteractions.java
@@ -20,33 +20,23 @@ package com.hedera.services.bdd.suites.contract.openzeppelin;
  * ‍
  */
 
-import com.google.protobuf.ByteString;
 import com.hedera.services.bdd.spec.HapiApiSpec;
 import com.hedera.services.bdd.spec.queries.QueryVerbs;
 import com.hedera.services.bdd.suites.HapiApiSuite;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.List;
 
 import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
-import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.ERC721_APPROVE_ABI;
-import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.ERC721_BYTECODE_PATH;
-import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.ERC721_MINT_ABI;
-import static com.hedera.services.bdd.spec.infrastructure.meta.ContractResources.ERC721_TRANSFER_FROM_ABI;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
-import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
-import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.newContractCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.newFileCreate;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyListNamed;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.updateLargeFile;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
-import static com.hedera.services.bdd.suites.contract.Utils.extractByteCode;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
 
 public class ERC721ContractInteractions extends HapiApiSuite {
@@ -72,7 +62,7 @@ public class ERC721ContractInteractions extends HapiApiSuite {
         final var PAYER = "tx_payer";
         final var CONTRACT_CREATOR = "contractCreator";
         final var NFT_SENDER = "sender";
-        final var CONTRACT_FILE_NAME = "ERC721ContractFile";
+        final var CONTRACT = "GameItem";
         final var NFT_ID = 1;
 
         final var CREATE_TX = "create";
@@ -90,13 +80,11 @@ public class ERC721ContractInteractions extends HapiApiSuite {
                         cryptoCreate(CONTRACT_CREATOR),
                         cryptoCreate(NFT_SENDER),
                         QueryVerbs.getAccountBalance(PAYER).logged(),
-                        fileCreate(CONTRACT_FILE_NAME).payingWith(PAYER).key(FILE_KEY_LIST),
-                        updateLargeFile(PAYER, CONTRACT_FILE_NAME, extractByteCode(ERC721_BYTECODE_PATH))
+                        newFileCreate(CONTRACT)
                 ).when(
                         QueryVerbs.getAccountBalance(PAYER).logged(),
-                        contractCreate("testContract")
+                        newContractCreate(CONTRACT)
                                 .payingWith(CONTRACT_CREATOR)
-                                .bytecode(CONTRACT_FILE_NAME)
                                 .hasKnownStatus(SUCCESS)
                                 .via(CREATE_TX)
                 ).then(
@@ -111,16 +99,22 @@ public class ERC721ContractInteractions extends HapiApiSuite {
                             final var approveParams = new Object[]{contractCreatorId, NFT_ID};
                             final var transferFromParams = new Object[]{nftSenderId, contractCreatorId, NFT_ID};
 
-                            final var mint = contractCall("testContract",
-                                    ERC721_MINT_ABI, mintParams).payingWith(CONTRACT_CREATOR).via(MINT_TX);
+                            final var mint = contractCall(CONTRACT, "mint", mintParams
+                            )
+                                    .payingWith(CONTRACT_CREATOR)
+                                    .via(MINT_TX);
                             allRunFor(spec, mint);
 
-                            final var approve = contractCall("testContract",
-                                    ERC721_APPROVE_ABI, approveParams).payingWith(NFT_SENDER).via(APPROVE_TX);
+                            final var approve = contractCall(CONTRACT, "approve", approveParams
+                            )
+                                    .payingWith(NFT_SENDER)
+                                    .via(APPROVE_TX);
                             allRunFor(spec, approve);
 
-                            final var transferFrom = contractCall("testContract",
-                                    ERC721_TRANSFER_FROM_ABI, transferFromParams).payingWith(CONTRACT_CREATOR).via(TRANSFER_FROM_TX);
+                            final var transferFrom = contractCall(CONTRACT, "transferFrom", transferFromParams
+                            )
+                                    .payingWith(CONTRACT_CREATOR)
+                                    .via(TRANSFER_FROM_TX);
                             allRunFor(spec, transferFrom);
                         }),
                         QueryVerbs.getTxnRecord(CREATE_TX).logged(),
