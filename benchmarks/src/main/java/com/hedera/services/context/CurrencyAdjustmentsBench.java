b1db28b8fa25c97030920f11e0ff5b299b731604
Neeharika-Sompalli <neeharika.sompalli@hedera.com>
2022-03-12 10:48:02 -0600

    update benchmark to have purging adjustments
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@hedera.com>

--- benchmarks/src/main/java/com/hedera/services/context/CurrencyAdjustmentsBench.java
+++ benchmarks/src/main/java/com/hedera/services/context/CurrencyAdjustmentsBench.java
@@ -8,24 +8,53 @@ import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
-import java.util.Random;
-
 @State(Scope.Benchmark)
 @Fork(1)
 @Warmup(iterations = 1, time = 5)
 @Measurement(iterations = 5, time = 10)
 public class CurrencyAdjustmentsBench {
-	private static final long SEED = 1_234_321L;
-	private static final Random r = new Random(SEED);
-
 	@Benchmark
 	public void getTrackedCurrencyAdjustments(Blackhole blackhole) {
-		final var account = r.nextInt(1000);
+		var account = 1000;
+		var amount = 2000;
+
 		SideEffectsTracker tracker = new SideEffectsTracker();
-		for (int i = 0; i < 20; i++) {
-			tracker.trackHbarChange(account, r.nextInt(1000));
+		for (int i = 0; i < 10; i++) {
+			tracker.trackHbarChange(account, amount + 10);
+			tracker.trackHbarChange(account, amount - 10);
+			account++;
+		}
+		for (int i = 0; i < 5; i++) {
+			tracker.trackHbarChange(account, amount);
+			tracker.trackHbarChange(account, -1 * amount);
+			account++;
 		}
 		final var result = tracker.getNetTrackedHbarChanges();
 		blackhole.consume(result);
 	}
+
+	/*
+	BEFORE THE CHANGE --- FOR REF, WILL BE DELETED BEFORE MERGING
+		@Benchmark
+	public void getTrackedCurrencyAdjustments(Blackhole blackhole) {
+		var acc = 1000;
+		var amount = 2000;
+		SideEffectsTracker tracker = new SideEffectsTracker();
+		for (int i = 0; i < 10; i++) {
+			var account = AccountID.newBuilder().setAccountNum(acc).build();
+			tracker.trackHbarChange(account, amount + 10);
+			tracker.trackHbarChange(account, amount - 10);
+			acc++;
+		}
+		for (int i = 0; i < 5; i++) {
+			var account = AccountID.newBuilder().setAccountNum(acc).build();
+			tracker.trackHbarChange(account, amount);
+			tracker.trackHbarChange(account, -1 * amount);
+			acc++;
+		}
+		final var netChanges = tracker.getNetTrackedHbarChanges();
+		final var result = CurrencyAdjustments.fromGrpc(netChanges);
+		blackhole.consume(result);
+	}
+	 */
 }
