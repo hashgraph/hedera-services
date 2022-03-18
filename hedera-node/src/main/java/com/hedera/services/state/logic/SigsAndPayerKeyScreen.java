a54b53d815ec42e783b28f661d00cae8a1baa409
tinker-michaelj <michael.tinker@hedera.com>
2022-03-18 11:51:58 -0500

    Fix code smell
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/state/logic/SigsAndPayerKeyScreen.java
+++ hedera-node/src/main/java/com/hedera/services/state/logic/SigsAndPayerKeyScreen.java
@@ -9,9 +9,9 @@ package com.hedera.services.state.logic;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,10 +65,8 @@ public class SigsAndPayerKeyScreen {
 		rationalization.performFor(accessor);
 
 		final var sigStatus = rationalization.finalStatus();
-		if (sigStatus == OK) {
-			if (rationalization.usedSyncVerification()) {
-				speedometers.cycleSyncVerifications();
-			}
+		if (sigStatus == OK && rationalization.usedSyncVerification()) {
+			speedometers.cycleSyncVerifications();
 		}
 
 		if (hasActivePayerSig(accessor)) {
