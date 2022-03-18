955b1b7ca934d2ba9fada5a5adb8aee2e020a9ae
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2022-03-18 17:52:17 +0200

    Fix #2976 HIP-218: failure paths need refinement (#2985)
    * Fix ERC20 and ERC721 token query response for balanceOf and tokenURI in case the inputs are incorrect
    Add general 'null' response in case of err during query execution
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    * Fix/increase coverage
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    * Skip "updated" `WorldStateTokenAccount`s, prioritize EIP-1014 address of NFT owner, etc (#3000)
    
    * Fix multiple issues
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Fix compile error
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Uncomment specs
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    
    * Enable redirects throughout CI
    
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    
    Co-authored-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/store/contracts/HederaWorldStateTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/contracts/HederaWorldStateTest.java
@@ -20,7 +20,6 @@ package com.hedera.services.store.contracts;
  * ‍
  */
 
-import com.hedera.services.context.SideEffectsTracker;
 import com.hedera.services.context.properties.GlobalDynamicProperties;
 import com.hedera.services.ledger.SigImpactHistorian;
 import com.hedera.services.ledger.accounts.ContractAliases;
@@ -82,7 +81,6 @@ import static org.mockito.BDDMockito.willThrow;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -101,8 +99,6 @@ class HederaWorldStateTest {
 	private ContractAliases aliases;
 	@Mock
 	private GlobalDynamicProperties dynamicProperties;
-	@Mock
-	private SideEffectsTracker sideEffectsTracker;
 
 	final long balance = 1_234L;
 	final Id sponsor = new Id(0, 0, 1);
