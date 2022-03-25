10c465edc78d147e7a99dfd487ca430aa7821b57
anighanta <anirudh.ghanta@hedera.com>
2022-03-25 17:06:10 -0500

    fix sonar core smells
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/store/models/UniqueTokenTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/models/UniqueTokenTest.java
@@ -21,9 +21,13 @@ package com.hedera.services.store.models;
  */
 
 import com.hedera.services.state.submerkle.RichInstant;
+import com.hedera.test.utils.IdUtils;
 import org.junit.jupiter.api.Test;
 
+import java.nio.charset.StandardCharsets;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class UniqueTokenTest {
 	@Test
@@ -48,4 +52,53 @@ class UniqueTokenTest {
 		assertEquals(RichInstant.MISSING_INSTANT, subj.getCreationTime());
 	}
 
+
+	@Test
+	void equalsWorks() {
+		final var token1 = IdUtils.asModelId("0.0.12345");
+		final var token2 = IdUtils.asModelId("0.0.12346");
+		final var owner1 = IdUtils.asModelId("0.0.12347");
+		final var spender1 = IdUtils.asModelId("0.0.12349");
+		final var meta1 = "aa".getBytes(StandardCharsets.UTF_8);
+		final var sub1 = new UniqueToken(token1, 1L);
+		final var sub2 = new UniqueToken(token1, 2L);
+		final var sub3 = new UniqueToken(token2, 1L);
+		final var identical = new UniqueToken(token1, 1L);
+		sub1.setOwner(owner1);
+		sub1.setSpender(spender1);
+		sub1.setMetadata(meta1);
+		sub2.setOwner(owner1);
+		sub2.setSpender(spender1);
+		sub2.setMetadata(meta1);
+		sub3.setOwner(owner1);
+		sub3.setSpender(spender1);
+		sub3.setMetadata(meta1);
+		identical.setOwner(owner1);
+		identical.setSpender(spender1);
+		identical.setMetadata(meta1);
+
+		assertEquals(sub1, identical);
+		assertNotEquals(sub1, sub2);
+		assertNotEquals(sub2, sub3);
+		assertNotEquals(sub1, sub3);
+	}
+
+	@Test
+	void toStringWorks() {
+		final var token1 = IdUtils.asModelId("0.0.12345");
+		final var owner1 = IdUtils.asModelId("0.0.12346");
+		final var spender1 = IdUtils.asModelId("0.0.12347");
+		final var meta1 = "aa".getBytes(StandardCharsets.UTF_8);
+		final var subject = new UniqueToken(token1, 1L);
+		subject.setOwner(owner1);
+		subject.setSpender(spender1);
+		subject.setMetadata(meta1);
+		subject.setCreationTime(RichInstant.MISSING_INSTANT);
+
+		final var expected = "UniqueToken{tokenID=Id[shard=0, realm=0, num=12345], serialNum=1," +
+				" metadata=[97, 97], creationTime=RichInstant{seconds=0, nanos=0}, " +
+				"owner=Id[shard=0, realm=0, num=12346], spender=Id[shard=0, realm=0, num=12347]}";
+
+		assertEquals(expected, subject.toString());
+	}
 }
