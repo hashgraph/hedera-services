5c79664ad4b15c94427c15f6eed80ca7aca5dddb
Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>
2022-03-29 15:45:20 +0300

    fix: added unit test for compute method with empty transaction body
    Signed-off-by: Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>

--- hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
+++ hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
@@ -497,6 +497,7 @@ public class HTSPrecompiledContract extends AbstractPrecompiledContract {
 		try {
 			this.transactionBody = this.precompile.body(input, aliasResolver);
 		} catch (Exception e) {
+			log.warn("Internal precompile failure", e);
 			transactionBody = null;
 		}
 	}
