73209fbe8c50ee3e98cbd97eb63ec5af506cb9d0
JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>
2022-04-12 11:36:01 -0500

    Adjust timing for token reconnect tests (#3176)
    * save
    
    * save

--- test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/ValidateDuplicateTransactionAfterReconnect.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/ValidateDuplicateTransactionAfterReconnect.java
@@ -75,7 +75,7 @@ public class ValidateDuplicateTransactionAfterReconnect extends HapiApiSuite {
 				)
 				.then(
 						withLiveNode("0.0.8")
-								.within(60, TimeUnit.SECONDS)
+								.within(90, TimeUnit.SECONDS)
 								.loggingAvailabilityEvery(10)
 								.sleepingBetweenRetriesFor(5),
 						UtilVerbs.sleepFor(30 * 1000),
