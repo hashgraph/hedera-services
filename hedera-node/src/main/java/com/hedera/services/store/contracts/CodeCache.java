3b95733a955787065643492b8117267fd2e28860
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2022-04-19 15:21:10 +0300

    Support HTS / HIP-218 precompile for direct calls
    CodeCache to return redirectBytecode if the address we're querying is a token account address
    ContractCallTransitionLogic to not load target from accountStore if target is a token
    E2E tests with direct calls to Fungible and NFT tokens
    
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/src/main/java/com/hedera/services/store/contracts/CodeCache.java
+++ hedera-node/src/main/java/com/hedera/services/store/contracts/CodeCache.java
@@ -32,6 +32,7 @@ import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.util.concurrent.TimeUnit;
 
+import static com.hedera.services.store.contracts.WorldStateTokenAccount.proxyBytecodeFor;
 import static com.hedera.services.utils.EntityIdUtils.accountIdFromEvmAddress;
 
 /**
@@ -66,13 +67,24 @@ public class CodeCache {
         final var cacheKey = new BytesKey(address.toArray());
 
         var code = cache.getIfPresent(cacheKey);
-        if (code == null) {
-            final var bytecode = entityAccess.fetchCodeIfPresent(accountIdFromEvmAddress(address));
-            if (bytecode != null) {
-                code = new Code(bytecode, Hash.hash(bytecode));
-                cache.put(cacheKey, code);
-            }
+
+        if (code != null) {
+            return code;
         }
+
+        if (entityAccess.isTokenAccount(address)) {
+            final var interpolatedBytecode = proxyBytecodeFor(address);
+            code = new Code(interpolatedBytecode, Hash.hash(interpolatedBytecode));
+            cache.put(cacheKey, code);
+            return code;
+        }
+
+        final var bytecode = entityAccess.fetchCodeIfPresent(accountIdFromEvmAddress(address));
+        if (bytecode != null) {
+            code = new Code(bytecode, Hash.hash(bytecode));
+            cache.put(cacheKey, code);
+        }
+
         return code;
     }
 
--- hedera-node/src/test/java/com/hedera/services/txns/contract/ContractCallTransitionLogicTest.java
+++ hedera-node/src/test/java/com/hedera/services/txns/contract/ContractCallTransitionLogicTest.java
@@ -30,12 +30,12 @@ import com.hedera.services.ledger.accounts.AliasManager;
 import com.hedera.services.records.TransactionRecordService;
 import com.hedera.services.store.AccountStore;
 import com.hedera.services.store.contracts.CodeCache;
+import com.hedera.services.store.contracts.EntityAccess;
 import com.hedera.services.store.contracts.HederaWorldState;
 import com.hedera.services.store.models.Account;
 import com.hedera.services.store.models.Id;
 import com.hedera.services.utils.EntityNum;
 import com.hedera.services.utils.accessors.PlatformTxnAccessor;
-import com.hedera.services.utils.accessors.SignedTxnAccessor;
 import com.hedera.test.utils.IdUtils;
 import com.hederahashgraph.api.proto.java.ContractCallTransactionBody;
 import com.hederahashgraph.api.proto.java.ContractID;
@@ -94,6 +94,8 @@ class ContractCallTransitionLogicTest {
 	private SigImpactHistorian sigImpactHistorian;
 	@Mock
 	private AliasManager aliasManager;
+	@Mock
+	private EntityAccess entityAccess;
 
 	private TransactionBody contractCallTxn;
 	private final Instant consensusTime = Instant.now();
@@ -105,7 +107,7 @@ class ContractCallTransitionLogicTest {
 	private void setup() {
 		subject = new ContractCallTransitionLogic(
 				txnCtx, accountStore, worldState, recordService,
-				evmTxProcessor, properties, codeCache, sigImpactHistorian, aliasManager);
+				evmTxProcessor, properties, codeCache, sigImpactHistorian, aliasManager, entityAccess);
 	}
 
 	@Test
