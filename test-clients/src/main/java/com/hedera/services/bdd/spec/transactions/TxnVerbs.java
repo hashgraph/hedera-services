c6c97ba6a817c07602aa3a4d1598fb6e3e1115f4
Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>
2022-05-05 14:37:06 +0300

    fix: fixes after merge
    Signed-off-by: Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>

--- test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnVerbs.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnVerbs.java
@@ -338,10 +338,6 @@ public class TxnVerbs {
 		return new HapiContractCall(abi, contract, params);
 	}
 
-	public static HapiContractCall explicitContractCall(String contract, String abi, Object... params) {
-		return new HapiContractCall(abi, contract, params);
-	}
-
 	/**
 	 * This method allows the developer to invoke a contract function by the name of the called contract and the name
 	 * of the desired function and make an ethereum call
