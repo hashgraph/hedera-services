a78d4c522eb460e0d2303674fc87a1b2a0d9cd95
anighanta <anirudh.ghanta@hedera.com>
2022-05-10 14:17:40 -0500

    address code smell
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
+++ hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/HTSPrecompiledContract.java
@@ -119,7 +119,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
