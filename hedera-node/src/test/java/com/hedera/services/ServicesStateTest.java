edbd5a84e67521c6fbc528cb559ee0b62abc46b6
Patrick Staton <2013403+statop@users.noreply.github.com>
2022-05-17 08:37:13 -0400

    feat(#3069): long term scheduled transactions - remove merge issue
    Signed-off-by: Patrick Staton <2013403+statop@users.noreply.github.com>

--- hedera-node/src/test/java/com/hedera/services/ServicesStateTest.java
+++ hedera-node/src/test/java/com/hedera/services/ServicesStateTest.java
@@ -819,11 +819,6 @@ class ServicesStateTest {
 		assertSame(specialFiles, copy.specialFiles());
 	}
 
-	@Test
-	void migrationWorksAsExpected() {
-		subject.migrate();
-	}
-
 	private List<MerkleNode> legacyChildrenWith(
 			AddressBook addressBook,
 			MerkleNetworkContext networkContext,
