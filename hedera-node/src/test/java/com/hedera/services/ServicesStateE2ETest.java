3dd287b0db6b22df984356689c79fe1777b4c9dc
Ray Juang <ray.juang@swirldslabs.com>
2022-05-18 14:29:45 -0700

    Add E2E unit tests for migration and genesis init.
    Signed-off-by: Ray Juang <ray.juang@swirldslabs.com>

--- hedera-node/src/test/java/com/hedera/services/ServicesStateE2ETest.java
+++ hedera-node/src/test/java/com/hedera/services/ServicesStateE2ETest.java
@@ -20,8 +20,25 @@ package com.hedera.services;
  * ‍
  */
 
+import com.hedera.services.context.MutableStateChildren;
+import com.hedera.services.context.init.ServicesInitFlow;
+import com.hedera.services.state.DualStateAccessor;
+import com.hedera.services.state.forensics.HashLogger;
+import com.hedera.services.state.migration.StateChildIndices;
+import com.hedera.services.state.org.StateMetadata;
+import com.hedera.services.stream.RecordsRunningHashLeaf;
 import com.hedera.test.utils.ClassLoaderHelper;
+import com.swirlds.common.crypto.Hash;
+import com.swirlds.common.crypto.RunningHash;
+import com.swirlds.common.crypto.SerializablePublicKey;
+import com.swirlds.common.system.Address;
+import com.swirlds.common.system.AddressBook;
+import com.swirlds.common.system.NodeId;
+import com.swirlds.common.system.Platform;
+import com.swirlds.common.system.SwirldDualState;
+import com.swirlds.fchashmap.FCHashMap;
 import com.swirlds.platform.SignedStateFileManager;
+import com.swirlds.platform.state.DualStateImpl;
 import com.swirlds.platform.state.SignedState;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -29,6 +46,12 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+
+import static com.hedera.services.context.AppsManager.APPS;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * These tests are responsible for testing loading of signed state data generated for various scenarios from various
@@ -49,7 +72,55 @@ public class ServicesStateE2ETest {
 
 	@Test
 	void testNftsFromSignedStateV24() {
-		Assertions.assertDoesNotThrow(() -> loadSignedState(signedStateDir + "v0.24.2-nfts/SignedState.swh"));
+		assertDoesNotThrow(() -> loadSignedState(signedStateDir + "v0.24.2-nfts/SignedState.swh"));
+	}
+
+	@Test
+	void testMigrationFromSignedStateV24() throws IOException {
+		SignedState signedState = loadSignedState(signedStateDir + "v0.24.2-nfts/SignedState.swh");
+		AddressBook addressBook = signedState.getAddressBook();
+		SwirldDualState swirldDualState = signedState.getState().getSwirldDualState();
+
+		Platform platform = createMockPlatform();
+		ServicesState servicesState = (ServicesState) signedState.getSwirldState();
+		servicesState.init(platform, addressBook, swirldDualState);
+		servicesState.setMetadata(new StateMetadata(createMockApp(), new FCHashMap<>()));
+		servicesState.migrate();
+	}
+
+	@Test
+	void testGenesisState() {
+		SwirldDualState swirldDualState = new DualStateImpl();
+		ServicesState servicesState = new ServicesState();
+		RecordsRunningHashLeaf recordsRunningHashLeaf = new RecordsRunningHashLeaf();
+		recordsRunningHashLeaf.setRunningHash(new RunningHash(new Hash()));
+		servicesState.setChild(StateChildIndices.RECORD_STREAM_RUNNING_HASH, recordsRunningHashLeaf);
+		Platform platform = createMockPlatform();
+		long nodeId = platform.getSelfId().getId();
+		Address address;
+		address = new Address(
+				nodeId, "", "", 1L, false, null, -1, null, -1, null, -1, null, -1,
+				null, null, (SerializablePublicKey)null, "");
+		AddressBook addressBook = new AddressBook(List.of(address));
+		ServicesApp mockApp = createMockApp();
+
+		APPS.save(platform.getSelfId().getId(), mockApp);
+		assertDoesNotThrow(() -> servicesState.genesisInit(platform, addressBook, swirldDualState));
+	}
+
+
+	private static ServicesApp createMockApp() {
+		ServicesApp mockApp = mock(ServicesApp.class);
+		when(mockApp.dualStateAccessor()).thenReturn(new DualStateAccessor());
+		when(mockApp.initializationFlow()).thenReturn(mock(ServicesInitFlow.class));
+		when(mockApp.hashLogger()).thenReturn(new HashLogger());
+		when(mockApp.workingState()).thenReturn(new MutableStateChildren());
+		return mockApp;
+	}
+	private static Platform createMockPlatform() {
+		Platform platform = mock(Platform.class);
+		when(platform.getSelfId()).thenReturn(new NodeId(false, 0));
+		return platform;
 	}
 
 	private static SignedState loadSignedState(final String path) throws IOException {
