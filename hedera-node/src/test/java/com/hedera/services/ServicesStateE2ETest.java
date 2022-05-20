e1911090a2eb58db16e54abd17b7575792174264
Ray Juang <ray.juang@swirldslabs.com>
2022-05-20 09:32:25 -0700

    Updated ServicesStateE2ETest to use less mocks.
    Signed-off-by: Ray Juang <ray.juang@swirldslabs.com>

--- hedera-node/src/test/java/com/hedera/services/ServicesStateE2ETest.java
+++ hedera-node/src/test/java/com/hedera/services/ServicesStateE2ETest.java
@@ -20,10 +20,7 @@ package com.hedera.services;
  * ‍
  */
 
-import com.hedera.services.context.MutableStateChildren;
-import com.hedera.services.context.init.ServicesInitFlow;
-import com.hedera.services.state.DualStateAccessor;
-import com.hedera.services.state.forensics.HashLogger;
+import com.hedera.services.context.properties.BootstrapProperties;
 import com.hedera.services.state.migration.StateChildIndices;
 import com.hedera.services.state.org.StateMetadata;
 import com.hedera.services.stream.RecordsRunningHashLeaf;
@@ -31,11 +28,11 @@ import com.hedera.test.utils.ClassLoaderHelper;
 import com.swirlds.common.crypto.Hash;
 import com.swirlds.common.crypto.RunningHash;
 import com.swirlds.common.crypto.SerializablePublicKey;
+import com.swirlds.common.crypto.engine.CryptoEngine;
 import com.swirlds.common.system.Address;
 import com.swirlds.common.system.AddressBook;
 import com.swirlds.common.system.NodeId;
 import com.swirlds.common.system.Platform;
-import com.swirlds.common.system.SwirldDualState;
 import com.swirlds.fchashmap.FCHashMap;
 import com.swirlds.platform.SignedStateFileManager;
 import com.swirlds.platform.state.DualStateImpl;
@@ -46,6 +43,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 import static com.hedera.services.ServicesState.EMPTY_HASH;
@@ -83,7 +81,7 @@ public class ServicesStateE2ETest {
 	}
 
 	@Test
-	void testGenesisState() {
+	void testGenesisState() throws NoSuchAlgorithmException, IOException {
 		final var swirldDualState = new DualStateImpl();
 		final var servicesState = new ServicesState();
 		final var recordsRunningHashLeaf = new RecordsRunningHashLeaf();
@@ -95,13 +93,13 @@ public class ServicesStateE2ETest {
 				nodeId, "", "", 1L, false, null, -1, null, -1, null, -1, null, -1,
 				null, null, (SerializablePublicKey)null, "");
 		final var addressBook = new AddressBook(List.of(address));
-		final var mockApp = createMockApp();
+		final var app = createApp(platform);
 
-		APPS.save(platform.getSelfId().getId(), mockApp);
+		APPS.save(platform.getSelfId().getId(), app);
 		assertDoesNotThrow(() -> servicesState.genesisInit(platform, addressBook, swirldDualState));
 	}
 
-	private static Hash migrate(String dataPath) throws IOException, InterruptedException {
+	private static Hash migrate(String dataPath) throws IOException, InterruptedException, NoSuchAlgorithmException {
 		final var signedState = loadSignedState(dataPath);
 		final var addressBook = signedState.getAddressBook();
 		final var swirldDualState = signedState.getState().getSwirldDualState();
@@ -109,23 +107,25 @@ public class ServicesStateE2ETest {
 		final var platform = createMockPlatform();
 		final var servicesState = (ServicesState) signedState.getSwirldState();
 		servicesState.init(platform, addressBook, swirldDualState);
-		servicesState.setMetadata(new StateMetadata(createMockApp(), new FCHashMap<>()));
+		servicesState.setMetadata(new StateMetadata(createApp(platform), new FCHashMap<>()));
 		servicesState.migrate();
 		return servicesState.runningHashLeaf().currentRunningHash();
 	}
 
-	private static ServicesApp createMockApp() {
-		final var mockApp = mock(ServicesApp.class);
-		when(mockApp.dualStateAccessor()).thenReturn(new DualStateAccessor());
-		when(mockApp.initializationFlow()).thenReturn(mock(ServicesInitFlow.class));
-		when(mockApp.hashLogger()).thenReturn(new HashLogger());
-		when(mockApp.workingState()).thenReturn(new MutableStateChildren());
-		return mockApp;
+	private static ServicesApp createApp(Platform platform) throws NoSuchAlgorithmException, IOException {
+		return DaggerServicesApp.builder()
+				.initialHash(new Hash())
+				.platform(platform)
+				.selfId(platform.getSelfId().getId())
+				.staticAccountMemo("memo")
+				.bootstrapProps(new BootstrapProperties())
+				.build();
 	}
 
 	private static Platform createMockPlatform() {
 		final var platform = mock(Platform.class);
 		when(platform.getSelfId()).thenReturn(new NodeId(false, 0));
+		when(platform.getCryptography()).thenReturn(new CryptoEngine());
 		return platform;
 	}
 
