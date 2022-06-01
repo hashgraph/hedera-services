dc7fe0d19a06bddeb3b95ebe880e828bec1f1a79
anighanta <anirudh.ghanta@hedera.com>
2022-06-01 15:09:36 -0500

    use static props for generating node reward account id as well
    Signed-off-by: anighanta <anirudh.ghanta@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/fees/charging/NarratedLedgerCharging.java
+++ hedera-node/src/main/java/com/hedera/services/fees/charging/NarratedLedgerCharging.java
@@ -26,7 +26,6 @@ import com.hedera.services.context.properties.GlobalDynamicProperties;
 import com.hedera.services.fees.FeeExemptions;
 import com.hedera.services.ledger.HederaLedger;
 import com.hedera.services.state.merkle.MerkleAccount;
-import com.hedera.services.state.submerkle.EntityId;
 import com.hedera.services.utils.EntityNum;
 import com.hedera.services.utils.accessors.TxnAccessor;
 import com.hederahashgraph.api.proto.java.AccountID;
@@ -39,7 +38,6 @@ import java.util.Optional;
 import java.util.function.Supplier;
 
 import static com.hedera.services.context.properties.StaticPropertiesHolder.STATIC_PROPERTIES;
-import static com.hedera.services.utils.EntityIdUtils.asAccount;
 
 /**
  * Implements the {@link NarratedCharging} contract using a injected {@link HederaLedger}
@@ -85,7 +83,7 @@ public class NarratedLedgerCharging implements NarratedCharging {
 		this.feeExemptions = feeExemptions;
 		this.dynamicProperties = dynamicProperties;
 		stakingRewardAccountId = STATIC_PROPERTIES.scopedAccountWith(accountNumbers.stakingRewardAccount());
-		nodeRewardAccountId = asAccount(EntityId.fromIdentityCode((int) accountNumbers.nodeRewardAccount()));
+		nodeRewardAccountId = STATIC_PROPERTIES.scopedAccountWith(accountNumbers.nodeRewardAccount());
 	}
 
 	@Override
