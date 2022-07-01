75efbe878e0e1480102c2fc3aa8e6395194bca6b
Nathan Klick <nathan@swirldslabs.com>
2022-07-01 15:44:26 -0500

    Fixes sonarqube analysis issues
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
@@ -1,3 +1,4 @@
+import gradle.kotlin.dsl.accessors._de3ff27eccbd9efdc5c099f60a1d8f4c.check
 import org.sonarqube.gradle.SonarQubeTask
 import java.text.SimpleDateFormat
 import java.util.*
@@ -26,7 +27,6 @@ plugins {
     `java-library`
     `maven-publish`
     jacoco
-    id("org.sonarqube")
     id("com.diffplug.spotless")
 }
 
@@ -245,35 +245,7 @@ tasks.jacocoTestReport {
 }
 
 tasks.check {
-    dependsOn(tasks.jacocoTestReport)
+    dependsOn(tasks.named<JacocoReport>("jacocoTestReport"))
 }
 
-// Configure the Sonarqube extension for SonarCloud reporting. These properties should not be changed so no need to
-// have them in the gradle.properties defintions.
-sonarqube {
-    properties {
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "hashgraph")
-        property("sonar.projectKey", "com.hedera.hashgraph:hedera-services")
-        property("sonar.projectName", "Hedera Services")
-        property("sonar.projectVersion", project.version)
-        property("sonar.projectDescription", "Hedera Services (crypto, file, contract, consensus) on the Platform")
-        property("sonar.links.homepage", "https://github.com/hashgraph/hedera-services")
-        property("sonar.links.ci", "https://github.com/hashgraph/hedera-services/actions")
-        property("sonar.links.issue", "https://github.com/hashgraph/hedera-services/issues")
-        property("sonar.links.scm", "https://github.com/hashgraph/hedera-services.git")
 
-        property("sonar.coverage.exclusions", "**/test-clients/**")
-
-        // Ignored to match pom.xml setup
-        property("sonar.issue.ignore.multicriteria", "e1,e2")
-        property("sonar.issue.ignore.multicriteria.e1.resourceKey", "**/*.java")
-        property("sonar.issue.ignore.multicriteria.e1.ruleKey", "java:S125")
-        property("sonar.issue.ignore.multicriteria.e2.resourceKey", "**/*.java")
-        property("sonar.issue.ignore.multicriteria.e2.ruleKey", "java:S1874")
-    }
-}
-
-tasks.withType<SonarQubeTask>().configureEach {
-    dependsOn(tasks.jacocoTestReport)
-}
