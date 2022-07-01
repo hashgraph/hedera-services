2778c76b58ffd9e638c49c24736d7fdbc39aa3f9
Nathan Klick <nathan@swirldslabs.com>
2022-07-01 10:24:59 -0500

    Add suppression clauses for two SonarCloud warnings
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/HapiApiSuite.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/HapiApiSuite.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 package com.hedera.services.bdd.suites;
 
 import static com.hedera.services.bdd.suites.HapiApiSuite.FinalOutcome.SUITE_FAILED;
@@ -47,6 +47,7 @@ public abstract class HapiApiSuite {
         SUITE_FAILED
     }
 
+    @SuppressWarnings("java:S2245")
     private static final Random RANDOM = new Random();
 
     protected abstract Logger getResultsLogger();
--- test-clients/src/main/java/com/hedera/services/bdd/suites/SuiteRunner.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/SuiteRunner.java
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 package com.hedera.services.bdd.suites;
 
 import static com.hedera.services.bdd.spec.HapiSpecSetup.NodeSelection.FIXED;
@@ -218,6 +218,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class SuiteRunner {
+    @SuppressWarnings("java:S2245")
     private static final Random RANDOM = new Random();
     private static final Logger log = LogManager.getLogger(SuiteRunner.class);
     private static final int SUITE_NAME_WIDTH = 32;
