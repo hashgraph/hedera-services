68deb206b0e897f5f9a347ebb7414c7f250d9e2f
Nathan Klick <nathan@swirldslabs.com>
2022-07-01 08:06:58 -0500

    Fix spotless license header application
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
@@ -112,7 +112,7 @@ spotless {
             * See the License for the specific language governing permissions and
             * limitations under the License.
             */ 
-        """.trimIndent(),"(package|import)")
+        """.trimIndent(), "import")
     })
 }
 
