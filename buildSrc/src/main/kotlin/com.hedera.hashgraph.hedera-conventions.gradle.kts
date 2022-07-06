019899fdbe65a2a363cc2f88bcb6aacc1122fbe2
Nathan Klick <nathan@swirldslabs.com>
2022-07-05 20:24:24 -0500

    Fix for extra space after license header
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.hedera.hashgraph.hedera-conventions.gradle.kts
@@ -117,7 +117,7 @@ spotless {
             * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
             * See the License for the specific language governing permissions and
             * limitations under the License.
-            */ 
+            */
         """.trimIndent(), "(package|import)")
     })
 }
