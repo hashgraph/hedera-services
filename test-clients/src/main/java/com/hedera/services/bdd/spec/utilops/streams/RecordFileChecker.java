48659622f1c8482759d4a6938751acd809541b43
tinker-michaelj <michael.tinker@hedera.com>
2022-07-06 19:22:30 -0500

    Add missing files
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/streams/RecordFileChecker.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/streams/RecordFileChecker.java
@@ -3,15 +3,15 @@ package com.hedera.services.bdd.spec.utilops.streams;
 /*-
  * ‌
  * Hedera Services Test Clients
- *
+ * ​
  * Copyright (C) 2018 - 2022 Hedera Hashgraph, LLC
- *
+ * ​
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,6 +19,7 @@ package com.hedera.services.bdd.spec.utilops.streams;
  * limitations under the License.
  * ‍
  */
+
 
 import com.hedera.services.bdd.spec.HapiApiSpec;
 import com.hedera.services.bdd.spec.queries.file.HapiGetFileContents;
