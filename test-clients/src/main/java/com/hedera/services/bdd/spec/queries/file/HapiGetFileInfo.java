1bb9107d5be678525b9689f7a02c84da6e329801
Nathan Klick <nathan@swirldslabs.com>
2022-07-08 01:13:58 -0500

    Cleans up sonar issues in HapiGetFileInfo.java
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- test-clients/src/main/java/com/hedera/services/bdd/spec/queries/file/HapiGetFileInfo.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/queries/file/HapiGetFileInfo.java
@@ -1,11 +1,6 @@
-package com.hedera.services.bdd.spec.queries.file;
-
-/*-
- * ‌
- * Hedera Services Test Clients
- * ​
- * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
- * ​
+/*
+ * Copyright (C) 2020-2022 Hedera Hashgraph, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,8 +12,11 @@ package com.hedera.services.bdd.spec.queries.file;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
+package com.hedera.services.bdd.spec.queries.file;
+
+import static com.hedera.services.bdd.spec.queries.QueryUtils.answerCostHeader;
+import static com.hedera.services.bdd.spec.queries.QueryUtils.answerHeader;
 
 import com.google.common.base.MoreObjects;
 import com.hedera.services.bdd.spec.HapiApiSpec;
@@ -30,165 +28,171 @@ import com.hederahashgraph.api.proto.java.HederaFunctionality;
 import com.hederahashgraph.api.proto.java.Query;
 import com.hederahashgraph.api.proto.java.Response;
 import com.hederahashgraph.api.proto.java.Transaction;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Assertions;
-
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.LongPredicate;
 import java.util.function.LongSupplier;
 import java.util.function.Supplier;
-
-import static com.hedera.services.bdd.spec.queries.QueryUtils.answerCostHeader;
-import static com.hedera.services.bdd.spec.queries.QueryUtils.answerHeader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Assertions;
 
 public class HapiGetFileInfo extends HapiQueryOp<HapiGetFileInfo> {
-	static final Logger log = LogManager.getLogger(HapiGetFileInfo.class);
+    static final Logger log = LogManager.getLogger(HapiGetFileInfo.class);
 
-	private static final String MISSING_FILE = "<n/a>";
+    private static final String MISSING_FILE = "<n/a>";
 
-	private String file = MISSING_FILE;
+    private String file = MISSING_FILE;
 
-	private boolean immutable = false;
-	private Optional<String> saveFileInfoToReg = Optional.empty();
-	private Optional<Boolean> expectedDeleted = Optional.empty();
-	private Optional<String> expectedWacl = Optional.empty();
-	private Optional<String> expectedMemo = Optional.empty();
-	private Optional<String> expectedKeyRepr = Optional.empty();
-	private Optional<LongPredicate> expiryTest = Optional.empty();
-	private Optional<Supplier<String>> fileSupplier = Optional.empty();
-	private Optional<Consumer<String>> keyReprObserver = Optional.empty();
+    private boolean immutable = false;
+    private Optional<String> saveFileInfoToReg = Optional.empty();
+    private Optional<Boolean> expectedDeleted = Optional.empty();
+    private Optional<String> expectedWacl = Optional.empty();
+    private Optional<String> expectedMemo = Optional.empty();
 
-	private FileID fileId;
+    @SuppressWarnings("java:S1068")
+    private Optional<String> expectedKeyRepr = Optional.empty();
 
-	@Override
-	public HederaFunctionality type() {
-		return HederaFunctionality.FileGetInfo;
-	}
+    private Optional<LongPredicate> expiryTest = Optional.empty();
+    private Optional<Supplier<String>> fileSupplier = Optional.empty();
+    private Optional<Consumer<String>> keyReprObserver = Optional.empty();
 
-	@Override
-	protected HapiGetFileInfo self() {
-		return this;
-	}
+    private FileID fileId;
 
-	public HapiGetFileInfo isUnmodifiable() {
-		immutable = true;
-		return this;
-	}
+    @Override
+    public HederaFunctionality type() {
+        return HederaFunctionality.FileGetInfo;
+    }
 
-	public HapiGetFileInfo hasKeyReprTo(String repr) {
-		expectedKeyRepr = Optional.of(repr);
-		return this;
-	}
+    @Override
+    protected HapiGetFileInfo self() {
+        return this;
+    }
 
-	public HapiGetFileInfo exposingKeyReprTo(Consumer<String> obs) {
-		keyReprObserver = Optional.of(obs);
-		return this;
-	}
+    public HapiGetFileInfo isUnmodifiable() {
+        immutable = true;
+        return this;
+    }
 
-	public HapiGetFileInfo hasMemo(String v) {
-		expectedMemo = Optional.of(v);
-		return this;
-	}
+    public HapiGetFileInfo hasKeyReprTo(String repr) {
+        expectedKeyRepr = Optional.of(repr);
+        return this;
+    }
 
-	public HapiGetFileInfo hasExpiry(LongSupplier expected) {
-		expiryTest = Optional.of(v -> v == expected.getAsLong());
-		return this;
-	}
+    public HapiGetFileInfo exposingKeyReprTo(Consumer<String> obs) {
+        keyReprObserver = Optional.of(obs);
+        return this;
+    }
 
-	public HapiGetFileInfo hasExpiryPassing(LongPredicate test) {
-		expiryTest = Optional.of(test);
-		return this;
-	}
+    public HapiGetFileInfo hasMemo(String v) {
+        expectedMemo = Optional.of(v);
+        return this;
+    }
 
-	public HapiGetFileInfo hasDeleted(boolean expected) {
-		expectedDeleted = Optional.of(expected);
-		return this;
-	}
+    public HapiGetFileInfo hasExpiry(LongSupplier expected) {
+        expiryTest = Optional.of(v -> v == expected.getAsLong());
+        return this;
+    }
 
-	public HapiGetFileInfo hasWacl(String expected) {
-		expectedWacl = Optional.of(expected);
-		return this;
-	}
+    public HapiGetFileInfo hasExpiryPassing(LongPredicate test) {
+        expiryTest = Optional.of(test);
+        return this;
+    }
 
-	public HapiGetFileInfo saveToRegistry(String name) {
-		saveFileInfoToReg = Optional.of(name);
-		return this;
-	}
+    public HapiGetFileInfo hasDeleted(boolean expected) {
+        expectedDeleted = Optional.of(expected);
+        return this;
+    }
 
-	public HapiGetFileInfo(String file) {
-		this.file = file;
-	}
+    public HapiGetFileInfo hasWacl(String expected) {
+        expectedWacl = Optional.of(expected);
+        return this;
+    }
 
-	public HapiGetFileInfo(Supplier<String> supplier) {
-		fileSupplier = Optional.of(supplier);
-	}
+    public HapiGetFileInfo saveToRegistry(String name) {
+        saveFileInfoToReg = Optional.of(name);
+        return this;
+    }
 
-	@Override
-	protected void submitWith(HapiApiSpec spec, Transaction payment) throws Throwable {
-		Query query = getFileInfoQuery(spec, payment, false);
-		response = spec.clients().getFileSvcStub(targetNodeFor(spec), useTls).getFileInfo(query);
-		if (verboseLoggingOn) {
-			log.info("Info for file '" + file + "': " + response.getFileGetInfo());
-		}
-		if (saveFileInfoToReg.isPresent()) {
-			spec.registry().saveFileInfo(saveFileInfoToReg.get(), response.getFileGetInfo().getFileInfo());
-		}
-	}
+    public HapiGetFileInfo(String file) {
+        this.file = file;
+    }
 
-	@Override
-	protected long lookupCostWith(HapiApiSpec spec, Transaction payment) throws Throwable {
-		Query query = getFileInfoQuery(spec, payment, true);
-		Response response = spec.clients().getFileSvcStub(targetNodeFor(spec), useTls).getFileInfo(query);
-		return costFrom(response);
-	}
+    public HapiGetFileInfo(Supplier<String> supplier) {
+        fileSupplier = Optional.of(supplier);
+    }
 
-	@Override
-	protected void assertExpectationsGiven(HapiApiSpec spec) throws Throwable {
-		var info = response.getFileGetInfo().getFileInfo();
+    @Override
+    protected void submitWith(HapiApiSpec spec, Transaction payment) throws Throwable {
+        Query query = getFileInfoQuery(spec, payment, false);
+        response = spec.clients().getFileSvcStub(targetNodeFor(spec), useTls).getFileInfo(query);
+        if (verboseLoggingOn) {
+            log.info("Info for file '{}': {}", file, response.getFileGetInfo());
+        }
+        if (saveFileInfoToReg.isPresent()) {
+            spec.registry()
+                    .saveFileInfo(saveFileInfoToReg.get(), response.getFileGetInfo().getFileInfo());
+        }
+    }
 
-		Assertions.assertEquals(
-				TxnUtils.asFileId(file, spec),
-				info.getFileID(),
-				"Wrong file id!");
-		keyReprObserver.ifPresent(obs -> obs.accept(info.getKeys().toString().replaceAll("\\s", "")));
+    @Override
+    protected long lookupCostWith(HapiApiSpec spec, Transaction payment) throws Throwable {
+        Query query = getFileInfoQuery(spec, payment, true);
+        Response response =
+                spec.clients().getFileSvcStub(targetNodeFor(spec), useTls).getFileInfo(query);
+        return costFrom(response);
+    }
 
-		if (immutable) {
-			Assertions.assertFalse(info.hasKeys(), "Should have no WACL, expected immutable!");
-		}
-		expectedWacl.ifPresent(k -> Assertions.assertEquals(
-				spec.registry().getKey(k).getKeyList(),
-				info.getKeys(),
-				"Bad WACL!"));
-		expectedDeleted.ifPresent(f -> Assertions.assertEquals(f, info.getDeleted(), "Bad deletion status!"));
-		long actual = info.getExpirationTime().getSeconds();
-		expiryTest.ifPresent(p ->
-				Assertions.assertTrue(p.test(actual), String.format("Expiry of %d was not as expected!", actual)));
-		expectedMemo.ifPresent(e -> Assertions.assertEquals(e, info.getMemo()));
-		expectedLedgerId.ifPresent(id -> Assertions.assertEquals(rationalize(id), info.getLedgerId()));
-	}
+    @Override
+    @SuppressWarnings("java:S5960")
+    protected void assertExpectationsGiven(HapiApiSpec spec) throws Throwable {
+        var info = response.getFileGetInfo().getFileInfo();
 
-	private Query getFileInfoQuery(HapiApiSpec spec, Transaction payment, boolean costOnly) {
-		file = fileSupplier.isPresent() ? fileSupplier.get().get() : file;
-		var id = TxnUtils.asFileId(file, spec);
-		fileId = id;
-		FileGetInfoQuery infoQuery = FileGetInfoQuery.newBuilder()
-				.setHeader(costOnly ? answerCostHeader(payment) : answerHeader(payment))
-				.setFileID(id)
-				.build();
-		return Query.newBuilder().setFileGetInfo(infoQuery).build();
-	}
+        Assertions.assertEquals(TxnUtils.asFileId(file, spec), info.getFileID(), "Wrong file id!");
+        keyReprObserver.ifPresent(
+                obs -> obs.accept(info.getKeys().toString().replaceAll("\\s", "")));
 
-	@Override
-	protected boolean needsPayment() {
-		return true;
-	}
+        if (immutable) {
+            Assertions.assertFalse(info.hasKeys(), "Should have no WACL, expected immutable!");
+        }
+        expectedWacl.ifPresent(
+                k ->
+                        Assertions.assertEquals(
+                                spec.registry().getKey(k).getKeyList(),
+                                info.getKeys(),
+                                "Bad WACL!"));
+        expectedDeleted.ifPresent(
+                f -> Assertions.assertEquals(f, info.getDeleted(), "Bad deletion status!"));
+        long actual = info.getExpirationTime().getSeconds();
+        expiryTest.ifPresent(
+                p ->
+                        Assertions.assertTrue(
+                                p.test(actual),
+                                String.format("Expiry of %d was not as expected!", actual)));
+        expectedMemo.ifPresent(e -> Assertions.assertEquals(e, info.getMemo()));
+        expectedLedgerId.ifPresent(
+                id -> Assertions.assertEquals(rationalize(id), info.getLedgerId()));
+    }
 
-	@Override
-	protected MoreObjects.ToStringHelper toStringHelper() {
-		return super.toStringHelper()
-				.add("file", file)
-				.add("fileId", fileId);
-	}
+    private Query getFileInfoQuery(HapiApiSpec spec, Transaction payment, boolean costOnly) {
+        file = fileSupplier.isPresent() ? fileSupplier.get().get() : file;
+        var id = TxnUtils.asFileId(file, spec);
+        fileId = id;
+        FileGetInfoQuery infoQuery =
+                FileGetInfoQuery.newBuilder()
+                        .setHeader(costOnly ? answerCostHeader(payment) : answerHeader(payment))
+                        .setFileID(id)
+                        .build();
+        return Query.newBuilder().setFileGetInfo(infoQuery).build();
+    }
+
+    @Override
+    protected boolean needsPayment() {
+        return true;
+    }
+
+    @Override
+    protected MoreObjects.ToStringHelper toStringHelper() {
+        return super.toStringHelper().add("file", file).add("fileId", fileId);
+    }
 }
