dafb2d9baf7f15c9c70e701294658fcf07920551
Nathan Klick <nathan@swirldslabs.com>
2022-07-11 14:54:51 -0500

    Cleans up sonar issues in StandardSerdes.java
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/utils/sysfiles/serdes/StandardSerdes.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/utils/sysfiles/serdes/StandardSerdes.java
@@ -1,11 +1,6 @@
-package com.hedera.services.bdd.suites.utils.sysfiles.serdes;
-
-/*-
- * ‌
- * Hedera Services Test Clients
- * ​
- * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
- * ​
+/*
+ * Copyright (C) 2020-2021 Hedera Hashgraph, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,20 +12,36 @@ package com.hedera.services.bdd.suites.utils.sysfiles.serdes;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
+package com.hedera.services.bdd.suites.utils.sysfiles.serdes;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class StandardSerdes {
-	public static final Map<Long, SysFileSerde<String>> SYS_FILE_SERDES = new HashMap<>() {{
-		put(101L, new AddrBkJsonToGrpcBytes());
-		put(102L, new NodesJsonToGrpcBytes());
-		put(111L, new FeesJsonToGrpcBytes());
-		put(112L, new XRatesJsonToGrpcBytes());
-		put(121L, new JutilPropsToSvcCfgBytes("application.properties"));
-		put(122L, new JutilPropsToSvcCfgBytes("api-permission.properties"));
-		put(123L, new ThrottlesJsonToGrpcBytes());
-	}};
+
+    /**
+     * Private constructor to prevent instantiation.
+     *
+     * @throws UnsupportedOperationException if this class is instantiated via reflection.
+     */
+    private StandardSerdes() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static final Map<Long, SysFileSerde<String>> SYS_FILE_SERDES =
+            Map.of(
+                    101L,
+                    new AddrBkJsonToGrpcBytes(),
+                    102L,
+                    new NodesJsonToGrpcBytes(),
+                    111L,
+                    new FeesJsonToGrpcBytes(),
+                    112L,
+                    new XRatesJsonToGrpcBytes(),
+                    121L,
+                    new JutilPropsToSvcCfgBytes("application.properties"),
+                    122L,
+                    new JutilPropsToSvcCfgBytes("api-permission.properties"),
+                    123L,
+                    new ThrottlesJsonToGrpcBytes());
 }
