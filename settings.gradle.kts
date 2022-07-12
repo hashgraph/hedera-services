cbe83dcaac9d3427c2d5870069971f905f567e29
tinker-michaelj <michael.tinker@hedera.com>
2022-07-12 14:26:11 -0500

    Bump SDK version
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -54,7 +54,7 @@ dependencyResolutionManagement {
             version("log4j-version", "2.17.2")
             version("netty-version", "4.1.66.Final")
             version("protobuf-java-version", "3.19.4")
-            version("swirlds-version", "0.28.0-alpha.2")
+            version("swirlds-version", "0.28.0-alpha.3")
             version("tuweni-version", "2.2.0")
 
             // List of bundles provided for us. When applicable, favor using these over individual libraries.
