e18e0fcb9d64c0742a4455ea36ddfa1ef8646e48
Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>
2022-07-14 08:42:22 +0300

    feat: apply spotless checkstyle
    Signed-off-by: Tanyu Dermendzhiev <tanyu.dermendzhiev@limechain.tech>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/WipeTokenAccountPrecompileSuite.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/WipeTokenAccountPrecompileSuite.java
@@ -1,10 +1,5 @@
-package com.hedera.services.bdd.suites.contract.precompile;
-
-/*-
- * ‌
- * Hedera Services Node
- *
- * Copyright (C) 2018 - 2022 Hedera Hashgraph, LLC
+/*
+ * Copyright (C) 2021-2022 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,8 +12,8 @@ package com.hedera.services.bdd.suites.contract.precompile;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
+package com.hedera.services.bdd.suites.contract.precompile;
 
 import com.hedera.services.bdd.spec.HapiApiSpec;
 import com.hedera.services.bdd.suites.HapiApiSuite;
