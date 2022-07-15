629a200b368117ba3add65c4669675702d4f9b6c
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2022-07-15 17:09:40 +0300

    Remove EETs from sequential tests if they're present in parallel tests
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- test-clients/src/itest/java/SequentialIntegrationTests.java
+++ test-clients/src/itest/java/SequentialIntegrationTests.java
@@ -15,14 +15,10 @@
  */
 import com.hedera.services.bdd.suites.autorenew.GracePeriodRestrictionsSuite;
 import com.hedera.services.bdd.suites.consensus.TopicGetInfoSuite;
-import com.hedera.services.bdd.suites.contract.hapi.ContractCallLocalSuite;
 import com.hedera.services.bdd.suites.contract.hapi.ContractCallSuite;
 import com.hedera.services.bdd.suites.contract.hapi.ContractCreateSuite;
-import com.hedera.services.bdd.suites.contract.hapi.ContractDeleteSuite;
-import com.hedera.services.bdd.suites.contract.hapi.ContractGetBytecodeSuite;
 import com.hedera.services.bdd.suites.contract.hapi.ContractGetInfoSuite;
 import com.hedera.services.bdd.suites.contract.hapi.ContractMusicalChairsSuite;
-import com.hedera.services.bdd.suites.contract.hapi.ContractUpdateSuite;
 import com.hedera.services.bdd.suites.contract.opcodes.BalanceOperationSuite;
 import com.hedera.services.bdd.suites.contract.opcodes.CallCodeOperationSuite;
 import com.hedera.services.bdd.suites.contract.opcodes.CallOperationSuite;
@@ -40,16 +36,9 @@ import com.hedera.services.bdd.suites.contract.openzeppelin.ERC1155ContractInter
 import com.hedera.services.bdd.suites.contract.openzeppelin.ERC20ContractInteractions;
 import com.hedera.services.bdd.suites.contract.openzeppelin.ERC721ContractInteractions;
 import com.hedera.services.bdd.suites.contract.precompile.AssociatePrecompileSuite;
-import com.hedera.services.bdd.suites.contract.precompile.ContractBurnHTSSuite;
-import com.hedera.services.bdd.suites.contract.precompile.ContractHTSSuite;
-import com.hedera.services.bdd.suites.contract.precompile.ContractKeysHTSSuite;
-import com.hedera.services.bdd.suites.contract.precompile.ContractMintHTSSuite;
 import com.hedera.services.bdd.suites.contract.precompile.CreatePrecompileSuite;
-import com.hedera.services.bdd.suites.contract.precompile.CryptoTransferHTSSuite;
 import com.hedera.services.bdd.suites.contract.precompile.DelegatePrecompileSuite;
-import com.hedera.services.bdd.suites.contract.precompile.DissociatePrecompileSuite;
 import com.hedera.services.bdd.suites.contract.precompile.ERCPrecompileSuite;
-import com.hedera.services.bdd.suites.contract.precompile.MixedHTSPrecompileTestsSuite;
 import com.hedera.services.bdd.suites.contract.records.LogsSuite;
 import com.hedera.services.bdd.suites.contract.records.RecordsSuite;
 import com.hedera.services.bdd.suites.contract.traceability.ContractTraceabilitySuite;
@@ -118,14 +107,10 @@ public class SequentialIntegrationTests extends IntegrationTestBase {
                 extractSpecsFromSuite(ScheduleCreateSpecs::new),
                 extractSpecsFromSuite(ScheduleSignSpecs::new),
                 // contract.hapi
-                extractSpecsFromSuite(ContractCallLocalSuite::new),
                 extractSpecsFromSuite(ContractCallSuite::new),
                 extractSpecsFromSuite(ContractCreateSuite::new),
-                extractSpecsFromSuite(ContractDeleteSuite::new),
-                extractSpecsFromSuite(ContractGetBytecodeSuite::new),
                 extractSpecsFromSuite(ContractGetInfoSuite::new),
                 extractSpecsFromSuite(ContractMusicalChairsSuite::new),
-                extractSpecsFromSuite(ContractUpdateSuite::new),
                 // contract.opcode
                 extractSpecsFromSuite(BalanceOperationSuite::new),
                 extractSpecsFromSuite(CallCodeOperationSuite::new),
@@ -146,16 +131,9 @@ public class SequentialIntegrationTests extends IntegrationTestBase {
                 extractSpecsFromSuite(ERC1155ContractInteractions::new),
                 // contract.precompile
                 extractSpecsFromSuite(AssociatePrecompileSuite::new),
-                extractSpecsFromSuite(ContractBurnHTSSuite::new),
-                extractSpecsFromSuite(ContractHTSSuite::new),
-                extractSpecsFromSuite(ContractKeysHTSSuite::new),
-                extractSpecsFromSuite(ContractMintHTSSuite::new),
                 extractSpecsFromSuite(CreatePrecompileSuite::new),
-                extractSpecsFromSuite(CryptoTransferHTSSuite::new),
                 extractSpecsFromSuite(DelegatePrecompileSuite::new),
-                extractSpecsFromSuite(DissociatePrecompileSuite::new),
                 extractSpecsFromSuite(ERCPrecompileSuite::new),
-                extractSpecsFromSuite(MixedHTSPrecompileTestsSuite::new),
                 // contract.records
                 extractSpecsFromSuite(LogsSuite::new),
                 extractSpecsFromSuite(RecordsSuite::new),
