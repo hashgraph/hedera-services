b7766e0b88ce30d465ffc752feb91eba0ea3f155
Ivan Kavaldzhiev <ivankavaldzhiev@Ivans-MacBook-Pro-4.local>
2022-07-18 10:37:19 +0300

    nit: fix PR comment
    Signed-off-by: Ivan Kavaldzhiev <ivankavaldzhiev@Ivans-MacBook-Pro-4.local>

--- hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/ParsingConstants.java
+++ hedera-node/src/main/java/com/hedera/services/store/contracts/precompile/ParsingConstants.java
@@ -28,22 +28,24 @@ public final class ParsingConstants {
         throw new UnsupportedOperationException("Utility class");
     }
 
-    public static final String ARRAY_BRACKETS = "[]";
-    public static final String INT = "(int)";
-    public static final String BOOL = "(bool)";
-    public static final String STRING = "(string)";
-    public static final String BYTES32 = "(bytes32)";
+    // data types
     public static final String ADDRESS = "(address)";
+    public static final String ARRAY_BRACKETS = "[]";
+    public static final String BOOL = "(bool)";
+    public static final String BYTES32 = "(bytes32)";
     public static final String BYTES32_PAIR_RAW_TYPE = "(bytes32,bytes32)";
+    public static final String INT = "(int)";
+    public static final String STRING = "(string)";
     public static final String UINT8 = "(uint8)";
     public static final String UINT256 = "(uint256)";
 
+    // struct types
     public static final String EXPIRY = "(uint32,address,uint32)";
-    public static final String KEY_VALUE = "(bool,address,bytes,bytes,address)";
-    public static final String TOKEN_KEY = "(uint256," + KEY_VALUE + ")";
     public static final String FIXED_FEE = "(uint32,address,bool,bool,address)";
     public static final String FRACTIONAL_FEE = "(uint32,uint32,uint32,uint32,bool,address)";
+    public static final String KEY_VALUE = "(bool,address,bytes,bytes,address)";
     public static final String ROYALTY_FEE = "(uint32,uint32,uint32,address,bool,address)";
+    public static final String TOKEN_KEY = "(uint256," + KEY_VALUE + ")";
 
     @VisibleForTesting
     public static void tryToInitialize() {
