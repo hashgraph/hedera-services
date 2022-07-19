c30598cbf4710b56998d5cbb5e0f10d73f3d091b
Danno Ferrin <danno.ferrin@hedera.com>
2022-07-18 18:43:39 -0600

    even moar sonar
    Signed-off-by: Danno Ferrin <danno.ferrin@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/store/contracts/precompile/codec/DecodingFacadeTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/contracts/precompile/codec/DecodingFacadeTest.java
@@ -370,9 +370,10 @@ class DecodingFacadeTest {
     @Test
     void decodeApproveForNFTHAPI() {
         given(ledgers.typeOf(any())).willReturn(TokenType.NON_FUNGIBLE_UNIQUE);
+        UnaryOperator<byte[]> identity = identity();
         final var decodedInput =
                 subject.decodeTokenApprove(
-                        APPROVE_NFT_INPUT_HAPI, null, false, identity(), ledgers);
+                        APPROVE_NFT_INPUT_HAPI, null, false, identity, ledgers);
 
         assertEquals(ACCOUNT_NUM_SPENDER_NFT, decodedInput.spender().getAccountNum());
         assertEquals(TOKEN_NUM_HAPI_TOKEN, decodedInput.tokenId().getTokenNum());
@@ -382,16 +383,17 @@ class DecodingFacadeTest {
                 IllegalArgumentException.class,
                 () ->
                         subject.decodeTokenApprove(
-                                APPROVE_NFT_INPUT_HAPI, null, true, identity(), ledgers));
+                                APPROVE_NFT_INPUT_HAPI, null, true, identity, ledgers));
     }
 
     @Test
     void decodeApproveForTokenAHPI() {
         given(ledgers.typeOf(any())).willReturn(TokenType.FUNGIBLE_COMMON);
 
+        UnaryOperator<byte[]> identity = identity();
         final var decodedInput =
                 subject.decodeTokenApprove(
-                        APPROVE_TOKEN_INPUT_HAPI, null, true, identity(), ledgers);
+                        APPROVE_TOKEN_INPUT_HAPI, null, true, identity, ledgers);
 
         assertEquals(ACCOUNT_NUM_SPENDER, decodedInput.spender().getAccountNum());
         assertEquals(TOKEN_NUM_HAPI_TOKEN, decodedInput.tokenId().getTokenNum());
@@ -401,7 +403,7 @@ class DecodingFacadeTest {
                 IllegalArgumentException.class,
                 () ->
                         subject.decodeTokenApprove(
-                                APPROVE_TOKEN_INPUT_HAPI, null, false, identity(), ledgers));
+                                APPROVE_TOKEN_INPUT_HAPI, null, false, identity, ledgers));
     }
 
     @Test
