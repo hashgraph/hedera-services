95e3c78c06009b6be49a5cf9454aacdd5443c1cc
tinker-michaelj <michael.tinker@hedera.com>
2022-07-25 22:08:26 -0500

    sA
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- test-clients/src/main/java/com/hedera/services/yahcli/commands/keys/ExtractDetailsCommand.java
+++ test-clients/src/main/java/com/hedera/services/yahcli/commands/keys/ExtractDetailsCommand.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.yahcli.commands.keys;
 
 import static com.hedera.services.yahcli.config.ConfigUtils.setLogLevels;
@@ -54,8 +69,7 @@ public class ExtractDetailsCommand implements Callable<Integer> {
         final var hexedPrivKey = CommonUtils.hex(privateKey.getSeed());
         COMMON_MESSAGES.info("The public key @ " + loc + " is : " + hexedPubKey);
         COMMON_MESSAGES.info("The private key @ " + loc + " is: " + hexedPrivKey);
-        COMMON_MESSAGES.info(
-                "  -> With DER prefix; " + DER_EDDSA_PREFIX + hexedPrivKey);
+        COMMON_MESSAGES.info("  -> With DER prefix; " + DER_EDDSA_PREFIX + hexedPrivKey);
 
         return 0;
     }
--- test-clients/src/main/java/com/hedera/services/yahcli/commands/keys/KeysCommand.java
+++ test-clients/src/main/java/com/hedera/services/yahcli/commands/keys/KeysCommand.java
@@ -1,11 +1,6 @@
-package com.hedera.services.yahcli.commands.keys;
-
-/*-
- * ‌
- * Hedera Services Test Clients
- * ​
- * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
- * ​
+/*
+ * Copyright (C) 2021-2022 Hedera Hashgraph, LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,37 +12,34 @@ package com.hedera.services.yahcli.commands.keys;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ‍
  */
+package com.hedera.services.yahcli.commands.keys;
 
 import com.hedera.services.yahcli.Yahcli;
+import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.ParentCommand;
 
-import java.util.concurrent.Callable;
-
 @Command(
-		name = "keys",
-		subcommands = {
-				picocli.CommandLine.HelpCommand.class,
-				NewPemCommand.class,
-				ExtractPublicCommand.class,
-				ExtractDetailsCommand.class
-		},
-		description = "Generates and inspects keys of various kinds")
+        name = "keys",
+        subcommands = {
+            picocli.CommandLine.HelpCommand.class,
+            NewPemCommand.class,
+            ExtractPublicCommand.class,
+            ExtractDetailsCommand.class
+        },
+        description = "Generates and inspects keys of various kinds")
 public class KeysCommand implements Callable<Integer> {
 
-	@ParentCommand
-	Yahcli yahcli;
+    @ParentCommand Yahcli yahcli;
 
-	@Override
-	public Integer call() throws Exception {
-		throw new picocli.CommandLine.ParameterException(
-				yahcli.getSpec().commandLine(),
-				"Please specify an keys subcommand!");
-	}
+    @Override
+    public Integer call() throws Exception {
+        throw new picocli.CommandLine.ParameterException(
+                yahcli.getSpec().commandLine(), "Please specify an keys subcommand!");
+    }
 
-	public Yahcli getYahcli() {
-		return yahcli;
-	}
+    public Yahcli getYahcli() {
+        return yahcli;
+    }
 }
