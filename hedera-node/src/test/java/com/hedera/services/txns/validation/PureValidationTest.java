6e2e7f48802bd01f0cd6f2583e8739a444feb03a
tinker-michaelj <michael.tinker@hedera.com>
2022-07-27 23:03:24 -0500

    Some spotless
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/test/java/com/hedera/services/txns/validation/PureValidationTest.java
+++ hedera-node/src/test/java/com/hedera/services/txns/validation/PureValidationTest.java
@@ -20,16 +20,6 @@ package com.hedera.services.txns.validation;
  * ‍
  */
 
-import com.hedera.services.context.NodeInfo;
-import com.hedera.services.state.merkle.MerkleAccount;
-import com.hedera.services.utils.EntityNum;
-import com.hedera.test.utils.TxnUtils;
-import com.hederahashgraph.api.proto.java.AccountID;
-import com.swirlds.merkle.map.MerkleMap;
-import org.junit.jupiter.api.Test;
-
-import java.time.Instant;
-
 import static com.hedera.services.ledger.accounts.HederaAccountCustomizer.STAKED_ACCOUNT_ID_CASE;
 import static com.hedera.services.ledger.accounts.HederaAccountCustomizer.STAKED_NODE_ID_CASE;
 import static com.hedera.test.utils.IdUtils.asAccount;
@@ -39,67 +29,87 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
+import com.hedera.services.context.NodeInfo;
+import com.hedera.services.state.merkle.MerkleAccount;
+import com.hedera.services.utils.EntityNum;
+import com.hedera.test.utils.TxnUtils;
+import com.hederahashgraph.api.proto.java.AccountID;
+import com.swirlds.merkle.map.MerkleMap;
+import java.time.Instant;
+import org.junit.jupiter.api.Test;
+
 class PureValidationTest {
-	private static final Instant now = Instant.now();
-	private static final long impossiblySmallSecs = Instant.MIN.getEpochSecond() - 1;
-	private static final int impossiblySmallNanos = -1;
-	private static final long impossiblyBigSecs = Instant.MAX.getEpochSecond() + 1;
-	private static final int impossiblyBigNanos = 1_000_000_000;
-	private static final NodeInfo nodeInfo = mock(NodeInfo.class);
+    private static final Instant now = Instant.now();
+    private static final long impossiblySmallSecs = Instant.MIN.getEpochSecond() - 1;
+    private static final int impossiblySmallNanos = -1;
+    private static final long impossiblyBigSecs = Instant.MAX.getEpochSecond() + 1;
+    private static final int impossiblyBigNanos = 1_000_000_000;
+    private static final NodeInfo nodeInfo = mock(NodeInfo.class);
 
-	@Test
-	void mapsSensibleTimestamp() {
-		final var proto = TxnUtils.timestampFrom(now.getEpochSecond(), now.getNano());
+    @Test
+    void mapsSensibleTimestamp() {
+        final var proto = TxnUtils.timestampFrom(now.getEpochSecond(), now.getNano());
 
-		assertEquals(now, PureValidation.asCoercedInstant(proto));
-	}
+        assertEquals(now, PureValidation.asCoercedInstant(proto));
+    }
 
-	@Test
-	void coercesTooSmallTimestamp() {
-		final var proto = TxnUtils.timestampFrom(impossiblySmallSecs, impossiblySmallNanos);
+    @Test
+    void coercesTooSmallTimestamp() {
+        final var proto = TxnUtils.timestampFrom(impossiblySmallSecs, impossiblySmallNanos);
 
-		assertEquals(Instant.MIN, PureValidation.asCoercedInstant(proto));
-	}
+        assertEquals(Instant.MIN, PureValidation.asCoercedInstant(proto));
+    }
 
-	@Test
-	void coercesTooBigTimestamp() {
-		final var proto = TxnUtils.timestampFrom(impossiblyBigSecs, impossiblyBigNanos);
+    @Test
+    void coercesTooBigTimestamp() {
+        final var proto = TxnUtils.timestampFrom(impossiblyBigSecs, impossiblyBigNanos);
 
-		assertEquals(Instant.MAX, PureValidation.asCoercedInstant(proto));
-	}
+        assertEquals(Instant.MAX, PureValidation.asCoercedInstant(proto));
+    }
 
-	@Test
-	void validatesStakedId() {
-		final var stakedAccountID = asAccount("0.0.2");
-		final var stakedNodeId = 0;
-		final MerkleMap<EntityNum, MerkleAccount> accounts = mock(MerkleMap.class);
-		given(accounts.get(EntityNum.fromAccountId(stakedAccountID))).willReturn(new MerkleAccount());
+    @Test
+    void validatesStakedId() {
+        final var stakedAccountID = asAccount("0.0.2");
+        final var stakedNodeId = 0;
+        final MerkleMap<EntityNum, MerkleAccount> accounts = mock(MerkleMap.class);
+        given(accounts.get(EntityNum.fromAccountId(stakedAccountID)))
+                .willReturn(new MerkleAccount());
 
-		assertTrue(PureValidation.isValidStakedId(STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
+        assertTrue(
+                PureValidation.isValidStakedId(
+                        STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
 
-		final var deletedAccount = new MerkleAccount();
-		deletedAccount.setDeleted(true);
-		given(accounts.get(EntityNum.fromAccountId(stakedAccountID))).willReturn(deletedAccount);
-		assertFalse(PureValidation.isValidStakedId(STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
+        final var deletedAccount = new MerkleAccount();
+        deletedAccount.setDeleted(true);
+        given(accounts.get(EntityNum.fromAccountId(stakedAccountID))).willReturn(deletedAccount);
+        assertFalse(
+                PureValidation.isValidStakedId(
+                        STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
 
-		final var smartContract = new MerkleAccount();
-		smartContract.setSmartContract(true);
-		given(accounts.get(EntityNum.fromAccountId(stakedAccountID))).willReturn(smartContract);
-		assertFalse(PureValidation.isValidStakedId(STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
-	}
+        final var smartContract = new MerkleAccount();
+        smartContract.setSmartContract(true);
+        given(accounts.get(EntityNum.fromAccountId(stakedAccountID))).willReturn(smartContract);
+        assertFalse(
+                PureValidation.isValidStakedId(
+                        STAKED_ACCOUNT_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
+    }
 
-	@Test
-	void validatesStakedNodeId() {
-		final var stakedAccountID = AccountID.getDefaultInstance();
-		final var stakedNodeId = 2;
+    @Test
+    void validatesStakedNodeId() {
+        final var stakedAccountID = AccountID.getDefaultInstance();
+        final var stakedNodeId = 2;
 
-		final MerkleMap<EntityNum, MerkleAccount> accounts = mock(MerkleMap.class);
-		final NodeInfo nodeInfo = mock(NodeInfo.class);
+        final MerkleMap<EntityNum, MerkleAccount> accounts = mock(MerkleMap.class);
+        final NodeInfo nodeInfo = mock(NodeInfo.class);
 
-		given(nodeInfo.isValidId(stakedNodeId)).willReturn(true);
-		assertTrue(PureValidation.isValidStakedId(STAKED_NODE_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
+        given(nodeInfo.isValidId(stakedNodeId)).willReturn(true);
+        assertTrue(
+                PureValidation.isValidStakedId(
+                        STAKED_NODE_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
 
-		given(nodeInfo.isValidId(stakedNodeId)).willReturn(false);
-		assertFalse(PureValidation.isValidStakedId(STAKED_NODE_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
-	}
+        given(nodeInfo.isValidId(stakedNodeId)).willReturn(false);
+        assertFalse(
+                PureValidation.isValidStakedId(
+                        STAKED_NODE_ID_CASE, stakedAccountID, stakedNodeId, accounts, nodeInfo));
+    }
 }
