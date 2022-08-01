5a35f4077f481f51d2479a92fc00daa32a17cd62
Dimitar Dinev <36386303+dimitar-dinev@users.noreply.github.com>
2022-08-01 18:09:52 +0300

    Generate sidecars for failing top-level contract creates (#3724)
    * fix: generate sidecars for failed top-level contract creates
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * test: SidecarUtils coverage
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * test: ContractCreateTransitionLogic coverage
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * test: TransactionRecordService coverage
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    * style: apply spotless
    
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>
    
    Co-authored-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/src/main/java/com/hedera/services/utils/SidecarUtils.java
+++ hedera-node/src/main/java/com/hedera/services/utils/SidecarUtils.java
@@ -88,4 +88,13 @@ public class SidecarUtils {
         }
         return grpc;
     }
+
+    public static TransactionSidecarRecord.Builder createContractBytecodeSidecarForFailedCreate(
+            final byte[] initCode) {
+        return TransactionSidecarRecord.newBuilder()
+                .setBytecode(
+                        ContractBytecode.newBuilder()
+                                .setInitcode(ByteStringUtils.wrapUnsafely(initCode))
+                                .build());
+    }
 }
