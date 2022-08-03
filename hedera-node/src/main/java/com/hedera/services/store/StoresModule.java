15374c15f01522ad8bc34ff77fd47418c61e3828
tinker-michaelj <michael.tinker@hedera.com>
2022-08-03 12:02:05 -0500

    Check no interceptor
    Signed-off-by: tinker-michaelj <michael.tinker@hedera.com>

--- hedera-node/src/main/java/com/hedera/services/store/StoresModule.java
+++ hedera-node/src/main/java/com/hedera/services/store/StoresModule.java
@@ -31,7 +31,6 @@ import com.hedera.services.ledger.backing.BackingStore;
 import com.hedera.services.ledger.backing.BackingTokenRels;
 import com.hedera.services.ledger.backing.BackingTokens;
 import com.hedera.services.ledger.interceptors.LinkAwareTokenRelsCommitInterceptor;
-import com.hedera.services.ledger.interceptors.LinkAwareUniqueTokensCommitInterceptor;
 import com.hedera.services.ledger.interceptors.StakingAccountsCommitInterceptor;
 import com.hedera.services.ledger.interceptors.TokenRelsLinkManager;
 import com.hedera.services.ledger.interceptors.TokensCommitInterceptor;
@@ -92,9 +91,6 @@ public interface StoresModule {
                         MerkleUniqueToken::new,
                         new BackingNfts(uniqueTokens),
                         new ChangeSummaryManager<>());
-        final var interceptor =
-                new LinkAwareUniqueTokensCommitInterceptor(usageLimits, uniqueTokensLinkManager);
-        uniqueTokensLedger.setCommitInterceptor(interceptor);
         return uniqueTokensLedger;
     }
 
