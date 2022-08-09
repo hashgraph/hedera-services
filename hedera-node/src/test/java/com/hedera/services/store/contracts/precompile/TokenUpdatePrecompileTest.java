ab5fc1797d1d013db60427ac95a58f5ee51c0f4c
Yanko Nikolov <nikolovqnko@gmail.com>
2022-08-09 15:13:27 +0300

    feat: adding unit tests for TokenUpdateLogic
    Signed-off-by: Yanko Nikolov <nikolovqnko@gmail.com>

--- hedera-node/src/test/java/com/hedera/services/store/contracts/precompile/TokenUpdatePrecompileTest.java
+++ hedera-node/src/test/java/com/hedera/services/store/contracts/precompile/TokenUpdatePrecompileTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.store.contracts.precompile;
 
 import com.esaulpaugh.headlong.util.Integers;
@@ -9,9 +24,7 @@ import com.hedera.services.contracts.sources.TxnAwareEvmSigsVerifier;
 import com.hedera.services.fees.FeeCalculator;
 import com.hedera.services.fees.HbarCentExchange;
 import com.hedera.services.fees.calculation.UsagePricesProvider;
-import com.hedera.services.grpc.marshalling.ImpliedTransfers;
 import com.hedera.services.grpc.marshalling.ImpliedTransfersMarshal;
-import com.hedera.services.grpc.marshalling.ImpliedTransfersMeta;
 import com.hedera.services.ledger.TransactionalLedger;
 import com.hedera.services.ledger.accounts.ContractAliases;
 import com.hedera.services.ledger.properties.AccountProperty;
@@ -26,7 +39,6 @@ import com.hedera.services.state.merkle.MerkleToken;
 import com.hedera.services.state.merkle.MerkleTokenRelStatus;
 import com.hedera.services.state.merkle.MerkleUniqueToken;
 import com.hedera.services.state.submerkle.ExpirableTxnRecord;
-import com.hedera.services.store.AccountStore;
 import com.hedera.services.store.contracts.HederaStackedWorldStateUpdater;
 import com.hedera.services.store.contracts.WorldLedgers;
 import com.hedera.services.store.contracts.precompile.codec.DecodingFacade;
@@ -36,18 +48,14 @@ import com.hedera.services.store.contracts.precompile.utils.PrecompilePricingUti
 import com.hedera.services.store.models.NftId;
 import com.hedera.services.store.tokens.HederaTokenStore;
 import com.hederahashgraph.api.proto.java.AccountID;
-import com.hederahashgraph.api.proto.java.CryptoTransferTransactionBody;
 import com.hederahashgraph.api.proto.java.ExchangeRate;
 import com.hederahashgraph.api.proto.java.Timestamp;
 import com.hederahashgraph.api.proto.java.TokenID;
-import com.hederahashgraph.api.proto.java.TokenUnfreezeAccountTransactionBody;
 import com.hederahashgraph.api.proto.java.TokenUpdateTransactionBody;
 import com.hederahashgraph.api.proto.java.TransactionBody;
-import com.hederahashgraph.fee.FeeObject;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.datatypes.Wei;
-import org.hyperledger.besu.evm.frame.BlockValues;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 import org.hyperledger.besu.evm.worldstate.WorldUpdater;
@@ -59,189 +67,161 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.Instant;
 import java.util.Collections;
-import java.util.Deque;
-import java.util.Iterator;
 import java.util.Optional;
 
 import static com.hedera.services.state.EntityCreator.EMPTY_MEMO;
-import static com.hedera.services.store.contracts.precompile.AbiConstants.ABI_ID_FREEZE;
 import static com.hedera.services.store.contracts.precompile.AbiConstants.ABI_ID_UPDATE_TOKEN_INFO;
-import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.DEFAULT_GAS_PRICE;
 import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.contractAddr;
 import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.contractAddress;
 import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.createFungibleTokenUpdateWrapperWithKeys;
 import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.fungibleTokenAddr;
 import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.successResult;
-import static com.hedera.services.store.contracts.precompile.HTSTestsUtil.tokenFreezeUnFreezeWrapper;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.OK;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class TokenUpdatePrecompileTest {
-	@Mock
-	private HederaTokenStore hederaTokenStore;
-	@Mock private GlobalDynamicProperties dynamicProperties;
-	@Mock private GasCalculator gasCalculator;
-	@Mock private MessageFrame frame;
-	@Mock private MessageFrame parentFrame;
-	@Mock private Deque<MessageFrame> frameDeque;
-	@Mock private Iterator<MessageFrame> dequeIterator;
-	@Mock private TxnAwareEvmSigsVerifier sigsVerifier;
-	@Mock private RecordsHistorian recordsHistorian;
-	@Mock private DecodingFacade decoder;
-	@Mock private EncodingFacade encoder;
-	@Mock private AccountStore accountStore;
-	@Mock private TokenUpdateLogic updateLogic;
-	@Mock private SideEffectsTracker sideEffects;
-	@Mock private TransactionBody.Builder mockSynthBodyBuilder;
-	@Mock private CryptoTransferTransactionBody cryptoTransferTransactionBody;
-	@Mock private ExpirableTxnRecord.Builder mockRecordBuilder;
-	@Mock private SyntheticTxnFactory syntheticTxnFactory;
-	@Mock private HederaStackedWorldStateUpdater worldUpdater;
-	@Mock private WorldLedgers wrappedLedgers;
-	@Mock private TransactionalLedger<NftId, NftProperty, MerkleUniqueToken> nfts;
+    @Mock private HederaTokenStore hederaTokenStore;
+    @Mock private GlobalDynamicProperties dynamicProperties;
+    @Mock private GasCalculator gasCalculator;
+    @Mock private MessageFrame frame;
+    @Mock private TxnAwareEvmSigsVerifier sigsVerifier;
+    @Mock private RecordsHistorian recordsHistorian;
+    @Mock private DecodingFacade decoder;
+    @Mock private EncodingFacade encoder;
+    @Mock private TokenUpdateLogic updateLogic;
+    @Mock private SideEffectsTracker sideEffects;
+    @Mock private ExpirableTxnRecord.Builder mockRecordBuilder;
+    @Mock private SyntheticTxnFactory syntheticTxnFactory;
+    @Mock private HederaStackedWorldStateUpdater worldUpdater;
+    @Mock private WorldLedgers wrappedLedgers;
+    @Mock private TransactionalLedger<NftId, NftProperty, MerkleUniqueToken> nfts;
+    @Mock
+    private TransactionalLedger<Pair<AccountID, TokenID>, TokenRelProperty, MerkleTokenRelStatus>
+            tokenRels;
+    @Mock private TransactionalLedger<AccountID, AccountProperty, MerkleAccount> accounts;
+    @Mock private TransactionalLedger<TokenID, TokenProperty, MerkleToken> tokens;
+    @Mock private ExpiringCreations creator;
+    @Mock private ImpliedTransfersMarshal impliedTransfersMarshal;
+    @Mock private FeeCalculator feeCalculator;
+    @Mock private StateView stateView;
+    @Mock private ContractAliases aliases;
+    @Mock private UsagePricesProvider resourceCosts;
+    @Mock private InfrastructureFactory infrastructureFactory;
+    @Mock private AssetsLoader assetLoader;
+    @Mock private HbarCentExchange exchange;
+    @Mock private ExchangeRate exchangeRate;
+    private final TokenUpdateWrapper updateWrapper = createFungibleTokenUpdateWrapperWithKeys(null);
 
-	@Mock
-	private TransactionalLedger<Pair<AccountID, TokenID>, TokenRelProperty, MerkleTokenRelStatus>
-			tokenRels;
+    private static final int CENTS_RATE = 12;
+    private static final int HBAR_RATE = 1;
 
-	@Mock private TransactionalLedger<AccountID, AccountProperty, MerkleAccount> accounts;
-	@Mock private TransactionalLedger<TokenID, TokenProperty, MerkleToken> tokens;
-	@Mock private ExpiringCreations creator;
-	@Mock private ImpliedTransfersMarshal impliedTransfersMarshal;
-	@Mock private ImpliedTransfers impliedTransfers;
-	@Mock private ImpliedTransfersMeta impliedTransfersMeta;
-	@Mock private FeeCalculator feeCalculator;
-	@Mock private FeeObject mockFeeObject;
-	@Mock private StateView stateView;
-	@Mock private ContractAliases aliases;
-	@Mock private UsagePricesProvider resourceCosts;
-	@Mock private InfrastructureFactory infrastructureFactory;
-	@Mock private AssetsLoader assetLoader;
-	@Mock private HbarCentExchange exchange;
-	@Mock private ExchangeRate exchangeRate;
-	private TokenUpdateWrapper updateWrapper = createFungibleTokenUpdateWrapperWithKeys(null);
+    private HTSPrecompiledContract subject;
 
-	private static final long TEST_SERVICE_FEE = 5_000_000;
-	private static final long TEST_NETWORK_FEE = 400_000;
-	private static final long TEST_NODE_FEE = 300_000;
-	private static final int CENTS_RATE = 12;
-	private static final int HBAR_RATE = 1;
-	private static final long EXPECTED_GAS_PRICE =
-			(TEST_SERVICE_FEE + TEST_NETWORK_FEE + TEST_NODE_FEE) / DEFAULT_GAS_PRICE * 6 / 5;
+    @BeforeEach
+    void setUp() {
+        PrecompilePricingUtils precompilePricingUtils =
+                new PrecompilePricingUtils(
+                        assetLoader, exchange, () -> feeCalculator, resourceCosts, stateView);
+        subject =
+                new HTSPrecompiledContract(
+                        dynamicProperties,
+                        gasCalculator,
+                        recordsHistorian,
+                        sigsVerifier,
+                        decoder,
+                        encoder,
+                        syntheticTxnFactory,
+                        creator,
+                        impliedTransfersMarshal,
+                        () -> feeCalculator,
+                        stateView,
+                        precompilePricingUtils,
+                        infrastructureFactory);
+        given(infrastructureFactory.newSideEffects()).willReturn(sideEffects);
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+    }
 
-	private HTSPrecompiledContract subject;
+    @Test
+    void computeCallsSuccessfullyForUpdateFungibleToken() {
+        // given
+        final var input = Bytes.of(Integers.toBytes(ABI_ID_UPDATE_TOKEN_INFO));
+        givenFrameContext();
+        givenLedgers();
+        givenMinimalContextForSuccessfulCall();
+        givenMinimalRecordStructureForSuccessfulCall();
+        givenUpdateTokenContext();
+        givenPricingUtilsContext();
+        // when
+        subject.prepareFields(frame);
+        subject.prepareComputation(input, a -> a);
+        subject.getPrecompile().getMinimumFeeInTinybars(Timestamp.getDefaultInstance());
+        final var result = subject.computeInternal(frame);
+        // then
+        assertEquals(successResult, result);
+    }
+    private void givenFrameContext() {
+        given(frame.getSenderAddress()).willReturn(contractAddress);
+        given(frame.getWorldUpdater()).willReturn(worldUpdater);
+        given(frame.getBlockValues())
+                .willReturn(new HederaBlockValues(10L, 123L, Instant.ofEpochSecond(123L)));
+        given(frame.getContractAddress()).willReturn(contractAddr);
+        given(frame.getRecipientAddress()).willReturn(fungibleTokenAddr);
+        given(frame.getRemainingGas()).willReturn(300L);
+        given(frame.getValue()).willReturn(Wei.ZERO);
+    }
 
-	@BeforeEach
-	void setUp() {
-		PrecompilePricingUtils precompilePricingUtils =
-				new PrecompilePricingUtils(
-						assetLoader, exchange, () -> feeCalculator, resourceCosts, stateView);
-		subject =
-				new HTSPrecompiledContract(
-						dynamicProperties,
-						gasCalculator,
-						recordsHistorian,
-						sigsVerifier,
-						decoder,
-						encoder,
-						syntheticTxnFactory,
-						creator,
-						impliedTransfersMarshal,
-						() -> feeCalculator,
-						stateView,
-						precompilePricingUtils,
-						infrastructureFactory);
-		given(infrastructureFactory.newSideEffects()).willReturn(sideEffects);
-		given(worldUpdater.permissivelyUnaliased(any()))
-				.willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
-	}
+    private void givenMinimalContextForSuccessfulCall() {
+        Optional<WorldUpdater> parent = Optional.of(worldUpdater);
+        given(worldUpdater.parentUpdater()).willReturn(parent);
+        given(worldUpdater.aliases()).willReturn(aliases);
+        given(aliases.resolveForEvm(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+    }
 
-	@Test
-	void computeCallsSuccessfullyForUpdateFungibleToken() {
-		// given
-		final var input = Bytes.of(Integers.toBytes(ABI_ID_UPDATE_TOKEN_INFO));
-		givenFrameContext();
-		givenLedgers();
-		givenMinimalContextForSuccessfulCall();
-		givenMinimalRecordStructureForSuccessfulCall();
-		givenUpdateTokenContext();
-		givenPricingUtilsContext();
+    private void givenUpdateTokenContext() {
+        given(sigsVerifier.hasActiveAdminKey(
+                                true, fungibleTokenAddr, fungibleTokenAddr, wrappedLedgers))
+                .willReturn(true);
+        given(infrastructureFactory.newHederaTokenStore(sideEffects, tokens, nfts, tokenRels))
+                .willReturn(hederaTokenStore);
+        given(
+                        infrastructureFactory.newTokenUpdateLogic(
+                                hederaTokenStore, wrappedLedgers, sideEffects))
+                .willReturn(updateLogic);
+        given(updateLogic.validate(any())).willReturn(OK);
+        given(decoder.decodeUpdateTokenInfo(any(), any())).willReturn(updateWrapper);
+        given(syntheticTxnFactory.createTokenUpdate(updateWrapper))
+                .willReturn(
+                        TransactionBody.newBuilder()
+                                .setTokenUpdate(TokenUpdateTransactionBody.newBuilder()));
+    }
 
-		// when
-		subject.prepareFields(frame);
-		subject.prepareComputation(input, a -> a);
-		subject.getPrecompile().getMinimumFeeInTinybars(Timestamp.getDefaultInstance());
-		final var result = subject.computeInternal(frame);
+    private void givenMinimalRecordStructureForSuccessfulCall() {
+        given(creator.createSuccessfulSyntheticRecord(
+                                Collections.emptyList(), sideEffects, EMPTY_MEMO))
+                .willReturn(mockRecordBuilder);
+    }
 
-		// then
-		assertEquals(successResult, result);
-	}
+    private void givenLedgers() {
+        given(worldUpdater.wrappedTrackingLedgers(any())).willReturn(wrappedLedgers);
+        given(wrappedLedgers.accounts()).willReturn(accounts);
+        given(wrappedLedgers.tokenRels()).willReturn(tokenRels);
+        given(wrappedLedgers.nfts()).willReturn(nfts);
+        given(wrappedLedgers.tokens()).willReturn(tokens);
+    }
 
-	private void givenMinimalFrameContext() {
-		given(frame.getSenderAddress()).willReturn(contractAddress);
-		given(frame.getWorldUpdater()).willReturn(worldUpdater);
-	}
-
-	private void givenFrameContext() {
-		givenMinimalFrameContext();
-		given(frame.getBlockValues()).willReturn(new HederaBlockValues(10L, 123L, Instant.ofEpochSecond(123L)));
-		given(frame.getContractAddress()).willReturn(contractAddr);
-		given(frame.getRecipientAddress()).willReturn(fungibleTokenAddr);
-		given(frame.getRemainingGas()).willReturn(300L);
-		given(frame.getValue()).willReturn(Wei.ZERO);
-	}
-
-	private void givenMinimalContextForSuccessfulCall() {
-		Optional<WorldUpdater> parent = Optional.of(worldUpdater);
-		given(worldUpdater.parentUpdater()).willReturn(parent);
-		given(worldUpdater.aliases()).willReturn(aliases);
-		given(aliases.resolveForEvm(any()))
-				.willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
-		given(worldUpdater.permissivelyUnaliased(any()))
-				.willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
-	}
-
-	private void givenUpdateTokenContext() {
-		given(sigsVerifier.hasActiveAdminKey(true,fungibleTokenAddr,fungibleTokenAddr,wrappedLedgers)).willReturn(true);
-		given(infrastructureFactory.newHederaTokenStore(sideEffects, tokens, nfts, tokenRels))
-				.willReturn(hederaTokenStore);
-		given(infrastructureFactory.newTokenUpdateLogic(hederaTokenStore,wrappedLedgers,sideEffects))
-				.willReturn(updateLogic);
-		given(updateLogic.validate(any())).willReturn(OK);
-		given(decoder.decodeUpdateTokenInfo(any(), any())).willReturn(updateWrapper);
-		given(syntheticTxnFactory.createTokenUpdate(updateWrapper))
-				.willReturn(
-						TransactionBody.newBuilder()
-								.setTokenUpdate(
-										TokenUpdateTransactionBody.newBuilder()));
-	}
-
-	private void givenMinimalRecordStructureForSuccessfulCall() {
-		given(creator.createSuccessfulSyntheticRecord(Collections.emptyList(), sideEffects, EMPTY_MEMO))
-				.willReturn(mockRecordBuilder);
-	}
-
-	private void givenLedgers() {
-		given(worldUpdater.wrappedTrackingLedgers(any())).willReturn(wrappedLedgers);
-		given(wrappedLedgers.accounts()).willReturn(accounts);
-		given(wrappedLedgers.tokenRels()).willReturn(tokenRels);
-		given(wrappedLedgers.nfts()).willReturn(nfts);
-		given(wrappedLedgers.tokens()).willReturn(tokens);
-	}
-
-	private void givenPricingUtilsContext() {
-		given(exchange.rate(any())).willReturn(exchangeRate);
-		given(exchangeRate.getCentEquiv()).willReturn(CENTS_RATE);
-		given(exchangeRate.getHbarEquiv()).willReturn(HBAR_RATE);
-		given(worldUpdater.aliases()).willReturn(aliases);
-		given(worldUpdater.permissivelyUnaliased(any()))
-				.willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
-	}
-
+    private void givenPricingUtilsContext() {
+        given(exchange.rate(any())).willReturn(exchangeRate);
+        given(exchangeRate.getCentEquiv()).willReturn(CENTS_RATE);
+        given(exchangeRate.getHbarEquiv()).willReturn(HBAR_RATE);
+        given(worldUpdater.aliases()).willReturn(aliases);
+        given(worldUpdater.permissivelyUnaliased(any()))
+                .willAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+    }
 }
