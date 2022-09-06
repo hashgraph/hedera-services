21b889059805d7c5d8c18f6f61c6b1af19bad661
Michael Tinker <michael.tinker@swirldslabs.com>
2022-09-06 08:25:23 -0500

    intermezzo
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/src/main/java/com/hedera/services/stats/ExpiryStats.java
+++ hedera-node/src/main/java/com/hedera/services/stats/ExpiryStats.java
@@ -3,14 +3,14 @@ package com.hedera.services.stats;
 import com.hedera.services.state.merkle.MerkleNetworkContext;
 import com.swirlds.common.system.Platform;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class ExpiryStats {
-    private final MerkleNetworkContext networkCtx;
 
-    public ExpiryStats(MerkleNetworkContext networkCtx) {
-        this.networkCtx = networkCtx;
+    @Inject
+    public ExpiryStats() {
     }
 
     public void registerWith(final Platform platform) {
