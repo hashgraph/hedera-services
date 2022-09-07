3f927289a1adff3e8ca298d0c29a1db93447b19c
Ray Juang <ray.juang@swirldslabs.com>
2022-09-07 16:19:55 -0700

    Fix code smell from sonar.
    Signed-off-by: Ray Juang <ray.juang@swirldslabs.com>

--- hedera-node/src/main/java/com/hedera/services/state/migration/UniqueTokenAdapter.java
+++ hedera-node/src/main/java/com/hedera/services/state/migration/UniqueTokenAdapter.java
@@ -107,6 +107,7 @@ public class UniqueTokenAdapter implements FastCopyable {
      *
      * @return whether the underlying instance is immutable.
      */
+    @Override
     public boolean isImmutable() {
         return isVirtual ? uniqueTokenValue.isImmutable() : merkleUniqueToken.isImmutable();
     }
