4643c5e25c048408da58b1719912823113f4b039
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-09-19 16:29:49 +0300

    refactor: PR comment
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-evm/src/main/java/com/hedera/evm/model/Id.java
+++ hedera-evm/src/main/java/com/hedera/evm/model/Id.java
@@ -15,7 +15,6 @@
  */
 package com.hedera.evm.model;
 
-import com.hederahashgraph.api.proto.java.AccountID;
 import org.hyperledger.besu.datatypes.Address;
 
 public record Id(long shard, long realm, long num) {
@@ -23,9 +22,5 @@ public record Id(long shard, long realm, long num) {
     // TODO: FIX THIS
     public Address asEvmAddress() {
         return Address.fromHexString("TODO");
-    }
-
-    public static Id fromGrpcAccount(final AccountID id) {
-        return new Id(id.getShardNum(), id.getRealmNum(), id.getAccountNum());
     }
 }
