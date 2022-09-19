aede8eaaa04215b0cf6489fef51ac1a60ffc7715
Matt Hess <matt.hess@swirldslabs.com>
2022-09-19 16:01:49 -0600

    Run spotlessApply on new api module
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-app-api/src/main/java/com/hedera/services/api/package-info.java
+++ hedera-node/hedera-app-api/src/main/java/com/hedera/services/api/package-info.java
@@ -1,9 +1,8 @@
 /**
- * Includes interface-only specifications of the public APIs provided
- * by the services in {@code hedera-node}. Any changes to said public
- * APIs must be reflected and documented in this package <i>before</i>
- * writing their implementations.
+ * Includes interface-only specifications of the public APIs provided by the services in {@code
+ * hedera-node}. Any changes to said public APIs must be reflected and documented in this package
+ * <i>before</i> writing their implementations.
  *
- * <b>There should be no implementation code inside this package.</b>
+ * <p><b>There should be no implementation code inside this package.</b>
  */
 package com.hedera.services.api;
