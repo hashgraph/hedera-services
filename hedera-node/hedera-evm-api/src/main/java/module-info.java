8a1b269293a6b605ff19bab7d485ed5f418662eb
Nathan Klick <nathan@swirldslabs.com>
2022-09-27 12:21:48 -0500

    Apply spotless formatting
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- hedera-node/hedera-evm-api/src/main/java/module-info.java
+++ hedera-node/hedera-evm-api/src/main/java/module-info.java
@@ -1,6 +1,4 @@
-/**
- * Provides the core interfaces for the Hedera EVM implementation.
- */
+/** Provides the core interfaces for the Hedera EVM implementation. */
 module com.hedera.services.evm {
     exports com.hedera.services.evm;
 }
