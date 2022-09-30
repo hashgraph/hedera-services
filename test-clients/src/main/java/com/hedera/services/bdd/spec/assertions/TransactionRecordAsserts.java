f2d48058cdbf2b50c9257b7e90fc684bebf1b816
Michael Tinker <michael.tinker@swirldslabs.com>
2022-09-30 09:24:23 -0500

    spotless
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- test-clients/src/main/java/com/hedera/services/bdd/spec/assertions/TransactionRecordAsserts.java
+++ test-clients/src/main/java/com/hedera/services/bdd/spec/assertions/TransactionRecordAsserts.java
@@ -97,7 +97,6 @@ public class TransactionRecordAsserts extends BaseErroringAssertsProvider<Transa
         return this;
     }
 
-
     @SuppressWarnings("java:S1181")
     public TransactionRecordAsserts assessedCustomFeeCount(final int n) {
         this.<List<AssessedCustomFee>>registerTypedProvider(
