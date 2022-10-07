7bf9f65be91c4bf22031817bf37e8600571feefa
Matt Hess <matt.hess@swirldslabs.com>
2022-10-07 12:57:28 -0600

    03896 Move src directory to new subproject (#4021)
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

---
+++ hedera-node/hedera-mono-service/build.gradle.kts
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2020-2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+plugins {
+    id("com.hedera.hashgraph.conventions")
+    id("com.hedera.hashgraph.benchmark-conventions")
+}
+
+description = "Hedera Application - MONO Service Implementation"
+
+dependencies {
+    annotationProcessor(libs.dagger.compiler)
+
+    implementation(project(":hapi-fees"))
+    implementation(project(":hapi-utils"))
+    implementation(libs.bundles.besu) {
+        exclude(group = "org.hyperledger.besu", module = "secp256r1")
+    }
+    implementation(libs.bundles.di)
+    implementation(libs.bundles.logging)
+    implementation(libs.bundles.swirlds)
+    implementation(libs.caffeine)
+    implementation(libs.hapi)
+    implementation(libs.headlong)
+    implementation(
+        variantOf(libs.netty.transport.native.epoll) {
+            classifier("linux-x86_64")
+        }
+    )
+    implementation(libs.commons.codec)
+    implementation(libs.commons.io)
+    implementation(libs.commons.collections4)
+    implementation(libs.eclipse.collections)
+
+    testImplementation(testLibs.bundles.testing)
+    testImplementation(testLibs.classgraph)
+
+    jmhImplementation(libs.swirlds.common)
+
+    runtimeOnly(libs.bundles.netty)
+}
+
+val apt = configurations.create("apt")
+dependencies {
+    @Suppress("UnstableApiUsage")
+    apt(libs.dagger.compiler)
+}
+
+tasks.withType<JavaCompile> {
+    options.annotationProcessorPath = apt
+}
+
+val jmhDaggerSources = file("build/generated/sources/annotationProcessor/java/jmh")
+java.sourceSets["jmh"].java.srcDir(jmhDaggerSources)
+
+val nodeWorkingDir = "" + rootProject.file("hedera-node")
+// Add all the libs dependencies into the jar manifest!
+tasks.jar {
+    manifest {
+        attributes(
+            "Main-Class" to "com.hedera.services.ServicesMain",
+            "Class-Path" to configurations.getByName("runtimeClasspath")
+                .joinToString(separator = " ") { "$nodeWorkingDir/data/lib/" + it.name }
+
+        )
+    }
+}
+
+// Replace variables in semantic-version.properties with build variables
+tasks.processResources {
+    filesMatching("semantic-version.properties") {
+        filter { line: String ->
+            if (line.contains("hapi-proto.version")) {
+                "hapi.proto.version=" + libs.versions.hapi.version.get()
+            } else if (line.contains("project.version")) {
+                "hedera.services.version=" + project.version
+            } else {
+                line
+            }
+        }
+    }
+}
+
+// Copy dependencies into `data/lib`
+val copyLib = tasks.register<Copy>("copyLib") {
+    from(project.configurations.getByName("runtimeClasspath"))
+    into("$nodeWorkingDir/data/lib")
+}
+
+// Copy built jar into `data/apps` and rename HederaNode.jar
+val copyApp = tasks.register<Copy>("copyApp") {
+    from(tasks.jar)
+    into("$nodeWorkingDir/data/apps")
+    rename { "HederaNode.jar" }
+    shouldRunAfter(tasks.getByName("copyLib"))
+}
+
+tasks.assemble {
+    dependsOn(copyLib)
+    dependsOn(copyApp)
+}
+
+// Create the "run" task for running a Hedera consensus node
+tasks.register<JavaExec>("run") {
+    dependsOn(tasks.assemble)
+    workingDir(nodeWorkingDir)
+    classpath("$nodeWorkingDir/data/apps/HederaNode.jar")
+}
+
+val cleanRun = tasks.register("cleanRun") {
+    project.delete(File(project.projectDir, "$nodeWorkingDir/database"))
+    project.delete(File(project.projectDir, "$nodeWorkingDir/output"))
+    project.delete(File(project.projectDir, "$nodeWorkingDir/settingsUsed.txt"))
+    project.delete(File(project.projectDir, "$nodeWorkingDir/swirlds.jar"))
+    project.projectDir.list { _, fileName -> fileName.startsWith(nodeWorkingDir + "MainNetStats") }
+        ?.forEach { file ->
+            project.delete(file)
+        }
+
+    val dataDir = File(project.projectDir, "$nodeWorkingDir/data")
+    project.delete(File(dataDir, "accountBalances"))
+    project.delete(File(dataDir, "apps"))
+    project.delete(File(dataDir, "lib"))
+    project.delete(File(dataDir, "recordstreams"))
+    project.delete(File(dataDir, "saved"))
+}
+
+tasks.clean {
+    dependsOn(cleanRun)
+}
+
+tasks.register("showHapiVersion") {
+    doLast {
+        println(versionCatalogs.named("libs").findVersion("hapi-version").get().requiredVersion)
+    }
+}
--- hedera-node/hedera-mono-service/src/main/java/com/hedera/services/store/contracts/precompile/codec/TokenUpdateKeysWrapper.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/services/store/contracts/precompile/codec/TokenUpdateKeysWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Hedera Hashgraph, LLC
+ * Copyright (C) 2020-2022 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-plugins {
-    id("com.hedera.hashgraph.conventions")
-    id("com.hedera.hashgraph.benchmark-conventions")
-}
+package com.hedera.services.store.contracts.precompile.codec;
 
-description = "Hedera Services Node"
+import com.hederahashgraph.api.proto.java.TokenID;
+import java.util.List;
+
+public record TokenUpdateKeysWrapper(TokenID tokenID, List<TokenKeyWrapper> tokenKeys) {}
