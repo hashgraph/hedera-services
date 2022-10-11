a9a3ae7e57b2ce1c0a10846dc7b78f17daa873af
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2022-10-11 12:50:53 -0500

    revert not needed changes
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/module-info.java
+++ hedera-node/hedera-app/src/main/java/module-info.java
@@ -1,4 +1,3 @@
 module com.hedera.hashgraph.app {
-    requires java.desktop; // Shouldn't need this, but Platform requires it for now...
-    requires com.hedera.hashgraph.base;
+    requires com.hedera.services.api;
 }
