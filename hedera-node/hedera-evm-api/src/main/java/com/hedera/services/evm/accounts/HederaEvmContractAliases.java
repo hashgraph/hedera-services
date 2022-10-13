6110a2c1ac9863080f346f1783f0a3b49717ac52
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-10-13 16:08:33 +0300

    feat: initial commit
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

---
+++ hedera-node/hedera-evm-api/src/main/java/com/hedera/services/evm/accounts/HederaEvmContractAliases.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2021-2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hedera.services.evm.accounts;
+
+import java.util.Arrays;
+import org.hyperledger.besu.datatypes.Address;
+
+public abstract class HederaEvmContractAliases {
+
+  public static final int EVM_ADDRESS_LEN = 20;
+  private static byte[] mirrorPrefix = null;
+
+  public abstract Address resolveForEvm(Address addressOrAlias);
+
+  public boolean isMirror(final Address address) {
+    return isMirror(address.toArrayUnsafe());
+  }
+
+  public boolean isMirror(final byte[] address) {
+    if (address.length != EVM_ADDRESS_LEN) {
+      return false;
+    }
+    if (mirrorPrefix == null) {
+      mirrorPrefix = new byte[12];
+      System.arraycopy(0L, 4, mirrorPrefix, 0, 4);
+      System.arraycopy(0L, 0, mirrorPrefix, 4, 8);
+    }
+    return Arrays.equals(mirrorPrefix, 0, 12, address, 0, 12);
+  }
+}
