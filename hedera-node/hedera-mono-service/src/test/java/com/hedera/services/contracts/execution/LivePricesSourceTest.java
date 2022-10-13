0a774c763e0ac840971b63d81039c25ce80b80bc
ivanparnarev <ivan.parnarev@limechain.tech>
2022-10-13 15:28:17 +0300

    fix: increate coverage
    Signed-off-by: ivanparnarev <ivan.parnarev@limechain.tech>

--- hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/execution/LivePricesSourceTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/execution/LivePricesSourceTest.java
@@ -16,6 +16,8 @@
 package com.hedera.services.contracts.execution;
 
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.ContractCall;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.CryptoCreate;
+import static com.hederahashgraph.fee.FeeBuilder.FEE_DIVISOR_FACTOR;
 import static com.hederahashgraph.fee.FeeBuilder.getTinybarsFromTinyCents;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
@@ -24,12 +26,14 @@ import com.hedera.services.context.TransactionContext;
 import com.hedera.services.fees.FeeMultiplierSource;
 import com.hedera.services.fees.HbarCentExchange;
 import com.hedera.services.fees.LivePricesSource;
+import com.hedera.services.fees.PricesAndFeesImpl;
 import com.hedera.services.fees.calculation.UsagePricesProvider;
 import com.hedera.services.utils.MiscUtils;
 import com.hedera.services.utils.accessors.TxnAccessor;
 import com.hederahashgraph.api.proto.java.ExchangeRate;
 import com.hederahashgraph.api.proto.java.FeeComponents;
 import com.hederahashgraph.api.proto.java.FeeData;
+import com.hederahashgraph.api.proto.java.SubType;
 import com.hederahashgraph.api.proto.java.Timestamp;
 import java.time.Instant;
 import java.util.function.ToLongFunction;
@@ -45,6 +49,8 @@ class LivePricesSourceTest {
     private static final Timestamp timeNow = MiscUtils.asTimestamp(now);
     private static final long gasPriceTinybars = 123;
     private static final long sbhPriceTinybars = 456;
+    private final ExchangeRate currentRate =
+            ExchangeRate.newBuilder().setCentEquiv(22).setHbarEquiv(1).build();
     private static final FeeComponents servicePrices =
             FeeComponents.newBuilder()
                     .setGas(gasPriceTinybars * 1000)
@@ -54,6 +60,23 @@ class LivePricesSourceTest {
             FeeData.newBuilder().setServicedata(servicePrices).build();
     private static final ExchangeRate activeRate =
             ExchangeRate.newBuilder().setHbarEquiv(1).setCentEquiv(12).build();
+    private final FeeComponents mockFees =
+            FeeComponents.newBuilder()
+                    .setMax(1_234_567L)
+                    .setGas(5_000_000L)
+                    .setBpr(1_000_000L)
+                    .setBpt(2_000_000L)
+                    .setRbh(3_000_000L)
+                    .setSbh(4_000_000L)
+                    .build();
+    private final FeeData mockFeeData =
+            FeeData.newBuilder()
+                    .setNetworkdata(mockFees)
+                    .setNodedata(mockFees)
+                    .setServicedata(mockFees)
+                    .setSubType(SubType.DEFAULT)
+                    .build();
+    private final FeeData defaultCurrentPrices = mockFeeData;
     private static final long reasonableMultiplier = 7;
     private static final long insaneMultiplier = Long.MAX_VALUE / 2;
 
@@ -62,7 +85,6 @@ class LivePricesSourceTest {
     @Mock private FeeMultiplierSource feeMultiplierSource;
     @Mock private TransactionContext txnCtx;
     @Mock private TxnAccessor accessor;
-
     private LivePricesSource subject;
 
     @BeforeEach
@@ -78,15 +100,6 @@ class LivePricesSourceTest {
                 getTinybarsFromTinyCents(activeRate, gasPriceTinybars) * reasonableMultiplier;
 
         assertEquals(expected, subject.currentPrice(timeNow, ContractCall, FeeComponents::getGas));
-    }
-
-    @Test
-    void getsCurrentGasPriceInTinyCents() {
-        given(usagePrices.defaultPricesGiven(ContractCall, timeNow)).willReturn(providerPrices);
-
-        ToLongFunction<FeeComponents> resourcePriceFn = FeeComponents::getGas;
-        final var expected = resourcePriceFn.applyAsLong(providerPrices.getServicedata()) / 1000;
-        assertEquals(expected, subject.currentGasPriceInTinycents(timeNow, ContractCall));
     }
 
     @Test
@@ -113,5 +126,30 @@ class LivePricesSourceTest {
         given(usagePrices.defaultPricesGiven(ContractCall, timeNow)).willReturn(providerPrices);
         given(feeMultiplierSource.currentMultiplier(accessor)).willReturn(multiplier);
         given(txnCtx.accessor()).willReturn(accessor);
+    }
+
+    @Test
+    void getsCurrentGasPriceInTinyCents() {
+        given(usagePrices.defaultPricesGiven(ContractCall, timeNow)).willReturn(providerPrices);
+
+        ToLongFunction<FeeComponents> resourcePriceFn = FeeComponents::getGas;
+        final var expected = resourcePriceFn.applyAsLong(providerPrices.getServicedata()) / 1000;
+        assertEquals(expected, subject.currentGasPriceInTinycents(timeNow, ContractCall));
+    }
+
+    @Test
+    void estimatesFutureGasPriceInTinybars() {
+        given(exchange.rate(timeNow)).willReturn(currentRate);
+        given(usagePrices.defaultPricesGiven(CryptoCreate, timeNow)).willReturn(defaultCurrentPrices);
+
+        // and:
+        long expected =
+                getTinybarsFromTinyCents(currentRate, mockFees.getGas() / FEE_DIVISOR_FACTOR);
+
+        // when:
+        long actual = subject.estimatedGasPrice(CryptoCreate, timeNow);
+
+        // then:
+        assertEquals(expected, actual);
     }
 }
--- hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/execution/PricesAndFeesTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/execution/PricesAndFeesTest.java
@@ -15,6 +15,7 @@
  */
 package com.hedera.services.contracts.execution;
 
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.ContractCall;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.CryptoCreate;
 import static com.hederahashgraph.fee.FeeBuilder.FEE_DIVISOR_FACTOR;
 import static com.hederahashgraph.fee.FeeBuilder.getTinybarsFromTinyCents;
@@ -29,14 +30,13 @@ import com.hedera.services.fees.LivePricesSource;
 import com.hedera.services.fees.PricesAndFeesImpl;
 import com.hedera.services.fees.calculation.UsagePricesProvider;
 import com.hedera.services.utils.MiscUtils;
-import com.hedera.services.utils.accessors.TxnAccessor;
 import com.hederahashgraph.api.proto.java.ExchangeRate;
 import com.hederahashgraph.api.proto.java.FeeComponents;
 import com.hederahashgraph.api.proto.java.FeeData;
-import com.hederahashgraph.api.proto.java.SubType;
 import com.hederahashgraph.api.proto.java.Timestamp;
 import java.time.Instant;
-import java.util.Map;
+import java.util.function.ToLongFunction;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -49,18 +49,13 @@ class PricesAndFeesTest {
     private static final Timestamp timeNow = MiscUtils.asTimestamp(now);
     private static final long gasPriceTinybars = 123;
     private static final long sbhPriceTinybars = 456;
-    private static final long reasonableMultiplier = 7;
-
     private static final FeeComponents servicePrices =
             FeeComponents.newBuilder()
                     .setGas(gasPriceTinybars * 1000)
                     .setSbh(sbhPriceTinybars * 1000)
                     .build();
-
     private static final FeeData providerPrices =
             FeeData.newBuilder().setServicedata(servicePrices).build();
-    private static final ExchangeRate activeRate =
-            ExchangeRate.newBuilder().setHbarEquiv(1).setCentEquiv(12).build();
     private final FeeComponents mockFees =
             FeeComponents.newBuilder()
                     .setMax(1_234_567L)
@@ -70,28 +65,12 @@ class PricesAndFeesTest {
                     .setRbh(3_000_000L)
                     .setSbh(4_000_000L)
                     .build();
-    private final FeeData mockFeeData =
-            FeeData.newBuilder()
-                    .setNetworkdata(mockFees)
-                    .setNodedata(mockFees)
-                    .setServicedata(mockFees)
-                    .setSubType(SubType.DEFAULT)
-                    .build();
-    private final Map<SubType, FeeData> currentPrices =
-            Map.of(
-                    SubType.DEFAULT, mockFeeData,
-                    SubType.TOKEN_FUNGIBLE_COMMON, mockFeeData,
-                    SubType.TOKEN_NON_FUNGIBLE_UNIQUE, mockFeeData);
-    private final FeeData defaultCurrentPrices = mockFeeData;
     private final ExchangeRate currentRate =
             ExchangeRate.newBuilder().setCentEquiv(22).setHbarEquiv(1).build();
 
     @Mock private HbarCentExchange exchange;
     @Mock private UsagePricesProvider usagePrices;
     @Mock private LivePricesSource livePricesSource;
-    @Mock private FeeMultiplierSource feeMultiplierSource;
-    @Mock private TransactionContext txnCtx;
-    @Mock private TxnAccessor accessor;
     private PricesAndFeesImpl subject;
 
     @BeforeEach
@@ -99,6 +78,39 @@ class PricesAndFeesTest {
         usagePrices = mock(UsagePricesProvider.class);
         exchange = mock(HbarCentExchange.class);
         subject = new PricesAndFeesImpl(exchange, usagePrices, livePricesSource);
+    }
+
+    @Test
+    void getsCurrentGasPriceInTinyCents() {
+        given(subject.currentGasPrice(now, CryptoCreate)).willReturn(123L);
+
+        ToLongFunction<FeeComponents> resourcePriceFn = FeeComponents::getGas;
+        final var expected = resourcePriceFn.applyAsLong(providerPrices.getServicedata()) / 1000;
+        assertEquals(expected, subject.currentGasPrice(now, CryptoCreate));
+    }
+
+    @Test
+    void getsCurrentFutureGasPriceInTinyCents() {
+        given(subject.defaultPricesGiven(ContractCall, timeNow)).willReturn(providerPrices);
+
+        ToLongFunction<FeeComponents> resourcePriceFn = FeeComponents::getGas;
+        final var expected = resourcePriceFn.applyAsLong(providerPrices.getServicedata());
+        assertEquals(expected, subject.defaultPricesGiven(ContractCall, timeNow).getServicedata().getGas());
+    }
+
+    @Test
+    void estimatesCentEquiv() {
+        given(subject.rate(timeNow)).willReturn(currentRate);
+
+        // and:
+        long expected =
+                currentRate.getCentEquiv();
+
+        // when:
+        long actual = subject.rate(timeNow).getCentEquiv();
+
+        // then:
+        assertEquals(expected, actual);
     }
 
     @Test
