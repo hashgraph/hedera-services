beeed63bfb20c7a3f840a0df84bd22b23671e82c
Michael Tinker <michael.tinker@swirldslabs.com>
2022-10-24 21:37:33 -0500

    fix unit test
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-mono-service/src/test/java/com/hedera/node/app/service/token/impl/CryptoPreTransactionHandlerNotImplTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/node/app/service/token/impl/CryptoPreTransactionHandlerNotImplTest.java
@@ -17,6 +17,7 @@ package com.hedera.node.app.service.token.impl;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,5 +36,32 @@ class CryptoPreTransactionHandlerNotImplTest {
     }
 
     @Test
-    void name() {}
+    void notImplementedStuffIsntImplemented() {
+        assertThrows(NotImplementedException.class, () -> subject.preHandleUpdateAccount(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleCryptoTransfer(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleCryptoDelete(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleApproveAllowances(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleDeleteAllowances(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleAddLiveHash(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleDeleteLiveHash(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleCreateToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleUpdateToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleMintToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleBurnToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleDeleteToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleWipeTokenAccount(null));
+        assertThrows(
+                NotImplementedException.class, () -> subject.preHandleFreezeTokenAccount(null));
+        assertThrows(
+                NotImplementedException.class, () -> subject.preHandleUnfreezeTokenAccount(null));
+        assertThrows(
+                NotImplementedException.class,
+                () -> subject.preHandleRevokeKycFromTokenAccount(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleAssociateTokens(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleDissociateTokens(null));
+        assertThrows(
+                NotImplementedException.class, () -> subject.preHandleUpdateTokenFeeSchedule(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandlePauseToken(null));
+        assertThrows(NotImplementedException.class, () -> subject.preHandleUnpauseToken(null));
+    }
 }
