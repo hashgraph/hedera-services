ea30b4ffc0dbb89cb9a9b252892fcc77210611ad
Michael Tinker <michael.tinker@swirldslabs.com>
2022-10-24 19:03:04 -0500

    spotless
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/contract/ContractPreTransactionHandler.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/contract/ContractPreTransactionHandler.java
@@ -1,43 +1,43 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.contract;
 
 import com.hedera.node.app.spi.PreTransactionHandler;
 import com.hedera.node.app.spi.meta.TransactionMetadata;
 import com.hederahashgraph.api.proto.java.TransactionBody;
-import com.hederahashgraph.api.proto.java.TransactionResponse;
 
 public interface ContractPreTransactionHandler extends PreTransactionHandler {
-    /**
-     * Creates a contract
-     */
+    /** Creates a contract */
     TransactionMetadata preHandleCreateContract(TransactionBody txn);
 
-    /**
-     * Updates a contract with the content
-     */
+    /** Updates a contract with the content */
     TransactionMetadata preHandleUpdateContract(TransactionBody txn);
 
-    /**
-     * Calls a contract
-     */
+    /** Calls a contract */
     TransactionMetadata preHandleContractCallMethod(TransactionBody txn);
 
-    /**
-     * Deletes a contract instance and transfers any remaining hbars to a specified receiver
-     */
+    /** Deletes a contract instance and transfers any remaining hbars to a specified receiver */
     TransactionMetadata preHandleDeleteContract(TransactionBody txn);
 
-    /**
-     * Deletes a contract if the submitting account has network admin privileges
-     */
+    /** Deletes a contract if the submitting account has network admin privileges */
     TransactionMetadata preHandleSystemDelete(TransactionBody txn);
 
-    /**
-     * Undeletes a contract if the submitting account has network admin privileges
-     */
+    /** Undeletes a contract if the submitting account has network admin privileges */
     TransactionMetadata preHandleSystemUndelete(TransactionBody txn);
 
-    /**
-     * Ethereum transaction
-     */
+    /** Ethereum transaction */
     TransactionMetadata preHandleCallEthereum(TransactionBody txn);
 }
--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/schedule/SchedulePreTransactionHandler.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/schedule/SchedulePreTransactionHandler.java
@@ -1,23 +1,31 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.schedule;
 
 import com.hedera.node.app.spi.PreTransactionHandler;
 import com.hedera.node.app.spi.meta.TransactionMetadata;
 import com.hederahashgraph.api.proto.java.TransactionBody;
-import com.hederahashgraph.api.proto.java.TransactionResponse;
 
 public interface SchedulePreTransactionHandler extends PreTransactionHandler {
-    /**
-     * Creates a new Schedule by submitting the transaction
-     */
+    /** Creates a new Schedule by submitting the transaction */
     TransactionMetadata preHandleCreateSchedule(TransactionBody txn);
 
-    /**
-     * Signs a new Schedule by submitting the transaction
-     */
+    /** Signs a new Schedule by submitting the transaction */
     TransactionMetadata preHandleSignSchedule(TransactionBody txn);
 
-    /**
-     * Deletes a new Schedule by submitting the transaction
-     */
+    /** Deletes a new Schedule by submitting the transaction */
     TransactionMetadata preHandleDeleteSchedule(TransactionBody txn);
 }
