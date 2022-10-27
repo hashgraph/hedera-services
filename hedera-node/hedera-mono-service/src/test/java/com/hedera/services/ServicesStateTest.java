296f00cb19b09e3ecc657f906afa3f0253d08201
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2022-10-26 23:04:21 -0500

    spotless

--- hedera-node/hedera-mono-service/src/test/java/com/hedera/services/ServicesStateTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/services/ServicesStateTest.java
@@ -39,11 +39,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
