1af08683a4a064b87e218bd625b5063f45391ba1
Neeha <52669918+Neeharika-Sompalli@users.noreply.github.com>
2022-10-26 21:03:26 -0500

    Adds `AccountStore` and `RebuiltStateImpl` (#4055)
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: Michael Tinker <michael.tinker@swirldslabs.com>

---
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/file/FileService.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hedera.node.app.service.file;
+
+import com.hedera.node.app.spi.Service;
+import com.hedera.node.app.spi.state.States;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Implements the HAPI <a
+ * href="https://github.com/hashgraph/hedera-protobufs/blob/main/services/file_service.proto">File
+ * Service</a>.
+ */
+public interface FileService extends Service {
+    /**
+     * Creates the file service pre-handler given a particular Hedera world state.
+     *
+     * @param states the state of the world
+     * @return the corresponding file service pre-handler
+     */
+    @NotNull
+    @Override
+    FilePreTransactionHandler createPreTransactionHandler(@NotNull States states);
+}
