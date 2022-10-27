e53d3b46cd4d271c3e7ab571e7febc7a93cabc1d
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-10-27 17:10:49 +0300

    style: formatting
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/models/AbstractLedgerEvmWorldUpdaterTest.java
+++ hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/models/AbstractLedgerEvmWorldUpdaterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.evm.store.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,22 +30,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class AbstractLedgerEvmWorldUpdaterTest {
-  private final Address address =
-      Address.fromHexString("0x000000000000000000000000000000000000077e");
-  AccountAccessor accountAccessor;
-  AbstractLedgerEvmWorldUpdater abstractLedgerEvmWorldUpdater = new AbstractLedgerEvmWorldUpdater(accountAccessor);
+    private final Address address =
+            Address.fromHexString("0x000000000000000000000000000000000000077e");
+    AccountAccessor accountAccessor;
+    AbstractLedgerEvmWorldUpdater abstractLedgerEvmWorldUpdater =
+            new AbstractLedgerEvmWorldUpdater(accountAccessor);
 
-  @Test
-  void accountTests() {
-    assertNull(abstractLedgerEvmWorldUpdater.createAccount(address, 1, Wei.ONE));
-    assertNull(abstractLedgerEvmWorldUpdater.getAccount(address));
-    assertEquals(Collections.emptyList(), abstractLedgerEvmWorldUpdater.getTouchedAccounts());
-    assertEquals(Collections.emptyList(), abstractLedgerEvmWorldUpdater.getDeletedAccountAddresses());
-  }
+    @Test
+    void accountTests() {
+        assertNull(abstractLedgerEvmWorldUpdater.createAccount(address, 1, Wei.ONE));
+        assertNull(abstractLedgerEvmWorldUpdater.getAccount(address));
+        assertEquals(Collections.emptyList(), abstractLedgerEvmWorldUpdater.getTouchedAccounts());
+        assertEquals(
+                Collections.emptyList(),
+                abstractLedgerEvmWorldUpdater.getDeletedAccountAddresses());
+    }
 
-  @Test
-  void updaterTest() {
-    assertEquals(Optional.empty(), abstractLedgerEvmWorldUpdater.parentUpdater());
-    assertNull(abstractLedgerEvmWorldUpdater.updater());
-  }
+    @Test
+    void updaterTest() {
+        assertEquals(Optional.empty(), abstractLedgerEvmWorldUpdater.parentUpdater());
+        assertNull(abstractLedgerEvmWorldUpdater.updater());
+    }
 }
