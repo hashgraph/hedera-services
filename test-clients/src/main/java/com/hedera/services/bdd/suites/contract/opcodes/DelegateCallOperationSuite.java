5a99a936d214e2d6f36236af92280a1501ce3fd8
IvanKavaldzhiev <ivankavaldzhiev@gmail.com>
2022-10-27 18:23:59 +0300

    Remove `ethereumj` dependency from `hedera-services` (#4095)
    Signed-off-by: Ivan <ivankavaldzhiev@gmail.com>

--- test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/DelegateCallOperationSuite.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/DelegateCallOperationSuite.java
@@ -19,6 +19,7 @@ import static com.hedera.services.bdd.spec.HapiApiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.uploadInitCode;
+import static com.hedera.services.bdd.spec.transactions.contract.HapiParserUtil.asHeadlongAddress;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_SOLIDITY_ADDRESS;
@@ -50,7 +51,7 @@ public class DelegateCallOperationSuite extends HapiApiSuite {
                 .given(uploadInitCode(contract), contractCreate(contract))
                 .when()
                 .then(
-                        contractCall(contract, "delegateCall", INVALID_ADDRESS)
+                        contractCall(contract, "delegateCall", asHeadlongAddress(INVALID_ADDRESS))
                                 .hasKnownStatus(INVALID_SOLIDITY_ADDRESS),
                         withOpContext(
                                 (spec, opLog) -> {
@@ -59,7 +60,10 @@ public class DelegateCallOperationSuite extends HapiApiSuite {
                                             HapiPropertySource.asHexedSolidityAddress(id);
 
                                     final var contractCall =
-                                            contractCall(contract, "delegateCall", solidityAddress)
+                                            contractCall(
+                                                            contract,
+                                                            "delegateCall",
+                                                            asHeadlongAddress(solidityAddress))
                                                     .hasKnownStatus(SUCCESS);
 
                                     allRunFor(spec, contractCall);
--- test-clients/src/main/java/com/hedera/services/bdd/suites/records/ContractRecordsSanityCheckSuite.java
+++ test-clients/src/main/java/com/hedera/services/bdd/suites/records/ContractRecordsSanityCheckSuite.java
@@ -127,7 +127,10 @@ public class ContractRecordsSanityCheckSuite extends HapiApiSuite {
                                         STAKING_REWARD,
                                         NODE_REWARD,
                                         DEFAULT_PAYER)))
-                .when(contractCall(PAYABLE_CONTRACT, "deposit", 1_000L).via("txn").sending(1_000L))
+                .when(
+                        contractCall(PAYABLE_CONTRACT, "deposit", BigInteger.valueOf(1_000L))
+                                .via("txn")
+                                .sending(1_000L))
                 .then(
                         validateTransferListForBalances(
                                 "txn",
