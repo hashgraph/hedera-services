720bc1d65d649422807f88290068e3582fb7b428
Dimitar Dinev <mitkojc@gmail.com>
2022-10-28 10:59:20 +0300

    revert some more unneeded changes
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/services/store/contracts/HederaWorldState.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/services/store/contracts/HederaWorldState.java
@@ -354,7 +354,8 @@ public class HederaWorldState implements HederaMutableWorldState {
 
         @Override
         public WorldUpdater updater() {
-            return new HederaStackedWorldStateUpdater(this, wrappedWorldView(), trackingLedgers().wrapped(), dynamicProperties);
+            return new HederaStackedWorldStateUpdater(
+                this, wrappedWorldView(), trackingLedgers().wrapped(), dynamicProperties);
         }
     }
 }
