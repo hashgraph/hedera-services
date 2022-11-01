359dcc63679cd1e4e3d996d1a64d06e462797fbd
Ivan <ivankavaldzhiev@gmail.com>
2022-11-01 15:50:18 +0200

    nit: spotless apply + fix tests
    Signed-off-by: Ivan <ivankavaldzhiev@gmail.com>

--- hedera-node/hedera-evm-api/src/main/java/com/hedera/services/evm/contracts/execution/HederaEvmMessageCallProcessor.java
+++ hedera-node/hedera-evm-api/src/main/java/com/hedera/services/evm/contracts/execution/HederaEvmMessageCallProcessor.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.evm.contracts.execution;
 
 import static org.hyperledger.besu.evm.frame.ExceptionalHaltReason.INSUFFICIENT_GAS;
@@ -23,68 +38,68 @@ import org.hyperledger.besu.evm.tracing.OperationTracer;
 
 /** Overrides Besu precompiler handling, so we can break model layers in Precompile execution */
 public class HederaEvmMessageCallProcessor extends MessageCallProcessor {
-  private static final String INVALID_TRANSFER_MSG = "Transfer of Value to Hedera Precompile";
-  private static final Optional<ExceptionalHaltReason> ILLEGAL_STATE_CHANGE =
-      Optional.of(ExceptionalHaltReason.ILLEGAL_STATE_CHANGE);
-  public static final Bytes INVALID_TRANSFER =
-      Bytes.of(INVALID_TRANSFER_MSG.getBytes(StandardCharsets.UTF_8));
+    private static final String INVALID_TRANSFER_MSG = "Transfer of Value to Hedera Precompile";
+    private static final Optional<ExceptionalHaltReason> ILLEGAL_STATE_CHANGE =
+            Optional.of(ExceptionalHaltReason.ILLEGAL_STATE_CHANGE);
+    public static final Bytes INVALID_TRANSFER =
+            Bytes.of(INVALID_TRANSFER_MSG.getBytes(StandardCharsets.UTF_8));
 
-  private final Map<Address, PrecompiledContract> hederaPrecompiles;
-  protected long gasRequirement;
-  protected Bytes output;
+    private final Map<Address, PrecompiledContract> hederaPrecompiles;
+    protected long gasRequirement;
+    protected Bytes output;
 
-  public HederaEvmMessageCallProcessor(
-      final EVM evm,
-      final PrecompileContractRegistry precompiles,
-      final Map<String, PrecompiledContract> hederaPrecompileList) {
-    super(evm, precompiles);
-    hederaPrecompiles = new HashMap<>();
-    hederaPrecompileList.forEach((k, v) -> hederaPrecompiles.put(Address.fromHexString(k), v));
-  }
+    public HederaEvmMessageCallProcessor(
+            final EVM evm,
+            final PrecompileContractRegistry precompiles,
+            final Map<String, PrecompiledContract> hederaPrecompileList) {
+        super(evm, precompiles);
+        hederaPrecompiles = new HashMap<>();
+        hederaPrecompileList.forEach((k, v) -> hederaPrecompiles.put(Address.fromHexString(k), v));
+    }
 
-  @Override
-  public void start(final MessageFrame frame, final OperationTracer operationTracer) {
-    final var hederaPrecompile = hederaPrecompiles.get(frame.getContractAddress());
-    if (hederaPrecompile != null) {
-      executeHederaPrecompile(hederaPrecompile, frame, operationTracer);
-    } else {
-      if (frame.getValue().greaterThan(Wei.ZERO)) {
-        final var updater = (AbstractLedgerEvmWorldUpdater) frame.getWorldUpdater();
-        if (updater.isTokenAddress(frame.getRecipientAddress())) {
-          frame.setExceptionalHaltReason(ILLEGAL_STATE_CHANGE);
-          frame.setState(MessageFrame.State.EXCEPTIONAL_HALT);
+    @Override
+    public void start(final MessageFrame frame, final OperationTracer operationTracer) {
+        final var hederaPrecompile = hederaPrecompiles.get(frame.getContractAddress());
+        if (hederaPrecompile != null) {
+            executeHederaPrecompile(hederaPrecompile, frame, operationTracer);
+        } else {
+            if (frame.getValue().greaterThan(Wei.ZERO)) {
+                final var updater = (AbstractLedgerEvmWorldUpdater) frame.getWorldUpdater();
+                if (updater.isTokenAddress(frame.getRecipientAddress())) {
+                    frame.setExceptionalHaltReason(ILLEGAL_STATE_CHANGE);
+                    frame.setState(MessageFrame.State.EXCEPTIONAL_HALT);
+                }
+            }
+            if (frame.getState() != EXCEPTIONAL_HALT) {
+                super.start(frame, operationTracer);
+            }
         }
-      }
-      if (frame.getState() != EXCEPTIONAL_HALT) {
-        super.start(frame, operationTracer);
-      }
     }
-  }
 
-  protected void executeHederaPrecompile(
-      final PrecompiledContract contract,
-      final MessageFrame frame,
-      final OperationTracer operationTracer) {
-    if(output!=null && !output.isEmpty()) {
-      output = contract.computePrecompile(frame.getInputData(), frame).getOutput();
-      }
-    if(gasRequirement == 0L) {
-      gasRequirement = contract.gasRequirement(frame.getInputData());
-      }
-    operationTracer.tracePrecompileCall(frame, gasRequirement, output);
-    if (frame.getState() == REVERT) {
-      return;
+    protected void executeHederaPrecompile(
+            final PrecompiledContract contract,
+            final MessageFrame frame,
+            final OperationTracer operationTracer) {
+        if (output == null || output.isEmpty()) {
+            output = contract.computePrecompile(frame.getInputData(), frame).getOutput();
+        }
+        if (gasRequirement == 0L) {
+            gasRequirement = contract.gasRequirement(frame.getInputData());
+        }
+        operationTracer.tracePrecompileCall(frame, gasRequirement, output);
+        if (frame.getState() == REVERT) {
+            return;
+        }
+        if (frame.getRemainingGas() < gasRequirement) {
+            frame.decrementRemainingGas(frame.getRemainingGas());
+            frame.setExceptionalHaltReason(Optional.of(INSUFFICIENT_GAS));
+            frame.setState(EXCEPTIONAL_HALT);
+        } else if (output != null) {
+            frame.decrementRemainingGas(gasRequirement);
+            frame.setOutputData(output);
+            frame.setState(COMPLETED_SUCCESS);
+        } else {
+            frame.setState(EXCEPTIONAL_HALT);
+        }
     }
-    if (frame.getRemainingGas() < gasRequirement) {
-      frame.decrementRemainingGas(frame.getRemainingGas());
-      frame.setExceptionalHaltReason(Optional.of(INSUFFICIENT_GAS));
-      frame.setState(EXCEPTIONAL_HALT);
-    } else if (output != null) {
-      frame.decrementRemainingGas(gasRequirement);
-      frame.setOutputData(output);
-      frame.setState(COMPLETED_SUCCESS);
-    } else {
-      frame.setState(EXCEPTIONAL_HALT);
-    }
-  }
 }
