1d3ab5f149164d690d2f7cd04cf4b6ea5740362e
Dimitar Dinev <mitkojc@gmail.com>
2022-11-02 17:55:05 +0200

    fix unwanted change
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/services/txns/crypto/AutoCreationLogic.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/services/txns/crypto/AutoCreationLogic.java
@@ -320,7 +320,7 @@ public class AutoCreationLogic {
     }
 
     @VisibleForTesting
-    List<InProgressChildRecord> getPendingCreations() {
+    public List<InProgressChildRecord> getPendingCreations() {
         return pendingCreations;
     }
 
