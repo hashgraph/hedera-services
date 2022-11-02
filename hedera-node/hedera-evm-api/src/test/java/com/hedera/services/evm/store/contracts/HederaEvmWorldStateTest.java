501a2d7da229207c4df2c9db060b927e21a0f214
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-11-02 16:35:45 +0200

    style: formatting
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/contracts/HederaEvmWorldStateTest.java
+++ hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/contracts/HederaEvmWorldStateTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.evm.store.contracts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,41 +30,37 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class HederaEvmWorldStateTest {
 
-  @Mock
-  private HederaEvmEntityAccess hederaEvmEntityAccess;
+    @Mock private HederaEvmEntityAccess hederaEvmEntityAccess;
 
-  @Mock
-  private EvmProperties evmProperties;
+    @Mock private EvmProperties evmProperties;
 
-  @Mock
-  private AbstractCodeCache abstractCodeCache;
+    @Mock private AbstractCodeCache abstractCodeCache;
 
-  private HederaEvmWorldState subject;
+    private HederaEvmWorldState subject;
 
-  @BeforeEach
-  void setUp() {
-    subject =
-        new HederaEvmWorldState(hederaEvmEntityAccess, evmProperties, abstractCodeCache) {
-          @Override
-          public HederaEvmWorldUpdater updater() {
-            return null;
-          }
-        };
-  }
+    @BeforeEach
+    void setUp() {
+        subject =
+                new HederaEvmWorldState(hederaEvmEntityAccess, evmProperties, abstractCodeCache) {
+                    @Override
+                    public HederaEvmWorldUpdater updater() {
+                        return null;
+                    }
+                };
+    }
 
-  @Test
-  void rootHash() {
-    assertEquals(Hash.EMPTY, subject.rootHash());
-  }
+    @Test
+    void rootHash() {
+        assertEquals(Hash.EMPTY, subject.rootHash());
+    }
 
-  @Test
-  void frontierRootHash() {
-    assertEquals(Hash.EMPTY, subject.frontierRootHash());
-  }
+    @Test
+    void frontierRootHash() {
+        assertEquals(Hash.EMPTY, subject.frontierRootHash());
+    }
 
-  @Test
-  void streamAccounts() {
-    assertNull(subject.streamAccounts(Bytes32.ZERO, 1));
-  }
-
+    @Test
+    void streamAccounts() {
+        assertNull(subject.streamAccounts(Bytes32.ZERO, 1));
+    }
 }
--- hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/contracts/WorldStateAccountTest.java
+++ hedera-node/hedera-evm-api/src/test/java/com/hedera/services/evm/store/contracts/WorldStateAccountTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.services.evm.store.contracts;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,35 +28,33 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 public class WorldStateAccountTest {
 
-  @Mock
-  HederaEvmEntityAccess entityAccess;
+    @Mock HederaEvmEntityAccess entityAccess;
 
-  MockAbstractCodeCache codeCache =  new MockAbstractCodeCache(100, entityAccess);;
+    MockAbstractCodeCache codeCache = new MockAbstractCodeCache(100, entityAccess);
+    ;
 
-  private final Address address =
-      Address.fromHexString("0x000000000000000000000000000000000000077e");
+    private final Address address =
+            Address.fromHexString("0x000000000000000000000000000000000000077e");
 
-  WorldStateAccount subject = new WorldStateAccount(address, Wei.ONE, codeCache,
-      entityAccess);
+    WorldStateAccount subject = new WorldStateAccount(address, Wei.ONE, codeCache, entityAccess);
 
-  @Test
-  void getAddress() {
-    assertEquals(address, subject.getAddress());
-  }
+    @Test
+    void getAddress() {
+        assertEquals(address, subject.getAddress());
+    }
 
-  @Test
-  void getAddressHash() {
-    assertEquals(Hash.EMPTY, subject.getAddressHash());
-  }
+    @Test
+    void getAddressHash() {
+        assertEquals(Hash.EMPTY, subject.getAddressHash());
+    }
 
-  @Test
-  void getNonce() {
-    assertEquals(0, subject.getNonce());
-  }
+    @Test
+    void getNonce() {
+        assertEquals(0, subject.getNonce());
+    }
 
-  @Test
-  void getBalance() {
-    assertEquals(Wei.ONE, subject.getBalance());
-  }
-
+    @Test
+    void getBalance() {
+        assertEquals(Wei.ONE, subject.getBalance());
+    }
 }
