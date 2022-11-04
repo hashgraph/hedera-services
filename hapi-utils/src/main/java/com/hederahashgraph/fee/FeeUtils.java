dd2bb7218cd17985419b590d19ff8509e64feab4
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2022-11-03 23:23:22 -0500

    fix unit tests
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hapi-utils/src/main/java/com/hederahashgraph/fee/FeeUtils.java
+++ hapi-utils/src/main/java/com/hederahashgraph/fee/FeeUtils.java
@@ -21,17 +21,17 @@ public class FeeUtils {
         throw new UnsupportedOperationException("Utility Class");
     }
 
-    public static long clampedMultiply(final long a, final long b) {
+    public static long clampedAdd(final long a, final long b) {
         try {
-            return Math.multiplyExact(a, b);
+            return Math.addExact(a, b);
         } catch (final ArithmeticException ae) {
             return a > 0 ? Long.MAX_VALUE : Long.MIN_VALUE;
         }
     }
 
-    public static long clampedAdd(final long a, final long b) {
+    public static long clampedMultiply(final long a, final long b) {
         try {
-            return Math.addExact(a, b);
+            return Math.multiplyExact(a, b);
         } catch (final ArithmeticException ae) {
             return ((a ^ b) < 0) ? Long.MIN_VALUE : Long.MAX_VALUE;
         }
