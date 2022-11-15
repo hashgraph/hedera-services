9b00d244e762aa6637b1e6f0cdcf15a08b5e38c0
Richard Bair <rbair23@users.noreply.github.com>
2022-11-14 18:02:45 -0800

    Apply spotless formatting
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-app-spi/src/main/java/module-info.java
+++ hedera-node/hedera-app-spi/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.hedera.node.app.spi {
     requires jsr305;
     requires com.hedera.hashgraph.protobuf.java.api;
+
     exports com.hedera.node.app.spi;
     exports com.hedera.node.app.spi.state;
     exports com.hedera.node.app.spi.key;
