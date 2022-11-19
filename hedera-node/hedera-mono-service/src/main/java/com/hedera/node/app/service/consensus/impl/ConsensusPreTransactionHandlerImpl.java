998912feb4aba231137f6ff43f77106166cbafd3
Matt Hess <matt.hess@swirldslabs.com>
2022-11-18 17:15:22 -0700

    More tests for ConsensusPreTransactionHandler#createTopic
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/consensus/impl/ConsensusPreTransactionHandlerImpl.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/consensus/impl/ConsensusPreTransactionHandlerImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.consensus.impl;
 
 import static com.hedera.node.app.Utils.asHederaKey;
@@ -12,48 +27,48 @@ import javax.annotation.Nonnull;
 import org.apache.commons.lang3.NotImplementedException;
 
 @SuppressWarnings("DanglingJavadoc")
-public class ConsensusPreTransactionHandlerImpl implements
-    com.hedera.node.app.service.consensus.ConsensusPreTransactionHandler {
+public class ConsensusPreTransactionHandlerImpl
+        implements com.hedera.node.app.service.consensus.ConsensusPreTransactionHandler {
 
-  private final AccountStore accountStore;
+    private final AccountStore accountStore;
 
-  public ConsensusPreTransactionHandlerImpl(@Nonnull final AccountStore accountStore) {
-    this.accountStore = accountStore;
-  }
-
-  @Override
-  /** {@inheritDoc} */
-  public TransactionMetadata preHandleCreateTopic(TransactionBody txn) {
-    final var op = txn.getConsensusCreateTopic();
-    final var payer = txn.getTransactionID().getAccountID();
-
-    final var adminKey = asHederaKey(op.getAdminKey());
-    final var submitKey = asHederaKey(op.getSubmitKey());
-    if (adminKey.isPresent() || submitKey.isPresent()) {
-      final var otherReqs = new ArrayList<HederaKey>();
-      adminKey.ifPresent(otherReqs::add);
-      submitKey.ifPresent(otherReqs::add);
-      return new SigTransactionMetadata(accountStore, txn, payer, otherReqs);
+    public ConsensusPreTransactionHandlerImpl(@Nonnull final AccountStore accountStore) {
+        this.accountStore = accountStore;
     }
 
-    return new SigTransactionMetadata(accountStore, txn, payer);
-  }
+    @Override
+    /** {@inheritDoc} */
+    public TransactionMetadata preHandleCreateTopic(TransactionBody txn) {
+        final var op = txn.getConsensusCreateTopic();
+        final var payer = txn.getTransactionID().getAccountID();
 
-  @Override
-  /** {@inheritDoc} */
-  public TransactionMetadata preHandleUpdateTopic(TransactionBody txn) {
-    throw new NotImplementedException();
-  }
+        final var adminKey = asHederaKey(op.getAdminKey());
+        final var submitKey = asHederaKey(op.getSubmitKey());
+        if (adminKey.isPresent() || submitKey.isPresent()) {
+            final var otherReqs = new ArrayList<HederaKey>();
+            adminKey.ifPresent(otherReqs::add);
+            submitKey.ifPresent(otherReqs::add);
+            return new SigTransactionMetadata(accountStore, txn, payer, otherReqs);
+        }
 
-  @Override
-  /** {@inheritDoc} */
-  public TransactionMetadata preHandleDeleteTopic(TransactionBody txn) {
-    throw new NotImplementedException();
-  }
+        return new SigTransactionMetadata(accountStore, txn, payer);
+    }
 
-  @Override
-  /** {@inheritDoc} */
-  public TransactionMetadata preHandleSubmitMessage(TransactionBody txn) {
-    throw new NotImplementedException();
-  }
+    @Override
+    /** {@inheritDoc} */
+    public TransactionMetadata preHandleUpdateTopic(TransactionBody txn) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    /** {@inheritDoc} */
+    public TransactionMetadata preHandleDeleteTopic(TransactionBody txn) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    /** {@inheritDoc} */
+    public TransactionMetadata preHandleSubmitMessage(TransactionBody txn) {
+        throw new NotImplementedException();
+    }
 }
--- hedera-node/hedera-mono-service/src/test/java/com/hedera/node/app/service/consensus/ConsensusPreTransactionHandlerImplTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/node/app/service/consensus/ConsensusPreTransactionHandlerImplTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.consensus;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -5,20 +20,19 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
-import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.node.app.Utils;
 import com.hedera.node.app.service.consensus.impl.ConsensusPreTransactionHandlerImpl;
 import com.hedera.node.app.service.token.impl.AccountStore;
 import com.hedera.node.app.service.token.impl.KeyOrLookupFailureReason;
 import com.hedera.node.app.spi.key.HederaKey;
+import com.hedera.node.app.spi.meta.TransactionMetadata;
 import com.hedera.services.utils.KeyUtils;
 import com.hedera.test.utils.IdUtils;
 import com.hederahashgraph.api.proto.java.AccountID;
 import com.hederahashgraph.api.proto.java.ConsensusCreateTopicTransactionBody;
 import com.hederahashgraph.api.proto.java.Key;
-import com.hederahashgraph.api.proto.java.KeyList;
 import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
-import com.hederahashgraph.api.proto.java.ThresholdKey;
 import com.hederahashgraph.api.proto.java.TransactionBody;
 import com.hederahashgraph.api.proto.java.TransactionID;
 import java.util.List;
@@ -34,11 +48,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ConsensusPreTransactionHandlerImplTest {
     private static final AccountID ACCOUNT_ID_3 = IdUtils.asAccount("0.0.3");
 
-    @Mock
-    private AccountStore accountStore;
+    @Mock private AccountStore accountStore;
 
     private ConsensusPreTransactionHandlerImpl subject;
-
 
     @BeforeEach
     void setUp() {
@@ -46,8 +58,20 @@ class ConsensusPreTransactionHandlerImplTest {
     }
 
     @Test
-    void createAddsNonNullKeys() {
+    void createAddsAllNonNullKeyInputs() {
+        final var payerKey = mockPayerLookup();
+        final var adminKey = KeyUtils.B_COMPLEX_KEY;
+        final var submitKey = KeyUtils.C_COMPLEX_KEY;
 
+        final var result = subject.preHandleCreateTopic(newCreateTxn(adminKey, submitKey));
+
+        assertOkResponse(result);
+        Assertions.assertTrue(result.getReqKeys().contains(payerKey));
+        final var expectedHederaAdminKey = Utils.asHederaKey(adminKey).orElseThrow();
+        Assertions.assertTrue(result.getReqKeys().contains(expectedHederaAdminKey));
+        final var expectedHederaSubmitKey = Utils.asHederaKey(submitKey).orElseThrow();
+        Assertions.assertTrue(result.getReqKeys().contains(expectedHederaSubmitKey));
+        Assertions.assertEquals(3, result.getReqKeys().size());
     }
 
     @Test
@@ -56,66 +80,98 @@ class ConsensusPreTransactionHandlerImplTest {
 
         final var result = subject.preHandleCreateTopic(newCreateTxn(null, null));
 
-        Assertions.assertEquals(ResponseCodeEnum.OK, result.status());
+        assertOkResponse(result);
         Assertions.assertEquals(List.of(payerKey), result.getReqKeys());
     }
 
     @Test
-    void createAddsSeparateAdminKey() {
-        final var payerKey = Utils.asHederaKey(KeyUtils.A_COMPLEX_KEY).get();
-        given(accountStore.getKey(ACCOUNT_ID_3)).willReturn(KeyOrLookupFailureReason.withKey(payerKey));
+    void createAddsDifferentAdminKey() {
+        final var payerKey = mockPayerLookup();
         final var adminKey = KeyUtils.B_COMPLEX_KEY;
 
         final var result = subject.preHandleCreateTopic(newCreateTxn(adminKey, null));
 
+        assertOkResponse(result);
         Assertions.assertTrue(result.getReqKeys().contains(payerKey));
-        Assertions.assertTrue(result.getReqKeys().contains(Utils.asHederaKey(adminKey).get()));
+        final var expectedHederaAdminKey = Utils.asHederaKey(adminKey).orElseThrow();
+        Assertions.assertTrue(result.getReqKeys().contains(expectedHederaAdminKey));
+        Assertions.assertEquals(2, result.getReqKeys().size());
     }
 
     @Test
-    void createAddsSeparateSubmitKey() {
+    void createAddsDifferentSubmitKey() {
+        final var payerKey = mockPayerLookup();
+        final var submitKey = KeyUtils.B_COMPLEX_KEY;
 
+        final var result = subject.preHandleCreateTopic(newCreateTxn(null, submitKey));
+
+        assertOkResponse(result);
+        Assertions.assertTrue(result.getReqKeys().contains(payerKey));
+        final var expectedHederaSubmitKey = Utils.asHederaKey(submitKey).orElseThrow();
+        Assertions.assertTrue(result.getReqKeys().contains(expectedHederaSubmitKey));
+        Assertions.assertEquals(2, result.getReqKeys().size());
     }
 
     @Test
-    void createAddsPayerAsAdmin() {
+    void createAddsPayerAsAdmin() throws InvalidProtocolBufferException {
+        final var payerKey = mockPayerLookup();
+        final var protoPayerKey = Key.parseFrom(KeyUtils.A_COMPLEX_KEY.toByteArray());
 
+        final var result = subject.preHandleCreateTopic(newCreateTxn(protoPayerKey, null));
+
+        assertOkResponse(result);
+        Assertions.assertEquals(List.of(payerKey), result.getReqKeys());
     }
 
     @Test
-    void createAddsPayerAsSubmitter() {
+    void createAddsPayerAsSubmitter() throws InvalidProtocolBufferException {
+        final var payerKey = mockPayerLookup();
+        final var protoPayerKey = Key.parseFrom(KeyUtils.A_COMPLEX_KEY.toByteArray());
 
+        final var result = subject.preHandleCreateTopic(newCreateTxn(null, protoPayerKey));
+
+        assertOkResponse(result);
+        Assertions.assertEquals(List.of(payerKey), result.getReqKeys());
     }
 
     @Test
-    void createFailsWhenPayerIsntFound() {
+    void createFailsWhenPayerNotFound() {
+        given(accountStore.getKey(any()))
+                .willReturn(
+                        KeyOrLookupFailureReason.withFailureReason(
+                                ResponseCodeEnum
+                                        .ACCOUNT_ID_DOES_NOT_EXIST)); // Any error response code
         final var inputTxn = newCreateTxn(null, null);
 
         final var result = subject.preHandleCreateTopic(inputTxn);
 
-        Assertions.assertEquals(ResponseCodeEnum.FAIL_BALANCE, result.status());
+        Assertions.assertEquals(ResponseCodeEnum.INVALID_PAYER_ACCOUNT_ID, result.status());
+        Assertions.assertTrue(result.failed());
         Assertions.assertTrue(result.getReqKeys().isEmpty());
     }
 
     @Test
     void notImplementedMethodsThrowException() {
-        assertThrows(NotImplementedException.class, () -> subject.preHandleUpdateTopic(
-            mock(TransactionBody.class)));
-        assertThrows(NotImplementedException.class, () -> subject.preHandleDeleteTopic(
-            mock(TransactionBody.class)));
-        assertThrows(NotImplementedException.class, () -> subject.preHandleSubmitMessage(
-            mock(TransactionBody.class)));
+        assertThrows(
+                NotImplementedException.class,
+                () -> subject.preHandleUpdateTopic(mock(TransactionBody.class)));
+        assertThrows(
+                NotImplementedException.class,
+                () -> subject.preHandleDeleteTopic(mock(TransactionBody.class)));
+        assertThrows(
+                NotImplementedException.class,
+                () -> subject.preHandleSubmitMessage(mock(TransactionBody.class)));
     }
 
     private HederaKey mockPayerLookup() {
         final var returnKey = Utils.asHederaKey(KeyUtils.A_COMPLEX_KEY).orElseThrow();
-        given(accountStore.getKey(ACCOUNT_ID_3)).willReturn(KeyOrLookupFailureReason.withKey(returnKey));
+        given(accountStore.getKey(ACCOUNT_ID_3))
+                .willReturn(KeyOrLookupFailureReason.withKey(returnKey));
         return returnKey;
     }
 
     private static TransactionBody newCreateTxn(Key adminKey, Key submitKey) {
-        final var txnId = TransactionID.newBuilder().setAccountID(ACCOUNT_ID_3)
-            .build();
+        final var txnId = TransactionID.newBuilder().setAccountID(ACCOUNT_ID_3).build();
         final var createTopicBuilder = ConsensusCreateTopicTransactionBody.newBuilder();
         if (adminKey != null) {
             createTopicBuilder.setAdminKey(adminKey);
@@ -124,8 +180,13 @@ class ConsensusPreTransactionHandlerImplTest {
             createTopicBuilder.setSubmitKey(submitKey);
         }
         return TransactionBody.newBuilder()
-            .setTransactionID(txnId)
-            .setConsensusCreateTopic(createTopicBuilder.build())
-            .build();
+                .setTransactionID(txnId)
+                .setConsensusCreateTopic(createTopicBuilder.build())
+                .build();
+    }
+
+    private static void assertOkResponse(TransactionMetadata result) {
+        Assertions.assertEquals(ResponseCodeEnum.OK, result.status());
+        Assertions.assertFalse(result.failed());
     }
 }
