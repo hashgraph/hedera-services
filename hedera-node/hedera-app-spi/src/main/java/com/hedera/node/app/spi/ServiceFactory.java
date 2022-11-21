023fa6a55da04b2564b0f0c5c1b973131f5fdd15
Hendrik Ebbers <hendrik.ebbers@web.de>
2022-11-21 18:38:19 +0100

    spotless
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/ServiceFactory.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/ServiceFactory.java
@@ -20,10 +20,25 @@ import java.util.Iterator;
 import java.util.Objects;
 import java.util.ServiceLoader;
 
+/**
+ * This class provides the ability to load {@link Service} implementations at runtime. The Java SPI
+ * (see {@link ServiceLoader}) is used to provide such information at runtime. Since we use the Java
+ * module system the {@link ServiceLoader} instance can not be created in the factory. It must be
+ * created in the module that add "uses" information for the module to the {@code module-info.java}.
+ */
 public final class ServiceFactory {
 
     private ServiceFactory() {}
 
+    /**
+     * This method returns a service instance of the given service that is provided by the Java SPI.
+     *
+     * @param type the service type
+     * @param serviceLoader the service loaded that will be used
+     * @param <S> the service type
+     * @return the service instance
+     * @throws IllegalStateException if no or multiple services are found
+     */
     @NonNull
     public static <S extends Service> S loadService(
             @NonNull final Class<S> type, @NonNull final ServiceLoader<S> serviceLoader) {
