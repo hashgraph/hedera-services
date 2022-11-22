4230d405f5d626c34f020762e64648ef650d732b
Dimitar Dinev <mitkojc@gmail.com>
2022-11-22 17:22:21 +0200

    test: add unit coverage
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>

--- hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/operation/HederaCallOperationTest.java
+++ hedera-node/hedera-mono-service/src/test/java/com/hedera/services/contracts/operation/HederaCallOperationTest.java
@@ -15,28 +15,6 @@
  */
 package com.hedera.services.contracts.operation;
 
-/*
- * -
- * ‌
- * Hedera Services Node
- * ​
- * Copyright (C) 2018 - 2021 Hedera Hashgraph, LLC
- * ​
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ‍
- *
- */
-
 import static com.hedera.services.contracts.operation.CommonCallSetup.commonSetup;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -161,5 +139,39 @@ class HederaCallOperationTest {
         assertEquals(
                 Optional.of(HederaExceptionalHaltReason.INVALID_SIGNATURE),
                 invalidSignaturesRes.getHaltReason());
+    }
+
+    @Test
+    void executesLazyCreateAsExpected() {
+        commonSetup(evmMsgFrame, worldUpdater, acc);
+        given(
+                        calc.callOperationGasCost(
+                                any(), anyLong(), anyLong(), anyLong(), anyLong(), anyLong(), any(),
+                                any(), any()))
+                .willReturn(cost);
+        // and:
+        given(evmMsgFrame.getStackItem(0)).willReturn(Bytes.EMPTY);
+        given(evmMsgFrame.getStackItem(1)).willReturn(Bytes.EMPTY);
+        given(evmMsgFrame.getStackItem(2)).willReturn(Bytes.of(2)); // frame value
+        given(evmMsgFrame.getStackItem(3)).willReturn(Bytes.EMPTY);
+        given(evmMsgFrame.getStackItem(4)).willReturn(Bytes.EMPTY);
+        given(evmMsgFrame.getStackItem(5)).willReturn(Bytes.EMPTY);
+        given(evmMsgFrame.getStackItem(6)).willReturn(Bytes.EMPTY);
+        // and:
+        given(evmMsgFrame.stackSize()).willReturn(20);
+        given(evmMsgFrame.getRemainingGas()).willReturn(cost);
+        given(evmMsgFrame.getMessageStackDepth()).willReturn(1025);
+
+        given(evmMsgFrame.getRecipientAddress()).willReturn(Address.ALTBN128_ADD);
+
+        given(worldUpdater.get(any())).willReturn(acc);
+        given(acc.getBalance()).willReturn(Wei.of(100));
+        given(calc.gasAvailableForChildCall(any(), anyLong(), anyBoolean())).willReturn(10L);
+        given(addressValidator.test(any(), any())).willReturn(false);
+
+        var opRes = subject.execute(evmMsgFrame, evm);
+        assertEquals(Optional.empty(), opRes.getHaltReason());
+        assertTrue(opRes.getGasCost().isPresent());
+        assertEquals(opRes.getGasCost().getAsLong(), cost);
     }
 }
