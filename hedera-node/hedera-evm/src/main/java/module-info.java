91b771457657af1e35361ec22beee9d9f3bcdd66
Hendrik Ebbers <hendrik.ebbers@web.de>
2022-12-06 14:14:20 +0100

    dependencies refactored
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-evm/src/main/java/module-info.java
+++ hedera-node/hedera-evm/src/main/java/module-info.java
@@ -1,26 +1,28 @@
 /** Provides the core interfaces for the Hedera EVM implementation. */
 module com.hedera.node.app.service.evm {
-    requires org.hyperledger.besu.evm;
-    requires org.hyperledger.besu.datatypes;
-    requires org.hyperledger.besu.secp256k1;
-    requires tuweni.bytes;
-    requires tuweni.units;
-    requires com.swirlds.common;
-    requires com.github.benmanes.caffeine;
-    requires org.bouncycastle.provider;
-    requires com.google.common;
-    requires com.google.protobuf;
-    requires org.apache.commons.lang3;
-    requires com.sun.jna;
-    requires com.hedera.hashgraph.protobuf.java.api;
+	requires static transitive com.github.spotbugs.annotations;
+	requires transitive com.swirlds.common;
+	requires transitive org.hyperledger.besu.evm;
+	requires transitive org.hyperledger.besu.datatypes;
+	requires transitive org.hyperledger.besu.secp256k1;
 
-    exports com.hedera.node.app.service.evm.store.contracts.utils;
-    exports com.hedera.node.app.service.evm.contracts.execution;
-    exports com.hedera.node.app.service.evm.store.contracts;
-    exports com.hedera.node.app.service.evm.store.models;
-    exports com.hedera.node.app.service.evm;
-    exports com.hedera.node.app.service.evm.accounts;
-    exports com.hedera.node.app.service.evm.contracts.operations;
-    exports com.hedera.node.app.service.evm.contracts.execution.traceability;
-    exports com.hedera.node.app.service.evm.utils;
+	requires tuweni.bytes;
+	requires tuweni.units;
+	requires com.github.benmanes.caffeine;
+	requires org.bouncycastle.provider;
+	requires com.google.common;
+	requires com.google.protobuf;
+	requires org.apache.commons.lang3;
+	requires com.sun.jna;
+	requires com.hedera.hashgraph.protobuf.java.api;
+
+	exports com.hedera.node.app.service.evm.store.contracts.utils;
+	exports com.hedera.node.app.service.evm.contracts.execution;
+	exports com.hedera.node.app.service.evm.store.contracts;
+	exports com.hedera.node.app.service.evm.store.models;
+	exports com.hedera.node.app.service.evm;
+	exports com.hedera.node.app.service.evm.accounts;
+	exports com.hedera.node.app.service.evm.contracts.operations;
+	exports com.hedera.node.app.service.evm.contracts.execution.traceability;
+	exports com.hedera.node.app.service.evm.utils;
 }
