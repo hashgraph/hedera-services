79c17e87562ef6b2c33cb3010e22a1d8d73c1573
Mustafa Uzun <mustafa.uzun@limechain.tech>
2022-12-06 16:49:26 +0200

    style: formatting
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-node/hedera-evm/src/test/java/com/hedera/node/app/service/evm/store/contracts/precompile/impl/EvmAllowancePrecompileTest.java
+++ hedera-node/hedera-evm/src/test/java/com/hedera/node/app/service/evm/store/contracts/precompile/impl/EvmAllowancePrecompileTest.java
@@ -1,7 +1,20 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.evm.store.contracts.precompile.impl;
 
-import static java.util.function.UnaryOperator.identity;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.tuweni.bytes.Bytes;
@@ -12,16 +25,16 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class EvmAllowancePrecompileTest {
 
-  public static final Bytes ALLOWANCE_INPUT_ERC =
-      Bytes.fromHexString(
-          "0x618dc65e00000000000000000000000000000000000003ecdd62ed3e00000000000000000000000000000000000000000000000000000000000003e900000000000000000000000000000000000000000000000000000000000003ea");
+    public static final Bytes ALLOWANCE_INPUT_ERC =
+            Bytes.fromHexString(
+                    "0x618dc65e00000000000000000000000000000000000003ecdd62ed3e00000000000000000000000000000000000000000000000000000000000003e900000000000000000000000000000000000000000000000000000000000003ea");
 
-  @Test
-  void decodeAllowanceInputERC() {
-    final var decodedInput = EvmAllowancePrecompile.decodeTokenAllowance(ALLOWANCE_INPUT_ERC);
+    @Test
+    void decodeAllowanceInputERC() {
+        final var decodedInput = EvmAllowancePrecompile.decodeTokenAllowance(ALLOWANCE_INPUT_ERC);
 
-    assertTrue(decodedInput.token().length > 0);
-    assertTrue(decodedInput.owner().length > 0);
-    assertTrue(decodedInput.spender().length > 0);
-  }
+        assertTrue(decodedInput.token().length > 0);
+        assertTrue(decodedInput.owner().length > 0);
+        assertTrue(decodedInput.spender().length > 0);
+    }
 }
--- hedera-node/hedera-evm/src/test/java/com/hedera/node/app/service/evm/store/contracts/precompile/impl/EvmOwnerOfPrecompileTest.java
+++ hedera-node/hedera-evm/src/test/java/com/hedera/node/app/service/evm/store/contracts/precompile/impl/EvmOwnerOfPrecompileTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2022 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.evm.store.contracts.precompile.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -10,14 +25,14 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class EvmOwnerOfPrecompileTest {
 
-  private static final Bytes OWNER_OF_INPUT =
-      Bytes.fromHexString(
-          "0x6352211e0000000000000000000000000000000000000000000000000000000000000001");
+    private static final Bytes OWNER_OF_INPUT =
+            Bytes.fromHexString(
+                    "0x6352211e0000000000000000000000000000000000000000000000000000000000000001");
 
-  @Test
-  void decodeOwnerOf() {
-    final var decodedInput = EvmOwnerOfPrecompile.decodeOwnerOf(OWNER_OF_INPUT);
+    @Test
+    void decodeOwnerOf() {
+        final var decodedInput = EvmOwnerOfPrecompile.decodeOwnerOf(OWNER_OF_INPUT);
 
-    assertEquals(1, decodedInput.serialNo());
-  }
+        assertEquals(1, decodedInput.serialNo());
+    }
 }
