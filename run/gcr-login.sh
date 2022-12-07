bff9e37a1eae475929eff5345256012f83163dc5
Hendrik Ebbers <hendrik.ebbers@web.de>
2022-12-07 14:55:22 +0100

    old skripts removed
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- run/gcr-login.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-if [ $# -lt 1 ]; then
-  echo "USAGE: $0 <JSON keyfile>"
-  exit 1
-fi
-
-KEYFILE_JSON=$1
-cat $KEYFILE_JSON | \
-  docker login -u _json_key --password-stdin https://gcr.io
