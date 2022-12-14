520398cd3c14238e6ffb9bc1e186130721bb0aa3
Signed-off-by: Vasil Vasilev <vasil.yanev15@gmail.com>
2022-12-14 17:27:17 +0200

    fixed Fees and Admin checks. Refactored code.
    Signed-off-by: Vasil Vasilev <vasil.yanev15@gmail.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/token/impl/TokenPreTransactionHandlerImpl.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/token/impl/TokenPreTransactionHandlerImpl.java
@@ -15,18 +15,20 @@
  */
 package com.hedera.node.app.service.mono.token.impl;
 
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_ALLOWANCE_OWNER_ID;
+import static com.hedera.node.app.service.mono.utils.MiscUtils.asUsableFcKey;
+import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.*;
 
-import com.hedera.node.app.service.mono.SigTransactionMetadata;
 import com.hedera.node.app.service.token.TokenPreTransactionHandler;
 import com.hedera.node.app.spi.PreHandleContext;
-import com.hedera.node.app.spi.key.HederaKey;
+import com.hedera.node.app.spi.meta.SigTransactionMetadataBuilder;
 import com.hedera.node.app.spi.meta.TransactionMetadata;
 import com.hederahashgraph.api.proto.java.AccountID;
+import com.hederahashgraph.api.proto.java.CustomFee;
 import com.hederahashgraph.api.proto.java.TransactionBody;
 import edu.umd.cs.findbugs.annotations.NonNull;
+
+import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 
 /**
  * A {@code TokenPreTransactionHandler} implementation that pre-computes the required signing keys
@@ -37,9 +39,6 @@ public final class TokenPreTransactionHandlerImpl implements TokenPreTransaction
     private final AccountStore accountStore;
     private final PreHandleContext preHandleContext;
 
-    // Not implemented
-    //    private TokenSignatureWaiversImpl waivers;
-
     public TokenPreTransactionHandlerImpl(
             @NonNull final AccountStore accountStore, @NonNull final PreHandleContext ctx) {
         this.accountStore = Objects.requireNonNull(accountStore);
@@ -48,33 +47,9 @@ public final class TokenPreTransactionHandlerImpl implements TokenPreTransaction
 
     @Override
     public TransactionMetadata preHandleCreateToken(final TransactionBody txn) {
-        final var op = txn.getTokenCreation();
         final var payer = txn.getTransactionID().getAccountID();
-        //        final var adminKey = // TODO get admin key
-        final var treasuryId = op.getTreasury();
-        final var autoRenewalAccountId = op.getAutoRenewAccount();
-        final var customFeesList = op.getCustomFeesList();
-        final var meta = new SigTransactionMetadata(accountStore, txn, payer);
-
-        //        meta.addNonPayerKey(adminKey); ?
-        meta.addNonPayerKey(treasuryId);
-        meta.addNonPayerKey(autoRenewalAccountId, INVALID_ALLOWANCE_OWNER_ID);
-
-        for (final var customFee : customFeesList) {
-            final var collector = customFee.getFeeCollectorAccountId();
-            /* A fractional fee collector and a collector for a fixed fee denominated
-            in the units of the newly created token both must always sign a TokenCreate,
-            since these are automatically associated to the newly created token. */
-            final var rFee = customFee.getRoyaltyFee();
-            if (customFee.hasFixedFee() || customFee.hasFractionalFee() || rFee.hasFallbackFee()) {
-                meta.addNonPayerKey(collector);
-            }
-        }
-        return meta;
-
-        //        Treasury key is required to sign
-        //        Admin key, if specified
-        //        Transaction fee payer key
+        final var receiverSigReq = accountStore.getKeyIfReceiverSigRequired(payer) == null;
+        return buildSigTransactionMetadata(txn, payer, receiverSigReq);
     }
 
     @Override
@@ -148,37 +123,65 @@ public final class TokenPreTransactionHandlerImpl implements TokenPreTransaction
     }
 
     /**
-     * Returns metadata for {@code TokenCreate} transaction needed to validate signatures needed for
-     * signing the transaction
+     * Returns metadata for {@code TokenCreate} transaction needed to validate signatures needed
+     * for signing the transaction
      *
-     * @param tx given transaction body
-     * @param key key provided in the transaction body
+     * @param txn            given transaction body
+     * @param payer          payer for the transaction
      * @param receiverSigReq flag for receiverSigReq on the given transaction body
-     * @param payer payer for the transaction
      * @return transaction's metadata needed to validate signatures
      */
-    private TransactionMetadata createTokenSigningMetadata(
-            final TransactionBody tx,
-            final Optional<HederaKey> key,
-            final boolean receiverSigReq,
-            final AccountID payer) {
-        final var meta = new SigTransactionMetadata(accountStore, tx, payer);
-        if (receiverSigReq && key.isPresent()) {
-            meta.addToReqKeys(key.get());
+    private TransactionMetadata buildSigTransactionMetadata(
+            final TransactionBody txn,
+            final AccountID payer,
+            final boolean receiverSigReq) {
+        final var tokenCreateTxnBody = txn.getTokenCreation();
+        final var customFees = tokenCreateTxnBody.getCustomFeesList();
+        final var treasuryId = tokenCreateTxnBody.getTreasury();
+        final var autoRenewalAccountId = tokenCreateTxnBody.getAutoRenewAccount();
+        final var meta =
+                new SigTransactionMetadataBuilder(accountStore).payerKeyFor(payer).txnBody(txn);
+        meta.addNonPayerKey(treasuryId, INVALID_TREASURY_ACCOUNT_FOR_TOKEN);
+        meta.addNonPayerKey(autoRenewalAccountId, INVALID_AUTORENEW_ACCOUNT);
+        if (tokenCreateTxnBody.hasAdminKey()) {
+            final var adminKey = asUsableFcKey(tokenCreateTxnBody.getAdminKey());
+            adminKey.ifPresent(meta::addToReqKeys);
         }
-        return meta;
+        addCustomFeeKey(meta, customFees, receiverSigReq);
+        return meta.build();
     }
 
-    //    /**
-    //     * @param waivers signature waivers for crypto service
-    //     * @deprecated This method is needed for testing until {@link CryptoSignatureWaiversImpl}
-    // is
-    //     *     implemented. FUTURE: This method should be removed once {@link
-    //     *     CryptoSignatureWaiversImpl} is implemented.
-    //     */
-    //    @Deprecated(forRemoval = true)
-    //    @VisibleForTesting
-    //    void setWaivers(final CryptoSignatureWaiversImpl waivers) {
-    //        this.waivers = waivers;
-    //    }
+    private void addCustomFeeKey(final SigTransactionMetadataBuilder meta, final List<CustomFee> customFeesList,
+                                 final boolean sigRequirement) {
+        final var failureStatus = INVALID_FEE_COLLECTOR_ACCOUNT_ID;
+        for (final var customFee : customFeesList) {
+            final var collector = customFee.getFeeCollectorAccountId();
+            /* A fractional fee collector and a collector for a fixed fee denominated
+            in the units of the newly created token both must always sign a TokenCreate,
+            since these are automatically associated to the newly created token. */
+            final boolean alwaysAdd;
+            if (customFee.hasFixedFee()) {
+                final var fixedFee = customFee.getFixedFee();
+                alwaysAdd = fixedFee.hasDenominatingTokenId()
+                        && fixedFee.getDenominatingTokenId().getTokenNum() == 0L;
+                if (alwaysAdd || sigRequirement) {
+                    meta.addNonPayerKey(collector, failureStatus);
+                }
+            } else if (customFee.hasFractionalFee()) {
+                meta.addNonPayerKey(collector, failureStatus);
+            } else if (customFee.hasRoyaltyFee()) {
+                final var royaltyFee = customFee.getRoyaltyFee();
+                if (royaltyFee.hasFallbackFee()) {
+                    final var fFee = royaltyFee.getFallbackFee();
+                    alwaysAdd = fFee.hasDenominatingTokenId() && fFee.getDenominatingTokenId().getTokenNum() == 0;
+                    if (alwaysAdd || sigRequirement) {
+                        meta.addNonPayerKey(collector, failureStatus);
+                    }
+                }
+            } else {
+                meta.addNonPayerKey(collector, INVALID_CUSTOM_FEE_COLLECTOR);
+            }
+        }
+    }
+
 }
