9511180180698b6e2696b67586e420d2f2f1d7da
Hendrik Ebbers <hendrik.ebbers@web.de>
2022-12-22 10:02:06 +0100

    spotless
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-admin-service/src/main/java/module-info.java
+++ hedera-node/hedera-admin-service/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.hedera.node.app.service.admin {
-	exports com.hedera.node.app.service.admin;
+    exports com.hedera.node.app.service.admin;
 
-	uses com.hedera.node.app.service.admin.FreezeService;
+    uses com.hedera.node.app.service.admin.FreezeService;
 
-	requires transitive com.hedera.node.app.spi;
-	requires com.hedera.hashgraph.protobuf.java.api;
-	requires static com.github.spotbugs.annotations;
+    requires transitive com.hedera.node.app.spi;
+    requires com.hedera.hashgraph.protobuf.java.api;
+    requires static com.github.spotbugs.annotations;
 }
