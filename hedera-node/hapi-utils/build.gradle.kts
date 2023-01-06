eee9cf9d0ab6eabb16e3ffd88415b92b0724e28a
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-01-06 16:16:17 -0600

    save
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- hedera-node/hapi-utils/build.gradle.kts
+++ hedera-node/hapi-utils/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2022 Hedera Hashgraph, LLC
+ * Copyright (C) 2020-2023 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,8 +34,8 @@ dependencies {
     api(project(":hedera-node:hedera-evm"))
 
     implementation(libs.jackson)
-    implementation(libs.log4j.logging)
     implementation(libs.bundles.bouncycastle)
+    implementation(libs.bundles.logging)
     implementation(libs.headlong)
     implementation(libs.commons.codec)
     implementation(libs.jna)
