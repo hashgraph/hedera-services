ca822dc507e087a0db62c3d81ce7f16e6f1ba067
Kim Rader <kim.rader@swirldslabs.com>
2023-01-23 15:14:56 -0800

    Implement parity tests
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/hedera-schedule-service-impl/src/test/java/module-info.java
+++ hedera-node/hedera-schedule-service-impl/src/test/java/module-info.java
@@ -9,6 +9,7 @@ module com.hedera.node.app.service.schedule.impl.test {
     requires com.hedera.node.app.service.mono.testFixtures;
     requires org.apache.commons.lang3;
     requires org.apache.commons.codec;
+    requires hedera.services.hedera.node.hedera.app.spi.testFixtures;
 
     opens com.hedera.node.app.service.schedule.impl.test to
             org.junit.platform.commons,
