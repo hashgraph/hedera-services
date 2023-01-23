4ec8989095a22a840acc6f8452192e008aec2754
Matt Hess <matt.hess@swirldslabs.com>
2023-01-23 15:27:27 -0700

    Remove unneeded public modifier
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-consensus-service-impl/src/test/java/com/hedera/node/app/service/consensus/impl/handlers/test/ConsensusCreateTopicHandlerParityTest.java
+++ hedera-node/hedera-consensus-service-impl/src/test/java/com/hedera/node/app/service/consensus/impl/handlers/test/ConsensusCreateTopicHandlerParityTest.java
@@ -43,7 +43,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class ConsensusCreateTopicHandlerParityTest {
+class ConsensusCreateTopicHandlerParityTest {
     private final ConsensusCreateTopicHandler subject = new ConsensusCreateTopicHandler();
     private AccountKeyLookup keyLookup;
 
