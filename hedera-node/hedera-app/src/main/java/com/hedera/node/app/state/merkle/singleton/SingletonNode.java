702004839b52689a4f687df2279cef0e4450d396
Richard Bair <rbair23@users.noreply.github.com>
2023-01-26 15:40:58 -0800

    Implement SingletonNode
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/merkle/singleton/SingletonNode.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/merkle/singleton/SingletonNode.java
@@ -15,37 +15,69 @@
  */
 package com.hedera.node.app.state.merkle.singleton;
 
+import com.hedera.node.app.state.merkle.StateMetadata;
 import com.swirlds.common.merkle.MerkleInternal;
 import com.swirlds.common.merkle.impl.PartialBinaryMerkleInternal;
 import com.swirlds.common.utility.Labeled;
+import edu.umd.cs.findbugs.annotations.NonNull;
 
-// two child nodes, one with the label, one with the value
+/**
+ * A merkle node with a string (the label) as the left child, and the merkle node value as the right
+ * child. We actually support a raw type (any type!) as the value, and we serialize it and put it
+ * into a simple merkle node.
+ *
+ * @param <T> The value type
+ */
 public class SingletonNode<T> extends PartialBinaryMerkleInternal
         implements Labeled, MerkleInternal {
+    private static final long CLASS_ID = 0x3832CC837AB77BFL;
+    public static final int CLASS_VERSION = 1;
+
+    // Only exists for constructable registry as it works today. Remove ASAP!
+    @Deprecated(forRemoval = true)
+    public SingletonNode() {
+        setLeft(new StringLeaf());
+        setRight(null);
+    }
+
+    public SingletonNode(@NonNull final StateMetadata<?, T> md, @NonNull final T value) {
+        setLeft(new StringLeaf(md.stateDefinition().stateKey()));
+        setRight(new ValueLeaf<T>(md, value));
+    }
+
+    private SingletonNode(@NonNull final SingletonNode<T> other) {
+        this.setLeft(other.getLeft().copy());
+        this.setRight(other.getRight().copy());
+    }
 
     @Override
-    public MerkleInternal copy() {
-        return null;
+    public SingletonNode<T> copy() {
+        return new SingletonNode<>(this);
     }
 
     @Override
     public long getClassId() {
-        return 0;
+        return CLASS_ID;
     }
 
     @Override
     public int getVersion() {
-        return 0;
+        return CLASS_VERSION;
     }
 
     @Override
     public String getLabel() {
-        return null;
+        final StringLeaf left = getLeft();
+        return left.getLabel();
     }
 
     public T getValue() {
-        return null;
+        final ValueLeaf<T> right = getRight();
+        return right.getValue();
     }
 
-    public void setValue(T value) {}
+    public void setValue(T value) {
+        ValueLeaf<T> right = getRight();
+        right.setValue(value);
+    }
 }
