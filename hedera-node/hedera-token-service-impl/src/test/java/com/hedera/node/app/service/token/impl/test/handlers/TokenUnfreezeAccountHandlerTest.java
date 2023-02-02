88146e17615c6262ff37082cead8dc7b633939f4
Dimitar Dinev <mitkojc@gmail.com>
2023-02-02 16:49:19 +0200

    fix: sonar smell
    Signed-off-by: Dimitar Dinev <mitkojc@gmail.com>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenUnfreezeAccountHandlerTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenUnfreezeAccountHandlerTest.java
@@ -1,12 +1,27 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hedera.node.app.service.token.impl.test.handlers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.hedera.node.app.service.token.impl.handlers.TokenUnfreezeAccountHandler;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class TokenUnfreezeAccountHandlerTest {
+class TokenUnfreezeAccountHandlerTest {
 
     private TokenUnfreezeAccountHandler subject;
 
@@ -14,6 +29,7 @@ public class TokenUnfreezeAccountHandlerTest {
     void setUp() {
         subject = new TokenUnfreezeAccountHandler();
     }
+
     @Test
     void handleFunctionalityTest() {
         final var notImplemented = "Not implemented";
