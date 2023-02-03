bab14da1b238149f40d46f0906bcc197d6d54f06
Matt Hess <matt.hess@swirldslabs.com>
2023-02-03 12:13:18 -0700

    Increment platform version to 0.36.0-alpha.1
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -116,7 +116,7 @@ dependencyResolutionManagement {
             version("netty-version", "4.1.66.Final")
             version("protobuf-java-version", "3.19.4")
             version("slf4j-version", "2.0.3")
-            version("swirlds-version", "0.36.0-alpha.0")
+            version("swirlds-version", "0.36.0-alpha.1")
             version("tuweni-version", "2.2.0")
             version("jna-version", "5.12.1")
             version("jsr305-version", "3.0.2")
