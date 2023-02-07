1337a11e48ab147e062936951fefecd20defcf99
Michael Heinrichs <netopyr@users.noreply.github.com>
2023-02-07 12:42:45 +0100

    04208 query workflow (#4709)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/onset/WorkflowOnset.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/onset/WorkflowOnset.java
@@ -38,6 +38,10 @@ import java.nio.ByteBuffer;
 /**
  * This class does some pre-processing before each workflow. It parses the provided {@link
  * ByteBuffer} and checks it.
+ *
+ * <p>This is used in every workflow that deals with transactions, i.e. in all workflows except the
+ * query workflow. And even in the query workflow, it is used when dealing with the contained {@link
+ * com.hederahashgraph.api.proto.java.CryptoTransfer}.
  */
 public class WorkflowOnset {
 
@@ -54,7 +58,11 @@ public class WorkflowOnset {
     }
 
     /**
-     * Parse the given {@link ByteBuffer} and check its validity
+     * Parse the given {@link ByteBuffer} and check its validity.
+     *
+     * <p>The checks are very general: syntax checks, size limit checks, and some general semantic
+     * checks that apply to all transactions (e.g. does the transaction have a payer, are the
+     * timestamps valid).
      *
      * @param ctx the {@link SessionContext}
      * @param buffer the {@code ByteBuffer} with the serialized transaction
@@ -74,6 +82,10 @@ public class WorkflowOnset {
     /**
      * Parse the given {@link ByteBuffer} and check its validity
      *
+     * <p>The checks are very general: syntax checks, size limit checks, and some general semantic
+     * checks that apply to all transactions (e.g. does the transaction have a payer, are the
+     * timestamps valid).
+     *
      * @param ctx the {@link SessionContext}
      * @param buffer the {@code ByteBuffer} with the serialized transaction
      * @return an {@link OnsetResult} with the parsed and checked entities
@@ -89,6 +101,28 @@ public class WorkflowOnset {
         return doParseAndCheck(ctx, () -> ctx.txParser().parseFrom(buffer));
     }
 
+    /**
+     * Check the validity of the provided {@link Transaction}
+     *
+     * <p>The checks are very general: syntax checks, size limit checks, and some general semantic
+     * checks that apply to all transactions (e.g. does the transaction have a payer, are the
+     * timestamps valid).
+     *
+     * @param ctx the {@link SessionContext}
+     * @param transaction the {@link Transaction} that needs to be checked
+     * @return an {@link OnsetResult} with the parsed and checked entities
+     * @throws PreCheckException if the data is not valid
+     * @throws NullPointerException if one of the arguments is {@code null}
+     */
+    public OnsetResult doParseAndCheck(
+            @NonNull final SessionContext ctx, @NonNull final Transaction transaction)
+            throws PreCheckException {
+        requireNonNull(ctx);
+        requireNonNull(transaction);
+
+        return doParseAndCheck(ctx, () -> transaction);
+    }
+
     @SuppressWarnings("deprecation")
     private OnsetResult doParseAndCheck(
             @NonNull final SessionContext ctx, @NonNull final TransactionSupplier txSupplier)
@@ -97,7 +131,7 @@ public class WorkflowOnset {
         // 1. Parse the transaction object
         final Transaction tx;
         try {
-            tx = txSupplier.parse();
+            tx = txSupplier.supply();
         } catch (InvalidProtocolBufferException e) {
             throw new PreCheckException(INVALID_TRANSACTION);
         }
@@ -134,12 +168,13 @@ public class WorkflowOnset {
         }
 
         // 4. return TransactionBody
-        return new OnsetResult(txBody, errorCode, signatureMap, functionality);
+        return new OnsetResult(
+                txBody, bodyBytes.toByteArray(), errorCode, signatureMap, functionality);
     }
 
     @FunctionalInterface
-    private interface TransactionSupplier {
-        Transaction parse() throws InvalidProtocolBufferException;
+    public interface TransactionSupplier {
+        Transaction supply() throws InvalidProtocolBufferException;
     }
 
     private static <T> T parse(
