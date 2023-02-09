3ff3d49288ad757f0caa2c339c49a6b17006ec16
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-02-09 16:21:23 -0600

    Add log
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/exports/FileSignTool.java
+++ hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/exports/FileSignTool.java
@@ -466,6 +466,8 @@ public class FileSignTool {
             } catch (final IOException e) {
                 LOGGER.error(MARKER, "Got IOException", e);
             }
+        } else {
+            System.out.println("No log4j configuration file found!")
         }
     }
 
