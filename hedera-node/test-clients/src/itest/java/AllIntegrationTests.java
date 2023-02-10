3c87656b6431d521cade29cf07eb95154268374c
Matt Hess <matt.hess@swirldslabs.com>
2023-02-10 12:09:39 -0700

    spotless
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/test-clients/src/itest/java/AllIntegrationTests.java
+++ hedera-node/test-clients/src/itest/java/AllIntegrationTests.java
@@ -14,10 +14,8 @@
  * limitations under the License.
  */
 import com.hedera.services.bdd.junit.BalanceReconciliationValidator;
-import com.hedera.services.bdd.junit.validators.BlockNoValidator;
 import com.hedera.services.bdd.junit.ExpiryRecordsValidator;
 import com.hedera.services.bdd.junit.validators.BlockNoValidator;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
