511c1c3b6ff2f744ea9b16ee623310b3c4e16ec0
Stoyan Stoyanov <122104047+stoyan-lime@users.noreply.github.com>
2023-02-16 15:10:14 +0200

    Implement TokenDeleteHandler preHandle (#4987)
    Signed-off-by: Stoyan Stoyanov <stoyan.stoyanov@limechain.tech>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/dispatcher/TransactionDispatcherTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/dispatcher/TransactionDispatcherTest.java
@@ -626,7 +626,8 @@ class TransactionDispatcherTest {
                                 .build(),
                         (BiConsumer<TransactionHandlers, PreHandleContext>)
                                 (handlers, meta) ->
-                                        verify(handlers.tokenDeleteHandler()).preHandle(meta)),
+                                        verify(handlers.tokenDeleteHandler())
+                                                .preHandle(eq(meta), any())),
                 Arguments.of(
                         TransactionBody.newBuilder()
                                 .setTokenWipe(TokenWipeAccountTransactionBody.getDefaultInstance())
