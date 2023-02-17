9903813f6241db8a64f46cda4b9069b0560c4948
Kim Rader <kim.rader@swirldslabs.com>
2023-02-17 16:04:00 +0100

    Initial commit
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/test-clients/src/itest/java/AllIntegrationTests.java
+++ hedera-node/test-clients/src/itest/java/AllIntegrationTests.java
@@ -15,6 +15,7 @@
  */
 
 import com.hedera.services.bdd.junit.BalanceReconciliationValidator;
+import com.hedera.services.bdd.junit.TokenReconciliationValidator;
 import com.hedera.services.bdd.junit.validators.BlockNoValidator;
 import java.util.Arrays;
 import java.util.Collection;
@@ -75,6 +76,6 @@ class AllIntegrationTests extends IntegrationTestBase {
     @TestFactory
     List<DynamicTest> recordStreamValidation() {
         return List.of(recordStreamValidation(
-                TEST_CONTAINER_NODE0_STREAMS, new BalanceReconciliationValidator(), new BlockNoValidator()));
+                TEST_CONTAINER_NODE0_STREAMS, new BalanceReconciliationValidator(), new BlockNoValidator(), new TokenReconciliationValidator()));
     }
 }
