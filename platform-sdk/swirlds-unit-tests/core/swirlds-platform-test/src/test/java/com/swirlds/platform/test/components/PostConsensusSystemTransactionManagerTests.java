27911a722a0d2e7c2795beeda79005faa3000a3a
Austin Littley <austin.littley@swirldslabs.com>
2023-02-28 08:56:45 -0500

    Move utility function out of test file
    Signed-off-by: Austin Littley <austin.littley@swirldslabs.com>

--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/components/PostConsensusSystemTransactionManagerTests.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/components/PostConsensusSystemTransactionManagerTests.java
@@ -16,28 +16,19 @@
 
 package com.swirlds.platform.test.components;
 
-import static com.swirlds.platform.test.components.TransactionHandlingTestUtils.newDummyEvent;
+import static com.swirlds.platform.test.components.TransactionHandlingTestUtils.newDummyRound;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 
-import com.swirlds.common.crypto.CryptographyHolder;
-import com.swirlds.common.system.events.BaseEventHashedData;
-import com.swirlds.common.system.events.BaseEventUnhashedData;
-import com.swirlds.common.system.transaction.internal.SystemTransaction;
 import com.swirlds.common.test.DummySystemTransaction;
 import com.swirlds.platform.components.transaction.system.PostConsensusSystemTransactionHandler;
 import com.swirlds.platform.components.transaction.system.PostConsensusSystemTransactionManager;
 import com.swirlds.platform.components.transaction.system.PostConsensusSystemTransactionManagerFactory;
 import com.swirlds.platform.components.transaction.system.PostConsensusSystemTransactionTypedHandler;
-import com.swirlds.platform.consensus.GraphGenerations;
-import com.swirlds.platform.internal.ConsensusRound;
-import com.swirlds.platform.internal.EventImpl;
 import com.swirlds.platform.state.State;
 import com.swirlds.test.framework.TestComponentTags;
 import com.swirlds.test.framework.TestTypeTags;
-import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.DisplayName;
@@ -95,21 +86,5 @@ class PostConsensusSystemTransactionManagerTests {
         assertDoesNotThrow(
                 () -> handler.handleRound(mock(State.class), newDummyRound(List.of(1))),
                 "should not throw");
-    }
-
-    /**
-     * Generates a new round, with specified number of events, containing DummySystemTransactions
-     *
-     * @param roundContents a list of integers, where each list element results in an event being added the output
-     *                      round, and the element value specifies number of transactions to include in the event
-     * @return a new round, with specified contents
-     */
-    public static ConsensusRound newDummyRound(final List<Integer> roundContents) {
-        final List<EventImpl> events = new ArrayList<>();
-        for (Integer transactionCount : roundContents) {
-            events.add(newDummyEvent(transactionCount));
-        }
-
-        return new ConsensusRound(events, mock(GraphGenerations.class));
     }
 }
--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/components/TransactionHandlingTestUtils.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/components/TransactionHandlingTestUtils.java
@@ -1,12 +1,18 @@
 package com.swirlds.platform.test.components;
 
+import static org.mockito.Mockito.mock;
+
 import com.swirlds.common.crypto.CryptographyHolder;
 import com.swirlds.common.system.events.BaseEventHashedData;
 import com.swirlds.common.system.events.BaseEventUnhashedData;
 import com.swirlds.common.system.transaction.internal.SystemTransaction;
 import com.swirlds.common.test.DummySystemTransaction;
+import com.swirlds.platform.consensus.GraphGenerations;
+import com.swirlds.platform.internal.ConsensusRound;
 import com.swirlds.platform.internal.EventImpl;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Utility functions for testing system transaction handling
@@ -35,5 +41,21 @@ public final class TransactionHandlingTestUtils {
                         Instant.now(),
                         transactions),
                 new BaseEventUnhashedData(0L, new byte[0]));
+    }
+
+    /**
+     * Generates a new round, with specified number of events, containing DummySystemTransactions
+     *
+     * @param roundContents a list of integers, where each list element results in an event being added the output
+     *                      round, and the element value specifies number of transactions to include in the event
+     * @return a new round, with specified contents
+     */
+    public static ConsensusRound newDummyRound(final List<Integer> roundContents) {
+        final List<EventImpl> events = new ArrayList<>();
+        for (Integer transactionCount : roundContents) {
+            events.add(newDummyEvent(transactionCount));
+        }
+
+        return new ConsensusRound(events, mock(GraphGenerations.class));
     }
 }
