5521c795e509adec9012e46fe2da45aefc54dd51
Cody Littley <cody@swirldslabs.com>
2023-03-03 14:02:53 -0600

    Create CountUpLatch primitive.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

---
+++ platform-sdk/swirlds-common/src/main/java/com/swirlds/common/threading/CountUpLatch.java
@@ -0,0 +1,146 @@
+package com.swirlds.common.threading;
+
+import static com.swirlds.common.utility.CompareTo.isLessThan;
+
+import com.swirlds.common.time.OSTime;
+import com.swirlds.common.time.Time;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.Phaser;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Similar to a {@link java.util.concurrent.CountDownLatch}, but counts up instead.
+ * <p>
+ * Note: this may not be performant if waiting for a very large number of count increases
+ * or for very high frequency updates. Each time the count increases we touch concurrency objects.
+ */
+public class CountUpLatch {
+
+    private final Time time;
+    private final AtomicLong currentCount;
+    private final Phaser phaser;
+
+    /**
+     * Create a new CountUpLatch with an initial count of 0.
+     */
+    public CountUpLatch() {
+        this(0, OSTime.getInstance());
+    }
+
+    /**
+     * Create a new CountUpLatch with the given initial count.
+     *
+     * @param initialCount the initial count
+     */
+    public CountUpLatch(final long initialCount) {
+        this(initialCount, OSTime.getInstance());
+    }
+
+    /**
+     * Create a new CountUpLatch with the given initial count.
+     *
+     * @param initialCount the initial count
+     * @param time         provides wall clock time
+     */
+    public CountUpLatch(final long initialCount, final Time time) {
+        this.time = time;
+        this.currentCount = new AtomicLong(initialCount);
+        this.phaser = new Phaser(1);
+    }
+
+    /**
+     * Get the current count.
+     *
+     * @return the current count
+     */
+    public long getCount() {
+        return currentCount.get();
+    }
+
+    /**
+     * Increment the count by 1.
+     */
+    public void increment() {
+        add(1);
+    }
+
+    /**
+     * Set the count to a higher value.
+     *
+     * @param count the new count, must be greater than the current value
+     * @throws IllegalArgumentException if the new count is less than the current count
+     */
+    public void set(final long count) {
+        this.currentCount.getAndUpdate(original -> {
+            if (count < original) {
+                throw new IllegalArgumentException("Current count is " + original +
+                        " but new count is " + count + ", cannot set count to a lower value");
+            }
+            return count;
+        });
+        phaser.arriveAndAwaitAdvance();
+    }
+
+    /**
+     * Increment the count by the given delta.
+     *
+     * @param delta the amount to increment the count by
+     */
+    public void add(final long delta) {
+        currentCount.addAndGet(delta);
+    }
+
+    /**
+     * Wait until the count reaches the given value.
+     *
+     * @param count the count to wait for
+     * @throws InterruptedException if interrupted while waiting
+     */
+    public void await(final long count) throws InterruptedException {
+        if (currentCount.get() >= count) {
+            return;
+        }
+
+        phaser.register();
+        try {
+            while (getCount() < count) {
+                if (currentCount.get() >= count) {
+                    return;
+                }
+                phaser.arriveAndAwaitAdvance();
+            }
+        } finally {
+            phaser.arriveAndDeregister();
+        }
+    }
+
+    /**
+     * Wait until the count reaches the given value.
+     *
+     * @param count      the count to wait for
+     * @param timeToWait the maximum time to wait
+     * @return true if the count reached the given value, false if the time to wait expired
+     * @throws InterruptedException if interrupted while waiting
+     */
+    public boolean await(final long count, final Duration timeToWait) throws InterruptedException {
+        if (currentCount.get() >= count) {
+            return true;
+        }
+
+        final Instant start = time.now();
+        phaser.register();
+        try {
+            while (getCount() < count && isLessThan(Duration.between(start, time.now()), timeToWait)) {
+                if (currentCount.get() >= count) {
+                    return true;
+                }
+                phaser.arriveAndAwaitAdvance();
+            }
+        } finally {
+            phaser.arriveAndDeregister();
+        }
+
+        return currentCount.get() >= count;
+    }
+}
---
+++ platform-sdk/swirlds-unit-tests/common/swirlds-common-test/src/test/java/com/swirlds/common/test/threading/CountUpLatchTests.java
@@ -0,0 +1,207 @@
+package com.swirlds.common.test.threading;
+
+import static com.swirlds.common.test.RandomUtils.getRandomPrintSeed;
+import static com.swirlds.common.threading.manager.AdHocThreadManager.getStaticThreadManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.swirlds.common.threading.CountUpLatch;
+import com.swirlds.common.threading.framework.config.ThreadConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+@DisplayName("CountUpLatch Tests")
+class CountUpLatchTests {
+
+    @Test
+    @DisplayName("Track Count Test")
+    void trackCountTest() {
+        final Random random = getRandomPrintSeed();
+
+        final int start = random.nextInt();
+        final CountUpLatch latch = new CountUpLatch(start);
+
+        long sum = start;
+        for (int i = 0; i < 100; i++) {
+            latch.increment();
+            sum++;
+
+            final int amount = random.nextInt(100);
+            latch.add(amount);
+            sum += amount;
+
+            assertEquals(sum, latch.getCount());
+        }
+    }
+
+    @Test
+    @DisplayName("Set Test")
+    void setTest() {
+        final Random random = getRandomPrintSeed();
+
+        long count = Long.MIN_VALUE;
+        final CountUpLatch latch = new CountUpLatch(count);
+
+        for (int i = 0; i < 100; i++) {
+            final long proposedCount = random.nextLong();
+
+            if (proposedCount >= count) {
+                latch.set(proposedCount);
+                assertEquals(proposedCount, latch.getCount());
+                count = proposedCount;
+            } else {
+                assertThrows(IllegalArgumentException.class, () -> latch.set(proposedCount));
+            }
+        }
+    }
+
+    // TODO test that is more likely to increment over the finish line
+
+    private record MinMaxCount(long minCount, long maxCount) {
+
+    }
+
+    protected static Stream<Arguments> buildArguments() {
+        final List<Arguments> arguments = new ArrayList<>();
+        arguments.add(Arguments.of(new MinMaxCount(Long.MIN_VALUE / 4, Long.MAX_VALUE / 4)));
+        arguments.add(Arguments.of(new MinMaxCount(0, Long.MAX_VALUE)));
+        arguments.add(Arguments.of(new MinMaxCount(Long.MIN_VALUE + 1, 0)));
+        arguments.add(Arguments.of(new MinMaxCount(-10_000, 10_000)));
+        arguments.add(Arguments.of(new MinMaxCount(-10_000, 0)));
+        arguments.add(Arguments.of(new MinMaxCount(0, 10_000)));
+        return arguments.stream();
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("buildArguments")
+    @DisplayName("Increment On Creating Thread Test")
+    void incrementOnCreatingThreadTest(final MinMaxCount minMaxCount) throws InterruptedException {
+        final Random random = getRandomPrintSeed();
+
+        final long minCount = minMaxCount.minCount();
+        final long maxCount = minMaxCount.maxCount();
+
+        final int threadCount = 10;
+        final int waitsPerThread = 100;
+        final long maxIncrement = (maxCount - minCount) / 1000;
+
+        final AtomicBoolean error = new AtomicBoolean(false);
+        final CountDownLatch finishedLatch = new CountDownLatch(threadCount);
+
+        final CountUpLatch latch = new CountUpLatch(minCount);
+
+        // Create a bunch of threads that will wait for random counts
+        for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
+            final Random threadRandom = new Random(random.nextLong());
+            new ThreadConfiguration(getStaticThreadManager())
+                    .setThreadName("testThread-" + threadIndex)
+                    .setInterruptableRunnable(() -> {
+                        long desiredCount = minCount;
+                        for (int iteration = 0; iteration < waitsPerThread; iteration++) {
+                            desiredCount = threadRandom.nextLong(desiredCount, maxCount);
+
+                            latch.await(desiredCount);
+                            if (latch.getCount() < desiredCount) {
+                                error.set(true);
+                                break;
+                            }
+
+                        }
+                        finishedLatch.countDown();
+                    })
+                    .build(true);
+        }
+
+        // Increment the count a little at a time
+        while (latch.getCount() < maxCount - maxIncrement) {
+            if (random.nextBoolean()) {
+                latch.increment();
+            } else {
+                latch.add(random.nextLong(maxIncrement));
+            }
+        }
+
+        latch.set(maxCount);
+
+        assertTrue(finishedLatch.await(1, TimeUnit.SECONDS));
+        assertFalse(error.get());
+    }
+
+    @ParameterizedTest
+    @MethodSource("buildArguments")
+    @DisplayName("Increment On Another Thread Test")
+    void incrementOnAnotherThreadTest(final MinMaxCount minMaxCount) throws InterruptedException {
+        final Random random = getRandomPrintSeed();
+
+        final long minCount = minMaxCount.minCount();
+        final long maxCount = minMaxCount.maxCount();
+
+        final int threadCount = 10;
+        final int waitsPerThread = 100;
+        final long maxIncrement = (maxCount - minCount) / 1000;
+
+        final AtomicBoolean error = new AtomicBoolean(false);
+        final CountDownLatch finishedLatch = new CountDownLatch(threadCount);
+
+        final CountUpLatch latch = new CountUpLatch(minCount);
+
+        // Create a bunch of threads that will wait for random counts
+        for (int threadIndex = 0; threadIndex < threadCount; threadIndex++) {
+            final Random threadRandom = new Random(random.nextLong());
+            new ThreadConfiguration(getStaticThreadManager())
+                    .setThreadName("testThread-" + threadIndex)
+                    .setInterruptableRunnable(() -> {
+                        long desiredCount = minCount;
+                        for (int iteration = 0; iteration < waitsPerThread; iteration++) {
+                            desiredCount = threadRandom.nextLong(desiredCount, maxCount);
+
+                            latch.await(desiredCount);
+                            if (latch.getCount() < desiredCount) {
+                                error.set(true);
+                                break;
+                            }
+
+                        }
+                        finishedLatch.countDown();
+                    })
+                    .build(true);
+        }
+
+        final CountDownLatch incrementLatch = new CountDownLatch(1);
+
+        new ThreadConfiguration(getStaticThreadManager())
+                .setThreadName("incrementThread")
+                .setRunnable(() -> {
+                    // Increment the count a little at a time
+                    while (latch.getCount() < maxCount - maxIncrement) {
+                        if (random.nextBoolean()) {
+                            latch.increment();
+                        } else {
+                            latch.add(random.nextLong(maxIncrement));
+                        }
+                    }
+
+                    latch.set(maxCount);
+                    incrementLatch.countDown();
+                })
+                .build(true);
+
+       incrementLatch.await();
+
+        assertTrue(finishedLatch.await(1, TimeUnit.SECONDS));
+        assertFalse(error.get());
+    }
+}
