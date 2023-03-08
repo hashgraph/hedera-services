71cbb316a66ffd1de6f774e53bf4211e3c39d1ce
Cody Littley <cody@swirldslabs.com>
2023-03-08 14:40:52 -0600

    Updated javadocs.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-common/src/main/java/com/swirlds/common/system/SwirldState.java
+++ platform-sdk/swirlds-common/src/main/java/com/swirlds/common/system/SwirldState.java
@@ -55,13 +55,42 @@ public interface SwirldState extends MerkleNode {
 
     /**
      * Provides the application an opportunity to perform operations on transactions in an event prior to handling.
-     * Called against a given {@link Event} only once, globally (not once per state instance) This method may modify the
-     * {@link Transaction}s in the event by doing nothing, adding additional signatures, removing existing signatures,
-     * replacing signatures with versions that expand the public key from an application specific identifier to an
-     * actual public key, or attaching metadata. Additional signatures extracted from the transaction payload can also
-     * be added to the list of signatures to be verified.
+     * Called against a given {@link Event} only once, globally (not once per state instance). This method may modify
+     * the {@link Transaction}s in the event, but such modification is not necessary.
      * <p>
-     * <strong>This method is always invoked on an immutable state.</strong>
+     * No ordering guarantees are given for this method.
+     * The only guarantee is that for all events that reach consensus,
+     * this method will eventually be called as long as the node does not crash.
+     * <ul>
+     * <li>
+     * This method is usually invoked on an event before it reaches consensus,
+     * but that is not a hard guarantee.
+     * </li>
+     * <li>
+     * This method may be called after {@link #handleConsensusRound(Round, SwirldDualState)} has been called on that
+     * event's round (although such behavior is expected to be uncommon).
+     * <ul>
+     * <li>
+     * If it is desired that pre-handle always come before handle for a given event, then the application should
+     * implement its own ordering mechanism that causes handle to wait until pre-handle has completed.
+     * </li>
+     * </ul>
+     * </li>
+     * <li>
+     * The order that events may be passed to this method may be different from their eventual consensus order.
+     * </li>
+     * <li>
+     * It is possible for this method to be called on an event, but for that
+     * event never to reach consensus (thus becoming stale).
+     * </li>
+     * <li>
+     * For events that become stale, this method may be called on some nodes but not others.
+     * </li>
+     * </ul>
+     *
+     * <p>
+     * <strong>This method is always invoked on an immutable state. </strong> Which immutable state copy
+     * is not guaranteed, although it will always be a recent copy.
      *
      * @param event the event to perform pre-handling on
      * @see #handleConsensusRound(Round, SwirldDualState)
@@ -72,6 +101,20 @@ public interface SwirldState extends MerkleNode {
 
     /**
      * Handle transactions in a round apply them to the state.
+     * This method is always called on the mutable state, i.e. the most recent copy.
+     * <p>
+     * After this method returns, all side effects of the transactions in this round must be written to the state.
+     * It is not ok to asynchronously modify the state after this method returns.
+     * <p>
+     * All modifications to the state in this method must be deterministic. That is, the state that results
+     * from applying this method must be guaranteed to always have the same cryptographic merkle hash.
+     * <p>
+     * This method is called on all rounds in sequential order. This method is not called for round N+1 until round
+     * N has been fully handled. Between each call of this method, the state will be fast copied. Each copy of the
+     * state will only have this method called on it once.
+     * <p>
+     * With the exception of the {@link #init(Platform, SwirldDualState, InitTrigger, SoftwareVersion)} method
+     * and serialization migration hooks, it is NEVER ok to modify the state except in this method.
      *
      * @param round           the round to be handled and applied to this state
      * @param swirldDualState the dual state for this round
