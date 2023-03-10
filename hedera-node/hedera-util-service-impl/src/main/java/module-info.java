479292cc398b403f543e0e86cc3b776f9372cd91
Iris Simon <122310714+iwsimon@users.noreply.github.com>
2023-03-10 10:18:50 -0500

    5436 use pbj util service (#5563)
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hedera-node/hedera-util-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-util-service-impl/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.hedera.node.app.service.util.impl {
     requires com.github.spotbugs.annotations;
     requires javax.inject;
     requires dagger;
+    requires com.hedera.pbj.runtime;
+    requires com.hedera.node.hapi;
 
     provides com.hedera.node.app.service.util.UtilService with
             UtilServiceImpl;
