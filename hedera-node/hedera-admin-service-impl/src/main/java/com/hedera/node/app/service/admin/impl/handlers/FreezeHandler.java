d69c54486a817d588ae6bd61c94ce3afaebe7058
Kim Rader <kim.rader@swirldslabs.com>
2023-03-30 14:28:45 -0700

    Respond to reviewer comments
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/hedera-admin-service-impl/src/main/java/com/hedera/node/app/service/admin/impl/handlers/FreezeHandler.java
+++ hedera-node/hedera-admin-service-impl/src/main/java/com/hedera/node/app/service/admin/impl/handlers/FreezeHandler.java
@@ -21,11 +21,6 @@ import static com.hedera.hapi.node.base.ResponseCodeEnum.FREEZE_UPDATE_FILE_DOES
 import static com.hedera.hapi.node.base.ResponseCodeEnum.FREEZE_UPDATE_FILE_HASH_DOES_NOT_MATCH;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_FREEZE_TRANSACTION_BODY;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.OK;
-import static com.hedera.hapi.node.freeze.FreezeType.FREEZE_ONLY;
-import static com.hedera.hapi.node.freeze.FreezeType.FREEZE_UPGRADE;
-import static com.hedera.hapi.node.freeze.FreezeType.PREPARE_UPGRADE;
-import static com.hedera.hapi.node.freeze.FreezeType.TELEMETRY_UPGRADE;
-import static com.hedera.hapi.node.freeze.FreezeType.UNKNOWN_FREEZE_TYPE;
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.node.base.FileID;
@@ -72,16 +67,9 @@ public class FreezeHandler implements TransactionHandler {
         requireNonNull(context);
 
         FreezeTransactionBody freezeTxn = context.getTxn().freezeOrThrow();
-        final FreezeType freezeType = freezeTxn.freezeType();
 
         try {
-            // default value for freezeType is UNKNOWN_FREEZE_TYPE
-            // reject any freeze transactions that do not set freezeType or set it to UNKNOWN_FREEZE_TYPE
-            if (freezeType == UNKNOWN_FREEZE_TYPE) {
-                throw new PreCheckException(INVALID_FREEZE_TRANSACTION_BODY);
-            }
-
-            // freeze.proto properties startHour, startMin, endHour, endMin are deprecated
+            // freeze.proto properties startHour, startMin, endHour, endMin are deprecated in the protobuf
             // reject any freeze transactions that set these properties
             if (freezeTxn.startHour() != 0
                     || freezeTxn.startMin() != 0
@@ -90,59 +78,91 @@ public class FreezeHandler implements TransactionHandler {
                 throw new PreCheckException(INVALID_FREEZE_TRANSACTION_BODY);
             }
 
-            // for these freeze types, freezeStartTime is required and it must be in the future
-            if (freezeType == FREEZE_ONLY || freezeType == FREEZE_UPGRADE || freezeType == TELEMETRY_UPGRADE) {
-                final Timestamp freezeStartTime = freezeTxn.startTime();
-                if (!freezeTxn.hasStartTime() || Timestamp.DEFAULT.equals(freezeStartTime)) {
-                    throw new PreCheckException(INVALID_FREEZE_TRANSACTION_BODY);
+            final FreezeType freezeType = freezeTxn.freezeType();
+            switch (freezeType) {
+                    // default value for freezeType is UNKNOWN_FREEZE_TYPE
+                    // reject any freeze transactions that do not set freezeType or set it to UNKNOWN_FREEZE_TYPE
+                case UNKNOWN_FREEZE_TYPE -> throw new PreCheckException(INVALID_FREEZE_TRANSACTION_BODY);
+
+                    // FREEZE_ONLY requires a valid start_time
+                case FREEZE_ONLY -> verifyFreezeStartTimeIsInFuture(
+                        freezeTxn, context.getTxn().transactionID().transactionValidStart());
+
+                    // PREPARE_UPGRADE requires valid update_file and file_hash values
+                case PREPARE_UPGRADE -> verifyUpdateFileAndHash(freezeTxn, specialFileStore);
+
+                    // FREEZE_UPGRADE and TELEMETRY_UPGRADE require a valid start_time and valid update_file and
+                    // file_hash values
+                case FREEZE_UPGRADE, TELEMETRY_UPGRADE -> {
+                    verifyFreezeStartTimeIsInFuture(
+                            freezeTxn, context.getTxn().transactionID().transactionValidStart());
+
+                    // from proto specs, it looks like update file not required for FREEZE_UPGRADE and TELEMETRY_UPGRADE
+                    // but specs aren't very clear
+                    // current code in FreezeTransitionLogic checks for the file in specialFiles
+                    // so we will do the same
+                    verifyUpdateFileAndHash(freezeTxn, specialFileStore);
                 }
-                final Instant freezeStartTimeInstant =
-                        Instant.ofEpochSecond(freezeStartTime.seconds(), freezeStartTime.nanos());
 
-                // get current consensus time
-                final Timestamp ts = context.getTxn().transactionID().transactionValidStart();
-                final Instant effectiveNowInstant = Instant.ofEpochSecond(ts.seconds(), ts.nanos());
-
-                // make sure freezeStartTime is after current consensus time
-                final boolean freezeStartTimeIsInFuture = freezeStartTimeInstant.isAfter(effectiveNowInstant);
-                if (!freezeStartTimeIsInFuture) {
-                    throw new PreCheckException(FREEZE_START_TIME_MUST_BE_FUTURE);
+                    // FREEZE_ABORT does not require any additional checks
+                case FREEZE_ABORT -> {
+                    // do nothing
                 }
             }
 
-            // for these freeze types, updateFile and fileHash are required
-            if (freezeType == PREPARE_UPGRADE || freezeType == FREEZE_UPGRADE || freezeType == TELEMETRY_UPGRADE) {
-                final FileID updateFile = freezeTxn.updateFile();
-                // from proto specs, it looks like update file not required for FREEZE_UPGRADE and TELEMETRY_UPGRADE
-                // but specs aren't very clear
-                // current code in FreezeTransitionLogic checks for the file in specialFiles
-                // so we will do the same here
+            // no need to add any keys to the context because this transaction does not require any signatures
+            // it must be submitted by an account with superuser privileges, that is checked during ingest
 
-                if (!freezeTxn.hasUpdateFile()
-                        || updateFile == null
-                        || specialFileStore.get(updateFile.fileNum()).isEmpty()) {
-                    throw new PreCheckException(FREEZE_UPDATE_FILE_DOES_NOT_EXIST);
-                }
-
-                final Bytes fileHash = freezeTxn.fileHash();
-                if (fileHash == null || Bytes.EMPTY.equals(fileHash) || fileHash.length() != UPDATE_FILE_HASH_LEN) {
-                    throw new PreCheckException(FREEZE_UPDATE_FILE_HASH_DOES_NOT_MATCH);
-                }
-
-                // FREEZE_ABORT is the last freeze type. It requires no additional checks.
-                // FREEZE_ABORT can be run multiple times to allow DevOps to submit
-                // multiple such transactions if disconnected nodes missed the previous
-
-                // no need to add any keys to the context because this transaction does not require any signatures
-                // it must be submitted by an account with superuser privileges, that is checked during ingest
-
-                // all checks have passed
-                context.status(OK);
-            }
+            // all checks have passed
+            context.status(OK);
         } catch (PreCheckException e) {
             // instead of catching this exception, would like to allow it to propagate up
             // this will be implemented in issue #5880
             context.status(e.responseCode());
+        }
+    }
+
+    /**
+     * For freeze types FREEZE_ONLY, FREEZE_UPGRADE, and TELEMETRY_UPGRADE, the startTime field must be set to
+     * a time in the future, where future is defined as a time after the current consensus time.
+     * @throws PreCheckException if startTime is not in the future
+     */
+    private void verifyFreezeStartTimeIsInFuture(FreezeTransactionBody freezeTxn, Timestamp curConsensusTime)
+            throws PreCheckException {
+        final Timestamp freezeStartTime = freezeTxn.startTime();
+        if (!freezeTxn.hasStartTime() || Timestamp.DEFAULT.equals(freezeStartTime)) {
+            throw new PreCheckException(INVALID_FREEZE_TRANSACTION_BODY);
+        }
+        final Instant freezeStartTimeInstant =
+                Instant.ofEpochSecond(freezeStartTime.seconds(), freezeStartTime.nanos());
+
+        final Instant effectiveNowInstant = Instant.ofEpochSecond(curConsensusTime.seconds(), curConsensusTime.nanos());
+
+        // make sure freezeStartTime is after current consensus time
+        final boolean freezeStartTimeIsInFuture = freezeStartTimeInstant.isAfter(effectiveNowInstant);
+        if (!freezeStartTimeIsInFuture) {
+            throw new PreCheckException(FREEZE_START_TIME_MUST_BE_FUTURE);
+        }
+    }
+
+    /**
+     * For freeze types PREPARE_UPGRADE, FREEZE_UPGRADE, and TELEMETRY_UPGRADE, the updateFile and fileHash fields must be set.
+     * @throws PreCheckException if updateFile or fileHash are not set or don't pass sanity checks
+     */
+    private void verifyUpdateFileAndHash(FreezeTransactionBody freezeTxn, ReadableSpecialFileStore specialFileStore)
+            throws PreCheckException {
+        final FileID updateFile = freezeTxn.updateFile();
+
+        if (!freezeTxn.hasUpdateFile()
+                || updateFile == null
+                || specialFileStore.get(updateFile.fileNum()).isEmpty()) {
+            throw new PreCheckException(FREEZE_UPDATE_FILE_DOES_NOT_EXIST);
+        }
+
+        final Bytes fileHash = freezeTxn.fileHash();
+        // don't verify the hash, just make sure it is not null or empty and is the correct length
+        if (fileHash == null || Bytes.EMPTY.equals(fileHash) || fileHash.length() != UPDATE_FILE_HASH_LEN) {
+            throw new PreCheckException(FREEZE_UPDATE_FILE_HASH_DOES_NOT_MATCH);
         }
     }
 }
