b421bd19c6cb13951b587d212396285668ba6e18
Michael Tinker <michael.tinker@swirldslabs.com>
2023-04-03 17:25:18 -0500

    Add lowLevelEcrecCallBehavior() spec
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/resource/contract/contracts/TestContract/TestContract.sol
+++ hedera-node/test-clients/src/main/resource/contract/contracts/TestContract/TestContract.sol
@@ -17,4 +17,14 @@ contract TestContract {
     function vacateAddress() public {
         selfdestruct(payable(owner));
     }
+
+    function lowLevelECREC() external payable {
+        bytes32 hash = bytes32("hash");
+        uint8 v = uint8(1);
+        bytes32 r = bytes32("r");
+        bytes32 s = bytes32("s");
+        (bool success, ) = address(0x01).call{value: 1}(
+            abi.encodeWithSignature("ecrecover(bytes32,uint8,bytes32,bytes32)", hash, v, r, s));    
+        require(success);
+    }
 }
