f2c683298eefbeaa6affdb98ae97199ff43a285f
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-04-05 11:46:15 +0200

    export fixed
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-base/src/main/java/module-info.java
+++ platform-sdk/swirlds-base/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.swirlds.base {
     exports com.swirlds.base;
     exports com.swirlds.base.function;
     exports com.swirlds.base.state;
-    exports com.swirlds.base.functions;
 
     requires static com.github.spotbugs.annotations;
 }
