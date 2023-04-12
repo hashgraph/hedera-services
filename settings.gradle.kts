3e00bea37d700e28db21a11e45843efec48a497f
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-04-12 18:17:04 -0500

    change branch name
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -100,7 +100,7 @@ gitRepositories {
     // choose tag or branch of HAPI you would like to test with
     // this looks for a tag in hedera-protobufs repo
     // This version needs to match tha HAPI version below in versionCatalogs
-    tag.set("05111-use-pbj-for-token-state")
+    tag.set("add-pbj-types-for-state")
     // do not load project from repo
     autoInclude.set(false)
   }
