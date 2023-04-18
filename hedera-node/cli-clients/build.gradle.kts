fc62ede2817c388b9dfa927fef9279b4b5767525
David Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2023-04-17 17:53:05 -0700

    Example plugin for pcli in the services repo
    Example pcli plugin implements one command - summarize a signed state file.
    This is simply a command I had at hand that uses services functionality
    (specificially `hedera-mono-service`) on top of platform functionality.
    
    Purpose is to 1) enable development of pcli plugins in the services repo, and
    2) provide a working example as PoC.
    
    Fixes #5694
    
    Signed-off-by: David Bakin <117694041+david-bakin-sl@users.noreply.github.com>

---
+++ hedera-node/cli-clients/build.gradle.kts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+  id("com.hedera.hashgraph.conventions")
+  id("com.hedera.hashgraph.shadow-jar")
+  id("org.gradle.java-test-fixtures")
+}
+
+description = "Hedera Services Command-Line Clients"
+
+tasks.jar { manifest { attributes("Automatic-Module-Name" to "com.hedera.services.cli") } }
+
+configurations.all {
+  exclude("javax.anno0tation", "javax.annotation.api")
+  exclude("com.google.code.findbugs", "jsr305")
+  exclude("org.jetbrains", "annotations")
+  exclude("org.checkerframework", "checker-qual")
+  exclude("org.hamcrest", "hamcrest-core")
+}
+
+dependencies {
+  compileOnly(libs.spotbugs.annotations)
+  implementation(libs.bundles.swirlds)
+  implementation(project(":hedera-node:hedera-mono-service"))
+  implementation(testLibs.classgraph)
+  implementation(testLibs.picocli)
+  testImplementation(testLibs.bundles.testing)
+}
---
+++ hedera-node/cli-clients/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.hedera.services.cli {
+    exports com.hedera.services.cli;
+
+    requires static com.github.spotbugs.annotations;
+    requires com.hedera.node.app.service.mono;
+    requires com.swirlds.cli;
+    requires com.swirlds.virtualmap;
+    requires com.swirlds.platform;
+    requires com.swirlds.common;
+    requires info.picocli;
+    requires io.github.classgraph;
+}
