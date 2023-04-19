88b2f8638fca271114ab34b21e1a6d07c36fb795
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-04-19 10:51:01 +0200

    test fixed
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/config/ConfigProviderImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/config/ConfigProviderImplTest.java
@@ -18,10 +18,16 @@ package com.hedera.node.app.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 import com.hedera.node.app.service.mono.context.properties.PropertySource;
+import com.hedera.node.app.spi.config.Profile;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mock.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,6 +37,26 @@ class ConfigProviderImplTest {
 
     @Mock(strictness = Strictness.LENIENT)
     private PropertySource propertySource;
+
+    @BeforeEach
+    void configureMockForConfigData() {
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Integer.class), ArgumentMatchers.any()))
+                .thenReturn(1);
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Long.class), ArgumentMatchers.any()))
+                .thenReturn(Long.MAX_VALUE);
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Double.class), ArgumentMatchers.any()))
+                .thenReturn(1.2D);
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Profile.class), ArgumentMatchers.any()))
+                .thenReturn(Profile.TEST);
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(String.class), ArgumentMatchers.any()))
+                .thenReturn("test");
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Boolean.class), ArgumentMatchers.any()))
+                .thenReturn(true);
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(List.class), ArgumentMatchers.any()))
+                .thenReturn(List.of());
+        when(propertySource.getTypedProperty(ArgumentMatchers.eq(Set.class), ArgumentMatchers.any()))
+                .thenReturn(Set.of());
+    }
 
     @Test
     void testInvalidCreation() {
