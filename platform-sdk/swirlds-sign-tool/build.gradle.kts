bc1c357dc5fc13c5b5c8c05137c96e4c8d15b85c
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-04-21 16:06:01 +0200

    SigningTool defined as app
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-sign-tool/build.gradle.kts
+++ platform-sdk/swirlds-sign-tool/build.gradle.kts
@@ -18,6 +18,7 @@ plugins {
   id("com.swirlds.platform.conventions")
   id("com.swirlds.platform.library")
   id("com.swirlds.platform.maven-publish")
+  id("com.swirlds.platform.application")
 }
 
 dependencies {
