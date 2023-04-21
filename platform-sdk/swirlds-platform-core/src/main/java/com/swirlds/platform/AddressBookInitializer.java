a6026a539cff5ca4796be39e8935e6ded32d9176
Edward Wertz <123979964+edward-swirldslabs@users.noreply.github.com>
2023-04-21 13:58:16 -0500

    06141 - Fix AddressBookTestingToolState errors for test scenarios (#6182)
    Signed-off-by: Edward Wertz <edward@swirldslabs.com>
    Signed-off-by: Edward Wertz <123979964+edward-swirldslabs@users.noreply.github.com>
    Co-authored-by: Cody Littley <56973212+cody-littley@users.noreply.github.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/AddressBookInitializer.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/AddressBookInitializer.java
@@ -19,6 +19,7 @@ package com.swirlds.platform;
 import static com.swirlds.logging.LogMarker.EXCEPTION;
 import static com.swirlds.logging.LogMarker.STARTUP;
 
+import com.swirlds.common.context.PlatformContext;
 import com.swirlds.common.system.SoftwareVersion;
 import com.swirlds.common.system.address.AddressBook;
 import com.swirlds.common.system.address.AddressBookValidator;
@@ -64,6 +65,9 @@ public class AddressBookInitializer {
             DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-m-ss").withZone(ZoneId.systemDefault());
     /** For logging info, warn, and error. */
     private static final Logger logger = LogManager.getLogger(AddressBookInitializer.class);
+    /** The context for the platform */
+    @NonNull
+    private final PlatformContext platformContext;
     /** The current version of the application from config.txt. */
     @NonNull
     private final SoftwareVersion currentVersion;
@@ -93,22 +97,24 @@ public class AddressBookInitializer {
      * Constructs an AddressBookInitializer to initialize an address book from config.txt, the saved state from disk, or
      * the SwirldState on upgrade.
      *
-     * @param currentVersion    The current version of the application. Must not be null.
+     * @param currentVersion    The current version of the application.
      * @param softwareUpgrade   Indicate that the software version has upgraded.
-     * @param signedState       The signed state loaded from disk.  May be null.
-     * @param configAddressBook The address book derived from config.txt. Must not be null.
-     * @param addressBookConfig The configuration settings for AddressBooks.
+     * @param signedState       The signed state loaded from disk.
+     * @param configAddressBook The address book derived from config.txt.
+     * @param platformContext   The context for the platform.
      */
     public AddressBookInitializer(
             @NonNull final SoftwareVersion currentVersion,
             final boolean softwareUpgrade,
             @Nullable final SignedState signedState,
             @NonNull final AddressBook configAddressBook,
-            @NonNull final AddressBookConfig addressBookConfig) {
+            @NonNull final PlatformContext platformContext) {
         this.currentVersion = Objects.requireNonNull(currentVersion, "The currentVersion must not be null.");
         this.softwareUpgrade = softwareUpgrade;
         this.configAddressBook = Objects.requireNonNull(configAddressBook, "The configAddressBook must not be null.");
-        Objects.requireNonNull(addressBookConfig, "The addressBookConfig must not be null.");
+        this.platformContext = Objects.requireNonNull(platformContext, "The platformContext must not be null.");
+        final AddressBookConfig addressBookConfig =
+                platformContext.getConfiguration().getConfigData(AddressBookConfig.class);
         this.loadedSignedState = signedState;
         this.loadedAddressBook = loadedSignedState == null ? null : loadedSignedState.getAddressBook();
         this.pathToAddressBookDirectory = Path.of(addressBookConfig.addressBookDirectory());
@@ -168,7 +174,7 @@ public class AddressBookInitializer {
                     "The address book weight may be updated by the application using data from the state snapshot.");
             candidateAddressBook = loadedSignedState
                     .getSwirldState()
-                    .updateWeight(configAddressBook.copy())
+                    .updateWeight(configAddressBook.copy(), platformContext)
                     .copy();
         }
         candidateAddressBook = checkCandidateAddressBookValidity(candidateAddressBook);
