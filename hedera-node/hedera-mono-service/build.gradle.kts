8de32313ec5604a487796a66a3ba33275faa507e
Kim Rader <kim.rader@swirldslabs.com>
2023-04-21 10:40:21 -0700

    Fixed ServicesStateTest OOM and SizeLimitedStorageTest
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/hedera-mono-service/build.gradle.kts
+++ hedera-node/hedera-mono-service/build.gradle.kts
@@ -96,6 +96,7 @@ dependencies {
 }
 
 tasks.withType<JavaCompile> { options.annotationProcessorPath = apt }
+tasks.withType<Test> { maxHeapSize = "4096m" }
 
 val jmhDaggerSources = file("build/generated/sources/annotationProcessor/java/jmh")
 
