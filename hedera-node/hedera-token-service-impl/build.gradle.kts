4ac211c059cf817402d6eb333865580fec101124
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-04-25 14:03:40 +0200

    gradle file refactored
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-token-service-impl/build.gradle.kts
+++ hedera-node/hedera-token-service-impl/build.gradle.kts
@@ -19,27 +19,27 @@ plugins { id("com.hedera.hashgraph.conventions") }
 description = "Default Hedera Token Service Implementation"
 
 configurations.all {
-  exclude("javax.annotation", "javax.annotation-api")
+    exclude("javax.annotation", "javax.annotation-api")
 
-  exclude("io.grpc", "grpc-core")
-  exclude("io.grpc", "grpc-context")
-  exclude("io.grpc", "grpc-api")
-  exclude("io.grpc", "grpc-testing")
+    exclude("io.grpc", "grpc-core")
+    exclude("io.grpc", "grpc-context")
+    exclude("io.grpc", "grpc-api")
+    exclude("io.grpc", "grpc-testing")
 }
 
 dependencies {
-  implementation(project(":hedera-node:hapi"))
-  annotationProcessor(libs.dagger.compiler)
-  api(project(":hedera-node:hedera-token-service"))
-  implementation(project(":hedera-node:hedera-mono-service"))
-  implementation(libs.bundles.di)
-  implementation(libs.pbj.runtime)
+    annotationProcessor(libs.dagger.compiler)
+    api(project(":hedera-node:hedera-token-service"))
+    implementation(project(":hedera-node:hapi"))
+    implementation(project(":hedera-node:hedera-mono-service"))
+    implementation(libs.bundles.di)
+    implementation(libs.pbj.runtime)
+    implementation(libs.swirlds.virtualmap)
+    implementation(libs.swirlds.jasperdb)
 
-  implementation(libs.swirlds.virtualmap)
-  implementation(libs.swirlds.jasperdb)
-  testImplementation(testLibs.bundles.testing)
-  testImplementation(testFixtures(project(":hedera-node:hedera-mono-service")))
-  testImplementation(testFixtures(project(":hedera-node:hedera-app-spi")))
-  testImplementation(testLibs.mockito.inline)
-  testImplementation(project(":hedera-node:hedera-app-spi"))
+    testImplementation(testLibs.bundles.testing)
+    testImplementation(testFixtures(project(":hedera-node:hedera-mono-service")))
+    testImplementation(testFixtures(project(":hedera-node:hedera-app-spi")))
+    testImplementation(testLibs.mockito.inline)
+    testImplementation(project(":hedera-node:hedera-app-spi"))
 }
