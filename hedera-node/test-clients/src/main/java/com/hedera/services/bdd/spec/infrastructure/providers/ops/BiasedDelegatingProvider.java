aa3b42d99a8d6c82cdbb8a4f4a78cea666bcbe3d
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2023-05-03 17:31:03 +0300

    Add comment for logic for setting default payer for ops
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/infrastructure/providers/ops/BiasedDelegatingProvider.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/infrastructure/providers/ops/BiasedDelegatingProvider.java
@@ -128,6 +128,8 @@ public class BiasedDelegatingProvider implements OpProvider {
             ((HapiTxnOp) op).deferStatusResolution();
         }
 
+        // if we have not set the payer when we created the operation e.g. `RandomAccountDeletion`, we should default to `UNIQUE_PAYER_ACCOUNT`
+        // for the cases when we have set the payer e.g. `RandomOperationSignedBy`, do not overwrite it
         if (op.getPayer().isEmpty()) {
             if (isTxnOp) {
                 ((HapiTxnOp) op).payingWith(UNIQUE_PAYER_ACCOUNT).fee(TRANSACTION_FEE);
