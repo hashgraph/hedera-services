e0d2a0797dbb1aed65cf00b9ed9d65a38e0d4708
Oleg Mazurov <oleg.mazurov@swirldslabs.com>
2023-05-02 18:21:16 -0700

    more spotless
    Signed-off-by: Oleg Mazurov <oleg.mazurov@swirldslabs.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/store/models/Token.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/store/models/Token.java
@@ -55,7 +55,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
-
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
