c6c2fabfd4859f727aaece554cc44966937e4691
Oleg Mazurov <oleg.mazurov@swirldslabs.com>
2023-05-02 17:50:16 -0700

    Defer parameter evaluation
    Signed-off-by: Oleg Mazurov <oleg.mazurov@swirldslabs.com>

--- hedera-node/hedera-evm/src/main/java/com/hedera/node/app/service/evm/utils/ValidationUtils.java
+++ hedera-node/hedera-evm/src/main/java/com/hedera/node/app/service/evm/utils/ValidationUtils.java
@@ -19,6 +19,8 @@ package com.hedera.node.app.service.evm.utils;
 import com.hedera.node.app.service.evm.exceptions.InvalidTransactionException;
 import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 
+import java.util.function.Supplier;
+
 /**
  * A minimalist collection of helpers to improve readability of code that throws an {@code
  * InvalidTransactionException}.
@@ -46,6 +48,13 @@ public final class ValidationUtils {
         }
     }
 
+    public static void validateTrue(
+            final boolean flag, final ResponseCodeEnum code, final Supplier<String> failureMsg) {
+        if (!flag) {
+            throw new InvalidTransactionException(failureMsg.get(), code);
+        }
+    }
+
     public static void validateFalse(final boolean flag, final ResponseCodeEnum code) {
         if (flag) {
             throw new InvalidTransactionException(code);
@@ -55,6 +64,13 @@ public final class ValidationUtils {
     public static void validateFalse(final boolean flag, final ResponseCodeEnum code, final String failureMsg) {
         if (flag) {
             throw new InvalidTransactionException(failureMsg, code);
+        }
+    }
+
+    public static void validateFalse(
+            final boolean flag, final ResponseCodeEnum code, final Supplier<String> failureMsg) {
+        if (flag) {
+            throw new InvalidTransactionException(failureMsg.get(), code);
         }
     }
 
