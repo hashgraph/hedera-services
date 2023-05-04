f75e00290d8a4e05ba6c77331d50f317f5779237
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-05-04 12:52:41 -0500

    update branch
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -102,7 +102,7 @@ gitRepositories {
     // choose tag or branch of HAPI you would like to test with
     // this looks for a tag in hedera-protobufs repo
     // This version needs to match tha HAPI version below in versionCatalogs
-    tag.set("add-missing-account-fields")
+    tag.set("add-pbj-types-for-state")
     // do not load project from repo
     autoInclude.set(false)
   }
