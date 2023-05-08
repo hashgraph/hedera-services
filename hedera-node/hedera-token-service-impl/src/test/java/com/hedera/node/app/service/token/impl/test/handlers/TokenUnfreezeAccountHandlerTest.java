1266eb82c8f0baf212926c77a4f5b7da4d325760
Matt Hess <matt.hess@swirldslabs.com>
2023-05-08 14:02:03 -0600

    Implement token unfreeze handle
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenUnfreezeAccountHandlerTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenUnfreezeAccountHandlerTest.java
@@ -19,53 +19,77 @@ package com.hedera.node.app.service.token.impl.test.handlers;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_ACCOUNT_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TOKEN_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_HAS_NO_FREEZE_KEY;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_NOT_ASSOCIATED_TO_ACCOUNT;
+import static com.hedera.node.app.service.mono.pbj.PbjConverter.toPbj;
 import static com.hedera.node.app.service.token.impl.test.handlers.AdapterUtils.txnFrom;
 import static com.hedera.node.app.service.token.impl.test.util.MetaAssertion.basicContextAssertions;
 import static com.hedera.node.app.spi.fixtures.Assertions.assertThrowsPreCheck;
+import static com.hedera.node.app.spi.fixtures.workflows.ExceptionConditions.responseCode;
 import static com.hedera.test.factories.scenarios.TokenUnfreezeScenarios.UNFREEZE_WITH_INVALID_TOKEN;
 import static com.hedera.test.factories.scenarios.TokenUnfreezeScenarios.UNFREEZE_WITH_MISSING_FREEZE_TOKEN;
 import static com.hedera.test.factories.scenarios.TokenUnfreezeScenarios.VALID_UNFREEZE_WITH_EXTANT_TOKEN;
+import static com.hedera.test.factories.scenarios.TxnHandlingScenario.KNOWN_TOKEN_NO_SPECIAL_KEYS;
+import static com.hedera.test.factories.scenarios.TxnHandlingScenario.KNOWN_TOKEN_WITH_FREEZE;
+import static com.hedera.test.factories.scenarios.TxnHandlingScenario.SECOND_TOKEN_SENDER_KT;
 import static com.hedera.test.factories.scenarios.TxnHandlingScenario.TOKEN_FREEZE_KT;
 import static com.hedera.test.factories.txns.SignedTxnFactory.DEFAULT_PAYER_KT;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 import com.hedera.hapi.node.base.AccountID;
+import com.hedera.hapi.node.base.Key;
 import com.hedera.hapi.node.base.TokenID;
 import com.hedera.hapi.node.base.TransactionID;
+import com.hedera.hapi.node.state.token.Account;
+import com.hedera.hapi.node.state.token.TokenRelation;
 import com.hedera.hapi.node.token.TokenUnfreezeAccountTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.service.token.ReadableAccountStore;
 import com.hedera.node.app.service.token.ReadableTokenStore;
+import com.hedera.node.app.service.token.impl.WritableTokenRelationStore;
 import com.hedera.node.app.service.token.impl.handlers.TokenUnfreezeAccountHandler;
 import com.hedera.node.app.service.token.impl.test.util.SigReqAdapterUtils;
 import com.hedera.node.app.spi.fixtures.Assertions;
 import com.hedera.node.app.spi.fixtures.workflows.FakePreHandleContext;
+import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.PreCheckException;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 class TokenUnfreezeAccountHandlerTest {
 
     private static final AccountID ACCOUNT_13257 =
             AccountID.newBuilder().accountNum(13257L).build();
-
-    private ReadableAccountStore accountStore;
-    private ReadableTokenStore tokenStore;
-
     private TokenUnfreezeAccountHandler subject;
 
     @BeforeEach
     void setUp() {
-        accountStore = SigReqAdapterUtils.wellKnownAccountStoreAt();
-        tokenStore = SigReqAdapterUtils.wellKnownTokenStoreAt();
         subject = new TokenUnfreezeAccountHandler();
     }
 
     @Nested
-    class PreHandleTest {
+    class PreHandleTests {
+        private ReadableAccountStore accountStore;
+        private ReadableTokenStore tokenStore;
+
+        @BeforeEach
+        void setUp() {
+            accountStore = SigReqAdapterUtils.wellKnownAccountStoreAt();
+            tokenStore = SigReqAdapterUtils.wellKnownTokenStoreAt();
+        }
+
         @Test
         void tokenUnfreezeWithNoToken() throws PreCheckException {
             final var theTxn = TransactionBody.newBuilder()
@@ -94,7 +118,173 @@ class TokenUnfreezeAccountHandlerTest {
     }
 
     @Nested
+    @ExtendWith(MockitoExtension.class)
+    class HandleTests {
+        private static final TokenID MISSING_TOKEN_97531 =
+                TokenID.newBuilder().tokenNum(97531).build();
+
+        @Mock
+        private ReadableTokenStore tokenStore;
+
+        @Mock
+        private ReadableAccountStore accountStore;
+
+        @Mock
+        private WritableTokenRelationStore tokenRelStore;
+
+        @SuppressWarnings("DataFlowIssue")
+        @Test
+        void nullArgThrowsException() {
+            assertThatThrownBy(() -> subject.handle(null, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(NullPointerException.class);
+            assertThatThrownBy(() -> subject.handle(mock(TransactionBody.class), null, tokenStore, tokenRelStore))
+                    .isInstanceOf(NullPointerException.class);
+            assertThatThrownBy(() -> subject.handle(mock(TransactionBody.class), accountStore, null, tokenRelStore))
+                    .isInstanceOf(NullPointerException.class);
+            assertThatThrownBy(() -> subject.handle(mock(TransactionBody.class), accountStore, tokenStore, null))
+                    .isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        void tokenNotPresentInTxnBody() {
+            final var noTokenTxn = newUnfreezeTxn(null, ACCOUNT_13257);
+
+            assertThatThrownBy(() -> subject.handle(noTokenTxn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(INVALID_TOKEN_ID));
+            verifyNoPut();
+        }
+
+        @Test
+        void accountNotPresentInTxnBody() {
+            final var pbjToken = toPbj(KNOWN_TOKEN_WITH_FREEZE);
+            final var noAcctTxn = newUnfreezeTxn(pbjToken, null);
+            given(tokenStore.getTokenMeta(pbjToken)).willReturn(tokenMetaWithFreezeKey());
+
+            assertThatThrownBy(() -> subject.handle(noAcctTxn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(INVALID_ACCOUNT_ID));
+            verifyNoPut();
+        }
+
+        @Test
+        void tokenNotFound() {
+            final var token = MISSING_TOKEN_97531;
+            given(tokenStore.getTokenMeta(token)).willReturn(null);
+            final var txn = newUnfreezeTxn(token);
+
+            assertThatThrownBy(() -> subject.handle(txn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(INVALID_TOKEN_ID));
+            verifyNoPut();
+        }
+
+        @Test
+        void tokenHasNoFreezeKey() {
+            final var token = toPbj(KNOWN_TOKEN_NO_SPECIAL_KEYS);
+            given(tokenStore.getTokenMeta(token)).willReturn(tokenMetaWithFreezeKey(null));
+            final var txn = newUnfreezeTxn(token);
+
+            assertThatThrownBy(() -> subject.handle(txn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(TOKEN_HAS_NO_FREEZE_KEY));
+            verifyNoPut();
+        }
+
+        @Test
+        void accountNotFound() {
+            final var token = toPbj(KNOWN_TOKEN_WITH_FREEZE);
+            given(tokenStore.getTokenMeta(token)).willReturn(tokenMetaWithFreezeKey());
+            given(accountStore.getAccountById(ACCOUNT_13257)).willReturn(null);
+            final var txn = newUnfreezeTxn(token);
+
+            assertThatThrownBy(() -> subject.handle(txn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(INVALID_ACCOUNT_ID));
+            verifyNoPut();
+        }
+
+        @Test
+        void tokenRelNotFound() throws HandleException {
+            final var token = toPbj(KNOWN_TOKEN_WITH_FREEZE);
+            final var accountNumber = (long) ACCOUNT_13257.accountNumOrThrow();
+            given(tokenStore.getTokenMeta(token)).willReturn(tokenMetaWithFreezeKey());
+            given(accountStore.getAccountById(ACCOUNT_13257))
+                    .willReturn(
+                            Account.newBuilder().accountNumber(accountNumber).build());
+            given(tokenRelStore.getForModify(token.tokenNum(), accountNumber)).willReturn(Optional.empty());
+            final var txn = newUnfreezeTxn(token);
+
+            assertThatThrownBy(() -> subject.handle(txn, accountStore, tokenStore, tokenRelStore))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(TOKEN_NOT_ASSOCIATED_TO_ACCOUNT));
+            verifyNoPut();
+        }
+
+        @Test
+        void tokenRelUnfreezeSuccessful() {
+            final var token = toPbj(KNOWN_TOKEN_WITH_FREEZE);
+            final var accountNumber = (long) ACCOUNT_13257.accountNumOrThrow();
+            given(tokenStore.getTokenMeta(token)).willReturn(tokenMetaWithFreezeKey());
+            given(accountStore.getAccountById(ACCOUNT_13257))
+                    .willReturn(
+                            Account.newBuilder().accountNumber(accountNumber).build());
+            given(tokenRelStore.getForModify(token.tokenNum(), accountNumber))
+                    .willReturn(Optional.of(TokenRelation.newBuilder()
+                            .tokenNumber(token.tokenNum())
+                            .accountNumber(accountNumber)
+                            .build()));
+            final var txn = newUnfreezeTxn(token);
+
+            subject.handle(txn, accountStore, tokenStore, tokenRelStore);
+            verify(tokenRelStore)
+                    .put(TokenRelation.newBuilder()
+                            .tokenNumber(token.tokenNum())
+                            .accountNumber(accountNumber)
+                            .frozen(false)
+                            .build());
+        }
+
+        private void verifyNoPut() {
+            verify(tokenRelStore, never()).put(any());
+        }
+
+        private ReadableTokenStore.TokenMetadata tokenMetaWithFreezeKey() {
+            return tokenMetaWithFreezeKey(SECOND_TOKEN_SENDER_KT.asPbjKey());
+        }
+
+        private ReadableTokenStore.TokenMetadata tokenMetaWithFreezeKey(Key freezeKey) {
+            return new ReadableTokenStore.TokenMetadata(null, null, null, freezeKey, null, null, null, false, 25L);
+        }
+
+        private TransactionBody newUnfreezeTxn(TokenID token) {
+            return newUnfreezeTxn(token, ACCOUNT_13257);
+        }
+
+        private TransactionBody newUnfreezeTxn(TokenID token, AccountID account) {
+            TokenUnfreezeAccountTransactionBody.Builder unfreezeTxnBodyBuilder =
+                    TokenUnfreezeAccountTransactionBody.newBuilder();
+            if (token != null) unfreezeTxnBodyBuilder.token(token);
+            if (account != null) unfreezeTxnBodyBuilder.account(account);
+            return TransactionBody.newBuilder()
+                    .transactionID(
+                            TransactionID.newBuilder().accountID(ACCOUNT_13257).build())
+                    .tokenUnfreeze(unfreezeTxnBodyBuilder)
+                    .build();
+        }
+    }
+
+    @Nested
     class TokenUnfreezeAccountHandlerParityTest {
+        private ReadableAccountStore accountStore;
+        private ReadableTokenStore tokenStore;
+
+        @BeforeEach
+        void setUp() {
+            accountStore = SigReqAdapterUtils.wellKnownAccountStoreAt();
+            tokenStore = SigReqAdapterUtils.wellKnownTokenStoreAt();
+        }
+
         @Test
         void tokenUnfreezeWithExtantFreezable() throws PreCheckException {
             final var txn = txnFrom(VALID_UNFREEZE_WITH_EXTANT_TOKEN);
