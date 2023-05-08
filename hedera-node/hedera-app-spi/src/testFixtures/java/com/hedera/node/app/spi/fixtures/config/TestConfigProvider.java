0b6bbbd1cce70902992e5868ddda47df92ffc81f
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-05-08 10:13:25 +0200

    spotless
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-app-spi/src/testFixtures/java/com/hedera/node/app/spi/fixtures/config/TestConfigProvider.java
+++ hedera-node/hedera-app-spi/src/testFixtures/java/com/hedera/node/app/spi/fixtures/config/TestConfigProvider.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.hedera.node.app.spi.fixtures.config;
@@ -138,7 +137,7 @@ public class TestConfigProvider implements ConfigProvider {
 
     @Override
     public VersionedConfiguration getConfiguration() {
-        try (var ignore = creationLock.lock();) {
+        try (var ignore = creationLock.lock(); ) {
             if (versionedConfiguration != null) {
                 final Configuration configuration = configurationBuilder.build();
                 versionedConfiguration = new VersionedConfiguration() {
