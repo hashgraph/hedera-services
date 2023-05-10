ed76dba27fda161a438048c339d7a24bf331afe2
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-05-10 17:51:45 +0200

    api to get config
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/meta/HandleContext.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/meta/HandleContext.java
@@ -19,13 +19,13 @@ package com.hedera.node.app.spi.meta;
 import com.hedera.node.app.spi.validation.AttributeValidator;
 import com.hedera.node.app.spi.validation.ExpiryValidator;
 import com.hedera.node.app.spi.workflows.TransactionHandler;
+import com.swirlds.config.api.Configuration;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.time.Instant;
 import java.util.function.LongSupplier;
 
 /**
- * Bundles up the in-handle application context required by {@link TransactionHandler}
- * implementations.
+ * Bundles up the in-handle application context required by {@link TransactionHandler} implementations.
  *
  * <p>At present, only supplies the context needed for Consensus Service handlers in the
  * limited form described by https://github.com/hashgraph/hedera-services/issues/4945.
@@ -53,8 +53,8 @@ public interface HandleContext {
     AttributeValidator attributeValidator();
 
     /**
-     * Returns the validator for expiry metadata (both explicit expiration times and
-     * auto-renew configuration) of entities created or updated by handlers.
+     * Returns the validator for expiry metadata (both explicit expiration times and auto-renew configuration) of
+     * entities created or updated by handlers.
      *
      * @return the validator for expiry metadata
      */
@@ -64,11 +64,21 @@ public interface HandleContext {
      * Create a new store given the store's interface. This gives read-only access to the store.
      *
      * @param storeInterface The store interface to find and create a store for
+     * @param <C>            Interface class for a Store
      * @return An implementation of store interface provided, or null if the store
-     * @param <C> Interface class for a Store
      * @throws IllegalArgumentException if the storeInterface class provided is unknown to the app
-     * @throws NullPointerException if {@code clazz} is {@code null}
+     * @throws NullPointerException     if {@code clazz} is {@code null}
      */
     @NonNull
     <C> C createReadableStore(@NonNull Class<C> storeInterface);
+
+    /**
+     * Returns the current {@link Configuration}.
+     *
+     * @return the {@link Configuration}
+     */
+    @NonNull
+    default Configuration getConfiguration() {
+        throw new UnsupportedOperationException("getConfiguration() not implemented, will be done by next PR");
+    }
 }
