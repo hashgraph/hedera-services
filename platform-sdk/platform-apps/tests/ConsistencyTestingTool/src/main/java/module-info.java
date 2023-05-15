e80a9feedb6880b25dcef5a8c62ea1bc784d7bbd
Austin Littley <102969658+alittley@users.noreply.github.com>
2023-05-15 15:20:54 -0400

    05783 consistency testing tool (#6505)
    Signed-off-by: Austin Littley <austin@swirldslabs.com>
    Co-authored-by: JeffreyDallas <39912573+JeffreyDallas@users.noreply.github.com>

---
+++ platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.swirlds.demo.consistency {
+    requires com.swirlds.base;
+    requires com.swirlds.platform;
+    requires com.swirlds.common;
+    requires com.swirlds.logging;
+    requires lazysodium.java;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.pkix;
+    requires org.apache.logging.log4j;
+    requires static com.github.spotbugs.annotations;
+    requires com.swirlds.config;
+}
--- platform-sdk/settings.gradle.kts
+++ platform-sdk/settings.gradle.kts
@@ -68,6 +68,8 @@ include(":swirlds-platform-apps:tests:MigrationTestingTool")
 
 include(":swirlds-platform-apps:tests:PlatformTestingTool")
 
+include(":swirlds-platform-apps:tests:ConsistencyTestingTool")
+
 include(":swirlds-platform-apps:tests:StatsSigningTestingTool")
 
 include(":swirlds-benchmarks")
