7739677281a588fe1d50248bd331748e7f53b6da
Richard Bair <rbair23@users.noreply.github.com>
2023-05-24 13:02:49 -0700

    Remove synchronized and use ConcurrentSkipListSet instead
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/recordcache/DeduplicationCacheImpl.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/recordcache/DeduplicationCacheImpl.java
@@ -27,7 +27,7 @@ import com.hedera.node.app.state.DeduplicationCache;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.time.Instant;
 import java.util.Set;
-import java.util.TreeSet;
+import java.util.concurrent.ConcurrentSkipListSet;
 import javax.inject.Inject;
 
 /** An implementation of {@link DeduplicationCache}. */
@@ -36,7 +36,7 @@ public final class DeduplicationCacheImpl implements DeduplicationCache {
      * The {@link TransactionID}s that this node has already submitted to the platform, sorted by transaction start
      * time, such that earlier start times come first. We guard this data structure within a synchronized block.
      */
-    private final Set<TransactionID> submittedTxns = new TreeSet<>((t1, t2) ->
+    private final Set<TransactionID> submittedTxns = new ConcurrentSkipListSet<>((t1, t2) ->
             TIMESTAMP_COMPARATOR.compare(t1.transactionValidStartOrThrow(), t2.transactionValidStartOrThrow()));
 
     /** Used for looking up the max transaction duration window. To be replaced by some new config object */
@@ -50,7 +50,7 @@ public final class DeduplicationCacheImpl implements DeduplicationCache {
 
     /** {@inheritDoc} */
     @Override
-    public synchronized void add(@NonNull final TransactionID transactionID) {
+    public void add(@NonNull final TransactionID transactionID) {
         // We don't want to use another thread to prune the set, so we will take the opportunity here to do so.
         // Remember that at this point we have passed through all the throttles, so this method is only called
         // at most 10,000 / (Number of nodes) times per second, which is not a lot.
@@ -58,14 +58,14 @@ public final class DeduplicationCacheImpl implements DeduplicationCache {
         removeTransactionsOlderThan(epochSeconds);
 
         // If the transaction is within the max transaction duration window, then add it to the set.
-        if (transactionID.transactionValidStartOrThrow().seconds() > epochSeconds) {
+        if (transactionID.transactionValidStartOrThrow().seconds() >= epochSeconds) {
             submittedTxns.add(transactionID);
         }
     }
 
     /** {@inheritDoc} */
     @Override
-    public synchronized boolean contains(@NonNull final TransactionID transactionID) {
+    public boolean contains(@NonNull final TransactionID transactionID) {
         // We will prune the set here as well. By pruning before looking up, we are sure that we only return true
         // if the transactionID is still valid
         final var epochSeconds = earliestEpicSecond();
@@ -90,7 +90,7 @@ public final class DeduplicationCacheImpl implements DeduplicationCache {
      *
      * @param earliestEpochSecond The earliest epoch second that should be kept in the cache.
      */
-    private synchronized void removeTransactionsOlderThan(final long earliestEpochSecond) {
+    private void removeTransactionsOlderThan(final long earliestEpochSecond) {
         final var itr = submittedTxns.iterator();
         while (itr.hasNext()) {
             final var txId = itr.next();
