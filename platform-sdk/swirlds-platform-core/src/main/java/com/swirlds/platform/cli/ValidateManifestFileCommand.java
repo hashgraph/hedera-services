03cbb7c63a8744bdba582ec2d214f3d06f0a9266
Lazar Petrovic <lpetrovic05@gmail.com>
2023-05-24 14:55:50 +0200

    Update platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/cli/ValidateManifestFileCommand.java
    Co-authored-by: Cody Littley <56973212+cody-littley@users.noreply.github.com>
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/cli/ValidateManifestFileCommand.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/cli/ValidateManifestFileCommand.java
@@ -58,7 +58,7 @@ public class ValidateManifestFileCommand extends AbstractCommand {
         validateFieldExists(file.recovery().boostrap().timestamp(), "recovery->boostrap->timestamp");
         validateFieldExists(file.recovery().pkg(), "recovery->package");
         validateFieldExists(file.recovery().pkg().locations(), "recovery->package->locations");
-        if (file.recovery().pkg().locations().size() == 0) {
+        if (file.recovery().pkg().locations().isEmpty()) {
             throw new IOException(
                     "The file should have at least one location in the recovery->package->locations field");
         }
