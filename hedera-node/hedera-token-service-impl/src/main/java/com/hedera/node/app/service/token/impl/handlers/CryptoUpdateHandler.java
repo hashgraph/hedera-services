66d8bd571158115e1cbfbf77276731f0a10729bd
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-05-23 23:35:04 -0500

    midway
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoUpdateHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoUpdateHandler.java
@@ -16,15 +16,16 @@
 
 package com.hedera.node.app.service.token.impl.handlers;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.ACCOUNT_EXPIRED_AND_PENDING_REMOVAL;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.EXISTING_AUTOMATIC_ASSOCIATIONS_EXCEED_GIVEN_LIMIT;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.EXPIRATION_REDUCTION_NOT_ALLOWED;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_ACCOUNT_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.PROXY_ACCOUNT_ID_FIELD_IS_DEPRECATED;
-import static com.hedera.node.app.service.evm.utils.ValidationUtils.validateFalse;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT;
+import static com.hedera.node.app.spi.validation.ExpiryMeta.NA;
+import static com.hedera.node.app.spi.workflows.HandleException.validateFalse;
 import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.ACCOUNT_EXPIRED_AND_PENDING_REMOVAL;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.EXISTING_AUTOMATIC_ASSOCIATIONS_EXCEED_GIVEN_LIMIT;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.EXPIRATION_REDUCTION_NOT_ALLOWED;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_EXPIRATION_TIME;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT;
+
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.node.base.AccountID;
@@ -33,10 +34,10 @@ import com.hedera.hapi.node.state.token.Account;
 import com.hedera.hapi.node.state.token.Account.Builder;
 import com.hedera.hapi.node.token.CryptoUpdateTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
-import com.hedera.node.app.service.mono.ledger.properties.AccountProperty;
 import com.hedera.node.app.service.token.CryptoSignatureWaivers;
 import com.hedera.node.app.service.token.impl.WritableAccountStore;
 import com.hedera.node.app.spi.meta.HandleContext;
+import com.hedera.node.app.spi.validation.ExpiryMeta;
 import com.hedera.node.app.spi.workflows.PreCheckException;
 import com.hedera.node.app.spi.workflows.PreHandleContext;
 import com.hedera.node.app.spi.workflows.TransactionHandler;
@@ -44,7 +45,9 @@ import com.hedera.node.config.data.AutoRenewConfig;
 import com.hedera.node.config.data.EntitiesConfig;
 import com.hedera.node.config.data.LedgerConfig;
 import com.hedera.node.config.data.TokensConfig;
+
 import edu.umd.cs.findbugs.annotations.NonNull;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -101,12 +104,14 @@ public class CryptoUpdateHandler extends BaseCryptoHandler implements Transactio
             @NonNull final WritableAccountStore accountStore) {
         final var op = txn.cryptoUpdateAccount();
         final var target = op.accountIDToUpdateOrThrow();
-        final var builder = asBuilder(op);
 
+        // Customize the account based on fields set in transaction body
+        final var builder = updateBuilder(op);
+
+        // validate all checks that involve config and state
         validateSemantics(context, accountStore, op, builder);
 
         if (context.expiryValidator().isDetached(target)) {
-            // We can only get here if with a valid extension
             builder.expiredAndPendingRemoval(false);
         }
 
@@ -114,9 +119,8 @@ public class CryptoUpdateHandler extends BaseCryptoHandler implements Transactio
         accountStore.put(builder.build());
     }
 
-    private Account.Builder asBuilder(@NonNull final CryptoUpdateTransactionBody op) {
+    private Account.Builder updateBuilder(@NonNull final CryptoUpdateTransactionBody op) {
         final var builder = Account.newBuilder();
-
         if (op.hasKey()) {
             /* Note that {@code this.validateSemantics} will have rejected any txn with an invalid key. */
             builder.key(op.key());
@@ -145,7 +149,6 @@ public class CryptoUpdateHandler extends BaseCryptoHandler implements Transactio
             builder.stakedNumber(getStakedId(op.stakedId().kind().toString(),
                     op.stakedNodeId(), op.stakedAccountId()));
         }
-
         return builder;
     }
 
@@ -156,33 +159,32 @@ public class CryptoUpdateHandler extends BaseCryptoHandler implements Transactio
         final var expiryValidator = context.expiryValidator();
         final var builderAccount = builder.build();
 
+        final var target = op.accountIDToUpdateOrThrow();
+        final var optionalAccount = accountStore.get(target);
+        validateTrue(optionalAccount.isPresent(), INVALID_ACCOUNT_ID);
+        
+        final var updateAccount = optionalAccount.get();
+        validateTrue(!updateAccount.smartContract(), INVALID_ACCOUNT_ID);
+
         final var tokensConfig = context.getConfiguration().getConfigData(TokensConfig.class);
         final var autoRenewConfig = context.getConfiguration().getConfigData(AutoRenewConfig.class);
         final var ledgerConfig = context.getConfiguration().getConfigData(LedgerConfig.class);
         final var entitiesConfig = context.getConfiguration().getConfigData(EntitiesConfig.class);
+        
+        final var currentMetadata = new ExpiryMeta(updateAccount.expiry(), updateAccount.autoRenewSecs(), updateAccount.autoRenewAccountNumber());
+        final var updateMeta = new ExpiryMeta(op.expirationTime().seconds(), op.autoRenewPeriod().seconds(), NA);
+        final var resolvedMeta = context.expiryValidator().resolveUpdateAttempt(currentMetadata, updateMeta);
 
-        if (op.hasExpirationTime() && !context.expiryValidator().resolveUpdateAttempt(op.expirationTime())) {
-            txnCtx.setStatus(INVALID_EXPIRATION_TIME);
-            return;
-        }
-        final var target = op.accountIDToUpdateOrThrow();
-        final var updateAccount = accountStore.get(target);
-        validateTrue(updateAccount.isPresent(), INVALID_ACCOUNT_ID);
-        validateTrue(!updateAccount.get().smartContract(), INVALID_ACCOUNT_ID);
-
-        validateTrue(expiryValidator.isDetached(target) && builderAccount.expiry() != 0
-                , ACCOUNT_EXPIRED_AND_PENDING_REMOVAL);
+        validateTrue(expiryValidator.isDetached(updateAccount, autoRenewConfig.isAutoRenewEnabled(), autoRenewConfig.expireContracts(), autoRenewConfig.expireAccounts()) 
+                        && builderAccount.expiry() != 0, ACCOUNT_EXPIRED_AND_PENDING_REMOVAL);
 
         if (builderAccount.expiry() != 0) {
-            final long newExpiry = (long) changes.get(AccountProperty.EXPIRY);
-            if (newExpiry < ledger.expiry(target)) {
-                return EXPIRATION_REDUCTION_NOT_ALLOWED;
-            }
+            validateFalse(op.expirationTime().seconds() < updateAccount.expiry(), EXPIRATION_REDUCTION_NOT_ALLOWED);
         }
 
         if (builderAccount.maxAutoAssociations() != 0) {
             final long newMax = builderAccount.maxAutoAssociations();
-            validateFalse(newMax < updateAccount.get().usedAutoAssociations(), EXISTING_AUTOMATIC_ASSOCIATIONS_EXCEED_GIVEN_LIMIT);
+            validateFalse(newMax < optionalAccount.get().usedAutoAssociations(), EXISTING_AUTOMATIC_ASSOCIATIONS_EXCEED_GIVEN_LIMIT);
             validateFalse(newMax > ledgerConfig.maxAutoAssociations(), REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT);
             validateFalse(entitiesConfig.limitTokenAssociations() && newMax > tokensConfig.maxPerAccount(),
                     REQUESTED_NUM_AUTOMATIC_ASSOCIATIONS_EXCEEDS_ASSOCIATION_LIMIT);
