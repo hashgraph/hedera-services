51438bb45b667b2e306869a88ab933971361f651
Matt Hess <matt.hess@swirldslabs.com>
2023-05-30 17:16:15 -0600

    Implement semantic validations for token dissociate
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenDissociateFromAccountHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenDissociateFromAccountHandler.java
@@ -18,19 +18,31 @@ package com.hedera.node.app.service.token.impl.handlers;
 
 import static com.hedera.hapi.node.base.ResponseCodeEnum.*;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_ACCOUNT_ID;
+import static com.hedera.node.app.spi.workflows.HandleException.validateFalse;
+import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.HederaFunctionality;
+import com.hedera.hapi.node.base.TokenID;
+import com.hedera.hapi.node.state.token.Account;
+import com.hedera.hapi.node.state.token.Token;
+import com.hedera.hapi.node.state.token.TokenRelation;
 import com.hedera.hapi.node.token.TokenDissociateTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
+import com.hedera.node.app.service.token.ReadableTokenStore;
+import com.hedera.node.app.service.token.impl.WritableAccountStore;
+import com.hedera.node.app.service.token.impl.WritableTokenRelationStore;
 import com.hedera.node.app.service.token.impl.validators.TokenListChecks;
 import com.hedera.node.app.spi.workflows.HandleContext;
-import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.PreCheckException;
 import com.hedera.node.app.spi.workflows.PreHandleContext;
 import com.hedera.node.app.spi.workflows.TransactionHandler;
+import com.hedera.node.config.data.AutoRenewConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -55,9 +67,28 @@ public class TokenDissociateFromAccountHandler implements TransactionHandler {
         context.requireKeyOrThrow(target, INVALID_ACCOUNT_ID);
     }
 
-    @Override
-    public void handle(@NonNull final HandleContext context) throws HandleException {
-        throw new UnsupportedOperationException("Not implemented");
+    /**
+     * This method is called during the handle workflow. It executes the actual transaction.
+     *
+     * <p>
+     * Dissociates a token with an account, preventing various token operations from happening with
+     * respect to the given account
+     *
+     * @throws NullPointerException if one of the arguments is {@code null}
+     */
+    public void handle(@NonNull final HandleContext context) {
+        requireNonNull(context);
+        final var accountStore = context.writableStore(WritableAccountStore.class);
+        final var tokenStore = context.readableStore(ReadableTokenStore.class);
+        final var tokenRelStore = context.writableStore(WritableTokenRelationStore.class);
+        final var txn = context.body();
+        final var op = txn.tokenDissociateOrThrow();
+        final var tokenIds = op.tokensOrThrow();
+        final var autoRenewConfig = context.configuration().getConfigData(AutoRenewConfig.class);
+        final var dissociations = validateSemantics(
+                op.accountOrThrow(), tokenIds, accountStore, tokenStore, tokenRelStore, autoRenewConfig);
+
+        // @todo: finish implementation
     }
 
     @Override
@@ -71,4 +102,54 @@ public class TokenDissociateFromAccountHandler implements TransactionHandler {
             throw new PreCheckException(TOKEN_ID_REPEATED_IN_TOKEN_LIST);
         }
     }
+
+    /**
+     * Performs checks that require state and context
+     */
+    @NonNull
+    private List<Dissociation> validateSemantics(
+            @NonNull final AccountID accountId,
+            @NonNull final List<TokenID> tokenIds,
+            @NonNull final WritableAccountStore accountStore,
+            @NonNull final ReadableTokenStore tokenStore,
+            @NonNull final WritableTokenRelationStore tokenRelStore,
+            @NonNull final AutoRenewConfig autoRenewConfig) {
+        // Check that the account is usable
+        final var acct = ContextualRetriever.getIfUsable(accountId, accountStore, autoRenewConfig);
+
+        // Construct the dissociation for each token ID
+        final var dissociations = new ArrayList<Dissociation>();
+        for (final var tokenId : tokenIds) {
+            final var tokenRel = tokenRelStore.get(accountId, tokenId).orElse(null);
+            validateTrue(tokenRel != null, TOKEN_NOT_ASSOCIATED_TO_ACCOUNT);
+
+            // Here we check/retrieve a token that may not be "usable," but since we are dissociating token relations,
+            // we don't require a usable token (or even an existing token). We only need to update the token relation
+            final var possiblyUnusableToken = tokenStore.get(tokenId);
+            final TokenRelation dissociatedTokenTreasuryRel;
+            if (possiblyUnusableToken != null) {
+                validateFalse(possiblyUnusableToken.paused(), TOKEN_IS_PAUSED);
+                final var tokenTreasuryAcct = AccountID.newBuilder()
+                        .accountNum(possiblyUnusableToken.treasuryAccountNumber())
+                        .build();
+                dissociatedTokenTreasuryRel =
+                        tokenRelStore.get(tokenTreasuryAcct, tokenId).orElse(null);
+            } else {
+                // If the token isn't found, assume the treasury rel is null
+                dissociatedTokenTreasuryRel = null;
+            }
+
+            final Dissociation dissociation =
+                    new Dissociation(possiblyUnusableToken, tokenRel, acct, dissociatedTokenTreasuryRel);
+            dissociations.add(dissociation);
+        }
+
+        return dissociations;
+    }
+
+    private record Dissociation(
+            @Nullable Token token,
+            @NonNull TokenRelation tokenRel,
+            @NonNull Account acct,
+            @Nullable TokenRelation treasuryTokenRel) {}
 }
--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenDissociateFromAccountHandlerTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenDissociateFromAccountHandlerTest.java
@@ -16,8 +16,15 @@
 
 package com.hedera.node.app.service.token.impl.test.handlers;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.ACCOUNT_DELETED;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.ACCOUNT_EXPIRED_AND_PENDING_REMOVAL;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_ACCOUNT_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_ID_REPEATED_IN_TOKEN_LIST;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_IS_PAUSED;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_NOT_ASSOCIATED_TO_ACCOUNT;
+import static com.hedera.node.app.service.mono.pbj.PbjConverter.fromPbj;
+import static com.hedera.node.app.service.token.impl.TokenServiceImpl.TOKENS_KEY;
+import static com.hedera.node.app.service.token.impl.test.handlers.AdapterUtils.mockStates;
 import static com.hedera.node.app.spi.fixtures.Assertions.assertThrowsPreCheck;
 import static com.hedera.node.app.spi.fixtures.workflows.ExceptionConditions.responseCode;
 import static com.hedera.test.factories.scenarios.TokenDissociateScenarios.TOKEN_DISSOCIATE_WITH_CUSTOM_PAYER_PAID_KNOWN_TARGET;
@@ -30,17 +37,37 @@ import static com.hedera.test.factories.scenarios.TxnHandlingScenario.MISC_ACCOU
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
 
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.TokenID;
 import com.hedera.hapi.node.base.TransactionID;
+import com.hedera.hapi.node.state.token.Account;
+import com.hedera.hapi.node.state.token.Token;
+import com.hedera.hapi.node.state.token.TokenRelation;
 import com.hedera.hapi.node.token.TokenDissociateTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
+import com.hedera.node.app.service.mono.utils.EntityNum;
+import com.hedera.node.app.service.token.ReadableTokenStore;
+import com.hedera.node.app.service.token.impl.ReadableTokenStoreImpl;
+import com.hedera.node.app.service.token.impl.WritableAccountStore;
+import com.hedera.node.app.service.token.impl.WritableTokenRelationStore;
 import com.hedera.node.app.service.token.impl.handlers.TokenDissociateFromAccountHandler;
+import com.hedera.node.app.spi.fixtures.state.MapWritableKVState;
 import com.hedera.node.app.spi.fixtures.workflows.FakePreHandleContext;
+import com.hedera.node.app.spi.workflows.HandleContext;
+import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.PreCheckException;
+import com.hedera.node.config.data.AutoRenewConfig;
+import com.swirlds.config.api.Configuration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.assertj.core.api.Assertions;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class TokenDissociateFromAccountHandlerTest extends ParityTestBase {
@@ -51,68 +78,217 @@ class TokenDissociateFromAccountHandlerTest extends ParityTestBase {
 
     private final TokenDissociateFromAccountHandler subject = new TokenDissociateFromAccountHandler();
 
-    @SuppressWarnings("DataFlowIssue")
-    @Test
-    void nullArgsThrows() {
-        assertThatThrownBy(() -> subject.preHandle(null)).isInstanceOf(NullPointerException.class);
+    @Nested
+    class PreHandleTests {
+        @SuppressWarnings("DataFlowIssue")
+        @Test
+        void nullArgsThrows() {
+            assertThatThrownBy(() -> subject.preHandle(null)).isInstanceOf(NullPointerException.class);
+        }
+
+        @Test
+        void pureChecksRejectsDissociateWithMissingAccount() {
+            final var txn = newDissociateTxn(null, List.of(TOKEN_555));
+
+            assertThatThrownBy(() -> subject.pureChecks(txn))
+                    .isInstanceOf(PreCheckException.class)
+                    .has(responseCode(INVALID_ACCOUNT_ID));
+        }
+
+        @Test
+        void pureChecksRejectsDissociateWithRepeatedTokenId() {
+            final var txn = newDissociateTxn(ACCOUNT_1339, List.of(TOKEN_555, TOKEN_666, TOKEN_555));
+
+            assertThatThrownBy(() -> subject.pureChecks(txn))
+                    .isInstanceOf(PreCheckException.class)
+                    .has(responseCode(TOKEN_ID_REPEATED_IN_TOKEN_LIST));
+        }
+
+        @Test
+        void tokenDissociateWithKnownTargetScenario() throws PreCheckException {
+            final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_KNOWN_TARGET);
+
+            final var context = new FakePreHandleContext(readableAccountStore, theTxn);
+            subject.preHandle(context);
+
+            assertEquals(1, context.requiredNonPayerKeys().size());
+            assertThat(context.requiredNonPayerKeys(), Matchers.contains(MISC_ACCOUNT_KT.asPbjKey()));
+        }
+
+        @Test
+        void tokenDissociateWithSelfPaidKnownTargetScenario() throws PreCheckException {
+            final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_SELF_PAID_KNOWN_TARGET);
+
+            final var context = new FakePreHandleContext(readableAccountStore, theTxn);
+            subject.preHandle(context);
+
+            assertEquals(0, context.requiredNonPayerKeys().size());
+        }
+
+        @Test
+        void tokenDissociateWithCustomPaidKnownTargetScenario() throws PreCheckException {
+            final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_CUSTOM_PAYER_PAID_KNOWN_TARGET);
+
+            final var context = new FakePreHandleContext(readableAccountStore, theTxn);
+            subject.preHandle(context);
+
+            assertEquals(1, context.requiredNonPayerKeys().size());
+            assertThat(context.requiredNonPayerKeys(), Matchers.contains(CUSTOM_PAYER_ACCOUNT_KT.asPbjKey()));
+        }
+
+        @Test
+        void tokenDissociateWithMissingTargetScenario() throws PreCheckException {
+            final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_MISSING_TARGET);
+
+            final var context = new FakePreHandleContext(readableAccountStore, theTxn);
+            assertThrowsPreCheck(() -> subject.preHandle(context), INVALID_ACCOUNT_ID);
+        }
     }
 
-    @Test
-    void pureChecksRejectsDissociateWithMissingAccount() {
-        final var txn = newDissociateTxn(null, List.of(TOKEN_555));
+    @Nested
+    class HandleTests {
+        @Test
+        void rejectsNonexistingAccount() {
+            final var context = mockContext();
+            final var txn = newDissociateTxn(AccountID.newBuilder().build(), List.of(TOKEN_555));
+            given(context.body()).willReturn(txn);
 
-        assertThatThrownBy(() -> subject.pureChecks(txn))
-                .isInstanceOf(PreCheckException.class)
-                .has(responseCode(INVALID_ACCOUNT_ID));
+            assertThatThrownBy(() -> subject.handle(context))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(INVALID_ACCOUNT_ID));
+        }
+
+        @Test
+        void rejectsExpiredAccount() {
+            // Create and commit an account that is expired
+            final var accountNumber = 12345L;
+            writableAccountStore.put(Account.newBuilder()
+                    .accountNumber(accountNumber)
+                    .expiredAndPendingRemoval(true)
+                    .deleted(false)
+                    .build());
+            writableAccountStore.commit();
+
+            // Create the context and transaction
+            final var context = mockContext();
+            final var txn = newDissociateTxn(
+                    AccountID.newBuilder().accountNum(accountNumber).build(), List.of(TOKEN_555));
+            given(context.body()).willReturn(txn);
+
+            Assertions.assertThatThrownBy(() -> subject.handle(context))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(ACCOUNT_EXPIRED_AND_PENDING_REMOVAL));
+        }
+
+        @Test
+        void rejectsDeletedAccount() {
+            // Create and commit an account that is deleted
+            final var accountNumber = 53135;
+            writableAccountStore.put(Account.newBuilder()
+                    .accountNumber(accountNumber)
+                    .expiredAndPendingRemoval(false)
+                    .deleted(true)
+                    .build());
+            writableAccountStore.commit();
+
+            // Create the context and transaction
+            final var context = mockContext();
+            final var txn = newDissociateTxn(
+                    AccountID.newBuilder().accountNum(accountNumber).build(), List.of(TOKEN_555));
+            given(context.body()).willReturn(txn);
+
+            Assertions.assertThatThrownBy(() -> subject.handle(context))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(ACCOUNT_DELETED));
+        }
+
+        @Test
+        void rejectsNonexistingTokenRel() {
+            final var context = mockContext();
+            final var txn = newDissociateTxn(ACCOUNT_1339, List.of(TOKEN_555));
+            given(context.body()).willReturn(txn);
+
+            Assertions.assertThatThrownBy(() -> subject.handle(context))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(TOKEN_NOT_ASSOCIATED_TO_ACCOUNT));
+        }
+
+        @Test
+        void rejectsPausedToken() {
+            // Create a readable store with a paused token
+            final var pausedToken = Token.newBuilder()
+                    .tokenNumber(TOKEN_555.tokenNum())
+                    .paused(true)
+                    .build();
+            readableTokenStore = newReadableStoreWithTokens(pausedToken);
+
+            // Create the token rel for the paused token
+            writableTokenRelStore.put(TokenRelation.newBuilder()
+                    .accountNumber(ACCOUNT_1339.accountNumOrThrow())
+                    .tokenNumber(TOKEN_555.tokenNum())
+                    .build());
+            writableTokenRelStore.commit();
+
+            // Create the context and transaction
+            final var context = mockContext();
+            final var txn = newDissociateTxn(ACCOUNT_1339, List.of(TOKEN_555));
+            given(context.body()).willReturn(txn);
+
+            Assertions.assertThatThrownBy(() -> subject.handle(context))
+                    .isInstanceOf(HandleException.class)
+                    .has(responseCode(TOKEN_IS_PAUSED));
+        }
+
+        @Test // @todo: implement
+        void acceptsTokenWithTreasury() {}
+
+        @Test // @todo: implement
+        void acceptsTokenWithoutTreasury() {}
+
+        @Test // @todo: implement
+        void acceptsDeletedToken() {}
+
+        private ReadableTokenStore newReadableStoreWithTokens(Token... tokens) {
+            final var backingMap = new HashMap<EntityNum, Token>();
+            for (final Token token : tokens) {
+                backingMap.put(
+                        EntityNum.fromTokenId(fromPbj(TokenID.newBuilder()
+                                .tokenNum(token.tokenNumber())
+                                .build())),
+                        token);
+            }
+
+            final var wrappingState = new MapWritableKVState<>(TOKENS_KEY, backingMap);
+            return new ReadableTokenStoreImpl(mockStates(Map.of(TOKENS_KEY, wrappingState)));
+        }
     }
 
-    @Test
-    void pureChecksRejectsDissociateWithRepeatedTokenId() {
-        final var txn = newDissociateTxn(ACCOUNT_1339, List.of(TOKEN_555, TOKEN_666, TOKEN_555));
-
-        assertThatThrownBy(() -> subject.pureChecks(txn))
-                .isInstanceOf(PreCheckException.class)
-                .has(responseCode(TOKEN_ID_REPEATED_IN_TOKEN_LIST));
+    private HandleContext mockContext() {
+        return mockContext(true, true);
     }
 
-    @Test
-    void tokenDissociateWithKnownTargetScenario() throws PreCheckException {
-        final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_KNOWN_TARGET);
+    private HandleContext mockContext(final boolean renewAccounts, final boolean renewContracts) {
+        final var handleContext = mock(HandleContext.class);
+        mockConfig(handleContext, renewAccounts, renewContracts);
 
-        final var context = new FakePreHandleContext(readableAccountStore, theTxn);
-        subject.preHandle(context);
+        lenient().when(handleContext.writableStore(WritableAccountStore.class)).thenReturn(writableAccountStore);
+        lenient().when(handleContext.readableStore(ReadableTokenStore.class)).thenReturn(readableTokenStore);
+        lenient()
+                .when(handleContext.writableStore(WritableTokenRelationStore.class))
+                .thenReturn(writableTokenRelStore);
 
-        assertEquals(1, context.requiredNonPayerKeys().size());
-        assertThat(context.requiredNonPayerKeys(), Matchers.contains(MISC_ACCOUNT_KT.asPbjKey()));
+        return handleContext;
     }
 
-    @Test
-    void tokenDissociateWithSelfPaidKnownTargetScenario() throws PreCheckException {
-        final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_SELF_PAID_KNOWN_TARGET);
+    private void mockConfig(
+            final HandleContext mockedContext, final boolean renewAccounts, final boolean renewContracts) {
+        final var config = mock(Configuration.class);
+        lenient().when(mockedContext.configuration()).thenReturn(config);
 
-        final var context = new FakePreHandleContext(readableAccountStore, theTxn);
-        subject.preHandle(context);
-
-        assertEquals(0, context.requiredNonPayerKeys().size());
-    }
-
-    @Test
-    void tokenDissociateWithCustomPaidKnownTargetScenario() throws PreCheckException {
-        final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_CUSTOM_PAYER_PAID_KNOWN_TARGET);
-
-        final var context = new FakePreHandleContext(readableAccountStore, theTxn);
-        subject.preHandle(context);
-
-        assertEquals(1, context.requiredNonPayerKeys().size());
-        assertThat(context.requiredNonPayerKeys(), Matchers.contains(CUSTOM_PAYER_ACCOUNT_KT.asPbjKey()));
-    }
-
-    @Test
-    void tokenDissociateWithMissingTargetScenario() throws PreCheckException {
-        final var theTxn = txnFrom(TOKEN_DISSOCIATE_WITH_MISSING_TARGET);
-
-        final var context = new FakePreHandleContext(readableAccountStore, theTxn);
-        assertThrowsPreCheck(() -> subject.preHandle(context), INVALID_ACCOUNT_ID);
+        final var autoRenewConfig = mock(AutoRenewConfig.class);
+        lenient().when(autoRenewConfig.shouldAutoRenewAccounts()).thenReturn(renewAccounts);
+        lenient().when(autoRenewConfig.shouldAutoRenewContracts()).thenReturn(renewContracts);
+        lenient().when(config.getConfigData(AutoRenewConfig.class)).thenReturn(autoRenewConfig);
     }
 
     private TransactionBody newDissociateTxn(AccountID account, List<TokenID> tokens) {
