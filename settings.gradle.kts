8f385c364d76b62cc54f9dc6599e441b59cab67a
Michael Tinker <michael.tinker@swirldslabs.com>
2023-06-01 14:33:58 -0500

    Update HAPI version (#6884)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -114,7 +114,7 @@ dependencyResolutionManagement {
     // runtime.
     create("libs") {
       // The HAPI API version to use, this need to match the tag set on gitRepositories above
-      version("hapi-version", "0.40.0-alpha.0-SNAPSHOT")
+      version("hapi-version", "0.40.0-contract-nft-state-SNAPSHOT")
 
       // Definition of version numbers for all libraries
       version("pbj-version", "0.6.0")
