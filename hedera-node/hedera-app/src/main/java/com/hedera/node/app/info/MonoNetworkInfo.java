02a67d9402e3128c800c72344546b545aafc96a6
Kim Rader <kim.rader@swirldslabs.com>
2023-06-04 21:09:52 -0700

    Spotless
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/info/MonoNetworkInfo.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/info/MonoNetworkInfo.java
@@ -36,7 +36,7 @@ public class MonoNetworkInfo implements NetworkInfo {
     /* From https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string */
     private static final Pattern SEMVER_SPEC_REGEX = Pattern.compile(
             "^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)"
-                        + "(?:\\."
+                    + "(?:\\."
                     + "(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)"
                     + "*))?$");
 
