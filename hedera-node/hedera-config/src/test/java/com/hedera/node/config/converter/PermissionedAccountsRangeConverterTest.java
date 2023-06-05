52bd6d3a9e5d543521bf87fe7c9c2fdadf2dbfcc
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-06-05 16:50:58 +0200

    based on review
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- hedera-node/hedera-config/src/test/java/com/hedera/node/config/converter/PermissionedAccountsRangeConverterTest.java
+++ hedera-node/hedera-config/src/test/java/com/hedera/node/config/converter/PermissionedAccountsRangeConverterTest.java
@@ -19,8 +19,11 @@ package com.hedera.node.config.converter;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.hedera.node.app.service.mono.context.domain.security.PermissionedAccountsRange;
+import java.util.stream.Stream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 final class PermissionedAccountsRangeConverterTest {
 
@@ -33,7 +36,8 @@ final class PermissionedAccountsRangeConverterTest {
         assertThatThrownBy(() -> converter.convert(null)).isInstanceOf(NullPointerException.class);
     }
 
-    @Test
+    @ParameterizedTest
+    @MethodSource("invalidDataProvider")
     void testInvalidParam() {
         // given
         final PermissionedAccountsRangeConverter converter = new PermissionedAccountsRangeConverter();
@@ -42,17 +46,35 @@ final class PermissionedAccountsRangeConverterTest {
         assertThatThrownBy(() -> converter.convert("null")).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void testValidParam() {
+    @ParameterizedTest
+    @MethodSource("validDataProvider")
+    void testValidParam(final TestDataTuple testDataTuple) {
         // given
         final PermissionedAccountsRangeConverter converter = new PermissionedAccountsRangeConverter();
 
         // when
-        final PermissionedAccountsRange converted = converter.convert("1-14");
+        final PermissionedAccountsRange converted = converter.convert(testDataTuple.input());
 
         // then
         Assertions.assertThat(converted).isNotNull();
-        Assertions.assertThat(converted).extracting(c -> c.from()).isEqualTo(1L);
-        Assertions.assertThat(converted).extracting(c -> c.inclusiveTo()).isEqualTo(14L);
+        Assertions.assertThat(converted)
+                .extracting(c -> c.from())
+                .isEqualTo(testDataTuple.output().from());
+        Assertions.assertThat(converted)
+                .extracting(c -> c.inclusiveTo())
+                .isEqualTo(testDataTuple.output().inclusiveTo());
+    }
+
+    final record TestDataTuple(String input, PermissionedAccountsRange output) {}
+
+    static Stream<TestDataTuple> validDataProvider() {
+        return Stream.of(
+                new TestDataTuple("1-14", new PermissionedAccountsRange(1L, 14L)),
+                new TestDataTuple("1-*", new PermissionedAccountsRange(1L, Long.MAX_VALUE)),
+                new TestDataTuple("10-*", new PermissionedAccountsRange(10L, Long.MAX_VALUE)));
+    }
+
+    static Stream<String> invalidDataProvider() {
+        return Stream.of("10-1", "*-1", "*-10", "1", "-1", "-1-10", "null");
     }
 }
