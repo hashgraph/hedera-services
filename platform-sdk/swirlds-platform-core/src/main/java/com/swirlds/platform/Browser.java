5abf21aabab3262b10d0fc03592cd71b58680c5a
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-06-10 21:16:43 -0500

    save

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/Browser.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/Browser.java
@@ -564,7 +564,7 @@ public class Browser {
             }
         logger.info(STARTUP.getMarker(), "Starting thread dump generator in {}", dir);
             ThreadDumpGenerator.generateThreadDumpAtIntervals(
-                    dir, 100);
+                    dir, 40);
         //}
     }
 
