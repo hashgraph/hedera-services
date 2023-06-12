3cb50d280aba7ca4f9de7295394d5d8c0e5ebc15
Austin Littley <austin@swirldslabs.com>
2023-06-12 13:49:28 -0400

    Write tests for ACTIVE status
    Signed-off-by: Austin Littley <austin@swirldslabs.com>

---
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/system/platformstatus/statuslogic/ActiveStatusLogicTests.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.swirlds.common.system.platformstatus.statuslogic;
+
+import static com.swirlds.common.system.platformstatus.statuslogic.StatusLogicTestUtils.triggerActionAndAssertException;
+import static com.swirlds.common.system.platformstatus.statuslogic.StatusLogicTestUtils.triggerActionAndAssertNoTransition;
+import static com.swirlds.common.system.platformstatus.statuslogic.StatusLogicTestUtils.triggerActionAndAssertTransition;
+
+import com.swirlds.common.system.platformstatus.PlatformStatus;
+import com.swirlds.common.system.platformstatus.PlatformStatusAction;
+import com.swirlds.common.system.platformstatus.PlatformStatusConfig;
+import com.swirlds.common.test.fixtures.FakeTime;
+import com.swirlds.config.api.Configuration;
+import com.swirlds.test.framework.config.TestConfigBuilder;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ActiveStatusLogicTests {
+    private FakeTime time;
+    private ActiveStatusLogic logic;
+
+    @BeforeEach
+    void setup() {
+        time = new FakeTime();
+        Configuration configuration = new TestConfigBuilder()
+                .withValue("platformStatus.activeStatusDelay", "5s")
+                .getOrCreateConfig();
+        logic = new ActiveStatusLogic(time, configuration.getConfigData(PlatformStatusConfig.class));
+    }
+
+    @Test
+    @DisplayName("Go from ACTIVE to FREEZING")
+    void activeToFreezing() {
+        triggerActionAndAssertTransition(logic, PlatformStatusAction.FREEZE_PERIOD_ENTERED, PlatformStatus.FREEZING);
+    }
+
+    @Test
+    @DisplayName("Go from ACTIVE to CHECKING")
+    void activeToChecking() {
+        triggerActionAndAssertNoTransition(logic, PlatformStatusAction.TIME_ELAPSED);
+
+        time.tick(Duration.ofSeconds(2));
+        triggerActionAndAssertNoTransition(logic, PlatformStatusAction.TIME_ELAPSED);
+
+        // restart the timer that will trigger the status change to checking
+        triggerActionAndAssertNoTransition(logic, PlatformStatusAction.OWN_EVENT_REACHED_CONSENSUS);
+
+        // if the own event reaching consensus successfully restarted the timer, then the status should still be active
+        time.tick(Duration.ofSeconds(4));
+        triggerActionAndAssertNoTransition(logic, PlatformStatusAction.TIME_ELAPSED);
+
+        time.tick(Duration.ofSeconds(2));
+        triggerActionAndAssertTransition(logic, PlatformStatusAction.TIME_ELAPSED, PlatformStatus.CHECKING);
+    }
+
+    @Test
+    @DisplayName("Go from ACTIVE to BEHIND")
+    void activeToBehind() {
+        triggerActionAndAssertTransition(logic, PlatformStatusAction.FALLEN_BEHIND, PlatformStatus.BEHIND);
+    }
+
+    @Test
+    @DisplayName("Go from ACTIVE to CATASTROPHIC_FAILURE")
+    void activeToCatastrophicFailure() {
+        triggerActionAndAssertTransition(
+                logic, PlatformStatusAction.CATASTROPHIC_FAILURE, PlatformStatus.CATASTROPHIC_FAILURE);
+    }
+
+    @Test
+    @DisplayName("Irrelevant actions shouldn't cause transitions")
+    void irrelevantActions() {
+        triggerActionAndAssertNoTransition(logic, PlatformStatusAction.STATE_WRITTEN_TO_DISK);
+    }
+
+    @Test
+    @DisplayName("Unexpected actions should cause exceptions")
+    void unexpectedActions() {
+        triggerActionAndAssertException(logic, PlatformStatusAction.RECONNECT_COMPLETE);
+        triggerActionAndAssertException(logic, PlatformStatusAction.DONE_REPLAYING_EVENTS);
+        triggerActionAndAssertException(logic, PlatformStatusAction.STARTED_REPLAYING_EVENTS);
+    }
+
+    //    STARTED_REPLAYING_EVENTS
+    //    DONE_REPLAYING_EVENTS         X
+    //    OWN_EVENT_REACHED_CONSENSUS   X
+    //    FREEZE_PERIOD_ENTERED         X
+    //    FALLEN_BEHIND                 X
+    //    RECONNECT_COMPLETE            X
+    //    STATE_WRITTEN_TO_DISK         X
+    //    CATASTROPHIC_FAILURE          X
+    //    TIME_ELAPSED                  X
+
+    //    STARTED_REPLAYING_EVENTS
+    //    DONE_REPLAYING_EVENTS
+    //    OWN_EVENT_REACHED_CONSENSUS
+    //    FREEZE_PERIOD_ENTERED
+    //    FALLEN_BEHIND
+    //    RECONNECT_COMPLETE
+    //    STATE_WRITTEN_TO_DISK
+    //    CATASTROPHIC_FAILURE
+    //    TIME_ELAPSED
+}
---
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/system/platformstatus/statuslogic/StatusLogicTestUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.swirlds.common.system.platformstatus.statuslogic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.swirlds.common.system.platformstatus.PlatformStatus;
+import com.swirlds.common.system.platformstatus.PlatformStatusAction;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Utility methods for testing {@link PlatformStatusLogic} implementations.
+ */
+public class StatusLogicTestUtils {
+    /**
+     * Hidden constructor
+     */
+    private StatusLogicTestUtils() {}
+
+    /**
+     * Trigger an action and assert that the new status is as expected.
+     *
+     * @param logic          the logic to test
+     * @param action         the action to trigger
+     * @param expectedStatus the expected status after the action is triggered
+     */
+    public static void triggerActionAndAssertTransition(
+            @NonNull final PlatformStatusLogic logic,
+            @NonNull final PlatformStatusAction action,
+            @NonNull final PlatformStatus expectedStatus) {
+
+        final PlatformStatus newStatus = logic.processStatusAction(action);
+        assertEquals(expectedStatus, newStatus);
+    }
+
+    /**
+     * Trigger an action and assert that the status does not change.
+     *
+     * @param logic  the logic to test
+     * @param action the action to trigger
+     */
+    public static void triggerActionAndAssertNoTransition(
+            @NonNull final PlatformStatusLogic logic, @NonNull final PlatformStatusAction action) {
+
+        final PlatformStatus originalStatus = logic.getStatus();
+        final PlatformStatus newStatus = logic.processStatusAction(action);
+
+        assertEquals(originalStatus, newStatus);
+    }
+
+    /**
+     * Trigger an action and assert that an exception is thrown.
+     *
+     * @param logic  the logic to test
+     * @param action the action to trigger
+     */
+    public static void triggerActionAndAssertException(
+            @NonNull final PlatformStatusLogic logic, @NonNull final PlatformStatusAction action) {
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            logic.processStatusAction(action);
+        });
+    }
+}
