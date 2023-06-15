5ee51ddee3f5874eb064fe04d76174687f2965cc
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-06-15 09:28:09 +0200

    Objects.requireNonNull used
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-fchashmap/src/main/java/com/swirlds/fchashmap/FCHashMap.java
+++ platform-sdk/swirlds-fchashmap/src/main/java/com/swirlds/fchashmap/FCHashMap.java
@@ -16,14 +16,14 @@
 
 package com.swirlds.fchashmap;
 
-import static com.swirlds.base.ArgumentUtils.throwArgNull;
-
 import com.swirlds.common.FastCopyable;
 import com.swirlds.common.exceptions.ReferenceCountException;
 import com.swirlds.fchashmap.internal.FCHashMapEntrySet;
 import com.swirlds.fchashmap.internal.FCHashMapFamily;
 import com.swirlds.fchashmap.internal.Mutation;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.AbstractMap;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -286,13 +286,12 @@ public class FCHashMap<K, V> extends AbstractMap<K, V> implements FastCopyable {
     /**
      * {@inheritDoc}
      *
-     * @throws NullPointerException
-     * 		if the key or value is null
+     * @throws NullPointerException if the key or value is null
      */
     @Override
-    public V put(final K key, final V value) {
-        throwArgNull(key, "key");
-        throwArgNull(value, "value");
+    public V put(@NonNull final K key, @NonNull final V value) {
+        Objects.requireNonNull(key, "key must not be null");
+        Objects.requireNonNull(value, "value must not be null");
         throwIfImmutable();
         return family.mutate(key, value, size);
     }
@@ -302,8 +301,8 @@ public class FCHashMap<K, V> extends AbstractMap<K, V> implements FastCopyable {
      */
     @SuppressWarnings("unchecked")
     @Override
-    public V remove(final Object key) {
-        throwArgNull(key, "key");
+    public V remove(@NonNull final Object key) {
+        Objects.requireNonNull(key, "key must not be null");
         throwIfImmutable();
         return family.mutate((K) key, null, size);
     }
