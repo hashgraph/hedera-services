f8131fcbf029d9f42f29adfaa32a4a0fd82b13a9
Michael Heinrichs <netopyr@users.noreply.github.com>
2023-06-19 11:21:52 +0200

    Set default timeout to 20 sec.
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/HederaConfig.java
+++ hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/HederaConfig.java
@@ -55,7 +55,7 @@ public record HederaConfig(
         @ConfigProperty(value = "recordStream.queueCapacity", defaultValue = "5000") int recordStreamQueueCapacity,
         @ConfigProperty(value = "recordStream.logEveryTransaction", defaultValue = "false")
                 boolean recordStreamLogEveryTransaction,
-        @ConfigProperty(value = "workflow.verificationTimeoutMS", defaultValue = "3000")
+        @ConfigProperty(value = "workflow.verificationTimeoutMS", defaultValue = "20000")
                 long workflowVerificationTimeoutMS
         // @ConfigProperty("workflows.enabled", defaultValue = "") Set<HederaFunctionality> workflowsEnabled
         ) {}
