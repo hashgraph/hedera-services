911234bcab679754323d0e29c4746a715538ae72
Richard Bair <rbair23@users.noreply.github.com>
2023-06-20 13:59:16 -0700

    Start TLS and non-TLS servers (#7073)
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/NettyConfig.java
+++ hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/NettyConfig.java
@@ -19,6 +19,21 @@ package com.hedera.node.config.data;
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
 
+/**
+ *
+ * @param prodFlowControlWindow
+ * @param prodMaxConcurrentCalls
+ * @param prodMaxConnectionAge
+ * @param prodMaxConnectionAgeGrace
+ * @param prodMaxConnectionIdle
+ * @param prodKeepAliveTime
+ * @param prodKeepAliveTimeout
+ * @param startRetries The number of times to retry starting the gRPC servers, if they fail to start. Defaults to 90.
+ * @param startRetryIntervalMs The number of milliseconds between retries. Defaults to 1000ms. Minimum value is 1.
+ * @param terminationTimeout The timeout, *in seconds*, to wait for the servers to terminate.
+ * @param tlsCrtPath
+ * @param tlsKeyPath
+ */
 @ConfigData("netty")
 public record NettyConfig(
         // @ConfigProperty(defaultValue = "PROD") Profile mode,
@@ -31,5 +46,20 @@ public record NettyConfig(
         @ConfigProperty(value = "prod.keepAliveTimeout", defaultValue = "3") long prodKeepAliveTimeout,
         @ConfigProperty(defaultValue = "90") int startRetries,
         @ConfigProperty(defaultValue = "1000") long startRetryIntervalMs,
+        @ConfigProperty(defaultValue = "5") long terminationTimeout,
         @ConfigProperty(value = "tlsCrt.path", defaultValue = "hedera.crt") String tlsCrtPath,
-        @ConfigProperty(value = "tlsKey.path", defaultValue = "hedera.key") String tlsKeyPath) {}
+        @ConfigProperty(value = "tlsKey.path", defaultValue = "hedera.key") String tlsKeyPath) {
+    public NettyConfig {
+        if (startRetries < 0) {
+            throw new IllegalArgumentException("startRetries must be non-negative.");
+        }
+
+        if (startRetryIntervalMs < 1) {
+            throw new IllegalArgumentException("startRetryIntervalMs cannot be less than 1ms");
+        }
+
+        if (terminationTimeout < 0) {
+            throw new IllegalArgumentException("terminationTimeout must be non-negative");
+        }
+    }
+}
