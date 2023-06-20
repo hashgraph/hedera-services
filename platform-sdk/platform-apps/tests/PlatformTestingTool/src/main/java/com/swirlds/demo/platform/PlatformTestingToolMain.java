867ea8dd6770436d97fdaeabb9e7b47b9e4e9d69
Ivan Malygin <ivan@swirldslabs.com>
2023-06-20 17:08:17 -0400

    Spotless
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/com/swirlds/demo/platform/PlatformTestingToolMain.java
+++ platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/com/swirlds/demo/platform/PlatformTestingToolMain.java
@@ -557,7 +557,8 @@ public class PlatformTestingToolMain implements SwirldMain {
             // Parameters[1]: JSON file for app client (parsed below), optional
 
             // Parameters[2]: indicates whether to use MerkleDb (true) or JasperDB (false) for virtual maps, optional
-            final boolean useMerkleDb = parameters != null && parameters.length > 2 &&  Boolean.parseBoolean(parameters[2]);
+            final boolean useMerkleDb =
+                    parameters != null && parameters.length > 2 && Boolean.parseBoolean(parameters[2]);
             logger.info(LOGM_DEMO_INFO, "Using {} data sources", (useMerkleDb ? "MerkleDb" : "JasperDB"));
 
             final ProgressCfg progressCfg = new ProgressCfg();
