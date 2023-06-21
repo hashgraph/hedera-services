97719e47da704be6e3a0fb3e2c0393ee28bc8c25
Matt Hess <matt.hess@swirldslabs.com>
2023-06-21 14:49:24 -0600

    Fix broken test
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/query/QueryComponentTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/query/QueryComponentTest.java
@@ -26,6 +26,13 @@ import com.hedera.node.app.DaggerHederaInjectionComponent;
 import com.hedera.node.app.HederaInjectionComponent;
 import com.hedera.node.app.components.QueryInjectionComponent;
 import com.hedera.node.app.service.mono.context.properties.BootstrapProperties;
+import com.hedera.node.app.spi.fixtures.state.MapWritableKVState;
+import com.hedera.node.app.spi.state.ReadableStates;
+import com.hedera.node.app.spi.state.WritableKVState;
+import com.hedera.node.app.spi.state.WritableQueueState;
+import com.hedera.node.app.spi.state.WritableSingletonState;
+import com.hedera.node.app.spi.state.WritableStates;
+import com.hedera.node.app.state.HederaState;
 import com.hedera.node.config.testfixtures.HederaTestConfigBuilder;
 import com.swirlds.common.context.PlatformContext;
 import com.swirlds.common.crypto.Cryptography;
@@ -34,8 +41,15 @@ import com.swirlds.common.crypto.Hash;
 import com.swirlds.common.system.InitTrigger;
 import com.swirlds.common.system.NodeId;
 import com.swirlds.common.system.Platform;
+import com.swirlds.common.utility.EmptyIterator;
 import com.swirlds.config.api.Configuration;
 import com.swirlds.platform.gui.SwirldsGui;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.function.Predicate;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -75,6 +89,10 @@ class QueryComponentTest {
                 .maxSignedTxnSize(1024)
                 .genesisUsage(false)
                 .build();
+
+        // Simulate an empty (but iterable) state for the record cache
+        final var emptyIterableQueueState = new EmptyIterableQueueState(new EmptyIterableQueueWritableStates());
+        app.workingStateAccessor().setHederaState(emptyIterableQueueState);
     }
 
     @Test
@@ -85,5 +103,107 @@ class QueryComponentTest {
                 app.queryComponentFactory().get().create();
 
         assertNotNull(subject.queryWorkflow());
+    }
+
+    // The following classes only exist to load enough of an empty state, such that the graph roots can be instantiated
+    // for the test
+    private record EmptyIterableQueueState(WritableStates writableStates) implements HederaState {
+        @NotNull
+        @Override
+        public ReadableStates createReadableStates(@NotNull String serviceName) {
+            return writableStates;
+        }
+
+        @NotNull
+        @Override
+        public WritableStates createWritableStates(@NotNull String serviceName) {
+            return writableStates;
+        }
+    }
+
+    private static final class EmptyIterableQueueWritableStates implements WritableStates {
+        @NotNull
+        @Override
+        public <K, V> WritableKVState<K, V> get(@NotNull String stateKey) {
+            return new MapWritableKVState<>(stateKey, Collections.emptyMap());
+        }
+
+        @NotNull
+        @Override
+        public <T> WritableSingletonState<T> getSingleton(@NotNull String stateKey) {
+            return new WritableSingletonState<T>() {
+                @Override
+                public void put(@Nullable T value) {}
+
+                @Override
+                public boolean isModified() {
+                    return false;
+                }
+
+                @NotNull
+                @Override
+                public String getStateKey() {
+                    return "BOGUS STATE KEY";
+                }
+
+                @Nullable
+                @Override
+                public T get() {
+                    return null;
+                }
+
+                @Override
+                public boolean isRead() {
+                    return false;
+                }
+            };
+        }
+
+        @NotNull
+        @Override
+        public <E> WritableQueueState<E> getQueue(@NotNull String stateKey) {
+            return (WritableQueueState<E>) new EmptyWritableQueueState();
+        }
+
+        @Override
+        public boolean contains(@NotNull String stateKey) {
+            return false;
+        }
+
+        @NotNull
+        @Override
+        public Set<String> stateKeys() {
+            return Set.of();
+        }
+    }
+
+    private static final class EmptyWritableQueueState implements WritableQueueState<Object> {
+
+        @NotNull
+        @Override
+        public String getStateKey() {
+            return "BOGUS STATE KEY";
+        }
+
+        @Nullable
+        @Override
+        public Object peek() {
+            return null;
+        }
+
+        @NotNull
+        @Override
+        public Iterator<Object> iterator() {
+            return new EmptyIterator<>();
+        }
+
+        @Override
+        public void add(@NotNull Object element) {}
+
+        @Nullable
+        @Override
+        public Object removeIf(@NotNull Predicate predicate) {
+            return null;
+        }
     }
 }
