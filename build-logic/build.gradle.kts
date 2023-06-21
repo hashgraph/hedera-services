5f564ae486ec1c4b8fdd571ed41c1f6a56a60980
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-06-21 16:12:49 +0200

    Introduce Platform (BOM) for version management (#6965)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Co-authored-by: Nathan Klick <nathan@swirldslabs.com>

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -35,6 +35,6 @@ dependencies {
     implementation("net.swiftzer.semver:semver:1.1.2")
     implementation("org.gradlex:extra-java-module-info:1.4")
     implementation("org.gradlex:java-ecosystem-capabilities:1.1")
-    implementation("org.gradlex:java-module-dependencies:1.3")
+    implementation("org.gradlex:java-module-dependencies:1.3.1")
     implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:3.5.0.2730")
 }
