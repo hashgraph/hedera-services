65f0c2bb2b9bb12551f44d5783c9e65e874e2b89
Timo Brandstätter <timo@swirldslabs.com>
2023-06-23 20:23:51 +0200

    Fix #7247 Log warnings only for actually applied mappings
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/internal/ConfigMappings.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/internal/ConfigMappings.java
@@ -88,6 +88,7 @@ public final class ConfigMappings {
      */
     @NonNull
     public static ConfigSource addConfigMapping(@NonNull final ConfigSource configSource) {
+        PlatformConfigUtils.logAppliedMappedProperties(configSource.getPropertyNames());
         final MappedConfigSource withAliases = new MappedConfigSource(configSource);
         MAPPINGS.forEach(withAliases::addMapping);
 
