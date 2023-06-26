502c65b74947696c90a949e2da52340b19b0e141
Timo Brandstätter <timo@swirldslabs.com>
2023-06-26 20:21:46 +0200

    Make sure configuration is applied
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/SyncManagerTest.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/SyncManagerTest.java
@@ -32,6 +32,7 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
 import com.swirlds.common.config.EventConfig;
+import com.swirlds.common.config.singleton.ConfigurationHolder;
 import com.swirlds.common.merkle.synchronization.config.ReconnectConfig;
 import com.swirlds.common.metrics.noop.NoOpMetrics;
 import com.swirlds.common.system.EventCreationRuleResponse;
@@ -50,6 +51,7 @@ import com.swirlds.platform.state.SwirldStateManager;
 import com.swirlds.test.framework.config.TestConfigBuilder;
 import java.util.List;
 import java.util.Random;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -136,6 +138,15 @@ public class SyncManagerTest {
                             addressBook, selfId, connectionGraph, () -> {}, () -> {}, reconnectConfig),
                     eventConfig);
         }
+    }
+
+    @BeforeAll
+    void beforeAll() {
+        final Configuration configuration = new TestConfigBuilder()
+                .withValue("sync.maxIncomingSyncsInc", 10)
+                .withValue("sync.maxOutgoingSyncs", 10)
+                .getOrCreateConfig();
+        ConfigurationHolder.getInstance().setConfiguration(configuration);
     }
 
     /**
