b93cf85538d48c459014f9ad9b1704abf47e1e85
Richard Bair <rbair23@users.noreply.github.com>
2023-06-29 12:53:30 -0700

    User `docker buildx build` instead of having `docker build` as an alias
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/docker/docker-build.sh
+++ hedera-node/docker/docker-build.sh
@@ -11,4 +11,4 @@ echo "Using project directory: ${2}"
 echo
 
 echo "Building container:"
-docker build -t "services-node:${TAG}" --build-context services-data=../data . || exit "${?}"
+docker buildx build -t "services-node:${TAG}" --build-context services-data=../data . || exit "${?}"
