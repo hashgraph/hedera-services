524dc5f55ea0ca57027261393b714db4932d69e1
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-07-02 11:15:43 +0200

    develop merged
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-common/src/main/java/module-info.java
+++ platform-sdk/swirlds-common/src/main/java/module-info.java
@@ -54,7 +54,6 @@ module com.swirlds.common {
     exports com.swirlds.common.sequence.map;
     exports com.swirlds.common.sequence.set;
     exports com.swirlds.common.settings;
-    exports com.swirlds.common.statistics;
     exports com.swirlds.common.stream;
     exports com.swirlds.common.stream.internal;
     exports com.swirlds.common.system;
@@ -104,14 +103,6 @@ module com.swirlds.common {
             com.fasterxml.jackson.databind;
     opens com.swirlds.common.stream to
             com.fasterxml.jackson.databind;
-
-    exports com.swirlds.common.statistics.internal to
-            com.swirlds.common.test,
-            com.swirlds.demo.platform,
-            com.swirlds.platform,
-            com.swirlds.platform.test,
-            com.swirlds.jrs;
-
     opens com.swirlds.common.merkle.copy to
             com.fasterxml.jackson.databind;
 
