59bd00451cf9632791528cf9558338681b282483
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-07-04 14:34:24 -0500

    spotless
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/transfers/ChangeNFTOwnersStepTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/transfers/ChangeNFTOwnersStepTest.java
@@ -17,6 +17,7 @@
 package com.hedera.node.app.service.token.impl.test.handlers.transfers;
 
 import static com.hedera.hapi.node.base.ResponseCodeEnum.SPENDER_DOES_NOT_HAVE_ALLOWANCE;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.UNEXPECTED_TOKEN_DECIMALS;
 import static com.hedera.node.app.service.token.impl.handlers.BaseCryptoHandler.asAccount;
 import static com.hedera.node.app.service.token.impl.test.handlers.transfers.Utils.aaWith;
 import static com.hedera.node.app.service.token.impl.test.handlers.transfers.Utils.nftTransferWithAllowance;
@@ -113,6 +114,32 @@ class ChangeNFTOwnersStepTest extends StepsBase {
     }
 
     @Test
+    void failsWIthDifferingExpectedDecimals() {
+        body = CryptoTransferTransactionBody.newBuilder()
+                .transfers(TransferList.newBuilder()
+                        .accountAmounts(aaWith(ownerId, -1_000), aaWith(unknownAliasedId, +1_000))
+                        .build())
+                .tokenTransfers(TokenTransferList.newBuilder()
+                        .token(nonFungibleTokenId)
+                        .expectedDecimals(20)
+                        .nftTransfers(nftTransferWithAllowance(ownerId, unknownAliasedId1, 1))
+                        .build())
+                .build();
+        givenTxn(body, spenderId);
+        ensureAliasesStep = new EnsureAliasesStep(body);
+        replaceAliasesWithIDsInOp = new ReplaceAliasesWithIDsInOp();
+        associateTokenRecepientsStep = new AssociateTokenRecepientsStep(body);
+        transferContext = new TransferContextImpl(handleContext);
+
+        final var replacedOp = getReplacedOp();
+        changeNFTOwnersStep = new NFTOwnersChangeStep(replacedOp, spenderId);
+
+        assertThatThrownBy(() -> changeNFTOwnersStep.doIn(transferContext))
+                .isInstanceOf(HandleException.class)
+                .has(responseCode(UNEXPECTED_TOKEN_DECIMALS));
+    }
+
+    @Test
     void changesNftOwnersWithAllowance() {
         body = CryptoTransferTransactionBody.newBuilder()
                 .transfers(TransferList.newBuilder()
@@ -120,6 +147,7 @@ class ChangeNFTOwnersStepTest extends StepsBase {
                         .build())
                 .tokenTransfers(TokenTransferList.newBuilder()
                         .token(nonFungibleTokenId)
+                        .expectedDecimals(1000)
                         .nftTransfers(nftTransferWithAllowance(ownerId, unknownAliasedId1, 1))
                         .build())
                 .build();
