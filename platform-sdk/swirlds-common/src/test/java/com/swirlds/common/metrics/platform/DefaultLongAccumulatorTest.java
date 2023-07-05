b79d7972b194c49a6a54118b4830568b264d609a
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-07-05 16:27:45 +0200

    Statistics moved (#7354)
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/platform/DefaultLongAccumulatorTest.java
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/platform/DefaultLongAccumulatorTest.java
@@ -26,7 +26,7 @@ import com.swirlds.common.metrics.IntegerGauge;
 import com.swirlds.common.metrics.LongAccumulator;
 import com.swirlds.common.metrics.Metric;
 import com.swirlds.common.metrics.platform.Snapshot.SnapshotEntry;
-import com.swirlds.common.statistics.StatsBuffered;
+import com.swirlds.common.metrics.statistics.StatsBuffered;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
