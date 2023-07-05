309d81511549804da5e7d4269942695ea5e4c66a
Michael Tinker <michael.tinker@swirldslabs.com>
2023-07-05 00:07:22 -0500

    Add gas refunding, support top-level `CONTRACT_CREATION` messages (#7333)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/hevm/HederaWorldUpdater.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/hevm/HederaWorldUpdater.java
@@ -124,18 +124,18 @@ public interface HederaWorldUpdater extends WorldUpdater {
     Optional<ExceptionalHaltReason> tryTrackingDeletion(@NonNull Address deleted, @NonNull Address beneficiary);
 
     /**
-     * Given the possibly zero address of the recipient of a {@code CONTRACT_CREATION} message,
+     * Given the possibly zero address of the origin of a {@code CONTRACT_CREATION} message,
      * sets up the {@link PendingCreation} this {@link ProxyWorldUpdater} will use to complete
      * the creation of the new account in {@link ProxyWorldUpdater#createAccount(Address, long, Wei)};
      * returns the "long-zero" address to be assigned to the new account.
      *
-     * @param receiver the address of the recipient of a {@code CONTRACT_CREATION} message, zero if a top-level message
+     * @param origin the address of the origin of a {@code CONTRACT_CREATION} message, zero if a top-level message
      * @return the "long-zero" address to be assigned to the new account
      */
-    Address setupCreate(@NonNull Address receiver);
+    Address setupCreate(@NonNull Address origin);
 
     /**
-     * Given the possibly zero address of the recipient of a {@code CONTRACT_CREATION} message,
+     * Given the possibly zero address of the origin of a {@code CONTRACT_CREATION} message,
      * and either the canonical {@code CREATE1} address, or the EIP-1014 address computed by an
      * in-progress {@code CREATE2} operation, sets up the {@link PendingCreation} this
      * {@link ProxyWorldUpdater} will use to complete the creation of the new account in
@@ -143,10 +143,10 @@ public interface HederaWorldUpdater extends WorldUpdater {
      *
      * <p>Does not return anything, as the {@code CREATE2} address is already known.
      *
-     * @param receiver the address of the recipient of a {@code CONTRACT_CREATION} message, zero if a top-level message
+     * @param origin the address of the origin of a {@code CONTRACT_CREATION} message, zero if a top-level message
      * @param alias    the EIP-1014 address computed by an in-progress {@code CREATE2} operation
      */
-    void setupAliasedCreate(@NonNull Address receiver, @NonNull Address alias);
+    void setupAliasedCreate(@NonNull Address origin, @NonNull Address alias);
 
     /**
      * Returns whether this address refers to a hollow account (i.e. a lazy-created account that
