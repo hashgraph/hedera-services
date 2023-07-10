994687fe65e79b0f37f57597d5ebce2ff8a7bf78
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-07-10 16:59:49 +0200

    io classes moved to textfixtures (#7237)
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-common/src/testFixtures/java/com/swirlds/common/test/fixtures/merkle/util/MerkleSerializeUtils.java
+++ platform-sdk/swirlds-common/src/testFixtures/java/com/swirlds/common/test/fixtures/merkle/util/MerkleSerializeUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-package com.swirlds.common.test.merkle.util;
+package com.swirlds.common.test.fixtures.merkle.util;
 
 import com.swirlds.common.merkle.MerkleNode;
-import com.swirlds.common.test.io.InputOutputStream;
+import com.swirlds.common.test.fixtures.io.InputOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 
