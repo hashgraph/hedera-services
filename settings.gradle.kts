6efc7b96a3302e3818260b1fa2feed3c89cc3482
Michael Tinker <michael.tinker@swirldslabs.com>
2023-07-10 11:07:03 -0500

    Use 0.40.0 + v0.40.0 proto assets
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -102,8 +102,8 @@ gradleEnterprise {
 
 // The HAPI API version to use for Protobuf sources. This can be a tag or branch
 // name from the hedera-protobufs GIT repo.
-val hapiProtoVersion = "0.40.0-blocks-state-SNAPSHOT"
-val hapiProtoBranchOrTag = "add-pbj-types-for-state" // hapiProtoVersion
+val hapiProtoVersion = "0.40.0"
+val hapiProtoBranchOrTag = "v0.40.0" // hapiProtoVersion
 
 gitRepositories {
     checkoutsDirectory.set(File(rootDir, "hedera-node/hapi"))
