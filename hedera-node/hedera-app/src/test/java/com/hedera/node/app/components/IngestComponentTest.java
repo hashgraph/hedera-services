53b3ed0f4bb7394eafd127f706a204b926ef4c4b
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2023-07-12 14:48:07 +0300

    test: remove not needed mock for the IngestComponentTest
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/components/IngestComponentTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/components/IngestComponentTest.java
@@ -26,11 +26,9 @@ import com.hedera.hapi.node.base.SemanticVersion;
 import com.hedera.node.app.DaggerHederaInjectionComponent;
 import com.hedera.node.app.HederaInjectionComponent;
 import com.hedera.node.app.config.ConfigProviderImpl;
-import com.hedera.node.app.fixtures.state.FakeHederaState;
 import com.hedera.node.app.info.SelfNodeInfoImpl;
 import com.hedera.node.app.service.mono.context.properties.BootstrapProperties;
 import com.hedera.node.app.spi.info.SelfNodeInfo;
-import com.hedera.node.app.state.recordcache.RecordCacheService;
 import com.hedera.node.app.version.HederaSoftwareVersion;
 import com.hedera.node.config.testfixtures.HederaTestConfigBuilder;
 import com.swirlds.common.context.PlatformContext;
@@ -42,8 +40,6 @@ import com.swirlds.common.system.Platform;
 import com.swirlds.common.system.status.PlatformStatus;
 import com.swirlds.config.api.Configuration;
 import java.time.InstantSource;
-import java.util.LinkedList;
-import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -96,16 +92,7 @@ class IngestComponentTest {
         given(platform.getSelfId()).willReturn(new NodeId(0L));
 
         final var subject = app.ingestComponentFactory().get().create();
-        final var fakeHederaState = generateFakeHederaState();
-        app.workingStateAccessor().setHederaState(fakeHederaState);
 
         assertNotNull(subject.ingestWorkflow());
-    }
-
-    private static FakeHederaState generateFakeHederaState() {
-        final var fakeHederaState = new FakeHederaState();
-        fakeHederaState.addService(
-            RecordCacheService.NAME, Map.of(RecordCacheService.TXN_RECORD_QUEUE, new LinkedList<>()));
-        return fakeHederaState;
     }
 }
