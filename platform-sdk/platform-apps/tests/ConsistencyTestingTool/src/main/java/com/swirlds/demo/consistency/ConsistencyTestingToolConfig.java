8f4419f1b6bdd1ac07d5f4ea473c2bd37930eaf6
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2023-07-13 11:42:13 -0500

    07494 ctt freeze (#7521)
    Signed-off-by: Cody Littley <cody@swirldslabs.com>
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/com/swirlds/demo/consistency/ConsistencyTestingToolConfig.java
+++ platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/com/swirlds/demo/consistency/ConsistencyTestingToolConfig.java
@@ -18,13 +18,16 @@ package com.swirlds.demo.consistency;
 
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
+import java.time.Duration;
 
 /**
  * Config for consistency testing tool
  *
- * @param logfileDirectory the directory where consistency information is stored, relative to
- * {@link com.swirlds.common.config.StateConfig#savedStateDirectory()}.
+ * @param logfileDirectory   the directory where consistency information is stored, relative to
+ * @param freezeAfterGenesis if not 0, describes a moment in time, relative to genesis, when a freeze is scheduled
+ *                           {@link com.swirlds.common.config.StateConfig#savedStateDirectory()}.
  */
 @ConfigData("consistencyTestingTool")
 public record ConsistencyTestingToolConfig(
-        @ConfigProperty(defaultValue = "consistency-test") String logfileDirectory) {}
+        @ConfigProperty(defaultValue = "consistency-test") String logfileDirectory,
+        @ConfigProperty(defaultValue = "0") Duration freezeAfterGenesis) {}
