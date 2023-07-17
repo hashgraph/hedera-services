5b8506a6fe008b1cd5c7cf11e0730fa18888d25a
Edward Wertz <edward@swirldslabs.com>
2023-07-17 14:54:49 -0500

    07586 - AddressBookTestingTool Freeze After Genesis Set By Configuration
    Signed-off-by: Edward Wertz <edward@swirldslabs.com>

--- platform-sdk/platform-apps/tests/AddressBookTestingTool/src/main/java/com/swirlds/demo/addressbook/AddressBookTestingToolConfig.java
+++ platform-sdk/platform-apps/tests/AddressBookTestingTool/src/main/java/com/swirlds/demo/addressbook/AddressBookTestingToolConfig.java
@@ -18,20 +18,24 @@ package com.swirlds.demo.addressbook;
 
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
+import java.time.Duration;
 
 /**
  * Config for address books and utilities that deal with address books.
  *
- * @param softwareVersion
- * 		The integer value of the software version of the AddressBookTestingToolMain SwirldMain application.
- * @param weightingBehavior
- *      The integer value of the weighting behavior of the AddressBookTestingToolState SwirldState.
- * @param testScenario
- *     The string value of the test scenario being run for validation. This must match an enumerated value in {@link AddressBookTestScenario}.
+ * @param softwareVersion    The integer value of the software version of the AddressBookTestingToolMain SwirldMain
+ *                           application.
+ * @param weightingBehavior  The integer value of the weighting behavior of the AddressBookTestingToolState
+ *                           SwirldState.
+ * @param testScenario       The string value of the test scenario being run for validation. This must match an
+ *                           enumerated value in {@link AddressBookTestScenario}.
+ * @param freezeAfterGenesis if not 0, describes a moment in time, relative to genesis, when a freeze is scheduled
+ *                           {@link com.swirlds.common.config.StateConfig#savedStateDirectory()}.
  */
 @ConfigData("addressBookTestingTool")
 public record AddressBookTestingToolConfig(
         @ConfigProperty(defaultValue = "1") int softwareVersion,
         @ConfigProperty(defaultValue = "0") int weightingBehavior,
         // The testScenario should be updated to be an enumerated type in the future instead of a string.
-        @ConfigProperty(defaultValue = "SKIP_VALIDATION") String testScenario) {}
+        @ConfigProperty(defaultValue = "SKIP_VALIDATION") String testScenario,
+        @ConfigProperty(defaultValue = "0") Duration freezeAfterGenesis) {}
