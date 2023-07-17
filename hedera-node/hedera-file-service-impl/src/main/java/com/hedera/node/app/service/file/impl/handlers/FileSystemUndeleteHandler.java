2b13fc0b6fa9832968c768c5ef803d4bd21f3a75
Michael Heinrichs <netopyr@users.noreply.github.com>
2023-07-17 09:51:07 +0200

    Minor fixes in stores (#7380)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileSystemUndeleteHandler.java
+++ hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileSystemUndeleteHandler.java
@@ -24,7 +24,7 @@ import static java.util.Objects.requireNonNull;
 import com.hedera.hapi.node.base.HederaFunctionality;
 import com.hedera.hapi.node.state.file.File;
 import com.hedera.node.app.service.file.impl.ReadableFileStoreImpl;
-import com.hedera.node.app.service.file.impl.WritableFileStoreImpl;
+import com.hedera.node.app.service.file.impl.WritableFileStore;
 import com.hedera.node.app.service.file.impl.utils.FileServiceUtils;
 import com.hedera.node.app.spi.workflows.HandleContext;
 import com.hedera.node.app.spi.workflows.HandleException;
@@ -78,7 +78,7 @@ public class FileSystemUndeleteHandler implements TransactionHandler {
         var fileId = systemUndeleteTransactionBody.fileIDOrThrow();
         final var ledgerConfig = handleContext.configuration().getConfigData(LedgerConfig.class);
 
-        final var fileStore = handleContext.writableStore(WritableFileStoreImpl.class);
+        final var fileStore = handleContext.writableStore(WritableFileStore.class);
         final File file = FileServiceUtils.verifyNotSystemFile(ledgerConfig, fileStore, fileId, true);
 
         final var oldExpiry = file.expirationTime();
