8da76886f09ca8b97ac7d6e2ac24ecb08f97276d
Edward Wertz <edward@swirldslabs.com>
2023-07-17 11:07:44 -0500

    06989 - should fix failing unit test.
    Signed-off-by: Edward Wertz <edward@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/legacy/LegacyConfigPropertiesLoader.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/config/legacy/LegacyConfigPropertiesLoader.java
@@ -17,7 +17,6 @@
 package com.swirlds.platform.config.legacy;
 
 import static com.swirlds.logging.LogMarker.EXCEPTION;
-import static com.swirlds.platform.system.SystemExitUtils.exitSystem;
 
 import com.swirlds.common.internal.ConfigurationException;
 import com.swirlds.common.system.NodeId;
@@ -25,7 +24,6 @@ import com.swirlds.common.system.address.Address;
 import com.swirlds.common.system.address.AddressBook;
 import com.swirlds.common.system.address.AddressBookUtils;
 import com.swirlds.common.utility.CommonUtils;
-import com.swirlds.platform.system.SystemExitCode;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -147,8 +145,7 @@ public final class LegacyConfigPropertiesLoader {
             }
             if (!nextNodeIdParsed) {
                 onError(ERROR_NO_PARAMETER.formatted(NEXT_NODE_ID_PROPERTY_NAME));
-                logger.error("config.txt did not have a `nextnodeid` property.");
-                exitSystem(SystemExitCode.CONFIGURATION_ERROR);
+                throw new ConfigurationException("config.txt did not have a `nextnodeid` property.");
             }
             if (addressBook.getSize() > 0) {
                 configurationProperties.setAddressBook(addressBook);
