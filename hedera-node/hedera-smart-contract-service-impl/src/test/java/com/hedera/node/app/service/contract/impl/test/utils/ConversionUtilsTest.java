93aa74d26c613c8d4a5db24c9d77b1962529ffc1
Michael Tinker <michael.tinker@swirldslabs.com>
2023-07-17 10:25:50 -0500

    Remove unwanted changes
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/utils/ConversionUtilsTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/utils/ConversionUtilsTest.java
@@ -35,7 +35,6 @@ import static org.mockito.BDDMockito.given;
 import com.hedera.hapi.node.base.ContractID;
 import com.hedera.hapi.node.contract.ContractLoginfo;
 import com.hedera.hapi.node.state.common.EntityNumber;
-import com.hedera.hapi.streams.CallOperationType;
 import com.hedera.hapi.streams.ContractStateChange;
 import com.hedera.hapi.streams.ContractStateChanges;
 import com.hedera.hapi.streams.StorageChange;
@@ -50,8 +49,6 @@ import org.hyperledger.besu.evm.log.Log;
 import org.hyperledger.besu.evm.log.LogsBloomFilter;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -59,20 +56,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class ConversionUtilsTest {
     @Mock
     private Dispatch dispatch;
-
-    @ParameterizedTest
-    @CsvSource({
-        "0xF0,OP_CREATE",
-        "0xF1,OP_CALL",
-        "0xF2,OP_CALLCODE",
-        "0xF4,OP_DELEGATECALL",
-        "0xF5,OP_CREATE2",
-        "0xFA,OP_STATICCALL",
-        "0x12,OP_UNKNOWN",
-    })
-    void convertsOpcodesToCallOpsAsExpected(final int opCode, @NonNull final CallOperationType type) {
-        assertEquals(type, asCallOperationType(opCode));
-    }
 
     @Test
     void numberedIdsRequireLongZeroAddress() {
