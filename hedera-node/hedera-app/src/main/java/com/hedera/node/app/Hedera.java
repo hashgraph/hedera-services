50c780c00c7c1a36031a85bbab7820afc691068e
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-07-26 12:36:39 -0500

    add TODO
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/Hedera.java
@@ -398,9 +398,9 @@ public final class Hedera implements SwirldMain {
             notifications.register(PlatformStatusChangeListener.class, notification -> {
                 switch (notification.getNewStatus()) {
                     case ACTIVE -> {
-                        // This is also called from ServicesMain#run() when the node is restarted.
+                        // TODO: This is also called from ServicesMain#run() when the node is restarted.
                         // Need to confirm if this is needed
-                        //                        run();
+                        // run();
                         logger.info("Hederanode#{} is ACTIVE", nodeId);
                     }
                     case BEHIND -> {
