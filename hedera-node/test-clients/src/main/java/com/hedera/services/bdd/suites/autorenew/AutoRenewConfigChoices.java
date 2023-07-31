184e4cacf62403f1e4441e10e33f2170d891c4dd
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2023-07-31 09:13:29 +0300

    nit: removing not needed annotation in AutoRenewConfigChoices
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/autorenew/AutoRenewConfigChoices.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/autorenew/AutoRenewConfigChoices.java
@@ -20,12 +20,10 @@ import static com.hedera.services.bdd.spec.transactions.TxnVerbs.fileUpdate;
 import static com.hedera.services.bdd.suites.HapiSuite.APP_PROPERTIES;
 import static com.hedera.services.bdd.suites.HapiSuite.GENESIS;
 
-import com.hedera.services.bdd.junit.HapiTestSuite;
 import com.hedera.services.bdd.spec.HapiSpecOperation;
 import com.hedera.services.bdd.spec.HapiSpecSetup;
 import java.util.Map;
 
-@HapiTestSuite
 public class AutoRenewConfigChoices {
 
     static final int DEFAULT_HIGH_TOUCH_COUNT = 10_000;
