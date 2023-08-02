3833e7ad938da715723e8427598e69d125391412
Yanko Nikolov <nikolovqnko@gmail.com>
2023-08-02 22:08:24 +0300

    fix: removing protos from ExchangeRateManager.java
    Signed-off-by: Yanko Nikolov <nikolovqnko@gmail.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/ExchangeRateManager.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/ExchangeRateManager.java
@@ -16,20 +16,18 @@
 
 package com.hedera.node.app.workflows.handle;
 
+import static com.hedera.hapi.node.transaction.ExchangeRateSet.PROTOBUF;
 import static com.hedera.node.app.service.file.impl.FileServiceImpl.BLOBS_KEY;
-import static com.hederahashgraph.api.proto.java.ExchangeRateSet.parseFrom;
 
-import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.hapi.node.base.FileID;
-import com.hedera.hapi.node.base.TimestampSeconds;
 import com.hedera.hapi.node.state.file.File;
-import com.hedera.hapi.node.transaction.ExchangeRate;
 import com.hedera.hapi.node.transaction.ExchangeRateSet;
 import com.hedera.node.app.service.file.FileService;
 import com.hedera.node.app.spi.state.ReadableKVState;
 import com.hedera.node.app.state.HederaState;
 import com.hedera.node.config.data.FilesConfig;
 import com.swirlds.config.api.Configuration;
+import java.io.IOException;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.apache.logging.log4j.LogManager;
@@ -54,7 +52,7 @@ public class ExchangeRateManager {
     }
 
     public void createUpdateExchangeRates(final HederaState hederaState, final Configuration configuration)
-            throws InvalidProtocolBufferException {
+            throws IOException {
         final var readableStates = hederaState.createReadableStates(FileService.NAME);
         final ReadableKVState<FileID, File> files = readableStates.get(BLOBS_KEY);
         final var fileConfig = configuration.getConfigData(FilesConfig.class);
@@ -62,20 +60,24 @@ public class ExchangeRateManager {
                 FileID.newBuilder().fileNum(fileConfig.exchangeRates()).build();
         final var exchangeRateFile = files.get(fileId);
 
-        final var exchangeRateSetProto = parseFrom(exchangeRateFile.contents().toByteArray());
-
-        final var currentRate = exchangeRateSetProto.hasCurrentRate()
-                ? convertRatesFromProto(exchangeRateSetProto.getCurrentRate())
-                : null;
-        final var nextRate = exchangeRateSetProto.hasCurrentRate()
-                ? convertRatesFromProto(exchangeRateSetProto.getNextRate())
-                : null;
-        populateFields(currentRate, nextRate);
-        exchangeRateSet = ExchangeRateSet.newBuilder()
-                .currentRate(currentRate)
-                .nextRate(nextRate)
-                .build();
+        exchangeRateSet = PROTOBUF.parse(exchangeRateFile.contents().toReadableSequentialData());
         isInitiated = true;
+        populateExchangeRateFields();
+    }
+
+    private void populateExchangeRateFields() {
+        if (exchangeRateSet.hasCurrentRate()) {
+            final var currentRate = exchangeRateSet.currentRate();
+            currHbarEquiv = currentRate.hbarEquiv();
+            currCentEquiv = currentRate.centEquiv();
+            currExpiry = currentRate.expirationTime().seconds();
+        }
+        if (exchangeRateSet.hasNextRate()) {
+            final var nextRate = exchangeRateSet.nextRate();
+            nextHbarEquiv = nextRate.hbarEquiv();
+            nextCentEquiv = nextRate.centEquiv();
+            nextExpiry = nextRate.expirationTime().seconds();
+        }
     }
 
     public int getCurrHbarEquiv() {
@@ -108,32 +110,5 @@ public class ExchangeRateManager {
 
     public ExchangeRateSet getExchangeRateSet() {
         return exchangeRateSet;
-    }
-
-    private void populateFields(ExchangeRate currentRate, ExchangeRate nextRate) {
-        if (currentRate != null) {
-            currHbarEquiv = currentRate.hbarEquiv();
-            currCentEquiv = currentRate.centEquiv();
-            currExpiry = currentRate.expirationTime().seconds();
-        }
-        if (nextRate != null) {
-            nextHbarEquiv = nextRate.hbarEquiv();
-            nextCentEquiv = nextRate.centEquiv();
-            nextExpiry = nextRate.expirationTime().seconds();
-        }
-    }
-
-    private com.hedera.hapi.node.transaction.ExchangeRate convertRatesFromProto(
-            final com.hederahashgraph.api.proto.java.ExchangeRate exchangeRateProto) {
-        final var hbarEquiv = exchangeRateProto.getHbarEquiv();
-        final var centEquiv = exchangeRateProto.getCentEquiv();
-        final var expiry = exchangeRateProto.getExpirationTime().getSeconds();
-        final var expirationTime = TimestampSeconds.newBuilder().seconds(expiry).build();
-
-        return com.hedera.hapi.node.transaction.ExchangeRate.newBuilder()
-                .hbarEquiv(hbarEquiv)
-                .centEquiv(centEquiv)
-                .expirationTime(expirationTime)
-                .build();
     }
 }
