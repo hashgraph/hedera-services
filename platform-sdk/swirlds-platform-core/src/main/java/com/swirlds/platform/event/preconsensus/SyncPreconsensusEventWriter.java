e72c72844341f7eb525a32ed2d01f39017f4fdf5
Cody Littley <cody@swirldslabs.com>
2023-08-03 08:08:14 -0500

    Delay pruning until after PCES replay.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/preconsensus/SyncPreconsensusEventWriter.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/preconsensus/SyncPreconsensusEventWriter.java
@@ -295,6 +295,13 @@ public class SyncPreconsensusEventWriter implements PreconsensusEventWriter, Sta
      * Delete old files from the disk.
      */
     private void pruneOldFiles() {
+        if (!streamingNewEvents) {
+            // Don't attempt to prune files until we are done replaying the event stream (at start up).
+            // Files are being iterated on a different thread, and so it isn't thread safe to prune files
+            // while they are being iterated.
+            return;
+        }
+
         try {
             fileManager.pruneOldFiles(minimumGenerationToStore);
         } catch (final IOException e) {
