f65bb7fd6ca13c0c1fa0c015036a94451e257612
Austin Littley <austin@swirldslabs.com>
2023-08-03 15:19:12 -0400

    Address PR comments
    Signed-off-by: Austin Littley <austin@swirldslabs.com>

--- platform-sdk/swirlds-cli/color-logs.py
+++ platform-sdk/swirlds-cli/color-logs.py
@@ -41,7 +41,7 @@ marker_regex = '([A-Za-z0-9_]+)'
 thread_name_regex = '(<<?[^>]+>>?)'
 class_thread_name_regex = '([A-Za-z0-9_]+)'
 remainder_of_line_regex = '(.*)'
-status_log_regex = "(.*spent )(.*)( in )(.*)(\. Now in )([A-Z_]+)(.*)"
+status_log_regex = '(.*spent )(.*)( in )(.*)(\. Now in )([A-Z_]+)(.*)'
 
 full_regex = timestamp_regex + whitespace_regex + \
              log_number_regex + captured_whitespace_regex + \
@@ -64,30 +64,30 @@ BRIGHT_WHITE = '\033[97m'
 END = '\033[0m'
 
 def format_timestamp(timestamp):
-  return GRAY + timestamp + END
+    return GRAY + timestamp + END
 
 def format_log_number(log_number):
-  return WHITE + log_number + END
+    return WHITE + log_number + END
 
 def format_log_level(log_level):
     if log_level in ('TRACE', 'DEBUG', 'INFO'):
-      return GREEN + log_level + END
+        return GREEN + log_level + END
     elif log_level == 'WARN':
-      return YELLOW + log_level + END
+        return YELLOW + log_level + END
     else:
-      return RED + log_level + END
+        return RED + log_level + END
 
 def format_marker(marker):
     if marker == 'PLATFORM_STATUS':
-      return PURPLE + marker + END
+        return PURPLE + marker + END
     else:
-      return BRIGHT_BLUE + marker + END
+        return BRIGHT_BLUE + marker + END
 
 def format_thread_name(thread_name):
-  return BRIGHT_WHITE + thread_name + END
+    return BRIGHT_WHITE + thread_name + END
 
 def format_class_name(class_name):
-  return TEAL + class_name + END
+    return TEAL + class_name + END
 
 # Provide specific formatting for the platform status log message
 def format_platform_status_log(log_message):
@@ -129,57 +129,58 @@ def format_platform_status_log(log_message):
 
 def format_remainder(marker, remainder):
     if marker == 'PLATFORM_STATUS':
-      return format_platform_status_log(remainder)
+        return format_platform_status_log(remainder)
     else:
-      return remainder
+        return remainder
 
 def format(line):
-  match = regex.match(line)
-  if match is None:
-    return line
+    match = regex.match(line)
+    if match is None:
+        return line
 
-  index = 1
+    index = 1
 
-  timestamp = match.group(index)
-  index += 1
+    timestamp = match.group(index)
+    index += 1
 
-  log_number = match.group(index)
-  index += 1
+    log_number = match.group(index)
+    index += 1
 
-  log_number_whitespace = match.group(index)
-  index += 1
+    log_number_whitespace = match.group(index)
+    index += 1
 
-  log_level = match.group(index)
-  index += 1
+    log_level = match.group(index)
+    index += 1
 
-  log_level_whitespace = match.group(index)
-  index += 1
+    log_level_whitespace = match.group(index)
+    index += 1
 
-  marker = match.group(index)
-  index += 1
+    marker = match.group(index)
+    index += 1
 
-  marker_whitespace = match.group(index)
-  index += 1
+    marker_whitespace = match.group(index)
+    index += 1
 
-  thread_name = match.group(index)
-  index += 1
+    thread_name = match.group(index)
+    index += 1
 
-  class_name = match.group(index)
-  index += 1
+    class_name = match.group(index)
+    index += 1
 
-  remainder = match.group(index)
-  index += 1
+    remainder = match.group(index)
+    index += 1
 
-  return format_timestamp(timestamp) + ' ' + \
-          format_log_number(log_number) + log_number_whitespace + \
-          format_log_level(log_level) + log_level_whitespace + \
-          format_marker(marker) + marker_whitespace + \
-          format_thread_name(thread_name) + ' ' + \
-          format_class_name(class_name) + \
-          format_remainder(marker, remainder) + "\n"
+    return format_timestamp(timestamp) + ' ' + \
+        format_log_number(log_number) + log_number_whitespace + \
+        format_log_level(log_level) + log_level_whitespace + \
+        format_marker(marker) + marker_whitespace + \
+        format_thread_name(thread_name) + ' ' + \
+        format_class_name(class_name) + \
+        format_remainder(marker, remainder) + '\n'
+
 
 try:
-  for line in stdin:
-    print(format(line), end='')
+    for line in stdin:
+        print(format(line), end='')
 except KeyboardInterrupt:
-  pass
+    pass
