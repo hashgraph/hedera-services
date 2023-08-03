53c576692d40b441bbd72a36b7ed9e74af3d0d17
Lazar Petrovic <lpetrovic05@gmail.com>
2023-08-03 17:17:45 +0200

    spotless
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-unit-tests/common/swirlds-common-test/src/test/java/com/swirlds/common/test/metrics/extensions/BusyTimeTest.java
+++ platform-sdk/swirlds-unit-tests/common/swirlds-common-test/src/test/java/com/swirlds/common/test/metrics/extensions/BusyTimeTest.java
@@ -140,18 +140,20 @@ class BusyTimeTest {
     }
 
     @Test
-    void metricNotReset(){
+    void metricNotReset() {
         metric.startingWork();
         clock.tick(Duration.ofMinutes(30));
         metric.finishedWork();
         clock.tick(Duration.ofMinutes(10));
         metric.startingWork();
         metric.finishedWork();
-        assertEquals(
-                -1.0, metric.getAndReset(), 0.01, "this in an overflow, so -1 is expected");
+        assertEquals(-1.0, metric.getAndReset(), 0.01, "this in an overflow, so -1 is expected");
         clock.tick(Duration.ofMinutes(1));
         metric.startingWork();
         clock.tick(Duration.ofMinutes(1));
-        assertEquals(0.5, metric.getAndReset(), "after the reset the metric should start tracking again, so 0.5 is expected");
+        assertEquals(
+                0.5,
+                metric.getAndReset(),
+                "after the reset the metric should start tracking again, so 0.5 is expected");
     }
 }
