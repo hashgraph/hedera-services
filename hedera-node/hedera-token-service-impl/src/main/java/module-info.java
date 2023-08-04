2e29cbce61b99e00144be11b7e848e06b7133856
neeharika.sompalli <neeharika.sompalli@swirldslabs.com>
2023-08-04 11:01:58 -0500

    move all record builders
    Signed-off-by: neeharika.sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-token-service-impl/src/main/java/module-info.java
@@ -27,8 +27,6 @@ module com.hedera.node.app.service.token.impl {
             com.hedera.node.app.service.token.impl.test;
     exports com.hedera.node.app.service.token.impl.serdes;
     exports com.hedera.node.app.service.token.impl;
-    exports com.hedera.node.app.service.token.records.records to
-            com.hedera.node.app;
     exports com.hedera.node.app.service.token.impl.api to
             com.hedera.node.app,
             com.hedera.node.app.service.token.impl.api.test;
