dc4229cad0f2352132abc1b75d22943768c3172f
Lazar Petrovic <lpetrovic05@gmail.com>
2023-08-08 14:07:23 +0200

    todos
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/event/EventBuilder.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/event/EventBuilder.java
@@ -32,6 +32,8 @@ import com.swirlds.platform.internal.EventImpl;
 import java.time.Instant;
 import java.util.Random;
 
+
+//TODO move to fixtures
 public class EventBuilder {
     private static final Instant DEFAULT_TIMESTAMP = Instant.ofEpochMilli(1588771316678L);
     private Random random;
