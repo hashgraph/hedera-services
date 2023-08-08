d1146a2ce9140ae663bb3cf187e16f2db4920e53
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2023-08-08 14:22:17 +0300

    Increase test code coverage
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/ExchangeRateManagerTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/fees/ExchangeRateManagerTest.java
@@ -17,25 +17,39 @@
 package com.hedera.node.app.fees;
 
 import static com.hedera.hapi.node.transaction.ExchangeRateSet.PROTOBUF;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.hedera.hapi.node.base.TimestampSeconds;
 import com.hedera.hapi.node.transaction.ExchangeRate;
 import com.hedera.hapi.node.transaction.ExchangeRateSet;
+import com.hedera.node.app.spi.fixtures.util.LogCaptor;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.stream.Stream;
+import org.apache.logging.log4j.LogManager;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ExchangeRateManagerTest {
     static final int hbarEquiv = 30_000;
     static final int centEquiv = 120_000;
-    TimestampSeconds expirationTime =
+    static TimestampSeconds expirationTime =
             TimestampSeconds.newBuilder().seconds(150_000L).build();
-    ExchangeRate.Builder someRate =
+    static ExchangeRate.Builder someRate =
             ExchangeRate.newBuilder().hbarEquiv(hbarEquiv).centEquiv(centEquiv).expirationTime(expirationTime);
+    static ExchangeRate.Builder anotherRate = ExchangeRate.newBuilder()
+            .hbarEquiv(hbarEquiv * 2)
+            .centEquiv(centEquiv * 2)
+            .expirationTime(expirationTime);
     ExchangeRateSet validRatesObj = ExchangeRateSet.newBuilder()
             .currentRate(someRate)
-            .nextRate(someRate)
+            .nextRate(anotherRate)
             .build();
 
     Bytes validRateBytes = ExchangeRateSet.PROTOBUF.toBytes(validRatesObj);
@@ -50,9 +64,9 @@ class ExchangeRateManagerTest {
         final var curr = subject.exchangeRates().currentRateOrThrow();
         final var next = subject.exchangeRates().nextRateOrThrow();
         assertEquals(hbarEquiv, curr.hbarEquiv());
-        assertEquals(hbarEquiv, next.hbarEquiv());
+        assertEquals(hbarEquiv * 2, next.hbarEquiv());
         assertEquals(centEquiv, curr.centEquiv());
-        assertEquals(centEquiv, next.centEquiv());
+        assertEquals(centEquiv * 2, next.centEquiv());
         assertEquals(expirationTime.seconds(), curr.expirationTimeOrThrow().seconds());
         assertEquals(expirationTime.seconds(), next.expirationTimeOrThrow().seconds());
         assertEquals(PROTOBUF.parse(validRateBytes.toReadableSequentialData()), subject.exchangeRates());
@@ -98,5 +112,54 @@ class ExchangeRateManagerTest {
         assertEquals(centEquiv, next.centEquiv());
         assertEquals(0, curr.expirationTimeOrElse(TimestampSeconds.DEFAULT).seconds());
         assertEquals(expirationTime.seconds(), next.expirationTimeOrThrow().seconds());
+    }
+
+    @Test
+    void updateWithInvalidExchangeRateBytes() {
+        // given
+        subject = new ExchangeRateManager();
+        final var logCaptor = new LogCaptor(LogManager.getLogger(ExchangeRateManager.class));
+
+        // when
+        subject.update(Bytes.wrap(new byte[] {0x06}));
+
+        // expect
+        assertEquals(ExchangeRateSet.DEFAULT, subject.exchangeRates());
+        assertThat(logCaptor.warnLogs(), hasItems(startsWith("Unable to parse exchange rate file")));
+        assertThat(logCaptor.warnLogs(), hasItems(startsWith("Exchange rate file did not contain a current rate!")));
+        assertThat(
+                logCaptor.warnLogs(),
+                hasItems(startsWith("Exchange rate current rate did not contain an expiration time! Defaulting to 0")));
+        assertThat(
+                logCaptor.warnLogs(),
+                hasItems(startsWith(
+                        "Exchange rate file did not contain a next rate! Will default to the current rate")));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideConsensusTimesForActiveRate")
+    void activeRateWorksAsExpected(Instant consensusTime, ExchangeRate expectedExchangeRate) {
+        // given
+        subject = new ExchangeRateManager();
+        subject.update(validRateBytes);
+
+        // when
+        final var activeRate = subject.activeRate(consensusTime);
+
+        // expect
+        assertEquals(activeRate, expectedExchangeRate);
+    }
+
+    private static Stream<Arguments> provideConsensusTimesForActiveRate() {
+        return Stream.of(
+                Arguments.of(
+                        Instant.ofEpochSecond(expirationTime.seconds() - 1L), // consensus time before expiration
+                        someRate.build()),
+                Arguments.of(
+                        Instant.ofEpochSecond(expirationTime.seconds()), // consensus time at expiration
+                        someRate.build()),
+                Arguments.of(
+                        Instant.ofEpochSecond(expirationTime.seconds() + 1L), // consensus time after expiration
+                        anotherRate.build()));
     }
 }
