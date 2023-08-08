2c9b8d1b1cb682bb0e395a3fda8b9dd06aff08eb
Cody Littley <cody@swirldslabs.com>
2023-08-08 11:33:54 -0500

    Made suggested change.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/tipset/TipsetEventCreatorImpl.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/tipset/TipsetEventCreatorImpl.java
@@ -211,7 +211,7 @@ public class TipsetEventCreatorImpl implements TipsetEventCreator {
         tipsetMetrics.getSelfishnessMetric().update(selfishness);
 
         // Never bother with anti-selfishness techniques if we have a selfishness score of 1.
-        // We are pretty much guaranteed to selfish to ~1/3 of other nodes by a score of 1.
+        // We are pretty much guaranteed to be selfish to ~1/3 of other nodes by a score of 1.
         final double beNiceChance = (selfishness - 1) / antiSelfishnessFactor;
 
         if (beNiceChance > 0 && random.nextDouble() < beNiceChance) {
