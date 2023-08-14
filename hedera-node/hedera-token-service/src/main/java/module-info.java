7641872bdfeeeb84d1c85efb8e70236fb23d8c90
Jendrik Johannes <jendrik.johannes@gmail.com>
2023-08-14 11:39:52 +0200

    Align source set and module name
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- hedera-node/hedera-token-service/src/main/java/module-info.java
+++ hedera-node/hedera-token-service/src/main/java/module-info.java
@@ -4,7 +4,7 @@ module com.hedera.node.app.service.token {
             com.hedera.node.app.service.contract.impl,
             com.hedera.node.app,
             com.hedera.node.app.service.token.impl,
-            com.hedera.node.app.service.token.fixtures;
+            com.hedera.node.app.service.token.test.fixtures;
     exports com.hedera.node.app.service.token.records to
             com.hedera.node.app.service.contract.impl,
             com.hedera.node.app,
