37b55c30ef7e8422038b86ed495752a563b4bf36
Timo Brandstätter <timo@swirldslabs.com>
2023-08-16 11:50:13 +0200

    Add deprecation and JavaDoc
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-base/src/main/java/com/swirlds/base/utility/Triple.java
+++ platform-sdk/swirlds-base/src/main/java/com/swirlds/base/utility/Triple.java
@@ -19,9 +19,36 @@ package com.swirlds.base.utility;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 
+/**
+ * A simple triple of objects.
+ * <p>
+ * @param left value for the left object
+ * @param middle value for the middle object
+ * @param right value for the right object
+ * @param <L> type of the left object
+ * @param <M> type of the middle object
+ * @param <R> type of the right object
+ * @deprecated Please consider using own data structure instead of this class.
+ *             To have more meaningful names than left, middle and right
+ */
+@Deprecated(forRemoval = true)
 public record Triple<L, M, R>(L left, M middle, R right) {
 
+    /**
+     * Creates a new triple of objects.
+     *
+     * @param left value for the left object
+     * @param middle value for the middle object
+     * @param right value for the right object
+     * @param <L> type of the left object
+     * @param <M> type of the middle object
+     * @param <R> type of the right object
+     *
+     * @return a new triple of objects
+     * @deprecated Please consider using own data structure instead of this class.
+     */
     @NonNull
+    @Deprecated(forRemoval = true)
     public static <L, M, R> Triple<L, M, R> of(@Nullable L left, @Nullable M middle, @Nullable R right) {
         return new Triple<>(left, middle, right);
     }
