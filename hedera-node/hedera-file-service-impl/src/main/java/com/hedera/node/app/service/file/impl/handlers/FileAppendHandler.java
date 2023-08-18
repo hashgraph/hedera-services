a9065139738111b5ddb21ebadc152eea9c708d26
Lev Povolotsky <lev@swirldslabs.com>
2023-08-18 13:32:25 -0400

    used fileconfig directly instead of the injection
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileAppendHandler.java
+++ hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileAppendHandler.java
@@ -36,7 +36,6 @@ import com.hedera.node.app.service.file.ReadableFileStore;
 import com.hedera.node.app.service.file.impl.WritableFileStore;
 import com.hedera.node.app.service.file.impl.WritableUpgradeFileStore;
 import com.hedera.node.app.service.mono.pbj.PbjConverter;
-import com.hedera.node.app.spi.numbers.HederaFileNumbers;
 import com.hedera.node.app.spi.workflows.HandleContext;
 import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.PreCheckException;
@@ -58,13 +57,9 @@ import org.apache.logging.log4j.Logger;
 @Singleton
 public class FileAppendHandler implements TransactionHandler {
     private static final Logger logger = LogManager.getLogger(FileAppendHandler.class);
-    private HederaFileNumbers fileNumbers;
 
     @Inject
-    public FileAppendHandler(@NonNull final HederaFileNumbers fileNumbers) {
-        // Exists for injection
-        this.fileNumbers = fileNumbers;
-    }
+    public FileAppendHandler() {}
 
     /**
      * This method is called during the pre-handle workflow.
@@ -116,7 +111,7 @@ public class FileAppendHandler implements TransactionHandler {
         }
         final var file = optionalFile.get();
 
-        feeCalculation(handleContext, fileAppend, file);
+        feeCalculation(handleContext, fileAppend, file, fileServiceConfig);
 
         // TODO: skip at least the mutability check for privileged "payer" accounts
 
@@ -148,7 +143,11 @@ public class FileAppendHandler implements TransactionHandler {
         fileStore.put(fileBuilder.build());
     }
 
-    private void feeCalculation(HandleContext handleContext, FileAppendTransactionBody fileAppend, File file) {
+    private void feeCalculation(
+            HandleContext handleContext,
+            FileAppendTransactionBody fileAppend,
+            File file,
+            @NonNull FilesConfig fileServiceConfig) {
         final var dataLength =
                 (fileAppend.contents() != null) ? fileAppend.contents().length() : 0;
 
@@ -157,7 +156,12 @@ public class FileAppendHandler implements TransactionHandler {
          * PR conversation: 8089
          */
         final long effectiveLifeTime;
-        if (fileNumbers.isSoftwareUpdateFile(file.fileId().fileNum())) {
+        final var fileNum = file.fileId().fileNum();
+        final var firstSoftwareUpdateFile =
+                fileServiceConfig.softwareUpdateRange().left();
+        final var lastSoftwareUpdateFile =
+                fileServiceConfig.softwareUpdateRange().right();
+        if (firstSoftwareUpdateFile <= fileNum && fileNum <= lastSoftwareUpdateFile) {
             effectiveLifeTime = THREE_MONTHS_IN_SECONDS;
         } else {
             final var effCreationTime =
