025756e8f07a9c9587c7f3a6b202e1c111bc888b
Kelly Greco <kelly@swirldslabs.com>
2023-08-22 08:39:31 -0500

    fix build
    Signed-off-by: Kelly Greco <kelly@swirldslabs.com>

--- platform-sdk/platform-apps/tests/StatsSigningTestingTool/src/main/java/module-info.java
+++ platform-sdk/platform-apps/tests/StatsSigningTestingTool/src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.swirlds.demo.stats.signing {
     requires com.swirlds.platform;
     requires com.swirlds.common;
     requires com.swirlds.logging;
+    requires lazysodium.java;
+    requires org.bouncycastle.provider;
+    requires org.bouncycastle.pkix;
     requires org.apache.logging.log4j;
     requires static com.github.spotbugs.annotations;
-    requires com.swirlds.config;
 }
