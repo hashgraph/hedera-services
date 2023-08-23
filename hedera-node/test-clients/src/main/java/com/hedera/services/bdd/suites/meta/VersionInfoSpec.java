9e5a4799a8f71738ca98849f8f752b6e6aab54cc
Kim Rader <kim.rader@swirldslabs.com>
2023-08-23 07:52:55 -0700

    fix VersionInfoSpec, UpdateFileForUpgrade e2e tests in NetworkAdminService (#8135)
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>
    Co-authored-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/meta/VersionInfoSpec.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/meta/VersionInfoSpec.java
@@ -20,6 +20,7 @@ import static com.hedera.services.bdd.spec.HapiSpec.customHapiSpec;
 import static com.hedera.services.bdd.spec.HapiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getVersionInfo;
 
+import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.junit.HapiTestSuite;
 import com.hedera.services.bdd.spec.HapiSpec;
 import com.hedera.services.bdd.suites.BddTestNameDoesNotMatchMethodName;
@@ -73,6 +74,7 @@ public class VersionInfoSpec extends HapiSuite {
     }
 
     @BddTestNameDoesNotMatchMethodName
+    @HapiTest
     private HapiSpec discoversExpectedVersions() {
         if (specConfig != null) {
             return customHapiSpec("getVersionInfo")
