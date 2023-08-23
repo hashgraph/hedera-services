13fdf5631455f7850223d5432b5237b966a38540
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2023-08-23 13:44:31 +0300

    feature: add the throttle definition configuration to the FE throttling
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/throttle/MonoThrottleAccumulatorTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/throttle/MonoThrottleAccumulatorTest.java
@@ -18,19 +18,28 @@ package com.hedera.node.app.throttle;
 
 import static com.hedera.hapi.node.base.HederaFunctionality.CRYPTO_TRANSFER;
 import static com.hedera.node.app.service.mono.pbj.PbjConverter.fromPbj;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.google.protobuf.InvalidProtocolBufferException;
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.HederaFunctionality;
 import com.hedera.hapi.node.base.TransactionID;
 import com.hedera.hapi.node.token.CryptoTransferTransactionBody;
 import com.hedera.hapi.node.transaction.Query;
+import com.hedera.hapi.node.transaction.ThrottleBucket;
+import com.hedera.hapi.node.transaction.ThrottleDefinitions;
+import com.hedera.hapi.node.transaction.ThrottleGroup;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.service.mono.throttling.FunctionalityThrottling;
 import com.hedera.node.app.service.mono.utils.accessors.TxnAccessor;
+import com.hedera.pbj.runtime.io.buffer.Bytes;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,28 +49,58 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class MonoThrottleAccumulatorTest {
+
     @Mock
     private FunctionalityThrottling hapiThrottling;
 
     private MonoThrottleAccumulator subject;
 
+    @Mock
+    private ThrottleManager throttleManager;
+
+    ThrottleGroup throttleGroup = ThrottleGroup.newBuilder()
+        .operations(List.of(HederaFunctionality.CRYPTO_CREATE, HederaFunctionality.FREEZE))
+        .milliOpsPerSec(100)
+        .build();
+
+    ThrottleBucket throttleBucket = ThrottleBucket.newBuilder()
+        .name("throttle1")
+        .burstPeriodMs(100L)
+        .throttleGroups(throttleGroup)
+        .build();
+
+    ThrottleDefinitions throttleDefinitions = com.hedera.hapi.node.transaction.ThrottleDefinitions.newBuilder()
+        .throttleBuckets(throttleBucket)
+        .build();
+    Bytes throttleDefinitionsByes =
+        com.hedera.hapi.node.transaction.ThrottleDefinitions.PROTOBUF.toBytes(throttleDefinitions);
+
     @BeforeEach
-    void setUp() {
-        subject = new MonoThrottleAccumulator(hapiThrottling);
+    void setUp() throws InvalidProtocolBufferException {
+        com.hederahashgraph.api.proto.java.ThrottleDefinitions throttleDefinitionsProto =
+            com.hederahashgraph.api.proto.java.ThrottleDefinitions.parseFrom(throttleDefinitionsByes.toByteArray());
+        when(throttleManager.throttleDefinitionsProto()).thenReturn(throttleDefinitionsProto);
+
+        subject = new MonoThrottleAccumulator(hapiThrottling, throttleManager);
+    }
+
+    @Test
+    void verifyTheConstructorIsConfiguringTheThrottleDefinitions() {
+        verify(throttleManager, times(1)).throttleDefinitionsProto();
+        verify(hapiThrottling, times(1)).rebuildFor(any());
     }
 
     @Test
     void delegatesToMonoThrottlingForTransactions() {
         final ArgumentCaptor<TxnAccessor> captor = ArgumentCaptor.forClass(TxnAccessor.class);
 
-        final var txnFunction = CRYPTO_TRANSFER;
         given(hapiThrottling.shouldThrottleTxn(any())).willReturn(true);
 
         assertTrue(subject.shouldThrottle(TRANSACTION_BODY));
 
         verify(hapiThrottling).shouldThrottleTxn(captor.capture());
         final var throttledFunction = captor.getValue().getFunction();
-        assertEquals(fromPbj(txnFunction), throttledFunction);
+        assertEquals(fromPbj(CRYPTO_TRANSFER), throttledFunction);
     }
 
     @Test
