6024f56fa194630ae9352af8b2eeaad8eb957718
Lev Povolotsky <16233475+povolev15@users.noreply.github.com>
2023-08-28 09:47:02 -0400

    fix getFileMetadata and getFileLeaf that the param will be @NonNull (#8210)
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileUpdateHandler.java
+++ hedera-node/hedera-file-service-impl/src/main/java/com/hedera/node/app/service/file/impl/handlers/FileUpdateHandler.java
@@ -80,9 +80,10 @@ public class FileUpdateHandler implements TransactionHandler {
         requireNonNull(context);
         final var transactionBody = context.body().fileUpdateOrThrow();
         final var fileStore = context.createStore(ReadableFileStore.class);
-        preValidate(transactionBody.fileID(), fileStore, context, false);
+        final var transactionFileId = requireNonNull(transactionBody.fileID());
+        preValidate(transactionFileId, fileStore, context, false);
 
-        var file = fileStore.getFileLeaf(transactionBody.fileID());
+        var file = fileStore.getFileLeaf(transactionFileId);
         validateAndAddRequiredKeys(file, transactionBody.keys(), context);
     }
 
