fd20737b194d76107b3842f8f0107abf60838f89
Richard Bair <rbair23@users.noreply.github.com>
2023-08-28 15:28:26 -0700

    Remove unused variable
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/TokenServiceImpl.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/TokenServiceImpl.java
@@ -16,47 +16,15 @@
 
 package com.hedera.node.app.service.token.impl;
 
-import static com.hedera.node.app.service.token.impl.handlers.BaseCryptoHandler.asAccount;
-import static com.hedera.node.app.spi.HapiUtils.EMPTY_KEY_LIST;
-import static com.hedera.node.app.spi.HapiUtils.FUNDING_ACCOUNT_EXPIRY;
 import static java.util.Objects.requireNonNull;
 
-import com.hedera.hapi.node.base.AccountID;
-import com.hedera.hapi.node.base.Key;
-import com.hedera.hapi.node.base.NftID;
-import com.hedera.hapi.node.base.SemanticVersion;
-import com.hedera.hapi.node.base.TokenID;
-import com.hedera.hapi.node.state.common.EntityIDPair;
-import com.hedera.hapi.node.state.common.EntityNumber;
-import com.hedera.hapi.node.state.primitives.ProtoBytes;
-import com.hedera.hapi.node.state.token.Account;
-import com.hedera.hapi.node.state.token.NetworkStakingRewards;
-import com.hedera.hapi.node.state.token.Nft;
-import com.hedera.hapi.node.state.token.StakingNodeInfo;
-import com.hedera.hapi.node.state.token.Token;
-import com.hedera.hapi.node.state.token.TokenRelation;
 import com.hedera.node.app.service.token.TokenService;
-import com.hedera.node.app.spi.state.MigrationContext;
-import com.hedera.node.app.spi.state.Schema;
+import com.hedera.node.app.service.token.impl.schemas.GenesisSchema;
 import com.hedera.node.app.spi.state.SchemaRegistry;
-import com.hedera.node.app.spi.state.StateDefinition;
-import com.hedera.node.app.spi.state.WritableKVState;
-import com.hedera.node.config.data.AccountsConfig;
-import com.hedera.node.config.data.BootstrapConfig;
-import com.hedera.node.config.data.HederaConfig;
-import com.hedera.node.config.data.LedgerConfig;
-import com.hedera.node.config.data.StakingConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.Arrays;
-import java.util.Set;
 
 /** An implementation of the {@link TokenService} interface. */
 public class TokenServiceImpl implements TokenService {
-    private static final int MAX_ACCOUNTS = 1024;
-    private static final int MAX_TOKEN_RELS = 1042;
-    private static final int MAX_MINTABLE_NFTS = 4096;
-    private static final SemanticVersion GENESIS_VERSION = SemanticVersion.DEFAULT;
-
     public static final String NFTS_KEY = "NFTS";
     public static final String TOKENS_KEY = "TOKENS";
     public static final String ALIASES_KEY = "ALIASES";
@@ -68,133 +36,6 @@ public class TokenServiceImpl implements TokenService {
     @Override
     public void registerSchemas(@NonNull SchemaRegistry registry) {
         requireNonNull(registry);
-        registry.register(tokenSchema());
-    }
-
-    private Schema tokenSchema() {
-        // Everything on disk that can be
-        return new Schema(GENESIS_VERSION) {
-            @NonNull
-            @Override
-            public Set<StateDefinition> statesToCreate() {
-                return Set.of(
-                        StateDefinition.inMemory(TOKENS_KEY, TokenID.PROTOBUF, Token.PROTOBUF),
-                        StateDefinition.onDisk(ACCOUNTS_KEY, AccountID.PROTOBUF, Account.PROTOBUF, MAX_ACCOUNTS),
-                        StateDefinition.onDisk(ALIASES_KEY, ProtoBytes.PROTOBUF, AccountID.PROTOBUF, MAX_ACCOUNTS),
-                        StateDefinition.onDisk(NFTS_KEY, NftID.PROTOBUF, Nft.PROTOBUF, MAX_MINTABLE_NFTS),
-                        StateDefinition.onDisk(
-                                TOKEN_RELS_KEY, EntityIDPair.PROTOBUF, TokenRelation.PROTOBUF, MAX_TOKEN_RELS),
-                        StateDefinition.inMemory(STAKING_INFO_KEY, EntityNumber.PROTOBUF, StakingNodeInfo.PROTOBUF),
-                        StateDefinition.singleton(STAKING_NETWORK_REWARDS_KEY, NetworkStakingRewards.PROTOBUF));
-            }
-
-            @Override
-            public void migrate(@NonNull MigrationContext ctx) {
-                // TBD Verify this is correct. We need to preload all the special accounts
-                final var accounts = ctx.newStates().get(ACCOUNTS_KEY);
-                final var accountsConfig = ctx.configuration().getConfigData(AccountsConfig.class);
-                final var bootstrapConfig = ctx.configuration().getConfigData(BootstrapConfig.class);
-                final var ledgerConfig = ctx.configuration().getConfigData(LedgerConfig.class);
-                final var hederaConfig = ctx.configuration().getConfigData(HederaConfig.class);
-                final var superUserKeyBytes = bootstrapConfig.genesisPublicKey();
-                if (superUserKeyBytes.length() != 32) {
-                    throw new IllegalStateException("'" + superUserKeyBytes + "' is not a possible Ed25519 public key");
-                }
-                final var superUserKey =
-                        Key.newBuilder().ed25519(superUserKeyBytes).build();
-
-                final var numSystemAccounts = ledgerConfig.numSystemAccounts();
-                final var expiry = bootstrapConfig.systemEntityExpiry();
-                final var tinyBarFloat = ledgerConfig.totalTinyBarFloat();
-
-                for (long num = 1; num <= numSystemAccounts; num++) {
-                    final var id = AccountID.newBuilder()
-                            .shardNum(hederaConfig.shard())
-                            .realmNum(hederaConfig.realm())
-                            .accountNum(num)
-                            .build();
-
-                    if (accounts.contains(id)) {
-                        continue;
-                    }
-
-                    final var accountTinyBars = num == accountsConfig.treasury() ? tinyBarFloat : 0L;
-                    assert accountTinyBars >= 0L : "Negative account balance!";
-
-                    accounts.put(
-                            id,
-                            Account.newBuilder()
-                                    .receiverSigRequired(false)
-                                    .deleted(false)
-                                    .expirationSecond(expiry)
-                                    .memo("")
-                                    .smartContract(false)
-                                    .key(superUserKey)
-                                    .autoRenewSeconds(expiry) // TODO is this right?
-                                    .accountId(id)
-                                    .tinybarBalance(accountTinyBars)
-                                    .build());
-                }
-                addStakingAccounts(asAccount(800), accounts, FUNDING_ACCOUNT_EXPIRY);
-                addStakingAccounts(asAccount(801), accounts, FUNDING_ACCOUNT_EXPIRY);
-
-                updateNetworkRewards(ctx);
-                updateStakingNodeInfo(ctx);
-            }
-        };
-    }
-
-    private void updateStakingNodeInfo(final MigrationContext ctx) {
-        // TODO: This need to go through address book and set all the nodes
-        final var config = ctx.configuration();
-        final var ledgerConfig = config.getConfigData(LedgerConfig.class);
-        final var stakingConfig = config.getConfigData(StakingConfig.class);
-        final var numberOfNodes = 1;
-
-        final long maxStakePerNode = ledgerConfig.totalTinyBarFloat() / numberOfNodes;
-        final long minStakePerNode = maxStakePerNode / 2;
-
-        final var numRewardHistoryStoredPeriods = stakingConfig.rewardHistoryNumStoredPeriods();
-        final var stakingInfoState = ctx.newStates().get(STAKING_INFO_KEY);
-        final var rewardSumHistory = new Long[numRewardHistoryStoredPeriods];
-        Arrays.fill(rewardSumHistory, 0L);
-
-        final var stakingInfo = StakingNodeInfo.newBuilder()
-                .nodeNumber(0)
-                .maxStake(maxStakePerNode)
-                .minStake(minStakePerNode)
-                .rewardSumHistory(Arrays.asList(rewardSumHistory))
-                .weight(500)
-                .build();
-        stakingInfoState.put(EntityNumber.newBuilder().number(0L).build(), stakingInfo);
-    }
-
-    private void updateNetworkRewards(final MigrationContext ctx) {
-        // Set genesis network rewards state
-        final var networkRewardsState = ctx.newStates().getSingleton(STAKING_NETWORK_REWARDS_KEY);
-        final var networkRewards = NetworkStakingRewards.newBuilder()
-                .pendingRewards(0)
-                .totalStakedRewardStart(0)
-                .totalStakedStart(0)
-                .stakingRewardsActivated(true)
-                .build();
-        networkRewardsState.put(networkRewards);
-    }
-
-    private void addStakingAccounts(
-            final AccountID id, final WritableKVState<Object, Object> accounts, final long expiry) {
-        accounts.put(
-                id,
-                Account.newBuilder()
-                        .receiverSigRequired(false)
-                        .deleted(false)
-                        .memo("")
-                        .smartContract(false)
-                        .key(EMPTY_KEY_LIST)
-                        .expirationSecond(expiry)
-                        .accountId(id)
-                        .maxAutoAssociations(0)
-                        .tinybarBalance(0)
-                        .build());
+        registry.register(new GenesisSchema());
     }
 }
