59e23fd5ad57863e5e7712cbec2ca6127de7ebd8
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2023-09-04 14:54:20 +0300

    Update hedera-node/settings.gradles.kts instead of root one
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- hedera-node/settings.gradle.kts
+++ hedera-node/settings.gradle.kts
@@ -82,14 +82,15 @@ fun include(name: String, path: String) {
 // The HAPI API version to use for Protobuf sources. This can be a tag or branch
 // name from the hedera-protobufs GIT repo.
 val hapiProtoVersion = "0.40.0-blocks-state-SNAPSHOT"
-val hapiProtoBranchOrTag = "add-pbj-types-for-state" // hapiProtoVersion
+val hapiProtoBranchOrTag = "add-throttle-and-congestion-types" // hapiProtoVersion
 
 gitRepositories {
     checkoutsDirectory.set(File(rootDir, "hapi"))
     // check branch in repo for updates every second
     refreshIntervalMillis.set(1000)
     include("hedera-protobufs") {
-        uri.set("https://github.com/hashgraph/hedera-protobufs.git")
+        uri.set("https://github.com/LimeChain/hedera-protobufs.git") // TODO: Temporary override for Limechain fork, revert when
+        // https://github.com/hashgraph/hedera-protobufs/pull/304 is merged
         // HAPI repo version
         tag.set(hapiProtoBranchOrTag)
         // do not load project from repo
