664a248f7a7b6008e8f8f97dc4a113a55f9b196b
Michael Tinker <michael.tinker@swirldslabs.com>
2023-09-05 19:03:02 -0500

    Implement `HtsSystemContract` skeleton and `balanceOf()` view redirect (#8325)
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>
    Signed-off-by: Richard Bair <rbair23@users.noreply.github.com>
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: lukelee-sl <luke.lee@swirldslabs.com>
    Co-authored-by: Richard Bair <rbair23@users.noreply.github.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/TestHelpers.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/TestHelpers.java
@@ -39,6 +39,7 @@ import com.hedera.hapi.node.contract.EthereumTransactionBody;
 import com.hedera.hapi.node.state.common.EntityNumber;
 import com.hedera.hapi.node.state.contract.Bytecode;
 import com.hedera.hapi.node.state.token.Token;
+import com.hedera.hapi.node.state.token.TokenRelation;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.hapi.streams.CallOperationType;
 import com.hedera.hapi.streams.ContractAction;
@@ -64,6 +65,7 @@ import com.swirlds.config.api.Configuration;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
@@ -165,8 +167,16 @@ public class TestHelpers {
             FileID.newBuilder().fileNum(7890L).build();
     public static final TokenID FUNGIBLE_TOKEN_ID =
             TokenID.newBuilder().tokenNum(9876L).build();
-    public static final Token FUNGIBLE_TOKEN =
-            Token.newBuilder().tokenType(TokenType.FUNGIBLE_COMMON).build();
+    public static final TokenID NON_FUNGIBLE_TOKEN_ID =
+            TokenID.newBuilder().tokenNum(9898L).build();
+    public static final Token FUNGIBLE_TOKEN = Token.newBuilder()
+            .tokenId(FUNGIBLE_TOKEN_ID)
+            .tokenType(TokenType.FUNGIBLE_COMMON)
+            .build();
+    public static final Token NON_FUNGIBLE_TOKEN = Token.newBuilder()
+            .tokenId(NON_FUNGIBLE_TOKEN_ID)
+            .tokenType(TokenType.NON_FUNGIBLE_UNIQUE)
+            .build();
     public static final AccountID NON_SYSTEM_ACCOUNT_ID = AccountID.newBuilder()
             .accountNum(numberOfLongZero(NON_SYSTEM_LONG_ZERO_ADDRESS))
             .build();
@@ -181,6 +191,11 @@ public class TestHelpers {
             .contractNum(numberOfLongZero(NON_SYSTEM_LONG_ZERO_ADDRESS))
             .build();
     public static final Address EIP_1014_ADDRESS = Address.fromHexString("0x89abcdef89abcdef89abcdef89abcdef89abcdef");
+    public static final TokenRelation A_FUNGIBLE_RELATION = TokenRelation.newBuilder()
+            .tokenId(FUNGIBLE_TOKEN_ID)
+            .accountId(A_NEW_ACCOUNT_ID)
+            .balance(123L)
+            .build();
     public static final Bytes CANONICAL_ALIAS = tuweniToPbjBytes(EIP_1014_ADDRESS);
     public static final ContractID CALLED_CONTRACT_EVM_ADDRESS =
             ContractID.newBuilder().evmAddress(CANONICAL_ALIAS).build();
@@ -405,5 +420,20 @@ public class TestHelpers {
             @NonNull final Runnable something, @NonNull final ResponseCodeEnum status) {
         final var ex = assertThrows(ResourceExhaustedException.class, something::run);
         assertEquals(status, ex.getStatus());
+    }
+
+    public static com.esaulpaugh.headlong.abi.Address asHeadlongAddress(final Address address) {
+        return asHeadlongAddress(address.toArrayUnsafe());
+    }
+
+    public static com.esaulpaugh.headlong.abi.Address asHeadlongAddress(final byte[] address) {
+        final var addressBytes = org.apache.tuweni.bytes.Bytes.wrap(address);
+        final var addressAsInteger = addressBytes.toUnsignedBigInteger();
+        return com.esaulpaugh.headlong.abi.Address.wrap(
+                com.esaulpaugh.headlong.abi.Address.toChecksumAddress(addressAsInteger));
+    }
+
+    public static org.apache.tuweni.bytes.Bytes revertOutputFor(final ResponseCodeEnum status) {
+        return org.apache.tuweni.bytes.Bytes.wrap(status.protoName().getBytes(StandardCharsets.UTF_8));
     }
 }
