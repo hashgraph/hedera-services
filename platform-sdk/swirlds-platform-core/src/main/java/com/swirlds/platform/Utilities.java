9f7bc491a9043a5b6fc6e88453dba0cd91156445
Edward Wertz <edward@swirldslabs.com>
2023-09-11 15:17:42 -0500

    07762 - Adding NonNull Annotations.
    Signed-off-by: Edward Wertz <edward@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/Utilities.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/Utilities.java
@@ -20,6 +20,7 @@ import com.swirlds.common.io.streams.SerializableDataInputStream;
 import com.swirlds.common.io.streams.SerializableDataOutputStream;
 import com.swirlds.platform.internal.Deserializer;
 import com.swirlds.platform.internal.Serializer;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.IOException;
 import java.net.SocketException;
 import java.util.Arrays;
@@ -334,14 +335,14 @@ public final class Utilities {
     /**
      * Checks all nesting of causes for any instance of the supplied type.
      *
-     * @param t
+     * @param throwable
      * 		the throwable to unwrap
      * @param type
      * 		the type to check against
      * @return true if any of the causes matches the supplied type, false otherwise.
      */
-    public static boolean hasAnyCauseSuppliedType(final Throwable t, final Class<? extends Throwable> type) {
-        Throwable cause = t;
+    public static boolean hasAnyCauseSuppliedType(@NonNull final Throwable throwable, @NonNull final Class<? extends Throwable> type) {
+        Throwable cause = throwable;
         // check all causes
         while (cause != null) {
             if (type.isInstance(cause)) {
