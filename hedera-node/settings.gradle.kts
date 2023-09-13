f5bb488b0e8c188bc4d6e4334fc488c95b363579
Kim Rader <kim.rader@swirldslabs.com>
2023-09-13 08:45:28 -0700

    Fix UnsupportedQueriesRegression HapiSuite (#8536)
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/settings.gradle.kts
+++ hedera-node/settings.gradle.kts
@@ -82,7 +82,8 @@ fun include(name: String, path: String) {
 // The HAPI API version to use for Protobuf sources. This can be a tag or branch
 // name from the hedera-protobufs GIT repo.
 val hapiProtoVersion = "0.40.0-blocks-state-SNAPSHOT"
-val hapiProtoBranchOrTag = "add-pbj-types-for-state" // hapiProtoVersion
+// val hapiProtoBranchOrTag = "add-pbj-types-for-state" // hapiProtoVersion
+val hapiProtoBranchOrTag = "7680-Get-Fast-Record" // temporary branch
 
 gitRepositories {
     checkoutsDirectory.set(File(rootDir, "hapi"))
