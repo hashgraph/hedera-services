f810279ba5e2e202e77170a8ef4d6216ebc24de2
Michael Tinker <michael.tinker@swirldslabs.com>
2023-09-19 14:47:54 -0500

    Add javadoc
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/workflows/record/DeleteCapableTransactionRecordBuilder.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/workflows/record/DeleteCapableTransactionRecordBuilder.java
@@ -17,9 +17,24 @@
 package com.hedera.node.app.spi.workflows.record;
 
 import com.hedera.hapi.node.base.AccountID;
+import com.hedera.hapi.node.base.HederaFunctionality;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 
+/**
+ * Base record builder for any transaction that can delete accounts or contracts. These include,
+ * <ol>
+ *     <li>{@link HederaFunctionality#CRYPTO_DELETE}</li>
+ *     <li>{@link HederaFunctionality#CONTRACT_DELETE}</li>
+ *     <li>{@link HederaFunctionality#CONTRACT_CREATE}</li>
+ *     <li>{@link HederaFunctionality#CONTRACT_CALL}</li>
+ *     <li>{@link HederaFunctionality#ETHEREUM_TRANSACTION}</li>
+ * </ol>
+ *
+ * <p>We need to track the beneficiary account id for each deleted account id because if a
+ * just-deleted account was going to receive staking rewards in the transaction, those rewards
+ * should be redirected to the beneficiary account.
+ */
 public interface DeleteCapableTransactionRecordBuilder extends SingleTransactionRecordBuilder {
     /**
      * Gets number of deleted accounts in this transaction.
