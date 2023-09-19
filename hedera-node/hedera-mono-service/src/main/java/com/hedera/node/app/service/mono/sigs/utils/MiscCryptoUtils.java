366149acac1da3132d5a63860120b0de3fa41e1b
jasperpotts <jasperpotts@users.noreply.github.com>
2023-09-19 16:30:20 -0700

    Change 65 to constants
    Signed-off-by: jasperpotts <jasperpotts@users.noreply.github.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/sigs/utils/MiscCryptoUtils.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/sigs/utils/MiscCryptoUtils.java
@@ -35,7 +35,10 @@ public class MiscCryptoUtils {
             ByteBuffer uncompressedPublicKeyByteBuffer,
             LongByReference length) {
         public ThreadLocalCache() {
-            this(new LibSecp256k1.secp256k1_pubkey(), new byte[65], ByteBuffer.allocate(65), new LongByReference());
+            this(new LibSecp256k1.secp256k1_pubkey(),
+                    new byte[ECDSA_UNCOMPRESSED_KEY_SIZE_WITH_HEADER_BYTE],
+                    ByteBuffer.allocate(ECDSA_UNCOMPRESSED_KEY_SIZE_WITH_HEADER_BYTE),
+                    new LongByReference());
             // set the type header byte for uncompressed public keys, this is always the same
             uncompressedPublicKeyInput[0] = 0x04;
         }
