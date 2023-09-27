747c78ecf5039500b267293927b78e13326bc491
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2023-09-27 10:59:26 +0300

    test: add more tests
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/grantapproval/GrantApprovalTranslatorTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/grantapproval/GrantApprovalTranslatorTest.java
@@ -16,14 +16,33 @@
 
 package com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.hts.grantapproval;
 
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.FUNGIBLE_TOKEN_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.FUNGIBLE_TOKEN_ID;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.NON_FUNGIBLE_TOKEN_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.NON_FUNGIBLE_TOKEN_ID;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.OWNER_BESU_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.OWNER_ID;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.UNAUTHORIZED_SPENDER_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.UNAUTHORIZED_SPENDER_ID;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import com.esaulpaugh.headlong.abi.Tuple;
+import com.hedera.hapi.node.base.TokenType;
+import com.hedera.node.app.service.contract.impl.exec.scope.VerificationStrategy;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.AddressIdConverter;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.DispatchForResponseCodeHtsCall;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCallAttempt;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.grantapproval.ERCGrantApprovalCall;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.grantapproval.GrantApprovalDecoder;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.grantapproval.GrantApprovalTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.transfer.ClassicTransfersTranslator;
+import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
+import java.math.BigInteger;
+import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -35,6 +54,15 @@ class GrantApprovalTranslatorTest {
 
     @Mock
     private HtsCallAttempt attempt;
+
+    @Mock
+    private AddressIdConverter addressIdConverter;
+
+    @Mock
+    private HederaWorldUpdater.Enhancement enhancement;
+
+    @Mock
+    private VerificationStrategy verificationStrategy;
 
     private final GrantApprovalDecoder decoder = new GrantApprovalDecoder();
     private GrantApprovalTranslator subject;
@@ -59,6 +87,13 @@ class GrantApprovalTranslatorTest {
     }
 
     @Test
+    void ERCGrantApprovalNFTMatches() {
+        given(attempt.selector()).willReturn(GrantApprovalTranslator.ERC_GRANT_APPROVAL_NFT.selector());
+        final var matches = subject.matches(attempt);
+        assertTrue(matches);
+    }
+
+    @Test
     void grantApprovalNFTMatches() {
         given(attempt.selector()).willReturn(GrantApprovalTranslator.GRANT_APPROVAL_NFT.selector());
         final var matches = subject.matches(attempt);
@@ -70,5 +105,79 @@ class GrantApprovalTranslatorTest {
         given(attempt.selector()).willReturn(ClassicTransfersTranslator.CRYPTO_TRANSFER.selector());
         final var matches = subject.matches(attempt);
         assertFalse(matches);
+    }
+
+    @Test
+    void callFromHapiFungible() {
+        final Tuple tuple = new Tuple(
+                FUNGIBLE_TOKEN_HEADLONG_ADDRESS, UNAUTHORIZED_SPENDER_HEADLONG_ADDRESS, BigInteger.valueOf(123L));
+        final byte[] inputBytes = Bytes.wrapByteBuffer(GrantApprovalTranslator.GRANT_APPROVAL.encodeCall(tuple))
+                .toArray();
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convert(any())).willReturn(UNAUTHORIZED_SPENDER_ID);
+        given(attempt.addressIdConverter().convertSender(any())).willReturn(OWNER_ID);
+        given(attempt.senderAddress()).willReturn(OWNER_BESU_ADDRESS);
+        given(attempt.enhancement()).willReturn(enhancement);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.selector()).willReturn(GrantApprovalTranslator.GRANT_APPROVAL.selector());
+        given(attempt.inputBytes()).willReturn(inputBytes);
+
+        final var call = subject.callFrom(attempt);
+        assertInstanceOf(DispatchForResponseCodeHtsCall.class, call);
+    }
+
+    @Test
+    void callFromHapiNonFungible() {
+        final Tuple tuple = new Tuple(
+                NON_FUNGIBLE_TOKEN_HEADLONG_ADDRESS, UNAUTHORIZED_SPENDER_HEADLONG_ADDRESS, BigInteger.valueOf(123L));
+        final byte[] inputBytes = Bytes.wrapByteBuffer(GrantApprovalTranslator.GRANT_APPROVAL_NFT.encodeCall(tuple))
+                .toArray();
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convert(any())).willReturn(UNAUTHORIZED_SPENDER_ID);
+        given(attempt.addressIdConverter().convertSender(any())).willReturn(OWNER_ID);
+        given(attempt.senderAddress()).willReturn(OWNER_BESU_ADDRESS);
+        given(attempt.enhancement()).willReturn(enhancement);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.selector()).willReturn(GrantApprovalTranslator.GRANT_APPROVAL_NFT.selector());
+        given(attempt.inputBytes()).willReturn(inputBytes);
+
+        final var call = subject.callFrom(attempt);
+        assertInstanceOf(DispatchForResponseCodeHtsCall.class, call);
+    }
+
+    @Test
+    void callFromERCFungible() {
+        final Tuple tuple = new Tuple(UNAUTHORIZED_SPENDER_HEADLONG_ADDRESS, BigInteger.valueOf(123L));
+        final byte[] inputBytes = Bytes.wrapByteBuffer(GrantApprovalTranslator.ERC_GRANT_APPROVAL.encodeCall(tuple))
+                .toArray();
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convert(any())).willReturn(UNAUTHORIZED_SPENDER_ID);
+        given(attempt.redirectTokenId()).willReturn(FUNGIBLE_TOKEN_ID);
+        given(attempt.redirectTokenType()).willReturn(TokenType.FUNGIBLE_COMMON);
+        given(attempt.enhancement()).willReturn(enhancement);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.selector()).willReturn(GrantApprovalTranslator.ERC_GRANT_APPROVAL.selector());
+        given(attempt.inputBytes()).willReturn(inputBytes);
+
+        final var call = subject.callFrom(attempt);
+        assertInstanceOf(ERCGrantApprovalCall.class, call);
+    }
+
+    @Test
+    void callFromERCNonFungible() {
+        final Tuple tuple = new Tuple(UNAUTHORIZED_SPENDER_HEADLONG_ADDRESS, BigInteger.valueOf(123L));
+        final byte[] inputBytes = Bytes.wrapByteBuffer(GrantApprovalTranslator.ERC_GRANT_APPROVAL_NFT.encodeCall(tuple))
+                .toArray();
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convert(any())).willReturn(UNAUTHORIZED_SPENDER_ID);
+        given(attempt.redirectTokenId()).willReturn(NON_FUNGIBLE_TOKEN_ID);
+        given(attempt.redirectTokenType()).willReturn(TokenType.NON_FUNGIBLE_UNIQUE);
+        given(attempt.enhancement()).willReturn(enhancement);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.selector()).willReturn(GrantApprovalTranslator.ERC_GRANT_APPROVAL_NFT.selector());
+        given(attempt.inputBytes()).willReturn(inputBytes);
+
+        final var call = subject.callFrom(attempt);
+        assertInstanceOf(ERCGrantApprovalCall.class, call);
     }
 }
