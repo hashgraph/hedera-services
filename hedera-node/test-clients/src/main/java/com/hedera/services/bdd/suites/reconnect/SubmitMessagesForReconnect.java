f9fbdcd7bf770864c343b46ef109b265fd1430c5
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-09-29 11:02:59 -0500

    Merge
    Signed-off-by: Jeffrey Tang <jeffrey@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/SubmitMessagesForReconnect.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/SubmitMessagesForReconnect.java
@@ -21,6 +21,7 @@ import static com.hedera.services.bdd.spec.transactions.TxnUtils.randomUtf8Bytes
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.submitMessageTo;
 import static com.hedera.services.bdd.spec.utilops.LoadTest.defaultLoadTest;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.logIt;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.sleepFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.BUSY;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.PLATFORM_TRANSACTION_NOT_CREATED;
@@ -67,7 +68,8 @@ public class SubmitMessagesForReconnect extends HapiSuite {
                                 (spec, ignore) -> settings.setFrom(spec.setup().ciPropertiesMap())),
                         logIt(ignore -> settings.toString()))
                 .when()
-                .then(defaultLoadTest(submitBurst, settings));
+                .then(sleepFor(10000),
+                        defaultLoadTest(submitBurst, settings));
     }
 
     @Override
