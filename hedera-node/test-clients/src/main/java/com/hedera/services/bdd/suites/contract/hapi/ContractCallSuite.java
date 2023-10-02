d1c5389cf2106810a1dabfcdd3557d9ea22ad6c3
Mustafa Uzun <mustafa.uzun@limechain.tech>
2023-10-02 12:39:49 +0300

    style: formatting
    Signed-off-by: Mustafa Uzun <mustafa.uzun@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
@@ -2312,14 +2312,12 @@ public class ContractCallSuite extends HapiSuite {
                 .then(
                         getTxnRecord(failingCall)
                                 .exposingTo(failureRecord -> parentConsTime.set(failureRecord.getConsensusTimestamp())),
-                                                sourcing(() -> childRecordsCheck(
-                                                        failingCall,
-                                                        CONTRACT_REVERT_EXECUTED,
-                                                        recordWith()
-                                                                .status(INSUFFICIENT_GAS)
-                                                                .consensusTimeImpliedByNonce(parentConsTime.get(),
-                         1)))
-                        );
+                        sourcing(() -> childRecordsCheck(
+                                failingCall,
+                                CONTRACT_REVERT_EXECUTED,
+                                recordWith()
+                                        .status(INSUFFICIENT_GAS)
+                                        .consensusTimeImpliedByNonce(parentConsTime.get(), 1))));
     }
 
     private String getNestedContractAddress(final String contract, final HapiSpec spec) {
