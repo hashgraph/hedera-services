16fac80af8cd44e53597fcffcf4f0327c1a71591
Jeffrey Tang <jeffrey@swirldslabs.com>
2023-10-02 19:49:14 -0500

    save

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/ValidateCongestionPricingAfterReconnect.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/reconnect/ValidateCongestionPricingAfterReconnect.java
@@ -140,7 +140,7 @@ public class ValidateCongestionPricingAfterReconnect extends HapiSuite {
                         // then we can send more transactions. Otherwise, transactions may be
                         // pending for too long
                         // and we will get UNKNOWN status
-                        sleepFor(30000),
+                        sleepFor(80000),
                         blockingOrder(IntStream.range(0, 110)
                                 .mapToObj(i -> new HapiSpecOperation[] {
                                     usableTxnIdNamed("uncheckedTxn2" + i).payerId(civilianAccount),
