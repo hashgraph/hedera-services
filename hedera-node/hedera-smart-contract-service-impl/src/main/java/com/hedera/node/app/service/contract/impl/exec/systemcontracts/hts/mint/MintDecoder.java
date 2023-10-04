2c5a8d48153dd29c4cdc2863cb438530cafefec1
Deyan Dimitrov <dikelito@tutamail.com>
2023-10-04 04:58:26 +0300

    implement system contracts for mint (#8856)
    Signed-off-by: dikel <dikelito@tutamail.com>

---
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/mint/MintDecoder.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.mint;
+
+import com.esaulpaugh.headlong.abi.Address;
+import com.hedera.hapi.node.token.TokenMintTransactionBody;
+import com.hedera.hapi.node.transaction.TransactionBody;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCallAttempt;
+import com.hedera.node.app.service.contract.impl.utils.ConversionUtils;
+import com.hedera.pbj.runtime.io.buffer.Bytes;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class MintDecoder {
+
+    @Inject
+    public MintDecoder() {
+        // Dagger2
+    }
+
+    public TransactionBody decodeMint(@NonNull final HtsCallAttempt attempt) {
+        final var call = MintTranslator.MINT.decodeCall(attempt.inputBytes());
+        final var amount = ((BigInteger) call.get(1)).longValueExact();
+        return TransactionBody.newBuilder()
+                .tokenMint(mint(call.get(0), amount, call.get(2)))
+                .build();
+    }
+
+    public TransactionBody decodeMintV2(@NonNull final HtsCallAttempt attempt) {
+        final var call = MintTranslator.MINT_V2.decodeCall(attempt.inputBytes());
+        return TransactionBody.newBuilder()
+                .tokenMint(mint(call.get(0), call.get(1), call.get(2)))
+                .build();
+    }
+
+    private TokenMintTransactionBody mint(
+            @NonNull final Address token, final long amount, @NonNull final byte[][] metadataArray) {
+        final List<Bytes> metadata = new ArrayList<>();
+        for (final var data : metadataArray) {
+            metadata.add(Bytes.wrap(data));
+        }
+
+        return TokenMintTransactionBody.newBuilder()
+                .token(ConversionUtils.asTokenId(token))
+                .amount(amount)
+                .metadata(metadata)
+                .build();
+    }
+}
