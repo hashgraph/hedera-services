ec4a077377fca12a41670521fdd9ba862dc1ce28
Ivan Malygin <ivan@swirldslabs.com>
2023-10-06 19:37:59 -0400

    7501
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-common/src/main/java/module-info.java
+++ platform-sdk/swirlds-common/src/main/java/module-info.java
@@ -158,7 +158,6 @@ module com.swirlds.common {
             com.swirlds.platform.test,
             com.swirlds.platform.gui;
     exports com.swirlds.common.startup;
-    exports com.swirlds.common.threading.queue;
 
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.databind;
