f7b090a60a2edc87d7019e927b6eaf26f64747e2
Cody Littley <cody@swirldslabs.com>
2023-10-13 13:57:43 -0500

    Ignore PCES signature txns after ISS.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-common/src/main/java/com/swirlds/common/scratchpad/StandardScratchpad.java
+++ platform-sdk/swirlds-common/src/main/java/com/swirlds/common/scratchpad/StandardScratchpad.java
@@ -49,6 +49,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -218,6 +219,33 @@ public class Scratchpad<K extends Enum<K> & ScratchpadType> {
         try (final Locked ignored = lock.lock()) {
             operation.accept(data);
             flush();
+        }
+    }
+
+    // TODO test
+    /**
+     * Perform an arbitrary atomic operation on the scratchpad. This operation is atomic with respect to reads, writes,
+     * and other calls to this method.
+     * <p>
+     * The map provided to the operation should not be accessed after the operation returns. Doing so is not thread safe
+     * and may result in undefined behavior.
+     * <p>
+     * It is safe to keep references to the objects in the map after the operation returns as long as these objects are
+     * treated as if they are immutable. Modifying these objects in any way may cause the scratchpad to become
+     * corrupted.
+     *
+     * @param operation the operation to perform, is provided a map of all scratchpad fields to their current values. If
+     *                  a field is not present in the map, then it should be considered to have the value null. The
+     *                  return value of this operation indicates if the scratchpad was modified. This method should
+     *                  return true if the scratchpad was modified, and false otherwise. If this method modifies data
+     *                  and returns false then data may be lost after a restart.
+     */
+    public void atomicOperation(@NonNull final Function<Map<K, SelfSerializable>, Boolean> operation) {
+        try (final Locked ignored = lock.lock()) {
+            final boolean modified = operation.apply(data);
+            if (modified) {
+                flush();
+            }
         }
     }
 
