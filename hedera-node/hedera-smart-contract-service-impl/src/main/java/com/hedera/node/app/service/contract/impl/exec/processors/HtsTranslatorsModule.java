c379e54d696ab394b7975c0a487b81d982e40079
Deyan Dimitrov <dikelito@tutamail.com>
2023-10-16 16:26:58 +0300

    Implement system contract `TOKEN_UPDATE_EXPIRY` (#9185)
    Signed-off-by: dikel <dikelito@tutamail.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/processors/HtsTranslatorsModule.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/processors/HtsTranslatorsModule.java
@@ -48,6 +48,7 @@ import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.totals
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.transfer.ClassicTransfersTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.transfer.Erc20TransfersTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.transfer.Erc721TransferFromTranslator;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.update.UpdateExpiryTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.update.UpdateTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.wipe.WipeTranslator;
 import dagger.Module;
@@ -300,6 +301,13 @@ public interface HtsTranslatorsModule {
     @IntoSet
     static HtsCallTranslator provideUpdateTranslator(@NonNull final UpdateTranslator translator) {
 
+        return translator;
+    }
+
+    @Provides
+    @Singleton
+    @IntoSet
+    static HtsCallTranslator provideUpdateExpiryTranslator(@NonNull final UpdateExpiryTranslator translator) {
         return translator;
     }
 }
