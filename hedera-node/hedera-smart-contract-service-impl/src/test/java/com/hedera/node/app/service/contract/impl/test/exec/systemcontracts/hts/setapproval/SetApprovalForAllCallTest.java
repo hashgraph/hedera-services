92a4ffc4cb825b87f9cbc61bec0204bb37563ddb
Valentin Valkanov <valentin.valkanov@limechain.tech>
2023-10-17 11:08:50 +0300

    tests: test checks & conditions in SetApprovalForAllCall
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/setapproval/SetApprovalForAllCallTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/setapproval/SetApprovalForAllCallTest.java
@@ -18,6 +18,7 @@ package com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.hts.
 
 import static com.hedera.node.app.service.contract.impl.test.TestHelpers.OWNER_ID;
 import static com.hedera.node.app.service.contract.impl.test.TestHelpers.asBytesResult;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.revertOutputFor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -32,6 +33,8 @@ import com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.hts.H
 import com.hedera.node.app.service.token.records.CryptoTransferRecordBuilder;
 import java.math.BigInteger;
 import org.hyperledger.besu.evm.frame.MessageFrame;
+import org.hyperledger.besu.evm.precompile.PrecompiledContract.PrecompileContractResult;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -49,28 +52,77 @@ public class SetApprovalForAllCallTest extends HtsCallTestBase {
     @Mock
     private AddressIdConverter addressIdConverter;
 
-    @Test
-    void setApprovalForAllCall_works() {
-        // Given
+    private SetApprovalForAllCall subject;
+
+    @BeforeEach
+    void setup() {
         given(attempt.enhancement()).willReturn(mockEnhancement());
         given(attempt.addressIdConverter()).willReturn(addressIdConverter);
         given(attempt.addressIdConverter().convertSender(any())).willReturn(OWNER_ID);
 
-        final var subject =
+        subject =
                 new SetApprovalForAllCall(attempt, TransactionBody.newBuilder().build());
 
         given(systemContractOperations.dispatch(any(), any(), any(), any())).willReturn(recordBuilder);
+    }
+
+    @Test
+    void setApprovalForAllCall_works() {
+        // Given
         given(recordBuilder.status()).willReturn(ResponseCodeEnum.SUCCESS);
 
         // When
         final var result = subject.execute().fullResult().result();
 
         // Then
+        verifyResultStatus(result, ResponseCodeEnum.SUCCESS);
+    }
+
+    @Test
+    void setApprovalForAllCallBadStatus_reverts() {
+        // Given
+        given(recordBuilder.status()).willReturn(ResponseCodeEnum.ACCOUNT_DELETED);
+
+        // When
+        final var result = subject.execute().fullResult().result();
+
+        // Then
+        assertEquals(MessageFrame.State.REVERT, result.getState());
+        assertEquals(revertOutputFor(ResponseCodeEnum.ACCOUNT_DELETED), result.getOutput());
+    }
+
+    @Test
+    void setApprovalForAllCallInvalidToken_success() {
+        // Given
+        given(recordBuilder.status()).willReturn(ResponseCodeEnum.INVALID_TOKEN_ID);
+
+        // When
+        final var result = subject.execute().fullResult().result();
+
+        // Then
+        verifyResultStatus(result, ResponseCodeEnum.INVALID_TOKEN_ID);
+    }
+
+    @Test
+    void setApprovalForAllCallInvalidAccount_success() {
+        // Given
+        given(recordBuilder.status()).willReturn(ResponseCodeEnum.INVALID_ALLOWANCE_SPENDER_ID);
+
+        // When
+        final var result = subject.execute().fullResult().result();
+
+        // Then
+        assertEquals(MessageFrame.State.REVERT, result.getState());
+        assertEquals(revertOutputFor(ResponseCodeEnum.TOKEN_NOT_ASSOCIATED_TO_ACCOUNT), result.getOutput());
+    }
+
+    private static void verifyResultStatus(
+            final PrecompileContractResult result, final ResponseCodeEnum expectedStatus) {
         assertEquals(MessageFrame.State.COMPLETED_SUCCESS, result.getState());
         assertEquals(
                 asBytesResult(SetApprovalForAllTranslator.SET_APPROVAL_FOR_ALL
                         .getOutputs()
-                        .encodeElements(BigInteger.valueOf(ResponseCodeEnum.SUCCESS.protoOrdinal()))),
+                        .encodeElements(BigInteger.valueOf(expectedStatus.protoOrdinal()))),
                 result.getOutput());
     }
 }
