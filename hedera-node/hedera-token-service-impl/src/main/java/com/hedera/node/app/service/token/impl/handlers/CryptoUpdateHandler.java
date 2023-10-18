9d988df418090d6142a2b4a78e70eeaaff9aa8b3
Iris Simon <iris.simon@swirldslabs.com>
2023-10-18 15:49:00 -0400

    Enabled tests in AutoAccountUpdateSuite. Added Fee debug line to check inconsistent INSUFFICIENT_TX_FEE error.
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoUpdateHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoUpdateHandler.java
@@ -409,9 +409,15 @@ public class CryptoUpdateHandler extends BaseCryptoHandler implements Transactio
                 Math.max(explicitAutoAssocSlotLifetime, oldLifetime),
                 newSlotsUsage,
                 Math.max(explicitAutoAssocSlotLifetime, newLifetime));
-        return feeCalculator
+        var fees = feeCalculator
                 .addBytesPerTransaction(baseSize)
                 .addRamByteSeconds((rbsDelta + slotRbsDelta) > 0 ? rbsDelta + slotRbsDelta : 0)
                 .calculate();
+        System.out.println("Fees.networkFee: " + fees.networkFee() + " txBody: " + body);
+        return fees;
+        //        return feeCalculator
+        //                .addBytesPerTransaction(baseSize)
+        //                .addRamByteSeconds((rbsDelta + slotRbsDelta) > 0 ? rbsDelta + slotRbsDelta : 0)
+        //                .calculate();
     }
 }
