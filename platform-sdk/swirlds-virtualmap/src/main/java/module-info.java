21f4afdb948ae2db4bd54aabc7627c9f19aea58e
Timo Brandstätter <timo@swirldslabs.com>
2023-10-26 15:53:28 +0200

    Remove java.sql dependency
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-virtualmap/src/main/java/module-info.java
+++ platform-sdk/swirlds-virtualmap/src/main/java/module-info.java
@@ -14,7 +14,5 @@ open module com.swirlds.virtualmap {
     requires com.swirlds.logging;
     requires org.apache.logging.log4j;
     requires java.management; // Test dependency
-    requires java.sql;
-    requires org.apache.logging.log4j;
     requires static com.github.spotbugs.annotations;
 }
