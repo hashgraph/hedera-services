60c0cfea36c22760fa85c72c8243ff36be7f3fd9
Michael Heinrichs <netopyr@users.noreply.github.com>
2023-11-01 11:01:43 +0100

    Add support for reversible preceding transactions (no unit tests yet)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/HandleContextImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/HandleContextImplTest.java
@@ -89,6 +89,7 @@ import com.swirlds.config.api.Configuration;
 import java.lang.reflect.InvocationTargetException;
 import java.time.Instant;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -739,7 +740,7 @@ class HandleContextImplTest extends StateTestBase implements Scenarios {
 
         @BeforeEach
         void setup() {
-            final var baseKVState = new MapWritableKVState<>(FRUIT_STATE_KEY, BASE_DATA);
+            final var baseKVState = new MapWritableKVState<>(FRUIT_STATE_KEY, new HashMap<>(BASE_DATA));
             final var writableStates =
                     MapWritableStates.builder().state(baseKVState).build();
             when(baseState.createReadableStates(FOOD_SERVICE)).thenReturn(writableStates);
--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/stack/SavepointStackImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/workflows/handle/stack/SavepointStackImplTest.java
@@ -45,21 +45,21 @@ class SavepointStackImplTest extends StateTestBase {
 
     private static final String FOOD_SERVICE = "FOOD_SERVICE";
 
-    private final Map<String, String> BASE_DATA = new HashMap<>(Map.of(
+    private final Map<String, String> BASE_DATA = Map.of(
             A_KEY, APPLE,
             B_KEY, BANANA,
             C_KEY, CHERRY,
             D_KEY, DATE,
             E_KEY, EGGPLANT,
             F_KEY, FIG,
-            G_KEY, GRAPE));
+            G_KEY, GRAPE);
 
     @Mock(strictness = LENIENT)
     private HederaState baseState;
 
     @BeforeEach
     void setup() {
-        final var baseKVState = new MapWritableKVState<>(FRUIT_STATE_KEY, BASE_DATA);
+        final var baseKVState = new MapWritableKVState<>(FRUIT_STATE_KEY, new HashMap<>(BASE_DATA));
         final var writableStates =
                 MapWritableStates.builder().state(baseKVState).build();
         when(baseState.createReadableStates(FOOD_SERVICE)).thenReturn(writableStates);
@@ -598,7 +598,6 @@ class SavepointStackImplTest extends StateTestBase {
             stack.commitFullStack();
 
             // then
-            assertThatThrownBy(stack::createSavepoint).isInstanceOf(IllegalStateException.class);
             assertThatThrownBy(stack::commit).isInstanceOf(IllegalStateException.class);
             assertThatThrownBy(stack::rollback).isInstanceOf(IllegalStateException.class);
             assertThat(stack.depth()).isZero();
@@ -608,6 +607,7 @@ class SavepointStackImplTest extends StateTestBase {
                     .isInstanceOf(IllegalStateException.class);
             assertThatThrownBy(() -> stack.createWritableStates(FOOD_SERVICE))
                     .isInstanceOf(IllegalStateException.class);
+            assertThatCode(stack::createSavepoint).doesNotThrowAnyException();
         }
     }
 
