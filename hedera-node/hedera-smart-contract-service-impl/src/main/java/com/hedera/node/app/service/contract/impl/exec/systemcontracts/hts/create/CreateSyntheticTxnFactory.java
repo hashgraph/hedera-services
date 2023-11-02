ec63071394923ee3a061c633e485c2a7fec06b44
Valentin Valkanov <valentin.valkanov@limechain.tech>
2023-11-02 17:50:14 +0200

    refactor: spotless check
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/create/CreateSyntheticTxnFactory.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/create/CreateSyntheticTxnFactory.java
@@ -21,13 +21,11 @@ import com.hedera.hapi.node.base.TokenSupplyType;
 import com.hedera.hapi.node.base.TokenType;
 import com.hedera.hapi.node.token.TokenCreateTransactionBody;
 import com.hedera.hapi.node.token.TokenCreateTransactionBody.Builder;
-import com.hedera.hapi.node.transaction.CustomFee;
 import com.hedera.node.app.service.contract.impl.exec.utils.TokenCreateWrapper;
 import com.hedera.node.app.service.contract.impl.exec.utils.TokenCreateWrapper.FixedFeeWrapper;
 import com.hedera.node.app.service.contract.impl.exec.utils.TokenCreateWrapper.FractionalFeeWrapper;
 import com.hedera.node.app.service.contract.impl.exec.utils.TokenCreateWrapper.RoyaltyFeeWrapper;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 import javax.inject.Singleton;
@@ -70,8 +68,8 @@ public class CreateSyntheticTxnFactory {
         return txnBodyBuilder;
     }
 
-    private static void setTokenKeys(@NonNull final TokenCreateWrapper tokenCreateWrapper,
-            final Builder txnBodyBuilder) {
+    private static void setTokenKeys(
+            @NonNull final TokenCreateWrapper tokenCreateWrapper, final Builder txnBodyBuilder) {
         tokenCreateWrapper.getTokenKeys().forEach(tokenKeyWrapper -> {
             final var key = tokenKeyWrapper.key().asGrpc();
             if (tokenKeyWrapper.isUsedForAdminKey()) {
@@ -98,8 +96,8 @@ public class CreateSyntheticTxnFactory {
         });
     }
 
-    private static void setExpiry(@NonNull final TokenCreateWrapper tokenCreateWrapper,
-            @NonNull final Builder txnBodyBuilder) {
+    private static void setExpiry(
+            @NonNull final TokenCreateWrapper tokenCreateWrapper, @NonNull final Builder txnBodyBuilder) {
         if (tokenCreateWrapper.getExpiry().second() != 0) {
             txnBodyBuilder.expiry(Timestamp.newBuilder()
                     .seconds(tokenCreateWrapper.getExpiry().second())
@@ -113,22 +111,19 @@ public class CreateSyntheticTxnFactory {
         }
     }
 
-    private static void addCustomFees(@NonNull final TokenCreateWrapper tokenCreateWrapper,
-            @NonNull final Builder txnBodyBuilder) {
-        final var fractionalFees = tokenCreateWrapper.getFractionalFees().stream()
-                .map(FractionalFeeWrapper::asGrpc);
-        final var fixedFees = tokenCreateWrapper.getFixedFees().stream()
-                .map(FixedFeeWrapper::asGrpc);
-        final var royaltyFees = tokenCreateWrapper.getRoyaltyFees().stream()
-                .map(RoyaltyFeeWrapper::asGrpc);
+    private static void addCustomFees(
+            @NonNull final TokenCreateWrapper tokenCreateWrapper, @NonNull final Builder txnBodyBuilder) {
+        final var fractionalFees =
+                tokenCreateWrapper.getFractionalFees().stream().map(FractionalFeeWrapper::asGrpc);
+        final var fixedFees = tokenCreateWrapper.getFixedFees().stream().map(FixedFeeWrapper::asGrpc);
+        final var royaltyFees = tokenCreateWrapper.getRoyaltyFees().stream().map(RoyaltyFeeWrapper::asGrpc);
 
         var allFees = Stream.of(fractionalFees, fixedFees, royaltyFees)
                 .flatMap(Function.identity())
                 .toList();
 
-        if(!allFees.isEmpty()) {
+        if (!allFees.isEmpty()) {
             txnBodyBuilder.customFees(allFees);
         }
     }
-
 }
