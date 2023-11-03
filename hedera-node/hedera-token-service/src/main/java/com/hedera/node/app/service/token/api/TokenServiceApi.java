77e42b9a698e8f391e020bd99ff45d66f767cc81
Valentin Tronkov <valentin.tronkov@limechain.tech>
2023-11-03 16:13:20 +0200

    Implement ContractUpdateHandler.handle (#9379)
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
    Signed-off-by: Valentin Tronkov <valentin.tronkov@limechain.tech>
    Signed-off-by: Venkatachalam Vairavan <vvairava@in.ibm.com>

--- hedera-node/hedera-token-service/src/main/java/com/hedera/node/app/service/token/api/TokenServiceApi.java
+++ hedera-node/hedera-token-service/src/main/java/com/hedera/node/app/service/token/api/TokenServiceApi.java
@@ -18,6 +18,7 @@ package com.hedera.node.app.service.token.api;
 
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.ContractID;
+import com.hedera.hapi.node.state.token.Account;
 import com.hedera.hapi.node.token.CryptoTransferTransactionBody;
 import com.hedera.node.app.service.token.ReadableAccountStore;
 import com.hedera.node.app.spi.fees.Fees;
@@ -185,4 +186,10 @@ public interface TokenServiceApi {
      * @return the number of storage slots used by the given account before any changes were made
      */
     long originalKvUsageFor(@NonNull AccountID id);
+
+    /**
+     * Updates the passed contract
+     * @param contract the contract that is updated
+     */
+    void updateContract(Account contract);
 }
