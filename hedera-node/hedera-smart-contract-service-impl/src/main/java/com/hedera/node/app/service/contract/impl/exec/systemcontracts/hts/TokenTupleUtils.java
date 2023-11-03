efbca67f272b462856cbab3126d810e76f7075b1
Matt Hess <matt.hess@swirldslabs.com>
2023-11-03 17:12:02 -0500

    Updated Tests (#9672)
    Signed-off-by: ilko-iliev-lime <ilko.iliev@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/TokenTupleUtils.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/TokenTupleUtils.java
@@ -32,6 +32,7 @@ import com.hedera.hapi.node.transaction.CustomFee;
 import com.hedera.hapi.node.transaction.FixedFee;
 import com.hedera.hapi.node.transaction.FractionalFee;
 import com.hedera.hapi.node.transaction.RoyaltyFee;
+import com.hedera.hapi.node.state.token.Account;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import com.hederahashgraph.api.proto.java.TokenSupplyType;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -60,7 +61,7 @@ public class TokenTupleUtils {
         return Tuple.of(
                 token.expirationSecond(),
                 headlongAddressOf(token.autoRenewAccountIdOrElse(ZERO_ACCOUNT_ID)),
-                token.autoRenewSeconds());
+                (token.autoRenewSeconds() > 0 ? token.autoRenewSeconds() : 0));
     }
 
     /**
@@ -230,14 +231,15 @@ public class TokenTupleUtils {
             @NonNull final Token token,
             @NonNull final Nft nft,
             final long serialNumber,
-            @NonNull final String ledgerId) {
+            @NonNull final String ledgerId,
+            Account ownerAccount) {
 
         final var nftMetaData = nft.metadata() != null ? nft.metadata().toByteArray() : Bytes.EMPTY.toByteArray();
 
         return Tuple.of(
                 tokenInfoTupleFor(token, ledgerId),
                 serialNumber,
-                headlongAddressOf(nft.ownerIdOrElse(ZERO_ACCOUNT_ID)),
+                headlongAddressOf(ownerAccount),
                 nft.mintTimeOrElse(new Timestamp(0, 0)).seconds(),
                 nftMetaData,
                 headlongAddressOf(nft.spenderIdOrElse(ZERO_ACCOUNT_ID)));
