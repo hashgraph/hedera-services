1640e50206fb3a5fe11b096c1bd1b79101cf1c03
Petar Tonev <petar.tonev@limechain.tech>
2023-11-08 20:40:37 +0200

    Fix E2E tests in TokenExpiryInfoSuite (#9686)
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>
    Signed-off-by: Austin Littley <austin@swirldslabs.com>
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>
    Signed-off-by: Joseph Sinclair <joseph.sinclair@swirldslabs.com>
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Signed-off-by: Kelly Greco <kelly@swirldslabs.com>
    Signed-off-by: Cody Littley <cody@swirldslabs.com>
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>
    Co-authored-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
    Co-authored-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>
    Co-authored-by: Hendrik Ebbers <hendrik.ebbers@web.de>
    Co-authored-by: Timo Brandstätter <timo@swirldslabs.com>
    Co-authored-by: Austin Littley <102969658+alittley@users.noreply.github.com>
    Co-authored-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: Matt Hess <matt.hess@swirldslabs.com>
    Co-authored-by: Ivan Malygin <ivan@swirldslabs.com>
    Co-authored-by: Joseph Sinclair <121976561+jsync-swirlds@users.noreply.github.com>
    Co-authored-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Co-authored-by: Kelly Greco <82919061+poulok@users.noreply.github.com>
    Co-authored-by: Cody Littley <56973212+cody-littley@users.noreply.github.com>
    Co-authored-by: Lazar Petrovic <lpetrovic05@gmail.com>
    Co-authored-by: Lev Povolotsky <16233475+povolev15@users.noreply.github.com>
    Signed-off-by: Venkatachalam Vairavan <vvairava@in.ibm.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/tokenexpiry/TokenExpiryCall.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/tokenexpiry/TokenExpiryCall.java
@@ -16,21 +16,31 @@
 
 package com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokenexpiry;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TOKEN_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.SUCCESS;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.HederaSystemContract.FullResult.revertResult;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.HederaSystemContract.FullResult.successResult;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.HtsSystemContract.HTS_PRECOMPILE_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCall.PricedResult.gasOnly;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.TokenTupleUtils.expiryTupleFor;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokenexpiry.TokenExpiryTranslator.TOKEN_EXPIRY;
+import static com.hedera.node.app.service.contract.impl.utils.ConversionUtils.asEvmContractId;
+import static com.hedera.node.app.service.contract.impl.utils.SystemContractUtils.contractFunctionResultFailedFor;
+import static com.hedera.node.app.service.contract.impl.utils.SystemContractUtils.contractFunctionResultSuccessFor;
 import static java.util.Objects.requireNonNull;
 
+import com.hedera.hapi.node.base.ContractID;
 import com.hedera.hapi.node.base.ResponseCodeEnum;
 import com.hedera.hapi.node.state.token.Token;
 import com.hedera.node.app.service.contract.impl.exec.gas.SystemContractGasCalculator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.HederaSystemContract.FullResult;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.AbstractNonRevertibleTokenViewCall;
 import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
+import com.hedera.node.app.service.contract.impl.utils.SystemContractUtils;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
+import org.apache.tuweni.bytes.Bytes;
+import org.hyperledger.besu.datatypes.Address;
 
 public class TokenExpiryCall extends AbstractNonRevertibleTokenViewCall {
     private final boolean isStaticCall;
@@ -67,5 +77,36 @@ public class TokenExpiryCall extends AbstractNonRevertibleTokenViewCall {
         }
         return successResult(
                 TOKEN_EXPIRY.getOutputs().encodeElements(status.protoOrdinal(), expiryTupleFor(token)), gasRequirement);
+    }
+
+    @Override
+    public @NonNull PricedResult execute() {
+        PricedResult result;
+        long gasRequirement;
+        Bytes output;
+        ContractID contractID = asEvmContractId(Address.fromHexString(HTS_PRECOMPILE_ADDRESS));
+        if (token == null) {
+            result = gasOnly(viewCallResultWith(INVALID_TOKEN_ID, gasCalculator.viewGasRequirement()));
+
+            gasRequirement = result.fullResult().gasRequirement();
+            enhancement
+                    .systemOperations()
+                    .externalizeResult(
+                            contractFunctionResultFailedFor(gasRequirement, INVALID_TOKEN_ID.toString(), contractID),
+                            SystemContractUtils.ResultStatus.IS_ERROR,
+                            INVALID_TOKEN_ID);
+        } else {
+            result = gasOnly(resultOfViewingToken(token));
+
+            gasRequirement = result.fullResult().gasRequirement();
+            output = result.fullResult().result().getOutput();
+            enhancement
+                    .systemOperations()
+                    .externalizeResult(
+                            contractFunctionResultSuccessFor(gasRequirement, output, contractID),
+                            SystemContractUtils.ResultStatus.IS_SUCCESS,
+                            SUCCESS);
+        }
+        return result;
     }
 }
