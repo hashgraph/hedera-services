5822560790f2e7d621a32090ac140b19b86437f2
nikolay <n.atanasow94@gmail.com>
2023-11-09 15:21:20 +0200

    Edit contracts module
    Signed-off-by: nikolay <n.atanasow94@gmail.com>

--- hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/contracts/execution/HederaCallLocalEvmTxProcessorV045.java
+++ hedera-node/hedera-mono-service/src/main/java/com/hedera/node/app/service/mono/contracts/execution/HederaCallLocalEvmTxProcessorV045.java
@@ -16,28 +16,32 @@
 
 package com.hedera.node.app.service.mono.contracts.execution;
 
-import static com.hedera.node.app.service.evm.utils.ValidationUtils.validateTrue;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_CONTRACT_ID;
-
+import com.hedera.hapi.node.base.ResponseCodeEnum;
 import com.hedera.node.app.service.mono.context.properties.GlobalDynamicProperties;
 import com.hedera.node.app.service.mono.ledger.accounts.AliasManager;
 import com.hedera.node.app.service.mono.store.contracts.CodeCache;
 import com.hedera.node.app.service.mono.store.models.Account;
+import com.hedera.node.app.spi.workflows.HandleException;
 import com.hederahashgraph.api.proto.java.HederaFunctionality;
 import java.util.Map;
 import javax.inject.Provider;
 import org.apache.tuweni.bytes.Bytes;
 import org.hyperledger.besu.datatypes.Address;
+import org.hyperledger.besu.evm.code.CodeV0;
 import org.hyperledger.besu.evm.frame.MessageFrame;
 import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 import org.hyperledger.besu.evm.processor.ContractCreationProcessor;
 import org.hyperledger.besu.evm.processor.MessageCallProcessor;
 
-public class CallLocalEvmTxProcessor extends EvmTxProcessor {
+/**
+ * Extension of the base {@link EvmTxProcessor} that provides interface for executing {@link
+ * com.hederahashgraph.api.proto.java.ContractCallLocal} queries
+ */
+public class HederaCallLocalEvmTxProcessorV045 extends CallLocalEvmTxProcessor {
     private final CodeCache codeCache;
     private final AliasManager aliasManager;
 
-    public CallLocalEvmTxProcessor(
+    public HederaCallLocalEvmTxProcessorV045(
             final CodeCache codeCache,
             final LivePricesSource livePricesSource,
             final GlobalDynamicProperties dynamicProperties,
@@ -45,7 +49,7 @@ public class CallLocalEvmTxProcessor extends EvmTxProcessor {
             final Map<String, Provider<MessageCallProcessor>> mcps,
             final Map<String, Provider<ContractCreationProcessor>> ccps,
             final AliasManager aliasManager) {
-        super(livePricesSource, dynamicProperties, gasCalculator, mcps, ccps);
+        super(codeCache, livePricesSource, dynamicProperties, gasCalculator, mcps, ccps, aliasManager);
         this.codeCache = codeCache;
         this.aliasManager = aliasManager;
     }
@@ -86,14 +90,17 @@ public class CallLocalEvmTxProcessor extends EvmTxProcessor {
          * _account_ has been created, but not yet its _bytecode_. So if `code` is null here,
          * it doesn't mean a system invariant has been violated (FAIL_INVALID); instead it means
          * the target contract is not yet in a valid state to be queried (INVALID_CONTRACT_ID). */
-        validateTrue(code != null, INVALID_CONTRACT_ID);
+        //                validateTrue(code != null, INVALID_CONTRACT_ID);
+        if (!dynamicProperties.allowCallsToNonContractAccounts() && code == null) {
+            throw new HandleException(ResponseCodeEnum.INVALID_CONTRACT_ID);
+        }
 
         return baseInitialFrame
                 .type(MessageFrame.Type.MESSAGE_CALL)
                 .address(to)
                 .contract(to)
                 .inputData(payload)
-                .code(code)
+                .code(code == null ? CodeV0.EMPTY_CODE : code)
                 .build();
     }
 }
