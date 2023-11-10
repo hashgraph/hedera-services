4f7c0cdc1f3e811d1fe0c47332b2f43437af45ca
lukelee-sl <109538178+lukelee-sl@users.noreply.github.com>
2023-11-10 13:16:38 -0800

    Minor improvements to exception handling and null checks (#9813)
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/HtsCallAttempt.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/HtsCallAttempt.java
@@ -36,6 +36,7 @@ import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
 import com.swirlds.config.api.Configuration;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
+import java.nio.BufferUnderflowException;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.tuweni.bytes.Bytes;
@@ -97,7 +98,7 @@ public class HtsCallAttempt {
             try {
                 // First try to decode the redirect with standard ABI encoding using a 32-byte address
                 abiCall = REDIRECT_FOR_TOKEN.decodeCall(input.toArrayUnsafe());
-            } catch (Exception ignore) {
+            } catch (IllegalArgumentException | BufferUnderflowException | IndexOutOfBoundsException ignore) {
                 // Otherwise use the "packed" encoding with a 20-byte address
             }
             final Address tokenAddress;
