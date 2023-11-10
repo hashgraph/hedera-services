c2ec2684555a2be0a355e88f5d1f1c47f6545e31
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-10 15:16:32 +0100

    jmh benchmarks added to swirlds-base (#8074)
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>
    Signed-off-by: Venkatachalam Vairavan <vvairava@in.ibm.com>

--- platform-sdk/swirlds-base/build.gradle.kts
+++ platform-sdk/swirlds-base/build.gradle.kts
@@ -17,6 +17,7 @@
 plugins {
     id("com.hedera.hashgraph.sdk.conventions")
     id("com.hedera.hashgraph.platform-maven-publish")
+    id("com.hedera.hashgraph.benchmark-conventions")
     id("java-test-fixtures")
 }
 
