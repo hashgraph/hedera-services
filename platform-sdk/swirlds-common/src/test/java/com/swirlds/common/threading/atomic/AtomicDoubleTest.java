e490da379560ff8e40762be0d92cca0973f0cdcf
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-16 09:34:06 +0100

    Atomic double test moved and old AtomicDouble class removed (#9900)
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>
    Signed-off-by: Venkatachalam Vairavan <vvairava@in.ibm.com>

--- platform-sdk/swirlds-common/src/test/java/com/swirlds/common/threading/atomic/AtomicDoubleTest.java
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/threading/atomic/AtomicDoubleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022-2023 Hedera Hashgraph, LLC
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,12 +14,11 @@
  * limitations under the License.
  */
 
-package com.swirlds.common.metrics.atomic;
+package com.swirlds.common.threading.atomic;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.within;
 
-import com.swirlds.common.threading.atomic.AtomicDouble;
 import org.junit.jupiter.api.Test;
 
 class AtomicDoubleTest {
