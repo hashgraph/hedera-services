038ef902d14f660ab080c7714b762699f32f91f5
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2023-11-16 10:56:27 +0200

    Add/remove tests and scenarios
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/validation/Evm45ValidationSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/validation/Evm45ValidationSuite.java
@@ -17,8 +17,9 @@
 package com.hedera.services.bdd.suites.validation;
 
 import static com.hedera.services.bdd.spec.HapiPropertySource.asContractIdWithEvmAddress;
-import static com.hedera.services.bdd.spec.HapiPropertySource.idAsHeadlongAddress;
 import static com.hedera.services.bdd.spec.HapiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.assertions.ContractFnResultAsserts.resultWith;
+import static com.hedera.services.bdd.spec.assertions.TransactionRecordAsserts.recordWith;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCallWithFunctionAbi;
@@ -27,20 +28,19 @@ import static com.hedera.services.bdd.spec.transactions.TxnVerbs.uploadInitCode;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
 import static com.hedera.services.bdd.suites.contract.Utils.getABIFor;
+import static com.hedera.services.bdd.suites.contract.Utils.mirrorAddrWith;
 import static com.hedera.services.bdd.suites.contract.Utils.nonMirrorAddrWith;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_REVERT_EXECUTED;
+import static com.hedera.services.bdd.suites.utils.contracts.SimpleBytesResult.bigIntResult;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_SOLIDITY_ADDRESS;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.SUCCESS;
 import static com.swirlds.common.utility.CommonUtils.unhex;
 
 import com.google.protobuf.ByteString;
 import com.hedera.services.bdd.spec.HapiSpec;
-import com.hedera.services.bdd.spec.assertions.ContractFnResultAsserts;
-import com.hedera.services.bdd.spec.assertions.TransactionRecordAsserts;
 import com.hedera.services.bdd.suites.HapiSuite;
-import com.hederahashgraph.api.proto.java.AccountID;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -52,6 +52,16 @@ public class Evm45ValidationSuite extends HapiSuite {
     private static final String ERC_721_ABI = "ERC721ABI";
     private static final String NON_EXISTING_MIRROR_ADDRESS = "0000000000000000000000000000000000123456";
     private static final String NON_EXISTING_NON_MIRROR_ADDRESS = "1234561234561234561234561234568888123456";
+    private static final String INTERNAL_CALLER_CONTRACT = "InternalCaller";
+    private static final String INTERNAL_CALLEE_CONTRACT = "InternalCallee";
+    private static final String INNER_TXN = "innerTx";
+    private static final String CALL_NON_EXISTING_FUNCTION = "callNonExisting";
+    private static final String CALL_EXTERNAL_FUNCTION = "callExternalFunction";
+    private static final String CALL_REVERT_WITH_REVERT_REASON_FUNCTION = "callRevertWithRevertReason";
+    private static final String CALL_REVERT_WITHOUT_REVERT_REASON_FUNCTION = "callRevertWithoutRevertReason";
+    private static final String TRANSFER_TO_FUNCTION = "transferTo";
+    private static final String SEND_TO_FUNCTION = "sendTo";
+    private static final String CALL_WITH_VALUE_TO_FUNCTION = "callWithValueTo";
 
     public static void main(String... args) {
         new Evm45ValidationSuite().runSuiteAsync();
@@ -65,36 +75,50 @@ public class Evm45ValidationSuite extends HapiSuite {
     @Override
     public List<HapiSpec> getSpecsInSuite() {
         return List.of(
-                directCallToNonExistingLongZeroAddress(),
-                internalCallToNonExistingLongZeroAddress(),
-                //                //                internalCallToExistingLongZeroAddress(),
-                internalCallToNonExistingEvmAddress(),
-                //                //                internalCallToExistingEvmAddress(),
-                internalTransferToNonExistingLongZeroAddress(),
-                //                //                internalTransferToExistingLongZeroAddress(),
-                internalTransferToNonExistingEvmAddress(),
-                //                //                internalTransferToExistingEvmAddress(),
-                internalCallWithValueToNonExistingLongZeroAddress(),
-                //                //                internalCallWithValueToExistingLongZeroAddress(),
-                internalCallWithValueToNonExistingEvmAddress()
-                //                //                internalCallWithValueToExistingEvmAddress()
+                // Top-level calls:
+                // EOA -calls-> NonExistingMirror, expect noop success
+                directCallToNonExistingMirrorAddressResultsInSuccessfulNoOp(),
+                // EOA -calls-> NonExistingNonMirror, expect noop success
+                directCallToNonExistingNonMirrorAddressResultsInSuccessfulNoOp(),
+
+                // Internal calls:
+                // EOA -calls-> InternalCaller -calls-> NonExistingMirror, expect noop success
+                internalCallToNonExistingMirrorAddressResultsInNoopSuccess(),
+                // EOA -calls-> InternalCaller -calls-> ExistingMirror, expect successful call
+                internalCallToExistingMirrorAddressResultsInSuccessfulCall(),
+                // EOA -calls-> InternalCaller -calls-> NonExistingNonMirror, expect noop success
+                internalCallToNonExistingNonMirrorAddressResultsInNoopSuccess(),
+                // todo EOA -calls-> InternalCaller -calls-> ExistingNonMirror, expect successful call
+
+                // Internal transfers:
+                // EOA -calls-> InternalCaller -transfer-> NonExistingMirror, expect revert due to failed internal
+                // transfer
+                internalTransferToNonExistingMirrorAddressResultsInRevert()
+                // todo EOA -calls-> InternalCaller -transfer-> ExistingMirror, expect ?
+                // todo EOA -calls-> InternalCaller -transfer-> NonExistingNonMirror, expect ?
+                // todo EOA -calls-> InternalCaller -transfer-> ExistingNonMirror, expect ?
+
+                // Internal sends:
+                // todo EOA -calls-> InternalCaller -send-> NonExistingMirror, expect ?
+                // todo EOA -calls-> InternalCaller -send-> ExistingMirror, expect ?
+                // todo EOA -calls-> InternalCaller -send-> NonExistingNonMirror, expect ?
+                // todo EOA -calls-> InternalCaller -send-> ExistingNonMirror, expect ?
+
+                // Internal calls with value:
+                // todo EOA -calls-> InternalCaller -callWValue-> NonExistingMirror, expect ?
+                // todo EOA -calls-> InternalCaller -callWValue-> ExistingMirror, expect ?
+                // todo EOA -calls-> InternalCaller -callWValue-> NonExistingNonMirror, expect ?
+                // todo EOA -calls-> InternalCaller -callWValue-> ExistingNonMirror, expect ?
                 );
     }
 
-    private HapiSpec directCallToNonExistingLongZeroAddress() {
+    private HapiSpec directCallToNonExistingMirrorAddressResultsInSuccessfulNoOp() {
 
-        return defaultHapiSpec("directCallToNonExistingLongZeroAddress")
-                .given(
-                        withOpContext((spec, ctxLog) -> spec.registry()
-                                .saveContractId(
-                                        "nonExistingMirrorAddress",
-                                        asContractIdWithEvmAddress(
-                                                ByteString.copyFrom(unhex(NON_EXISTING_MIRROR_ADDRESS))))),
-                        withOpContext((spec, ctxLog) -> spec.registry()
-                                .saveContractId(
-                                        "nonExistingNonMirrorAddress",
-                                        asContractIdWithEvmAddress(
-                                                ByteString.copyFrom(unhex(NON_EXISTING_NON_MIRROR_ADDRESS))))))
+        return defaultHapiSpec("directCallToNonExistingMirrorAddressResultsInSuccessfulNoOp")
+                .given(withOpContext((spec, ctxLog) -> spec.registry()
+                        .saveContractId(
+                                "nonExistingMirrorAddress",
+                                asContractIdWithEvmAddress(ByteString.copyFrom(unhex(NON_EXISTING_MIRROR_ADDRESS))))))
                 .when(withOpContext((spec, ctxLog) -> allRunFor(
                         spec,
                         contractCallWithFunctionAbi(
@@ -104,7 +128,23 @@ public class Evm45ValidationSuite extends HapiSuite {
                                                 NAME,
                                                 ERC_721_ABI))
                                 .gas(24_000L)
-                                .via("directCallToNonExistingMirrorAddress"),
+                                .via("directCallToNonExistingMirrorAddress"))))
+                .then(getTxnRecord("directCallToNonExistingMirrorAddress")
+                        .hasPriority(recordWith()
+                                .status(SUCCESS)
+                                .contractCallResult(resultWith().gasUsed(21000))));
+    }
+
+    private HapiSpec directCallToNonExistingNonMirrorAddressResultsInSuccessfulNoOp() {
+
+        return defaultHapiSpec("directCallToNonExistingNonMirrorAddressResultsInSuccessfulNoOp")
+                .given(withOpContext((spec, ctxLog) -> spec.registry()
+                        .saveContractId(
+                                "nonExistingNonMirrorAddress",
+                                asContractIdWithEvmAddress(
+                                        ByteString.copyFrom(unhex(NON_EXISTING_NON_MIRROR_ADDRESS))))))
+                .when(withOpContext((spec, ctxLog) -> allRunFor(
+                        spec,
                         contractCallWithFunctionAbi(
                                         "nonExistingNonMirrorAddress",
                                         getABIFor(
@@ -113,107 +153,88 @@ public class Evm45ValidationSuite extends HapiSuite {
                                                 ERC_721_ABI))
                                 .gas(24_000L)
                                 .via("directCallToNonExistingNonMirrorAddress"))))
-                .then(
-                        getTxnRecord("directCallToNonExistingMirrorAddress")
-                                .hasPriority(TransactionRecordAsserts.recordWith()
-                                        .status(SUCCESS)
-                                        .contractCallResult(ContractFnResultAsserts.resultWith()
-                                                .gasUsed(21364))),
-                        getTxnRecord("directCallToNonExistingNonMirrorAddress")
-                                .hasPriority(TransactionRecordAsserts.recordWith()
-                                        .status(SUCCESS)
-                                        .contractCallResult(ContractFnResultAsserts.resultWith()
-                                                .gasUsed(21364))));
+                .then(getTxnRecord("directCallToNonExistingNonMirrorAddress")
+                        .hasPriority(recordWith()
+                                .status(SUCCESS)
+                                .contractCallResult(resultWith().gasUsed(21000))));
     }
 
-    private HapiSpec internalCallToNonExistingLongZeroAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
+    private HapiSpec internalCallToNonExistingMirrorAddressResultsInNoopSuccess() {
 
-        return defaultHapiSpec("internalCallToNonExistingLongZeroAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
+        return defaultHapiSpec("internalCallToNonExistingMirrorAddressResultsInNoopSuccess")
+                .given(
+                        uploadInitCode(INTERNAL_CALLER_CONTRACT),
+                        contractCreate(INTERNAL_CALLER_CONTRACT).balance(ONE_HBAR))
                 .when(contractCall(
-                                contract,
-                                "callContract",
-                                idAsHeadlongAddress(AccountID.newBuilder()
-                                        .setAccountNum(564400L)
-                                        .build()))
-                        .via(innerTx)
-                        .hasKnownStatus(SUCCESS))
-                .then();
+                                INTERNAL_CALLER_CONTRACT,
+                                CALL_NON_EXISTING_FUNCTION,
+                                mirrorAddrWith(new Random().nextLong()))
+                        .gas(25000)
+                        .via(INNER_TXN))
+                .then(getTxnRecord(INNER_TXN)
+                        .hasPriority(recordWith()
+                                .status(SUCCESS)
+                                .contractCallResult(
+                                        resultWith().createdContractIdsCount(0).gasUsed(24618))));
     }
 
-    private HapiSpec internalCallToNonExistingEvmAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
+    private HapiSpec internalCallToExistingMirrorAddressResultsInSuccessfulCall() {
 
-        return defaultHapiSpec("internalCallToNonExistingEvmAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
-                .when(contractCall(contract, "callContract", nonMirrorAddrWith(new Random().nextLong()))
-                        .via(innerTx)
-                        .hasKnownStatus(SUCCESS))
-                .then(getTxnRecord(innerTx));
+        final AtomicLong calleeNum = new AtomicLong();
+
+        return defaultHapiSpec("internalCallToNonExistingMirrorAddressResultsInNoopSuccess")
+                .given(
+                        uploadInitCode(INTERNAL_CALLER_CONTRACT, INTERNAL_CALLEE_CONTRACT),
+                        contractCreate(INTERNAL_CALLER_CONTRACT).balance(ONE_HBAR),
+                        contractCreate(INTERNAL_CALLEE_CONTRACT).exposingNumTo(calleeNum::set))
+                .when(withOpContext((spec, ignored) -> allRunFor(
+                        spec,
+                        contractCall(INTERNAL_CALLER_CONTRACT, CALL_EXTERNAL_FUNCTION, mirrorAddrWith(calleeNum.get()))
+                                .gas(50000)
+                                .via(INNER_TXN))))
+                .then(getTxnRecord(INNER_TXN)
+                        .logged()
+                        .hasPriority(recordWith()
+                                .status(SUCCESS)
+                                .contractCallResult(resultWith()
+                                        .createdContractIdsCount(0)
+                                        .contractCallResult(bigIntResult(1))
+                                        .gasUsed(47751))));
     }
 
-    private HapiSpec internalTransferToNonExistingLongZeroAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
+    private HapiSpec internalCallToNonExistingNonMirrorAddressResultsInNoopSuccess() {
 
-        return defaultHapiSpec("internalTransferToNonExistingLongZeroAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
+        return defaultHapiSpec("internalCallToNonExistingNonMirrorAddressResultsInNoopSuccess")
+                .given(
+                        uploadInitCode(INTERNAL_CALLER_CONTRACT),
+                        contractCreate(INTERNAL_CALLER_CONTRACT).balance(ONE_HBAR))
                 .when(contractCall(
-                                contract,
-                                "callTransfer",
-                                idAsHeadlongAddress(AccountID.newBuilder()
-                                        .setAccountNum(564400L)
-                                        .build()))
-                        .via(innerTx)
+                                INTERNAL_CALLER_CONTRACT,
+                                CALL_NON_EXISTING_FUNCTION,
+                                nonMirrorAddrWith(new Random().nextLong()))
+                        .gas(25000)
+                        .via(INNER_TXN))
+                .then(getTxnRecord(INNER_TXN)
+                        .hasPriority(recordWith()
+                                .status(SUCCESS)
+                                .contractCallResult(
+                                        resultWith().createdContractIdsCount(0).gasUsed(24618))));
+    }
+
+    private HapiSpec internalTransferToNonExistingMirrorAddressResultsInRevert() {
+
+        return defaultHapiSpec("internalTransferToNonExistingMirrorAddressResultsInRevert")
+                .given(
+                        uploadInitCode(INTERNAL_CALLER_CONTRACT),
+                        contractCreate(INTERNAL_CALLER_CONTRACT).balance(ONE_HBAR))
+                .when(contractCall(
+                                INTERNAL_CALLER_CONTRACT, TRANSFER_TO_FUNCTION, mirrorAddrWith(new Random().nextLong()))
+                        .gas(100000)
+                        .via(INNER_TXN)
                         .hasKnownStatus(INVALID_SOLIDITY_ADDRESS))
-                .then(getTxnRecord(innerTx));
-    }
-
-    private HapiSpec internalTransferToNonExistingEvmAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
-
-        return defaultHapiSpec("internalTransferToNonExistingEvmAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
-                .when(contractCall(contract, "callTransfer", nonMirrorAddrWith(new Random().nextLong()))
-                        .via(innerTx)
-                        .hasKnownStatus(CONTRACT_REVERT_EXECUTED))
-                .then(getTxnRecord(innerTx));
-    }
-
-    private HapiSpec internalCallWithValueToNonExistingLongZeroAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
-
-        return defaultHapiSpec("internalCallWithValueToNonExistingLongZeroAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
-                .when(contractCall(
-                                contract,
-                                "callWithValue",
-                                idAsHeadlongAddress(AccountID.newBuilder()
-                                        .setAccountNum(564400L)
-                                        .build()))
-                        .sending(1L)
-                        .via(innerTx)
-                        .gas(1_000_000L)
-                        .hasKnownStatus(CONTRACT_REVERT_EXECUTED))
-                .then(getTxnRecord(innerTx));
-    }
-
-    private HapiSpec internalCallWithValueToNonExistingEvmAddress() {
-        final var contract = "InternalCaller";
-        final var innerTx = "innerTx";
-
-        return defaultHapiSpec("internalCallWithValueToNonExistingEvmAddress")
-                .given(uploadInitCode(contract), contractCreate(contract).balance(ONE_HBAR))
-                .when(contractCall(contract, "callWithValue", nonMirrorAddrWith(new Random().nextLong()))
-                        .via(innerTx)
-                        .gas(1_000_000L)
-                        .hasKnownStatus(SUCCESS))
-                .then(getTxnRecord(innerTx));
+                .then(
+                        // todo is this all we expect? no contract function result?
+                        getTxnRecord(INNER_TXN).hasPriority(recordWith().status(INVALID_SOLIDITY_ADDRESS)));
     }
 
     @Override
--- hedera-node/test-clients/src/main/resource/contract/contracts/InternalCaller/InternalCaller.sol
+++ hedera-node/test-clients/src/main/resource/contract/contracts/InternalCaller/InternalCaller.sol
@@ -3,15 +3,33 @@ pragma solidity ^0.8.0;
 
 contract InternalCaller {
     constructor() payable {}
-    function callContract(address _addr) external {
+
+    function callNonExisting(address _addr) external {
         _addr.call(abi.encodeWithSignature("nonExisting()"));
     }
 
-    function callTransfer(address _addr) external {
+    function callExternalFunction(address _addr) external returns (uint) {
+        (bool success, bytes memory result) = _addr.call(abi.encodeWithSignature("externalFunction()"));
+        return abi.decode(result, (uint));
+    }
+
+    function callRevertWithRevertReason(address _addr) external {
+        _addr.call(abi.encodeWithSignature("revertWithRevertReason()"));
+    }
+
+    function callRevertWithoutRevertReason(address _addr) external {
+        _addr.call(abi.encodeWithSignature("revertWithoutRevertReason()"));
+    }
+
+    function sendTo(address payable _addr) external {
+        _addr.send(1);
+    }
+
+    function transferTo(address payable _addr) external {
         _addr.transfer(1);
     }
 
-    function callWithValue(address _addr) external {
+    function callWithValueTo(address _addr) external {
         _addr.call{value: 1}("");
     }
 }
