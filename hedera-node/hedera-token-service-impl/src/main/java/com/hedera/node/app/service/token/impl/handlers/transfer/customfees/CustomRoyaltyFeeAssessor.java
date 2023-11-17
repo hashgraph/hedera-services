91008819d405259e1a1d8111985798bf896df477
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-11-17 10:16:57 -0600

    fix royalty fee charging

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/transfer/customfees/CustomRoyaltyFeeAssessor.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/transfer/customfees/CustomRoyaltyFeeAssessor.java
@@ -90,22 +90,20 @@ public class CustomRoyaltyFeeAssessor {
                 final var fallbackFee = asFixedFee(
                         fallback.amount(), fallback.denominatingTokenId(), collector, fee.allCollectorsAreExempt());
                 fixedFeeAssessor.assessFixedFee(feeMeta, receiver, fallbackFee, result);
-                result.addToRoyaltiesPaid(Pair.of(receiver, fallback.denominatingTokenId()));
             } else {
                 if (!isPayerExempt(feeMeta, fee, sender)) {
                     chargeRoyalty(exchangedValue, feeMeta, fee, result);
                 }
             }
-
-            // We don't want to charge the fallback fee for each nft transfer, if the receiver has already
-            // paid it for this token
-            if (exchangedValue.isEmpty()) {
-                // Receiver pays fallback fees
-                result.addToRoyaltiesPaid(Pair.of(receiver, tokenId));
-            } else {
-                // Sender effectively pays percent royalties
-                result.addToRoyaltiesPaid(Pair.of(sender, tokenId));
-            }
+        }
+        // We don't want to charge the fallback fee for each nft transfer, if the receiver has already
+        // paid it for this token
+        if (exchangedValue.isEmpty()) {
+            // Receiver pays fallback fees
+            result.addToRoyaltiesPaid(Pair.of(receiver, tokenId));
+        } else {
+            // Sender effectively pays percent royalties
+            result.addToRoyaltiesPaid(Pair.of(sender, tokenId));
         }
     }
 
