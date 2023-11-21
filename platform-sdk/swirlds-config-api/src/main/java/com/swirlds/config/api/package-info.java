eddfae43b619be43358c089b424b747ff8a0af05
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-21 08:48:34 +0100

    Update platform-sdk/swirlds-config-api/src/main/java/com/swirlds/config/api/package-info.java
    Co-authored-by: Timo Brandstätter <timo@swirldslabs.com>
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-config-api/src/main/java/com/swirlds/config/api/package-info.java
+++ platform-sdk/swirlds-config-api/src/main/java/com/swirlds/config/api/package-info.java
@@ -17,6 +17,7 @@
 
 /**
  * The config API.
+ * At its core, it offers the primary interface, {@link com.swirlds.config.api.Configuration}, which serves as the central point for interacting with your application's configuration settings.
  *
  * @see com.swirlds.config.api.Configuration
  */
