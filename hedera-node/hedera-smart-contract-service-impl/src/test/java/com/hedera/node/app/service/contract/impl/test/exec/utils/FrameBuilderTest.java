8fd2964a376a458fa2d56c8687dc7d86d007a0f9
lukelee-sl <luke.lee@swirldslabs.com>
2023-11-21 20:12:04 -0800

    fix unit tests
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/utils/FrameBuilderTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/utils/FrameBuilderTest.java
@@ -76,6 +76,7 @@ class FrameBuilderTest {
     void constructsExpectedFrameForCallToExtantContractIncludingAccessTrackerWithSidecarEnabled() {
         final var transaction = wellKnownHapiCall();
         given(worldUpdater.getHederaAccount(NON_SYSTEM_LONG_ZERO_ADDRESS)).willReturn(account);
+        given(worldUpdater.getHederaAccount(CALLED_CONTRACT_ID)).willReturn(null);
         given(account.getEvmCode()).willReturn(CONTRACT_CODE);
         given(worldUpdater.updater()).willReturn(stackedUpdater);
         given(blocks.blockValuesOf(GAS_LIMIT)).willReturn(blockValues);
@@ -124,6 +125,7 @@ class FrameBuilderTest {
         given(blocks.blockValuesOf(GAS_LIMIT)).willReturn(blockValues);
         given(blocks.blockHashOf(SOME_BLOCK_NO)).willReturn(Hash.EMPTY);
         given(worldUpdater.getHederaAccount(NON_SYSTEM_LONG_ZERO_ADDRESS)).willReturn(account);
+        given(worldUpdater.getHederaAccount(CALLED_CONTRACT_ID)).willReturn(null);
         given(account.getEvmCode()).willReturn(CONTRACT_CODE);
         final var config = HederaTestConfigBuilder.create()
                 .withValue("ledger.fundingAccount", DEFAULT_COINBASE)
