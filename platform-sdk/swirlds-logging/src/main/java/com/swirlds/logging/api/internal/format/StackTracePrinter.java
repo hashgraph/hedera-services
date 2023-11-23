ef6e710a2449fa72684e801395222b5487de036d
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-23 14:40:08 +0100

    jmh benchmarks
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-logging/src/main/java/com/swirlds/logging/api/internal/format/StackTracePrinter.java
+++ platform-sdk/swirlds-logging/src/main/java/com/swirlds/logging/api/internal/format/StackTracePrinter.java
@@ -20,12 +20,18 @@ import com.swirlds.logging.api.extensions.emergency.EmergencyLogger;
 import com.swirlds.logging.api.extensions.emergency.EmergencyLoggerProvider;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StackTracePrinter {
 
     private static final EmergencyLogger EMERGENCY_LOGGER = EmergencyLoggerProvider.getEmergencyLogger();
 
-    private static void print(@NonNull final Appendable writer, @NonNull Throwable throwable, boolean rootCause)
+    private static void print(
+            @NonNull final Appendable writer,
+            @NonNull Throwable throwable,
+            @NonNull final Set<Throwable> alreadyPrinted,
+            @NonNull final StackTraceElement[] enclosingTrace)
             throws IOException {
         if (writer == null) {
             EMERGENCY_LOGGER.logNPE("printWriter");
@@ -33,19 +39,42 @@ public class StackTracePrinter {
         }
         if (throwable == null) {
             EMERGENCY_LOGGER.logNPE("throwable");
+            writer.append("[NULL REFERENCE]");
             return;
         }
-
-        if (!rootCause) {
-            writer.append("Caused by: ");
-            writer.append(System.lineSeparator());
+        if (alreadyPrinted == null) {
+            EMERGENCY_LOGGER.logNPE("alreadyPrinted");
+            writer.append("[INVALID REFERENCE]");
+            return;
         }
-
+        if (enclosingTrace == null) {
+            EMERGENCY_LOGGER.logNPE("enclosingTrace");
+            writer.append("[INVALID REFERENCE]");
+            return;
+        }
+        if (alreadyPrinted.contains(throwable)) {
+            writer.append("[CIRCULAR REFERENCE: " + throwable + "]");
+            return;
+        }
+        alreadyPrinted.add(throwable);
+        if (alreadyPrinted.size() > 1) {
+            writer.append("Cause: ");
+        }
         writer.append(throwable.getClass().getName());
         writer.append(": ");
         writer.append(throwable.getMessage());
         writer.append(System.lineSeparator());
-        for (StackTraceElement stackTraceElement : throwable.getStackTrace()) {
+
+        final StackTraceElement[] stackTrace = throwable.getStackTrace();
+        int m = stackTrace.length - 1;
+        int n = enclosingTrace.length - 1;
+        while (m >= 0 && n >= 0 && stackTrace[m].equals(enclosingTrace[n])) {
+            m--;
+            n--;
+        }
+        final int framesInCommon = stackTrace.length - 1 - m;
+        for (int i = 0; i <= m; i++) {
+            final StackTraceElement stackTraceElement = stackTrace[i];
             final String className = stackTraceElement.getClassName();
             final String methodName = stackTraceElement.getMethodName();
             final int line = stackTraceElement.getLineNumber();
@@ -60,13 +89,19 @@ public class StackTracePrinter {
             writer.append(")");
             writer.append(System.lineSeparator());
         }
+        if (framesInCommon != 0) {
+            writer.append("\t... ");
+            writer.append(Integer.toString(framesInCommon));
+            writer.append(" more");
+            writer.append(System.lineSeparator());
+        }
         final Throwable cause = throwable.getCause();
         if (cause != null) {
-            print(writer, cause, false);
+            print(writer, cause, alreadyPrinted, stackTrace);
         }
     }
 
     public static void print(@NonNull final Appendable writer, @NonNull Throwable throwable) throws IOException {
-        print(writer, throwable, true);
+        print(writer, throwable, new HashSet<>(), new StackTraceElement[0]);
     }
 }
