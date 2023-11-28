c9c7987d70c248def829c368e829bab655c59b47
lukelee-sl <luke.lee@swirldslabs.com>
2023-11-27 20:46:26 -0800

    add contract files
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

---
+++ hedera-node/test-clients/src/main/resource/contract/contracts/InternalCallee/InternalCallee.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract InternalCallee {
+    uint calledTimes = 0;
+
+    function externalFunction() external returns (uint) {
+        return ++calledTimes;
+    }
+
+    function revertWithRevertReason() public pure returns (bool) {
+        revert("RevertReason");
+    }
+
+    function revertWithoutRevertReason() public pure returns (bool) {
+        revert();
+    }
+}
---
+++ hedera-node/test-clients/src/main/resource/contract/contracts/InternalCaller/InternalCaller.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract InternalCaller {
+    constructor() payable {}
+
+    function callNonExisting(address _addr) external {
+        _addr.call(abi.encodeWithSignature("nonExisting()"));
+    }
+
+    function callExternalFunction(address _addr) external returns (uint) {
+        (bool success, bytes memory result) = _addr.call(abi.encodeWithSignature("externalFunction()"));
+        return abi.decode(result, (uint));
+    }
+
+    function callRevertWithRevertReason(address _addr) external {
+        _addr.call(abi.encodeWithSignature("revertWithRevertReason()"));
+    }
+
+    function callRevertWithoutRevertReason(address _addr) external {
+        _addr.call(abi.encodeWithSignature("revertWithoutRevertReason()"));
+    }
+
+    function sendTo(address payable _addr) external {
+        _addr.send(1);
+    }
+
+    function transferTo(address payable _addr) external {
+        _addr.transfer(1);
+    }
+
+    function callWithValueTo(address _addr) external {
+        _addr.call{value: 1}("");
+    }
+}
