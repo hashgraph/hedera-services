0927a94d89ec0c46160fa9b2a16fa7b5a357e086
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-28 10:21:39 +0100

    metrics extensions moved
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/PhaseTimerTests.java
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/PhaseTimerTests.java
@@ -5,20 +5,21 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
  */
 
-package com.swirlds.common.test.metrics.extensions;
+package com.swirlds.common.metrics.extensions;
 
-import static com.swirlds.common.test.metrics.extensions.TestPhases.BAR;
-import static com.swirlds.common.test.metrics.extensions.TestPhases.BAZ;
-import static com.swirlds.common.test.metrics.extensions.TestPhases.FOO;
+import static com.swirlds.common.metrics.extensions.TestPhases.BAR;
+import static com.swirlds.common.metrics.extensions.TestPhases.BAZ;
+import static com.swirlds.common.metrics.extensions.TestPhases.FOO;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -38,8 +39,6 @@ import com.swirlds.common.metrics.Metric;
 import com.swirlds.common.metrics.MetricConfig;
 import com.swirlds.common.metrics.Metrics;
 import com.swirlds.common.metrics.RunningAverageMetric;
-import com.swirlds.common.metrics.extensions.PhaseTimer;
-import com.swirlds.common.metrics.extensions.PhaseTimerBuilder;
 import com.swirlds.common.units.TimeUnit;
 import com.swirlds.test.framework.context.TestPlatformContextBuilder;
 import edu.umd.cs.findbugs.annotations.NonNull;
