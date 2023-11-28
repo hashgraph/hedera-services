9d4805567e680e87854c976f22088e728a28bd50
Hendrik Ebbers <hendrik.ebbers@web.de>
2023-11-28 10:31:05 +0100

    stream tests moved to correct module
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/CountPerSecondTest.java
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/CountPerSecondTest.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.swirlds.common.metrics.extensions;
@@ -26,7 +25,6 @@ import static org.mockito.Mockito.when;
 import com.swirlds.base.test.fixtures.time.FakeTime;
 import com.swirlds.common.metrics.IntegerPairAccumulator;
 import com.swirlds.common.metrics.Metrics;
-import com.swirlds.common.metrics.extensions.CountPerSecond;
 import com.swirlds.common.metrics.platform.DefaultIntegerPairAccumulator;
 import com.swirlds.common.time.IntegerEpochTime;
 import java.time.Duration;
--- platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/FractionalTimerTests.java
+++ platform-sdk/swirlds-common/src/test/java/com/swirlds/common/metrics/extensions/FractionalTimerTests.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.swirlds.common.metrics.extensions;
@@ -20,9 +19,6 @@ package com.swirlds.common.metrics.extensions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.swirlds.base.test.fixtures.time.FakeTime;
-import com.swirlds.common.metrics.extensions.FractionalTimer;
-import com.swirlds.common.metrics.extensions.NoOpFractionalTimer;
-import com.swirlds.common.metrics.extensions.StandardFractionalTimer;
 import java.time.Duration;
 import java.time.Instant;
 import org.junit.jupiter.api.BeforeEach;
