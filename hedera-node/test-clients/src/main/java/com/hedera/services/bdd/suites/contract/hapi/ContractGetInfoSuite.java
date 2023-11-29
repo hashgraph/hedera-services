1c7b3bdc0606072172284dada8d7ae9413510903
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2023-11-29 10:37:59 +0200

    Checkout latest eet versions from develop
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractGetInfoSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractGetInfoSuite.java
@@ -16,6 +16,7 @@
 
 package com.hedera.services.bdd.suites.contract.hapi;
 
+import static com.hedera.services.bdd.junit.TestTags.SMART_CONTRACT;
 import static com.hedera.services.bdd.spec.HapiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.assertions.ContractInfoAsserts.contractWith;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getContractInfo;
@@ -33,8 +34,10 @@ import com.hederahashgraph.api.proto.java.ResponseCodeEnum;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Tag;
 
 @HapiTestSuite
+@Tag(SMART_CONTRACT)
 public class ContractGetInfoSuite extends HapiSuite {
 
     private static final Logger log = LogManager.getLogger(ContractGetInfoSuite.class);
