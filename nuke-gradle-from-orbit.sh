d389d5b8ec6c38a9fe8107d9e94a3011b12ad029
Cody Littley <cody@swirldslabs.com>
2023-11-30 10:01:32 -0600

    Create script for cleaning gradle, for realz
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

---
+++ nuke-gradle-from-orbit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Cleans build files in a way that './gradlew clean' can only dream of.
+
+# The location were this script can be found.
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
+cd $SCRIPT_PATH
+
+rm -rf ~/.gradle
+rm -rf $(find . -name .gradle)
+rm -rf $(find . -name build)
