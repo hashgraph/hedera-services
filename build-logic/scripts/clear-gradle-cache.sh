867458331b656385b026a44b81de2a00c3ffdd37
Cody Littley <cody@swirldslabs.com>
2023-11-30 10:28:59 -0600

    Cleanup.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- build-logic/scripts/clear-gradle-cache.sh
+++ build-logic/scripts/clear-gradle-cache.sh
@@ -5,11 +5,10 @@
 # The location were this script can be found.
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 
-# Navigate to the root of the project.
 {
   cd $SCRIPT_PATH/../..
 
-  rm -rf ~/.gradle
+  rm -rvf ~/.gradle
 
   find . -name .gradle -exec rm -rvf {} \;
   find . -name build -exec rm -rvf {} \;
