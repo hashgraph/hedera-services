9bee465913e517d07cafb577a51db509030a9115
Cody Littley <cody@swirldslabs.com>
2023-11-30 10:15:20 -0600

    Made suggested change.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- build-logic/clear-gradle-cache.sh
+++ build-logic/clear-gradle-cache.sh
@@ -7,5 +7,5 @@ SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
 cd $SCRIPT_PATH
 
 rm -rf ~/.gradle
-rm -rf $(find . -name .gradle)
-rm -rf $(find . -name build)
+rm -rf $(find ../ -name .gradle)
+rm -rf $(find ../ -name build)
