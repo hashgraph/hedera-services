cfaf8e9d9692bbcf9e6c8fa4d04413ec30c6c0bc
Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>
2023-12-01 10:28:45 +0200

    fix: removed initialNonce parameter
    Signed-off-by: Alexander Gadzhalov <alexander.gadzhalov@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/api/TokenServiceApiImpl.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/api/TokenServiceApiImpl.java
@@ -161,7 +161,7 @@ public class TokenServiceApiImpl implements TokenServiceApi {
      * {@inheritDoc}
      */
     @Override
-    public void finalizeHollowAccountAsContract(@NonNull final AccountID hollowAccountId, final long initialNonce) {
+    public void finalizeHollowAccountAsContract(@NonNull final AccountID hollowAccountId) {
         requireNonNull(hollowAccountId);
         final var hollowAccount = requireNonNull(accountStore.get(hollowAccountId));
         if (!IMMUTABILITY_SENTINEL_KEY.equals(hollowAccount.keyOrThrow())) {
