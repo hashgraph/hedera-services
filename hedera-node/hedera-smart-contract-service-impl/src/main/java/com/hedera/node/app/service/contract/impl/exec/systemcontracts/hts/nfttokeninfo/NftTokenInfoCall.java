403c381777b626dba637a4f23edee0f7f1a91d4c
Michael Tinker <michael.tinker@swirldslabs.com>
2023-12-05 20:48:53 -0600

    spotless
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/nfttokeninfo/NftTokenInfoCall.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/nfttokeninfo/NftTokenInfoCall.java
@@ -35,12 +35,9 @@ import com.hedera.node.config.data.LedgerConfig;
 import com.swirlds.config.api.Configuration;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
 
 public class NftTokenInfoCall extends AbstractNonRevertibleTokenViewCall {
-    private static final Logger logger = LogManager.getLogger(NftTokenInfoCall.class);
     private final Configuration configuration;
     private final boolean isStaticCall;
     private final long serialNumber;
@@ -91,11 +88,6 @@ public class NftTokenInfoCall extends AbstractNonRevertibleTokenViewCall {
         }
 
         final var nonNullNft = nft != null ? nft : Nft.DEFAULT;
-        logger.info(
-                "NFT token info call for token {} and serial number {} " + "returned nft {}",
-                token.tokenIdOrElse(ZERO_TOKEN_ID).tokenNum(),
-                serialNumber,
-                nft);
         final var ledgerConfig = configuration.getConfigData(LedgerConfig.class);
         final var ledgerId = Bytes.wrap(ledgerConfig.id().toByteArray()).toString();
         final var nftTokenInfo = nftTokenInfoTupleFor(token, nonNullNft, serialNumber, ledgerId, nativeOperations());
