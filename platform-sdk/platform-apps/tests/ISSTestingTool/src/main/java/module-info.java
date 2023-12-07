8acd793863a9395e78546287ef16f973dffbdbce
Cody Littley <cody@swirldslabs.com>
2023-12-07 15:50:36 -0600

    Cleanup.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/platform-apps/tests/ISSTestingTool/src/main/java/module-info.java
+++ platform-sdk/platform-apps/tests/ISSTestingTool/src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.swirlds.demo.iss {
     requires com.swirlds.base;
     requires com.swirlds.common;
-    requires com.swirlds.platform.core;
     requires com.swirlds.config.api;
     requires com.swirlds.logging;
+    requires com.swirlds.platform.core;
     requires org.apache.logging.log4j;
     requires static com.github.spotbugs.annotations;
 }
