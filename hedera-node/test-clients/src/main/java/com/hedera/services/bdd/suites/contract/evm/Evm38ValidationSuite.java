e535f2042acf9f98b221b64c1e3c1117213f2e82
Stoyan Panayotov <stoyan.panayotov@limechain.tech>
2023-12-07 12:34:40 +0200

    Fix expected response code in Evm38ValidationSuite test case
    Signed-off-by: Stoyan Panayotov <stoyan.panayotov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/evm/Evm38ValidationSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/evm/Evm38ValidationSuite.java
@@ -43,7 +43,6 @@ import static com.hedera.services.bdd.suites.contract.Utils.FunctionType.FUNCTIO
 import static com.hedera.services.bdd.suites.contract.Utils.asAddress;
 import static com.hedera.services.bdd.suites.contract.Utils.captureOneChildCreate2MetaFor;
 import static com.hedera.services.bdd.suites.contract.Utils.getABIFor;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_DELETED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_REVERT_EXECUTED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_CONTRACT_ID;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INVALID_SOLIDITY_ADDRESS;
@@ -78,37 +77,12 @@ import org.junit.jupiter.api.Tag;
 public class Evm38ValidationSuite extends HapiSuite {
 
     private static final Logger LOG = LogManager.getLogger(Evm38ValidationSuite.class);
-    private static final String NAME = "name";
-    private static final String ERC_721_ABI = "ERC721ABI";
-    private static final String NON_EXISTING_MIRROR_ADDRESS = "0000000000000000000000000000000000123456";
-    private static final String NON_EXISTING_NON_MIRROR_ADDRESS = "1234561234561234561234561234568888123456";
-    private static final String INTERNAL_CALLER_CONTRACT = "InternalCaller";
-    private static final String INTERNAL_CALLEE_CONTRACT = "InternalCallee";
-    private static final String REVERT_WITH_REVERT_REASON_FUNCTION = "revertWithRevertReason";
-    private static final String REVERT_WITHOUT_REVERT_REASON_FUNCTION = "revertWithoutRevertReason";
-    private static final String CALL_NON_EXISTING_FUNCTION = "callNonExisting";
-    private static final String CALL_EXTERNAL_FUNCTION = "callExternalFunction";
-    private static final String CALL_REVERT_WITH_REVERT_REASON_FUNCTION = "callRevertWithRevertReason";
-    private static final String CALL_REVERT_WITHOUT_REVERT_REASON_FUNCTION = "callRevertWithoutRevertReason";
-    private static final String TRANSFER_TO_FUNCTION = "transferTo";
-    private static final String SEND_TO_FUNCTION = "sendTo";
-    private static final String CALL_WITH_VALUE_TO_FUNCTION = "callWithValueTo";
-    private static final String INNER_TXN = "innerTx";
-    private static final Long INTRINSIC_GAS_COST = 21000L;
-    private static final Long GAS_LIMIT_FOR_CALL = 25000L;
-    private static final Long NOT_ENOUGH_GAS_LIMIT_FOR_CREATION = 500_000L;
-    private static final Long ENOUGH_GAS_LIMIT_FOR_CREATION = 900_000L;
-    private static final String RECEIVER = "receiver";
-    private static final String ECDSA_KEY = "ecdsaKey";
-    private static final String CUSTOM_PAYER = "customPayer";
     private static final String EVM_VERSION_PROPERTY = "contracts.evm.version";
     private static final String DYNAMIC_EVM_PROPERTY = "contracts.evm.version.dynamic";
     private static final String EVM_VERSION_038 = "v0.38";
     private static final String CREATE_TRIVIAL = "CreateTrivial";
     private static final String BALANCE_OF = "balanceOf";
-    private static final String CREATION = "creation";
     private static final String CREATE_2_TXN = "create2Txn";
-    private static final String SWISS = "swiss";
     private static final String RETURNER = "Returner";
     private static final String CALL_RETURNER = "callReturner";
     public static final String SALT = "aabbccddeeff0011aabbccddeeff0011aabbccddeeff0011aabbccddeeff0011";
@@ -119,11 +93,6 @@ public class Evm38ValidationSuite extends HapiSuite {
 
     public static void main(String... args) {
         new Evm38ValidationSuite().runSuiteSync();
-    }
-
-    @Override
-    public boolean canRunConcurrent() {
-        return false;
     }
 
     @Override
@@ -675,7 +644,7 @@ public class Evm38ValidationSuite extends HapiSuite {
                                 .gas(1_000_000L)))
                 .then(contractCall(SIMPLE_UPDATE_CONTRACT, "set", BigInteger.valueOf(15), BigInteger.valueOf(434))
                         .gas(350_000L)
-                        .hasKnownStatus(CONTRACT_DELETED));
+                        .hasKnownStatus(INVALID_CONTRACT_ID));
     }
 
     @Override
