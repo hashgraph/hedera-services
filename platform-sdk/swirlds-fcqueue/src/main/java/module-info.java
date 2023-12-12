242858f00f474a76dfffc7c18ceb2453a80e5b8c
Maxi Tartaglia <maxi@swirldslabs.com>
2023-12-12 09:14:04 -0300

    add base package to swirlds-fcqueue not as transitive
    Signed-off-by: Maxi Tartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-fcqueue/src/main/java/module-info.java
+++ platform-sdk/swirlds-fcqueue/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.swirlds.fcqueue {
     exports com.swirlds.fcqueue;
 
-    requires transitive com.swirlds.base;
+    requires com.swirlds.base;
     requires transitive com.swirlds.common;
     requires static com.github.spotbugs.annotations;
 }
