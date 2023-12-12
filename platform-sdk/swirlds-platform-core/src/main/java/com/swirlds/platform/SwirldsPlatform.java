2ab85e23c9b9a4f36862a2cd5491960ae666a154
Lazar Petrovic <lpetrovic05@gmail.com>
2023-12-12 12:22:53 +0100

    spot
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/SwirldsPlatform.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/SwirldsPlatform.java
@@ -1307,13 +1307,15 @@ public class SwirldsPlatform implements Platform {
         replayPreconsensusEvents();
         try (final ReservedSignedState reservedState = latestImmutableState.getState("Get PCES recovery state")) {
             if (reservedState == null) {
-                logger.warn(STATE_TO_DISK.getMarker(),
+                logger.warn(
+                        STATE_TO_DISK.getMarker(),
                         "Trying to dump PCES recovery state to disk, but no state is available.");
             } else {
                 final SignedState signedState = reservedState.get();
                 signedState.markAsStateToSave(StateToDiskReason.PCES_RECOVERY_COMPLETE);
 
-                final StateDumpRequest request = StateDumpRequest.create(signedState.reserve("dumping PCES recovery state"));
+                final StateDumpRequest request =
+                        StateDumpRequest.create(signedState.reserve("dumping PCES recovery state"));
 
                 signedStateFileManager.dumpStateTask(request);
                 request.waitForFinished().run();
