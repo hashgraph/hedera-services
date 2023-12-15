c9e0b52b7ac7e3d17b73fa9e351ab0c313f0d468
Maxi Tartaglia <maxi@swirldslabs.com>
2023-12-15 19:06:20 -0300

    Enum converter test
    Signed-off-by: Maxi Tartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-config-impl/src/test/java/com/swirlds/config/impl/internal/ConfigDataServiceTest.java
+++ platform-sdk/swirlds-config-impl/src/test/java/com/swirlds/config/impl/internal/ConfigDataServiceTest.java
@@ -23,6 +23,8 @@ import com.swirlds.config.extensions.sources.SimpleConfigSource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ConfigDataServiceTest {
 
@@ -35,59 +37,98 @@ class ConfigDataServiceTest {
     }
 
     @Test
-    public void converterReturnsNotNull() {
+    void itSuccessfullyCreatesAndReturnsAConverter() {
         // given
-        ConverterService cs = converterService;
+        final ConverterService cs = converterService;
 
         // then:
-        Assertions.assertNotNull(cs.getConverterForType(TestPlainEnum.class));
+        Assertions.assertNotNull(cs.getConverterForType(NumberEnum.class));
     }
 
     @Test
-    public void converterConvertsValueToPlainTestEnum() {
+    void itSuccessfullyConvertsValueToRequestedEnum() {
         // given
-        ConverterService cs = converterService;
+        final ConverterService cs = converterService;
 
         // then:
-        Assertions.assertEquals(TestPlainEnum.UNO, cs.convert("UNO", TestPlainEnum.class));
+        Assertions.assertEquals(NumberEnum.ONE, cs.convert("ONE", NumberEnum.class));
     }
 
     @Test
-    public void converterConvertsValueToComplexTestEnum() {
+    void itSuccessfullyConvertsValueToRequestedEnum2() {
         // given
-        ConverterService cs = converterService;
+        final ConverterService cs = converterService;
 
         // then:
-        Assertions.assertEquals(TestComplexEnum.ONE, cs.convert("ONE", TestComplexEnum.class));
+        Assertions.assertEquals(NumberAndValueEnum.ONE, cs.convert("ONE", NumberAndValueEnum.class));
     }
 
     @Test
-    public void converterConvertsValueToComplexTestEnumEvenHavingASimilarEnum() {
+    void itSuccessfullyConvertsValueToRequestedEnum3() {
         // given
-        ConverterService cs = converterService;
-        cs.getConverterForType(TestPlainEnum.class);
+        final ConverterService cs = converterService;
 
         // then:
-        Assertions.assertEquals(TestComplexEnum.ONE, cs.convert("ONE", TestComplexEnum.class));
+        Assertions.assertEquals(SpecialCharacterEnum.Ñ, cs.convert("Ñ", SpecialCharacterEnum.class));
     }
 
     @Test
-    public void itDoesNotCreateANewInstanceForAlreadyAskedConverters() {
+    void itSuccessfullyConvertsEnumWithSameConstantName() {
         // given
-        ConverterService cs = converterService;
-        ConfigConverter<TestPlainEnum> converterForTypePlainTestEnum = cs.getConverterForType(TestPlainEnum.class);
+        final ConverterService cs = converterService;
+        // pre-call this enum has the same constants as the next call, should not interfere
+        cs.getConverterForType(NumberEnum.class);
 
         // then:
-        Assertions.assertSame(converterForTypePlainTestEnum, cs.getConverterForType(TestPlainEnum.class));
+        Assertions.assertEquals(NumberAndValueEnum.ONE, cs.convert("ONE", NumberAndValueEnum.class));
     }
 
     @Test
-    public void converterThrowsIllegalArgumentIfDoesNotMatch() {
+    void itDoesNotCreateANewInstanceForAlreadyCachedConverters() {
         // given
-        ConverterService cs = converterService;
+        final ConverterService cs = converterService;
+        ConfigConverter<NumberEnum> converter = cs.getConverterForType(NumberEnum.class);
 
         // then:
-        Assertions.assertThrows(IllegalArgumentException.class, () -> cs.convert("DOS", TestPlainEnum.class));
+        Assertions.assertSame(converter, cs.getConverterForType(NumberEnum.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"One", "one", "onE", "oNe", " ONE", "ONE ", "DOS", "", "OnE", "null"})
+    void itFailsConvertingInvalidEnumValues(final String param) {
+        // given
+        final ConverterService cs = converterService;
+        // then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> cs.convert(param, NumberEnum.class));
+    }
+
+    @Test
+    void itReturnsNullForNullValues() {
+        // given
+        final ConverterService cs = converterService;
+        // then:
+        Assertions.assertNull(cs.convert(null, NumberAndValueEnum.class));
+    }
+
+    @Test
+    void itSuccessfullyUsesConfiguredEnumConverterAboveDefaultConverter() {
+        // given
+        ConverterService cs = new ConverterService();
+        FakeEnumConverter converter = new FakeEnumConverter();
+        cs.addConverter(converter); // creates a new enumConverter for NumberAndValueEnum
+        cs.init();
+
+        // then:
+        // NumberValueEnum  gets converted with FakeEnumConverter
+        Assertions.assertSame(converter, cs.getConverterForType(NumberAndValueEnum.class));
+        Assertions.assertEquals(NumberAndValueEnum.ONE, cs.convert("ONE", NumberAndValueEnum.class));
+        Assertions.assertEquals(NumberAndValueEnum.ONE, cs.convert("", NumberAndValueEnum.class));
+        Assertions.assertEquals(NumberAndValueEnum.ONE, cs.convert("DOS", NumberAndValueEnum.class));
+        // and:
+        // NumberEnum stills gets converted with defaultEnumConverter
+        Assertions.assertEquals(NumberEnum.ONE, cs.convert("ONE", NumberEnum.class));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> cs.convert("", NumberEnum.class));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> cs.convert("DOS", NumberEnum.class));
     }
 
     @Test
@@ -95,43 +136,51 @@ class ConfigDataServiceTest {
 
         // given
         final Configuration configuration = ConfigurationBuilder.create()
-                .withSource(new SimpleConfigSource("plain-value", "UNO"))
+                .withSource(new SimpleConfigSource("plain-value", "ONE"))
                 .withSource(new SimpleConfigSource("complex-value-one", "ONE"))
                 .withSource(new SimpleConfigSource("complex-value-two", "DOS"))
                 .withSource(new SimpleConfigSource("complex-value-three", "THREE"))
-                .withConverter(new TestComplexEnumConverter())
                 .build();
 
         // when
-        final TestPlainEnum uno = configuration.getValue("plain-value", TestPlainEnum.class);
-        final TestComplexEnum one = configuration.getValue("complex-value-one", TestComplexEnum.class);
-        final TestComplexEnum two = configuration.getValue("complex-value-two", TestComplexEnum.class);
-        final TestComplexEnum three = configuration.getValue("complex-value-three", TestComplexEnum.class);
+        final NumberEnum uno = configuration.getValue("plain-value", NumberEnum.class);
+        final NumberAndValueEnum one = configuration.getValue("complex-value-one", NumberAndValueEnum.class);
 
         // then
-        Assertions.assertEquals(uno, TestPlainEnum.UNO);
-        Assertions.assertEquals(one, TestComplexEnum.ONE);
-        Assertions.assertEquals(two, TestComplexEnum.ONE);
-        Assertions.assertEquals(three, TestComplexEnum.ONE);
+        Assertions.assertEquals(uno, NumberEnum.ONE);
+        Assertions.assertEquals(one, NumberAndValueEnum.ONE);
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> configuration.getValue("complex-value-two", NumberAndValueEnum.class));
+        Assertions.assertThrows(
+                IllegalArgumentException.class,
+                () -> configuration.getValue("complex-value-three", NumberAndValueEnum.class));
     }
 
-    private static class TestComplexEnumConverter implements ConfigConverter<TestComplexEnum> {
+    /**
+     * An enum converter that always returns the same constant
+     */
+    private static class FakeEnumConverter implements ConfigConverter<NumberAndValueEnum> {
         @Override
-        public TestComplexEnum convert(String value) throws IllegalArgumentException, NullPointerException {
-            return TestComplexEnum.ONE;
+        public NumberAndValueEnum convert(String value) throws IllegalArgumentException, NullPointerException {
+            return NumberAndValueEnum.ONE;
         }
     }
 
-    enum TestPlainEnum {
-        UNO
+    private enum NumberEnum {
+        ONE
     }
 
-    enum TestComplexEnum {
+    private enum NumberAndValueEnum {
         ONE(1);
         final int value;
 
-        TestComplexEnum(int value) {
+        NumberAndValueEnum(int value) {
             this.value = value;
         }
+    }
+
+    private enum SpecialCharacterEnum {
+        Ñ,
     }
 }
