f51b0161da39cf7a8409a4dfd7b3fade2cde1305
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2023-12-26 13:28:30 -0600

    add inheritIO

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/SubProcessHapiTestNode.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/SubProcessHapiTestNode.java
@@ -188,7 +188,8 @@ final class SubProcessHapiTestNode implements HapiTestNode {
                             Long.toString(nodeId))
                     .directory(workingDir.toFile())
                     .redirectOutput(stdout.toFile())
-                    .redirectError(stderr.toFile());
+                    .redirectError(stderr.toFile())
+                    .inheritIO();
 
             handle = builder.start().toHandle();
         } catch (Exception e) {
