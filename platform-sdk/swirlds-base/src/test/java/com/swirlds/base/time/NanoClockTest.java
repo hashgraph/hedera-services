dfab19644ea1af1c401a33f8b1153e91c3a4122e
Maxi Tartaglia <maxi@swirldslabs.com>
2023-12-26 12:26:46 -0300

    NanoClock test
    Signed-off-by: Maxi Tartaglia <maxi@swirldslabs.com>

--- platform-sdk/swirlds-base/src/test/java/com/swirlds/base/time/NanoClockTest.java
+++ platform-sdk/swirlds-base/src/test/java/com/swirlds/base/time/NanoClockTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.swirlds.base.time;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -51,7 +67,6 @@ class NanoClockTest {
         assertEquals(newZone, newClock.getZone());
     }
 
-
     @Test
     void instantReturnsExpectedValueWhenClockProvided() {
         // given
@@ -63,7 +78,6 @@ class NanoClockTest {
         Instant laterInstant = nanoClock.instant();
 
         // then
-        assertEquals(initialInstant, laterInstant.minus( laterInstant.getNano(),ChronoUnit.NANOS));
+        assertEquals(initialInstant, laterInstant.minus(laterInstant.getNano(), ChronoUnit.NANOS));
     }
-
 }
