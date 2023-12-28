c8778c7ee69c139ee137ede18163b83c39aa10e4
Cody Littley <cody@swirldslabs.com>
2023-12-28 14:58:22 -0600

    Fix build issues.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- hedera-node/hedera-config/src/testFixtures/java/module-info.java
+++ hedera-node/hedera-config/src/testFixtures/java/module-info.java
@@ -5,7 +5,6 @@ module com.hedera.node.config.test.fixtures {
     requires transitive com.swirlds.test.framework;
     requires com.hedera.node.config;
     requires com.swirlds.common;
-    requires com.swirlds.fchashmap;
     requires com.swirlds.merkledb;
     requires com.swirlds.platform.core;
     requires com.swirlds.virtualmap;
