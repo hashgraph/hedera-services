45c0fa35bccf5c73bce38af9c5475a371ac8fd5b
Cody Littley <cody@swirldslabs.com>
2023-12-29 15:32:59 -0600

    Fixed bug.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-common/src/main/java/com/swirlds/common/config/TransactionConfig.java
+++ platform-sdk/swirlds-common/src/main/java/com/swirlds/common/config/TransactionConfig.java
@@ -18,25 +18,21 @@ package com.swirlds.common.config;
 
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
-import com.swirlds.config.api.validation.annotation.ConstraintMethod;
 
 /**
  * Configuration regarding transactions
  *
- * @param transactionMaxBytes
- * 		maximum number of bytes allowed in a transaction
- * @param maxTransactionBytesPerEvent
- *      the maximum number of bytes that a single event may contain, not including the event headers. if a single
- *      transaction exceeds this limit, then the event will contain the single transaction only
- * @param maxTransactionCountPerEvent
- * 		the maximum number of transactions that a single event may contain
- * @param throttleTransactionQueueSize
- * 		Stop accepting new non-system transactions into the 4 transaction queues if any of them have more than this
- * 		many.
+ * @param transactionMaxBytes          maximum number of bytes allowed in a transaction
+ * @param maxTransactionBytesPerEvent  the maximum number of bytes that a single event may contain, not including the
+ *                                     event headers. if a single transaction exceeds this limit, then the event will
+ *                                     contain the single transaction only
+ * @param maxTransactionCountPerEvent  the maximum number of transactions that a single event may contain
+ * @param throttleTransactionQueueSize Stop accepting new non-system transactions into the 4 transaction queues if any
+ *                                     of them have more than this many.
  */
 @ConfigData("transaction")
 public record TransactionConfig(
-        @ConstraintMethod("validateTransactionBytes") @ConfigProperty(defaultValue = "6144") int transactionMaxBytes,
+        @ConfigProperty(defaultValue = "6144") int transactionMaxBytes,
         @ConfigProperty(defaultValue = "245760") int maxTransactionBytesPerEvent,
         @ConfigProperty(defaultValue = "245760") int maxTransactionCountPerEvent,
         @ConfigProperty(defaultValue = "100000") int throttleTransactionQueueSize) {}
