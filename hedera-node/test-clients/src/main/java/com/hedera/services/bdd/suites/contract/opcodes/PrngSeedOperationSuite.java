3a2036d42bb588319511e0c7b5ef778927b0d9d4
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2024-01-02 16:35:06 +0200

    fix: remove fuzzy match annotation
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/PrngSeedOperationSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/PrngSeedOperationSuite.java
@@ -50,7 +50,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Tag;
 
-@HapiTestSuite(fuzzyMatch = true)
+@HapiTestSuite
 @Tag(SMART_CONTRACT)
 public class PrngSeedOperationSuite extends HapiSuite {
 
