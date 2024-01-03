373046d0d915b7c796fb8413f78aec0609f559bc
Ivo Yankov <ivo@devlabs.bg>
2024-01-03 16:45:43 +0200

    chore: removed unused method
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/HandleContextImpl.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/HandleContextImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2023 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -902,10 +902,6 @@ public class HandleContextImpl implements HandleContext, FeeContext {
      */
     private boolean dispatchNeedsHapiPayerChecks(@NonNull final TransactionCategory category) {
         return category == SCHEDULED;
-    }
-
-    public HederaFunctionality getFunctionality() {
-        return this.functionality;
     }
 
     private record DispatchValidationResult(@NonNull Key key, @NonNull Fees fees) {
