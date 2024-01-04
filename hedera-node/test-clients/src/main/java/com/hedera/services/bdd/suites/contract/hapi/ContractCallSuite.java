0b1c8a9622a9b0711e4f9f6f6a0edde8e10d1f5b
lukelee-sl <luke.lee@swirldslabs.com>
2024-01-04 07:44:54 -0800

    attempt to fix failing spec
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
@@ -1575,7 +1575,8 @@ public class ContractCallSuite extends HapiSuite {
         final var function = getABIFor(FUNCTION, "getIndirect", CREATE_TRIVIAL);
 
         return defaultHapiSpec("InvalidContract")
-                .given(withOpContext((spec, ctxLog) -> spec.registry().saveContractId("invalid", asContract("1.1.2"))))
+                .given(withOpContext(
+                        (spec, ctxLog) -> spec.registry().saveContractId("invalid", asContract("0.0.100000001"))))
                 .when(withOpContext((spec, opLog) -> allRunFor(
                         spec,
                         ifHapiTest(
