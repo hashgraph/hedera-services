eaf0a23998acee044771f2ca058d012e5bf18b47
Ivo Yankov <ivo@devlabs.bg>
2024-01-08 16:27:19 +0200

    fix: add looser fee matching
    Signed-off-by: Ivo Yankov <ivo@devlabs.bg>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/SStoreSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/opcodes/SStoreSuite.java
@@ -27,8 +27,8 @@ import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.uploadInitCode;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
+import static com.hedera.services.bdd.spec.utilops.records.SnapshotMatchMode.HIGHLY_NON_DETERMINISTIC_FEES;
 import static com.hedera.services.bdd.spec.utilops.records.SnapshotMatchMode.NONDETERMINISTIC_FUNCTION_PARAMETERS;
-import static com.hedera.services.bdd.spec.utilops.records.SnapshotMatchMode.NONDETERMINISTIC_TRANSACTION_FEES;
 import static com.hedera.services.bdd.suites.contract.Utils.FunctionType.FUNCTION;
 import static com.hedera.services.bdd.suites.contract.Utils.getABIFor;
 
@@ -79,9 +79,7 @@ public class SStoreSuite extends HapiSuite {
         final var contract = "GrowArray";
         final var GAS_TO_OFFER = 6_000_000L;
         return HapiSpec.defaultHapiSpec(
-                        "multipleSStoreOpsSucceed",
-                        NONDETERMINISTIC_FUNCTION_PARAMETERS,
-                        NONDETERMINISTIC_TRANSACTION_FEES)
+                        "multipleSStoreOpsSucceed", NONDETERMINISTIC_FUNCTION_PARAMETERS, HIGHLY_NON_DETERMINISTIC_FEES)
                 .given(uploadInitCode(contract), contractCreate(contract))
                 .when(withOpContext((spec, opLog) -> {
                     final var step = 16;
@@ -116,7 +114,7 @@ public class SStoreSuite extends HapiSuite {
     HapiSpec childStorage() {
         // Successfully exceeds deprecated max contract storage of 1 KB
         final var contract = "ChildStorage";
-        return defaultHapiSpec("ChildStorage", NONDETERMINISTIC_TRANSACTION_FEES)
+        return defaultHapiSpec("ChildStorage", HIGHLY_NON_DETERMINISTIC_FEES)
                 .given(uploadInitCode(contract), contractCreate(contract))
                 .when(withOpContext((spec, opLog) -> {
                     final var almostFullKb = MAX_CONTRACT_STORAGE_KB * 3 / 4;
@@ -182,7 +180,7 @@ public class SStoreSuite extends HapiSuite {
         final var GAS_LIMIT = 1_000_000;
         var value = Bytes.fromHexString("0x0000000000000000000000000000000000000000000000000000000000000005")
                 .toArray();
-        return defaultHapiSpec("benchmarkSingleSetter", NONDETERMINISTIC_TRANSACTION_FEES)
+        return defaultHapiSpec("benchmarkSingleSetter", HIGHLY_NON_DETERMINISTIC_FEES)
                 .given(cryptoCreate("payer").balance(10 * ONE_HUNDRED_HBARS), uploadInitCode(contract))
                 .when(
                         contractCreate(contract)
