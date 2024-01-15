084059949c9c373a9df3b4854d1e97b33b7bd171
Cody Littley <cody@swirldslabs.com>
2024-01-15 14:02:05 -0600

    Added more tests. Broken test found.
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/sync/turbo/StaticTurboSyncTests.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/sync/turbo/StaticTurboSyncTests.java
@@ -26,14 +26,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.swirlds.common.constructable.ConstructableRegistry;
 import com.swirlds.common.constructable.ConstructableRegistryException;
+import com.swirlds.common.crypto.Hash;
 import com.swirlds.common.test.fixtures.RandomAddressBookGenerator;
+import com.swirlds.platform.gossip.shadowgraph.SyncUtils;
 import com.swirlds.platform.internal.EventImpl;
 import com.swirlds.platform.system.address.AddressBook;
 import com.swirlds.platform.test.sync.turbo.TurboSyncTestFramework.TestSynchronizationResult;
 import java.io.IOException;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -71,6 +76,9 @@ class StaticTurboSyncTests {
         assertTrue(result.eventsReceivedB().isEmpty());
     }
 
+    /**
+     * One node has many events, the other has zero events.
+     */
     @Test
     void oneNodeHasAllTheEvents() throws IOException {
         final Random random = getRandomPrintSeed();
@@ -90,5 +98,112 @@ class StaticTurboSyncTests {
 
         assertFalse(wereDuplicateEventsSent(initialEventsB, result.eventsReceivedB()));
         assertTrue(nodeHasAllEvents(initialEventsB, result.eventsReceivedB(), initialEventsA));
+    }
+
+    /**
+     * One node has many events, the other has some events but not all of them.
+     */
+    @Test
+    void oneNodeHasManyMoreEventsThanPeer() throws IOException {
+        final Random random = getRandomPrintSeed();
+
+        final AddressBook addressBook =
+                new RandomAddressBookGenerator(random).setSize(8).build();
+
+        final Instant startingTime = Instant.ofEpochMilli(random.nextInt());
+
+        final List<EventImpl> initialEventsA = generateEvents(random, addressBook, startingTime, 100);
+
+        final List<EventImpl> initialEventsB = new ArrayList<>(50);
+        for (int i = 0; i < 50; i++) {
+            initialEventsB.add(initialEventsA.get(i));
+        }
+
+        final TestSynchronizationResult result =
+                simulateSynchronization(random, addressBook, initialEventsA, initialEventsB, startingTime);
+
+        assertTrue(result.eventsReceivedA().isEmpty());
+
+        assertFalse(wereDuplicateEventsSent(initialEventsB, result.eventsReceivedB()));
+        assertTrue(nodeHasAllEvents(initialEventsB, result.eventsReceivedB(), initialEventsA));
+    }
+
+    /**
+     * Both nodes have a lot of events, and they both have the same events.
+     */
+    @Test
+    void bothNodesHaveTheSameEvents() throws IOException {
+        final Random random = getRandomPrintSeed();
+
+        final AddressBook addressBook =
+                new RandomAddressBookGenerator(random).setSize(8).build();
+
+        final Instant startingTime = Instant.ofEpochMilli(random.nextInt());
+
+        final List<EventImpl> initialEventsA = generateEvents(random, addressBook, startingTime, 100);
+        final List<EventImpl> initialEventsB = new ArrayList<>(initialEventsA);
+
+        final TestSynchronizationResult result =
+                simulateSynchronization(random, addressBook, initialEventsA, initialEventsB, startingTime);
+
+        assertTrue(result.eventsReceivedA().isEmpty());
+        assertTrue(result.eventsReceivedB().isEmpty());
+    }
+
+    /**
+     * Create a situation where each node has events needed by their peer.
+     */
+    @Test
+    void nodesEachHaveEventsTheOtherNeeds() throws IOException {
+        final Random random = getRandomPrintSeed(5650957212556381039L); // TODO
+
+        final AddressBook addressBook =
+                new RandomAddressBookGenerator(random).setSize(8).build();
+
+        final Instant startingTime = Instant.ofEpochMilli(random.nextInt());
+        final List<EventImpl> allEvents = generateEvents(random, addressBook, startingTime, 100);
+
+        // "borrow" this handy method to find the tips of the hashgraph.
+        // We want to set up each peer so that they don't have the same events,
+        // but we don't want to leave one peer without a topologically sound
+        // hashgraph. Use the tips to decide which events are safe to remove
+        // from the original knowledge of each peer.
+        final List<EventImpl> tips = SyncUtils.computeSentTips(List.of(), allEvents, 0);
+
+        final Set<Hash> tipHashes = new HashSet<>();
+        for (final EventImpl tip : tips) {
+            tipHashes.add(tip.getBaseHash());
+        }
+
+        final List<EventImpl> initialEventsA = new ArrayList<>(allEvents.size());
+        final List<EventImpl> initialEventsB = new ArrayList<>(allEvents.size());
+        for (final EventImpl event : allEvents) {
+            if (!tipHashes.contains(event.getBaseHash())) {
+                initialEventsA.add(event);
+                initialEventsB.add(event);
+            }
+        }
+
+        for (int i = 0; i < tips.size(); i++) {
+            if (i % 2 == 0) {
+                initialEventsA.add(tips.get(i));
+            } else {
+                initialEventsB.add(tips.get(i));
+            }
+        }
+
+        System.out.println("tip count: " + tips.size()); // TODO
+
+        final TestSynchronizationResult result =
+                simulateSynchronization(random, addressBook, initialEventsA, initialEventsB, startingTime);
+
+        System.out.println("A received: " + result.eventsReceivedA().size()); // TODO
+        System.out.println("B received: " + result.eventsReceivedB().size()); // TODO
+
+        assertFalse(wereDuplicateEventsSent(initialEventsA, result.eventsReceivedA()));
+        assertTrue(nodeHasAllEvents(initialEventsA, result.eventsReceivedA(), allEvents));
+
+        assertFalse(wereDuplicateEventsSent(initialEventsB, result.eventsReceivedB()));
+        assertTrue(nodeHasAllEvents(initialEventsB, result.eventsReceivedB(), allEvents));
     }
 }
