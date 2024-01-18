2ea4e00b4263015586232fda2987d8e42ffaa812
vvairava <30792299+vvairava@users.noreply.github.com>
2024-01-18 18:26:41 +0530

    feat(HIP796): Implement HIP-796 HapiSpecOperation primitives #9886 (updasteTokenPartitionDefinition only) (#10966)
    Signed-off-by: Venkatachalam Vairavan <vvairava@in.ibm.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnVerbs.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/transactions/TxnVerbs.java
@@ -68,6 +68,7 @@ import com.hedera.services.bdd.spec.transactions.system.HapiFreeze;
 import com.hedera.services.bdd.spec.transactions.system.HapiSysDelete;
 import com.hedera.services.bdd.spec.transactions.system.HapiSysUndelete;
 import com.hedera.services.bdd.spec.transactions.token.HapiPartitionCreate;
+import com.hedera.services.bdd.spec.transactions.token.HapiPartitionUpdate;
 import com.hedera.services.bdd.spec.transactions.token.HapiTokenAssociate;
 import com.hedera.services.bdd.spec.transactions.token.HapiTokenBurn;
 import com.hedera.services.bdd.spec.transactions.token.HapiTokenCreate;
@@ -638,5 +639,9 @@ public class TxnVerbs {
 
     public static HapiPartitionCreate createNewPartitionDefinitions(String token) {
         return new HapiPartitionCreate(token);
+    }
+
+    public static HapiPartitionUpdate updatePartitionDefinitionsMemo(String token) {
+        return new HapiPartitionUpdate(token);
     }
 }
