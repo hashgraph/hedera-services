debec4f9d39e6088cc89ecf82e826930cd4b0986
Matt Hess <matt.hess@swirldslabs.com>
2024-01-18 09:47:44 -0700

    Put module requirements in alphabetical order
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-token-service-impl/src/main/java/module-info.java
@@ -11,15 +11,15 @@ module com.hedera.node.app.service.token.impl {
     requires transitive com.swirlds.virtualmap;
     requires transitive dagger;
     requires transitive javax.inject;
-    requires com.swirlds.common;
     requires com.hedera.node.app.hapi.utils;
     requires com.hedera.node.app.service.evm;
     requires com.google.common;
     requires com.swirlds.base;
+    requires com.swirlds.common;
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j;
-    requires static com.github.spotbugs.annotations;
     requires org.bouncycastle.provider;
+    requires static com.github.spotbugs.annotations;
     requires static java.compiler; // javax.annotation.processing.Generated
 
     provides com.hedera.node.app.service.token.TokenService with
