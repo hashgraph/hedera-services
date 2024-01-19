288539c92aeb96596b06713c93cfe252078d3ee4
Michael Heinrichs <netopyr@users.noreply.github.com>
2024-01-19 09:48:40 +0100

    Spotless
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/CacheWarmer.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/CacheWarmer.java
@@ -5,14 +5,13 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.hedera.node.app.workflows.handle;
@@ -28,7 +27,6 @@ import com.hedera.node.app.workflows.TransactionChecker;
 import com.hedera.node.app.workflows.dispatcher.ReadableStoreFactory;
 import com.hedera.node.app.workflows.dispatcher.TransactionDispatcher;
 import com.hedera.node.app.workflows.prehandle.PreHandleResult;
-import com.hedera.node.app.workflows.prehandle.WarmupContextImpl;
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import com.swirlds.platform.system.Round;
 import com.swirlds.platform.system.events.ConsensusEvent;
@@ -77,15 +75,13 @@ public class CacheWarmer {
         executor.execute(() -> {
             final ReadableStoreFactory storeFactory = new ReadableStoreFactory(state);
             for (final ConsensusEvent event : round) {
-                event.forEachTransaction(platformTransaction ->
-                        executor.execute(() -> {
-                            final TransactionBody txBody = extractTransactionBody(platformTransaction);
-                            if (txBody != null) {
-                                final var context = new WarmupContextImpl(txBody, storeFactory);
-                                dispatcher.dispatchWarmup(context);
-                            }
-                        })
-                );
+                event.forEachTransaction(platformTransaction -> executor.execute(() -> {
+                    final TransactionBody txBody = extractTransactionBody(platformTransaction);
+                    if (txBody != null) {
+                        final var context = new WarmupContextImpl(txBody, storeFactory);
+                        dispatcher.dispatchWarmup(context);
+                    }
+                }));
             }
         });
     }
@@ -95,7 +91,7 @@ public class CacheWarmer {
         // First we check if the transaction was already parsed during pre-handle (should be almost always the case)
         final var metadata = platformTransaction.getMetadata();
         if (metadata instanceof PreHandleResult result) {
-            return result.txInfo() == null? null : result.txInfo().txBody();
+            return result.txInfo() == null ? null : result.txInfo().txBody();
         }
 
         // If not we parse it here using existing code. This is not ideal but should be rare.
--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/WarmupContextImpl.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/WarmupContextImpl.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.hedera.node.app.workflows.prehandle;
+package com.hedera.node.app.workflows.handle;
 
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.spi.workflows.WarmupContext;
