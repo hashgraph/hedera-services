a8b8c337b5dc83b3401adface240340c95cbc208
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-01-30 12:08:58 -0600

    test if we are breaking anything else

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/SolvencyPreCheck.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/SolvencyPreCheck.java
@@ -18,7 +18,6 @@ package com.hedera.node.app.workflows;
 
 import static com.hedera.hapi.node.base.HederaFunctionality.CONTRACT_CALL;
 import static com.hedera.hapi.node.base.HederaFunctionality.CONTRACT_CREATE;
-import static com.hedera.hapi.node.base.ResponseCodeEnum.INSUFFICIENT_ACCOUNT_BALANCE;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INSUFFICIENT_PAYER_BALANCE;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INSUFFICIENT_TX_FEE;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
@@ -146,24 +145,30 @@ public class SolvencyPreCheck {
         final var availableBalance = account.tinybarBalance();
         final var offeredFee = txBody.transactionFee();
         final ResponseCodeEnum insufficientFeeResponseCode;
-        // Use this response code for either ingest or triggered schedule transaction
-        if (ingestCheck || fees.totalWithoutServiceFee() == 0L) {
-            insufficientFeeResponseCode = INSUFFICIENT_PAYER_BALANCE;
-        } else {
-            insufficientFeeResponseCode = INSUFFICIENT_ACCOUNT_BALANCE;
+        //        // Use this response code for either ingest or triggered schedule transaction
+        //        if (ingestCheck || fees.totalWithoutServiceFee() == 0L) {
+        //            insufficientFeeResponseCode = INSUFFICIENT_PAYER_BALANCE;
+        //        } else {
+        //            insufficientFeeResponseCode = INSUFFICIENT_ACCOUNT_BALANCE;
+        //        }
+        if (txBody.hasTokenAssociate() && txBody.tokenAssociate().account().accountNum() == 3027557) {
+            System.out.println("totalFee: " + totalFee + ", availableBalance: "
+                    + availableBalance + ", offeredFee: " + offeredFee + ", insufficientFeeResponseCode: "
+                    + INSUFFICIENT_PAYER_BALANCE + " fees "
+                    + fees);
         }
         if (offeredFee < fees.networkFee()) {
             throw new InsufficientNetworkFeeException(INSUFFICIENT_TX_FEE, totalFee);
         }
         if (availableBalance < fees.networkFee()) {
-            throw new InsufficientNetworkFeeException(insufficientFeeResponseCode, totalFee);
+            throw new InsufficientNetworkFeeException(INSUFFICIENT_PAYER_BALANCE, totalFee);
         }
         if (offeredFee < totalFee) {
             throw new InsufficientServiceFeeException(INSUFFICIENT_TX_FEE, totalFee);
         }
 
         if (availableBalance < totalFee) {
-            throw new InsufficientServiceFeeException(insufficientFeeResponseCode, totalFee);
+            throw new InsufficientServiceFeeException(INSUFFICIENT_PAYER_BALANCE, totalFee);
         }
 
         final long additionalCosts;
@@ -178,7 +183,7 @@ public class SolvencyPreCheck {
         if (availableBalance < totalFee + additionalCosts) {
             // FUTURE: This should be checked earlier
             expiryValidation.checkAccountExpiry(account);
-            throw new InsufficientNonFeeDebitsException(insufficientFeeResponseCode, totalFee);
+            throw new InsufficientNonFeeDebitsException(INSUFFICIENT_PAYER_BALANCE, totalFee);
         }
     }
 
