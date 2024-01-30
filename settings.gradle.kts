6ec17eb7ccfe8e44e543d3cb760e399d9f1a74a1
Michael Tinker <michael.tinker@swirldslabs.com>
2024-01-29 21:58:41 -0600

    chore: bump HAPI proto version (#11232)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -138,7 +138,7 @@ fun includeAllProjects(containingFolder: String) {
 }
 
 // The HAPI API version to use for Protobuf sources.
-val hapiProtoVersion = "0.44.0"
+val hapiProtoVersion = "0.46.0"
 
 dependencyResolutionManagement {
     // Protobuf tool versions
