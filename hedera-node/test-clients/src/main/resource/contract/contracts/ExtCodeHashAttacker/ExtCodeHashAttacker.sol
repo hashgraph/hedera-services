e129ee240e535717c4a4d0e2984753eddd0226f8
lukelee-sl <luke.lee@swirldslabs.com>
2024-02-01 20:06:36 -0800

    feat: address cold read issue in ExtCodeHash operation
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>

---
+++ hedera-node/test-clients/src/main/resource/contract/contracts/ExtCodeHashAttacker/ExtCodeHashAttacker.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IExtCodeOperationsChecker {
+    function hashOf(address _address) external view returns (bytes32 hash);
+}
+
+contract ExtCodeHashAttacker {
+
+    // Function that attempts to call hashOf inside a loop, generating addresses on the fly
+    function callHashOfInLoopWithTryCatch(
+        address extCodeOperationsCheckerAddress,
+        uint256 gasLimit,
+        uint256 numberOfTries
+    ) external {
+        IExtCodeOperationsChecker checker = IExtCodeOperationsChecker(extCodeOperationsCheckerAddress);
+
+        for (uint256 i = 0; i < numberOfTries; i++) {
+            // These addresses are non-existent so, extcodehash might not be io intensive
+            // A real attack would involve addresses in existence
+            address generatedAddress = address(uint160(1000 + i));
+
+            //control the gasLimit to leave 0 gas for extcodehash op
+            try checker.hashOf{gas: gasLimit}(generatedAddress) returns (bytes32 result) {
+
+            } catch {
+
+            }
+        }
+    }
+}
+
+turn-off-allowCallsToNonContractAccounts
