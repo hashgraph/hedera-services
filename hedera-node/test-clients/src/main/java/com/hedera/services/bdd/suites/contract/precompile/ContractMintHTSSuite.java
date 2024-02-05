2b43914931f6de7b8a5d71a16a054bd773e38ba5
Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>
2024-02-05 15:48:32 +0200

    test: add security V2 tests for mint with staticcall and callcode
    Signed-off-by: Anastasia Kovaliova <anastasiya.kovaliova@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/ContractMintHTSSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/ContractMintHTSSuite.java
@@ -189,7 +189,7 @@ public class ContractMintHTSSuite extends HapiSuite {
                                 .payingWith(TOKEN_TREASURY)
                                 .signedBy(TOKEN_TREASURY)
                                 .via(v2SecuritySendNftAfterNestedMint)
-                                .gas(2 * GAS_TO_OFFER)
+                                .gas(3 * GAS_TO_OFFER)
                                 .hasKnownStatus(SUCCESS),
                         getTxnRecord(v2SecuritySendNftAfterNestedMint)
                                 .andAllChildRecords()
