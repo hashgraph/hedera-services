a270f00c6ed872e1d2205ce75ea6f9e7ccb8934b
mxtartaglia <maxi@swirldslabs.com>
2024-02-07 13:04:18 -0300

    build fixes
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/base-apps/swirlds-base-sample/src/main/java/module-info.java
+++ platform-sdk/base-apps/swirlds-base-sample/src/main/java/module-info.java
@@ -1,14 +1,13 @@
 module com.swirlds.base.sample {
+    requires com.fasterxml.jackson.databind;
+    requires com.google.common;
     requires com.swirlds.base;
     requires com.swirlds.common;
     requires com.swirlds.config.api;
-    requires com.swirlds.config.impl;
     requires com.swirlds.config.extensions;
+    requires com.swirlds.config.impl;
     requires com.swirlds.metrics.api;
-    requires com.google.common;
     requires io.undertow;
-    requires static com.github.spotbugs.annotations;
     requires jdk.management;
-    requires com.fasterxml.jackson.databind;
     requires org.apache.logging.log4j;
 }
