763c9a466892980a6eb7def0a5b5fa3645aaede3
Austin Littley <austin@swirldslabs.com>
2024-02-08 11:43:52 -0500

    Use ordered soldering for PCES flush requests
    Signed-off-by: Austin Littley <austin@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/LinkedEventIntakeWiring.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/LinkedEventIntakeWiring.java
@@ -20,7 +20,6 @@ import com.swirlds.common.wiring.schedulers.TaskScheduler;
 import com.swirlds.common.wiring.wires.input.BindableInputWire;
 import com.swirlds.common.wiring.wires.input.InputWire;
 import com.swirlds.common.wiring.wires.output.OutputWire;
-import com.swirlds.common.wiring.wires.output.StandardOutputWire;
 import com.swirlds.platform.components.LinkedEventIntake;
 import com.swirlds.platform.event.GossipEvent;
 import com.swirlds.platform.internal.ConsensusRound;
@@ -31,20 +30,18 @@ import java.util.List;
 /**
  * Wiring for the {@link LinkedEventIntake}.
  *
- * @param eventInput                        the input wire for events to be added to the hashgraph
- * @param pauseInput                        the input wire for pausing the linked event intake
- * @param consensusRoundOutput              the output wire for consensus rounds
- * @param consensusEventsOutput             the output wire for consensus events, transformed from the consensus round
- *                                          output
- * @param keystoneEventSequenceNumberOutput the output wire for the keystone event sequence number
- * @param flushRunnable                     the runnable to flush the intake
+ * @param eventInput            the input wire for events to be added to the hashgraph
+ * @param pauseInput            the input wire for pausing the linked event intake
+ * @param consensusRoundOutput  the output wire for consensus rounds
+ * @param consensusEventsOutput the output wire for consensus events, transformed from the consensus round
+ *                              output
+ * @param flushRunnable         the runnable to flush the intake
  */
 public record LinkedEventIntakeWiring(
         @NonNull InputWire<EventImpl> eventInput,
         @NonNull InputWire<Boolean> pauseInput,
         @NonNull OutputWire<ConsensusRound> consensusRoundOutput,
         @NonNull OutputWire<List<EventImpl>> consensusEventsOutput,
-        @NonNull StandardOutputWire<Long> keystoneEventSequenceNumberOutput,
         @NonNull Runnable flushRunnable) {
 
     /**
@@ -62,7 +59,6 @@ public record LinkedEventIntakeWiring(
                 taskScheduler.buildInputWire("pause"),
                 consensusRoundOutput,
                 consensusRoundOutput.buildTransformer("getEvents", "rounds", ConsensusRound::getConsensusEvents),
-                taskScheduler.buildSecondaryOutputWire(),
                 taskScheduler::flush);
     }
 
