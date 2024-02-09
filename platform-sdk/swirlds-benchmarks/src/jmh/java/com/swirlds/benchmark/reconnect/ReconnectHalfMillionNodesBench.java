95ec9de3ce04f0c5fb55aefc0aa9c46dd4aca741
Anthony Petrov <anthony@swirldslabs.com>
2024-02-09 13:42:12 -0800

    feat: 11347: introduce ReconnectHalfMillionNodesBench
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

---
+++ platform-sdk/swirlds-benchmarks/src/jmh/java/com/swirlds/benchmark/reconnect/ReconnectHalfMillionNodesBench.java
@@ -0,0 +1,63 @@
+package com.swirlds.benchmark.reconnect;
+
+import com.swirlds.common.constructable.ConstructableRegistryException;
+import com.swirlds.virtualmap.test.fixtures.TestKey;
+import com.swirlds.virtualmap.test.fixtures.TestValue;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Thread)
+public class ReconnectHalfMillionNodesBench extends VirtualMapReconnectBenchBase {
+
+    private static final Map<TestKey, TestValue> testTeacherMap = new HashMap<>();
+    private static final Map<TestKey, TestValue> testLearnerMap = new HashMap<>();
+
+    static {
+        try {
+            VirtualMapReconnectBenchBase.startup();
+        } catch (ConstructableRegistryException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Create a state to be reused in every run
+        StateBuilder.buildState(
+                new Random(9823452658L),
+                500_000,
+                0.15,
+                .15,
+                testTeacherMap::put,
+                testLearnerMap::put
+        );
+    }
+
+    @Setup(Level.Invocation)
+    @Override
+    public void setupEach() {
+        super.setupEach();
+
+        testTeacherMap.entrySet()
+                .forEach(e -> teacherMap.put(e.getKey(), e.getValue()));
+        testLearnerMap.entrySet()
+                .forEach(e -> learnerMap.put(e.getKey(), e.getValue()));
+    }
+
+    @Benchmark
+    public void reconnectHalfMillionNodes() throws Exception {
+        super.reconnect();
+    }
+
+}
