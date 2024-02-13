061af6aef921da3634db59eed4364d6d9f5e8803
Michael Tinker <michael.tinker@swirldslabs.com>
2024-02-13 11:03:43 -0600

    chore: use shorter contract names (#11491)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: Michael Heinrichs <netopyr@users.noreply.github.com>
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- hedera-node/test-clients/src/main/resource/contract/contracts/ConsValueSysContract/ConsValueSysContract.sol
+++ hedera-node/test-clients/src/main/resource/contract/contracts/ConsValueSysContract/ConsValueSysContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
-contract ReverterConstructorCallWithValueToHederaPrecompile {
+contract ConsValueSysContract {
     constructor() payable {
         (bool success,) = address(0x167).call{value: 1}("");
 
