91c9b355f7dd200393c294a4fd6ef09d8f25a65a
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-02-14 17:39:34 +0100

    fix: Improvements for ConcurrentTestSupport (#11276)
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>
    Co-authored-by: Maxi Tartaglia <152629744+mxtartaglia-sl@users.noreply.github.com>

--- platform-sdk/swirlds-base/src/testFixtures/java/com/swirlds/base/test/fixtures/concurrent/TestExecutor.java
+++ platform-sdk/swirlds-base/src/testFixtures/java/com/swirlds/base/test/fixtures/concurrent/TestExecutor.java
@@ -16,6 +16,8 @@
 
 package com.swirlds.base.test.fixtures.concurrent;
 
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -36,7 +38,7 @@ public interface TestExecutor {
      *
      * @param runnables the runnables to execute
      */
-    void executeAndWait(Collection<Runnable> runnables);
+    void executeAndWait(@NonNull Collection<Runnable> runnables);
 
     /**
      * Executes the given runnables in parallel. This method will block until all the callables have completed. If the
@@ -45,7 +47,7 @@ public interface TestExecutor {
      *
      * @param runnables the runnables to execute
      */
-    void executeAndWait(Runnable... runnables);
+    void executeAndWait(@NonNull Runnable... runnables);
 
     /**
      * Executes the given callables in parallel. This method will block until all the callables have completed. If the
@@ -56,16 +58,16 @@ public interface TestExecutor {
      * @param <V>       the type of the callable result
      * @return the results of the callables
      */
-    <V> List<V> submitAndWait(Collection<Callable<V>> callables);
+    <V> @NonNull List<V> submitAndWait(Collection<Callable<V>> callables);
 
     /**
-     * Executes the given callables in parallel. This method will block until all the callables have completed. If the
-     * callables do not return within the maximum wait time, then the callables will be cancelled and a
+     * Executes the given callable in parallel. This method will block until all the callable have completed. If the
+     * callable do not return within the maximum wait time, then the callable will be cancelled and a
      * {@link TimeoutException} will be thrown.
      *
-     * @param callables the callables to execute
-     * @param <V>       the type of the callable result
-     * @return the results of the callables
+     * @param callable a callable array to execute
+     * @param <V>      the type of the callable result
+     * @return the result of the callable
      */
-    <V> List<V> submitAndWait(Callable<V>... callables);
+    <V> @Nullable List<V> submitAndWait(@NonNull Callable<V>... callable);
 }
