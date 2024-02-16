19e98347fe4129f1181c84ba22a7b97b1e158326
georgi-l95 <glazarov95@gmail.com>
2024-02-16 16:46:15 +0200

    feat: add entry point
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

---
+++ block-node/blocknode-core/src/main/java/com/hedera/node/blocknode/core/BlockNode.java
@@ -0,0 +1,8 @@
+package com.hedera.node.blocknode.core;
+
+public class BlockNode {
+
+    public static void main(final String... args) {
+        System.out.println("Starting Block Node");
+    }
+}
