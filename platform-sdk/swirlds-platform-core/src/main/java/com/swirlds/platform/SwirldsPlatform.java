e54e5c3e36623fcf498fde5fd431fb222e428f52
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2024-02-20 07:52:56 -0600

    fix: reset event creator as part of reconnect cleanup (#11463)
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/SwirldsPlatform.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/SwirldsPlatform.java
@@ -603,7 +603,6 @@ public class SwirldsPlatform implements Platform {
 
         final EventCreationManager eventCreationManager = buildEventCreationManager(
                 platformContext,
-                time,
                 this,
                 currentAddressBook,
                 selfId,
