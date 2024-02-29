e52f577a2a2206488dcde2d4efb78468f2ad22af
Kore Aguda <kore@swirldslabs.com>
2024-02-29 17:50:46 -0600

    Flatten Gossip class
    Signed-off-by: Kore Aguda <kore@swirldslabs.com>

--- platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/network/TopologyTest.java
+++ platform-sdk/swirlds-unit-tests/core/swirlds-platform-test/src/test/java/com/swirlds/platform/test/network/TopologyTest.java
@@ -35,7 +35,6 @@ import java.util.Random;
 import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
