8446f96526cbf2fcb7c3b80e1c901e90b0e89a89
Michael Tinker <michael.tinker@swirldslabs.com>
2024-03-04 10:23:17 -0600

    Use <= not < when checking HBAR transfers max len
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/merkle/MerkleHederaState.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/state/merkle/MerkleHederaState.java
@@ -112,7 +112,7 @@ public class MerkleHederaState extends PartialNaryMerkleInternal implements Merk
     private static final long CLASS_ID = 0x2de3ead3caf06392L;
     // Uncomment the following class ID to run a mono -> modular state migration
     // NOTE: also change class ID of ServicesState
-    //    private static final long CLASS_ID = 0x8e300b0dfdafbb1aL;
+//        private static final long CLASS_ID = 0x8e300b0dfdafbb1aL;
     private static final int VERSION_1 = 30;
     private static final int CURRENT_VERSION = VERSION_1;
 
