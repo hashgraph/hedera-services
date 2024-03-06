e2599c63ed97c2fa34f1ffc01f396d714edfa8a9
Michael Tinker <michael.tinker@swirldslabs.com>
2024-03-05 22:19:03 -0600

    Fix response codes
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/tokenuri/TokenUriCall.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/tokenuri/TokenUriCall.java
@@ -16,7 +16,10 @@
 
 package com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokenuri;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TOKEN_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.SUCCESS;
+import static com.hedera.hapi.node.base.TokenType.FUNGIBLE_COMMON;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.FullResult.haltResult;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.FullResult.successResult;
 import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCall.PricedResult.gasOnly;
 
@@ -24,6 +27,7 @@ import com.hedera.hapi.node.state.token.Token;
 import com.hedera.node.app.service.contract.impl.exec.gas.SystemContractGasCalculator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.AbstractHtsCall;
 import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
+import com.hedera.node.app.service.evm.contracts.operations.HederaExceptionalHaltReason;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import org.hyperledger.besu.evm.frame.MessageFrame;
@@ -58,6 +62,16 @@ public class TokenUriCall extends AbstractHtsCall {
     public @NonNull PricedResult execute(MessageFrame frame) {
         var metadata = URI_QUERY_NON_EXISTING_TOKEN_ERROR;
         if (token != null) {
+            if (token.tokenType() == FUNGIBLE_COMMON) {
+                // (FUTURE) consider removing this pattern, but for now match
+                // mono-service by halting on an invalid token type
+                return gasOnly(
+                        haltResult(
+                                HederaExceptionalHaltReason.ERROR_DECODING_PRECOMPILE_INPUT,
+                                gasCalculator.viewGasRequirement()),
+                        INVALID_TOKEN_ID,
+                        false);
+            }
             final var nft = nativeOperations().getNft(token.tokenIdOrThrow().tokenNum(), serialNo);
             if (nft != null) {
                 metadata = new String(nft.metadata().toByteArray());
