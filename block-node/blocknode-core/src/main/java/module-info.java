895e3560acdb631f8581d245666a683041fa122a
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-03-07 14:49:52 +0200

    Orders modules in module-info alphabetically
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- block-node/blocknode-core/src/main/java/module-info.java
+++ block-node/blocknode-core/src/main/java/module-info.java
@@ -17,8 +17,8 @@ module com.hedera.storage.blocknode.core {
     requires com.swirlds.config.extensions;
     requires grpc.netty;
     requires grpc.stub;
-    requires io.netty.transport;
     requires io.netty.transport.classes.epoll;
+    requires io.netty.transport;
     requires org.apache.logging.log4j;
     // Require modules which are needed for compilation and should be available to all modules that depend on this
     // module (including tests and other source sets).
@@ -27,8 +27,8 @@ module com.hedera.storage.blocknode.core {
     requires transitive com.hedera.storage.blocknode.filesystem.api;
     requires transitive com.hedera.storage.blocknode.grpc.api;
     requires transitive com.hedera.storage.blocknode.state;
-    requires transitive com.hedera.node.hapi;
     requires transitive com.hedera.node.app;
+    requires transitive com.hedera.node.hapi;
     requires static com.github.spotbugs.annotations;
 }
 
