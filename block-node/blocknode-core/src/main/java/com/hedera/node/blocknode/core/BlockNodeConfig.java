1383a9bb3cee0c8626ecd75aa39e7470d421c0e9
Konstantina Blazhukova <konstantina.blajukova@gmail.com>
2024-03-07 15:24:29 +0200

    Deletes unused config
    Signed-off-by: Konstantina Blazhukova <konstantina.blajukova@gmail.com>

--- block-node/blocknode-core/src/main/java/com/hedera/node/blocknode/core/BlockNodeConfig.java
+++
@@ -1,9 +0,0 @@
-package com.hedera.node.blocknode.core;
-
-import com.hedera.node.config.NodeProperty;
-import com.hedera.node.config.types.Profile;
-import com.swirlds.config.api.ConfigProperty;
-
-public record BlockNodeConfig (
-        @ConfigProperty(value = "profiles.active", defaultValue = "PROD") @NodeProperty Profile activeProfile
-) {}
