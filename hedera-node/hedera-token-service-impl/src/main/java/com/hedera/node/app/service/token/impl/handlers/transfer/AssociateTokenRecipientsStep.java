5c81bc704b3c054ae1362ad9a1b839e416c677a8
Michael Tinker <michael.tinker@swirldslabs.com>
2024-03-08 20:38:15 -0600

    Precise translation for TOKEN_NOT_ASSOCIATED
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/transfer/AssociateTokenRecipientsStep.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/transfer/AssociateTokenRecipientsStep.java
@@ -18,19 +18,25 @@ package com.hedera.node.app.service.token.impl.handlers.transfer;
 
 import static com.hedera.hapi.node.base.ResponseCodeEnum.ACCOUNT_FROZEN_FOR_TOKEN;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.ACCOUNT_KYC_NOT_GRANTED_FOR_TOKEN;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.AMOUNT_EXCEEDS_ALLOWANCE;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_ACCOUNT_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_NFT_ID;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.NO_REMAINING_AUTOMATIC_ASSOCIATIONS;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.SPENDER_DOES_NOT_HAVE_ALLOWANCE;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.TOKEN_NOT_ASSOCIATED_TO_ACCOUNT;
+import static com.hedera.node.app.service.token.impl.handlers.transfer.NFTOwnersChangeStep.validateSpenderHasAllowance;
 import static com.hedera.node.app.service.token.impl.util.TokenHandlerHelper.getIfUsable;
 import static com.hedera.node.app.spi.workflows.HandleException.validateFalse;
 import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
 import static java.util.Collections.emptyList;
 import static java.util.Objects.requireNonNull;
 
+import com.hedera.hapi.node.base.AccountAmount;
 import com.hedera.hapi.node.base.AccountID;
+import com.hedera.hapi.node.base.NftTransfer;
 import com.hedera.hapi.node.base.TokenAssociation;
 import com.hedera.hapi.node.base.TokenID;
+import com.hedera.hapi.node.state.token.Account;
 import com.hedera.hapi.node.state.token.Token;
 import com.hedera.hapi.node.token.CryptoTransferTransactionBody;
 import com.hedera.node.app.service.token.impl.WritableAccountStore;
@@ -39,6 +45,7 @@ import com.hedera.node.app.service.token.impl.WritableTokenRelationStore;
 import com.hedera.node.app.service.token.impl.WritableTokenStore;
 import com.hedera.node.app.service.token.impl.handlers.BaseTokenHandler;
 import com.hedera.node.app.spi.workflows.HandleContext;
+import com.hedera.node.app.spi.workflows.HandleException;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.ArrayList;
 import java.util.List;
@@ -62,33 +69,58 @@ public class AssociateTokenRecipientsStep extends BaseTokenHandler implements Tr
         final var tokenRelStore = handleContext.writableStore(WritableTokenRelationStore.class);
         final var accountStore = handleContext.writableStore(WritableAccountStore.class);
         final var nftStore = handleContext.writableStore(WritableNftStore.class);
-        final var expiryValidator = handleContext.expiryValidator();
         final List<TokenAssociation> newAssociations = new ArrayList<>();
-        final var payer = handleContext.payer();
 
         for (final var xfers : op.tokenTransfersOrElse(emptyList())) {
             final var tokenId = xfers.tokenOrThrow();
             final var token = getIfUsable(tokenId, tokenStore);
 
             for (final var aa : xfers.transfersOrElse(emptyList())) {
-                final var accountId = aa.accountID();
-                final TokenAssociation newAssociation = validateAndBuildAutoAssociation(
-                        accountId, tokenId, token, accountStore, tokenRelStore, handleContext);
+                final var accountId = aa.accountIDOrElse(AccountID.DEFAULT);
+                final TokenAssociation newAssociation;
+                try {
+                    newAssociation = validateAndBuildAutoAssociation(
+                            accountId, tokenId, token, accountStore, tokenRelStore, handleContext);
+                } catch (HandleException e) {
+                    // (FUTURE) Remove this catch and stop translating TOKEN_NOT_ASSOCIATED_TO_ACCOUNT
+                    // into e.g. SPENDER_DOES_NOT_HAVE_ALLOWANCE; we need this only for mono-service
+                    // fidelity during diff testing
+                    if (mayNeedTranslation(e, aa)) {
+                        validateFungibleAllowance(
+                                requireNonNull(accountStore.getAccountById(aa.accountIDOrThrow())),
+                                handleContext.payer(),
+                                tokenId,
+                                aa.amount());
+                    }
+                    throw e;
+                }
                 if (newAssociation != null) {
                     newAssociations.add(newAssociation);
                 }
             }
 
             for (final var nftTransfer : xfers.nftTransfersOrElse(emptyList())) {
-                final var receiverId = nftTransfer.receiverAccountID();
-                final var senderId = nftTransfer.senderAccountID();
+                final var receiverId = nftTransfer.receiverAccountIDOrElse(AccountID.DEFAULT);
+                final var senderId = nftTransfer.senderAccountIDOrElse(AccountID.DEFAULT);
                 // sender should be associated already. If not throw exception
-                validateTrue(tokenRelStore.get(senderId, tokenId) != null, TOKEN_NOT_ASSOCIATED_TO_ACCOUNT);
-
                 final var nft = nftStore.get(tokenId, nftTransfer.serialNumber());
+                try {
+                    validateTrue(tokenRelStore.get(senderId, tokenId) != null, TOKEN_NOT_ASSOCIATED_TO_ACCOUNT);
+                } catch (HandleException e) {
+                    // (FUTURE) Remove this catch and stop translating TOKEN_NOT_ASSOCIATED_TO_ACCOUNT
+                    // into e.g. SPENDER_DOES_NOT_HAVE_ALLOWANCE; we need this only for mono-service
+                    // fidelity during diff testing
+                    if (nft != null && mayNeedTranslation(e, nftTransfer)) {
+                        validateSpenderHasAllowance(
+                                requireNonNull(accountStore.getAccountById(senderId)),
+                                handleContext.payer(),
+                                tokenId,
+                                nft);
+                    }
+                    throw e;
+                }
                 validateTrue(nft != null, INVALID_NFT_ID);
-
-                final TokenAssociation newAssociation = validateAndBuildAutoAssociation(
+                final var newAssociation = validateAndBuildAutoAssociation(
                         receiverId, tokenId, token, accountStore, tokenRelStore, handleContext);
                 if (newAssociation != null) {
                     newAssociations.add(newAssociation);
@@ -101,16 +133,24 @@ public class AssociateTokenRecipientsStep extends BaseTokenHandler implements Tr
         }
     }
 
+    private boolean mayNeedTranslation(final HandleException e, final AccountAmount adjustment) {
+        return e.getStatus() == TOKEN_NOT_ASSOCIATED_TO_ACCOUNT && adjustment.isApproval() && adjustment.amount() < 0;
+    }
+
+    private boolean mayNeedTranslation(final HandleException e, final NftTransfer nftTransfer) {
+        return e.getStatus() == TOKEN_NOT_ASSOCIATED_TO_ACCOUNT && nftTransfer.isApproval();
+    }
+
     /**
      * Associates the token with the account if it is not already associated. It is auto-associated only if there are
      * open auto-associations available on the account.
      *
-     * @param accountId         The account to associate the token with
-     * @param tokenId           The tokenID of the token to associate with the account
-     * @param token             The token to associate with the account
-     * @param accountStore      The account store
-     * @param tokenRelStore     The token relation store
-     * @param handleContext     The context
+     * @param accountId The account to associate the token with
+     * @param tokenId The tokenID of the token to associate with the account
+     * @param token The token to associate with the account
+     * @param accountStore The account store
+     * @param tokenRelStore The token relation store
+     * @param handleContext The context
      */
     private TokenAssociation validateAndBuildAutoAssociation(
             @NonNull final AccountID accountId,
@@ -136,5 +176,21 @@ public class AssociateTokenRecipientsStep extends BaseTokenHandler implements Tr
             validateFalse(tokenRel.frozen(), ACCOUNT_FROZEN_FOR_TOKEN);
             return null;
         }
+    }
+
+    private void validateFungibleAllowance(
+            @NonNull final Account account,
+            @NonNull final AccountID topLevelPayer,
+            @NonNull final TokenID tokenId,
+            final long amount) {
+        final var tokenAllowances = account.tokenAllowancesOrElse(emptyList());
+        for (final var allowance : tokenAllowances) {
+            if (topLevelPayer.equals(allowance.spenderId()) && tokenId.equals(allowance.tokenId())) {
+                final var newAllowanceAmount = allowance.amount() + amount;
+                validateTrue(newAllowanceAmount >= 0, AMOUNT_EXCEEDS_ALLOWANCE);
+                return;
+            }
+        }
+        throw new HandleException(SPENDER_DOES_NOT_HAVE_ALLOWANCE);
     }
 }
