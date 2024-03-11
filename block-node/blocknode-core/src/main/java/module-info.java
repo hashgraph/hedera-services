d2554d468f7b99acba6cb06d87f6f2c901e6636f
georgi-l95 <glazarov95@gmail.com>
2024-03-11 13:40:04 +0200

    fix: module checks
    Signed-off-by: georgi-l95 <glazarov95@gmail.com>

--- block-node/blocknode-core/src/main/java/module-info.java
+++ block-node/blocknode-core/src/main/java/module-info.java
@@ -4,20 +4,11 @@ module com.hedera.storage.blocknode.core {
             com.hedera.storage.blocknode.core.test;
 
     // Require the modules needed for compilation.
-    requires com.hedera.storage.blocknode.filesystem.local;
-    requires com.hedera.storage.blocknode.filesystem.s3;
-    requires com.swirlds.config.extensions;
-    requires grpc.netty;
-    requires grpc.stub;
+    requires com.hedera.storage.blocknode.config;
+    requires com.swirlds.config.api;
+    requires io.grpc;
     requires org.apache.logging.log4j;
-
     // Require modules which are needed for compilation and should be available to all modules that depend on this
     // module (including tests and other source sets).
-    requires transitive com.hedera.storage.blocknode.config;
-    requires transitive com.hedera.storage.blocknode.core.spi;
-    requires transitive com.hedera.storage.blocknode.filesystem.api;
-    requires transitive com.hedera.storage.blocknode.grpc.api;
-    requires transitive com.hedera.storage.blocknode.state;
-    requires transitive com.hedera.node.hapi;
     requires static com.github.spotbugs.annotations;
 }
