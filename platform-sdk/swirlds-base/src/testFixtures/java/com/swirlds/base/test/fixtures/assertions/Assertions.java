75045728cba512b7b0b770a2b4e31c135cb74919
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-03-12 14:38:23 +0100

    assertion helper
    Signed-off-by: Hendrik Ebbers <hendrik.ebbers@web.de>

--- platform-sdk/swirlds-base/src/testFixtures/java/com/swirlds/base/test/fixtures/assertions/Assertions.java
+++ platform-sdk/swirlds-base/src/testFixtures/java/com/swirlds/base/test/fixtures/assertions/Assertions.java
@@ -16,27 +16,64 @@
 
 package com.swirlds.base.test.fixtures.assertions;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.Objects;
+import org.assertj.core.api.AbstractStringAssert;
 import org.assertj.core.api.AbstractThrowableAssert;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.assertj.core.api.InstanceOfAssertFactory;
 
+/**
+ * A collection of assertion methods for use in tests.
+ */
 public class Assertions {
 
+    /**
+     * Asserts that the {@link Throwable} of the given {@link AbstractThrowableAssert} has a non-null message.
+     *
+     * @param abstractThrowableAssert the assert to check
+     */
     private static void assertHasAnyMessage(
             @NonNull final AbstractThrowableAssert<?, ? extends Throwable> abstractThrowableAssert) {
         if (abstractThrowableAssert == null) {
             throw new NullPointerException("abstractThrowableAssert must not be null");
         }
-        abstractThrowableAssert.extracting(Throwable::getMessage).isNotNull();
+        abstractThrowableAssert
+                .extracting(Throwable::getMessage, asString())
+                .isNotEmpty()
+                .isNotBlank()
+                .isNotNull();
     }
 
+    /**
+     * Returns an {@link InstanceOfAssertFactory} for {@link String} instances: {@link  AbstractStringAssert}
+     *
+     * @return the factory
+     */
+    private static InstanceOfAssertFactory<String, AbstractStringAssert<?>> asString() {
+        return as(InstanceOfAssertFactories.STRING);
+    }
+
+    /**
+     * Asserts that the given {@link Throwable} has a non-null message.
+     *
+     * @param t the throwable to check
+     */
     private static void assertHasAnyMessage(@NonNull final Throwable t) {
         assertHasAnyMessage(assertThat(t).isNotNull());
     }
 
+    /**
+     * Asserts that the given {@link Throwable} is an instance of the given {@link Class} and has a non-null message.
+     *
+     * @param throwableClass the class of the expected throwable
+     * @param runnable       the runnable to check
+     * @param <T>            the type of the expected throwable
+     */
     public static <T extends Throwable> void assertThrowsWithMessage(
             @NonNull final Class<T> throwableClass, @NonNull final Runnable runnable) {
         Objects.requireNonNull(throwableClass, "throwableClass must not be null");
@@ -45,14 +82,16 @@ public class Assertions {
             runnable.run();
             fail("Expected an exception to be thrown");
         } catch (final Throwable t) {
+            assertThat(t).isInstanceOf(throwableClass);
             assertHasAnyMessage(t);
-            assertThat(t)
-                    .isInstanceOf(NullPointerException.class)
-                    .extracting(Throwable::getMessage)
-                    .isNotNull();
         }
     }
 
+    /**
+     * Asserts that the given {@link Runnable} throws an {@link NullPointerException} with a non-null message.
+     *
+     * @param runnable the runnable to check
+     */
     public static void assertThrowsNPE(@NonNull final Runnable runnable) {
         assertThrowsWithMessage(NullPointerException.class, runnable);
     }
