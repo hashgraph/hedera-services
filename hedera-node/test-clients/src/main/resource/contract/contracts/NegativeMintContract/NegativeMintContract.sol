463f830f06d8168ce8680ea96668586b1259ad44
Valentin Valkanov <valentin.valkanov@limechain.tech>
2024-03-13 13:08:47 +0200

    fix: improve solidity contract
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/test-clients/src/main/resource/contract/contracts/NegativeMintContract/NegativeMintContract.sol
+++ hedera-node/test-clients/src/main/resource/contract/contracts/NegativeMintContract/NegativeMintContract.sol
@@ -8,14 +8,14 @@ contract NegativeMintContract is HederaTokenService {
 
     int128 constant TOO_BIG_VALUE = 78900000000000000000;
     int128 constant TOO_BIG_NEGATIVE_VALUE = -78900000000000000000;
-    int32 constant INVALID_ADDRESS = 12300;
+    int32 constant INVALID_ADDRESS = 99911999;
 
     function mintToken(bytes[] memory metadata, address tokenAddress, int64 amount) external {
-        (bool success, bytes memory result) = precompileAddress.call(
-            abi.encodeWithSelector(IHederaTokenService.mintToken.selector,
-                tokenAddress, amount, metadata));
+        (int rc, uint64 newSupply, int64[] memory sns) = HederaTokenService.mintToken(tokenAddress, amount, metadata);
 
-        require(success);
+        if (rc != HederaResponseCodes.SUCCESS) {
+            revert("Failed to mint token");
+        }
     }
 
     function mintExtremeValue(bytes[] memory metadata, bool useNegative, address tokenAddress) external {
