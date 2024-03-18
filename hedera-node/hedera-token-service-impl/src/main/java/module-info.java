ca6c59ebc0f37de8a00005b7c711598196c1a4e8
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-03-18 12:24:05 -0500

    module info fixes
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-token-service-impl/src/main/java/module-info.java
@@ -16,7 +16,6 @@ module com.hedera.node.app.service.token.impl {
     requires com.google.common;
     requires com.swirlds.base;
     requires com.swirlds.common;
-    requires com.swirlds.platform.core;
     requires org.apache.commons.lang3;
     requires org.apache.logging.log4j;
     requires org.bouncycastle.provider;
