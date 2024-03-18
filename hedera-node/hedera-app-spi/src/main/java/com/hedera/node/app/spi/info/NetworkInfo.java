3310fc9e19e9f1d97d22cebdff914394fba214d6
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-03-18 11:57:33 -0500

    javadocs
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/info/NetworkInfo.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/info/NetworkInfo.java
@@ -43,5 +43,10 @@ public interface NetworkInfo {
     @Nullable
     NodeInfo nodeInfo(long nodeId);
 
+    /**
+     * Returns true if the network contains a node with the given ID.
+     * @param nodeId the ID of the node to check for
+     * @return true if the network contains a node with the given ID
+     */
     boolean containsNode(long nodeId);
 }
