ae1fce2722f867d1c4a926f59f7b14542f692f96
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-03-18 16:30:19 -0500

    Update to use main protobufs branch
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hapi/build.gradle.kts
+++ hedera-node/hapi/build.gradle.kts
@@ -25,7 +25,7 @@ description = "Hedera API"
 
 // Add downloaded HAPI repo protobuf files into build directory and add to sources to build them
 tasks.cloneHederaProtobufs {
-    branchOrTag = "remove-deleted-from-token-relation"
+    branchOrTag = "main"
     // As long as the 'branchOrTag' above is not stable, run always:
     outputs.upToDateWhen { false }
 }
