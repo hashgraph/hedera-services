5b3a4ae5e8c3167df5f32c16a17b3264891b7f51
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2024-03-19 10:15:27 -0500

    fix: Fix `updateWeight` for removed nodes (#12122)
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/schemas/InitialModServiceTokenSchema.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/schemas/InitialModServiceTokenSchema.java
@@ -186,6 +186,29 @@ public class InitialModServiceTokenSchema extends Schema {
     }
 
     @Override
+    public void restart(@NonNull MigrationContext ctx) {
+        // We need to validate and mark any node that are removed during upgrade as deleted.
+        // Since restart is called in the schema after an upgrade, and we don't want to depend on schema version change
+        // validate all the nodeIds from the addressBook in state and mark them as deleted if they are not yet deleted
+        // in staking info.
+        final var stakingToState = ctx.newStates().<EntityNumber, StakingNodeInfo>get(STAKING_INFO_KEY);
+        final var networkInfo = ctx.networkInfo();
+        stakingToState.keys().forEachRemaining(nodeId -> {
+            final var stakingInfo = requireNonNull(stakingToState.get(nodeId));
+            if (!networkInfo.containsNode(nodeId.number()) && !stakingInfo.deleted()) {
+                stakingToState.put(
+                        nodeId, stakingInfo.copyBuilder().deleted(true).build());
+                log.info(
+                        "Node {} is marked deleted since it is deleted from addressBook during restart.",
+                        nodeId.number());
+            }
+        });
+        if (stakingToState.isModified()) {
+            ((WritableKVStateBase) stakingToState).commit();
+        }
+    }
+
+    @Override
     public void migrate(@NonNull final MigrationContext ctx) {
         final var isGenesis = ctx.previousVersion() == null;
         if (isGenesis) {
