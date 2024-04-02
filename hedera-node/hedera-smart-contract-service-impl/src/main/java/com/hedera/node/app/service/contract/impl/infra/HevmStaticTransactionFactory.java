285e0e78486f976c25be873fb19aa8e2aeb3b947
Kim Rader <kim.rader@swirldslabs.com>
2024-04-02 10:56:34 -0700

    fix: ContractCallLocal INSUFFICIENT_GAS ERROR (#12484)
    Signed-off-by: Kim Rader <kim.rader@swirldslabs.com>

--- hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/infra/HevmStaticTransactionFactory.java
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/infra/HevmStaticTransactionFactory.java
@@ -16,12 +16,13 @@
 
 package com.hedera.node.app.service.contract.impl.infra;
 
-import static com.hedera.hapi.node.base.ResponseCodeEnum.CONTRACT_NEGATIVE_GAS;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INSUFFICIENT_GAS;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.MAX_GAS_LIMIT_EXCEEDED;
 import static com.hedera.node.app.service.contract.impl.hevm.HederaEvmTransaction.NOT_APPLICABLE;
 import static com.hedera.node.app.service.contract.impl.utils.ConversionUtils.asPriorityId;
 import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
 import static java.util.Objects.requireNonNull;
+import static org.apache.tuweni.bytes.Bytes.EMPTY;
 
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.contract.ContractCallLocalQuery;
@@ -29,10 +30,12 @@ import com.hedera.hapi.node.transaction.Query;
 import com.hedera.node.app.service.contract.impl.annotations.QueryScope;
 import com.hedera.node.app.service.contract.impl.hevm.HederaEvmTransaction;
 import com.hedera.node.app.service.token.ReadableAccountStore;
+import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.QueryContext;
 import com.hedera.node.config.data.ContractsConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import javax.inject.Inject;
+import org.hyperledger.besu.evm.gascalculator.GasCalculator;
 
 /**
  * A factory that creates a {@link HederaEvmTransaction} for static calls.
@@ -41,14 +44,18 @@ import javax.inject.Inject;
  */
 @QueryScope
 public class HevmStaticTransactionFactory {
+    private static final long INTRINSIC_GAS_LOWER_BOUND = 21_000L;
     private final ContractsConfig contractsConfig;
+    private final GasCalculator gasCalculator;
     private final QueryContext context;
     private final AccountID payerId;
 
     @Inject
-    public HevmStaticTransactionFactory(@NonNull final QueryContext context) {
+    public HevmStaticTransactionFactory(
+            @NonNull final QueryContext context, @NonNull final GasCalculator gasCalculator) {
         this.context = requireNonNull(context);
         this.contractsConfig = context.configuration().getConfigData(ContractsConfig.class);
+        this.gasCalculator = requireNonNull(gasCalculator);
         this.payerId = requireNonNull(context.payer());
     }
 
@@ -80,8 +87,40 @@ public class HevmStaticTransactionFactory {
                 null);
     }
 
+    /**
+     * Given a {@link Query} and an {@link Exception},
+     * create and return a {@link HederaEvmTransaction} containing the exception and gas limit
+     *
+     * @param query the {@link ContractCallLocalQuery} to convert
+     * @param exception the {@link Exception} to wrap
+     * @return the implied {@link HederaEvmTransaction}
+     */
+    @NonNull
+    public HederaEvmTransaction fromHapiQueryException(
+            @NonNull final Query query, @NonNull final HandleException exception) {
+        final var op = query.contractCallLocalOrThrow();
+        final var senderId = op.hasSenderId() ? op.senderIdOrThrow() : payerId;
+        // For mono-service fidelity, allow calls using 0.0.X id even to contracts with a priority EVM address
+        final var targetId = asPriorityId(op.contractIDOrThrow(), context.createStore(ReadableAccountStore.class));
+        return new HederaEvmTransaction(
+                senderId,
+                null,
+                targetId,
+                NOT_APPLICABLE,
+                op.functionParameters(),
+                null,
+                0L,
+                op.gas(),
+                1L,
+                0L,
+                null,
+                exception);
+    }
+
     private void assertValidCall(@NonNull final ContractCallLocalQuery body) {
-        validateTrue(body.gas() >= 0, CONTRACT_NEGATIVE_GAS);
+        final var minGasLimit =
+                Math.max(INTRINSIC_GAS_LOWER_BOUND, gasCalculator.transactionIntrinsicGasCost(EMPTY, false));
+        validateTrue(body.gas() >= minGasLimit, INSUFFICIENT_GAS);
         validateTrue(body.gas() <= contractsConfig.maxGasPerSec(), MAX_GAS_LIMIT_EXCEEDED);
     }
 }
