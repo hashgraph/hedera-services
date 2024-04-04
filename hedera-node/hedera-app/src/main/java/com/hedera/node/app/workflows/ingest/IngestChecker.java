9d7043bc5d54c4d2684a0698c07824108b7002c6
Michael Tinker <michael.tinker@swirldslabs.com>
2024-04-04 12:45:04 -0500

    Add javadoc
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/ingest/IngestChecker.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/ingest/IngestChecker.java
@@ -276,7 +276,8 @@ public final class IngestChecker {
             // This adds a mild restriction that privileged transactions can only
             // be issued by system accounts; (FUTURE) consider giving non-trivial
             // minimum fees to privileged transactions that fail with UNAUTHORIZED
-            // at consensus, and adding them to normal throttle buckets
+            // at consensus, and adding them to normal throttle buckets, c.f.
+            // https://github.com/hashgraph/hedera-services/issues/12559
             if (payerNum >= hederaConfig.firstUserEntity()) {
                 throw new PreCheckException(UNAUTHORIZED);
             }
