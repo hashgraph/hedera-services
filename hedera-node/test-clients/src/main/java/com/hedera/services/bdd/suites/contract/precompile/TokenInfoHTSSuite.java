7c65f5d84a43b4e46840c96c271800081dd99d34
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2024-04-05 12:50:27 +0300

    fix: post merge side effects
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/TokenInfoHTSSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/TokenInfoHTSSuite.java
@@ -158,7 +158,6 @@ public class TokenInfoHTSSuite extends HapiSuite {
                 getInfoForFungibleTokenByNFTTokenAddressWorks(),
                 getInfoForNFTByFungibleTokenAddressFails(),
                 getInfoForTokenByAccountAddressFails(),
-                getInfoOnDeletedNonFungibleTokenFails(),
                 getTokenCustomFeesNegativeCases());
     }
 
