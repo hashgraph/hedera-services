575d3a2fc8a48691330dd79add48686b5901128f
Michael Heinrichs <netopyr@users.noreply.github.com>
2024-04-11 11:08:48 +0200

    chore: Improve HAPI test to contain only checked error (#12719)
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>
    (cherry picked from commit b3b59183cf630911ce56b10007af91caf2147aac)

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/token/TokenTransactSpecs.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/token/TokenTransactSpecs.java
@@ -859,13 +859,14 @@ public class TokenTransactSpecs extends HapiSuite {
     @HapiTest
     public HapiSpec duplicateAccountsInTokenTransferRejected() {
         return defaultHapiSpec("DuplicateAccountsInTokenTransferRejected")
-                .given(
-                        cryptoCreate(FIRST_TREASURY).balance(0L),
-                        cryptoCreate(BENEFICIARY).balance(0L))
-                .when(tokenCreate(A_TOKEN))
+                .given(cryptoCreate(FIRST_TREASURY), cryptoCreate(FIRST_USER), cryptoCreate(SECOND_USER))
+                .when(
+                        tokenCreate(A_TOKEN).initialSupply(TOTAL_SUPPLY).treasury(FIRST_TREASURY),
+                        tokenAssociate(FIRST_USER, A_TOKEN),
+                        tokenAssociate(SECOND_USER, A_TOKEN))
                 .then(cryptoTransfer(
-                                moving(1, A_TOKEN).between(FIRST_TREASURY, BENEFICIARY),
-                                moving(1, A_TOKEN).from(FIRST_TREASURY))
+                                moving(1, A_TOKEN).between(FIRST_TREASURY, FIRST_USER),
+                                moving(1, A_TOKEN).between(FIRST_TREASURY, SECOND_USER))
                         .dontFullyAggregateTokenTransfers()
                         .hasPrecheck(ACCOUNT_REPEATED_IN_ACCOUNT_AMOUNTS));
     }
