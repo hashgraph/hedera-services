c7e950d24778c119b2f8c51fa6847ba0af1cdc88
Anthony Petrov <anthony@swirldslabs.com>
2024-04-12 11:56:01 -0700

    spotless
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- build-logic/project-plugins/src/main/kotlin/com.hedera.hashgraph.java.gradle.kts
+++ build-logic/project-plugins/src/main/kotlin/com.hedera.hashgraph.java.gradle.kts
@@ -41,11 +41,18 @@ version =
 // Fail the build if Gradle is started with an old version of Java
 val MIN_JAVA_VERSION = JavaVersion.VERSION_21
 val CUR_JAVA_VERSION = JavaVersion.current()
+
 if (CUR_JAVA_VERSION.ordinal < MIN_JAVA_VERSION.ordinal) {
-    throw StopExecutionException("ERROR: Gradle is started with Java " + CUR_JAVA_VERSION
-            + ". This project requires running Gradle with Java " + MIN_JAVA_VERSION + " or above."
-            + " Please check your JAVA_HOME and/or PATH and configure the default JDK to use Java version "
-            + MIN_JAVA_VERSION + " or above.");
+    throw StopExecutionException(
+        "ERROR: Gradle is started with Java " +
+            CUR_JAVA_VERSION +
+            ". This project requires running Gradle with Java " +
+            MIN_JAVA_VERSION +
+            " or above." +
+            " Please check your JAVA_HOME and/or PATH and configure the default JDK to use Java version " +
+            MIN_JAVA_VERSION +
+            " or above."
+    )
 }
 
 java {
