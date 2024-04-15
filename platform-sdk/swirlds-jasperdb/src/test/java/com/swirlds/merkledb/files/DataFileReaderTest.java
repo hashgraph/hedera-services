d0d219a2ae044f56ec2ec3668d4070b8a3c0a452
artemananiev <33361937+artemananiev@users.noreply.github.com>
2024-04-15 09:44:35 -0700

    feat: 8344: Drop JDB format support in MerkleDb (#12582)
    Fixes: https://github.com/hashgraph/hedera-services/issues/8344
    Reviewed-by: Anthony Petrov <anthony@swirldslabs.com>, Cody Littley <cody@swirldslabs.com>, Ivan Malygin <ivan@swirldslabs.com>, Michael Tinker <michael.tinker@swirldslabs.com>
    Signed-off-by: Artem Ananev <artem.ananev@swirldslabs.com>

--- platform-sdk/swirlds-jasperdb/src/test/java/com/swirlds/merkledb/files/DataFileReaderTest.java
+++ platform-sdk/swirlds-jasperdb/src/test/java/com/swirlds/merkledb/files/DataFileReaderTest.java
@@ -29,7 +29,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-class DataFileReaderPbjTest {
+class DataFileReaderTest {
 
     private final MerkleDbConfig dbConfig = ConfigurationHolder.getConfigData(MerkleDbConfig.class);
 
@@ -37,14 +37,14 @@ class DataFileReaderPbjTest {
     private DataFileMetadata dataFileMetadata;
 
     private File file;
-    private DataFileReaderPbj dataFileReaderPbj;
+    private DataFileReader dataFileReader;
 
     @BeforeEach
     void setUp() throws IOException {
         openMocks(this);
         file = File.createTempFile("file-reader", "test");
-        dataFileReaderPbj =
-                new DataFileReaderPbj(dbConfig, file.toPath(), new ExampleFixedSizeDataSerializer(), dataFileMetadata);
+        dataFileReader =
+                new DataFileReader(dbConfig, file.toPath(), new ExampleFixedSizeDataSerializer(), dataFileMetadata);
     }
 
     /**
@@ -55,85 +55,81 @@ class DataFileReaderPbjTest {
      */
     @Test
     void testLeaseFileChannel() throws IOException {
-        for (int i = 0; i < dataFileReaderPbj.getThreadsPerFileChannel(); i++) {
-            int lease = dataFileReaderPbj.leaseFileChannel();
+        for (int i = 0; i < dataFileReader.getThreadsPerFileChannel(); i++) {
+            int lease = dataFileReader.leaseFileChannel();
             assertEquals(0, lease);
         }
 
         // opening new channel
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
 
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
         for (int i = 0; i < 5; i++) {
-            dataFileReaderPbj.leaseFileChannel();
+            dataFileReader.leaseFileChannel();
         }
 
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(2, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(2, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(2, dataFileReader.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(2, dataFileReader.leaseFileChannel());
     }
 
     @Test
     void testLeaseReleaseFileChannel() throws IOException {
-        for (int i = 0; i < dataFileReaderPbj.getThreadsPerFileChannel(); i++) {
-            int lease = dataFileReaderPbj.leaseFileChannel();
+        for (int i = 0; i < dataFileReader.getThreadsPerFileChannel(); i++) {
+            int lease = dataFileReader.leaseFileChannel();
             assertEquals(0, lease);
         }
 
         // opening new channel
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
 
-        dataFileReaderPbj.releaseFileChannel();
-        dataFileReaderPbj.releaseFileChannel();
-        dataFileReaderPbj.releaseFileChannel();
+        dataFileReader.releaseFileChannel();
+        dataFileReader.releaseFileChannel();
+        dataFileReader.releaseFileChannel();
 
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
     }
 
     @Test
     void testLeaseFileChannel_maxFileChannels() throws IOException {
-        for (int i = 0;
-                i < dataFileReaderPbj.getThreadsPerFileChannel() * dataFileReaderPbj.getMaxFileChannels();
-                i++) {
-            dataFileReaderPbj.leaseFileChannel();
+        for (int i = 0; i < dataFileReader.getThreadsPerFileChannel() * dataFileReader.getMaxFileChannels(); i++) {
+            dataFileReader.leaseFileChannel();
         }
 
         // verifying that all channels were created
         assertEquals(
-                dataFileReaderPbj.getMaxFileChannels(),
-                dataFileReaderPbj.fileChannelsCount.get(),
+                dataFileReader.getMaxFileChannels(),
+                dataFileReader.getFileChannelsCount(),
                 "File channel count is unexpected");
 
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(2, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(2, dataFileReader.leaseFileChannel());
 
         // verifying that no additional channels were created
         assertEquals(
-                dataFileReaderPbj.getMaxFileChannels(),
-                dataFileReaderPbj.fileChannelsCount.get(),
+                dataFileReader.getMaxFileChannels(),
+                dataFileReader.getFileChannelsCount(),
                 "File channel count is unexpected");
     }
 
     @Test
     void testLeaseFileChannel_leaseLeastUsed() throws IOException {
-        for (int i = 0;
-                i < dataFileReaderPbj.getThreadsPerFileChannel() * dataFileReaderPbj.getMaxFileChannels();
-                i++) {
-            dataFileReaderPbj.leaseFileChannel();
+        for (int i = 0; i < dataFileReader.getThreadsPerFileChannel() * dataFileReader.getMaxFileChannels(); i++) {
+            dataFileReader.leaseFileChannel();
         }
 
-        dataFileReaderPbj.releaseFileChannel();
+        dataFileReader.releaseFileChannel();
 
-        assertEquals(0, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(1, dataFileReaderPbj.leaseFileChannel());
-        assertEquals(2, dataFileReaderPbj.leaseFileChannel());
+        assertEquals(0, dataFileReader.leaseFileChannel());
+        assertEquals(1, dataFileReader.leaseFileChannel());
+        assertEquals(2, dataFileReader.leaseFileChannel());
     }
 
     @AfterEach
