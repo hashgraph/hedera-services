3bf865f1a70e6301213b50bb276a8565f0a75fe1
Cody Littley <56973212+cody-littley@users.noreply.github.com>
2024-04-19 14:05:55 -0500

    feat: wait for round durability using wiring framework (#12854)
    Signed-off-by: Cody Littley <cody@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/PlatformSchedulersConfig.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/wiring/PlatformSchedulersConfig.java
@@ -55,9 +55,6 @@ import java.time.Duration;
  *                                                          writer scheduler
  * @param pcesSequencer                                     configuration for the preconsensus event sequencer
  *                                                          scheduler
- * @param eventDurabilityNexusSchedulerType                 the durability nexus scheduler type
- * @param eventDurabilityNexusUnhandledTaskCapacity         number of unhandled tasks allowed in the durability nexus
- *                                                          scheduler
  * @param applicationTransactionPrehandlerSchedulerType     the application transaction prehandler scheduler type
  * @param applicationTransactionPrehandlerUnhandledCapacity number of unhandled tasks allowed for the application
  *                                                          transaction prehandler
@@ -84,6 +81,7 @@ import java.time.Duration;
  *                                                          garbage collector
  * @param platformPublisher                                 configuration for the platform publisher scheduler
  * @param consensusEventStream                              configuration for the consensus event stream scheduler
+ * @param roundDurabilityBuffer                             configuration for the round durability buffer scheduler
  */
 @ConfigData("platformSchedulers")
 public record PlatformSchedulersConfig(
@@ -114,8 +112,6 @@ public record PlatformSchedulersConfig(
         @ConfigProperty(defaultValue = "SEQUENTIAL_THREAD") TaskSchedulerType pcesWriterSchedulerType,
         @ConfigProperty(defaultValue = "500") int pcesWriterUnhandledCapacity,
         @ConfigProperty(defaultValue = "DIRECT") TaskSchedulerConfiguration pcesSequencer,
-        @ConfigProperty(defaultValue = "DIRECT") TaskSchedulerType eventDurabilityNexusSchedulerType,
-        @ConfigProperty(defaultValue = "-1") int eventDurabilityNexusUnhandledTaskCapacity,
         @ConfigProperty(defaultValue = "CONCURRENT") TaskSchedulerType applicationTransactionPrehandlerSchedulerType,
         @ConfigProperty(defaultValue = "500") int applicationTransactionPrehandlerUnhandledCapacity,
         @ConfigProperty(defaultValue = "SEQUENTIAL") TaskSchedulerType stateSignatureCollectorSchedulerType,
@@ -138,4 +134,6 @@ public record PlatformSchedulersConfig(
         @ConfigProperty(defaultValue = "200ms") Duration stateGarbageCollectorHeartbeatPeriod,
         @ConfigProperty(defaultValue = "SEQUENTIAL CAPACITY(500) UNHANDLED_TASK_METRIC")
                 TaskSchedulerConfiguration platformPublisher,
-        @ConfigProperty(defaultValue = "DIRECT_THREADSAFE") TaskSchedulerConfiguration consensusEventStream) {}
+        @ConfigProperty(defaultValue = "DIRECT_THREADSAFE") TaskSchedulerConfiguration consensusEventStream,
+        @ConfigProperty(defaultValue = "SEQUENTIAL CAPACITY(5) FLUSHABLE UNHANDLED_TASK_METRIC")
+                TaskSchedulerConfiguration roundDurabilityBuffer) {}
