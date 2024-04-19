909367ea5a53c9804e5fd1826653f581d51b8212
Ivan Malygin <ivan@swirldslabs.com>
2024-04-19 14:36:51 -0400

    chore: 12862 Added null checks to `Hash.serialize` and `Hash.deserialize` (#12879)
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-common/src/main/java/com/swirlds/common/crypto/Hash.java
+++ platform-sdk/swirlds-common/src/main/java/com/swirlds/common/crypto/Hash.java
@@ -18,6 +18,7 @@ package com.swirlds.common.crypto;
 
 import static com.swirlds.common.utility.CommonUtils.hex;
 import static com.swirlds.common.utility.Mnemonics.generateMnemonic;
+import static java.util.Objects.requireNonNull;
 
 import com.swirlds.common.io.SerializableWithKnownLength;
 import com.swirlds.common.io.exceptions.BadIOException;
@@ -183,6 +184,8 @@ public class Hash implements Comparable<Hash>, SerializableWithKnownLength, Seri
      */
     @Override
     public void serialize(final SerializableDataOutputStream out) throws IOException {
+        requireNonNull(digestType, "digestType");
+        requireNonNull(value, "value");
         out.writeInt(digestType.id());
         out.writeByteArray(value);
     }
@@ -205,6 +208,10 @@ public class Hash implements Comparable<Hash>, SerializableWithKnownLength, Seri
 
         this.digestType = digestType;
         this.value = in.readByteArray(digestType.digestLength());
+
+        if (value == null) {
+            throw new BadIOException("Invalid hash value read from the stream");
+        }
     }
 
     /**
