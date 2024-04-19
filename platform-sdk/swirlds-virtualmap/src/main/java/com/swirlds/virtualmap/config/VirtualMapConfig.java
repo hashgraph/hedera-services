24f2a3769a76f1cf4a0eef000b880b7c0c6568df
Artem Ananev <artem.ananev@swirldslabs.com>
2024-04-18 19:39:09 -0700

    Changed default virtual map reconnect mode to pull/twoPhasePessimistic for testing purposes
    Signed-off-by: Artem Ananev <artem.ananev@swirldslabs.com>

--- platform-sdk/swirlds-virtualmap/src/main/java/com/swirlds/virtualmap/config/VirtualMapConfig.java
+++ platform-sdk/swirlds-virtualmap/src/main/java/com/swirlds/virtualmap/config/VirtualMapConfig.java
@@ -16,6 +16,7 @@
 
 package com.swirlds.virtualmap.config;
 
+import static com.swirlds.virtualmap.config.VirtualMapReconnectMode.PULL_TWO_PHASE_PESSIMISTIC;
 import static com.swirlds.virtualmap.config.VirtualMapReconnectMode.PUSH;
 
 import com.swirlds.config.api.ConfigData;
@@ -94,7 +95,7 @@ public record VirtualMapConfig(
                 double percentHashThreads, // FUTURE WORK: We need to add min/max support for double values
         @Min(-1) @ConfigProperty(defaultValue = "-1") int numHashThreads,
         @Min(1) @Max(64) @ConfigProperty(defaultValue = "3") int virtualHasherChunkHeight,
-        @ConfigProperty(defaultValue = PUSH) String reconnectMode,
+        @ConfigProperty(defaultValue = PULL_TWO_PHASE_PESSIMISTIC) String reconnectMode,
         @Min(0) @ConfigProperty(defaultValue = "500000") int reconnectFlushInterval,
         @Min(0) @Max(100) @ConfigProperty(defaultValue = "25.0")
                 double percentCleanerThreads, // FUTURE WORK: We need to add min/max support for double values
