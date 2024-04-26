70da0c8c5f6ae708040d2dc20a48de62f523d5c3
Oleg Mazurov <23125269+OlegMazurov@users.noreply.github.com>
2024-04-26 09:27:27 -0700

    perf: warm up more entities (#13005)
    Signed-off-by: Oleg Mazurov <oleg.mazurov@swirldslabs.com>
    (cherry picked from commit d86d02030721b91aa9b462dfa3a097ef452855b3)

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoTransferHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/CryptoTransferHandler.java
@@ -173,13 +173,14 @@ public class CryptoTransferHandler implements TransactionHandler {
                     if (treasuryID != null) accountStore.warm(treasuryID);
                     final List<NftTransfer> nftTransfers = tokenTransferList.nftTransfersOrThrow();
                     for (final NftTransfer nftTransfer : nftTransfers) {
-                        warmNftTransfer(accountStore, nftStore, tokenRelationStore, tokenID, nftTransfer);
+                        warmNftTransfer(accountStore, tokenStore, nftStore, tokenRelationStore, tokenID, nftTransfer);
                     }
                 });
     }
 
     private void warmNftTransfer(
             @NonNull final ReadableAccountStore accountStore,
+            @NonNull final ReadableTokenStore tokenStore,
             @NonNull final ReadableNftStore nftStore,
             @NonNull final ReadableTokenRelationStore tokenRelationStore,
             @NonNull final TokenID tokenID,
@@ -197,7 +198,10 @@ public class CryptoTransferHandler implements TransactionHandler {
         nftTransfer.ifReceiverAccountID(receiverAccountID -> {
             final Account receiver = accountStore.getAliasedAccountById(receiverAccountID);
             if (receiver != null) {
-                receiver.ifHeadTokenId(headTokenID -> tokenRelationStore.warm(receiverAccountID, headTokenID));
+                receiver.ifHeadTokenId(headTokenID -> {
+                    tokenRelationStore.warm(receiverAccountID, headTokenID);
+                    tokenStore.warm(headTokenID);
+                });
                 receiver.ifHeadNftId(nftStore::warm);
             }
             tokenRelationStore.warm(receiverAccountID, tokenID);
