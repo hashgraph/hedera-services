0f232f1b80f72f0eb2b341a7876247581f497530
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-05-06 12:20:59 -0500

    fix comments
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/fees/Fees.java
+++ hedera-node/hedera-app-spi/src/main/java/com/hedera/node/app/spi/fees/Fees.java
@@ -40,7 +40,7 @@ public record Fees(long nodeFee, long networkFee, long serviceFee) {
     /** A constant representing zero fees. */
     public static final Fees FREE = new Fees(0, 0, 0);
     /**
-     * A constant representing fees of 1 tinybar for each of the node, network, and service components.
+     * A constant representing fees of 1 constant resource usage for each of the node, network, and service components.
      * This is useful when a fee is required, but the entity is not present in state to determine the actual fee.
      */
     public static final FeeData CONSTANT_FEE_DATA = FeeData.newBuilder()
