2a3ab010c3d72a925b3af4826338acca5be880b1
Valentin Valkanov <valentin.valkanov@limechain.tech>
2024-05-13 12:45:40 +0300

    revert changes that are not for this branch
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenRejectHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenRejectHandler.java
@@ -16,16 +16,9 @@
 
 package com.hedera.node.app.service.token.impl.handlers;
 
-import static com.hedera.hapi.node.base.SubType.TOKEN_FUNGIBLE_COMMON;
-import static com.hedera.hapi.node.base.SubType.TOKEN_NON_FUNGIBLE_UNIQUE;
-import static com.hedera.node.app.hapi.fees.usage.SingletonUsageProperties.USAGE_PROPERTIES;
-import static com.hedera.node.app.hapi.fees.usage.crypto.CryptoOpsUsage.LONG_ACCOUNT_AMOUNT_BYTES;
-import static com.hedera.node.app.hapi.fees.usage.token.TokenOpsUsage.LONG_BASIC_ENTITY_ID_SIZE;
-import static com.hedera.node.app.hapi.fees.usage.token.entities.TokenEntitySizes.TOKEN_ENTITY_SIZES;
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.node.base.HederaFunctionality;
-import com.hedera.hapi.node.base.SubType;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.spi.fees.FeeContext;
 import com.hedera.node.app.spi.fees.Fees;
@@ -34,7 +27,6 @@ import com.hedera.node.app.spi.workflows.HandleException;
 import com.hedera.node.app.spi.workflows.PreCheckException;
 import com.hedera.node.app.spi.workflows.PreHandleContext;
 import com.hedera.node.app.spi.workflows.TransactionHandler;
-import com.hedera.node.config.data.FeesConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -70,51 +62,7 @@ public class TokenRejectHandler extends BaseTokenHandler implements TransactionH
     @NonNull
     @Override
     public Fees calculateFees(final FeeContext feeContext) {
-        final var body = feeContext.body();
-        final var op = body.tokenRejectOrThrow();
-        final var config = feeContext.configuration();
-        final var tokenMultiplier =
-                requireNonNull(config).getConfigData(FeesConfig.class).tokenTransferUsageMultiplier();
-
-        final int totalRejections = op.rejections().size();
-        int numOfFungibleTokenRejections = 0;
-        int numOfNFTRejections = 0;
-        for (final var rejection : op.rejections()) {
-            if (rejection.hasFungibleToken()) {
-                numOfFungibleTokenRejections++;
-            } else {
-                numOfNFTRejections++;
-            }
-        }
-
-        final int weightedTokensInvolved = tokenMultiplier * totalRejections;
-        final int weightedFungibleTokens = tokenMultiplier * numOfFungibleTokenRejections;
-
-        final long bpt =
-                calculateBytesPerTransaction(weightedTokensInvolved, weightedFungibleTokens, numOfNFTRejections);
-        final long rbs = calculateRamByteSeconds(weightedTokensInvolved, weightedFungibleTokens, numOfNFTRejections);
-
-        return feeContext
-                .feeCalculator(getSubType(numOfNFTRejections))
-                .addBytesPerTransaction(bpt)
-                .addRamByteSeconds(rbs * USAGE_PROPERTIES.legacyReceiptStorageSecs())
-                .calculate();
-    }
-
-    private SubType getSubType(final int numOfNFTRejections) {
-        return numOfNFTRejections != 0 ? TOKEN_NON_FUNGIBLE_UNIQUE : TOKEN_FUNGIBLE_COMMON;
-    }
-
-    private long calculateBytesPerTransaction(
-            final int weightedTokens, final int weightedFungibleTokens, final int numNFTRejections) {
-        return weightedTokens * LONG_BASIC_ENTITY_ID_SIZE
-                + weightedFungibleTokens * LONG_ACCOUNT_AMOUNT_BYTES
-                + TOKEN_ENTITY_SIZES.bytesUsedForUniqueTokenTransfers(numNFTRejections);
-    }
-
-    private long calculateRamByteSeconds(
-            final int weightedTokensInvolved, final int weightedFungibleTokens, final int numOfNFTRejections) {
-        return USAGE_PROPERTIES.legacyReceiptStorageSecs() * TOKEN_ENTITY_SIZES.bytesUsedToRecordTokenTransfers(
-                weightedTokensInvolved, weightedFungibleTokens, numOfNFTRejections);
+        // Todo: Implement the logic for calculating fees
+        return Fees.FREE;
     }
 }
