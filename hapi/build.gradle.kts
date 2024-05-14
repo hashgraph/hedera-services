dafeeb9ce8352f6f7910b359166280254548477d
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-05-14 09:12:11 -0500

    revert branch changes
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hapi/build.gradle.kts
+++ hapi/build.gradle.kts
@@ -28,7 +28,7 @@ tasks.cloneHederaProtobufs {
     // uncomment below to use a specific tag
     //    tag = "v0.50.0-release"
     // uncomment below to use a specific branch
-    branch = "hip-904-max-auto-assciations-and-token-reject"
+    branch = "main"
 }
 
 sourceSets {
