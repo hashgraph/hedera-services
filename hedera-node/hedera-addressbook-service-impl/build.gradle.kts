889df3864a31c12c04edf5ab444053be3c788c09
Iris Simon <iris.simon@swirldslabs.com>
2024-05-24 09:48:32 -0400

    updated build files
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hedera-node/hedera-addressbook-service-impl/build.gradle.kts
+++ hedera-node/hedera-addressbook-service-impl/build.gradle.kts
@@ -24,7 +24,6 @@ description = "Default Hedera AddressBook Service Implementation"
 mainModuleInfo { annotationProcessor("dagger.compiler") }
 
 testModuleInfo {
-    requires("com.hedera.node.app")
     requires("com.hedera.node.app.service.addressbook.impl")
     requires("com.hedera.node.app.service.mono")
     requires("com.hedera.node.app.service.mono.test.fixtures")
