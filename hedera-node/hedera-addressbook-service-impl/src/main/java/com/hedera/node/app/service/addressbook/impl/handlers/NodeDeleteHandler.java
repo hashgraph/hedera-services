2e784a6eb43b04ae0278a9f49a6e7a81f957d93b
Lev Povolotsky <lev@swirldslabs.com>
2024-05-29 18:22:44 -0400

    feat: initial delete handler
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/handlers/NodeDeleteHandler.java
+++ hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/handlers/NodeDeleteHandler.java
@@ -62,7 +62,7 @@ public class NodeDeleteHandler implements TransactionHandler {
     }
 
     /**
-     * Given the appropriate context, deletes a node.
+     * Given the appropriate context, deletes a topic.
      *
      * @param context the {@link HandleContext} of the active transaction
      * @throws NullPointerException if one of the arguments is {@code null}
