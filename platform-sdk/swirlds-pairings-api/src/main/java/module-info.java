f0eff13c73634432feacde89ef9efde5aa77e954
Austin Littley <austin@swirldslabs.com>
2024-05-31 10:30:19 -0400

    Remove unneeded classes
    Signed-off-by: Austin Littley <austin@swirldslabs.com>

--- platform-sdk/swirlds-pairings-api/src/main/java/module-info.java
+++ platform-sdk/swirlds-pairings-api/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.swirlds.pairings.api {
     uses com.swirlds.pairings.spi.BilinearPairingProvider;
 
     exports com.swirlds.pairings.api;
-    exports com.swirlds.pairings.ecdh;
     exports com.swirlds.pairings.spi;
     exports com.swirlds.signaturescheme.api;
 
