81cca64f546fb3ba975850062ea2bfeef1efb29f
Neeharika Sompalli <52669918+Neeharika-Sompalli@users.noreply.github.com>
2024-06-03 10:03:30 -0500

    chore: Add `FakePlatform` and `FakeServicesRegistry` (#13549)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Signed-off-by: lukelee-sl <luke.lee@swirldslabs.com>
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
    Co-authored-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: lukelee-sl <luke.lee@swirldslabs.com>

--- hedera-node/hedera-app/src/testFixtures/java/module-info.java
+++ hedera-node/hedera-app/src/testFixtures/java/module-info.java
@@ -4,7 +4,9 @@ module com.hedera.node.app.test.fixtures {
     requires transitive com.hedera.node.app.spi.test.fixtures;
     requires transitive com.hedera.node.app.spi;
     requires transitive com.hedera.node.app;
+    requires transitive com.swirlds.common;
     requires transitive com.swirlds.platform.core.test.fixtures;
+    requires transitive com.swirlds.platform.core;
     requires transitive com.swirlds.state.api;
     requires com.hedera.node.app.service.mono;
     requires com.hedera.node.app.service.token;
@@ -12,11 +14,11 @@ module com.hedera.node.app.test.fixtures {
     requires com.hedera.node.config;
     requires com.hedera.node.hapi;
     requires com.hedera.pbj.runtime;
-    requires com.swirlds.common;
+    requires com.swirlds.base;
     requires com.swirlds.config.api;
     requires com.swirlds.config.extensions.test.fixtures;
     requires com.swirlds.metrics.api;
-    requires com.swirlds.platform.core;
+    requires org.apache.logging.log4j;
     requires org.assertj.core;
     requires static com.github.spotbugs.annotations;
 }
