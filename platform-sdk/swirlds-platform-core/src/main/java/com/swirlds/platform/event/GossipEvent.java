412ead1787994368d9826c7548c705eb818d8671
Lazar Petrovic <lpetrovic05@gmail.com>
2024-06-03 12:21:13 +0200

    fix: gui setting consensus data twice
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/GossipEvent.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/GossipEvent.java
@@ -120,6 +120,16 @@ public class GossipEvent implements Event, SelfSerializable {
     }
 
     /**
+     * Create a copy of this event while populating only the data received via gossip. Consensus data will not be
+     * copied.
+     *
+     * @return a copy of this event
+     */
+    public GossipEvent copyGossipedData() {
+        return new GossipEvent(hashedData, signature);
+    }
+
+    /**
      * Set the sequence number in the preconsensus event stream for this event.
      *
      * @param streamSequenceNumber the sequence number
