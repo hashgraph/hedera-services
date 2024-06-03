061742d8c4c72cb47aff7fbe493a510d9af49c6e
Lev Povolotsky <lev@swirldslabs.com>
2024-06-03 16:28:23 -0400

    feat: impl Node Get Info intial implementation include unit test
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/handlers/NodeGetInfoHandler.java
+++ hedera-node/hedera-addressbook-service-impl/src/main/java/com/hedera/node/app/service/addressbook/impl/handlers/NodeGetInfoHandler.java
@@ -16,22 +16,39 @@
 
 package com.hedera.node.app.service.addressbook.impl.handlers;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_NODE_ID;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TOPIC_ID;
+import static com.hedera.hapi.node.base.ResponseCodeEnum.OK;
+import static com.hedera.hapi.node.base.ResponseType.COST_ANSWER;
+import static com.hedera.node.app.service.mono.pbj.PbjConverter.fromPbj;
+import static com.hedera.node.app.spi.key.KeyUtils.isEmpty;
+import static com.hedera.node.app.spi.validation.Validations.mustExist;
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.hapi.node.addressbook.NodeGetInfoQuery;
 import com.hedera.hapi.node.addressbook.NodeGetInfoResponse;
+import com.hedera.hapi.node.addressbook.NodeInfo;
+import com.hedera.hapi.node.base.Duration;
 import com.hedera.hapi.node.base.HederaFunctionality;
 import com.hedera.hapi.node.base.QueryHeader;
 import com.hedera.hapi.node.base.ResponseHeader;
+import com.hedera.hapi.node.base.SubType;
+import com.hedera.hapi.node.base.Timestamp;
+import com.hedera.hapi.node.base.TopicID;
+import com.hedera.hapi.node.consensus.ConsensusGetTopicInfoResponse;
+import com.hedera.hapi.node.consensus.ConsensusTopicInfo;
 import com.hedera.hapi.node.transaction.Query;
 import com.hedera.hapi.node.transaction.Response;
 import com.hedera.node.app.service.addressbook.ReadableNodeStore;
+import com.hedera.node.app.service.mono.fees.calculation.file.queries.GetFileInfoResourceUsage;
 import com.hedera.node.app.spi.fees.Fees;
 import com.hedera.node.app.spi.workflows.PaidQueryHandler;
 import com.hedera.node.app.spi.workflows.PreCheckException;
 import com.hedera.node.app.spi.workflows.QueryContext;
 import com.hedera.node.config.data.LedgerConfig;
+import com.hedera.node.config.data.NodesConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -63,9 +80,17 @@ public class NodeGetInfoHandler extends PaidQueryHandler {
     public void validate(@NonNull final QueryContext context) throws PreCheckException {
         requireNonNull(context);
         final var query = context.query();
-        final var store = context.createStore(ReadableNodeStore.class);
+        final var nodeStore = context.createStore(ReadableNodeStore.class);
+        final var config = context.configuration().getConfigData(NodesConfig.class);
         final NodeGetInfoQuery op = query.nodeGetInfoOrThrow();
-        throw new UnsupportedOperationException("need implementation");
+        final long nodeId = op.nodeId();
+        if (nodeId > 0 && nodeId <= config.maxNumber()) {
+            // The node must exist
+            final var node = nodeStore.get(nodeId);
+            mustExist(node, INVALID_NODE_ID);
+        } else {
+            throw new PreCheckException(INVALID_NODE_ID);
+        }
     }
 
     @Override
@@ -74,7 +99,50 @@ public class NodeGetInfoHandler extends PaidQueryHandler {
         requireNonNull(header);
         final var query = context.query();
         final var config = context.configuration().getConfigData(LedgerConfig.class);
-        throw new UnsupportedOperationException("need implementation");
+        final var nodeStore = context.createStore(ReadableNodeStore.class);
+        final var op = query.nodeGetInfoOrThrow();
+        final var response = NodeGetInfoResponse.newBuilder();
+        final var node = NodeInfo.newBuilder().nodeId(op.nodeId()).build();
+        response.nodeInfo(node);
+
+        final var responseType = op.headerOrElse(QueryHeader.DEFAULT).responseType();
+        response.header(header);
+        if (header.nodeTransactionPrecheckCode() == OK && responseType != COST_ANSWER) {
+            final var optionalInfo = infoForNode(op.nodeId(), nodeStore, config);
+            optionalInfo.ifPresent(response::nodeInfo);
+        }
+
+        return Response.newBuilder().nodeGetInfo(response).build();
+    }
+
+    /**
+     * Provides information about a node.
+     * @param nodeId the node to get information about
+     * @param nodeStore the node store
+     * @param config the LedgerConfig
+     * @return the information about the node
+     */
+    private Optional<NodeInfo> infoForNode(
+            @NonNull final long nodeId,
+            @NonNull final ReadableNodeStore nodeStore,
+            @NonNull final LedgerConfig config) {
+        final var meta = nodeStore.get(nodeId);
+        if (meta == null) {
+            return Optional.empty();
+        } else {
+            final var info = NodeInfo.newBuilder();
+            info.nodeId(meta.nodeId());
+            if (meta.accountId() != null) info.accountId(meta.accountId());
+            info.description(meta.description());
+            info.gossipEndpoint(meta.gossipEndpoint());
+            info.serviceEndpoint(meta.serviceEndpoint());
+            info.gossipCaCertificate(meta.gossipCaCertificate());
+            info.grpcCertificateHash(meta.grpcCertificateHash());
+            info.weight(meta.weight());
+            info.deleted(meta.deleted());
+            info.ledgerId(config.id());
+            return Optional.of(info.build());
+        }
     }
 
     @NonNull
@@ -83,6 +151,6 @@ public class NodeGetInfoHandler extends PaidQueryHandler {
         final var query = queryContext.query();
         final var nodeStore = queryContext.createStore(ReadableNodeStore.class);
         final var op = query.nodeGetInfoOrThrow();
-        throw new UnsupportedOperationException("need implementation");
+        return queryContext.feeCalculator().calculate();
     }
 }
