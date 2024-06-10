a2c1aae727b8ab2326d3004ce4d7e680ee5a8263
Iris Simon <iris.simon@swirldslabs.com>
2024-06-10 17:00:37 -0400

    Added requires com.swirlds.platform.core;
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hedera-node/hedera-addressbook-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-addressbook-service-impl/src/main/java/module-info.java
@@ -11,6 +11,7 @@ module com.hedera.node.app.service.addressbook.impl {
     requires com.hedera.node.app.service.token;
     requires com.hedera.node.config;
     requires com.hedera.pbj.runtime;
+    requires com.swirlds.platform.core;
     requires org.apache.logging.log4j;
     requires static com.github.spotbugs.annotations;
     requires static java.compiler;
