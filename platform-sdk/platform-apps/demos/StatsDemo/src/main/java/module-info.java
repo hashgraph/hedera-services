289ad40647b6dc6ddd0a2bb8a65a1359a7925637
Artem Ananev <artem.ananev@swirldslabs.com>
2024-06-11 16:46:45 -0700

    Fixed spotless checks
    Signed-off-by: Artem Ananev <artem.ananev@swirldslabs.com>

--- platform-sdk/platform-apps/demos/StatsDemo/src/main/java/module-info.java
+++ platform-sdk/platform-apps/demos/StatsDemo/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.swirlds.demo.stats {
-    requires com.github.spotbugs.annotations;
     requires com.swirlds.base;
     requires com.swirlds.common;
     requires com.swirlds.metrics.api;
     requires com.swirlds.platform.core;
+    requires com.github.spotbugs.annotations;
     requires java.desktop;
 }
