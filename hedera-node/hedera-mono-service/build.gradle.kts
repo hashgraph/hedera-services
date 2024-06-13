a7f05fb08ad35e74fb42d28c0f67a1f3fc2c881e
Michael Tinker <michael.tinker@swirldslabs.com>
2024-06-12 23:18:55 -0500

    fix checkModuleInfo
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-mono-service/build.gradle.kts
+++ hedera-node/hedera-mono-service/build.gradle.kts
@@ -31,51 +31,6 @@ tasks.withType<JavaCompile>().configureEach {
 
 mainModuleInfo { annotationProcessor("dagger.compiler") }
 
-testModuleInfo {
-    annotationProcessor("dagger.compiler")
-    requires("com.hedera.node.app.service.mono")
-    requires("awaitility")
-    requires("com.github.benmanes.caffeine")
-    requires("io.github.classgraph")
-    requires("net.i2p.crypto.eddsa")
-    requires("org.apache.logging.log4j.core")
-    requires("org.apache.logging.log4j.core")
-    requires("org.assertj.core")
-    requires("org.hamcrest")
-    requires("org.junit.jupiter.api")
-    requires("org.junit.jupiter.params")
-    requires("org.junitpioneer")
-    requires("org.mockito")
-    requires("org.mockito.junit.jupiter")
-    requiresStatic("com.github.spotbugs.annotations")
-}
-
-jmhModuleInfo {
-    annotationProcessor("dagger.compiler")
-    requires("com.hedera.node.app.hapi.utils")
-    requires("com.hedera.node.app.service.mono.test.fixtures")
-    requires("com.hedera.node.app.spi")
-    requires("com.hedera.node.hapi")
-    requires("com.github.spotbugs.annotations")
-    requires("com.google.common")
-    requires("com.google.protobuf")
-    requires("com.swirlds.common")
-    requires("com.swirlds.fcqueue")
-    requires("com.swirlds.merkle")
-    requires("com.swirlds.merkledb")
-    requires("com.swirlds.virtualmap")
-    requires("dagger")
-    requires("javax.inject")
-    requires("jmh.core")
-    requires("org.apache.commons.io")
-    requires("org.apache.commons.lang3")
-    requires("org.hyperledger.besu.datatypes")
-    requires("org.hyperledger.besu.evm")
-    requires("org.mockito")
-    requires("tuweni.bytes")
-    requires("tuweni.units")
-}
-
 val writeSemanticVersionProperties =
     tasks.register<WriteProperties>("writeSemanticVersionProperties") {
         property("hapi.proto.version", libs.versions.hapi.proto.get())
