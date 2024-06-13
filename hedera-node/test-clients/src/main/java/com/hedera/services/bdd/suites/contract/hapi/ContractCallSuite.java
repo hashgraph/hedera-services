9bfc5819822e8dff45f861f64dd906d9c46edea8
David Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2024-06-13 09:03:44 -0700

    fix: Ethereum contract calls where high-bit of amount is set, fail (#13821)
    Ethereum contract calls have an amount - the amount of the native currency to transfer. In Ethereum this amount is unsigned. But as a recent regression we are interpreting this amount as signed (2's complement), leading to failure when the amount's high-order bit is 1.
    
    Bug was introduced in https://github.com/hashgraph/hedera-services/pull/12834.
    
    Fix is to (manually) revert the particular change that treats these amounts as signed.
    
    As a freebie code cleanup also renamed constants `WEIBARS_TO_TINYBAR` to `WEIBARS_IN_A_TINYBAR` to reflect their actual semantic meaning.
    
    Fixes #13816
    
    Signed-off-by: David S Bakin <117694041+david-bakin-sl@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hapi/ContractCallSuite.java
@@ -69,7 +69,6 @@ import static com.hedera.services.bdd.spec.utilops.UtilVerbs.assertionsHold;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.balanceSnapshot;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.childRecordsCheck;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.createLargeFile;
-import static com.hedera.services.bdd.spec.utilops.UtilVerbs.ifHapiTest;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.logIt;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyListNamed;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
@@ -112,7 +111,6 @@ import static com.hedera.services.bdd.suites.leaky.LeakyContractTestsSuite.NESTE
 import static com.hedera.services.bdd.suites.regression.factories.HollowAccountCompletedFuzzingFactory.CONTRACT;
 import static com.hedera.services.bdd.suites.utils.ECDSAKeysUtils.randomHeadlongAddress;
 import static com.hedera.services.bdd.suites.utils.contracts.SimpleBytesResult.bigIntResult;
-import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_NEGATIVE_VALUE;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.CONTRACT_REVERT_EXECUTED;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INSUFFICIENT_GAS;
 import static com.hederahashgraph.api.proto.java.ResponseCodeEnum.INSUFFICIENT_PAYER_BALANCE;
@@ -1569,38 +1567,6 @@ public class ContractCallSuite {
                         // It's also ok to use a default PBJ ContractID (i.e. an id with
                         // UNSET contract oneof) to make a no-op call to address 0x00...00
                         contractCall(DEFAULT_ID_SENTINEL));
-    }
-
-    @HapiTest
-    final Stream<DynamicTest> callFailsWhenAmountIsNegativeButStillChargedFee() {
-        final var payer = "payer";
-        return defaultHapiSpec("callFailsWhenAmountIsNegativeButStillChargedFee")
-                .given(
-                        uploadInitCode(PAY_RECEIVABLE_CONTRACT),
-                        contractCreate(PAY_RECEIVABLE_CONTRACT)
-                                .adminKey(THRESHOLD)
-                                .gas(1_000_000)
-                                .refusingEthConversion(),
-                        cryptoCreate(payer).balance(ONE_MILLION_HBARS).payingWith(GENESIS))
-                .when(ifHapiTest(withOpContext((spec, ignore) -> {
-                    final var subop1 = balanceSnapshot("balanceBefore0", payer);
-                    final var subop2 = contractCall(PAY_RECEIVABLE_CONTRACT)
-                            .via(PAY_TXN)
-                            .payingWith(payer)
-                            .sending(-DEPOSIT_AMOUNT)
-                            .hasKnownStatus(CONTRACT_NEGATIVE_VALUE)
-                            .refusingEthConversion();
-                    final var subop3 = getTxnRecord(PAY_TXN).logged();
-                    allRunFor(spec, subop1, subop2, subop3);
-                    final var delta = subop3.getResponseRecord()
-                            .getTransferList()
-                            .getAccountAmounts(0)
-                            .getAmount();
-                    final var subop4 =
-                            getAccountBalance(payer).hasTinyBars(changeFromSnapshot("balanceBefore0", -delta));
-                    allRunFor(spec, subop4);
-                })))
-                .then();
     }
 
     @HapiTest
