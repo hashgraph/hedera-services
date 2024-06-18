ff271190a460609fcade0a49afcdf357b6bcb30d
Iris Simon <iris.simon@swirldslabs.com>
2024-06-18 14:33:02 -0400

    Changed back to main branch.
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hapi/build.gradle.kts
+++ hapi/build.gradle.kts
@@ -32,8 +32,7 @@ tasks.cloneHederaProtobufs {
     // uncomment below to use a specific tag
     //    tag = "v0.51.0"
     // uncomment below to use a specific branch
-    //    branch = "main"
-    branch = "374-update-response"
+    branch = "main"
 }
 
 sourceSets {
