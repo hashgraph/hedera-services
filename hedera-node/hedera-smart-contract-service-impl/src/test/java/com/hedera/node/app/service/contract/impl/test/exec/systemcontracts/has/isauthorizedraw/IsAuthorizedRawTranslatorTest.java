2ae1e24967fe2a8072a6e2e1c86a7370b8ef27b5
David S Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2024-07-02 09:53:32 -0700

    Fix unit test w/ proper `Configuration` mocking
    Signed-off-by: David S Bakin <117694041+david-bakin-sl@users.noreply.github.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/has/isauthorizedraw/IsAuthorizedRawTranslatorTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/has/isauthorizedraw/IsAuthorizedRawTranslatorTest.java
@@ -25,31 +25,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 
 import com.esaulpaugh.headlong.abi.Tuple;
-import com.hedera.node.app.service.contract.impl.exec.FeatureFlags;
 import com.hedera.node.app.service.contract.impl.exec.gas.SystemContractGasCalculator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.has.HasCallAttempt;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.has.hbarallowance.HbarAllowanceTranslator;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.has.isauthorizedraw.IsAuthorizedRawCall;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.has.isauthorizedraw.IsAuthorizedRawTranslator;
+import com.hedera.node.app.service.contract.impl.exec.v051.Version051FeatureFlags;
 import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
+import com.hedera.node.config.testfixtures.HederaTestConfigBuilder;
+import com.swirlds.config.api.Configuration;
+import edu.umd.cs.findbugs.annotations.NonNull;
 import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mock.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class IsAuthorizedRawTranslatorTest {
 
-    @Mock
+    @Mock(strictness = Strictness.LENIENT) // might not use `configuration()`
     private HasCallAttempt attempt;
 
     @Mock
     private HederaWorldUpdater.Enhancement enhancement;
-
-    @Mock
-    private FeatureFlags featureFlags;
 
     @Mock
     private SystemContractGasCalculator gasCalculator;
@@ -58,18 +59,29 @@ public class IsAuthorizedRawTranslatorTest {
 
     @BeforeEach
     void setUp() {
+        final var featureFlags = new Version051FeatureFlags();
         subject = new IsAuthorizedRawTranslator(featureFlags);
     }
 
     @Test
-    void matchesIsAuthorizedRaw() {
+    void matchesIsAuthorizedRawWhenEnabled() {
+        given(attempt.configuration()).willReturn(getTestConfiguration(true));
         given(attempt.selector()).willReturn(IsAuthorizedRawTranslator.IS_AUTHORIZED_RAW.selector());
         var matches = subject.matches(attempt);
         assertTrue(matches);
     }
 
     @Test
+    void doesNotMatchIsAuthorizedRawWhenDisabled() {
+        given(attempt.configuration()).willReturn(getTestConfiguration(false));
+        given(attempt.selector()).willReturn(IsAuthorizedRawTranslator.IS_AUTHORIZED_RAW.selector());
+        var matches = subject.matches(attempt);
+        assertFalse(matches);
+    }
+
+    @Test
     void failsOnInvalidSelector() {
+        given(attempt.configuration()).willReturn(getTestConfiguration(true));
         given(attempt.selector()).willReturn(HbarAllowanceTranslator.HBAR_ALLOWANCE_PROXY.selector());
         final var matches = subject.matches(attempt);
         assertFalse(matches);
@@ -77,6 +89,7 @@ public class IsAuthorizedRawTranslatorTest {
 
     @Test
     void callFromIsAuthorizedRawTest() {
+        given(attempt.configuration()).willReturn(getTestConfiguration(true));
         final Bytes inputBytes = Bytes.wrapByteBuffer(IsAuthorizedRawTranslator.IS_AUTHORIZED_RAW.encodeCall(
                 Tuple.of(APPROVED_HEADLONG_ADDRESS, messageHash, signature)));
         givenCommonForCall(inputBytes);
@@ -90,5 +103,12 @@ public class IsAuthorizedRawTranslatorTest {
         given(attempt.selector()).willReturn(inputBytes.slice(0, 4).toArray());
         given(attempt.enhancement()).willReturn(enhancement);
         given(attempt.systemContractGasCalculator()).willReturn(gasCalculator);
+    }
+
+    @NonNull
+    Configuration getTestConfiguration(final boolean enableIsAuthorizedRaw) {
+        return HederaTestConfigBuilder.create()
+                .withValue("contracts.systemContract.accountService.isAuthorizedRawEnabled", enableIsAuthorizedRaw)
+                .getOrCreateConfig();
     }
 }
