c4ebc3c8da24334051d70770faffd0f339ff2e4b
Michael Tinker <michael.tinker@swirldslabs.com>
2024-07-02 10:59:38 -0500

    fix checkModuleInfo
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-addressbook-service-impl/build.gradle.kts
+++ hedera-node/hedera-addressbook-service-impl/build.gradle.kts
@@ -33,7 +33,6 @@ testModuleInfo {
     requires("com.hedera.node.app.service.token.impl")
     requires("com.hedera.node.config.test.fixtures")
     requires("com.swirlds.config.extensions.test.fixtures")
-    requires("com.swirlds.common")
     requires("com.swirlds.platform.core.test.fixtures")
     requires("com.hedera.node.app.spi.test.fixtures")
     requires("org.assertj.core")
