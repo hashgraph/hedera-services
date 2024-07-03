63641ffdab4ec13759b901a58682f95b64bd4651
David Bakin <117694041+david-bakin-sl@users.noreply.github.com>
2024-07-03 11:33:49 -0700

    feat: implement HIP-632 `isAuthorizedRaw` method (#14130)
    **Description**:
    
    Implement the Hedera Account Service `isAuthorizedRaw()` method
    
    **Related issue(s)**:
    
    Related to #13560 (one task of epic)
    
    **Related PR(s)**:
    
    Replaces #14108 which (for some unknown reasons) had problems rebasing against `develop`
    
    **Notes for reviewer**:
    
    **Checklist**
    
    - [x] Documented (Code comments, README, etc.)
    - [x] Tested (unit, integration, etc.)
    
    Signed-off-by: David S Bakin <117694041+david-bakin-sl@users.noreply.github.com>

--- hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/ethereum/EthTxSigs.java
+++ hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/ethereum/EthTxSigs.java
@@ -99,7 +99,25 @@ public record EthTxSigs(byte[] publicKey, byte[] address) {
                 s);
     }
 
-    static byte[] calculateSignableMessage(EthTxData ethTx) {
+    public static byte[] signMessage(final byte[] messageHash, byte[] privateKey) {
+        final LibSecp256k1.secp256k1_ecdsa_recoverable_signature signature =
+                new LibSecp256k1.secp256k1_ecdsa_recoverable_signature();
+        LibSecp256k1.secp256k1_ecdsa_sign_recoverable(CONTEXT, signature, messageHash, privateKey, null, null);
+
+        final ByteBuffer compactSig = ByteBuffer.allocate(64);
+        final IntByReference recId = new IntByReference(0);
+        LibSecp256k1.secp256k1_ecdsa_recoverable_signature_serialize_compact(
+                LibSecp256k1.CONTEXT, compactSig, recId, signature);
+        compactSig.flip();
+        final byte[] sig = compactSig.array();
+
+        final byte[] result = new byte[65];
+        System.arraycopy(sig, 0, result, 0, 64);
+        result[64] = (byte) (recId.getValue() + 27);
+        return result;
+    }
+
+    public static byte[] calculateSignableMessage(EthTxData ethTx) {
         return switch (ethTx.type()) {
             case LEGACY_ETHEREUM -> (ethTx.chainId() != null && ethTx.chainId().length > 0)
                     ? RLPEncoder.encodeAsList(
