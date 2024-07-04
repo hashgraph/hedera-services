e577abc0be7c6ff6c33458291124e4ae2fd8b451
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2024-07-04 19:19:21 +0300

    test: use temp dir for swirlds-platform-core data dir
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/SignedStateFileReadWriteTest.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/SignedStateFileReadWriteTest.java
@@ -141,7 +141,7 @@ class SignedStateFileReadWriteTest {
         final Path addressBookFile = directory.resolve(CURRENT_ADDRESS_BOOK_FILE_NAME);
 
         throwIfFileExists(stateFile, hashInfoFile, settingsUsedFile, directory);
-        final Configuration configuration = changeConfigAndConfigHolder("data/saved");
+        final Configuration configuration = changeConfigAndConfigHolder(testDirectory.resolve("data/saved").toString());
 
         final PlatformContext platformContext = TestPlatformContextBuilder.create()
                 .withConfiguration(configuration)
