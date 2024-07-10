ce5c8303a143b212ff83f92604a5ced34310fe2d
Michael Tinker <michael.tinker@swirldslabs.com>
2024-07-10 12:57:30 -0500

    Fix compile
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip993/NaturalDispatchOrderingTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip993/NaturalDispatchOrderingTest.java
@@ -18,7 +18,7 @@ package com.hedera.services.bdd.suites.hip993;
 
 import com.hedera.hapi.node.base.ResponseCodeEnum;
 import com.hedera.node.app.spi.workflows.HandleContext.TransactionCategory;
-import com.hedera.node.app.workflows.handle.record.SingleTransactionRecordBuilderImpl.ReversingBehavior;
+import com.hedera.node.app.spi.workflows.record.SingleTransactionRecordBuilder.ReversingBehavior;
 
 /**
  * Asserts the expected presence and order of all valid combinations of preceding and following stream items;
@@ -27,7 +27,7 @@ import com.hedera.node.app.workflows.handle.record.SingleTransactionRecordBuilde
  * [HIP-993](<a href="https://github.com/hashgraph/hedera-improvement-proposal/blob/a64bdb258d52ba4ce1ca26bede8e03871b9ade10/HIP/hip-993.md#natural-ordering-of-preceding-records">...</a>).
  * <p>
  * The only stream items created in a savepoint that are <b>not</b> expected to be present are those with reversing
- * behavior {@link ReversingBehavior#REMOVABLE}, and whose originating savepoint was rolled back.
+ * behavior {@link ReversingBehavior}, and whose originating savepoint was rolled back.
  * <p>
  * All other stream items are expected to be present in the record stream once created; but if they are
  * {@link ReversingBehavior#REVERSIBLE}, their status may be changed from {@link ResponseCodeEnum#SUCCESS} to
