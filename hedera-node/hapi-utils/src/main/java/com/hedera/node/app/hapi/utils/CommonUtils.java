7ba3e99203967ffae72b25ff7561c9cace58ea59
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2024-07-17 15:59:45 +0300

    Add airdrop configurations
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/CommonUtils.java
+++ hedera-node/hapi-utils/src/main/java/com/hedera/node/app/hapi/utils/CommonUtils.java
@@ -48,6 +48,7 @@ import static com.hederahashgraph.api.proto.java.HederaFunctionality.ScheduleSig
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.SystemDelete;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.SystemUndelete;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenAccountWipe;
+import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenAirdrop;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenAssociateToAccount;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenBurn;
 import static com.hederahashgraph.api.proto.java.HederaFunctionality.TokenCreate;
@@ -221,6 +222,7 @@ public final class CommonUtils {
             case SCHEDULESIGN -> ScheduleSign;
             case UTIL_PRNG -> UtilPrng;
             case TOKEN_UPDATE_NFTS -> TokenUpdateNfts;
+            case TOKENAIRDROP -> TokenAirdrop;
             case NODECREATE -> NodeCreate;
             case NODEDELETE -> NodeDelete;
             case NODEUPDATE -> NodeUpdate;
