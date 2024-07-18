a96da4bf9db608239197592cf5082e5eb7ea62cd
Michael Heinrichs <netopyr@users.noreply.github.com>
2024-07-18 16:24:27 +0200

    Change protobuf branch back to tmp-hip796-protos
    Signed-off-by: Michael Heinrichs <netopyr@users.noreply.github.com>

--- hapi/build.gradle.kts
+++ hapi/build.gradle.kts
@@ -32,7 +32,7 @@ tasks.cloneHederaProtobufs {
     // uncomment below to use a specific tag
     //    tag = "v0.51.0"
     // uncomment below to use a specific branch
-    branch = "tmp-hip796-protos-test"
+    branch = "tmp-hip796-protos"
 }
 
 sourceSets {
