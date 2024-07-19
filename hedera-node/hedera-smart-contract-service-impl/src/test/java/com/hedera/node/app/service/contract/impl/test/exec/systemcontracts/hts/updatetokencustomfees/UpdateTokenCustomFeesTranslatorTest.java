ab78a770315790365ca50e31e01553f1ac71a906
Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>
2024-07-19 12:50:40 +0300

    test: translator calls test
    Signed-off-by: Stanimir Stoyanov <stanimir.stoyanov@limechain.tech>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/updatetokencustomfees/UpdateTokenCustomFeesTranslatorTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/updatetokencustomfees/UpdateTokenCustomFeesTranslatorTest.java
@@ -16,13 +16,26 @@
 
 package com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.hts.updatetokencustomfees;
 
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.FUNGIBLE_TOKEN_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.NON_FUNGIBLE_TOKEN_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.OWNER_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.OWNER_ID;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.SENDER_ID;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
+import com.esaulpaugh.headlong.abi.Address;
+import com.esaulpaugh.headlong.abi.Tuple;
+import com.hedera.node.app.service.contract.impl.exec.scope.VerificationStrategy;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.AddressIdConverter;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.DispatchForResponseCodeHtsCall;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.HtsCallAttempt;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.updatetokencustomfees.UpdateTokenCustomFeesDecoder;
 import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.updatetokencustomfees.UpdateTokenCustomFeesTranslator;
 import com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.common.CallTestBase;
+import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -36,7 +49,12 @@ class UpdateTokenCustomFeesTranslatorTest extends CallTestBase {
     private HtsCallAttempt attempt;
 
     @Mock
-    private UpdateTokenCustomFeesDecoder decoder;
+    private AddressIdConverter addressIdConverter;
+
+    @Mock
+    private VerificationStrategy verificationStrategy;
+
+    private final UpdateTokenCustomFeesDecoder decoder = new UpdateTokenCustomFeesDecoder();
 
     private UpdateTokenCustomFeesTranslator subject;
 
@@ -61,5 +79,66 @@ class UpdateTokenCustomFeesTranslatorTest extends CallTestBase {
                 .willReturn(UpdateTokenCustomFeesTranslator.UPDATE_NON_FUNGIBLE_TOKEN_CUSTOM_FEES_FUNCTION.selector());
         // expect:
         assertTrue(subject.matches(attempt));
+    }
+
+    @Test
+    void callFromFungibleTest() {
+        Tuple tuple = new Tuple(
+                FUNGIBLE_TOKEN_HEADLONG_ADDRESS,
+                new Tuple[] {
+                    Tuple.of(
+                            10L,
+                            Address.wrap("0x0000000000000000000000000000000000000000"),
+                            true,
+                            false,
+                            OWNER_HEADLONG_ADDRESS)
+                },
+                new Tuple[0]);
+        byte[] inputBytes = Bytes.wrapByteBuffer(
+                        UpdateTokenCustomFeesTranslator.UPDATE_FUNGIBLE_TOKEN_CUSTOM_FEES_FUNCTION.encodeCall(tuple))
+                .toArray();
+        given(attempt.inputBytes()).willReturn(inputBytes);
+        given(attempt.selector())
+                .willReturn(UpdateTokenCustomFeesTranslator.UPDATE_FUNGIBLE_TOKEN_CUSTOM_FEES_FUNCTION.selector());
+        given(attempt.enhancement()).willReturn(mockEnhancement());
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convertSender(any())).willReturn(SENDER_ID);
+        given(addressIdConverter.convert(any())).willReturn(OWNER_ID);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.systemContractGasCalculator()).willReturn(gasCalculator);
+
+        final var call = subject.callFrom(attempt);
+        assertThat(call).isInstanceOf(DispatchForResponseCodeHtsCall.class);
+    }
+
+    @Test
+    void callFromNonFungibleTest() {
+        Tuple tuple = new Tuple(
+                NON_FUNGIBLE_TOKEN_HEADLONG_ADDRESS,
+                new Tuple[] {
+                    Tuple.of(
+                            10L,
+                            Address.wrap("0x0000000000000000000000000000000000000000"),
+                            true,
+                            false,
+                            OWNER_HEADLONG_ADDRESS)
+                },
+                new Tuple[0]);
+        byte[] inputBytes = Bytes.wrapByteBuffer(
+                        UpdateTokenCustomFeesTranslator.UPDATE_NON_FUNGIBLE_TOKEN_CUSTOM_FEES_FUNCTION.encodeCall(
+                                tuple))
+                .toArray();
+        given(attempt.inputBytes()).willReturn(inputBytes);
+        given(attempt.selector())
+                .willReturn(UpdateTokenCustomFeesTranslator.UPDATE_NON_FUNGIBLE_TOKEN_CUSTOM_FEES_FUNCTION.selector());
+        given(attempt.enhancement()).willReturn(mockEnhancement());
+        given(attempt.addressIdConverter()).willReturn(addressIdConverter);
+        given(addressIdConverter.convertSender(any())).willReturn(SENDER_ID);
+        given(addressIdConverter.convert(any())).willReturn(OWNER_ID);
+        given(attempt.defaultVerificationStrategy()).willReturn(verificationStrategy);
+        given(attempt.systemContractGasCalculator()).willReturn(gasCalculator);
+
+        final var call = subject.callFrom(attempt);
+        assertThat(call).isInstanceOf(DispatchForResponseCodeHtsCall.class);
     }
 }
