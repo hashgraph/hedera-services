dc38b952a15ddbce234dcb913d91b1503c366344
Thomas Moran <152873392+thomas-swirlds-labs@users.noreply.github.com>
2024-07-22 11:53:28 +0100

    PR review commments
    Signed-off-by: Thomas Moran <152873392+thomas-swirlds-labs@users.noreply.github.com>

--- gradle/plugins/src/main/kotlin/com.hedera.gradle.spotless.gradle.kts
+++ gradle/plugins/src/main/kotlin/com.hedera.gradle.spotless.gradle.kts
@@ -23,6 +23,16 @@ spotless {
     // optional: limit format enforcement to just the files changed by this feature branch
     ratchetFrom("origin/develop")
 
+    format("misc") {
+        // define the files to apply `misc` to
+        target("*.md", ".gitignore")
+
+        // define the steps to apply to those files
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
     format("actionYaml") {
         target(".github/workflows/*.yaml")
         /*
