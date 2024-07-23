4ce1a1d6e03d0aa52250498c5a432d78d01da585
mxtartaglia <maxi@swirldslabs.com>
2024-07-22 22:23:48 -0300

    native support library
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- example-apps/swirlds-platform-base-example/src/main/cpp/hello.cpp
+++
@@ -1,9 +0,0 @@
-
-#include <jni.h>
-#include <iostream>
-#include "com_swirlds_platform_base_example_jni_HelloWorld.h"  // This is the generated header file
-
-// Implementation of the native method
-JNIEXPORT void JNICALL Java_com_swirlds_platform_base_example_jni_HelloWorld_printHelloWorld(JNIEnv *, jobject) {
-    std::cout << "Hello, World from C++!" << std::endl;
-}
