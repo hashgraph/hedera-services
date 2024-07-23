6b10b153c28b108146039285f24666cabe87aad4
Thomas Moran <152873392+thomas-swirlds-labs@users.noreply.github.com>
2024-07-23 15:32:43 +0100

    chore: Implement Automatic Formatting for Markdown (#14238)
    Signed-off-by: Thomas Moran <152873392+thomas-swirlds-labs@users.noreply.github.com>
    Co-authored-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Co-authored-by: Nathan Klick <nathan@swirldslabs.com>

--- gradle/plugins/src/main/kotlin/com.hedera.gradle.root.gradle.kts
+++ gradle/plugins/src/main/kotlin/com.hedera.gradle.root.gradle.kts
@@ -23,6 +23,7 @@ plugins {
     id("com.hedera.gradle.repositories")
     id("com.hedera.gradle.nexus-publish")
     id("com.hedera.gradle.spotless-kotlin")
+    id("com.hedera.gradle.spotless-markdown")
     id("com.autonomousapps.dependency-analysis")
 }
 
