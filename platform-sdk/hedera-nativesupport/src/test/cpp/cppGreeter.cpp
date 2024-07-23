c1d515add9c93be0373a0a2b78a8017a25f04447
mxtartaglia <maxi@swirldslabs.com>
2024-07-23 17:39:33 -0300

    Linux statically linked lib
    Signed-off-by: mxtartaglia <maxi@swirldslabs.com>

--- platform-sdk/hedera-nativesupport/src/test/cpp/cppGreeter.cpp
+++ platform-sdk/hedera-nativesupport/src/test/cpp/cppGreeter.cpp
@@ -1,10 +1,9 @@
 
 #include <jni.h>
-#include <iostream>
 #include "com_hedera_nativesupport_jni_Greeter.h"  // This is the generated header file
 
 // Implementation of the native method
 JNIEXPORT jstring JNICALL  Java_com_hedera_nativesupport_jni_Greeter_getGreeting(JNIEnv *env, jobject) {
-   std::string helloStr = "Hello, World from C++!";
-   return env->NewStringUTF(helloStr.c_str());
+   const char *helloStr = "Hello, World from C++!";
+   return env->NewStringUTF(helloStr);
 }
