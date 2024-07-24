991ca2be7eaebbe518fc3b1c0cd072c5220c9910
Valentin Valkanov <valentin.valkanov@limechain.tech>
2024-07-24 12:19:38 +0300

    fix: solidity contract match bytecode
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/test-clients/src/main/resource/contract/contracts/NumericContract/NumericContract.sol
+++ hedera-node/test-clients/src/main/resource/contract/contracts/NumericContract/NumericContract.sol
@@ -31,18 +31,30 @@ contract NumericContract {
         }
     }
 
-    function burnTokenV1(address token, uint64 amount, int64[] memory serialNumbers) public {
-        (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("burnToken(address,uint64,int64[])", token, amount, serialNumbers));
-
-        if (success == false) {
+    function burnTokenV1(address _token, uint64 _amount, int64[] memory _serialNumbers) external
+    returns (int32 responseCode, uint64 newTotalSupply)
+    {
+        (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("burnToken(address,uint64,int64[])",
+            _token, _amount, _serialNumbers));
+        (responseCode, newTotalSupply) =
+            success
+                ? abi.decode(result, (int32, uint64))
+                : (int32(0), 0);
+        if (responseCode != 22) {
             revert();
         }
     }
 
-    function burnTokenV2(address token, int64 amount, int64[] memory serialNumbers) public {
-        (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("burnToken(address,int64,int64[])", token, amount, serialNumbers));
-
-        if (success == false) {
+    function burnTokenV2(address _token, int64 _amount, int64[] memory _serialNumbers) external
+    returns (int32 responseCode, uint64 newTotalSupply)
+    {
+        (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("burnToken(address,int64,int64[])",
+            _token, _amount, _serialNumbers));
+        (responseCode, newTotalSupply) =
+            success
+                ? abi.decode(result, (int32, uint64))
+                : (int32(0), 0);
+        if (responseCode != 22) {
             revert();
         }
     }
@@ -50,7 +62,9 @@ contract NumericContract {
     function mintTokenV1(address token, uint64 amount, bytes[] memory metadata) public {
         (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("mintToken(address,uint64,bytes[])", token, amount, metadata));
 
-        if (success == false) {
+        (int32 responseCode, uint64 newTotalSupply, int[] memory serialNumbers) = abi.decode(result, (int32, uint64, int[]));
+
+        if (responseCode != 22) {
             revert();
         }
     }
@@ -58,7 +72,9 @@ contract NumericContract {
     function mintTokenV2(address token, int64 amount, bytes[] memory metadata) public {
         (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("mintToken(address,int64,bytes[])", token, amount, metadata));
 
-        if (success == false) {
+        (int32 responseCode, uint64 newTotalSupply, int[] memory serialNumbers) = abi.decode(result, (int32, uint64, int[]));
+
+        if (responseCode != 22) {
             revert();
         }
     }
@@ -66,7 +82,8 @@ contract NumericContract {
     function wipeFungibleV1(address token, address account, uint32 amount) public {
         (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("wipeTokenAccount(address,address,uint32)", token, account, amount));
 
-        if (success == false) {
+        int32 responseCode = abi.decode(result, (int32));
+        if(responseCode != 22) {
             revert();
         }
     }
@@ -74,7 +91,8 @@ contract NumericContract {
     function wipeFungibleV2(address token, address account, int64 amount) public {
         (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("wipeTokenAccount(address,address,int64)", token, account, amount));
 
-        if (success == false) {
+        int32 responseCode = abi.decode(result, (int32));
+        if(responseCode != 22) {
             revert();
         }
     }
@@ -82,7 +100,8 @@ contract NumericContract {
     function wipeNFT(address token, address account, int64[] memory serialNumbers) public {
         (bool success, bytes memory result) = address(0x167).call(abi.encodeWithSignature("wipeTokenAccountNFT(address,address,int64[])", token, account, serialNumbers));
 
-        if (success == false) {
+        int32 responseCode = abi.decode(result, (int32));
+        if(responseCode != 22) {
             revert();
         }
     }
@@ -125,11 +144,11 @@ contract NumericContract {
     function tokenURI(address token, uint256 _tokenId) public view {
         (bool success, bytes memory result) = token.staticcall(abi.encodeWithSignature("tokenURI(uint256)", _tokenId));
         string memory resultString = abi.decode(result, (string));
-        string memory expectedBad = "ERC721Metadata: URI query for nonexistent token";
+        bytes memory expectedBad = "ERC721Metadata: URI query for nonexistent token";
 
         // tokenURI has different behaviour from the original ERC721 standard as it should revert when providing invalid
         // serialNumber, but instead it returns success with result describing the issue, that is why we compare the result.
-        if (keccak256(bytes(resultString)) == keccak256(bytes(expectedBad))) {
+        if (keccak256(expectedBad) == keccak256(bytes(resultString))) {
             revert();
         }
     }
