cbe668f5c92647903f32f473e107b8deb00d6cbd
Roger Barker <roger.barker@swirldslabs.com>
2024-07-25 14:39:18 -0500

    added removal to warnings list to ignore
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>

--- platform-sdk/swirlds-logging/build.gradle.kts
+++ platform-sdk/swirlds-logging/build.gradle.kts
@@ -24,8 +24,7 @@ plugins {
 // Remove the following line to enable all 'javac' lint checks that we have turned on by default
 // and then fix the reported issues.
 tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("-Xlint:all,-deprecation")
-    options.compilerArgs.add("-Xlint:-exports,-varargs")
+    options.compilerArgs.add("-Xlint:-deprecation,-exports,-removal,-varargs")
 }
 
 mainModuleInfo { annotationProcessor("com.google.auto.service.processor") }
