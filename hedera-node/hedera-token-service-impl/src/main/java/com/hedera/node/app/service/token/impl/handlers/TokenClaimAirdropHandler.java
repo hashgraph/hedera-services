10e6bbc864107a2bcff796be6acff43ec3d9ef20
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2024-07-29 09:05:36 +0300

    add handle method and basic hapi test
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenClaimAirdropHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenClaimAirdropHandler.java
@@ -16,8 +16,25 @@
 
 package com.hedera.node.app.service.token.impl.handlers;
 
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
+import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
+
+import com.hedera.hapi.node.base.AccountAmount;
+import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.HederaFunctionality;
+import com.hedera.hapi.node.base.NftTransfer;
+import com.hedera.hapi.node.base.TokenTransferList;
+import com.hedera.hapi.node.state.token.Token;
+import com.hedera.hapi.node.token.CryptoTransferTransactionBody;
 import com.hedera.hapi.node.transaction.TransactionBody;
+import com.hedera.node.app.service.token.ReadableTokenStore;
+import com.hedera.node.app.service.token.impl.WritableAccountStore;
+import com.hedera.node.app.service.token.impl.WritableAirdropStore;
+import com.hedera.node.app.service.token.impl.WritableTokenRelationStore;
+import com.hedera.node.app.service.token.impl.handlers.transfer.CryptoTransferExecutor;
+import com.hedera.node.app.service.token.impl.handlers.transfer.TransferContextImpl;
+import com.hedera.node.app.service.token.impl.validators.TokenAirdropValidator;
+import com.hedera.node.app.service.token.records.TokenAirdropRecordBuilder;
 import com.hedera.node.app.spi.fees.FeeContext;
 import com.hedera.node.app.spi.fees.Fees;
 import com.hedera.node.app.spi.workflows.HandleContext;
@@ -26,6 +43,8 @@ import com.hedera.node.app.spi.workflows.PreCheckException;
 import com.hedera.node.app.spi.workflows.PreHandleContext;
 import com.hedera.node.app.spi.workflows.TransactionHandler;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -34,11 +53,15 @@ import javax.inject.Singleton;
  * HederaFunctionality#TOKEN_CLAIM_AIRDROP}.
  */
 @Singleton
-public class TokenClaimAirdropHandler implements TransactionHandler {
+public class TokenClaimAirdropHandler extends BaseTokenHandler implements TransactionHandler {
+
+    private final CryptoTransferExecutor executor;
+    private final TokenAirdropValidator validator;
 
     @Inject
-    public TokenClaimAirdropHandler() {
-        // Exists for injection
+    public TokenClaimAirdropHandler(@NonNull CryptoTransferExecutor executor) {
+        this.executor = executor;
+        this.validator = new TokenAirdropValidator();
     }
 
     @Override
@@ -48,10 +71,92 @@ public class TokenClaimAirdropHandler implements TransactionHandler {
     public void pureChecks(@NonNull TransactionBody txn) throws PreCheckException {}
 
     @Override
-    public void handle(@NonNull HandleContext context) throws HandleException {}
+    public void handle(@NonNull HandleContext context) throws HandleException {
+        var pendingAirdropStore = context.storeFactory().writableStore(WritableAirdropStore.class);
+        var accountStore = context.storeFactory().writableStore(WritableAccountStore.class);
+        var tokenStore = context.storeFactory().readableStore(ReadableTokenStore.class);
+        var tokenRelStore = context.storeFactory().writableStore(WritableTokenRelationStore.class);
+        var op = context.body().tokenClaimAirdropOrThrow();
+        // todo implement record builder
+        var recordBuilder = context.savepointStack().getBaseBuilder(TokenAirdropRecordBuilder.class);
+
+        // todo validations for custom fees
+        // validate if pending airdrop is existing in state
+        op.pendingAirdrops().forEach(pendingAirdropId -> {
+            validateTrue(pendingAirdropStore.exists(pendingAirdropId), INVALID_TRANSACTION_BODY);
+        });
+
+        List<TokenTransferList> transfers = new ArrayList<>();
+        List<Token> tokensToAssociate = new ArrayList<>();
+        AccountID receiverId = op.pendingAirdrops().getFirst().receiverId();
+
+        for (var airdrop : op.pendingAirdrops()) {
+            final var tokenId = airdrop.hasFungibleTokenType()
+                    ? airdrop.fungibleTokenTypeOrThrow()
+                    : airdrop.nonFungibleTokenOrThrow().tokenId();
+            final var senderId = airdrop.senderIdOrThrow();
+
+            if (airdrop.hasFungibleTokenType()) {
+                // process fungible tokens
+                var accountPendingAirdrop = pendingAirdropStore.get(airdrop);
+                var senderAccountAmount = AccountAmount.newBuilder()
+                        .amount(-accountPendingAirdrop.pendingAirdropValue().amount())
+                        .accountID(senderId)
+                        .build();
+                var receiverAccountAmount = AccountAmount.newBuilder()
+                        .amount(accountPendingAirdrop.pendingAirdropValue().amount())
+                        .accountID(receiverId)
+                        .build();
+                transfers.add(TokenTransferList.newBuilder()
+                        .token(tokenId)
+                        .transfers(senderAccountAmount, receiverAccountAmount)
+                        .build());
+            } else {
+                // process non-fungible tokens
+                var nftTransfer = NftTransfer.newBuilder()
+                        // todo check if it is approval
+                        //                        .isApproval()
+                        .senderAccountID(senderId)
+                        .receiverAccountID(receiverId)
+                        .serialNumber(airdrop.nonFungibleToken().serialNumber())
+                        .build();
+                transfers.add(TokenTransferList.newBuilder()
+                        .token(tokenId)
+                        .nftTransfers(nftTransfer)
+                        .build());
+            }
+
+            var accountPendingAirdrop = pendingAirdropStore.get(airdrop);
+            pendingAirdropStore.remove(airdrop);
+            var senderAccount = accountStore.getAccountById(senderId);
+
+            // todo update pending airdrop state and account head (similar to TokenRelListCalculator)
+            if (!accountPendingAirdrop.hasPreviousAirdrop() && airdrop.equals(senderAccount.headPendingAirdropId())) {
+                // update the nex, if exists
+                // update accout's head
+            }
+
+            // check if we need new association
+            if (tokenRelStore.get(receiverId, tokenId) == null) {
+                tokensToAssociate.add(tokenStore.get(tokenId));
+            }
+        }
+
+        // associate for free
+        createAndLinkTokenRels(accountStore.getAccountById(receiverId), tokensToAssociate, accountStore, tokenRelStore);
+        var cryptoTransferBody = CryptoTransferTransactionBody.newBuilder()
+                .tokenTransfers(transfers)
+                .build();
+        var syntheticCryptoTransferTxn =
+                TransactionBody.newBuilder().cryptoTransfer(cryptoTransferBody).build();
+        final var transferContext = new TransferContextImpl(context, cryptoTransferBody, true);
+        // We should skip custom fee steps here, because they must be already prepaid
+        executor.executeCryptoTransferWithoutCustomFee(
+                syntheticCryptoTransferTxn, transferContext, context, validator, recordBuilder);
+    }
 
     @Override
     public Fees calculateFees(@NonNull FeeContext feeContext) {
-        return TransactionHandler.super.calculateFees(feeContext);
+        return Fees.FREE;
     }
 }
---
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip904/TokenClaimAirdropTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.services.bdd.suites.hip904;
+
+import static com.hedera.services.bdd.junit.TestTags.CRYPTO;
+import static com.hedera.services.bdd.spec.HapiSpec.defaultHapiSpec;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.mintToken;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenAirdrop;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenClaimAirdrop;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.tokenCreate;
+import static com.hedera.services.bdd.spec.transactions.token.HapiTokenClaimAirdrop.pendingAirdrop;
+import static com.hedera.services.bdd.spec.transactions.token.HapiTokenClaimAirdrop.pendingNFTAirdrop;
+import static com.hedera.services.bdd.spec.transactions.token.TokenMovement.moving;
+import static com.hedera.services.bdd.spec.transactions.token.TokenMovement.movingUnique;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.overriding;
+import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
+import static com.hederahashgraph.api.proto.java.TokenType.FUNGIBLE_COMMON;
+import static com.hederahashgraph.api.proto.java.TokenType.NON_FUNGIBLE_UNIQUE;
+
+import com.google.protobuf.ByteString;
+import com.hedera.services.bdd.junit.HapiTest;
+import com.hedera.services.bdd.junit.HapiTestLifecycle;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Tag;
+
+@Tag(CRYPTO)
+@HapiTestLifecycle
+@DisplayName("Claim token airdrop")
+public class TokenClaimAirdropTest {
+
+    @HapiTest
+    final Stream<DynamicTest> claimFungibleTokenAirdrop() {
+        var OWNER = "owner";
+        var OWNER_2 = "owner2";
+        var RECEIVER = "receiver";
+        var FUNGIBLE_TOKEN = "fungibleToken";
+        var FUNGIBLE_TOKEN_2 = "fungibleToken2";
+        var NON_FUNGIBLE_TOKEN = "nonFungibleToken";
+        var NFT_SUPPLY_KEY = "supplyKey";
+
+        return defaultHapiSpec("should transfer fungible tokens")
+                .given(
+                        overriding("tokens.airdrops.enabled", "true"),
+                        cryptoCreate(OWNER).balance(ONE_HUNDRED_HBARS).maxAutomaticTokenAssociations(0),
+                        cryptoCreate(OWNER_2).balance(ONE_HUNDRED_HBARS).maxAutomaticTokenAssociations(0),
+                        cryptoCreate(RECEIVER).balance(0L),
+                        tokenCreate(FUNGIBLE_TOKEN)
+                                .treasury(OWNER)
+                                .tokenType(FUNGIBLE_COMMON)
+                                .initialSupply(1000L),
+                        tokenCreate(FUNGIBLE_TOKEN_2)
+                                .treasury(OWNER_2)
+                                .tokenType(FUNGIBLE_COMMON)
+                                .initialSupply(1000L),
+                        newKeyNamed(NFT_SUPPLY_KEY),
+                        tokenCreate(NON_FUNGIBLE_TOKEN)
+                                .name(NON_FUNGIBLE_TOKEN)
+                                .treasury(OWNER)
+                                .tokenType(NON_FUNGIBLE_UNIQUE)
+                                .initialSupply(0L)
+                                .supplyKey(NFT_SUPPLY_KEY),
+                        mintToken(
+                                NON_FUNGIBLE_TOKEN,
+                                List.of(ByteString.copyFromUtf8("a"), ByteString.copyFromUtf8("b"))))
+                .when(
+                        // do pending airdrop
+                        tokenAirdrop(moving(10, FUNGIBLE_TOKEN).between(OWNER, RECEIVER))
+                                .payingWith(OWNER),
+                        tokenAirdrop(moving(10, FUNGIBLE_TOKEN_2).between(OWNER_2, RECEIVER))
+                                .payingWith(OWNER_2),
+                        tokenAirdrop(movingUnique(NON_FUNGIBLE_TOKEN, 1).between(OWNER, RECEIVER))
+                                .payingWith(OWNER),
+
+                        // do claim
+                        tokenClaimAirdrop(
+                                pendingAirdrop(OWNER, RECEIVER, FUNGIBLE_TOKEN),
+                                pendingAirdrop(OWNER_2, RECEIVER, FUNGIBLE_TOKEN_2),
+                                pendingNFTAirdrop(OWNER, RECEIVER, NON_FUNGIBLE_TOKEN, 1)))
+                .then( // assert balance fungible tokens
+                        getAccountBalance(OWNER).hasTokenBalance(FUNGIBLE_TOKEN, 990),
+                        getAccountBalance(OWNER_2).hasTokenBalance(FUNGIBLE_TOKEN_2, 990),
+                        getAccountBalance(RECEIVER).hasTokenBalance(FUNGIBLE_TOKEN, 10),
+                        getAccountBalance(RECEIVER).hasTokenBalance(FUNGIBLE_TOKEN_2, 10),
+                        // assert balances NFT
+                        getAccountBalance(RECEIVER).hasTokenBalance(NON_FUNGIBLE_TOKEN, 1),
+
+                        // should be free
+                        getAccountBalance(RECEIVER).hasTinyBars(0L));
+    }
+}
