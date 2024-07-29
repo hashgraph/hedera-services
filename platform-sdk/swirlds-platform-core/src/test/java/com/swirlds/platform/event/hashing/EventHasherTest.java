ed96705678e9ee14556688b007837cb4879e789f
Lazar Petrovic <lpetrovic05@gmail.com>
2024-07-29 12:04:44 +0200

    add test
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/hashing/EventHasherTest.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/event/hashing/EventHasherTest.java
@@ -2,12 +2,34 @@ package com.swirlds.platform.event.hashing;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import com.swirlds.common.crypto.Hash;
+import com.swirlds.common.test.fixtures.Randotron;
+import com.swirlds.platform.event.PlatformEvent;
+import com.swirlds.platform.test.fixtures.event.TestingEventBuilder;
+import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 class EventHasherTest {
+    /**
+     * Tests that different hashing methods produce the same hash.
+     */
     @Test
-    void pbjStreamHashingTest(){
+    void objHashingEquivalenceTest(){
+        final Random random = Randotron.create();
 
+        final PlatformEvent event = new TestingEventBuilder(random)
+                .setAppTransactionCount(2)
+                .setSystemTransactionCount(2)
+                .setSelfParent(new TestingEventBuilder(random).build())
+                .setOtherParent(new TestingEventBuilder(random).build())
+                .build();
+
+        new PbjBytesHasher().hashEvent(event);
+        final Hash bytesHash = event.getHash();
+        event.invalidateHash();
+        new PbjStreamHasher().hashEvent(event);
+
+        assertEquals(bytesHash, event.getHash(), "PBJ bytes hasher and PBJ stream hasher should produce the same hash");
     }
 
 }
