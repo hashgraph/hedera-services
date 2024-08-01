555912f9c522a6f40f3f2a45b753bed1507b7ec5
Lazar Petrovic <lpetrovic05@gmail.com>
2024-08-01 16:40:48 +0200

    some cleanup
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/eventhandling/DefaultTransactionHandlerTests.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/eventhandling/DefaultTransactionHandlerTests.java
@@ -16,72 +16,44 @@
 
 package com.swirlds.platform.eventhandling;
 
-import static com.swirlds.common.test.fixtures.RandomUtils.getRandomPrintSeed;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import com.swirlds.base.test.fixtures.time.FakeTime;
-import com.swirlds.base.time.Time;
-import com.swirlds.common.context.PlatformContext;
-import com.swirlds.common.crypto.Hash;
 import com.swirlds.common.test.fixtures.Randotron;
-import com.swirlds.common.test.fixtures.platform.TestPlatformContextBuilder;
 import com.swirlds.platform.consensus.EventWindow;
 import com.swirlds.platform.consensus.SyntheticSnapshot;
 import com.swirlds.platform.event.AncientMode;
 import com.swirlds.platform.event.PlatformEvent;
 import com.swirlds.platform.gossip.shadowgraph.Generations;
 import com.swirlds.platform.internal.ConsensusRound;
-import com.swirlds.platform.internal.EventImpl;
-import com.swirlds.platform.state.MerkleRoot;
-import com.swirlds.platform.state.PlatformState;
-import com.swirlds.platform.state.SwirldStateManager;
-import com.swirlds.platform.system.SoftwareVersion;
 import com.swirlds.platform.system.address.AddressBook;
-import com.swirlds.platform.system.events.CesEvent;
-import com.swirlds.platform.system.status.PlatformStatus;
-import com.swirlds.platform.system.status.StatusActionSubmitter;
 import com.swirlds.platform.system.status.actions.FreezePeriodEnteredAction;
 import com.swirlds.platform.test.fixtures.addressbook.RandomAddressBookBuilder;
-import com.swirlds.platform.test.fixtures.event.EventImplTestUtils;
 import com.swirlds.platform.test.fixtures.event.TestingEventBuilder;
 import com.swirlds.platform.wiring.components.StateAndRound;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 /**
  * Unit tests for {@link DefaultTransactionHandler}.
  */
 class DefaultTransactionHandlerTests {
-    private static final long ROUND_NUMBER = 5L;
-
     private Randotron random;
-    private Time time;
     private AddressBook addressBook;
 
     @BeforeEach
     void setUp() {
         random = Randotron.create();
-        time = new FakeTime();
         addressBook = RandomAddressBookBuilder.create(random)
                 .withRealKeysEnabled(false)
                 .withSize(4)
@@ -90,6 +62,7 @@ class DefaultTransactionHandlerTests {
 
     private ConsensusRound newConsensusRound(
             final boolean pcesRound) {
+        //TODO events with no transactions
         final List<PlatformEvent> events = Stream.generate(
                         () -> new TestingEventBuilder(random).setConsensusTimestamp(random.nextInstant()).build())
                 .limit(3)
@@ -111,62 +84,6 @@ class DefaultTransactionHandlerTests {
         return round;
     }
 
-
-    private static ConsensusRound mockConsensusRound(
-            @NonNull final EventImpl keystoneEvent,
-            @NonNull final List<EventImpl> events,
-            final boolean pcesRound) {
-        final ArrayList<CesEvent> streamedEvents = new ArrayList<>();
-        for (final Iterator<EventImpl> iterator = events.iterator(); iterator.hasNext(); ) {
-            final EventImpl event = iterator.next();
-            final CesEvent cesEvent = new CesEvent(event.getBaseEvent(), ROUND_NUMBER, !iterator.hasNext());
-            streamedEvents.add(cesEvent);
-            cesEvent.getRunningHash().setHash(mock(Hash.class));
-        }
-
-        final ConsensusRound consensusRound = mock(ConsensusRound.class);
-        when(consensusRound.getConsensusEvents())
-                .thenReturn(events.stream().map(EventImpl::getBaseEvent).collect(Collectors.toList()));
-        when(consensusRound.getNumEvents()).thenReturn(events.size());
-        when(consensusRound.getConsensusTimestamp())
-                .thenReturn(Time.getCurrent().now());
-        when(consensusRound.getKeystoneEvent()).thenReturn(keystoneEvent.getBaseEvent());
-        when(consensusRound.getRoundNum()).thenReturn(ROUND_NUMBER);
-        when(consensusRound.isEmpty()).thenReturn(events.isEmpty());
-        when(consensusRound.isPcesRound()).thenReturn(pcesRound);
-        when(consensusRound.getStreamedEvents()).thenReturn(streamedEvents);
-
-        return consensusRound;
-    }
-
-    private EventImpl buildEvent() {
-        final EventImpl event = EventImplTestUtils.createEventImpl(
-                new TestingEventBuilder(random).setConsensusTimestamp(time.now()), null, null);
-
-        event.getBaseEvent().signalPrehandleCompletion();
-
-        return event;
-    }
-
-    private static SwirldStateManager mockSwirldStateManager(@NonNull final PlatformState platformState) {
-        final MerkleRoot consensusState = mock(MerkleRoot.class);
-        final MerkleRoot stateForSigning = mock(MerkleRoot.class);
-        when(consensusState.getPlatformState()).thenReturn(platformState);
-        final SwirldStateManager swirldStateManager = mock(SwirldStateManager.class);
-        when(swirldStateManager.getConsensusState()).thenReturn(consensusState);
-        when(swirldStateManager.getStateForSigning()).thenReturn(stateForSigning);
-
-        return swirldStateManager;
-    }
-
-    private static void assertEventReachedConsensus(@NonNull final EventImpl event) {
-        assertTrue(event.getBaseEvent().getTransactionCount() > 0, "event should have transactions");
-        event.getBaseEvent()
-                .consensusTransactionIterator()
-                .forEachRemaining(transaction -> assertNotNull(
-                        transaction.getConsensusTimestamp(), "transaction should have a consensus timestamp"));
-    }
-
     private static void assertEventReachedConsensus(@NonNull final PlatformEvent event) {
         assertTrue(event.getTransactionCount() > 0, "event should have transactions");
         event.consensusTransactionIterator()
@@ -184,7 +101,7 @@ class DefaultTransactionHandlerTests {
     @DisplayName("Normal operation")
     @ParameterizedTest
     @CsvSource({"false", "true"})
-    void normalOperation(final boolean pcesRound) {
+    void normalOperation(final boolean pcesRound) throws InterruptedException {
         final TransactionHandlerTester tester = new TransactionHandlerTester(addressBook);
         final ConsensusRound consensusRound = newConsensusRound(pcesRound);
 
@@ -198,22 +115,32 @@ class DefaultTransactionHandlerTests {
         consensusRound.getConsensusEvents().forEach(DefaultTransactionHandlerTests::assertEventReachedConsensus);
 
         assertTrue(tester.getSubmittedActions().isEmpty(), "no status should have been submitted");
-        tester.verifyRoundHandled(consensusRound);
-        tester.verifyFrozen(false);
+        assertEquals(1, tester.getHandledRounds().size(), "a round should have been handled");
+        assertSame(consensusRound, tester.getHandledRounds().getFirst());
+        assertNull(tester.getPlatformState().getLastFrozenTime());
 
+        assertEquals(
+                tester.getPlatformState().getLegacyRunningEventHash(),
+                consensusRound
+                        .getStreamedEvents()
+                        .getLast()
+                        .getRunningHash()
+                        .getFutureHash()
+                        .getAndRethrow(),
+                "the running hash should be updated");
         assertEquals(pcesRound, handlerOutput.reservedSignedState().get().isPcesRound());
     }
 
     @Test
     @DisplayName("Round in freeze period")
-    void freezeHandling() {
+    void freezeHandling() throws InterruptedException {
         final TransactionHandlerTester tester = new TransactionHandlerTester(addressBook);
         final ConsensusRound consensusRound = newConsensusRound(false);
-        tester.platformState.setFreezeTime(consensusRound.getConsensusTimestamp());
+        tester.getPlatformState().setFreezeTime(consensusRound.getConsensusTimestamp());
 
 
         final StateAndRound handlerOutput = tester.getTransactionHandler().handleConsensusRound(consensusRound);
-        assertNotEquals(null, handlerOutput, "new state should have been created");
+        assertNotNull( handlerOutput, "new state should have been created");
         assertEquals(
                 1,
                 handlerOutput.reservedSignedState().get().getReservationCount(),
@@ -223,8 +150,9 @@ class DefaultTransactionHandlerTests {
 
         assertEquals(1, tester.getSubmittedActions().size(), "the freeze status should have been submitted");
         assertEquals(FreezePeriodEnteredAction.class, tester.getSubmittedActions().getFirst().getClass());
-        tester.verifyRoundHandled(consensusRound);
-        tester.verifyFrozen(true);
+        assertEquals(1, tester.getHandledRounds().size(), "a round should have been handled");
+        assertSame(consensusRound, tester.getHandledRounds().getFirst(), "it should be the round we provided");
+        assertNotNull(tester.getPlatformState().getLastFrozenTime());
 
         final ConsensusRound postFreezeConsensusRound = newConsensusRound(false);
         final StateAndRound postFreezeOutput = tester.getTransactionHandler().handleConsensusRound(postFreezeConsensusRound);
@@ -233,8 +161,16 @@ class DefaultTransactionHandlerTests {
         postFreezeConsensusRound.getConsensusEvents().forEach(DefaultTransactionHandlerTests::assertEventDidNotReachConsensus);
 
         assertEquals(1, tester.getSubmittedActions().size(), "no new status should have been submitted");
-        //verify(swirldStateManager).handleConsensusRound(consensusRound);
-        //TODO verify round ont handled
-        tester.verifyRoundHandled(consensusRound);
+        assertEquals(1, tester.getHandledRounds().size(), "no new rounds should have been handled");
+        assertSame(consensusRound, tester.getHandledRounds().getFirst(), "it should same round as before");
+        assertEquals(
+                tester.getPlatformState().getLegacyRunningEventHash(),
+                consensusRound
+                        .getStreamedEvents()
+                        .getLast()
+                        .getRunningHash()
+                        .getFutureHash()
+                        .getAndRethrow(),
+                "the running hash should from the freeze round");
     }
 }
--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/eventhandling/TransactionHandlerTester.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/eventhandling/TransactionHandlerTester.java
@@ -1,18 +1,18 @@
 package com.swirlds.platform.eventhandling;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.swirlds.common.context.PlatformContext;
 import com.swirlds.common.platform.NodeId;
 import com.swirlds.common.test.fixtures.platform.TestPlatformContextBuilder;
-import com.swirlds.platform.internal.ConsensusRound;
 import com.swirlds.platform.state.MerkleRoot;
 import com.swirlds.platform.state.PlatformState;
-import com.swirlds.platform.state.State;
 import com.swirlds.platform.state.SwirldStateManager;
 import com.swirlds.platform.system.BasicSoftwareVersion;
+import com.swirlds.platform.system.Round;
 import com.swirlds.platform.system.SoftwareVersion;
 import com.swirlds.platform.system.SwirldState;
 import com.swirlds.platform.system.address.AddressBook;
@@ -20,34 +20,29 @@ import com.swirlds.platform.system.status.StatusActionSubmitter;
 import com.swirlds.platform.system.status.actions.PlatformStatusAction;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 
 public class TransactionHandlerTester {
-
-    final AddressBook addressBook;
-    final SwirldStateManager swirldStateManager;
-    final SwirldState swirldState;
-    final PlatformState platformState;
-    final DefaultTransactionHandler defaultTransactionHandler;
+    private final PlatformState platformState;
+    private final DefaultTransactionHandler defaultTransactionHandler;
     private final List<PlatformStatusAction> submittedActions = new ArrayList<>();
+    private final List<Round> handledRounds = new ArrayList<>();
 
     public TransactionHandlerTester(final AddressBook addressBook) {
-        this.addressBook = addressBook;
         final PlatformContext platformContext =
                 TestPlatformContextBuilder.create().build();
-        //platformState = mock(PlatformState.class);
         platformState = new PlatformState();
 
-        final MerkleRoot consensusState = mock(State.class);
-        swirldState = mock(SwirldState.class);
+        final MerkleRoot consensusState = mock(MerkleRoot.class);
+        final SwirldState swirldState = mock(SwirldState.class);
         when(consensusState.getSwirldState()).thenReturn(swirldState);
-        //final MerkleRoot stateForSigning = mock(State.class);
         when(consensusState.copy()).thenReturn(consensusState);
         when(consensusState.getPlatformState()).thenReturn(platformState);
-        //when(stateForSigning.getPlatformState()).thenReturn(platformState);
-        //when(platformState.getConsensusTimestamp()).thenReturn(randotron.nextInstant());
+        doAnswer(i -> {
+            handledRounds.add(i.getArgument(0));
+            return null;
+        }).when(swirldState).handleConsensusRound(any(), any());
         final StatusActionSubmitter statusActionSubmitter = submittedActions::add;
-        swirldStateManager = new SwirldStateManager(
+        final SwirldStateManager swirldStateManager = new SwirldStateManager(
                 platformContext,
                 addressBook,
                 NodeId.FIRST_NODE_ID,
@@ -55,12 +50,6 @@ public class TransactionHandlerTester {
                 new BasicSoftwareVersion(1)
         );
         swirldStateManager.setInitialState(consensusState);
-//        swirldStateManager = mock(SwirldStateManager.class);
-//        when(swirldStateManager.getConsensusState()).thenReturn(consensusState);
-//        when(swirldStateManager.getStateForSigning()).thenReturn(stateForSigning);
-
-
-
         defaultTransactionHandler = new DefaultTransactionHandler(
                 platformContext, swirldStateManager, statusActionSubmitter, mock(SoftwareVersion.class));
     }
@@ -69,40 +58,15 @@ public class TransactionHandlerTester {
         return defaultTransactionHandler;
     }
 
+    public PlatformState getPlatformState() {
+        return platformState;
+    }
+
     public List<PlatformStatusAction> getSubmittedActions() {
         return submittedActions;
     }
 
-    public void verifyRoundHandled(final ConsensusRound consensusRound){
-        verify(swirldState).handleConsensusRound(consensusRound, platformState);
-
-        try {
-//            verify(platformState)
-//                    .setLegacyRunningEventHash(consensusRound
-//                            .getStreamedEvents()
-//                            .getLast()
-//                            .getRunningHash()
-//                            .getFutureHash()
-//                            .getAndRethrow());
-            Assertions.assertEquals(
-                    platformState.getLegacyRunningEventHash(),
-                    consensusRound
-                            .getStreamedEvents()
-                            .getLast()
-                            .getRunningHash()
-                            .getFutureHash()
-                            .getAndRethrow()
-            );
-        } catch (final InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void verifyFrozen(final boolean frozen){
-        if(frozen){
-            Assertions.assertNotNull(platformState.getLastFrozenTime());
-        }else{
-            Assertions.assertNull(platformState.getLastFrozenTime());
-        }
+    public List<Round> getHandledRounds() {
+        return handledRounds;
     }
 }
