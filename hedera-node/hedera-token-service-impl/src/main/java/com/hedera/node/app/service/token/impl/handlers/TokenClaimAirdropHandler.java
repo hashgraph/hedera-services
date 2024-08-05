c3253a73b2da1a04802dd60f314e8f209b76fe1e
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2024-08-05 16:18:13 +0300

    Update sender's pending airdrop count
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenClaimAirdropHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenClaimAirdropHandler.java
@@ -19,6 +19,7 @@ package com.hedera.node.app.service.token.impl.handlers;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TRANSACTION;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
 import static com.hedera.node.app.service.token.impl.handlers.transfer.customfees.CustomFeeMeta.customFeeMetaFrom;
+import static com.hedera.node.app.service.token.impl.util.PendingAirdropUpdater.removePendingAirdrops;
 import static com.hedera.node.app.service.token.impl.util.TokenHandlerHelper.getIfUsable;
 import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
 import static java.util.Objects.requireNonNull;
@@ -91,42 +92,18 @@ public class TokenClaimAirdropHandler extends BaseTokenHandler implements Transa
         List<Token> tokensToAssociate = new ArrayList<>();
         AccountID receiverId = op.pendingAirdrops().getFirst().receiverId();
 
+        // 1. validate pending airdrops and create transfer lists
         for (var airdrop : op.pendingAirdrops()) {
             final var tokenId = airdrop.hasFungibleTokenType()
                     ? airdrop.fungibleTokenTypeOrThrow()
                     : airdrop.nonFungibleTokenOrThrow().tokenId();
-            final var senderId = airdrop.senderIdOrThrow();
             // validate existence and custom fees
             validateTrue(pendingAirdropStore.exists(airdrop), INVALID_TRANSACTION_BODY);
             validateTrue(tokenHasNoCustomFeesPaidByReceiver(tokenId, tokenStore), INVALID_TRANSACTION);
 
-            if (airdrop.hasFungibleTokenType()) {
-                // process fungible tokens
-                var accountPendingAirdrop = pendingAirdropStore.get(airdrop);
-                var senderAccountAmount = AccountAmount.newBuilder()
-                        .amount(-accountPendingAirdrop.pendingAirdropValue().amount())
-                        .accountID(senderId)
-                        .build();
-                var receiverAccountAmount = AccountAmount.newBuilder()
-                        .amount(accountPendingAirdrop.pendingAirdropValue().amount())
-                        .accountID(receiverId)
-                        .build();
-                transfers.add(TokenTransferList.newBuilder()
-                        .token(tokenId)
-                        .transfers(senderAccountAmount, receiverAccountAmount)
-                        .build());
-            } else {
-                // process non-fungible tokens
-                var nftTransfer = NftTransfer.newBuilder()
-                        .senderAccountID(senderId)
-                        .receiverAccountID(receiverId)
-                        .serialNumber(airdrop.nonFungibleToken().serialNumber())
-                        .build();
-                transfers.add(TokenTransferList.newBuilder()
-                        .token(tokenId)
-                        .nftTransfers(nftTransfer)
-                        .build());
-            }
+            // build transfer lists
+            final var senderId = airdrop.senderIdOrThrow();
+            transfers.add(createTokenTransferList(airdrop, pendingAirdropStore, tokenId, senderId, receiverId));
 
             // check if we need new association
             if (tokenRelStore.get(receiverId, tokenId) == null) {
@@ -134,14 +111,73 @@ public class TokenClaimAirdropHandler extends BaseTokenHandler implements Transa
             }
         }
 
-        // associate for free
+        // associate tokens
+        associateForFree(tokensToAssociate, receiverId, accountStore, tokenRelStore, recordBuilder);
+
+        // do the crypto transfer
+        transferForFree(transfers, context, recordBuilder);
+
+        // Update state
+        removePendingAirdrops(op.pendingAirdrops(), pendingAirdropStore, accountStore);
+    }
+
+    @Override
+    public Fees calculateFees(@NonNull FeeContext feeContext) {
+        return Fees.FREE;
+    }
+
+    private TokenTransferList createTokenTransferList(
+            @NonNull PendingAirdropId airdrop,
+            @NonNull WritableAirdropStore airdropStore,
+            @NonNull TokenID tokenId,
+            @NonNull AccountID senderId,
+            @NonNull AccountID receiverId) {
+        var accountPendingAirdrop = airdropStore.get(airdrop);
+        if (airdrop.hasFungibleTokenType()) {
+            // process fungible tokens
+            var senderAccountAmount = AccountAmount.newBuilder()
+                    .amount(-accountPendingAirdrop.pendingAirdropValue().amount())
+                    .accountID(senderId)
+                    .build();
+            var receiverAccountAmount = AccountAmount.newBuilder()
+                    .amount(accountPendingAirdrop.pendingAirdropValue().amount())
+                    .accountID(receiverId)
+                    .build();
+            return TokenTransferList.newBuilder()
+                    .token(tokenId)
+                    .transfers(senderAccountAmount, receiverAccountAmount)
+                    .build();
+        } else {
+            // process non-fungible tokens
+            var nftTransfer = NftTransfer.newBuilder()
+                    .senderAccountID(senderId)
+                    .receiverAccountID(receiverId)
+                    .serialNumber(airdrop.nonFungibleToken().serialNumber())
+                    .build();
+            return TokenTransferList.newBuilder()
+                    .token(tokenId)
+                    .nftTransfers(nftTransfer)
+                    .build();
+        }
+    }
+
+    private void associateForFree(
+            @NonNull List<Token> tokensToAssociate,
+            @NonNull AccountID receiverId,
+            @NonNull WritableAccountStore accountStore,
+            @NonNull WritableTokenRelationStore tokenRelStore,
+            @NonNull TokenAirdropRecordBuilder recordBuilder) {
         createAndLinkTokenRels(accountStore.getAccountById(receiverId), tokensToAssociate, accountStore, tokenRelStore);
-        // externalize associations
         tokensToAssociate.forEach(token -> recordBuilder.addAutomaticTokenAssociation(TokenAssociation.newBuilder()
                 .tokenId(token.tokenId())
                 .accountId(receiverId)
                 .build()));
-        // do the crypto transfer
+    }
+
+    private void transferForFree(
+            @NonNull List<TokenTransferList> transfers,
+            @NonNull HandleContext context,
+            @NonNull TokenAirdropRecordBuilder recordBuilder) {
         var cryptoTransferBody = CryptoTransferTransactionBody.newBuilder()
                 .tokenTransfers(transfers)
                 .build();
@@ -151,73 +187,6 @@ public class TokenClaimAirdropHandler extends BaseTokenHandler implements Transa
         // We should skip custom fee steps here, because they must be already prepaid
         executor.executeCryptoTransferWithoutCustomFee(
                 syntheticCryptoTransferTxn, transferContext, context, validator, recordBuilder);
-        // removePendingAirdrop
-        op.pendingAirdrops()
-                .forEach(airdrop -> removePendingAirdropAndUpdateStores(airdrop, pendingAirdropStore, accountStore));
-    }
-
-    @Override
-    public Fees calculateFees(@NonNull FeeContext feeContext) {
-        return Fees.FREE;
-    }
-
-    private void removePendingAirdropAndUpdateStores(
-            PendingAirdropId airdrop, WritableAirdropStore pendingAirdropStore, WritableAccountStore accountStore) {
-        var currentAirdrop = requireNonNull(pendingAirdropStore.get(airdrop));
-        var previousAirdrop =
-                currentAirdrop.hasPreviousAirdrop() ? pendingAirdropStore.get(currentAirdrop.previousAirdrop()) : null;
-        var nextAirdrop =
-                currentAirdrop.hasNextAirdrop() ? pendingAirdropStore.get(currentAirdrop.nextAirdrop()) : null;
-
-        var senderAccount = requireNonNull(accountStore.getAccountById(airdrop.senderIdOrThrow()));
-
-        // if no prev, should check if we have next and update it + change the head id in the sender's account
-        if (!currentAirdrop.hasPreviousAirdrop() && airdrop.equals(senderAccount.headPendingAirdropId())) {
-            // update the nex, if exists
-            // update account's head
-            if (currentAirdrop.hasNextAirdrop()) {
-                // set prev to null and copy everything else
-                PendingAirdropId nullId = null;
-                var updatedNext =
-                        nextAirdrop.copyBuilder().previousAirdrop(nullId).build();
-                pendingAirdropStore.patch(currentAirdrop.nextAirdrop(), updatedNext);
-
-                // update sender account
-                var updatedSenderAccount = senderAccount
-                        .copyBuilder()
-                        .headPendingAirdropId(currentAirdrop.nextAirdrop())
-                        .build();
-                accountStore.put(updatedSenderAccount);
-            }
-        }
-
-        // if no next, should check if we have prev and update it
-        if (!currentAirdrop.hasNextAirdrop()) {
-            // update prev to point to null next, if exist
-            if (currentAirdrop.hasPreviousAirdrop()) {
-                PendingAirdropId nullId = null;
-                var updatedPrevious =
-                        previousAirdrop.copyBuilder().nextAirdrop(nullId).build();
-                pendingAirdropStore.patch(currentAirdrop.previousAirdrop(), updatedPrevious);
-            }
-        }
-
-        // updated prev and next
-        if (currentAirdrop.hasNextAirdrop() && currentAirdrop.hasPreviousAirdrop()) {
-            // update prev and next
-            var updatedNext = nextAirdrop
-                    .copyBuilder()
-                    .previousAirdrop(currentAirdrop.previousAirdrop())
-                    .build();
-            var updatedPrev = previousAirdrop
-                    .copyBuilder()
-                    .nextAirdrop(currentAirdrop.nextAirdrop())
-                    .build();
-            pendingAirdropStore.patch(currentAirdrop.nextAirdrop(), updatedNext);
-            pendingAirdropStore.patch(currentAirdrop.previousAirdrop(), updatedPrev);
-        }
-
-        pendingAirdropStore.remove(airdrop);
     }
 
     // todo: same validation is used in TokenAirdropHandler, so when it is merged we can reuse it here too
---
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/util/PendingAirdropUpdater.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.node.app.service.token.impl.util;
+
+import static com.hedera.hapi.node.base.ResponseCodeEnum.INVALID_TRANSACTION_BODY;
+import static com.hedera.node.app.spi.workflows.HandleException.validateTrue;
+import static java.util.Objects.requireNonNull;
+
+import com.hedera.hapi.node.base.AccountID;
+import com.hedera.hapi.node.base.PendingAirdropId;
+import com.hedera.hapi.node.state.token.Account;
+import com.hedera.hapi.node.state.token.AccountPendingAirdrop;
+import com.hedera.node.app.service.token.impl.WritableAccountStore;
+import com.hedera.node.app.service.token.impl.WritableAirdropStore;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PendingAirdropUpdater {
+
+    public static void removePendingAirdrops(
+            @NonNull final List<PendingAirdropId> airdropsToRemove,
+            @NonNull final WritableAirdropStore pendingAirdropStore,
+            @NonNull final WritableAccountStore accountStore) {
+        final Map<AccountID, Account> updatedSenders = new HashMap<>();
+        final Map<PendingAirdropId, AccountPendingAirdrop> updatedAirdrops = new HashMap<>();
+
+        // calculate state changes
+        for (PendingAirdropId id : airdropsToRemove) {
+            removePendingAirdropAndUpdateStores(id, pendingAirdropStore, accountStore, updatedSenders, updatedAirdrops);
+        }
+
+        // commit updates
+        updatedSenders.forEach((accountID, account) -> accountStore.put(account));
+        updatedAirdrops.forEach(pendingAirdropStore::patch);
+        airdropsToRemove.forEach(pendingAirdropStore::remove);
+    }
+
+    private static void removePendingAirdropAndUpdateStores(
+            PendingAirdropId airdropId,
+            WritableAirdropStore pendingAirdropStore,
+            WritableAccountStore accountStore,
+            Map<AccountID, Account> updatedSenders,
+            Map<PendingAirdropId, AccountPendingAirdrop> updatedAirdrops) {
+
+        var senderId = airdropId.senderIdOrThrow();
+        var senderAccount = updatedSenders.containsKey(senderId)
+                ? updatedSenders.get(senderId)
+                : requireNonNull(accountStore.getAccountById(senderId));
+
+        final var airdrop = updatedAirdrops.containsKey(airdropId)
+                ? updatedAirdrops.get(airdropId)
+                : pendingAirdropStore.getForModify(airdropId);
+        validateTrue(airdrop != null, INVALID_TRANSACTION_BODY);
+
+        // updated sender's head of pending airdrops
+        if (airdropId.equals(senderAccount.headPendingAirdropId())) {
+            final var updatedSender = senderAccount
+                    .copyBuilder()
+                    .headPendingAirdropId(airdrop.nextAirdrop())
+                    .build();
+            updatedSenders.put(updatedSender.accountId(), updatedSender);
+        }
+
+        // update pending airdrops links
+        final var prevAirdropId = airdrop.previousAirdrop();
+        final var nextAirdropId = airdrop.nextAirdrop();
+        if (prevAirdropId != null) {
+            final var prevAccountAirdrop = pendingAirdropStore.getForModify(prevAirdropId);
+            validateTrue(prevAccountAirdrop != null, INVALID_TRANSACTION_BODY);
+            final var prevAirdropToUpdate =
+                    prevAccountAirdrop.copyBuilder().nextAirdrop(nextAirdropId).build();
+            updatedAirdrops.put(prevAirdropId, prevAirdropToUpdate);
+        }
+        if (nextAirdropId != null) {
+            final var nextAccountAirdrop = pendingAirdropStore.getForModify(nextAirdropId);
+            validateTrue(nextAccountAirdrop != null, INVALID_TRANSACTION_BODY);
+            final var nextAirdropToUpdate = nextAccountAirdrop
+                    .copyBuilder()
+                    .previousAirdrop(prevAirdropId)
+                    .build();
+            updatedAirdrops.put(nextAirdropId, nextAirdropToUpdate);
+        }
+
+        // decrement the number of pending airdrops
+        senderAccount = updatedSenders.containsKey(senderId)
+                ? updatedSenders.get(senderId)
+                : requireNonNull(accountStore.getAccountById(senderId));
+        var updatedSenderAccount = senderAccount
+                .copyBuilder()
+                .numberPendingAirdrops(senderAccount.numberPendingAirdrops() - 1)
+                .build();
+        updatedSenders.put(updatedSenderAccount.accountId(), updatedSenderAccount);
+    }
+}
