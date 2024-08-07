8c3e2db8e8697b9e5af1adb48548839dccb28459
Michael Tinker <michael.tinker@swirldslabs.com>
2024-08-07 11:58:37 -0500

    chore: migrate `StateChangeListener` to States API (#14584)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/testFixtures/java/com/hedera/node/app/fixtures/state/FakeState.java
+++ hedera-node/hedera-app/src/testFixtures/java/com/hedera/node/app/fixtures/state/FakeState.java
@@ -16,15 +16,24 @@
 
 package com.hedera.node.app.fixtures.state;
 
+import static com.swirlds.state.StateChangeListener.StateType.MAP;
+import static com.swirlds.state.StateChangeListener.StateType.QUEUE;
+import static com.swirlds.state.StateChangeListener.StateType.SINGLETON;
+import static com.swirlds.state.merkle.StateUtils.computeLabel;
 import static java.util.Objects.requireNonNull;
 
 import com.hedera.node.app.spi.fixtures.state.MapWritableStates;
 import com.swirlds.state.State;
+import com.swirlds.state.StateChangeListener;
 import com.swirlds.state.spi.EmptyReadableStates;
 import com.swirlds.state.spi.EmptyWritableStates;
+import com.swirlds.state.spi.KVChangeListener;
+import com.swirlds.state.spi.QueueChangeListener;
 import com.swirlds.state.spi.ReadableKVState;
 import com.swirlds.state.spi.ReadableSingletonStateBase;
 import com.swirlds.state.spi.ReadableStates;
+import com.swirlds.state.spi.WritableKVStateBase;
+import com.swirlds.state.spi.WritableQueueStateBase;
 import com.swirlds.state.spi.WritableSingletonStateBase;
 import com.swirlds.state.spi.WritableStates;
 import com.swirlds.state.test.fixtures.ListReadableQueueState;
@@ -33,6 +42,8 @@ import com.swirlds.state.test.fixtures.MapReadableKVState;
 import com.swirlds.state.test.fixtures.MapReadableStates;
 import com.swirlds.state.test.fixtures.MapWritableKVState;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -46,6 +57,10 @@ public class FakeState implements State {
     private final Map<String, Map<String, Object>> states = new ConcurrentHashMap<>();
     private final Map<String, ReadableStates> readableStates = new ConcurrentHashMap<>();
     private final Map<String, WritableStates> writableStates = new ConcurrentHashMap<>();
+    /**
+     * Listeners to be notified of state changes on {@link MapWritableStates#commit()} calls for any service.
+     */
+    private final List<StateChangeListener> listeners = new ArrayList<>();
 
     /**
      * Adds to the service with the given name the {@link ReadableKVState} {@code states}
@@ -100,10 +115,10 @@ public class FakeState implements State {
         });
     }
 
-    @NonNull
     @Override
-    @SuppressWarnings("java:S3740") // provide the parameterized type for the generic state variable
-    public WritableStates getWritableStates(@NonNull String serviceName) {
+    @NonNull
+    public WritableStates getWritableStates(@NonNull final String serviceName) {
+        requireNonNull(serviceName);
         return writableStates.computeIfAbsent(serviceName, s -> {
             final var serviceStates = states.get(s);
             if (serviceStates == null) {
@@ -113,16 +128,26 @@ public class FakeState implements State {
             for (final var entry : serviceStates.entrySet()) {
                 final var stateName = entry.getKey();
                 final var state = entry.getValue();
-                if (state instanceof Queue queue) {
-                    data.put(stateName, new ListWritableQueueState(stateName, queue));
-                } else if (state instanceof Map map) {
-                    data.put(stateName, new MapWritableKVState(stateName, map));
-                } else if (state instanceof AtomicReference ref) {
-                    data.put(stateName, new WritableSingletonStateBase(stateName, ref::get, ref::set));
+                if (state instanceof Queue<?> queue) {
+                    data.put(
+                            stateName,
+                            withAnyRegisteredListeners(serviceName, new ListWritableQueueState<>(stateName, queue)));
+                } else if (state instanceof Map<?, ?> map) {
+                    data.put(
+                            stateName,
+                            withAnyRegisteredListeners(serviceName, new MapWritableKVState<>(stateName, map)));
+                } else if (state instanceof AtomicReference<?> ref) {
+                    data.put(stateName, withAnyRegisteredListeners(serviceName, stateName, ref));
                 }
             }
             return new MapWritableStates(data, () -> readableStates.remove(serviceName));
         });
+    }
+
+    @Override
+    public void registerCommitListener(@NonNull final StateChangeListener listener) {
+        requireNonNull(listener);
+        listeners.add(listener);
     }
 
     /**
@@ -132,6 +157,79 @@ public class FakeState implements State {
         writableStates.values().forEach(writableStates -> {
             if (writableStates instanceof MapWritableStates mapWritableStates) {
                 mapWritableStates.commit();
+            }
+        });
+    }
+
+    private <V> WritableSingletonStateBase<V> withAnyRegisteredListeners(
+            @NonNull final String serviceName, @NonNull final String stateKey, @NonNull final AtomicReference<V> ref) {
+        final var state = new WritableSingletonStateBase<>(stateKey, ref::get, ref::set);
+        listeners.forEach(listener -> {
+            if (listener.stateTypes().contains(SINGLETON)) {
+                registerSingletonListener(serviceName, state, listener);
+            }
+        });
+        return state;
+    }
+
+    private <K, V> MapWritableKVState<K, V> withAnyRegisteredListeners(
+            @NonNull final String serviceName, @NonNull final MapWritableKVState<K, V> state) {
+        listeners.forEach(listener -> {
+            if (listener.stateTypes().contains(MAP)) {
+                registerKVListener(serviceName, state, listener);
+            }
+        });
+        return state;
+    }
+
+    private <T> ListWritableQueueState<T> withAnyRegisteredListeners(
+            @NonNull final String serviceName, @NonNull final ListWritableQueueState<T> state) {
+        listeners.forEach(listener -> {
+            if (listener.stateTypes().contains(QUEUE)) {
+                registerQueueListener(serviceName, state, listener);
+            }
+        });
+        return state;
+    }
+
+    private <V> void registerSingletonListener(
+            @NonNull final String serviceName,
+            @NonNull final WritableSingletonStateBase<V> singletonState,
+            @NonNull final StateChangeListener listener) {
+        final var stateName = computeLabel(serviceName, singletonState.getStateKey());
+        singletonState.registerListener(value -> listener.singletonUpdateChange(stateName, value));
+    }
+
+    private <V> void registerQueueListener(
+            @NonNull final String serviceName,
+            @NonNull final WritableQueueStateBase<V> queueState,
+            @NonNull final StateChangeListener listener) {
+        final var stateName = computeLabel(serviceName, queueState.getStateKey());
+        queueState.registerListener(new QueueChangeListener<>() {
+            @Override
+            public void queuePushChange(@NonNull final V value) {
+                listener.queuePushChange(stateName, value);
+            }
+
+            @Override
+            public void queuePopChange() {
+                listener.queuePopChange(stateName);
+            }
+        });
+    }
+
+    private <K, V> void registerKVListener(
+            @NonNull final String serviceName, WritableKVStateBase<K, V> state, StateChangeListener listener) {
+        final var stateName = computeLabel(serviceName, state.getStateKey());
+        state.registerListener(new KVChangeListener<>() {
+            @Override
+            public void mapUpdateChange(@NonNull final K key, @NonNull final V value) {
+                listener.mapUpdateChange(stateName, key, value);
+            }
+
+            @Override
+            public void mapDeleteChange(@NonNull final K key) {
+                listener.mapDeleteChange(stateName, key);
             }
         });
     }
