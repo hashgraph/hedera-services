e78c877aaa1807889c19be430bc452a1b2b029a1
Lazar Petrovic <lpetrovic05@gmail.com>
2024-08-13 16:13:38 +0200

    working finally
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/hashing/HashingMigrationUtils.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/event/hashing/HashingMigrationUtils.java
@@ -1,11 +1,19 @@
 package com.swirlds.platform.event.hashing;
 
 import com.hedera.hapi.node.base.SemanticVersion;
+import com.swirlds.base.time.Time;
+import com.swirlds.common.utility.throttle.RateLimitedLogger;
+import com.swirlds.logging.legacy.LogMarker;
 import com.swirlds.platform.config.SemanticVersionConverter;
 import com.swirlds.platform.eventhandling.EventConfig;
 import com.swirlds.state.spi.HapiUtils;
+import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HashingMigrationUtils {
+    private static final Logger logger = LogManager.getLogger(HashingMigrationUtils.class);
+    private static final RateLimitedLogger tellMe = new RateLimitedLogger(logger, Time.getCurrent(), Duration.ofSeconds(1));
     private static final SemanticVersionConverter converter = new SemanticVersionConverter();
 
     public static UnsignedEventHasher getUnsignedEventHasher(
@@ -26,22 +34,34 @@ public class HashingMigrationUtils {
         return converter.convert(migrationVersionString);
     }
 
-    public static CombinedEventHasher getEventHasher(final SemanticVersion migrationVersion,
+    public static CombinedEventHasher getEventHasher(
+            final SemanticVersion migrationVersion,
             final SemanticVersion eventVersion) {
         if (migrationVersion == null) {
             // no migration version set, use the old event hashing algorithm
             return new StatefulEventHasher();
         }
-        return HapiUtils.SEMANTIC_VERSION_COMPARATOR.compare(eventVersion, migrationVersion) > 0
-                ? new StatefulEventHasher()
-                : new PbjHasher();
+//        logger.error(
+//                LogMarker.EXCEPTION.getMarker(),
+//                """
+//                        Event version {}
+//                        Migration version {}
+//                        Comparison {}
+//                        """,
+//                eventVersion,
+//                migrationVersion,
+//                HapiUtils.SEMANTIC_VERSION_COMPARATOR.compare(eventVersion, migrationVersion)
+//        );
+        return HapiUtils.SEMANTIC_VERSION_COMPARATOR.compare(eventVersion, migrationVersion) >= 0
+                ? new PbjHasher()
+                : new StatefulEventHasher();
     }
 
     public static void tellMe(final SemanticVersion migrationVersion,
-            final SemanticVersion currentSoftwareVersion){
-        System.out.printf("Current version%n %s %n", currentSoftwareVersion);
+            final SemanticVersion eventVersion){
+        System.out.printf("Current version%n %s %n", eventVersion);
         System.out.printf("Migration version%n %s %n", migrationVersion);
         System.out.printf("Comparison%n %d %n",
-                HapiUtils.SEMANTIC_VERSION_COMPARATOR.compare(currentSoftwareVersion, migrationVersion));
+                HapiUtils.SEMANTIC_VERSION_COMPARATOR.compare(eventVersion, migrationVersion));
     }
 }
