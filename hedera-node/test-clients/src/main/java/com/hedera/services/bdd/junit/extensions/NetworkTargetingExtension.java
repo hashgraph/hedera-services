b050024dcb781a8718cad351a282f1b45aa8959a
Michael Tinker <michael.tinker@swirldslabs.com>
2024-08-14 13:26:12 -0500

    feat: simplify `@HapiTest` throttle overrides (#14837)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/extensions/NetworkTargetingExtension.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/extensions/NetworkTargetingExtension.java
@@ -16,9 +16,11 @@
 
 package com.hedera.services.bdd.junit.extensions;
 
+import static com.hedera.services.bdd.junit.ContextRequirement.THROTTLE_OVERRIDES;
 import static com.hedera.services.bdd.junit.extensions.ExtensionUtils.hapiTestMethodOf;
 import static org.junit.platform.commons.support.AnnotationSupport.isAnnotated;
 
+import com.hedera.services.bdd.junit.ContextRequirement;
 import com.hedera.services.bdd.junit.GenesisHapiTest;
 import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.junit.LeakyEmbeddedHapiTest;
@@ -28,6 +30,7 @@ import com.hedera.services.bdd.junit.hedera.embedded.EmbeddedNetwork;
 import com.hedera.services.bdd.spec.HapiSpec;
 import com.hedera.services.bdd.spec.keys.RepeatableKeyGenerator;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.extension.AfterEachCallback;
@@ -55,13 +58,20 @@ public class NetworkTargetingExtension implements BeforeEachCallback, AfterEachC
                 HapiSpec.TARGET_NETWORK.set(targetNetwork);
             } else {
                 HapiSpec.TARGET_NETWORK.set(SHARED_NETWORK.get());
-                // If there are properties to preserve, bind that info to the thread before executing the test factory
+                // If there are properties to preserve or system files to override and restore, bind that info to the
+                // thread before executing the test factory
                 if (isAnnotated(method, LeakyHapiTest.class)) {
                     HapiSpec.PROPERTIES_TO_PRESERVE.set(
                             List.of(method.getAnnotation(LeakyHapiTest.class).overrides()));
+                    HapiSpec.THROTTLES_OVERRIDE.set(effectiveThrottleResource(
+                            method.getAnnotation(LeakyHapiTest.class).requirement(),
+                            method.getAnnotation(LeakyHapiTest.class).throttles()));
                 } else if (isAnnotated(method, LeakyEmbeddedHapiTest.class)) {
                     HapiSpec.PROPERTIES_TO_PRESERVE.set(List.of(
                             method.getAnnotation(LeakyEmbeddedHapiTest.class).overrides()));
+                    HapiSpec.THROTTLES_OVERRIDE.set(effectiveThrottleResource(
+                            method.getAnnotation(LeakyEmbeddedHapiTest.class).requirement(),
+                            method.getAnnotation(LeakyEmbeddedHapiTest.class).throttles()));
                 }
             }
         });
@@ -70,6 +80,22 @@ public class NetworkTargetingExtension implements BeforeEachCallback, AfterEachC
     @Override
     public void afterEach(@NonNull final ExtensionContext extensionContext) {
         HapiSpec.TARGET_NETWORK.remove();
+        HapiSpec.THROTTLES_OVERRIDE.remove();
         HapiSpec.PROPERTIES_TO_PRESERVE.remove();
+    }
+
+    /**
+     * If there is an explicit throttle resource to load, returns it; otherwise returns null if the test's
+     * context requirement does not include overriding throttles.
+     * @param contextRequirements the context requirements of the test
+     * @param throttles the path to the throttle resource
+     * @return the effective throttle resource
+     */
+    private @Nullable String effectiveThrottleResource(
+            @NonNull final ContextRequirement[] contextRequirements, @NonNull final String throttles) {
+        if (!throttles.isBlank()) {
+            return throttles;
+        }
+        return List.of(contextRequirements).contains(THROTTLE_OVERRIDES) ? "" : null;
     }
 }
