3485430829c142c2f6b3df61a80d1f1e5945f7a2
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-08-15 22:04:46 -0500

    fix compile error
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/module-info.java
+++ hedera-node/test-clients/src/main/java/module-info.java
@@ -60,7 +60,6 @@ module com.hedera.node.test.clients {
     exports com.hedera.services.bdd.junit.support.validators;
     exports com.hedera.services.bdd.junit.support;
     exports com.hedera.services.bdd.junit.support.validators.utils;
-    exports com.hedera.services.bdd.spec.transactions.node;
 
     requires transitive com.hedera.node.app.hapi.fees;
     requires transitive com.hedera.node.app.hapi.utils;
