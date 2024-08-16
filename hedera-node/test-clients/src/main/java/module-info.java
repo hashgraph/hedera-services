ff56c60150ae36aa46028d44bec2b483fe32cc07
Michael Tinker <michael.tinker@swirldslabs.com>
2024-08-15 19:28:53 -0500

    feat: yahcli DAB functionality (#14752)
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>
    Co-authored-by: Lev Povolotsky <lev@swirldslabs.com>
    Signed-off-by: Harsh Sawarkar <harshsawarkar111@gmail.com>

--- hedera-node/test-clients/src/main/java/module-info.java
+++ hedera-node/test-clients/src/main/java/module-info.java
@@ -36,6 +36,7 @@ module com.hedera.node.test.clients {
     exports com.hedera.services.bdd.junit.support.validators;
     exports com.hedera.services.bdd.junit.support;
     exports com.hedera.services.bdd.junit.support.validators.utils;
+    exports com.hedera.services.bdd.spec.transactions.node;
 
     requires transitive com.hedera.node.app.hapi.fees;
     requires transitive com.hedera.node.app.hapi.utils;
