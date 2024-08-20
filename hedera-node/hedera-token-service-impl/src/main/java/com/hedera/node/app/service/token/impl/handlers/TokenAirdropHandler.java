d933d7ecefb466f19c21d5b619df54cf90ad6fd1
Ivan Bankov <ivan.bankov@limechain.tech>
2024-08-20 10:11:16 +0300

    chore: add AllowanceStrategy enum (#14910)
    Signed-off-by: ibankov <ivan.bankov@limechain.tech>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenAirdropHandler.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/TokenAirdropHandler.java
@@ -322,7 +322,7 @@ public class TokenAirdropHandler extends TransferExecutor implements Transaction
      * the credits for the receivers.
      * @param fungibleAmounts the fungible airdrop amounts
      * @param sender the sender account id
-     * @param isApproval should be false for airdrop
+     * @param isApproval is approval
      * @param transferListBuilder the transfer list builder
      */
     private void addTransfersToTransferList(
