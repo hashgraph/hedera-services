0ecb145abc0575cdef32b2cb6c773b562014f1b5
Valentin Valkanov <valentin.valkanov@limechain.tech>
2024-08-28 16:19:37 +0300

    fix: tests
    Signed-off-by: Valentin Valkanov <valentin.valkanov@limechain.tech>

--- hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/ContractsConfig.java
+++ hedera-node/hedera-config/src/main/java/com/hedera/node/config/data/ContractsConfig.java
@@ -81,7 +81,8 @@ public record ContractsConfig(
         @ConfigProperty(value = "systemContract.accountService.isAuthorizedRawEnabled", defaultValue = "true")
                 @NetworkProperty
                 boolean systemContractAccountServiceIsAuthorizedRawEnabled,
-        @ConfigProperty(value = "systemContract.metadataKeyAndFieldSupport.enabled") @NetworkProperty
+        @ConfigProperty(value = "systemContract.metadataKeyAndFieldSupport.enabled", defaultValue = "true")
+                @NetworkProperty
                 boolean metadataKeyAndFieldEnabled,
         @ConfigProperty(value = "systemContract.updateCustomFees.enabled", defaultValue = "false") @NetworkProperty
                 boolean systemContractUpdateCustomFeesEnabled,
