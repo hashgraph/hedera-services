b9f36889aa01a0d311cc4319645814f3a342ad5c
Lev Povolotsky <lev@swirldslabs.com>
2024-08-28 08:02:11 -0400

    merge and spotless
    Signed-off-by: Lev Povolotsky <lev@swirldslabs.com>

--- hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenAirdropHandlerTest.java
+++ hedera-node/hedera-token-service-impl/src/test/java/com/hedera/node/app/service/token/impl/test/handlers/TokenAirdropHandlerTest.java
@@ -93,7 +93,6 @@ class TokenAirdropHandlerTest extends CryptoTransferHandlerTestBase {
     @Mock
     private FeeCalculator feeCalculator;
 
-
     @Mock
     private StoreMetricsService storeMetricsService;
 
