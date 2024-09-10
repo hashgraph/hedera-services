3c432ae9b0f2399866b5a1d46790e5114f6c4b22
Kore Aguda <kore@swirldslabs.com>
2024-09-10 16:52:24 -0500

    rename roster
    Signed-off-by: Kore Aguda <kore@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/module-info.java
+++ platform-sdk/swirlds-platform-core/src/main/java/module-info.java
@@ -1,3 +1,6 @@
+import com.swirlds.config.api.ConfigurationExtension;
+import com.swirlds.platform.config.PlatformConfigurationExtension;
+
 /**
  * The Swirlds public API module used by platform applications.
  */
