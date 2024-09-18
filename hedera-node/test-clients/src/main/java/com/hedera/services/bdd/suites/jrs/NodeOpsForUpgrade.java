72f1c7e48297467277f851651eaed0f6fcdd4fa7
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-09-18 10:49:11 -0500

    add all ops in same upgrade test
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/jrs/NodeOpsForUpgrade.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/jrs/NodeOpsForUpgrade.java
@@ -2,14 +2,17 @@ package com.hedera.services.bdd.suites.jrs;
 
 import com.hedera.services.bdd.spec.keys.KeyShape;
 import com.hedera.services.bdd.suites.HapiSuite;
+import com.hederahashgraph.api.proto.java.ServiceEndpoint;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DynamicTest;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static com.hedera.node.app.hapi.utils.CommonPbjConverters.toPbj;
+import static com.hedera.services.bdd.junit.hedera.utils.AddressBookUtils.endpointFor;
 import static com.hedera.services.bdd.spec.HapiPropertySource.asAccount;
 import static com.hedera.services.bdd.spec.HapiSpec.defaultHapiSpec;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.nodeCreate;
@@ -25,6 +28,9 @@ import static com.hedera.services.bdd.suites.hip869.NodeCreateTest.generateX509C
 
 public class NodeOpsForUpgrade extends HapiSuite {
     private static final Logger log = LogManager.getLogger(NodeOpsForUpgrade.class);
+    public static List<ServiceEndpoint> UPDATE_GOSSIP_ENDPOINTS_IPS =
+            Arrays.asList(endpointFor("192.168.1.202", 123), endpointFor("192.168.1.203", 123));
+
 
     public static void main(String... args) {
         new NodeOpsForUpgrade().runSuiteSync();
@@ -47,10 +53,10 @@ public class NodeOpsForUpgrade extends HapiSuite {
                 .given(initializeSettings())
                 .when(overriding("nodes.enableDAB", "true"),
                         newKeyNamed(ED_25519_KEY).shape(KeyShape.ED25519),
-                        nodeDelete("2").payingWith(GENESIS).signedBy(GENESIS),
+//                        nodeDelete("2").payingWith(GENESIS).signedBy(GENESIS),
                         nodeUpdate("0")
                                 .description("UpdatedNode0")
-                                .gossipEndpoint(toPbj(GOSSIP_ENDPOINTS_IPS))
+                                .gossipEndpoint(toPbj(UPDATE_GOSSIP_ENDPOINTS_IPS))
                                 .payingWith(GENESIS)
                                 .signedBy(GENESIS),
                         nodeCreate("node5")
