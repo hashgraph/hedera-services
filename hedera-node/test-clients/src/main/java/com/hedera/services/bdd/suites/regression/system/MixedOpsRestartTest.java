456d00048000f6ae2f014d57d6dc898fab181b10
Michael Tinker <michael.tinker@swirldslabs.com>
2024-09-19 21:37:30 -0500

    Add multi-part DAB edits before and after PREPARE_UPGRADE
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/system/MixedOpsRestartTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/system/MixedOpsRestartTest.java
@@ -16,21 +16,19 @@
 
 package com.hedera.services.bdd.suites.regression.system;
 
-import static com.hedera.services.bdd.junit.TestTags.RESTART;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.suites.regression.system.MixedOperations.burstOfTps;
 
 import com.hedera.services.bdd.junit.HapiTest;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Tag;
 
 /**
  * This test is to verify restart functionality. It submits a burst of mixed operations, then
  * freezes all nodes, shuts them down, restarts them, and submits the same burst of mixed operations
  * again.
  */
-@Tag(RESTART)
+// @Tag(RESTART)
 public class MixedOpsRestartTest implements LifecycleTest {
     private static final int MIXED_OPS_BURST_TPS = 50;
 
