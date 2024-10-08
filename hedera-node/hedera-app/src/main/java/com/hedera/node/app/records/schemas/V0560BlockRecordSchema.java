4f9d92df5a7faee80bfd5d2121c0956d29def8d4
Michael Tinker <michael.tinker@swirldslabs.com>
2024-10-08 09:09:53 -0500

    Always give BlockStreamSchema migration info for now
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/records/schemas/V0560BlockRecordSchema.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/records/schemas/V0560BlockRecordSchema.java
@@ -18,10 +18,8 @@ package com.hedera.node.app.records.schemas;
 
 import static com.hedera.node.app.records.schemas.V0490BlockRecordSchema.BLOCK_INFO_STATE_KEY;
 import static com.hedera.node.app.records.schemas.V0490BlockRecordSchema.RUNNING_HASHES_STATE_KEY;
-import static com.hedera.node.config.types.StreamMode.RECORDS;
 
 import com.hedera.hapi.node.base.SemanticVersion;
-import com.hedera.node.config.data.BlockStreamConfig;
 import com.swirlds.state.spi.MigrationContext;
 import com.swirlds.state.spi.Schema;
 import edu.umd.cs.findbugs.annotations.NonNull;
@@ -43,15 +41,11 @@ public class V0560BlockRecordSchema extends Schema {
     @Override
     public void restart(@NonNull final MigrationContext ctx) {
         if (ctx.previousVersion() != null) {
-            final var streamMode =
-                    ctx.configuration().getConfigData(BlockStreamConfig.class).streamMode();
-            if (streamMode != RECORDS) {
-                // We need to give the upcoming BlockStreamService schemas migration info needed
-                final var blocksState = ctx.newStates().getSingleton(BLOCK_INFO_STATE_KEY);
-                final var runningHashesState = ctx.newStates().getSingleton(RUNNING_HASHES_STATE_KEY);
-                ctx.sharedValues().put(SHARED_BLOCK_RECORD_INFO, blocksState.get());
-                ctx.sharedValues().put(SHARED_RUNNING_HASHES, runningHashesState.get());
-            }
+            // Upcoming BlockStreamService schemas may need migration info
+            final var blocksState = ctx.newStates().getSingleton(BLOCK_INFO_STATE_KEY);
+            final var runningHashesState = ctx.newStates().getSingleton(RUNNING_HASHES_STATE_KEY);
+            ctx.sharedValues().put(SHARED_BLOCK_RECORD_INFO, blocksState.get());
+            ctx.sharedValues().put(SHARED_RUNNING_HASHES, runningHashesState.get());
         }
     }
 }
