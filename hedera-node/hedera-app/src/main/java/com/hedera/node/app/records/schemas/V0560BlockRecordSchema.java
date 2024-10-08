fa319a9bd3ab2223b8ebc8875cb0a0ffcd60c386
Michael Tinker <michael.tinker@swirldslabs.com>
2024-10-07 19:06:43 -0500

    Support restarting from RECORDS -> BOTH
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/records/schemas/V0560BlockRecordSchema.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/records/schemas/V0560BlockRecordSchema.java
@@ -18,40 +18,40 @@ package com.hedera.node.app.records.schemas;
 
 import static com.hedera.node.app.records.schemas.V0490BlockRecordSchema.BLOCK_INFO_STATE_KEY;
 import static com.hedera.node.app.records.schemas.V0490BlockRecordSchema.RUNNING_HASHES_STATE_KEY;
+import static com.hedera.node.config.types.StreamMode.RECORDS;
 
 import com.hedera.hapi.node.base.SemanticVersion;
+import com.hedera.node.config.data.BlockStreamConfig;
 import com.swirlds.state.spi.MigrationContext;
 import com.swirlds.state.spi.Schema;
 import edu.umd.cs.findbugs.annotations.NonNull;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
-public class V0540BlockRecordSchema extends Schema {
-    private static final Logger logger = LogManager.getLogger(V0540BlockRecordSchema.class);
+public class V0560BlockRecordSchema extends Schema {
     /**
      * The version of the schema.
      */
     private static final SemanticVersion VERSION =
-            SemanticVersion.newBuilder().major(0).minor(54).patch(0).build();
+            SemanticVersion.newBuilder().major(0).minor(56).patch(0).build();
 
     private static final String SHARED_BLOCK_RECORD_INFO = "SHARED_BLOCK_RECORD_INFO";
     private static final String SHARED_RUNNING_HASHES = "SHARED_RUNNING_HASHES";
 
-    public V0540BlockRecordSchema() {
+    public V0560BlockRecordSchema() {
         super(VERSION);
     }
 
-    /**
-     * {@inheritDoc}
-     * */
     @Override
-    public void migrate(@NonNull final MigrationContext ctx) {
-        final var isGenesis = ctx.previousVersion() == null;
-        if (!isGenesis) {
-            final var blocksState = ctx.newStates().getSingleton(BLOCK_INFO_STATE_KEY);
-            final var runningHashesState = ctx.newStates().getSingleton(RUNNING_HASHES_STATE_KEY);
-            ctx.sharedValues().put(SHARED_BLOCK_RECORD_INFO, blocksState.get());
-            ctx.sharedValues().put(SHARED_RUNNING_HASHES, runningHashesState.get());
+    public void restart(@NonNull final MigrationContext ctx) {
+        if (ctx.previousVersion() != null) {
+            final var streamMode =
+                    ctx.configuration().getConfigData(BlockStreamConfig.class).streamMode();
+            if (streamMode != RECORDS) {
+                // We need to give the upcoming BlockStreamService schemas migration info needed
+                final var blocksState = ctx.newStates().getSingleton(BLOCK_INFO_STATE_KEY);
+                final var runningHashesState = ctx.newStates().getSingleton(RUNNING_HASHES_STATE_KEY);
+                ctx.sharedValues().put(SHARED_BLOCK_RECORD_INFO, blocksState.get());
+                ctx.sharedValues().put(SHARED_RUNNING_HASHES, runningHashesState.get());
+            }
         }
     }
 }
