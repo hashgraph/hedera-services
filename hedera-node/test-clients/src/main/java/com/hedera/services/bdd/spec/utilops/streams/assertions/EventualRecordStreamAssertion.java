bb4b6f3fc0e46637e320b84e40d95e7d8a16b569
Michael Tinker <michael.tinker@swirldslabs.com>
2024-10-16 19:11:11 -0500

    Insert a file before first observed txn ids
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/streams/assertions/EventualRecordStreamAssertion.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/streams/assertions/EventualRecordStreamAssertion.java
@@ -104,7 +104,8 @@ public class EventualRecordStreamAssertion extends AbstractEventualStreamAsserti
         assertion = requireNonNull(assertionFactory.apply(spec));
         unsubscribe = STREAM_FILE_ACCESS.subscribe(recordStreamLocFor(spec), new StreamDataListener() {
             @Override
-            public void onNewItem(RecordStreamItem item) {
+            public void onNewItem(@NonNull final RecordStreamItem item) {
+                requireNonNull(item);
                 if (assertion.isApplicableTo(item)) {
                     try {
                         if (assertion.test(item)) {
