80e172a6372f516730eb2182f5607bdce1ab844a
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-10-23 13:54:22 +0300

    fix: make public VirtualPipeline.pausePipelineAndExecute
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- platform-sdk/swirlds-virtualmap/src/main/java/com/swirlds/virtualmap/internal/pipeline/VirtualPipeline.java
+++ platform-sdk/swirlds-virtualmap/src/main/java/com/swirlds/virtualmap/internal/pipeline/VirtualPipeline.java
@@ -640,7 +640,7 @@ public class VirtualPipeline {
      * @param runnable
      * 		The runnable. Cannot be null.
      */
-    void pausePipelineAndExecute(final String label, final Runnable runnable) {
+    public void pausePipelineAndExecute(final String label, final Runnable runnable) {
         Objects.requireNonNull(runnable);
         final CountDownLatch waitForBackgroundThreadToStart = new CountDownLatch(1);
         final CountDownLatch waitForRunnableToFinish = new CountDownLatch(1);
