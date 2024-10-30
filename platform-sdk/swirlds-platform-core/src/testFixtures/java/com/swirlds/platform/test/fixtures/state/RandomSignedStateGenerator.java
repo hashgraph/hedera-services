ea4891133a986c11792065b29b80aaeefb16235b
Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>
2024-10-29 21:33:18 -0500

    fix unit tests
    Signed-off-by: Neeharika-Sompalli <neeharika.sompalli@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/state/RandomSignedStateGenerator.java
+++ platform-sdk/swirlds-platform-core/src/testFixtures/java/com/swirlds/platform/test/fixtures/state/RandomSignedStateGenerator.java
@@ -187,7 +187,7 @@ public class RandomSignedStateGenerator {
         } else {
             consensusSnapshotInstance = consensusSnapshot;
         }
-
+        FAKE_MERKLE_STATE_LIFECYCLES.initPlatformState((MerkleStateRoot) stateInstance);
         final PlatformStateModifier platformState = stateInstance.getWritablePlatformState();
 
         platformState.bulkUpdate(v -> {
