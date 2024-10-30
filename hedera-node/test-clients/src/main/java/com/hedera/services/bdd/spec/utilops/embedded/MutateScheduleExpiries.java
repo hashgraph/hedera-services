5e1e6fc298e835f7f35943b9b218a6e286274daf
Michael Tinker <michael.tinker@swirldslabs.com>
2024-10-30 12:59:24 -0500

    chore: add `EmbeddedHedera` workflow integration tests to `test` task (#16173)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/embedded/MutateScheduleExpiries.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/embedded/MutateScheduleExpiries.java
@@ -16,41 +16,30 @@
 
 package com.hedera.services.bdd.spec.utilops.embedded;
 
-import static com.hedera.node.app.hapi.utils.CommonPbjConverters.toPbj;
 import static java.util.Objects.requireNonNull;
 
-import com.hedera.hapi.node.state.token.Account;
+import com.hedera.hapi.node.state.primitives.ProtoLong;
+import com.hedera.hapi.node.state.schedule.ScheduleList;
 import com.hedera.services.bdd.spec.HapiSpec;
-import com.hedera.services.bdd.spec.transactions.TxnUtils;
 import com.hedera.services.bdd.spec.utilops.UtilOp;
+import com.swirlds.state.spi.WritableKVState;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import java.util.function.Consumer;
 
 /**
- * An operation that allows the test author to directly mutate an account in an embedded state.
+ * An operation that allows the test author to directly mutate the schedule experies in an embedded state.
  */
-public class MutateAccountOp extends UtilOp {
-    private final String account;
-    private final Consumer<Account.Builder> mutation;
+public class MutateScheduleExpiries extends UtilOp {
+    private final Consumer<WritableKVState<ProtoLong, ScheduleList>> mutation;
 
-    /**
-     * Constructs the operation.
-     * @param account the identifier of the account to mutate
-     * @param mutation the mutation to apply to the account
-     */
-    public MutateAccountOp(@NonNull final String account, @NonNull final Consumer<Account.Builder> mutation) {
-        this.account = requireNonNull(account);
+    public MutateScheduleExpiries(@NonNull final Consumer<WritableKVState<ProtoLong, ScheduleList>> mutation) {
         this.mutation = requireNonNull(mutation);
     }
 
     @Override
     protected boolean submitOp(@NonNull final HapiSpec spec) throws Throwable {
-        final var accounts = spec.embeddedAccountsOrThrow();
-        final var targetId = toPbj(TxnUtils.asId(account, spec));
-        final var account = requireNonNull(accounts.get(targetId));
-        final var builder = account.copyBuilder();
-        mutation.accept(builder);
-        accounts.put(targetId, builder.build());
+        final var state = spec.embeddedScheduleExpiriesOrThrow();
+        mutation.accept(state);
         spec.commitEmbeddedState();
         return false;
     }
