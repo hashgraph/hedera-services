c706c40591168a639830471e5d1f8b4e0897b5b5
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-11-05 13:51:19 +0200

    PTT CHILD_COUNT experiment
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/com/swirlds/demo/platform/PlatformTestingToolState.java
+++ platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/com/swirlds/demo/platform/PlatformTestingToolState.java
@@ -1684,7 +1684,7 @@ public class PlatformTestingToolState extends MerkleStateRoot {
 
         public static final int QUORUM_RESULT = 10;
 
-        public static final int CHILD_COUNT = 40;
+        public static final int CHILD_COUNT = 64;
     }
 
     @Override
