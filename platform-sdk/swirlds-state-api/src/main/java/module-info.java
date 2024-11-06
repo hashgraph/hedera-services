ddb89c780ba859da6d6aa418a2bdd27fdf599c58
Ivan Malygin <ivan@swirldslabs.com>
2024-11-05 19:10:50 -0500

    Spotless
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/swirlds-state-api/src/main/java/module-info.java
+++ platform-sdk/swirlds-state-api/src/main/java/module-info.java
@@ -7,8 +7,7 @@ module com.swirlds.state.api {
 
     requires transitive com.hedera.node.hapi;
     requires transitive com.swirlds.common;
-    requires transitive com.hedera.pbj.runtime;
     requires transitive com.swirlds.config.api;
-
+    requires transitive com.hedera.pbj.runtime;
     requires static transitive com.github.spotbugs.annotations;
 }
