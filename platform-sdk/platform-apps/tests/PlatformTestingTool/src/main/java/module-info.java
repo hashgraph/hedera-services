6db8463ccfb8c316b2c02b8896ed9c2c1a6f56dc
Nikita Lebedev <nikita.lebedev@limechain.tech>
2024-11-07 14:31:48 +0200

    fix: compilation error
    Signed-off-by: Nikita Lebedev <nikita.lebedev@limechain.tech>

--- platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/module-info.java
+++ platform-sdk/platform-apps/tests/PlatformTestingTool/src/main/java/module-info.java
@@ -36,6 +36,7 @@ module com.swirlds.demo.platform {
     requires com.swirlds.metrics.api;
     requires com.swirlds.platform.core;
     requires com.swirlds.state.api;
+    requires com.swirlds.state.impl;
     requires com.swirlds.virtualmap;
     requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
