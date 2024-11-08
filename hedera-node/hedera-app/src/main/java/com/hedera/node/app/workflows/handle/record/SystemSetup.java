5121a45f64fdb60c6a41ba1f4152247860dcbf8a
Michael Tinker <michael.tinker@swirldslabs.com>
2024-11-07 23:27:01 -0600

    Update node admin keys from override file
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/record/SystemSetup.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/workflows/handle/record/SystemSetup.java
@@ -21,6 +21,7 @@ import static com.hedera.hapi.node.base.ResponseCodeEnum.SUCCESS;
 import static com.hedera.hapi.node.base.ResponseCodeEnum.SUCCESS_BUT_MISSING_EXPECTED_OPERATION;
 import static com.hedera.hapi.util.HapiUtils.ACCOUNT_ID_COMPARATOR;
 import static com.hedera.hapi.util.HapiUtils.FUNDING_ACCOUNT_EXPIRY;
+import static com.hedera.hapi.util.HapiUtils.asTimestamp;
 import static com.hedera.node.app.ids.schemas.V0490EntityIdSchema.ENTITY_ID_STATE_KEY;
 import static com.hedera.node.app.service.file.impl.schemas.V0490FileSchema.dispatchSynthFileUpdate;
 import static com.hedera.node.app.service.file.impl.schemas.V0490FileSchema.parseConfigList;
@@ -29,11 +30,13 @@ import static com.hedera.node.app.spi.workflows.record.StreamBuilder.transaction
 import static com.hedera.node.app.util.FileUtilities.createFileID;
 import static java.util.Objects.requireNonNull;
 
+import com.hedera.hapi.node.addressbook.NodeUpdateTransactionBody;
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.base.CurrentAndNextFeeSchedule;
 import com.hedera.hapi.node.base.Duration;
-import com.hedera.hapi.node.base.FileID;
+import com.hedera.hapi.node.base.Key;
 import com.hedera.hapi.node.base.ResponseCodeEnum;
+import com.hedera.hapi.node.base.TransactionID;
 import com.hedera.hapi.node.base.TransferList;
 import com.hedera.hapi.node.state.common.EntityNumber;
 import com.hedera.hapi.node.state.token.Account;
@@ -42,6 +45,7 @@ import com.hedera.hapi.node.transaction.ExchangeRateSet;
 import com.hedera.hapi.node.transaction.TransactionBody;
 import com.hedera.node.app.ids.EntityIdService;
 import com.hedera.node.app.service.addressbook.ReadableNodeStore;
+import com.hedera.node.app.service.addressbook.impl.schemas.V053AddressBookSchema;
 import com.hedera.node.app.service.file.impl.FileServiceImpl;
 import com.hedera.node.app.service.file.impl.schemas.V0490FileSchema;
 import com.hedera.node.app.service.token.impl.schemas.SyntheticAccountCreator;
@@ -75,6 +79,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -104,6 +110,7 @@ public class SystemSetup {
     private SortedSet<Account> treasuryClones = new TreeSet<>(ACCOUNT_COMPARATOR);
     private SortedSet<Account> blocklistAccounts = new TreeSet<>(ACCOUNT_COMPARATOR);
 
+    private final AtomicInteger nextDispatchNonce = new AtomicInteger(1);
     private final FileServiceImpl fileService;
     private final SyntheticAccountCreator syntheticAccountCreator;
 
@@ -149,42 +156,67 @@ public class SystemSetup {
         // permissions from any upgrade files that are present in the configured directory
         final var filesConfig = config.getConfigData(FilesConfig.class);
         final var adminConfig = config.getConfigData(NetworkAdminConfig.class);
-        final List<AutoSysFileUpdate> autoUpdates = List.of(
-                new AutoSysFileUpdate(
-                        createFileID(filesConfig.feeSchedules(), config),
+        final List<AutoEntityUpdate<Bytes>> autoSysFileUpdates = List.of(
+                new AutoEntityUpdate<>(
+                        (ctx, bytes) ->
+                                dispatchSynthFileUpdate(ctx, createFileID(filesConfig.feeSchedules(), config), bytes),
                         adminConfig.upgradeFeeSchedulesFile(),
                         SystemSetup::parseFeeSchedules),
-                new AutoSysFileUpdate(
-                        createFileID(filesConfig.throttleDefinitions(), config),
+                new AutoEntityUpdate<>(
+                        (ctx, bytes) -> dispatchSynthFileUpdate(
+                                ctx, createFileID(filesConfig.throttleDefinitions(), config), bytes),
                         adminConfig.upgradeThrottlesFile(),
                         SystemSetup::parseThrottles),
-                new AutoSysFileUpdate(
-                        createFileID(filesConfig.networkProperties(), config),
+                new AutoEntityUpdate<>(
+                        (ctx, bytes) -> dispatchSynthFileUpdate(
+                                ctx, createFileID(filesConfig.networkProperties(), config), bytes),
                         adminConfig.upgradePropertyOverridesFile(),
                         in -> parseConfig("override network properties", in)),
-                new AutoSysFileUpdate(
-                        createFileID(filesConfig.hapiPermissions(), config),
+                new AutoEntityUpdate<>(
+                        (ctx, bytes) -> dispatchSynthFileUpdate(
+                                ctx, createFileID(filesConfig.hapiPermissions(), config), bytes),
                         adminConfig.upgradePermissionOverridesFile(),
                         in -> parseConfig("override HAPI permissions", in)));
-        autoUpdates.forEach(update -> {
+        autoSysFileUpdates.forEach(update -> {
             if (update.tryIfPresent(adminConfig.upgradeSysFilesLoc(), systemContext)) {
                 dispatch.stack().commitFullStack();
             }
         });
+        final var autoNodeAdminKeyUpdates = new AutoEntityUpdate<Map<Long, Key>>(
+                (ctx, nodeAdminKeys) -> nodeAdminKeys.forEach(
+                        (nodeId, key) -> ctx.dispatchAdmin(b -> b.nodeUpdate(NodeUpdateTransactionBody.newBuilder()
+                                .nodeId(nodeId)
+                                .adminKey(key)
+                                .build()))),
+                adminConfig.upgradeNodeAdminKeysFile(),
+                SystemSetup::parseNodeAdminKeys);
+        if (autoNodeAdminKeyUpdates.tryIfPresent(adminConfig.upgradeSysFilesLoc(), systemContext)) {
+            dispatch.stack().commitFullStack();
+        }
     }
 
     /**
-     * Encapsulates logic to attempt the automatic update of a system file by parsing the new contents of the
-     * corresponding file from the post-upgrade location.
-     *
-     * @param fileId the file ID of the system file
-     * @param upgradeFileName the name of the upgrade file
-     * @param upgradeFileParser the function to parse the upgrade file
+     * Defines an update based on a new representation of one or more system entities within a context.
+     * @param <T> the type of the update representation
      */
-    private record AutoSysFileUpdate(
-            @NonNull FileID fileId,
-            @NonNull String upgradeFileName,
-            @NonNull Function<InputStream, Bytes> upgradeFileParser) {
+    @FunctionalInterface
+    private interface AutoUpdate<T> {
+        void doUpdate(@NonNull SystemContext systemContext, @NonNull T update);
+    }
+
+    /**
+     * Process object encapsulating the automatic update of a system entity. Attempts to parse a
+     * representation of an update from a given file and then apply it within a system context
+     * using the given {@link AutoUpdate} function.
+     *
+     * @param updateFileName the name of the upgrade file
+     * @param updateParser the function to parse the upgrade file
+     * @param <T> the type of the update representation
+     */
+    private record AutoEntityUpdate<T>(
+            @NonNull AutoUpdate<T> autoUpdate,
+            @NonNull String updateFileName,
+            @NonNull Function<InputStream, T> updateParser) {
         /**
          * Attempts to update the system file using the given system context if the corresponding upgrade file is
          * present at the given location and can be parsed with this update's parser.
@@ -194,31 +226,25 @@ public class SystemSetup {
          * @return {@code true} if a synthetic update was dispatched, {@code false} otherwise
          */
         boolean tryIfPresent(@NonNull final String postUpgradeLoc, @NonNull final SystemContext systemContext) {
-            final var path = Paths.get(postUpgradeLoc, upgradeFileName);
+            final var path = Paths.get(postUpgradeLoc, updateFileName);
             if (!Files.exists(path)) {
                 log.info(
-                        "No post-upgrade file for {} found at {}, not updating",
-                        upgradeFileName,
-                        path.toAbsolutePath());
+                        "No post-upgrade file for {} found at {}, not updating", updateFileName, path.toAbsolutePath());
                 return false;
             }
             try (final var fin = Files.newInputStream(path)) {
-                final Bytes bytes;
+                final T update;
                 try {
-                    bytes = upgradeFileParser.apply(fin);
+                    update = updateParser.apply(fin);
                 } catch (Exception e) {
-                    log.error("Failed to parse upgrade file for {} at {}", upgradeFileName, path.toAbsolutePath(), e);
+                    log.error("Failed to parse update file at {}", path.toAbsolutePath(), e);
                     return false;
                 }
-                log.info(
-                        "Dispatching synthetic update for {} at {} with {} bytes",
-                        upgradeFileName,
-                        path.toAbsolutePath(),
-                        bytes.length());
-                dispatchSynthFileUpdate(systemContext, fileId, bytes);
+                log.info("Dispatching synthetic update based on contents of {}", path.toAbsolutePath());
+                autoUpdate.doUpdate(systemContext, update);
                 return true;
             } catch (IOException e) {
-                log.error("Failed to read upgrade file for {} at {}", upgradeFileName, path.toAbsolutePath(), e);
+                log.error("Failed to read update file at {}", path.toAbsolutePath(), e);
             }
             return false;
         }
@@ -258,12 +284,19 @@ public class SystemSetup {
             }
 
             @Override
-            public void dispatchUpdate(@NonNull final TransactionBody txBody) {
-                requireNonNull(txBody);
-                final var recordBuilder = dispatch.handleContext()
-                        .dispatchPrecedingTransaction(txBody, StreamBuilder.class, key -> true, systemAdminId);
-                if (!SUCCESSES.contains(recordBuilder.status())) {
-                    log.error("Failed to dispatch update transaction {} for - {}", txBody, recordBuilder.status());
+            public void dispatchAdmin(@NonNull final Consumer<TransactionBody.Builder> spec) {
+                final var bodyBuilder = TransactionBody.newBuilder()
+                        .transactionID(TransactionID.newBuilder()
+                                .accountID(systemAdminId)
+                                .transactionValidStart(asTimestamp(now()))
+                                .nonce(nextDispatchNonce.getAndIncrement())
+                                .build());
+                spec.accept(bodyBuilder);
+                final var body = bodyBuilder.build();
+                final var streamBuilder = dispatch.handleContext()
+                        .dispatchPrecedingTransaction(body, StreamBuilder.class, key -> true, systemAdminId);
+                if (!SUCCESSES.contains(streamBuilder.status())) {
+                    log.error("Failed to dispatch admin transaction {} for - {}", body, streamBuilder.status());
                 }
             }
 
@@ -453,6 +486,15 @@ public class SystemSetup {
         try {
             final var content = new String(in.readAllBytes());
             return parseConfigList(purpose, content);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private static Map<Long, Key> parseNodeAdminKeys(@NonNull final InputStream in) {
+        try {
+            final var json = new String(in.readAllBytes());
+            return V053AddressBookSchema.parseEd25519NodeAdminKeys(json);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
