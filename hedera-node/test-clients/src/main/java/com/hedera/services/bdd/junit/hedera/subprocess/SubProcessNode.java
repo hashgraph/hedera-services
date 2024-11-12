60a9b33da073b97d0ac9fded51243456edbf0944
Michael Tinker <michael.tinker@swirldslabs.com>
2024-11-12 10:40:23 -0600

    Stablize CI
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/hedera/subprocess/SubProcessNode.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/junit/hedera/subprocess/SubProcessNode.java
@@ -180,11 +180,14 @@ public class SubProcessNode extends AbstractLocalNode<SubProcessNode> implements
     }
 
     @Override
-    public void dumpThreads() {
+    public boolean dumpThreads() {
         try {
             triggerThreadDump();
+            return true;
         } catch (Exception e) {
-            log.warn("Unable to dump threads for node{}", metadata.nodeId(), e);
+            log.warn("Unable to dump threads for node{}, assuming it was stopped", metadata.nodeId(), e);
+            processHandle = null;
+            return false;
         }
     }
 
