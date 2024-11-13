751c5bbf4ee79e495f4494aa7adfc8837d84570c
Ivan Malygin <ivan@swirldslabs.com>
2024-11-13 10:43:09 -0500

    Added missing dependency
    Signed-off-by: Ivan Malygin <ivan@swirldslabs.com>

--- platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/module-info.java
+++ platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.swirlds.demo.consistency {
     requires com.swirlds.logging;
     requires com.swirlds.platform.core;
     requires com.swirlds.state.api;
+    requires com.swirlds.state.impl;
     requires com.hedera.pbj.runtime;
     requires org.apache.logging.log4j;
     requires static com.github.spotbugs.annotations;
