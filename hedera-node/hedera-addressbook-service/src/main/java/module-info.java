943b8c1d0bf8db13927b1fd2b76b1a7ff12220f4
Iris Simon <iris.simon@swirldslabs.com>
2024-11-18 17:25:28 -0500

    Fixed dependency check.
    Signed-off-by: Iris Simon <iris.simon@swirldslabs.com>

--- hedera-node/hedera-addressbook-service/src/main/java/module-info.java
+++ hedera-node/hedera-addressbook-service/src/main/java/module-info.java
@@ -10,6 +10,5 @@ module com.hedera.node.app.service.addressbook {
     requires com.swirlds.config.api;
     requires org.bouncycastle.pkix;
     requires org.bouncycastle.provider;
-    requires org.checkerframework.checker.qual;
     requires static com.github.spotbugs.annotations;
 }
