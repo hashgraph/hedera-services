d8c1c6c647ed8f3f7b8f8e8730d070e3e85052e4
Mihail Mihov <mihail.mihov@limechain.tech>
2024-11-27 10:16:01 +0200

    ci: Run spotlessApply
    Signed-off-by: Mihail Mihov <mihail.mihov@limechain.tech>

--- platform-sdk/swirlds-merkledb/src/testFixtures/java/module-info.java
+++ platform-sdk/swirlds-merkledb/src/testFixtures/java/module-info.java
@@ -5,8 +5,8 @@ module com.swirlds.merkledb.test.fixtures {
     requires transitive com.swirlds.config.api;
     requires transitive com.swirlds.metrics.api;
     requires transitive com.swirlds.virtualmap;
-    requires transitive org.apache.logging.log4j.core;
     requires transitive com.hedera.pbj.runtime;
+    requires transitive org.apache.logging.log4j.core;
     requires com.swirlds.base;
     requires com.swirlds.config.extensions.test.fixtures;
     requires com.swirlds.merkledb;
