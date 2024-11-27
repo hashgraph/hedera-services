899eef1074477aad7de32814e4b972dfc27fca5f
Derek Riley <derek.riley@swirldslabs.com>
2024-11-27 13:16:53 -0500

    module-info
    Signed-off-by: Derek Riley <derek.riley@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/module-info.java
+++ hedera-node/hedera-app/src/main/java/module-info.java
@@ -2,7 +2,6 @@ import com.hedera.node.app.config.ServicesConfigExtension;
 import com.swirlds.config.api.ConfigurationExtension;
 
 module com.hedera.node.app {
-    uses com.hedera.cryptography.pairings.api.PairingFriendlyCurve;
 
     requires transitive com.hedera.node.app.hapi.utils;
     requires transitive com.hedera.node.app.service.addressbook.impl;
