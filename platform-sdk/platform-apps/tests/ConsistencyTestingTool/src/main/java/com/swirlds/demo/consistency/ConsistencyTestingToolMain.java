519a13e0ddb4dbafd030587adb503884155fd45b
Anthony Petrov <anthony@swirldslabs.com>
2024-12-03 11:55:01 -0800

    spotless
    Signed-off-by: Anthony Petrov <anthony@swirldslabs.com>

--- platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/com/swirlds/demo/consistency/ConsistencyTestingToolMain.java
+++ platform-sdk/platform-apps/tests/ConsistencyTestingTool/src/main/java/com/swirlds/demo/consistency/ConsistencyTestingToolMain.java
@@ -57,8 +57,9 @@ public class ConsistencyTestingToolMain implements SwirldMain {
                     new ClassConstructorPair(ConsistencyTestingToolState.class, () -> {
                         ConsistencyTestingToolState consistencyTestingToolState = new ConsistencyTestingToolState(
                                 FAKE_MERKLE_STATE_LIFECYCLES, version -> new BasicSoftwareVersion(version.major()));
-                        //TODO: remove: The SignedStateFileReader and ReconnectLearner initialize the state stubs already
-                        //FAKE_MERKLE_STATE_LIFECYCLES.initStates(consistencyTestingToolState);
+                        // TODO: remove: The SignedStateFileReader and ReconnectLearner initialize the state stubs
+                        // already
+                        // FAKE_MERKLE_STATE_LIFECYCLES.initStates(consistencyTestingToolState);
                         return consistencyTestingToolState;
                     }));
             registerMerkleStateRootClassIds();
