acb5b039c77f2e16cacb2981d0c58cd37f6bc0e3
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-12-09 12:41:17 +0100

    chore: formatting
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- gradle/aggregation/build.gradle.kts
+++ gradle/aggregation/build.gradle.kts
@@ -1,22 +1,9 @@
-/*
- * Copyright (C) 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
 plugins {
     id("org.hiero.gradle.base.lifecycle")
     id("org.hiero.gradle.report.code-coverage")
+    id("org.hiero.gradle.check.spotless")
+    id("org.hiero.gradle.check.spotless-kotlin")
 }
 
 dependencies {
@@ -35,11 +22,23 @@ dependencies {
 tasks.testCodeCoverageReport {
     // Redo the setup done in 'JacocoReportAggregationPlugin', but treat the class file as file tree
     // and filter out selected classes by path.
-    val filteredClassFiles = configurations.aggregateCodeCoverageReportResults.get().incoming.artifactView {
-        componentFilter { id -> id is ProjectComponentIdentifier }
-        attributes.attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named(LibraryElements.CLASSES))
-    }.files.asFileTree.filter { file ->
-        listOf("test-clients", "testFixtures", "statedumpers", "example-apps").none { file.path.contains(it) }
-    }
+    val filteredClassFiles =
+        configurations.aggregateCodeCoverageReportResults
+            .get()
+            .incoming
+            .artifactView {
+                componentFilter { id -> id is ProjectComponentIdentifier }
+                attributes.attribute(
+                    LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE,
+                    objects.named(LibraryElements.CLASSES)
+                )
+            }
+            .files
+            .asFileTree
+            .filter { file ->
+                listOf("test-clients", "testFixtures", "statedumpers", "example-apps").none {
+                    file.path.contains(it)
+                }
+            }
     classDirectories.setFrom(filteredClassFiles)
 }
