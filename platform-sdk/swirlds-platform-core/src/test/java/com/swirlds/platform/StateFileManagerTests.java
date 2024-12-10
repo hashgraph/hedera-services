d98f7aa3fccf9d59fae63df14be703f05e3f0499
Alex Kuzmin <alex.kuzmin@swirldslabs.com>
2024-12-10 15:54:59 -0800

    Extra line for Spotless check
    Signed-off-by: Alex Kuzmin <alex.kuzmin@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/StateFileManagerTests.java
+++ platform-sdk/swirlds-platform-core/src/test/java/com/swirlds/platform/StateFileManagerTests.java
@@ -84,6 +84,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
 class StateFileManagerTests {
 
     private static final NodeId SELF_ID = NodeId.of(1234);
