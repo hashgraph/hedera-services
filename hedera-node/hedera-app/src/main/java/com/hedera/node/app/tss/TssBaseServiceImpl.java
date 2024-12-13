47516d87bf3d55dfc65d98846ab7263bed69ad89
Petar Tonev <petar.tonev@limechain.tech>
2024-12-13 17:59:55 +0200

    revert order change
    Signed-off-by: Petar Tonev <petar.tonev@limechain.tech>

--- hedera-node/hedera-app/src/main/java/com/hedera/node/app/tss/TssBaseServiceImpl.java
+++ hedera-node/hedera-app/src/main/java/com/hedera/node/app/tss/TssBaseServiceImpl.java
@@ -130,11 +130,6 @@ public class TssBaseServiceImpl implements TssBaseService {
     }
 
     @Override
-    public int migrationOrder() {
-        return MIGRATION_ORDER;
-    }
-
-    @Override
     public void registerSchemas(@NonNull final SchemaRegistry registry) {
         requireNonNull(registry);
         registry.register(new V0560TssBaseSchema());
