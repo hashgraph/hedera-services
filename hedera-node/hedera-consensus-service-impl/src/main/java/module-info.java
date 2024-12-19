3fc13504ecbe9b27df9868eef7e9f0a3584d2b7f
ibankov <ivan.bankov@limechain.tech>
2024-12-19 11:44:27 +0200

    spotless
    Signed-off-by: ibankov <ivan.bankov@limechain.tech>

--- hedera-node/hedera-consensus-service-impl/src/main/java/module-info.java
+++ hedera-node/hedera-consensus-service-impl/src/main/java/module-info.java
@@ -8,9 +8,9 @@ module com.hedera.node.app.service.consensus.impl {
     requires transitive com.hedera.node.app.service.token;
     requires transitive com.hedera.node.app.spi;
     requires transitive com.hedera.node.hapi;
+    requires transitive com.hedera.pbj.runtime;
     requires transitive com.swirlds.config.api;
     requires transitive com.swirlds.state.api;
-    requires transitive com.hedera.pbj.runtime;
     requires transitive dagger;
     requires transitive java.compiler; // javax.annotation.processing.Generated
     requires transitive javax.inject;
