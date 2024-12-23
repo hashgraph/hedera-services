1861d607dcf6ba2f3acf5500259d787c3259cf58
Luke Lee <luke.lee@hashgraph.com>
2024-12-23 12:20:20 -0800

    feat: consolidate hbar transfer list when decoding cryptoTransfer function
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

--- hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/transfer/ClassicTransfersDecoderTest.java
+++ hedera-node/hedera-smart-contract-service-impl/src/test/java/com/hedera/node/app/service/contract/impl/test/exec/systemcontracts/hts/transfer/ClassicTransfersDecoderTest.java
@@ -16,7 +16,13 @@
 
 package com.hedera.node.app.service.contract.impl.test.exec.systemcontracts.hts.transfer;
 
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.FUNGIBLE_TOKEN_HEADLONG_ADDRESS;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.tokenTransferList;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.tokenTransferLists;
+import static com.hedera.node.app.service.contract.impl.test.TestHelpers.transferList;
+
 import com.esaulpaugh.headlong.abi.Address;
+import com.esaulpaugh.headlong.abi.Tuple;
 import com.hedera.hapi.node.base.AccountAmount;
 import com.hedera.hapi.node.base.AccountID;
 import com.hedera.hapi.node.transaction.TransactionBody;
@@ -35,11 +41,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class ClassicTransfersDecoderTest {
+
     public static final int ACCOUNT_ID_41 = 41;
     public static final int ACCOUNT_ID_42 = 42;
     private static final Address ACCT_ADDR_1 = addressFromNum(ACCOUNT_ID_41);
     private static final Address ACCT_ADDR_2 = addressFromNum(ACCOUNT_ID_42);
     private static final Address TOKEN_ADDR_10 = addressFromNum(10);
+    private static final Tuple[] EMPTY_TUPLE_ARRAY = new Tuple[] {};
 
     @Mock
     private AddressIdConverter converter;
@@ -93,10 +101,72 @@ class ClassicTransfersDecoderTest {
         Assertions.assertThat(secondEntry.amount()).isEqualTo(-totalToTransfer);
     }
 
+    @Test
+    void decodeCryptoTransferConsolidates() {
+        final var totalToTransfer = 25L;
+        BDDMockito.given(converter.convert(ACCT_ADDR_1))
+                .willReturn(AccountID.newBuilder().accountNum(ACCOUNT_ID_41).build());
+        BDDMockito.given(converter.convertCredit(ACCT_ADDR_1))
+                .willReturn(AccountID.newBuilder().accountNum(ACCOUNT_ID_41).build());
+        final var encodedInput = ClassicTransfersTranslator.CRYPTO_TRANSFER_V2.encodeCallWithArgs(
+                transferList()
+                        .withAccountAmounts(
+                                Tuple.of(ACCT_ADDR_1, -totalToTransfer, false),
+                                Tuple.of(ACCT_ADDR_1, totalToTransfer, false))
+                        .build(),
+                EMPTY_TUPLE_ARRAY);
+        final var result = subject.decodeCryptoTransferV2(encodedInput.array(), converter);
+        Assertions.assertThat(unwrapTransferAmounts(result)).hasSize(1);
+        Assertions.assertThat(unwrapTransferAmounts(result).get(0).amount()).isEqualTo(0);
+    }
+
+    @Test
+    void decodeCryptoTransferOverflow() {
+        BDDMockito.given(converter.convertCredit(ACCT_ADDR_1))
+                .willReturn(AccountID.newBuilder().accountNum(ACCOUNT_ID_41).build());
+        final var encodedInput = ClassicTransfersTranslator.CRYPTO_TRANSFER_V2.encodeCallWithArgs(
+                transferList()
+                        .withAccountAmounts(
+                                Tuple.of(ACCT_ADDR_1, Long.MAX_VALUE - 1, false), Tuple.of(ACCT_ADDR_1, 2L, false))
+                        .build(),
+                EMPTY_TUPLE_ARRAY);
+        Assertions.assertThatThrownBy(() -> subject.decodeCryptoTransferV2(encodedInput.array(), converter))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount overflow when merging account amounts");
+    }
+
+    @Test
+    void decodeCryptoTokenTransferOverflow() {
+        BDDMockito.given(converter.convertCredit(ACCT_ADDR_1))
+                .willReturn(AccountID.newBuilder().accountNum(ACCOUNT_ID_41).build());
+        final var encodedInput = ClassicTransfersTranslator.CRYPTO_TRANSFER_V2.encodeCallWithArgs(
+                transferList().withAccountAmounts().build(),
+                tokenTransferLists()
+                        .withTokenTransferList(
+                                tokenTransferList()
+                                        .forToken(FUNGIBLE_TOKEN_HEADLONG_ADDRESS)
+                                        .withAccountAmounts(Tuple.of(ACCT_ADDR_1, Long.MAX_VALUE - 1, false))
+                                        .build(),
+                                tokenTransferList()
+                                        .forToken(FUNGIBLE_TOKEN_HEADLONG_ADDRESS)
+                                        .withAccountAmounts(Tuple.of(ACCT_ADDR_1, 2L, false))
+                                        .build())
+                        .build());
+        Assertions.assertThatThrownBy(() -> subject.decodeCryptoTransferV2(encodedInput.array(), converter))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Amount overflow when merging account amounts");
+    }
+
     private static List<AccountAmount> unwrapTokenAmounts(final TransactionBody body) {
         final var tokenTransfers = body.cryptoTransfer().tokenTransfers();
         // We shouldn't ever have more than one token transfer list
         return tokenTransfers.getFirst().transfers();
+    }
+
+    private static List<AccountAmount> unwrapTransferAmounts(final TransactionBody body) {
+        final var transfers = body.cryptoTransfer().transfers();
+        // We shouldn't ever have more than one transfer list
+        return transfers.accountAmounts();
     }
 
     private static Address addressFromNum(final long accountId) {
