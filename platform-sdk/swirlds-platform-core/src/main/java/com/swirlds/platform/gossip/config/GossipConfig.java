4af4a1c0c8e6ea809d072e4b282746837bfdeebc
Timo Brandstätter <timo@swirldslabs.com>
2024-12-30 18:02:39 +0100

    renaming
    Signed-off-by: Timo Brandstätter <timo@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/gossip/config/GossipConfig.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/gossip/config/GossipConfig.java
@@ -19,16 +19,35 @@ package com.swirlds.platform.gossip.config;
 import com.swirlds.config.api.ConfigData;
 import com.swirlds.config.api.ConfigProperty;
 import com.swirlds.config.api.Configuration;
+import com.swirlds.platform.network.connectivity.OutboundConnectionCreator;
+import com.swirlds.platform.network.connectivity.SocketFactory;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Configuration for the gossip layer of the node.
+ *
+ * @param interfaceBindings A list of interface bindings used in {@link SocketFactory}.
+ *                          These bindings allow overriding the default network behavior
+ *                          in specialized environments, such as containerized
+ *                          deployments, where custom network interfaces may be required.
+ *                          Each entry specifies how the node should bind to its network
+ *                          interfaces.
+ *
+ * @param endpointOverrides A list of endpoint overrides used in {@link OutboundConnectionCreator}.
+ *                          These overrides provide the ability to replace the default IP
+ *                          address and port of endpoints obtained from the roster. This is
+ *                          particularly useful in cases where the actual network configuration
+ *                          differs from the information specified in the roster, such as
+ *                          behind NATs or when using virtualized networks.
+ */
 @ConfigData("gossip")
 public record GossipConfig(
         @ConfigProperty(defaultValue = Configuration.EMPTY_LIST) List<NetworkEndpoint> interfaceBindings,
         @ConfigProperty(defaultValue = Configuration.EMPTY_LIST) List<NetworkEndpoint> endpointOverrides) {
 
     /**
-     * Returns the network endpoint for the given node ID.
+     * Returns the interface binding for the given node ID.
      * <p>
      *     <b>Note:</b> If there are multiple interface bindings for the same node ID, only the first one will be
      *     returned.
