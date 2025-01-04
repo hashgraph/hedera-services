c86700b274021717c64fa9d521765540800d7ea5
Michael Tinker <michael.tinker@swirldslabs.com>
2025-01-04 14:51:27 -0600

    Delete T1 KeyLoader code
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/crypto/EnhancedKeyStoreLoader.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/crypto/EnhancedKeyStoreLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2024-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,10 +25,6 @@ import static com.swirlds.platform.crypto.CryptoStatic.createEmptyTrustStore;
 import static com.swirlds.platform.crypto.CryptoStatic.loadKeys;
 import static com.swirlds.platform.state.address.AddressBookNetworkUtils.isLocal;
 
-import com.hedera.cryptography.asciiarmored.AsciiArmoredFiles;
-import com.hedera.cryptography.bls.BlsKeyPair;
-import com.hedera.cryptography.bls.BlsPrivateKey;
-import com.hedera.cryptography.bls.BlsPublicKey;
 import com.swirlds.common.crypto.config.CryptoConfig;
 import com.swirlds.common.platform.NodeId;
 import com.swirlds.config.api.Configuration;
@@ -209,16 +205,6 @@ public class EnhancedKeyStoreLoader {
     private final Map<NodeId, PrivateKey> agrPrivateKeys;
 
     /**
-     * The private tss encryption keys loaded from disk.
-     */
-    private final Map<NodeId, BlsPrivateKey> tssPrivateKeys;
-
-    /**
-     * The public tss encryption keys.
-     */
-    private final Map<NodeId, BlsPublicKey> tssPublicKeys;
-
-    /**
      * The X.509 Certificates loaded from the key stores.
      */
     private final Map<NodeId, Certificate> agrCertificates;
@@ -258,8 +244,6 @@ public class EnhancedKeyStoreLoader {
         this.agrPrivateKeys = HashMap.newHashMap(addressBook.getSize());
         this.agrCertificates = HashMap.newHashMap(addressBook.getSize());
         this.localNodes = HashSet.newHashSet(addressBook.getSize());
-        this.tssPrivateKeys = HashMap.newHashMap(addressBook.getSize());
-        this.tssPublicKeys = HashMap.newHashMap(addressBook.getSize());
     }
 
     /**
@@ -314,11 +298,6 @@ public class EnhancedKeyStoreLoader {
                 localNodes.add(nodeId);
                 sigPrivateKeys.compute(
                         nodeId, (k, v) -> resolveNodePrivateKey(nodeId, nodeAlias, KeyCertPurpose.SIGNING));
-
-                BlsPrivateKey tssPrivateKey = resolveTssPrivateKey(nodeId, nodeAlias);
-                if (tssPrivateKey != null) {
-                    tssPrivateKeys.put(nodeId, tssPrivateKey);
-                }
             }
 
             sigCertificates.compute(
@@ -328,32 +307,6 @@ public class EnhancedKeyStoreLoader {
 
         logger.trace(STARTUP.getMarker(), "Completed key store enumeration");
         return this;
-    }
-
-    @Nullable
-    private BlsPrivateKey resolveTssPrivateKey(@NonNull final NodeId nodeId, @NonNull final String nodeAlias)
-            throws KeyLoadingException {
-        Objects.requireNonNull(nodeId, MSG_NODE_ID_NON_NULL);
-        Objects.requireNonNull(nodeAlias, MSG_NODE_ALIAS_NON_NULL);
-
-        Path keyLocation = keyStoreDirectory.resolve(String.format("t-private-%s.tss", nodeAlias));
-        if (Files.exists(keyLocation)) {
-            logger.trace(
-                    STARTUP.getMarker(),
-                    "Found tss encryption private key for node {} [ fileName = {} ]",
-                    nodeId,
-                    keyLocation.getFileName());
-            try {
-                return AsciiArmoredFiles.readPrivateKey(keyLocation);
-            } catch (final Exception e) {
-                logger.warn(
-                        STARTUP.getMarker(),
-                        "Failed to read TSS encryption private key from disk, will generate a new key.",
-                        e);
-                return null;
-            }
-        }
-        return null;
     }
 
     /**
@@ -395,28 +348,6 @@ public class EnhancedKeyStoreLoader {
                         signingKeyPair,
                         SecureRandom.getInstanceStrong());
                 agrCertificates.put(node, agrCert);
-            }
-
-            if (!tssPrivateKeys.containsKey(node) || tssPrivateKeys.get(node) == null) {
-                // Create a new public/private key pair for the TSS encryption key
-                BlsKeyPair tssKeyPair = CryptoStatic.generateBlsKeyPair(SecureRandom.getInstanceStrong());
-                tssPrivateKeys.put(node, tssKeyPair.privateKey());
-                tssPublicKeys.put(node, tssKeyPair.publicKey());
-                // Write the private key to disk
-                final String nodeAlias =
-                        nameToAlias(addressBook.getAddress(node).getSelfName());
-                Path tssEncryptionPrivateKeyLocation =
-                        keyStoreDirectory.resolve(String.format("t-private-%s.tss", nodeAlias));
-                try {
-                    AsciiArmoredFiles.writeKey(tssEncryptionPrivateKeyLocation, tssKeyPair.privateKey());
-                } catch (IOException e) {
-                    logger.error(ERROR.getMarker(), "Failed to write TSS encryption private key to disk", e);
-                    throw new KeyGeneratingException("Failed to write TSS encryption private key to disk", e);
-                }
-            } else {
-                // Create the BlsPublicKey from the private TSS encryption key
-                BlsPrivateKey tssPrivateKey = tssPrivateKeys.get(node);
-                tssPublicKeys.put(node, tssPrivateKey.createPublicKey());
             }
         }
         return this;
@@ -474,16 +405,6 @@ public class EnhancedKeyStoreLoader {
                 if (!agrCertificates.containsKey(nodeId)) {
                     throw new KeyLoadingException("No certificate found for node %s [ alias = %s, purpose = %s ]"
                             .formatted(nodeId, nodeAlias, KeyCertPurpose.AGREEMENT));
-                }
-
-                if (!tssPrivateKeys.containsKey(nodeId)) {
-                    throw new KeyLoadingException(
-                            "No TSS private key found for node %s [ alias = %s ]".formatted(nodeId, nodeAlias));
-                }
-
-                if (!tssPublicKeys.containsKey(nodeId)) {
-                    throw new KeyLoadingException(
-                            "No TSS public key found for node %s [ alias = %s ]".formatted(nodeId, nodeAlias));
                 }
             }
 
@@ -559,12 +480,7 @@ public class EnhancedKeyStoreLoader {
 
                 final KeyPair sigKeyPair = new KeyPair(sigCert.getPublicKey(), sigPrivateKey);
                 final KeyPair agrKeyPair = new KeyPair(agrCert.getPublicKey(), agrPrivateKey);
-
-                final BlsPrivateKey tssPrivateKey = tssPrivateKeys.get(nodeId);
-                final BlsPublicKey tssPublicKey = tssPublicKeys.get(nodeId);
-
-                final KeysAndCerts kc = new KeysAndCerts(
-                        sigKeyPair, agrKeyPair, sigCert, agrCert, publicStores, tssPrivateKey, tssPublicKey);
+                final KeysAndCerts kc = new KeysAndCerts(sigKeyPair, agrKeyPair, sigCert, agrCert, publicStores);
 
                 keysAndCerts.put(nodeId, kc);
             }
