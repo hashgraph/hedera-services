358bbbaa6c0806eb64005ecdd69f1a956e08da9c
Roger Barker <roger.barker@swirldslabs.com>
2025-01-06 10:04:20 -0600

    chore: Cherry pick fixes for update of default branch to main (#17200)
    Signed-off-by: Roger Barker <roger.barker@swirldslabs.com>
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Co-authored-by: Jendrik Johannes <jendrik.johannes@gmail.com>
    Co-authored-by: Andrew Brandt <andrew.brandt@hashgraph.com>
    Co-authored-by: Thomas Moran <152873392+thomas-swirlds-labs@users.noreply.github.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/cli/JrsTestReaderReportCommand.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/cli/JrsTestReaderReportCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2023-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -185,9 +185,9 @@ public class JrsTestReaderReportCommand extends AbstractCommand {
 
     @Override
     public Integer call() {
-        // if no targets were specified, then default to the develop branch
+        // if no targets were specified, then default to the main branch
         if (targets == null) {
-            targets = List.of("develop");
+            targets = List.of("main");
         }
 
         final Map<JrsTestIdentifier, JrsTestMetadata> metadata = JtrUtils.getTestMetadata(metadataFile);
