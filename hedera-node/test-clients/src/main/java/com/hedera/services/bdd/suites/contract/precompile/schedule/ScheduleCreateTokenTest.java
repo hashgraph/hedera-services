5a5b7c63450caf5010dd4465e372b36f861d2f33
lukelee-sl <109538178+lukelee-sl@users.noreply.github.com>
2025-01-09 12:58:18 -0800

    feat: implement `signSchedule(address, bytes)` HSS function  (#17260)
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/schedule/ScheduleCreateTokenTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/schedule/ScheduleCreateTokenTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2024-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,11 +19,14 @@ package com.hedera.services.bdd.suites.contract.precompile.schedule;
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
 import static com.hedera.services.bdd.spec.keys.KeyShape.CONTRACT;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getScheduleInfo;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCallWithFunctionAbi;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.scheduleSign;
 import static com.hedera.services.bdd.spec.utilops.CustomSpecAssert.allRunFor;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.newKeyNamed;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.overriding;
 import static com.hedera.services.bdd.spec.utilops.UtilVerbs.withOpContext;
+import static com.hedera.services.bdd.suites.HapiSuite.ONE_HUNDRED_HBARS;
+import static com.hedera.services.bdd.suites.contract.Utils.getABIFor;
 
 import com.esaulpaugh.headlong.abi.Address;
 import com.hedera.node.app.service.contract.impl.utils.ConversionUtils;
@@ -51,6 +54,8 @@ import org.junit.jupiter.api.Order;
 public class ScheduleCreateTokenTest {
 
     private static final String CONTRACT_KEY = "contractKey";
+    private static final String SIGN_SCHEDULE = "signSchedule";
+    private static final String IHRC755 = "IHRC755";
 
     @Contract(contract = "HIP756Contract", creationGas = 4_000_000L, isImmutable = true)
     static SpecContract contract;
@@ -60,6 +65,12 @@ public class ScheduleCreateTokenTest {
 
     @Account
     static SpecAccount autoRenew;
+
+    @Account(tinybarBalance = ONE_HUNDRED_HBARS)
+    static SpecAccount treasury2;
+
+    @Account(tinybarBalance = ONE_HUNDRED_HBARS)
+    static SpecAccount autoRenew2;
 
     @Account
     static SpecAccount designatedPayer;
@@ -194,6 +205,24 @@ public class ScheduleCreateTokenTest {
         }));
     }
 
+    @HapiTest
+    @Order(6)
+    @DisplayName("Can successfully schedule a create fungible token operation and sign with EOA via proxy contract")
+    public Stream<DynamicTest> scheduledCreateTokenSignWithEOA() {
+        return hapiTest(withOpContext((spec, opLog) -> {
+            final var scheduleAddress = new AtomicReference<Address>();
+            allRunFor(
+                    spec,
+                    contract.call("scheduleCreateFT", autoRenew2, treasury2)
+                            .gas(1_000_000L)
+                            .exposingResultTo(res -> scheduleAddress.set((Address) res[1])));
+            final var scheduleID = ConversionUtils.asScheduleId(scheduleAddress.get());
+            final var scheduleNum = "0.0." + scheduleID.getScheduleNum();
+            spec.registry().saveScheduleId("scheduledCreateFTWithEOA", scheduleID);
+            assertScheduleAndSignWithEOA(spec, "scheduledCreateFTWithEOA", scheduleNum);
+        }));
+    }
+
     private static void assertScheduleAndSign(@NonNull final HapiSpec spec, @NonNull final String scheduleID) {
         allRunFor(
                 spec,
@@ -204,5 +233,32 @@ public class ScheduleCreateTokenTest {
                 getScheduleInfo(scheduleID)
                         .isExecuted()
                         .hasSignatories(CONTRACT_KEY, autoRenew.name(), treasury.name()));
+    }
+
+    private static void assertScheduleAndSignWithEOA(
+            @NonNull final HapiSpec spec, @NonNull final String scheduleID, @NonNull final String scheduleNum) {
+        allRunFor(
+                spec,
+                getScheduleInfo(scheduleID).hasScheduleId(scheduleID).isNotExecuted(),
+                contractCallWithFunctionAbi(
+                                scheduleNum,
+                                getABIFor(
+                                        com.hedera.services.bdd.suites.contract.Utils.FunctionType.FUNCTION,
+                                        SIGN_SCHEDULE,
+                                        IHRC755))
+                        .payingWith(treasury2.name())
+                        .gas(1_000_000),
+                getScheduleInfo(scheduleID).isNotExecuted().hasSignatories(CONTRACT_KEY, treasury2.name()),
+                contractCallWithFunctionAbi(
+                                scheduleNum,
+                                getABIFor(
+                                        com.hedera.services.bdd.suites.contract.Utils.FunctionType.FUNCTION,
+                                        SIGN_SCHEDULE,
+                                        IHRC755))
+                        .payingWith(autoRenew2.name())
+                        .gas(1_000_000),
+                getScheduleInfo(scheduleID)
+                        .isExecuted()
+                        .hasSignatories(CONTRACT_KEY, autoRenew2.name(), treasury2.name()));
     }
 }
