f4599bc3d927274a0e96fb9daf7851e2b80a6f24
Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>
2025-01-13 14:25:10 +0200

    test: refactor unit tests
    Signed-off-by: Miroslav Gatsanoga <miroslav.gatsanoga@limechain.tech>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/BlockStreamBucketUploaderMetricsTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/blocks/BlockStreamBucketUploaderMetricsTest.java
@@ -17,84 +17,120 @@
 package com.hedera.node.app.blocks;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import com.hedera.node.app.spi.fixtures.util.LogCaptor;
 import com.hedera.node.app.spi.fixtures.util.LogCaptureExtension;
 import com.hedera.node.app.spi.fixtures.util.LoggingSubject;
 import com.hedera.node.app.spi.fixtures.util.LoggingTarget;
-import com.swirlds.metrics.api.LongGauge;
+import com.hedera.node.app.utils.TestUtils;
+import com.swirlds.metrics.api.Metric;
 import com.swirlds.metrics.api.Metrics;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith({MockitoExtension.class, LogCaptureExtension.class})
 class BlockStreamBucketUploaderMetricsTest {
-    @Mock
-    private Metrics metrics;
-
-    @Mock
-    private LongGauge longGauge;
-
     @LoggingSubject
     private BlockStreamBucketUploaderMetrics blockStreamBucketUploaderMetrics;
 
     @LoggingTarget
     private LogCaptor logCaptor;
 
+    private Metrics metrics;
+    private static final long SELF_NODE_ID = 1L;
+
     @BeforeEach
     void setUp() {
-        when(metrics.getOrCreate(any(LongGauge.Config.class))).thenReturn(longGauge);
-        long selfNodeId = 1L;
-        blockStreamBucketUploaderMetrics = new BlockStreamBucketUploaderMetrics(metrics, selfNodeId);
+        metrics = TestUtils.metrics();
+        blockStreamBucketUploaderMetrics = new BlockStreamBucketUploaderMetrics(metrics, SELF_NODE_ID);
     }
 
     @Test
     void testUpdateBlocksRetainedCount() {
         blockStreamBucketUploaderMetrics.updateBlocksRetainedCount(10L);
-        verify(longGauge).set(10L);
+
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_RETAINED);
+        assertEquals(10L, metric.get(Metric.ValueType.VALUE));
     }
 
     @Test
     void testUpdateBlocksRetainedCountWithNegativeValue() {
         blockStreamBucketUploaderMetrics.updateBlocksRetainedCount(-1L);
 
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_RETAINED);
+        assertEquals(0L, metric.get(Metric.ValueType.VALUE));
         assertThat(logCaptor.warnLogs()).contains("Received number of retained blocks: -1");
-        verify(longGauge, never()).set(anyLong());
     }
 
     @Test
     void testUpdateBlocksUploadedCount() {
         blockStreamBucketUploaderMetrics.updateBlocksUploadedCount(10L);
-        verify(longGauge).set(10L);
+
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_UPLOADED);
+        assertEquals(10L, metric.get(Metric.ValueType.VALUE));
     }
 
     @Test
     void testUpdateBlocksUploadedCountWithNegativeValue() {
         blockStreamBucketUploaderMetrics.updateBlocksUploadedCount(-1L);
 
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_UPLOADED);
+        assertEquals(0L, metric.get(Metric.ValueType.VALUE));
         assertThat(logCaptor.warnLogs()).contains("Received number of uploaded blocks: -1");
-        verify(longGauge, never()).set(anyLong());
     }
 
     @Test
     void testUpdateBlocksHashMismatchCount() {
         blockStreamBucketUploaderMetrics.updateBlocksHashMismatchCount(10L);
-        verify(longGauge).set(10L);
+
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_HASH_MISMATCH);
+        assertEquals(10L, metric.get(Metric.ValueType.VALUE));
     }
 
     @Test
     void testUpdateBlockHashMismatchCountWithNegativeValue() {
         blockStreamBucketUploaderMetrics.updateBlocksHashMismatchCount(-1L);
 
+        final var metricCategory = String.format(BlockStreamBucketUploaderMetrics.PER_NODE_METRIC_PREFIX, SELF_NODE_ID);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.BLOCKS_HASH_MISMATCH);
+        assertEquals(0L, metric.get(Metric.ValueType.VALUE));
         assertThat(logCaptor.warnLogs()).contains("Received number of hash mismatched blocks: -1");
-        verify(longGauge, never()).set(anyLong());
+    }
+
+    @Test
+    void testIncrementSuccessfulUploads() {
+        final var providers = List.of("aws", "gcp");
+        for (final var provider : providers) {
+            blockStreamBucketUploaderMetrics.incrementSuccessfulUploads(provider);
+        }
+
+        for (final var provider : providers) {
+            final var metricCategory = String.format(
+                    BlockStreamBucketUploaderMetrics.PER_PROVIDER_PER_NODE_METRIC_PREFIX, SELF_NODE_ID, provider);
+            final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.UPLOADS_SUCCESS);
+            assertEquals(1L, metric.get(Metric.ValueType.VALUE));
+        }
+    }
+
+    @Test
+    void testIncrementSuccessfulUploadsWithInvalidProvider() {
+        final var invalidProvider = "invalid";
+        blockStreamBucketUploaderMetrics.incrementSuccessfulUploads(invalidProvider);
+
+        final var metricCategory = String.format(
+                BlockStreamBucketUploaderMetrics.PER_PROVIDER_PER_NODE_METRIC_PREFIX, SELF_NODE_ID, invalidProvider);
+        final var metric = metrics.getMetric(metricCategory, BlockStreamBucketUploaderMetrics.UPLOADS_SUCCESS);
+        assertNull(metric);
+        assertThat(logCaptor.warnLogs()).contains("Bucket provider " + invalidProvider + " not found");
     }
 }
