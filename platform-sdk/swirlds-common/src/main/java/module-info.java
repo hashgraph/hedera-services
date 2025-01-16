6682c10f0f73a50a87d6e13f9c0cca8983e87fb2
Lazar Petrovic <lpetrovic05@gmail.com>
2025-01-16 12:05:25 +0100

    chore: remove static configuration (#17391)
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-common/src/main/java/module-info.java
+++ platform-sdk/swirlds-common/src/main/java/module-info.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright (C) 2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module com.swirlds.common {
 
     /* Exported packages. This list should remain alphabetized. */
     exports com.swirlds.common;
     exports com.swirlds.common.config;
-    exports com.swirlds.common.config.singleton;
     exports com.swirlds.common.constructable;
     exports com.swirlds.common.constructable.internal;
     exports com.swirlds.common.context;
@@ -144,6 +159,7 @@ module com.swirlds.common {
     exports com.swirlds.common.concurrent;
     exports com.swirlds.common.merkle.synchronization.stats;
 
+    requires transitive com.hedera.pbj.runtime;
     requires transitive com.swirlds.base;
     requires transitive com.swirlds.config.api;
     requires transitive com.swirlds.logging;
@@ -151,7 +167,6 @@ module com.swirlds.common {
     requires transitive com.swirlds.metrics.impl;
     requires transitive com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.databind;
-    requires transitive com.hedera.pbj.runtime;
     requires transitive io.prometheus.simpleclient;
     requires transitive lazysodium.java;
     requires transitive org.apache.logging.log4j;
