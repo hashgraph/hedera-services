196be1f44331ca492bb26277b80646bb275146bf
Matt Hess <matt.hess@swirldslabs.com>
2025-01-16 23:52:51 -0700

    Fix module directives scope
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/module-info.java
+++ hedera-node/test-clients/src/main/java/module-info.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module com.hedera.node.test.clients {
     exports com.hedera.services.bdd.spec.dsl;
     exports com.hedera.services.bdd.spec.dsl.contracts;
@@ -82,6 +98,7 @@ module com.hedera.node.test.clients {
     requires com.swirlds.base;
     requires com.swirlds.common;
     requires com.swirlds.config.api;
+    requires com.swirlds.config.extensions;
     requires com.swirlds.merkledb;
     requires com.swirlds.metrics.api;
     requires com.swirlds.platform.core.test.fixtures;
