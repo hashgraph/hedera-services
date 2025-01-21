37ba94d7360636e26b50d5d4e485279b27fd9965
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2025-01-21 14:15:40 +0200

    feat: add shard and realm to asAccount
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/staking/EndOfStakingPeriodUpdater.java
+++ hedera-node/hedera-token-service-impl/src/main/java/com/hedera/node/app/service/token/impl/handlers/staking/EndOfStakingPeriodUpdater.java
@@ -42,6 +42,7 @@ import com.hedera.node.app.service.token.records.TokenContext;
 import com.hedera.node.app.spi.workflows.record.StreamBuilder;
 import com.hedera.node.config.ConfigProvider;
 import com.hedera.node.config.data.AccountsConfig;
+import com.hedera.node.config.data.HederaConfig;
 import com.hedera.node.config.data.StakingConfig;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
@@ -72,6 +73,7 @@ public class EndOfStakingPeriodUpdater {
 
     private final AccountsConfig accountsConfig;
     private final StakingRewardsHelper stakeRewardsHelper;
+    private final HederaConfig hederaConfig;
 
     /**
      * Constructs an {@link EndOfStakingPeriodUpdater} instance.
@@ -84,6 +86,7 @@ public class EndOfStakingPeriodUpdater {
         this.stakeRewardsHelper = stakeRewardsHelper;
         final var config = configProvider.getConfiguration();
         this.accountsConfig = config.getConfigData(AccountsConfig.class);
+        this.hederaConfig = config.getConfigData(HederaConfig.class);
     }
 
     /**
@@ -415,7 +418,8 @@ public class EndOfStakingPeriodUpdater {
     }
 
     private long getRewardsBalance(@NonNull final ReadableAccountStore accountStore) {
-        return requireNonNull(accountStore.getAccountById(asAccount(accountsConfig.stakingRewardAccount())))
+        return requireNonNull(accountStore.getAccountById(
+                        asAccount(hederaConfig.shard(), hederaConfig.realm(), accountsConfig.stakingRewardAccount())))
                 .tinybarBalance();
     }
 }
