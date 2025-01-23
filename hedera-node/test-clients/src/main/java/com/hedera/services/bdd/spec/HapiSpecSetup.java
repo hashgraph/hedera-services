622f8289e77c4f902a5bdffee44053be4fb91a76
Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>
2025-01-23 11:23:33 +0200

    refactor: delete not used methods
    Signed-off-by: Valentin Tronkov <99957253+vtronkov@users.noreply.github.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/HapiSpecSetup.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/HapiSpecSetup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2020-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -503,11 +503,11 @@ public class HapiSpecSetup {
     }
 
     public AccountID nodeRewardAccount() {
-        return asAccount(props.get("default.shard"),props.get("default.realm"),"801");
+        return asAccount(props.get("default.shard"), props.get("default.realm"), "801");
     }
 
     public AccountID stakingRewardAccount() {
-        return asAccount(props.get("default.shard"),props.get("default.realm"),"800");
+        return asAccount(props.get("default.shard"), props.get("default.realm"), "800");
     }
 
     public AccountID feeCollectorAccount() {
--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/queries/crypto/HapiGetAccountInfo.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/queries/crypto/HapiGetAccountInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2020-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -392,6 +392,6 @@ public class HapiGetAccountInfo extends HapiQueryOp<HapiGetAccountInfo> {
     }
 
     private String toEntityId(AccountID accountID) {
-        return accountID.getShardNum()+"."+accountID.getRealmNum()+"."+accountID.getAccountNum();
+        return accountID.getShardNum() + "." + accountID.getRealmNum() + "." + accountID.getAccountNum();
     }
 }
