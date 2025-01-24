c3cfc872b6f91e1069e6e030f10ed3525be0dd8f
Matt Hess <matt.hess@swirldslabs.com>
2025-01-24 13:40:07 -0700

    chore: Reapply "FileService address book and node details" (#17543)
    Signed-off-by: Matt Hess <matt.hess@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/grouping/GroupingVerbs.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/spec/utilops/grouping/GroupingVerbs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2024-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,5 +43,20 @@ public class GroupingVerbs {
      */
     public static SysFileLookups getSystemFiles(@NonNull final Consumer<Map<FileID, Bytes>> observer) {
         return new SysFileLookups(fileNum -> true, observer);
+    }
+
+    /**
+     * Returns a utility operation to retrieve the contents of specific system files and pass them to an observer.
+     *
+     * @param sysfileNub the system file number
+     * @param observer the consumer of the system file contents
+     * @return the utility operation
+     */
+    public static SysFileLookups getSystemFiles(final long sysfileNub, @NonNull final Consumer<Bytes> observer) {
+        final Consumer<Map<FileID, Bytes>> temp = map -> {
+            final Bytes contents = map.get(new FileID(0, 0, sysfileNub));
+            observer.accept(contents);
+        };
+        return new SysFileLookups(fileNum -> fileNum == sysfileNub, temp);
     }
 }
