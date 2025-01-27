1691261c6b00417ac482c1f4ba8cf7335efd681d
Oleg Mazurov <oleg.mazurov@swirldslabs.com>
2025-01-27 10:36:05 -0800

    fixed failedRehash() unit test
    Signed-off-by: Oleg Mazurov <oleg.mazurov@swirldslabs.com>

--- platform-sdk/swirlds-merkle/src/timingSensitive/java/com/swirlds/merkle/test/MerkleRehashTests.java
+++ platform-sdk/swirlds-merkle/src/timingSensitive/java/com/swirlds/merkle/test/MerkleRehashTests.java
@@ -160,8 +160,10 @@ class MerkleRehashTests {
     @DisplayName("Failed Rehash Behavior")
     public void failedRehash() {
 
-        DummyMerkleNode root = spy(generateRandomTree(0, 2, 1, 1, 0, 3, 1, 0.25));
-        when(root.getHash()).then(new Answer<Hash>() {
+        DummyMerkleNode root = generateRandomTree(0, 2, 1, 1, 0, 3, 1, 0.25);
+        MerkleNode child = spy(root.asInternal().getChild(0).copy());
+        root.asInternal().setChild(0, child);
+        when(child.getHash()).then(new Answer<Hash>() {
             private int count = 0;
 
             @Override
