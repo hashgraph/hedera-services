7b9cb9de7c810f92655adbdc18fdf90fad38b208
Lazar Petrovic <lpetrovic05@gmail.com>
2025-01-29 11:29:56 +0100

    s
    Signed-off-by: Lazar Petrovic <lpetrovic05@gmail.com>

--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/consensus/ConsensusRounds.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/consensus/ConsensusRounds.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023-2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2023-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,13 +52,10 @@ public class ConsensusRounds {
     private long minGenNonAncient = EventConstants.FIRST_GENERATION;
 
     /** Constructs an empty object */
-    public ConsensusRounds(
-            @NonNull final ConsensusConfig config,
-            @NonNull final Roster roster) {
+    public ConsensusRounds(@NonNull final ConsensusConfig config, @NonNull final Roster roster) {
         this.config = Objects.requireNonNull(config);
-        this.minimumJudgeStorage = new SequentialRingBuffer<>(
-                ConsensusConstants.ROUND_FIRST,
-                config.roundsExpired() * 2);
+        this.minimumJudgeStorage =
+                new SequentialRingBuffer<>(ConsensusConstants.ROUND_FIRST, config.roundsExpired() * 2);
         this.rosterEntryMap = RosterUtils.toMap(Objects.requireNonNull(roster));
         reset();
     }
