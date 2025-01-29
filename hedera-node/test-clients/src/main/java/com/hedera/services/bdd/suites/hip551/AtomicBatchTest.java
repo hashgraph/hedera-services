6262f36e676f4a529f23ae87fe855caf71ca1134
Zhivko Kelchev <zhivko.kelchev@limechain.tech>
2025-01-29 12:32:53 +0200

    Add tests
    Signed-off-by: Zhivko Kelchev <zhivko.kelchev@limechain.tech>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip551/AtomicBatchTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/hip551/AtomicBatchTest.java
@@ -17,15 +17,29 @@
 package com.hedera.services.bdd.suites.hip551;
 
 import static com.hedera.services.bdd.spec.HapiSpec.hapiTest;
+import static com.hedera.services.bdd.spec.HapiSpec.propertyPreservingHapiTest;
 import static com.hedera.services.bdd.spec.queries.QueryVerbs.getAccountBalance;
+import static com.hedera.services.bdd.spec.queries.QueryVerbs.getTxnRecord;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.atomicBatch;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCall;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
+import static com.hedera.services.bdd.spec.transactions.TxnVerbs.uploadInitCode;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.overriding;
+import static com.hedera.services.bdd.spec.utilops.UtilVerbs.usableTxnIdNamed;
+import static com.hedera.services.bdd.suites.HapiSuite.MAX_CALL_DATA_SIZE;
 import static com.hedera.services.bdd.suites.HapiSuite.ONE_HBAR;
 
 import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.junit.HapiTestLifecycle;
+
+import java.util.List;
 import java.util.stream.Stream;
+
+import com.hedera.services.bdd.junit.LeakyHapiTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
 
 @HapiTestLifecycle
 public class AtomicBatchTest {
@@ -35,17 +49,52 @@ public class AtomicBatchTest {
     // disabled for now because there is no handler logic and streamValidation is failing in CI
     public Stream<DynamicTest> simpleBatchTest() {
         return hapiTest(
-                //                cryptoCreate("PAYER").balance(ONE_HBAR),
-                // submit batch with HapiTxnOp
-                atomicBatch(cryptoCreate("PAYER").balance(ONE_HBAR)),
-                //                        cryptoCreate("SENDER").balance(1L)),
+                cryptoCreate("dummy").balance(ONE_HBAR),
+                usableTxnIdNamed("inner").payerId("dummy"),
+                atomicBatch(cryptoCreate("PAYER")
+                                // .setNode("0.0.3")
+                                .txnId("inner")
+                                .balance(ONE_HBAR)
+                                .payingWith("dummy")
+                                .batchKey("dummy"))
+                        //                        .payingWith("dummy")
+                        .via("batchTxn"),
+                getTxnRecord("batchTxn").andAllChildRecords().hasChildRecordCount(0),
+                getTxnRecord("inner").assertingNothingAboutHashes().logged(),
+                getAccountBalance("PAYER").logged());
+    }
 
-                // submit batch with Transaction
-                //                withOpContext((spec, opLog) -> {
-                //                    var txn = buildTxnFrom(spec, cryptoCreate("PAYER").balance(ONE_HBAR));
-                //                    var batch1 = atomicBatch(txn);
-                //                    allRunFor(spec, batch1);
-                //                }),
-                getAccountBalance("PAYER").hasTinyBars(ONE_HBAR));
+    @Nested
+    @DisplayName("Batch Constraints - POSITIVE")
+    class BatchConstraintsPositive {
+
+        @LeakyHapiTest
+        @DisplayName("Bach contract call with almost gas limit")
+        //  BATCH_03
+        public Stream<DynamicTest> contractCallGasLimit() {
+            final var contract = "CalldataSize";
+            final var function = "callme";
+            final var payload = new byte[100];
+            return propertyPreservingHapiTest(
+                    List.of("contracts.maxGasPerSec"),
+                    overriding("contracts.maxGasPerSec", "2000000"),
+                    uploadInitCode(contract),
+                    contractCreate(contract),
+                    atomicBatch(contractCall(contract, function, payload).gas(2000000)));
+        }
+
+        @HapiTest
+        @DisplayName("Bach contract call with 6kb payload")
+        //  BATCH_04
+        public Stream<DynamicTest> contractCallTxnSizeLimit() {
+            final var contract = "CalldataSize";
+            final var function = "callme";
+            // Adjust the payload size with 512 bytes, so the total size is just under 6kb
+            final var payload = new byte[MAX_CALL_DATA_SIZE - 512];
+            return hapiTest(
+                    uploadInitCode(contract),
+                    contractCreate(contract),
+                    atomicBatch(contractCall(contract, function, payload)));
+        }
     }
 }
