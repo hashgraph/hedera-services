2c007f9cec8388ac22fc2a2ca3fd8aee07b4c3e6
lukelee-sl <109538178+lukelee-sl@users.noreply.github.com>
2025-01-31 08:18:47 -0800

    chore: consolidate contracts related test into single package (#17632)
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hips/hip904/TokenAirdropBase.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/hips/hip904/TokenAirdropBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Hedera Hashgraph, LLC
+ * Copyright (C) 2024-2025 Hedera Hashgraph, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.hedera.services.bdd.suites.hip904;
+package com.hedera.services.bdd.suites.contract.hips.hip904;
 
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.contractCreate;
 import static com.hedera.services.bdd.spec.transactions.TxnVerbs.cryptoCreate;
