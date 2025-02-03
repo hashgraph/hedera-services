7c6bf397478c3baac22e4d40e82a21970c23396a
Michael Tinker <michael.tinker@swirldslabs.com>
2025-02-03 14:31:18 -0600

    chore: Update `HintsLibrary` for CRS mgmt functions (#17673)
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/test/java/com/hedera/node/app/hints/impl/HintsLibraryImplTest.java
+++ hedera-node/hedera-app/src/test/java/com/hedera/node/app/hints/impl/HintsLibraryImplTest.java
@@ -16,17 +16,23 @@
 
 package com.hedera.node.app.hints.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.hedera.pbj.runtime.io.buffer.Bytes;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
-class FakeHintsLibraryTest {
-    private final FakeHintsLibrary subject = new FakeHintsLibrary();
+class HintsLibraryImplTest {
+    private final HintsLibraryImpl subject = new HintsLibraryImpl();
 
     @Test
     void nothingSupportedYet() {
+        assertThrows(UnsupportedOperationException.class, () -> subject.newCrs(1));
+        assertThrows(UnsupportedOperationException.class, () -> subject.updateCrs(Bytes.EMPTY, Bytes.EMPTY));
+        assertThrows(
+                UnsupportedOperationException.class,
+                () -> subject.verifyCrsUpdate(Bytes.EMPTY, Bytes.EMPTY, Bytes.EMPTY));
+
         assertThrows(UnsupportedOperationException.class, subject::newBlsKeyPair);
         assertThrows(UnsupportedOperationException.class, () -> subject.computeHints(Bytes.EMPTY, 1, 1));
         assertThrows(UnsupportedOperationException.class, () -> subject.validateHintsKey(Bytes.EMPTY, 1, 1));
