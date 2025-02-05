abcd109e1b20457c18f3b83cdc74ec6215830905
Luke Lee <luke.lee@hashgraph.com>
2025-02-04 19:42:01 -0800

    feat: add bdd test to verify versioning
    Signed-off-by: Luke Lee <luke.lee@hashgraph.com>

---
+++ hedera-node/hedera-smart-contract-service-impl/src/main/java/com/hedera/node/app/service/contract/impl/exec/systemcontracts/hts/address_0x16c/tokentype/TokenTypeCall.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2023-2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokentype;
+
+import static com.hedera.hapi.node.base.ResponseCodeEnum.SUCCESS;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.FullResult.revertResult;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.FullResult.successResult;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.common.Call.PricedResult.gasOnly;
+import static com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.tokentype.TokenTypeTranslator.TOKEN_TYPE;
+import static java.util.Objects.requireNonNull;
+
+import com.esaulpaugh.headlong.abi.Tuple;
+import com.hedera.hapi.node.base.ResponseCodeEnum;
+import com.hedera.hapi.node.state.token.Token;
+import com.hedera.node.app.service.contract.impl.exec.gas.SystemContractGasCalculator;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.FullResult;
+import com.hedera.node.app.service.contract.impl.exec.systemcontracts.hts.AbstractNonRevertibleTokenViewCall;
+import com.hedera.node.app.service.contract.impl.hevm.HederaWorldUpdater;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+public class TokenTypeCall extends AbstractNonRevertibleTokenViewCall {
+    private final boolean isStaticCall;
+
+    public TokenTypeCall(
+            @NonNull final SystemContractGasCalculator gasCalculator,
+            @NonNull final HederaWorldUpdater.Enhancement enhancement,
+            final boolean isStaticCall,
+            @Nullable final Token token) {
+        super(gasCalculator, enhancement, token);
+        this.isStaticCall = isStaticCall;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected @NonNull PricedResult resultOfViewingToken(@NonNull final Token token) {
+        requireNonNull(token);
+        return gasOnly(
+                fullResultsFor(
+                        SUCCESS,
+                        gasCalculator.viewGasRequirement(),
+                        token.tokenType().protoOrdinal()),
+                SUCCESS,
+                true);
+    }
+
+    @Override
+    protected @NonNull FullResult viewCallResultWith(
+            @NonNull final ResponseCodeEnum status, final long gasRequirement) {
+        return fullResultsFor(status, gasRequirement, 0);
+    }
+
+    private @NonNull FullResult fullResultsFor(
+            @NonNull final ResponseCodeEnum status, final long gasRequirement, final int tokenType) {
+        // For backwards compatibility, we need to revert here per issue #8746.
+        if (isStaticCall && status != SUCCESS) {
+            return revertResult(status, gasRequirement);
+        }
+        return successResult(
+                TOKEN_TYPE.getOutputs().encode(Tuple.of(status.protoOrdinal(), tokenType)), gasRequirement);
+    }
+}
---
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/contract/precompile/address_16c/TokenTypeTest.java
@@ -0,0 +1,4 @@
+package com.hedera.services.bdd.suites.contract.address_16c;
+
+public class TokenTypeTest {
+}
