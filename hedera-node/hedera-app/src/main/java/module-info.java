761d9988734c13ac63fb5018866df4d01026edb4
Michael Tinker <michael.tinker@swirldslabs.com>
2025-02-07 10:19:14 -0600

    spotless
    Signed-off-by: Michael Tinker <michael.tinker@swirldslabs.com>

--- hedera-node/hedera-app/src/main/java/module-info.java
+++ hedera-node/hedera-app/src/main/java/module-info.java
@@ -120,7 +120,6 @@ module com.hedera.node.app {
     exports com.hedera.node.app.workflows.handle.metric;
     exports com.hedera.node.app.roster;
     exports com.hedera.node.app.tss;
-    exports com.hedera.node.app.statedumpers;
     exports com.hedera.node.app.workflows.handle.stack;
     exports com.hedera.node.app.fees.congestion;
     exports com.hedera.node.app.throttle.annotations;
