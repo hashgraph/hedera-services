44c46ab8efa884d3bc3edd9811eb8e9317084231
Tim Farber-Newman <tim.farber-newman@swirldslabs.com>
2025-02-13 09:05:36 -0600

    spotless
    Signed-off-by: Tim Farber-Newman <tim.farber-newman@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2025 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.hedera.services.bdd.suites.regression;
 
 import static com.hedera.services.bdd.junit.TestTags.ADHOC;
@@ -26,7 +42,6 @@ public class AncientAgeBirthRoundTest implements LifecycleTest {
         return hapiTest(
                 burstOfTps(MIXED_OPS_BURST_TPS, MIXED_OPS_BURST_DURATION),
                 upgradeToNextConfigVersion(Map.of("event.useBirthRoundAncientThreshold", "true")),
-                burstOfTps(MIXED_OPS_BURST_TPS, MIXED_OPS_BURST_DURATION)
-        );
+                burstOfTps(MIXED_OPS_BURST_TPS, MIXED_OPS_BURST_DURATION));
     }
 }
