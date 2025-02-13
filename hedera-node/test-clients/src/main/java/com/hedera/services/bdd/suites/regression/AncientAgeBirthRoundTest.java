70a26c5d6fc269f3ac6aca1f5a58aeeeda08d6df
Tim Farber-Newman <tim.farber-newman@swirldslabs.com>
2025-02-13 08:53:43 -0600

    disable test; cleanup
    Signed-off-by: Tim Farber-Newman <tim.farber-newman@swirldslabs.com>

--- hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
+++ hedera-node/test-clients/src/main/java/com/hedera/services/bdd/suites/regression/AncientAgeBirthRoundTest.java
@@ -8,7 +8,7 @@ import com.hedera.services.bdd.junit.HapiTest;
 import com.hedera.services.bdd.suites.regression.system.LifecycleTest;
 import java.util.Map;
 import java.util.stream.Stream;
-import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Tag;
 
@@ -16,8 +16,13 @@ import org.junit.jupiter.api.Tag;
 public class AncientAgeBirthRoundTest implements LifecycleTest {
 
     @HapiTest
-    @DisplayName("Upgrade to enable birth round ancient age")
+    @Disabled("Disabled due to expected failures while birth round ancient age is completed")
     final Stream<DynamicTest> upgradeToUseBirthRounds() {
+        /*
+        Note: This test should be run in subprocess mode. This is done by executing it with the 'testSubprocess' task:
+        :test-clients:testSubprocess --tests "com.hedera.services.bdd.suites.regression.AncientAgeBirthRoundTest"
+         */
+
         return hapiTest(
                 burstOfTps(MIXED_OPS_BURST_TPS, MIXED_OPS_BURST_DURATION),
                 upgradeToNextConfigVersion(Map.of("event.useBirthRoundAncientThreshold", "true")),
--- platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/state/BirthRoundStateMigration.java
+++ platform-sdk/swirlds-platform-core/src/main/java/com/swirlds/platform/state/BirthRoundStateMigration.java
@@ -104,10 +104,7 @@ public final class BirthRoundStateMigration {
                 consensusSnapshot.consensusTimestamp());
         writablePlatformState.setSnapshot(modifiedConsensusSnapshot);
 
-        System.out.println("A.1 hash: " + state.getHash());
         state.invalidateHash();
-        System.out.println("A.2 hash: " + state.getHash());
         MerkleCryptoFactory.getInstance().digestTreeSync(state);
-        System.out.println("A.3 hash: " + state.getHash());
     }
 }
